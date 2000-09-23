de17c6d32b12e9687ba06d09c78c723c583e384d
unknown <tim@threads.polyesthetic.msg>
2000-09-23 12:39:40 -0400

    safe_mysqld.sh	remove stray warning message from nohup nice configure.in	the & in 2>&1 gets replaced by sed during AC_SUBST
    configure.in:
      the & in 2>&1 gets replaced by sed during AC_SUBST
    scripts/safe_mysqld.sh:
      remove stray warning message from nohup nice

--- scripts/safe_mysqld.sh
+++ scripts/safe_mysqld.sh
@@ -91,7 +91,7 @@ fi
 NOHUP_NICENESS="nohup"
 if test -w /
 then
-  NOHUP_NICENESS=`nohup nice`
+  NOHUP_NICENESS=`nohup nice 2>&1`
  if test $? -eq 0 && test x"$NOHUP_NICENESS" != x0 && nice --1 echo foo > /dev/null 2>&1; then
     NOHUP_NICENESS="nice --$NOHUP_NICENESS nohup"
   else
