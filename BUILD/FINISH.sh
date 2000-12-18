27336dfe8fbd4e12b06e99778de5d8c1c7e837f0
unknown <tim@cane.mysql.fi>
2000-12-18 23:55:47 +0200

    Pass arguements to the build script on to configure.
    BUILD/FINISH.sh:
      - pass arguments to the build script on to configure

--- BUILD/FINISH.sh
+++ BUILD/FINISH.sh
@@ -1,6 +1,10 @@
 cflags="$c_warnings $extra_flags"
 cxxflags="$cxx_warnings $base_cxxflags $extra_flags"
 configure="./configure $base_configs $extra_configs"
+for arg in "$@"; do
+  configure="$configure "`echo "$arg" | sed -e 's,\([^a-zA-Z0-9_.-]\),\\\\\1,g'`
+done
+
 
 CFLAGS="$cflags" CXX=gcc CXXFLAGS="$cxxflags" eval "$configure"
 
