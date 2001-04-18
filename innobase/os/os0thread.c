9d36f27bb021a3496e3b08c91f62a27daaad7664
unknown <monty@donna.mysql.fi>
2001-04-18 23:47:11 +0300

    Added test for PTHREAD_YIELD Removed test of default master parameter Don't lock locked tables in REPAIR Changed optimzation for SELECT * from table,table ORDER BY keypart LIMIT
    BitKeeper/deleted/.del-have_default_master.inc~a54c86e65a6c4af:
      Delete: mysql-test/include/have_default_master.inc
    BitKeeper/deleted/.del-have_default_master.require~1465255ffdaf82f:
      Delete: mysql-test/r/have_default_master.require
    Docs/manual.texi:
      Changelog for 3.23.38
    acconfig.h:
      Added test for PTHREAD_YIELD
    acinclude.m4:
      Added test for PTHREAD_YIELD
    configure.in:
      Added test for PTHREAD_YIELD
    innobase/os/os0thread.c:
      Added test for PTHREAD_YIELD
    mysql-test/r/lock.result:
      Added test of lock bug
    mysql-test/t/lock.test:
      Added test of lock bug
    mysql-test/t/rpl000014.test:
      Removed test of default master parameter
    mysql-test/t/rpl000015.test:
      Removed test of default master parameter
    mysql-test/t/rpl000016.test:
      Removed test of default master parameter
    sql/ha_myisam.cc:
      Don't lock locked tables in REPAIR
    sql/sql_select.cc:
      Changed optimzation for SELECT * from table,table ORDER BY keypart LIMIT

--- innobase/os/os0thread.c
+++ innobase/os/os0thread.c
@@ -137,8 +137,10 @@ os_thread_yield(void)
 {
 #if defined(__WIN__)
 	Sleep(0);
-#elif defined(HAVE_PTHREAD_YIELD)
+#elif defined(HAVE_PTHREAD_YIELD_ZERO_ARG)
 	pthread_yield();
+#elif defined(HAVE_PTHREAD_YIELD_ONE_ARG)
+	pthread_yield(0);
 #else
         os_thread_sleep(0);
 #endif
