9ec46a996c4cfa7f2528a29c43cda2d29ec35bc7
unknown <tonu@x153.internalnet>
2001-08-29 15:37:29 +0800

    Typo fix

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -2605,7 +2605,7 @@ static struct option long_options[] = {
   {"master-port",           required_argument, 0, (int) OPT_MASTER_PORT},
   {"master-connect-retry",  required_argument, 0, (int) OPT_MASTER_CONNECT_RETRY},
   {"master-info-file",      required_argument, 0, (int) OPT_MASTER_INFO_FILE},
-#ifdef HAVE_SSL  
+#ifdef HAVE_OPENSSL  
   {"master-ssl",      	    optional_argument, 0, (int) OPT_MASTER_SSL},
   {"master-ssl-key",        optional_argument, 0, (int) OPT_MASTER_SSL_KEY},
   {"master-ssl-cert",       optional_argument, 0, (int) OPT_MASTER_SSL_CERT},
