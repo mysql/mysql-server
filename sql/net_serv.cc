421e64ae356a1633ea50294b48c5025ea387a64d
unknown <monty@hundin.mysql.fi>
2001-08-31 10:30:38 +0300

    ix compilition for computer in a previous timezone
    Build-tools/Do-compile:
      Fix compilition for computer in a previous timezone
    sql/net_serv.cc:
      Added comment
    sql/sql_parse.cc:
      Cleanup

--- sql/net_serv.cc
+++ sql/net_serv.cc
@@ -177,7 +177,7 @@ static my_bool net_realloc(NET *net, ulong length)
 void net_clear(NET *net)
 {
 #ifndef EXTRA_DEBUG
-  int count;
+  int count;					// One may get 'unused' warning
   bool is_blocking=vio_is_blocking(net->vio);
   if (is_blocking)
     vio_blocking(net->vio, FALSE);
