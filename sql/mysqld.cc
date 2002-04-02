a2e75474b51e41446af02081ba2917f0f296aee2
unknown <jani@hynda.mysql.fi>
2002-04-02 20:29:53 +0300

    Changed mysql, mysqladmin, mysqlshow, mysqldump, mysqlimport, mysqlcheck and myisamchk to use new my_getopt struct.
    client/client_priv.h:
      Changes for my_getopt
    client/mysql.cc:
      Uses new my_getopt
    client/mysqladmin.c:
      Uses new my_getopt
    client/mysqlcheck.c:
      Uses new my_getopt
    client/mysqldump.c:
      Uses new my_getopt
    client/mysqlimport.c:
      Uses new my_getopt
    client/mysqlshow.c:
      Uses new my_getopt
    include/my_getopt.h:
      Added GET_BOOL type
    include/sslopt-case.h:
      This is shouldn't be needed anymore.
    include/sslopt-longopts.h:
      Uses new my_getopt
    myisam/myisamchk.c:
      Uses new my_getopt
    mysys/my_getopt.c:
      Some bug fixes and small feature adds to my_getopt
    sql/mysqld.cc:
      mysqld.cc will be changed next. sslopt-* had to be removed
      temporarily.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -2832,7 +2832,9 @@ static struct option long_options[] = {
   {"socket",                required_argument, 0, (int) OPT_SOCKET},
   {"sql-bin-update-same",   no_argument,       0, (int) OPT_SQL_BIN_UPDATE_SAME},
   {"sql-mode",              required_argument, 0, (int) OPT_SQL_MODE},
+#ifdef TO_BE_DONE
 #include "sslopt-longopts.h"
+#endif
 #ifdef __WIN__
   {"standalone",            no_argument,       0, (int) OPT_STANDALONE},
 #endif
@@ -3992,8 +3994,10 @@ static void get_options(int argc,char **argv)
       strmov(mysql_charsets_dir, optarg);
       charsets_dir = mysql_charsets_dir;
       break;
+#ifdef TO_BE_DONE
 #include "sslopt-case.h"
     case OPT_DES_KEY_FILE:
+#endif
 #ifdef HAVE_OPENSSL
       des_key_file=optarg;
 #endif
