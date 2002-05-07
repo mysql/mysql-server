9f8a75a168774f9ada4b2b68fc494ac514d84388
unknown <jani@rhols221.adsl.netsonic.fi>
2002-05-07 20:35:06 +0300

    Fixed some bugs in my_getopt.c, added functionality for new GET_* types, migrated mysqld.cc to use my_getopt.
    include/my_getopt.h:
      
      Added missing types; GET_INT, GET_UINT, GET_ULONG, GET_ULL
    mysys/my_getopt.c:
      - Fixed a bug in processing short options; variable value was set
        wrongly after processing it in get_one_option(), when it needed
        to be done before it.
      - Fixed a bug in setting variable values; if type was OPT_ARG,
        a call without argument destroyed the default value
      - Added functionality for new GET_* types.
    sql/mysqld.cc:
      Changed mysqld.cc to use my_getopt.

--- include/my_getopt.h
+++ include/my_getopt.h
@@ -16,7 +16,8 @@
 
 C_MODE_START
 
-enum get_opt_var_type { GET_NO_ARG, GET_BOOL, GET_LONG, GET_LL, GET_STR };
+enum get_opt_var_type { GET_NO_ARG, GET_BOOL, GET_INT, GET_UINT, GET_LONG,
+			GET_ULONG, GET_LL, GET_ULL, GET_STR };
 enum get_opt_arg_type { NO_ARG, OPT_ARG, REQUIRED_ARG };
 
 struct my_option
