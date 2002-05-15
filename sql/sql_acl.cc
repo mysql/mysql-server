dc2b6e456fb16685a4191537bf83e1161cb28d93
unknown <monty@hundin.mysql.fi>
2002-05-15 18:41:36 +0300

    Fixed some things to make MySQL to compile again.
    libmysqld/lib_sql.cc:
      Fix to make the embedded server compile with the new USER_REOSURCEs.
    sql/sql_acl.cc:
      Fixed wrong test in password checking.
    sql/sql_cache.cc:
      Fixed wrong cast
    sql/sql_parse.cc:
      Cleanup

--- sql/sql_acl.cc
+++ sql/sql_acl.cc
@@ -1180,19 +1180,16 @@ static int replace_user_table(THD *thd, TABLE *table, const LEX_USER &combo,
   char *password,empty_string[1];
   DBUG_ENTER("replace_user_table");
 
+  password=empty_string;
+  empty_string[0]=0;
   if (combo.password.str && combo.password.str[0])
   {
-    if (combo.password.length <= HASH_PASSWORD_LENGTH)
+    if (combo.password.length != HASH_PASSWORD_LENGTH)
     {
       send_error(&thd->net, ER_PASSWORD_NO_MATCH);
       DBUG_RETURN(1);
     }
     password=combo.password.str;
-  }
-  else
-  {
-    password=empty_string;
-    empty_string[0]=0;
   }
 
   table->field[0]->store(combo.host.str,combo.host.length);
