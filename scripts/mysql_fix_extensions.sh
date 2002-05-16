e1288e07ac54d38390619c4f71c1c17be6eac9d0
arjen@fred.bitbike.com <>
2002-05-16 10:13:42 +1000

    mysql_fix_extensions.sh tool copied from 4.0 tree (referenced in manual).

---
+++ scripts/mysql_fix_extensions.sh
@@ -0,0 +1,16 @@
+#!@PERL@
+# This is a utility for MySQL. It is not needed by any standard part
+# of MySQL.
+
+# Usage: mysql_fix_extentions datadir
+# does not work with RAID, with InnoDB or BDB tables
+# makes .frm lowercase and .MYI/MYD/ISM/ISD uppercase
+# useful when datafiles are copied from windows
+
+die "Usage: $0 datadir\n" unless -d $ARGV[0];
+
+for $a (<$ARGV[0]/*/*.*>) { $_=$a;
+  s/\.frm$/.frm/i;
+  s/\.(is[md]|my[id])$/\U$&/i;
+  rename ($a, $_) || warn "Cannot rename $a => $_ : $!";
+}
