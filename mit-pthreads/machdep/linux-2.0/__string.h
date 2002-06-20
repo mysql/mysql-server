6ccf4e50a20c964f2abe7f987769089e94083dd7
monty@hundin.mysql.fi <>
2002-06-20 23:26:39 +0300

    Fixes for compiling distribution with MIT-threads

--- mit-pthreads/machdep/linux-2.0/__string.h
+++ mit-pthreads/machdep/linux-2.0/__string.h
@@ -5,7 +5,6 @@ typedef pthread_size_t  size_t;
 #endif
 
 /* Non-standard Linux string routines. */
-#if !defined(_ANSI_SOURCE) && !defined(_POSIX_SOURCE)
 __BEGIN_DECLS
 int	 bcmp __P_((const void *, const void *, size_t));
 void	 bcopy __P_((const void *, void *, size_t));
@@ -15,4 +14,3 @@ char	*rindex __P_((const char *, int));
 char	*strdup __P_((const char *));
 char	*strsep __P_((char **, const char *));
 __END_DECLS
-#endif 
