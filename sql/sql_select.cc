712f4fb047413536253fbe40750fa066190d81c1
Sinisa@sinisa.nasamreza.org <>
2002-07-29 17:05:41 +0300

    lot's of small changes

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -4037,9 +4037,12 @@ static bool create_myisam_tmp_table(TABLE *table,TMP_TABLE_PARAM *param,
   }
   MI_CREATE_INFO create_info;
   bzero((char*) &create_info,sizeof(create_info));
-  if ((options & (OPTION_BIG_TABLES | SELECT_SMALL_RESULT)) ==
-      OPTION_BIG_TABLES)
+  if ((options & (OPTION_BIG_TABLES | SELECT_SMALL_RESULT)) !=
+      SELECT_SMALL_RESULT)
+  {
     create_info.data_file_length= ~(ulonglong) 0;
+    create_info.max_rows= ~(ulonglong) 0;
+  }
 
   if ((error=mi_create(table->real_name,table->keys,&keydef,
 		       (uint) (param->recinfo-param->start_recinfo),
