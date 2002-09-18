f638ee6df91a32ed767fa67cf643dfa07f9a82d4
monty@mashka.mysql.fi <>
2002-09-18 21:04:49 +0300

    Added code to flush a bulk_insert index. This fixes a bug when doing multi-row inserts on table with an auto_increment key that is not in the first key segment.

--- include/my_base.h
+++ include/my_base.h
@@ -107,6 +107,7 @@ enum ha_extra_function {
   HA_EXTRA_NO_IGNORE_DUP_KEY,
   HA_EXTRA_DONT_USE_CURSOR_TO_UPDATE,	/* Cursor will not be used for update */
   HA_EXTRA_BULK_INSERT_BEGIN,
+  HA_EXTRA_BULK_INSERT_FLUSH,		/* Flush one index */
   HA_EXTRA_BULK_INSERT_END,
   HA_EXTRA_PREPARE_FOR_DELETE
 };
