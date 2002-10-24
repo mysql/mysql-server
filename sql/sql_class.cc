27d11e85b234de7ec48d57d5720c9871654adb2f
unknown <nick@mysql.com>
2002-10-24 16:48:34 -0600

    Added Rand_log_event

--- sql/sql_class.cc
+++ sql/sql_class.cc
@@ -81,7 +81,7 @@ static void free_var(user_var_entry *entry)
 
 THD::THD():user_time(0), fatal_error(0),
 	   last_insert_id_used(0),
-	   insert_id_used(0), in_lock_tables(0),
+	   insert_id_used(0), rand_used(0), in_lock_tables(0),
 	   global_read_lock(0), bootstrap(0)
 {
   host=user=priv_user=db=query=ip=0;
