7777d2b819f05281d98cffcd15fdd0e166d0f391
bell@sanja.is.com.ua <>
2002-10-27 21:29:40 +0200

    fixed union rexecution bug

--- sql/sql_lex.cc
+++ sql/sql_lex.cc
@@ -945,7 +945,7 @@ void st_select_lex_unit::init_query()
   select_limit_cnt= HA_POS_ERROR;
   offset_limit_cnt= 0;
   union_option= 0;
-  prepared= optimized= 0;
+  prepared= optimized= executed= 0;
   item= 0;
 }
 
