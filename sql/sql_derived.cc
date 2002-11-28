8a53d1ca6a3e1e3d93953aeb699e964bb9941c65
Sinisa@sinisa.nasamreza.org <>
2002-11-28 19:19:21 +0200

    adding mandatory aliases for derived tables as per SQL-99 doing it in sql_yacc.yy does not work this commit contains fixes for other tests there are fixes in subselect test which will have to be changed once fix_fields is fixed to work with aliased derived tables

--- sql/sql_derived.cc
+++ sql/sql_derived.cc
@@ -108,7 +108,7 @@ int mysql_derived(THD *thd, LEX *lex, SELECT_LEX_UNIT *unit, TABLE_LIST *t)
 	  table->tmp_table=TMP_TABLE;
 	  if (!lex->describe)
 	    sl->exclude();
-	  t->db=(char *)"";
+	  t->db=(tables && tables->db) ? tables->db : (char *)"";
 	  t->derived=(SELECT_LEX *)0; // just in case ...
 	}
       }
