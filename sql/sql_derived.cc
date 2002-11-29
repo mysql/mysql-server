1b921a995e3defb62aa51fc09fc4eaed9b0f4eab
unknown <Sinisa@sinisa.nasamreza.org>
2002-11-29 14:49:07 +0200

    many bug fixes
    mysql-test/r/derived.result:
      test for a bug fix which caused a deadlock !
      Also test for a bug fix with using() on derived tables
    mysql-test/r/subselect.result:
      correcting previous changes caused by a bug ..
    mysql-test/t/derived.test:
      test for a bug fix which caused a deadlock !
      Also test for a bug fix with using() on derived tables
    mysql-test/t/subselect.test:
      corrections of previous changes caused by a bug ..
    sql/sql_base.cc:
      A for for a bug which caused a deadlock !
      Also fix for a bug with using() on derived tables
    sql/sql_derived.cc:
      code cleanup
    sql/sql_lex.cc:
      fix for deadlock

--- sql/sql_derived.cc
+++ sql/sql_derived.cc
@@ -106,9 +106,9 @@ int mysql_derived(THD *thd, LEX *lex, SELECT_LEX_UNIT *unit, TABLE_LIST *t)
 	  t->table=table;
 	  table->derived_select_number= sl->select_number;
 	  table->tmp_table=TMP_TABLE;
-	  if (!lex->describe)
+	  if (lex->describe)
 	    sl->exclude();
-	  t->db=(tables && tables->db) ? tables->db : (char *)"";
+	  t->db=(char *)"";
 	  t->derived=(SELECT_LEX *)0; // just in case ...
 	}
       }
