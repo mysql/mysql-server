5f1b9717c53f19ef4f7a5206f59a7678a025dbe2
unknown <monty@mashka.mysql.fi>
2002-12-21 10:45:06 +0200

    Optimization of CASE bug fix

--- sql/item_cmpfunc.cc
+++ sql/item_cmpfunc.cc
@@ -685,10 +685,9 @@ String *Item_func_case::val_str(String *str)
     null_value=1;
     return 0;
   }
+  null_value= 0;
   if (!(res=item->val_str(str)))
-    null_value=1;
-  else
-    null_value=item->null_value;
+    null_value= 1;
   return res;
 }
 
