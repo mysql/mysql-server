885d9d903721ba9c11098ea4df4d6d4de6560ae5
Sinisa@sinisa.nasamreza.org <>
2002-12-31 18:01:53 +0200

    A fix for a charset bug in the select_export (select into outfile) class.

--- sql/item.cc
+++ sql/item.cc
@@ -220,6 +220,7 @@ String *Item_field::str_result(String *str)
 {
   if ((null_value=result_field->is_null()))
     return 0;
+  str->set_charset(str_value.charset());
   return result_field->val_str(str,&str_value);
 }
 
