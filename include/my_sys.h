fe47e5358c61b9cf78aa0ed515a74f9537834114
unknown <bar@bar.mysql.r18.ru>
2003-01-08 15:39:15 +0400

    New function to search for charset using charset name rather than collation name

--- include/my_sys.h
+++ include/my_sys.h
@@ -206,6 +206,7 @@ extern const char *get_charset_name(uint cs_number);
 extern CHARSET_INFO *get_charset(uint cs_number, myf flags);
 extern my_bool set_default_charset(uint cs, myf flags);
 extern CHARSET_INFO *get_charset_by_name(const char *cs_name, myf flags);
+extern CHARSET_INFO *get_charset_by_csname(const char *cs_name, myf flags);
 extern my_bool set_default_charset_by_name(const char *cs_name, myf flags);
 extern void free_charsets(void);
 extern char *list_charsets(myf want_flags); /* my_free() this string... */
