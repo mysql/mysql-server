77f5c5a7cf4e99fa3d760f1541bafec3b182def3
unknown <bell@sanja.is.com.ua>
2003-02-16 20:37:51 +0200

    fixed excluding st_select_lex from global select list fixed destroing not well constructed analize object fixed wrong detection of unions in derived tables
    sql/sql_analyse.h:
      fixed destroing not well constructed analize object
    sql/sql_derived.cc:
      fixed wrong detection of unions in derived tables
    sql/sql_lex.cc:
      fixed excluding st_select_lex from global select list

--- sql/sql_lex.cc
+++ sql/sql_lex.cc
@@ -1098,10 +1098,11 @@ void st_select_lex_node::fast_exclude()
   {
     if ((*link_prev= link_next))
       link_next->link_prev= link_prev;
-    // Remove slave structure
-    for (; slave; slave= slave->next)
-      slave->fast_exclude();
   }
+  // Remove slave structure
+  for (; slave; slave= slave->next)
+    slave->fast_exclude();
+  
 }
 
 /*
