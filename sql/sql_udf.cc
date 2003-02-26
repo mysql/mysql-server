4e1cc32318c685f76271faa22451093d2ddd1f90
bar@bar.mysql.r18.ru <>
2003-02-26 14:08:31 +0400

    sql_udf.cc, protocol.cc:   Less default_charset_info

--- sql/sql_udf.cc
+++ sql/sql_udf.cc
@@ -431,9 +431,9 @@ int mysql_create_function(THD *thd,udf_func *udf)
     goto err;
 
   restore_record(table,2);		// Get default values for fields
-  table->field[0]->store(u_d->name.str, u_d->name.length, default_charset_info);
+  table->field[0]->store(u_d->name.str, u_d->name.length, system_charset_info);
   table->field[1]->store((longlong) u_d->returns);
-  table->field[2]->store(u_d->dl,(uint) strlen(u_d->dl), default_charset_info);
+  table->field[2]->store(u_d->dl,(uint) strlen(u_d->dl), system_charset_info);
   if (table->fields >= 4)			// If not old func format
     table->field[3]->store((longlong) u_d->type);
   error = table->file->write_row(table->record[0]);
