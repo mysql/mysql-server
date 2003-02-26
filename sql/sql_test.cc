7e700e337444f3f70ad0decb4be0e97561c1cd0e
bar@bar.mysql.r18.ru <>
2003-02-26 14:30:39 +0400

    sql_test.cc:   Less default_charset_info

--- sql/sql_test.cc
+++ sql/sql_test.cc
@@ -52,7 +52,7 @@ print_where(COND *cond,const char *info)
   if (cond)
   {
     char buff[256];
-    String str(buff,(uint32) sizeof(buff), default_charset_info);
+    String str(buff,(uint32) sizeof(buff), system_charset_info);
     str.length(0);
     cond->print(&str);
     str.append('\0');
@@ -116,8 +116,8 @@ void print_cached_tables(void)
 void TEST_filesort(SORT_FIELD *sortorder,uint s_length)
 {
   char buff[256],buff2[256];
-  String str(buff,sizeof(buff),default_charset_info);
-  String out(buff2,sizeof(buff2),default_charset_info);
+  String str(buff,sizeof(buff),system_charset_info);
+  String out(buff2,sizeof(buff2),system_charset_info);
   const char *sep;
   DBUG_ENTER("TEST_filesort");
 
