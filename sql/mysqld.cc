5247c5bf515d5c1ef55b3d56ef87bc2d73628fe2
unknown <Sinisa@sinisa.nasamreza.org>
2003-03-04 18:23:15 +0200

    Fix for SHOW VARIABLES on 64-bit platforms

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -3162,7 +3162,7 @@ struct show_var_st init_vars[]= {
   {"record_rnd_buffer",       (char*) &record_rnd_cache_size,	    SHOW_LONG},
   {"query_buffer_size",       (char*) &query_buff_size,		    SHOW_LONG},
   {"safe_show_database",      (char*) &opt_safe_show_db,            SHOW_BOOL},
-  {"server_id",               (char*) &server_id,		    SHOW_LONG},
+  {"server_id",               (char*) &server_id,		    SHOW_INT},
   {"slave_net_timeout",       (char*) &slave_net_timeout,	    SHOW_LONG},
   {"skip_locking",            (char*) &my_disable_locking,          SHOW_MY_BOOL},
   {"skip_networking",         (char*) &opt_disable_networking,      SHOW_BOOL},
