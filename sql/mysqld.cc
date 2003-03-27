2e6c3197a931362d58c987098664783b2b8b54bf
unknown <heikki@hundin.mysql.fi>
2003-03-27 12:17:27 +0200

    mysqld.cc:   Make the default value of innodb_flush_log_at_trx_commit 1, not 0 which compromises the Durability of a transaction
    sql/mysqld.cc:
      Make the default value of innodb_flush_log_at_trx_commit 1, not 0 which compromises the Durability of a transaction

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -3293,7 +3293,7 @@ struct my_option my_long_options[] =
    "Set to 0 (write and flush once per second), 1 (write and flush at each commit) or 2 (write at commit, flush once per second)",
    (gptr*) &innobase_flush_log_at_trx_commit,
    (gptr*) &innobase_flush_log_at_trx_commit,
-   0, GET_UINT, OPT_ARG,  0, 0, 2, 0, 0, 0},
+   0, GET_UINT, OPT_ARG,  1, 0, 2, 0, 0, 0},
   {"innodb_flush_method", OPT_INNODB_FLUSH_METHOD,
    "With which method to flush data", (gptr*) &innobase_unix_file_flush_method,
    (gptr*) &innobase_unix_file_flush_method, 0, GET_STR, REQUIRED_ARG, 0, 0, 0,
