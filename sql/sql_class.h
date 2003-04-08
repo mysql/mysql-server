8c95b516254b3af107dc8d8407f864ebcce79ee5
unknown <bar@bar.mysql.r18.ru>
2003-04-08 14:38:17 +0500

    Use of CHARSWT_INFO *result_collation instead of bool convert_result_charset SET CHARACTER SET inststead of SET NAMES in tests

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -378,10 +378,10 @@ struct system_variables
   my_bool log_warnings;
   my_bool low_priority_updates;
   my_bool new_mode;
-  my_bool convert_result_charset;
-
+  
   CHARSET_INFO 	*client_collation;
   CHARSET_INFO  *literal_collation;
+  CHARSET_INFO  *result_collation;
 };
 
 void free_tmp_table(THD *thd, TABLE *entry);
@@ -663,8 +663,6 @@ public:
     DBUG_PRINT("error",("Fatal error set"));
   }
   inline CHARSET_INFO *charset() { return variables.client_collation; }
-  inline CHARSET_INFO *result_charset(CHARSET_INFO *cs)
-  { return variables.convert_result_charset ? charset() : cs; }
 };
 
 /*
