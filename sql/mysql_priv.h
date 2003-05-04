ac377a684ea89d62f395d5d804d9557ae255cf31
peter@mysql.com <>
2003-05-04 21:35:20 +0400

    Minor fix to previous code

--- sql/mysql_priv.h
+++ sql/mysql_priv.h
@@ -657,7 +657,10 @@ extern ulong binlog_cache_size, max_binlog_cache_size, open_files_limit;
 extern ulong max_binlog_size, rpl_recovery_rank, thread_cache_size;
 extern ulong com_stat[(uint) SQLCOM_END], com_other, back_log;
 extern ulong specialflag, current_pid;
+
+#ifdef HAVE_INNOBASE_DB
 extern ulong srv_max_buf_pool_modified_pct;
+#endif
 
 extern uint test_flags,select_errors,ha_open_options;
 extern uint protocol_version,dropping_tables;
