65335d37ff9d8ae046c70dcb16c7662b049fc456
bar@bar.mysql.r18.ru <>
2003-06-26 15:45:04 +0500

    New messages for 3-adic and N-adic operations REPLACE() now processed arguments collations according to standard

--- sql/item_func.cc
+++ sql/item_func.cc
@@ -33,7 +33,7 @@
 
 static void my_coll_agg_error(DTCollation &c1, DTCollation &c2, const char *fname)
 {
-  my_error(ER_CANT_AGGREGATE_COLLATIONS,MYF(0),
+  my_error(ER_CANT_AGGREGATE_2COLLATIONS,MYF(0),
   	   c1.collation->name,c1.derivation_name(),
 	   c2.collation->name,c2.derivation_name(),
 	   fname);
