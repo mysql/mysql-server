406b8ba21e6d3db2d5259549b5b4e11bb6c2876b
vva@eagle.mysql.r18.ru <>
2003-07-28 10:57:46 -0400

    changed for AUTO_VALUE_ON_ZERO

--- sql/sql_base.cc
+++ sql/sql_base.cc
@@ -2247,8 +2247,8 @@ fill_record(List<Item> &fields,List<Item> &values, bool ignore_errors)
     value=v++;
     Field *rfield= field->field;
     TABLE *table= rfield->table;
-    if (rfield==table->next_number_field)
-      table->auto_increment_field_is_null= false;
+    if (rfield == table->next_number_field)
+      table->auto_increment_field_not_null= true;
     if (value->save_in_field(rfield, 0) > 0 && !ignore_errors)
       DBUG_RETURN(1);
   }
@@ -2268,8 +2268,8 @@ fill_record(Field **ptr,List<Item> &values, bool ignore_errors)
   {
     value=v++;
     TABLE *table= field->table;
-    if (field==table->next_number_field)
-      table->auto_increment_field_is_null= false;
+    if (field == table->next_number_field)
+      table->auto_increment_field_not_null= true;
     if (value->save_in_field(field, 0) == 1 && !ignore_errors)
       DBUG_RETURN(1);
   }
