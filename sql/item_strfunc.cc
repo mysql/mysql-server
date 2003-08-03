e2a4c4a5d25c5f628415113c3fb12ea87905914a
unknown <monty@mashka.mysql.fi>
2003-08-03 16:33:04 +0300

    Cleaned up code comment

--- sql/item_strfunc.cc
+++ sql/item_strfunc.cc
@@ -1094,7 +1094,8 @@ String *Item_func_database::val_str(String *str)
 String *Item_func_user::val_str(String *str)
 {
   THD *thd=current_thd;
-  if (!(thd->user) || // for system threads (e.g. replication thread)
+  // For a replication thread user may be a null pointer (To be fixed)
+  if (!thd->user ||
       str->copy((const char*) thd->user,(uint) strlen(thd->user)) ||
       str->append('@') ||
       str->append(thd->host ? thd->host : thd->ip ? thd->ip : ""))
