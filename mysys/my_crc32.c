58bdc6f5e18671812942f4599d8bd99f4991f668
serg@serg.mylan <>
2003-08-21 18:30:43 +0200

    use crc32() from bundled zlib if system zlib is unavailable

---
+++ mysys/my_crc32.c
@@ -0,0 +1,36 @@
+/* Copyright (C) 2003 MySQL AB
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+#include "mysys_priv.h"
+
+#ifndef HAVE_COMPRESS
+
+/* minimal set of defines for using crc32() from zlib codebase */
+#define _ZLIB_H
+#define ZEXPORT
+#define Z_NULL  0
+#define OF(args)  args
+#undef DYNAMIC_CRC_TABLE
+typedef uchar  Byte;
+typedef uchar  Bytef;
+typedef uint   uInt;
+typedef ulong  uLong;
+typedef ulong  uLongf;
+
+#include "../zlib/crc32.c"
+
+#endif
+
