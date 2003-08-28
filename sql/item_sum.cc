3d2a3804a1e26906c3d0b1d969452465e97fa572
bell@laptop.sanja.is.com.ua <>
2003-08-28 03:10:14 +0300

    reset() split in 2 function clear() & add() aggregate function reinitialization (if no rows was found) made with help of clear() (fixed BUG#860)

--- sql/item_sum.cc
+++ sql/item_sum.cc
@@ -223,10 +223,9 @@ Item *Item_sum_sum::copy_or_same(THD* thd)
 }
 
 
-bool Item_sum_sum::reset()
+void Item_sum_sum::clear()
 {
   null_value=1; sum=0.0;
-  return Item_sum_sum::add();
 }
 
 
@@ -251,10 +250,9 @@ Item *Item_sum_count::copy_or_same(THD* thd)
 }
 
 
-bool Item_sum_count::reset()
+void Item_sum_count::clear()
 {
-  count=0;
-  return add();
+  count= 0;
 }
 
 
@@ -286,10 +284,9 @@ Item *Item_sum_avg::copy_or_same(THD* thd)
 }
 
 
-bool Item_sum_avg::reset()
+void Item_sum_avg::clear()
 {
   sum=0.0; count=0;
-  return Item_sum_avg::add();
 }
 
 
@@ -342,11 +339,10 @@ Item *Item_sum_variance::copy_or_same(THD* thd)
 }
 
 
-bool Item_sum_variance::reset()
+void Item_sum_variance::clear()
 {
   sum=sum_sqr=0.0; 
   count=0; 
-  return Item_sum_variance::add();
 }
 
 bool Item_sum_variance::add()
@@ -592,10 +588,9 @@ longlong Item_sum_bit::val_int()
 }
 
 
-bool Item_sum_bit::reset()
+void Item_sum_bit::clear()
 {
-  bits=reset_bits;
-  return add();
+  bits= reset_bits;
 }
 
 Item *Item_sum_or::copy_or_same(THD* thd)
@@ -1280,7 +1275,7 @@ Item *Item_sum_count_distinct::copy_or_same(THD* thd)
 }
 
 
-bool Item_sum_count_distinct::reset()
+void Item_sum_count_distinct::clear()
 {
   if (use_tree)
     reset_tree(tree);
@@ -1290,7 +1285,6 @@ bool Item_sum_count_distinct::reset()
     table->file->delete_all_rows();
     table->file->extra(HA_EXTRA_WRITE_CACHE);
   }
-  return add();
 }
 
 bool Item_sum_count_distinct::add()
@@ -1353,11 +1347,11 @@ longlong Item_sum_count_distinct::val_int()
 
 #ifdef HAVE_DLOPEN
 
-bool Item_udf_sum::clear()
+void Item_udf_sum::clear()
 {
-  DBUG_ENTER("Item_udf_sum::reset");
+  DBUG_ENTER("Item_udf_sum::clear");
   udf.clear();
-  DBUG_RETURN(0);
+  DBUG_VOID_RETURN;
 }
 
 bool Item_udf_sum::add()
@@ -1685,7 +1679,7 @@ Item *Item_func_group_concat::copy_or_same(THD* thd)
 }
 
 
-bool Item_func_group_concat::reset()
+void Item_func_group_concat::clear()
 {
   result.length(0);
   result.copy();
@@ -1699,7 +1693,6 @@ bool Item_func_group_concat::reset()
   }
   if (tree_mode)
     reset_tree(tree);
-  return add();
 }
 
 
