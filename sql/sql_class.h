6d17d7f5bb27cc07706fb8e368eafbcc2b06fde9
unknown <monty@narttu.mysql.fi>
2003-08-29 04:17:30 +0300

    Revert change of lower-case-table handling as this breaks how alias works. Changes like this should be done to 4.1, not to a stable release
    mysql-test/r/lowercase_table.result:
      Revert change of lower-case-table handling
    mysql-test/t/lowercase_table.test:
      Revert change of lower-case-table handling
    sql/sql_class.cc:
      Revert change of lower-case-table handling
    sql/sql_class.h:
      Revert change of lower-case-table handling
    sql/sql_parse.cc:
      Revert change of lower-case-table handling
    sql/sql_table.cc:
      Revert change of lower-case-table handling
    sql/sql_yacc.yy:
      Revert change of lower-case-table handling

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -762,8 +762,15 @@ class Table_ident :public Sql_alloc {
  public:
   LEX_STRING db;
   LEX_STRING table;
-  Table_ident(LEX_STRING db_arg,LEX_STRING table_arg,bool force);
-  Table_ident(LEX_STRING table_arg);
+  inline Table_ident(LEX_STRING db_arg,LEX_STRING table_arg,bool force)
+    :table(table_arg)
+  {
+    if (!force && (current_thd->client_capabilities & CLIENT_NO_SCHEMA))
+      db.str=0;
+    else
+      db= db_arg;
+  }
+  inline Table_ident(LEX_STRING table_arg) :table(table_arg) {db.str=0;}
   inline void change_db(char *db_name)
   { db.str= db_name; db.length=(uint) strlen(db_name); }
 };
