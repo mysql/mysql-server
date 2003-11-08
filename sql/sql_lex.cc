a88f4ea30f378234716e06d64dbc437babe79506
hf@deer.(none) <>
2003-11-08 16:43:16 +0400

    Fix for bug #1727 ( Crash in EXPLAIN & UNION & derived) another version of the fix. Here i removed a loop that seems to be superfluous

--- sql/sql_lex.cc
+++ sql/sql_lex.cc
@@ -1269,28 +1269,6 @@ bool st_select_lex::test_limit()
   return(0);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*  
   Interface method of table list creation for query
   
@@ -1315,13 +1293,8 @@ bool st_select_lex_unit::create_total_list(THD *thd, st_lex *lex,
 					   bool check_derived)
 {
   *result= 0;
-  for (SELECT_LEX_UNIT *unit= this; unit; unit= unit->next_unit())
-  {
-    if ((res= unit->create_total_list_n_last_return(thd, lex, &result,
-						    check_derived)))
-      return res;
-  }
-  return 0;
+  res= create_total_list_n_last_return(thd, lex, &result, check_derived);
+  return res;
 }
 
 /*  
