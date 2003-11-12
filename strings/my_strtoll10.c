82c0c6c42fa526fe5ebee22a8bea91845c67cfdd
greg@mysql.com <>
2003-11-12 02:25:01 -0500

    Resolved build problem: undeclared macros

--- strings/my_strtoll10.c
+++ strings/my_strtoll10.c
@@ -15,6 +15,7 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
 #include <my_global.h>
+#include <my_sys.h>
 #include <m_string.h>
 
 #define ULONGLONG_MAX		(~(ulonglong) 0)
