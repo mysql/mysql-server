b137384a38c938fd92416050d478187668aa38b1
jani@rhols221.adsl.netsonic.fi <>
2003-11-24 13:25:08 +0200

    Fixed Bug#1907, option of type GET_BOOL with arg type OPT_ARG did not call get_one_option().

--- mysys/my_getopt.c
+++ mysys/my_getopt.c
@@ -316,8 +316,6 @@ int handle_options(int *argc, char ***argv,
 	    else /* If argument differs from 0, enable option, else disable */
 	      *((my_bool*) optp->value)= (my_bool) atoi(optend) != 0;
 	  }
-	  (*argc)--;	    
-	  continue;
 	}
 	else if (optp->arg_type == REQUIRED_ARG && !optend)
 	{
