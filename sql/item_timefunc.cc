468dcb6092bce57a4c7529f40d10fcd7654f5ead
unknown <gluh@gluh.mysql.r18.ru>
2003-12-07 15:10:21 +0400

    WL#1175: more default_week_formats for iso compatibility
    New formats added for 'week()' function and 'default_week_format' option(4 - 7).
    Next formats is supported now:
    *Value* *Meaning*
      `0'     Week starts on Sunday; First Sunday of the year starts week 1.
      	  Week() returns 0-53.
      `1'     Week starts on Monday; Weeks numbered according to ISO 8601:1988.
    	  Week() returns 0-53.
      `2'     Week starts on Sunday; First Sunday of the year starts week 1.
      	  Week() returns 1-53.
      `3'     Week starts on Monday; Weeks numbered according to ISO 8601:1988.
    	  Week() returns 1-53.
      `4'     Week starts on Sunday; Weeks numbered according to ISO 8601:1988.
    	  Week() returns 0-53.
      `5'     Week starts on Monday;  First Monday of the year starts week 1.
      	  Week() returns 0-53.
      `6'     Week starts on Sunday; Weeks numbered according to ISO 8601:1988.
    	  Week() returns 1-53.
      `7'     Week starts on Monday;  First Monday of the year starts week 1.
      	  Week() returns 1-53.
    
    
    mysql-test/r/func_time.result:
      Test for 'default_week_format' option and 'week' function
    mysql-test/t/func_time.test:
      Test for 'default_week_format' option and 'week' function
    sql/item_timefunc.cc:
      WL#1175 more default_week_formats for iso compatibility
    sql/mysql_priv.h:
      WL#1175 more default_week_formats for iso compatibility
    sql/mysqld.cc:
      WL#1175 more default_week_formats for iso compatibility
    sql/time.cc:
      WL#1175 more default_week_formats for iso compatibility

--- sql/item_timefunc.cc
+++ sql/item_timefunc.cc
@@ -175,27 +175,51 @@ longlong Item_func_second::val_int()
 }
 
 
-/*
-  Returns the week of year.
+uint week_mode(uint mode)
+{
+  uint week_format= (mode & 7);
+  if (!(week_format & WEEK_MONDAY_FIRST))
+    week_format^= WEEK_FIRST_WEEKDAY;
+  return week_format;
+}
 
-  The bits in week_format has the following meaning:
-    0	If not set:	USA format: Sunday is first day of week
-        If set:		ISO format: Monday is first day of week
-    1   If not set:	Week is in range 0-53
-    	If set		Week is in range 1-53.
+/*
+  The bits in week_format(for calc_week() function) has the following meaning:
+   WEEK_MONDAY_FIRST (0)  If not set	Sunday is first day of week
+      		   	  If set	Monday is first day of week
+   WEEK_YEAR (1)	  If not set	Week is in range 0-53
+
+   	Week 0 is returned for the the last week of the previous year (for
+	a date at start of january) In this case one can get 53 for the
+	first week of next year.  This flag ensures that the week is
+	relevant for the given year. Note that this flag is only
+	releveant if WEEK_JANUARY is not set.
+
+			  If set	 Week is in range 1-53.
+
+	In this case one may get week 53 for a date in January (when
+	the week is that last week of previous year) and week 1 for a
+	date in December.
+
+  WEEK_FIRST_WEEKDAY (2)  If not set	Weeks are numbered according
+			   		to ISO 8601:1988
+			  If set	The week that contains the first
+					'first-day-of-week' is week 1.
+	
+	ISO 8601:1988 means that if the week containing January 1 has
+	four or more days in the new year, then it is week 1;
+	Otherwise it is the last week of the previous year, and the
+	next week is week 1.
 */
 
 longlong Item_func_week::val_int()
 {
   uint year;
-  uint week_format;
   TIME ltime;
   if (get_arg0_date(&ltime,0))
     return 0;
-  week_format= (uint) args[1]->val_int();
-  return (longlong) calc_week(&ltime, 
-			      (week_format & 2) != 0,
-			      (week_format & 1) == 0,
+  return (longlong) calc_week(&ltime,
+			      week_mode((uint) args[1]->val_int()),
 			      &year);
 }
 
@@ -206,7 +230,9 @@ longlong Item_func_yearweek::val_int()
   TIME ltime;
   if (get_arg0_date(&ltime,0))
     return 0;
-  week=calc_week(&ltime, 1, (args[1]->val_int() & 1) == 0, &year);
+  week= calc_week(&ltime, 
+		  (week_mode((uint) args[1]->val_int()) | WEEK_YEAR),
+		  &year);
   return week+year*100;
 }
 
@@ -844,7 +870,10 @@ String *Item_func_date_format::val_str(String *str)
       case 'u':
       {
 	uint year;
-	sprintf(intbuff,"%02d",calc_week(&l_time, 0, (*ptr) == 'U', &year));
+	sprintf(intbuff,"%02d",
+		calc_week(&l_time, 
+			  ((*ptr) == 'U' ? 
+			   WEEK_FIRST_WEEKDAY : WEEK_MONDAY_FIRST) , &year));
 	str->append(intbuff);
       }
       break;
@@ -852,7 +881,11 @@ String *Item_func_date_format::val_str(String *str)
       case 'V':
       {
 	uint year;
-	sprintf(intbuff,"%02d",calc_week(&l_time, 1, (*ptr) == 'V', &year));
+	sprintf(intbuff,"%02d",
+		calc_week(&l_time,
+			  ((*ptr) == 'V' ? WEEK_YEAR | WEEK_FIRST_WEEKDAY :
+			   WEEK_YEAR | WEEK_MONDAY_FIRST),
+			  &year));
 	str->append(intbuff);
       }
       break;
@@ -860,7 +893,10 @@ String *Item_func_date_format::val_str(String *str)
       case 'X':
       {
 	uint year;
-	(void) calc_week(&l_time, 1, (*ptr) == 'X', &year);
+	(void) calc_week(&l_time,
+			 ((*ptr) == 'X' ? WEEK_YEAR | WEEK_FIRST_WEEKDAY :
+			  WEEK_YEAR | WEEK_MONDAY_FIRST),
+			 &year);
 	sprintf(intbuff,"%04d",year);
 	str->append(intbuff);
       }
