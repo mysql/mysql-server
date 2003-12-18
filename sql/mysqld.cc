aa3f200c031913313d5373032a03709635bf0227
monty@mysql.com <>
2003-12-18 16:50:01 +0200

    Fixed that lower_case_table_names is set to 1 for case insensitive file systems.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -4955,7 +4955,8 @@ static void fix_paths(void)
   if (!lower_case_table_names &&
       test_if_case_insensitive(mysql_real_data_home) == 1)
   {
-    sql_print_error("Warning: Setting lower_case_table_names=1 becasue file system %s is case insensitve", mysql_real_data_home);
+    sql_print_error("Warning: Setting lower_case_table_names=1 becasue file system %s is case insensitive", mysql_real_data_home);
+    lower_case_table_names= 1;
   }
 }
 
