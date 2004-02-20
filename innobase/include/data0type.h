2b5020e127b9f69f68dd17508176440ac35d9e24
unknown <heikki@hundin.mysql.fi>
2004-02-20 09:50:33 +0200

    data0type.ic, data0type.h:   Fix a typo in a comment
    innobase/include/data0type.h:
      Fix a typo in a comment
    innobase/include/data0type.ic:
      Fix a typo in a comment

--- innobase/include/data0type.h
+++ innobase/include/data0type.h
@@ -154,7 +154,7 @@ dtype_is_binary_string_type(
 Checks if a type is a non-binary string type. That is, dtype_is_string_type is
 TRUE and dtype_is_binary_string_type is FALSE. Note that for tables created
 with < 4.0.14, we do not know if a DATA_BLOB column is a BLOB or a TEXT column.
-For those DATA_BLOB columns this function currently returns FALSE. */
+For those DATA_BLOB columns this function currently returns TRUE. */
 
 UNIV_INLINE
 ibool
