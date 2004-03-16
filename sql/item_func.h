579a94a328b5dd36bf1395182c3fd9055fe2f43d
bell@sanja.is.com.ua <>
2004-03-16 20:19:36 +0200

    after review & some other fixes

--- sql/item_func.h
+++ sql/item_func.h
@@ -512,6 +512,7 @@ public:
   double val();
   const char *func_name() const { return "rand"; }
   bool const_item() const { return 0; }
+  void update_used_tables();
   void fix_length_and_dec();
 };
 
