771233cefb7a8ad816b9582412dbccec29203b2c
unknown <monty@mysql.com>
2004-03-16 13:51:35 +0200

    Increase max size of number of elements in key. This fixed a bug when using count(DISTINCT) with lot of distinct values and big 'max_heap_table_size'

--- include/my_tree.h
+++ include/my_tree.h
@@ -20,7 +20,9 @@
 extern "C" {
 #endif
 
-#define MAX_TREE_HIGHT	40	/* = max 1048576 leafs in tree */
+/* Worst case tree is half full. This gives use 2^(MAX_TREE_HIGHT/2) leafs */
+#define MAX_TREE_HIGHT	64
+
 #define ELEMENT_KEY(tree,element)\
 (tree->offset_to_key ? (void*)((byte*) element+tree->offset_to_key) :\
 			*((void**) (element+1)))
