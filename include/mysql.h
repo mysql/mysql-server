75dda82526d8257962b952bf411da7edcaee5cf1
monty@mysql.com <>
2004-03-18 14:53:38 +0200

    Removed compiler warning Changed _XXX to _MY_XXX to solve conflict problem on Mac OS X

--- include/mysql.h
+++ include/mysql.h
@@ -590,7 +590,7 @@ typedef struct st_mysql_methods
   int (*unbuffered_fetch)(MYSQL *mysql, char **row);
   void (*free_embedded_thd)(MYSQL *mysql);
   const char *(*read_statistics)(MYSQL *mysql);
-  int (*next_result)(MYSQL *mysql);
+  my_bool (*next_result)(MYSQL *mysql);
   int (*read_change_user_result)(MYSQL *mysql, char *buff, const char *passwd);
 #endif
 } MYSQL_METHODS;
