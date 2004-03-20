0e75717d1f95d4f19b376f23b6990f779960eb3d
unknown <bell@sanja.is.com.ua>
2004-03-20 23:50:04 +0200

    fixed config parser to recognize correctly strings like following: test="\"#no comment here"#real comment

--- mysys/default.c
+++ mysys/default.c
@@ -449,11 +449,12 @@ static int search_default_file(DYNAMIC_ARRAY *args, MEM_ROOT *alloc,
 
 static char *remove_end_comment(char *ptr)
 {
-  char quote= 0;
+  char quote= 0;	/* we are inside quote marks */
+  char escape= 0;	/* symbol is protected by escape chagacter */
 
   for (; *ptr; ptr++)
   {
-    if (*ptr == '\'' || *ptr == '\"')
+    if ((*ptr == '\'' || *ptr == '\"') && !escape)
     {
       if (!quote)
 	quote= *ptr;
@@ -465,6 +466,7 @@ static char *remove_end_comment(char *ptr)
       *ptr= 0;
       return ptr;
     }
+    escape= (quote && *ptr == '\\' && !escape);
   }
   return ptr;
 }
