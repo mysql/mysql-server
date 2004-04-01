bf3f491e6d72498db028792076de6472f0e11355
unknown <brian@brian-akers-computer.local>
2004-04-01 01:02:42 -0800

    First commit of the skeleton storage engine. Use this example as a template to build storage engines. Also includes a new degine for marking storage engine methods as not being implemented.
    sql/examples/ha_example.h:
      First commit of example storage engine.
    BitKeeper/etc/logging_ok:
      Logging to logging@openlogging.org accepted

---
+++ sql/examples/ha_example.cc
@@ -0,0 +1,298 @@
+/* Copyright (C) 2003 MySQL AB
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+#ifdef __GNUC__
+#pragma implementation        // gcc: Class implementation
+#endif
+
+#include "mysql_priv.h"
+#include "ha_example.h"
+
+/* Stuff for shares */
+extern pthread_mutex_t LOCK_mysql_create_db;
+pthread_mutex_t example_mutex;
+static HASH example_open_tables;
+static int example_init= 0;
+
+static byte* example_get_key(EXAMLPE_SHARE *share,uint *length,
+                             my_bool not_used __attribute__((unused)))
+{
+  *length=share->table_name_length;
+  return (byte*) share->table_name;
+}
+
+
+/*
+  Simple lock controls.
+*/
+static EXAMLPE_SHARE *get_share(const char *table_name, TABLE *table)
+{
+  EXAMLPE_SHARE *share;
+  uint length;
+  char *tmp_name;
+
+  if (!example_init)
+  {
+    /* Hijack a mutex for init'ing the storage engine */
+    pthread_mutex_lock(&LOCK_mysql_create_db);
+    if (!example_init)
+    {
+      example_init++;
+      VOID(pthread_mutex_init(&example_mutex,MY_MUTEX_INIT_FAST));
+      (void) hash_init(&example_open_tables,system_charset_info,32,0,0,
+                       (hash_get_key) example_get_key,0,0);
+    }
+    pthread_mutex_unlock(&LOCK_mysql_create_db);
+  }
+  pthread_mutex_lock(&example_mutex);
+  length=(uint) strlen(table_name);
+
+  if (!(share=(EXAMLPE_SHARE*) hash_search(&example_open_tables,
+                                           (byte*) table_name,
+                                           length))){
+    if (!(share=(EXAMLPE_SHARE *)
+          my_multi_malloc(MYF(MY_WME | MY_ZEROFILL),
+                          &share, sizeof(*share),
+                          &tmp_name, length+1,
+                          NullS))) 
+    {
+      pthread_mutex_unlock(&example_mutex);
+      return NULL;
+    }
+
+    share->use_count=0;
+    share->table_name_length=length;
+    share->table_name=tmp_name;
+    strmov(share->table_name,table_name);
+    if (my_hash_insert(&example_open_tables, (byte*) share))
+      goto error;
+    thr_lock_init(&share->lock);
+    pthread_mutex_init(&share->mutex,MY_MUTEX_INIT_FAST);
+
+    if (get_mmap(share, 0) > 0)
+      goto error2;
+  }
+  share->use_count++;
+  pthread_mutex_unlock(&example_mutex);
+
+  return share;
+
+error2:
+  thr_lock_delete(&share->lock);
+  pthread_mutex_destroy(&share->mutex);
+error:
+  pthread_mutex_unlock(&example_mutex);
+  my_free((gptr) share, MYF(0));
+
+  return NULL;
+}
+
+
+/* 
+  Free lock controls.
+*/
+static int free_share(EXAMLPE_SHARE *share)
+{
+  pthread_mutex_lock(&example_mutex);
+  if (!--share->use_count){
+    hash_delete(&example_open_tables, (byte*) share);
+    thr_lock_delete(&share->lock);
+    pthread_mutex_destroy(&share->mutex);
+    my_free((gptr) share, MYF(0));
+  }
+  pthread_mutex_unlock(&example_mutex);
+
+  return 0;
+}
+
+
+const char **ha_example::bas_ext() const
+{ static const char *ext[]= { ".CSV", NullS }; return ext; }
+
+
+int ha_example::open(const char *name, int mode, uint test_if_locked)
+{
+  DBUG_ENTER("ha_example::open");
+  DBUG_PRINT("ha_example", ("MODE :%d:", mode));
+
+  if (!(share = get_share(name, table)))
+    DBUG_RETURN(1);
+  thr_lock_data_init(&share->lock,&lock,NULL);
+
+  DBUG_RETURN(0);
+}
+
+int ha_example::close(void)
+{
+  DBUG_ENTER("ha_example::close");
+  DBUG_RETURN(free_share(share));
+}
+
+int ha_example::write_row(byte * buf)
+{
+  DBUG_ENTER("ha_example::write_row");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED);
+}
+
+int ha_example::update_row(const byte * old_data, byte * new_data)
+{
+
+  DBUG_ENTER("ha_example::update_row");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED);
+}
+
+int ha_example::delete_row(const byte * buf)
+{
+  DBUG_ENTER("ha_example::delete_row");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED);
+}
+
+int ha_example::index_read(byte * buf, const byte * key,
+                           uint key_len __attribute__((unused)),
+                           enum ha_rkey_function find_flag
+                           __attribute__((unused)))
+{
+  DBUG_ENTER("ha_example::index_read");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED);
+}
+
+int ha_example::index_read_idx(byte * buf, uint index, const byte * key,
+                               uint key_len __attribute__((unused)),
+                               enum ha_rkey_function find_flag
+                               __attribute__((unused)))
+{
+  DBUG_ENTER("ha_example::index_read_idx");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED);
+}
+
+
+int ha_example::index_next(byte * buf)
+{
+  DBUG_ENTER("ha_example::index_next");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED);
+}
+
+int ha_example::index_prev(byte * buf)
+{
+  DBUG_ENTER("ha_example::index_prev");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED);
+}
+
+int ha_example::index_first(byte * buf)
+{
+  DBUG_ENTER("ha_example::index_first");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED);
+}
+
+int ha_example::index_last(byte * buf)
+{
+  DBUG_ENTER("ha_example::index_last");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED);
+}
+
+int ha_example::rnd_init(bool scan)
+{
+  DBUG_ENTER("ha_example::rnd_init");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED);
+}
+
+int ha_example::rnd_next(byte *buf)
+{
+  DBUG_ENTER("ha_example::rnd_next");
+  DBUG_RETURN(HA_ERR_END_OF_FILE);
+}
+
+void ha_example::position(const byte *record)
+{
+  DBUG_ENTER("ha_example::position");
+  DBUG_VOID_RETURN;
+}
+
+int ha_example::rnd_pos(byte * buf, byte *pos)
+{
+  DBUG_ENTER("ha_example::rnd_pos");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED));
+}
+
+void ha_example::info(uint flag)
+{
+  DBUG_ENTER("ha_example::info");
+  DBUG_VOID_RETURN;
+}
+
+int ha_example::extra(enum ha_extra_function operation)
+{
+  DBUG_ENTER("ha_example::extra");
+  DBUG_RETURN(0);
+}
+
+int ha_example::reset(void)
+{
+  DBUG_ENTER("ha_example::reset");
+  DBUG_RETURN(0);
+}
+
+
+int ha_example::delete_all_rows()
+{
+  DBUG_ENTER("ha_example::delete_all_rows");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED);
+}
+
+int ha_example::external_lock(THD *thd, int lock_type)
+{
+  DBUG_ENTER("ha_example::external_lock");
+  DBUG_RETURN(0);
+}
+
+THR_LOCK_DATA **ha_example::store_lock(THD *thd,
+                                       THR_LOCK_DATA **to,
+                                       enum thr_lock_type lock_type)
+{
+  if (lock_type != TL_IGNORE && lock.type == TL_UNLOCK)
+    lock.type=lock_type;
+  *to++= &lock;
+  return to;
+}
+
+int ha_example::delete_table(const char *name)
+{
+  DBUG_ENTER("ha_example::delete_table");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED);
+}
+
+int ha_example::rename_table(const char * from, const char * to)
+{
+  DBUG_ENTER("ha_example::rename_table ");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED);
+}
+
+ha_rows ha_example::records_in_range(int inx,
+                                     const byte *start_key,uint start_key_len,
+                                     enum ha_rkey_function start_search_flag,
+                                     const byte *end_key,uint end_key_len,
+                                     enum ha_rkey_function end_search_flag)
+{
+  DBUG_ENTER("ha_example::records_in_range ");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED);
+}
+
+
+int ha_example::create(const char *name, TABLE *table_arg, HA_CREATE_INFO *create_info)
+{
+  DBUG_ENTER("ha_example::create");
+  DBUG_RETURN(HA_ADMIN_NOT_IMPLEMENTED);
+}
---
+++ sql/examples/ha_example.h
@@ -0,0 +1,91 @@
+/* Copyright (C) 2003 MySQL AB
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+typedef struct st_example_share {
+  char *table_name;
+  uint table_name_length,use_count;
+  pthread_mutex_t mutex;
+  THR_LOCK lock;
+} EXAMLPE_SHARE;
+
+class ha_example: public handler
+{
+  THR_LOCK_DATA lock;    /* MySQL lock */
+  EXAMLPE_SHARE *share;    /* Shared lock info */
+
+public:
+  ha_example(TABLE *table): handler(table)
+  {
+  }
+  ~ha_example() 
+  {
+  }
+  const char *table_type() const { return "EXAMPLE"; }
+  const char *index_type(uint inx) { return "NONE"; }
+  const char **bas_ext() const;
+  ulong table_flags() const
+  {
+    return 0;
+  }
+  ulong index_flags(uint inx) const
+  {
+    return 0;
+  }
+  uint max_record_length() const { return HA_MAX_REC_LENGTH; }
+  uint max_keys()          const { return 0; }
+  uint max_key_parts()     const { return 0; }
+  uint max_key_length()    const { return 0; }
+  /*
+    Called in test_quick_select to determine if indexes should be used.
+  */
+  virtual double scan_time() { return (double) (records+deleted) / 20.0+10; }
+  /* The next method will never be called */
+  virtual double read_time(ha_rows rows) { return (double) rows /  20.0+1; }
+  virtual bool fast_key_read() { return 1;}
+
+  int open(const char *name, int mode, uint test_if_locked);
+  int close(void);
+  int write_row(byte * buf);
+  int update_row(const byte * old_data, byte * new_data);
+  int delete_row(const byte * buf);
+  int index_read(byte * buf, const byte * key,
+                 uint key_len, enum ha_rkey_function find_flag);
+  int index_read_idx(byte * buf, uint idx, const byte * key,
+                     uint key_len, enum ha_rkey_function find_flag);
+  int index_next(byte * buf);
+  int index_prev(byte * buf);
+  int index_first(byte * buf);
+  int index_last(byte * buf);
+  int rnd_init(bool scan=1);
+  int rnd_next(byte *buf);
+  int rnd_pos(byte * buf, byte *pos);
+  void position(const byte *record);
+  void info(uint);
+  int extra(enum ha_extra_function operation);
+  int reset(void);
+  int external_lock(THD *thd, int lock_type);
+  int delete_all_rows(void);
+  ha_rows records_in_range(int inx, const byte *start_key,uint start_key_len,
+                           enum ha_rkey_function start_search_flag,
+                           const byte *end_key,uint end_key_len,
+                           enum ha_rkey_function end_search_flag);
+  int delete_table(const char *from);
+  int rename_table(const char * from, const char * to);
+  int create(const char *name, TABLE *form, HA_CREATE_INFO *create_info);
+
+  THR_LOCK_DATA **store_lock(THD *thd, THR_LOCK_DATA **to,
+                             enum thr_lock_type lock_type);
+};
