917427f3b759355080a97e8c3f2f9936c4a879e1
joreland@mysql.com <>
2004-05-04 12:40:33 +0200

    yangf - yet another ndb gcc/g++ fix Hopefully last one

--- ndb/src/common/util/new.cpp
+++ ndb/src/common/util/new.cpp
@@ -37,10 +37,3 @@ void operator delete[] (void *ptr) throw ()
   if (ptr)
     free(ptr);
 }
-
-/**
- * GCC linking problem...
- */
-#if ( __GNUC__ == 3 )
-extern "C" { int __cxa_pure_virtual() {return 0;} }
-#endif
