a00117902f6821e1c521c335ba215bd84a9e200e
bell@sanja.is.com.ua <>
2004-05-10 13:29:02 +0300

    treat parameter as constant in ORDER BY check of fields in SELECT list(Bug #3686)

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -367,7 +367,8 @@ JOIN::prepare(Item ***rref_pointer_array,
       {
 	if (item->with_sum_func)
 	  flag|=1;
-	else if (!(flag & 2) && !item->const_item())
+	else if (!(flag & 2) &&
+		 test(item->used_tables() & ~PARAM_TABLE_BIT))
 	  flag|=2;
       }
       if (flag == 3)
