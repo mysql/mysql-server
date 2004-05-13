f9ada4071519454d7fe1f2821a07efc5f39e0f3d
magnus@neptunus.(none) <>
2004-05-13 11:21:56 +0200

    Fix for sql-bench/test-create

--- sql-bench/server-cfg.sh
+++ sql-bench/server-cfg.sh
@@ -188,6 +188,7 @@ sub new
   {
     $self->{'transactions'}	= 1;	# Transactions enabled
     $limits{'max_columns'}	= 90;	# Max number of columns in table
+    $limits{'max_tables'}	= 32;   # No comments
     $limits{'working_blobs'}	= 0;    # NDB tables can't handle BLOB's
   }
   if (defined($main::opt_create_options) &&
