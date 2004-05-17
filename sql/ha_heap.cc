eb936575073ba2d75c744f133fcd4a6e95e97993
serg@serg.mylan <>
2004-05-17 11:10:22 +0200

    typo fixed

--- sql/ha_heap.cc
+++ sql/ha_heap.cc
@@ -375,7 +375,7 @@ ha_rows ha_heap::records_in_range(uint inx, key_range *min_key,
       min_key->length != key->key_length ||
       min_key->flag != HA_READ_KEY_EXACT ||
       max_key->flag != HA_READ_AFTER_KEY)
-    return HA_POS_ERROR;			// Can't only use exact keys
+    return HA_POS_ERROR;			// Can only use exact keys
   return 10;					// Good guess
 }
 
