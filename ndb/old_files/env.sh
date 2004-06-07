20a42316aecdf85e908da780f4f8e9b285febc5e
tomas@poseidon.ndb.mysql.com <>
2004-06-07 17:31:32 +0000

    ndb source tree cleanup, see respective file

--- ndb/old_files/env.sh
+++
@@ -1,8 +0,0 @@
-#
-
-NDB_TOP=`pwd`
-export NDB_TOP
-
-NDB_PROJ_HOME=$NDB_TOP/home
-export NDB_PROJ_HOME
-
--- ndb/src/ndbapi/Ndberr.cpp
+++ ndb/src/ndbapi/Ndberr.cpp
@@ -18,7 +18,6 @@
 #include <NdbError.hpp>
 #include "NdbImpl.hpp"
 #include "NdbDictionaryImpl.hpp"
-#include <NdbSchemaCon.hpp>
 #include <NdbOperation.hpp>
 #include <NdbConnection.hpp>
 
@@ -63,13 +62,6 @@ NdbConnection::getNdbError() const {
 const 
 NdbError & 
 NdbOperation::getNdbError() const {
-  update(theError);
-  return theError;
-}
-
-const 
-NdbError & 
-NdbSchemaCon::getNdbError() const {
   update(theError);
   return theError;
 }
