e08b4cbf307fe1b0431995e392f20a227d98a438
unknown <heikki@hundin.mysql.fi>
2004-06-17 15:11:37 +0300

    log0log.h:   Revert the patch that put log debug code behind UNIV_LOG_DEBUG log0log.ic, log0recv.c, log0log.c, log0log.h:   Revert 1.1730.18.1 about UNIV_LOG_DEBUG: debug code is often needed in debugging the production version
    innobase/log/log0log.c:
      Revert 1.1730.18.1 about UNIV_LOG_DEBUG: debug code is often needed in debugging the production version
    innobase/log/log0recv.c:
      Revert 1.1730.18.1 about UNIV_LOG_DEBUG: debug code is often needed in debugging the production version
    innobase/include/log0log.ic:
      Revert 1.1730.18.1 about UNIV_LOG_DEBUG: debug code is often needed in debugging the production version
    innobase/include/log0log.h:
      Revert the patch that put log debug code behind UNIV_LOG_DEBUG

--- innobase/log/log0log.c
+++ innobase/log/log0log.c
@@ -31,9 +31,9 @@ ulint	log_fsp_current_free_limit		= 0;
 log_t*	log_sys	= NULL;
 
 ibool	log_do_write = TRUE;
-#ifdef UNIV_LOG_DEBUG
+
 ibool	log_debug_writes = FALSE;
-#endif /* UNIV_LOG_DEBUG */
+
 
 /* These control how often we print warnings if the last checkpoint is too
 old */
@@ -929,12 +929,10 @@ log_group_check_flush_completion(
 #endif /* UNIV_SYNC_DEBUG */
 
 	if (!log_sys->one_flushed && group->n_pending_writes == 0) {
-#ifdef UNIV_LOG_DEBUG
 		if (log_debug_writes) {
 			fprintf(stderr,
 				"Log flushed first to group %lu\n", group->id);
 		}
-#endif /* UNIV_LOG_DEBUG */
 	
 		log_sys->written_to_some_lsn = log_sys->write_lsn;
 		log_sys->one_flushed = TRUE;
@@ -942,12 +940,10 @@ log_group_check_flush_completion(
 		return(LOG_UNLOCK_NONE_FLUSHED_LOCK);
 	}
 
-#ifdef UNIV_LOG_DEBUG
 	if (log_debug_writes && (group->n_pending_writes == 0)) {
 
 		fprintf(stderr, "Log flushed to group %lu\n", group->id);
 	}
-#endif /* UNIV_LOG_DEBUG */
 
 	return(0);
 }
@@ -1023,13 +1019,11 @@ log_io_complete(
 		        fil_flush(group->space_id);
 		}
 
-#ifdef UNIV_LOG_DEBUG
 		if (log_debug_writes) {
 			fprintf(stderr,
 				"Checkpoint info written to group %lu\n",
 				group->id);
 		}
-#endif /* UNIV_LOG_DEBUG */
 
 		log_io_complete_checkpoint();
 
@@ -1092,13 +1086,11 @@ log_group_file_header_flush(
 
 	dest_offset = nth_file * group->file_size;
 
-#ifdef UNIV_LOG_DEBUG
 	if (log_debug_writes) {
 		fprintf(stderr,
 			"Writing log file header to group %lu file %lu\n",
 			group->id, nth_file);
 	}
-#endif /* UNIV_LOG_DEBUG */
 
 	if (log_do_write) {
 		log_sys->n_log_ios++;	
@@ -1181,7 +1173,6 @@ loop:
 		write_len = len;
 	}
 	
-#ifdef UNIV_LOG_DEBUG
 	if (log_debug_writes) {
 
 		fprintf(stderr,
@@ -1204,7 +1195,6 @@ loop:
 					+ i * OS_FILE_LOG_BLOCK_SIZE));
 		}
 	}
-#endif /* UNIV_LOG_DEBUG */
 
 	/* Calculate the checksums for each log block and write them to
 	the trailer fields of the log blocks */
@@ -1332,7 +1322,6 @@ loop:
 		return;
 	}
 
-#ifdef UNIV_LOG_DEBUG
 	if (log_debug_writes) {
 		fprintf(stderr,
 			"Writing log from %lu %lu up to lsn %lu %lu\n",
@@ -1341,7 +1330,6 @@ loop:
 					ut_dulint_get_high(log_sys->lsn),
 					ut_dulint_get_low(log_sys->lsn));
 	}
-#endif /* UNIV_LOG_DEBUG */
 
 	log_sys->n_pending_writes++;
 
@@ -1900,14 +1888,12 @@ log_checkpoint(
 
 	log_sys->next_checkpoint_lsn = oldest_lsn;
 
-#ifdef UNIV_LOG_DEBUG
 	if (log_debug_writes) {
 		fprintf(stderr, "Making checkpoint no %lu at lsn %lu %lu\n",
 			ut_dulint_get_low(log_sys->next_checkpoint_no),
 			ut_dulint_get_high(oldest_lsn),
 			ut_dulint_get_low(oldest_lsn));
 	}
-#endif /* UNIV_LOG_DEBUG */
 
 	log_groups_write_checkpoint_info();
 
@@ -2286,11 +2272,9 @@ loop:
 			exit(1);
 		}
 
-#ifdef UNIV_LOG_DEBUG
 		if (log_debug_writes) {
 			fprintf(stderr, "Created archive file %s\n", name);
 		}
-#endif /* UNIV_LOG_DEBUG */
 
 		ret = os_file_close(file_handle);
 	
@@ -2319,7 +2303,6 @@ loop:
 		len = group->file_size - (next_offset % group->file_size);
 	}
 	
-#ifdef UNIV_LOG_DEBUG
 	if (log_debug_writes) {
 		fprintf(stderr,
 		"Archiving starting at lsn %lu %lu, len %lu to group %lu\n",
@@ -2327,7 +2310,6 @@ loop:
 					ut_dulint_get_low(start_lsn),
 					len, group->id);
 	}
-#endif /* UNIV_LOG_DEBUG */
 
 	log_sys->n_pending_archive_ios++;
 
@@ -2418,13 +2400,11 @@ log_archive_write_complete_groups(void)
 		trunc_files = n_files - 1;
 	}
 
-#ifdef UNIV_LOG_DEBUG
 	if (log_debug_writes && trunc_files) {
 		fprintf(stderr,
 			"Complete file(s) archived to group %lu\n",
 								group->id);
 	}
-#endif /* UNIV_LOG_DEBUG */
 
 	/* Calculate the archive file space start lsn */
 	start_lsn = ut_dulint_subtract(log_sys->next_archived_lsn,
@@ -2447,11 +2427,9 @@ log_archive_write_complete_groups(void)
 	fil_space_truncate_start(group->archive_space_id,
 					trunc_files * group->file_size);
 
-#ifdef UNIV_LOG_DEBUG
 	if (log_debug_writes) {
 		fputs("Archiving writes completed\n", stderr);
 	}
-#endif /* UNIV_LOG_DEBUG */
 }
 
 /**********************************************************
@@ -2468,11 +2446,9 @@ log_archive_check_completion_low(void)
 	if (log_sys->n_pending_archive_ios == 0
 			&& log_sys->archiving_phase == LOG_ARCHIVE_READ) {
 
-#ifdef UNIV_LOG_DEBUG
 		if (log_debug_writes) {
 			fputs("Archiving read completed\n", stderr);
 		}
-#endif /* UNIV_LOG_DEBUG */
 
 	    	/* Archive buffer has now been read in: start archive writes */
 
@@ -2616,7 +2592,6 @@ loop:
 
 	log_sys->next_archived_lsn = limit_lsn;
 
-#ifdef UNIV_LOG_DEBUG
 	if (log_debug_writes) {
 		fprintf(stderr,
 			"Archiving from lsn %lu %lu to lsn %lu %lu\n",
@@ -2625,7 +2600,6 @@ loop:
 			ut_dulint_get_high(limit_lsn),
 			ut_dulint_get_low(limit_lsn));
 	}
-#endif /* UNIV_LOG_DEBUG */
 
 	/* Read the log segment to the archive buffer */
 	
@@ -2724,13 +2698,11 @@ log_archive_close_groups(
 			group->archived_file_no += 2;
 		}
 
-#ifdef UNIV_LOG_DEBUG
 		if (log_debug_writes) {
 			fprintf(stderr,
 			"Incrementing arch file no to %lu in log group %lu\n",
 				group->archived_file_no + 2, group->id);
 		}
-#endif /* UNIV_LOG_DEBUG */
 	}
 }
 
@@ -3118,7 +3090,6 @@ loop:
 	ut_a(0 == ut_dulint_cmp(lsn, log_sys->lsn));
 }
 
-#ifdef UNIV_LOG_DEBUG
 /**********************************************************
 Checks by parsing that the catenated log segment for a single mtr is
 consistent. */
@@ -3171,7 +3142,6 @@ log_check_log_recs(
 			
 	return(TRUE);
 }
-#endif /* UNIV_LOG_DEBUG */
 
 /**********************************************************
 Peeks the current lsn. */
--- innobase/log/log0recv.c
+++ innobase/log/log0recv.c
@@ -169,7 +169,6 @@ recv_sys_empty_hash(void)
 	recv_sys->addr_hash = hash_create(buf_pool_get_curr_size() / 256);
 }
 
-#ifndef UNIV_LOG_DEBUG
 /************************************************************
 Frees the recovery system. */
 static
@@ -189,7 +188,6 @@ recv_sys_free(void)
 
 	mutex_exit(&(recv_sys->mutex));
 }
-#endif /* !UNIV_LOG_DEBUG */
 
 /************************************************************
 Truncates possible corrupted or extra records from a log group. */
@@ -469,7 +467,6 @@ recv_find_max_checkpoint(
 			log_group_read_checkpoint_info(group, field);
 
 			if (!recv_check_cp_is_consistent(buf)) {
-#ifdef UNIV_LOG_DEBUG
 				if (log_debug_writes) {
 					fprintf(stderr, 
 	    "InnoDB: Checkpoint in group %lu at %lu invalid, %lu\n",
@@ -478,7 +475,6 @@ recv_find_max_checkpoint(
 					      + LOG_CHECKPOINT_CHECKSUM_1));
 
 				}
-#endif /* UNIV_LOG_DEBUG */
 
 				goto not_consistent;
 			}
@@ -492,13 +488,11 @@ recv_find_max_checkpoint(
 			checkpoint_no =
 				mach_read_from_8(buf + LOG_CHECKPOINT_NO);
 
-#ifdef UNIV_LOG_DEBUG
 			if (log_debug_writes) {
 				fprintf(stderr, 
 			"InnoDB: Checkpoint number %lu found in group %lu\n",
 				ut_dulint_get_low(checkpoint_no), group->id);
 			}
-#endif /* UNIV_LOG_DEBUG */
 				
 			if (ut_dulint_cmp(checkpoint_no, max_no) >= 0) {
 				*max_group = group;
@@ -1106,14 +1100,12 @@ recv_recover_page(
 				start_lsn = recv->start_lsn;
 			}
 
-#ifdef UNIV_LOG_DEBUG
 			if (log_debug_writes) {
 				fprintf(stderr, 
      "InnoDB: Applying log rec type %lu len %lu to space %lu page no %lu\n",
 			(ulint)recv->type, recv->len, recv_addr->space,
 				recv_addr->page_no);
 			}
-#endif /* UNIV_LOG_DEBUG */
 
 			recv_parse_or_apply_log_rec_body(recv->type, buf,
 						buf + recv->len, page, &mtr);
@@ -1357,7 +1349,6 @@ loop:
 	mutex_exit(&(recv_sys->mutex));
 }
 
-#ifdef UNIV_HOTBACKUP
 /***********************************************************************
 Applies log records in the hash table to a backup. */
 
@@ -1681,7 +1672,6 @@ recv_compare_spaces_low(
 
 	recv_compare_spaces(space1, space2, n_pages);
 }
-#endif
 
 /***********************************************************************
 Tries to parse a single log record and returns its length. */
@@ -1777,7 +1767,6 @@ recv_calc_lsn_on_data_add(
 	return(ut_dulint_add(lsn, lsn_len));
 }
 
-#ifdef UNIV_LOG_DEBUG
 /***********************************************************
 Checks that the parser recognizes incomplete initial segments of a log
 record as incomplete. */
@@ -1799,7 +1788,6 @@ recv_check_incomplete_log_recs(
 							&page_no, &body));
 	}
 }		
-#endif /* UNIV_LOG_DEBUG */
 
 /***********************************************************
 Prints diagnostic info of corrupt log. */
@@ -1932,13 +1920,11 @@ loop:
 		recv_sys->recovered_offset += len;
 		recv_sys->recovered_lsn = new_recovered_lsn;
 
-#ifdef UNIV_LOG_DEBUG
 		if (log_debug_writes) {
 			fprintf(stderr, 
 "InnoDB: Parsed a single log rec type %lu len %lu space %lu page no %lu\n",
 			(ulint)type, len, space, page_no);
 		}
-#endif /* UNIV_LOG_DEBUG */
 
 		if (type == MLOG_DUMMY_RECORD) {
 			/* Do nothing */
@@ -1953,7 +1939,7 @@ loop:
 			becomes identical with the original page */
 #ifdef UNIV_LOG_DEBUG
 			recv_check_incomplete_log_recs(ptr, len);
-#endif	
+#endif
 /*			recv_update_replicate(type, space, page_no, body,
 								ptr + len);
 			recv_compare_replicate(space, page_no);
@@ -1997,13 +1983,11 @@ loop:
 */
 			}
 			
-#ifdef UNIV_LOG_DEBUG
 			if (log_debug_writes) {
 				fprintf(stderr, 
 "InnoDB: Parsed a multi log rec type %lu len %lu space %lu page no %lu\n",
 				(ulint)type, len, space, page_no);
 			}
-#endif /* UNIV_LOG_DEBUG */
 
 			total_len += len;
 			n_recs++;
@@ -2407,7 +2391,6 @@ recv_group_scan_log_recs(
 		start_lsn = end_lsn;
 	}
 
-#ifdef UNIV_LOG_DEBUG
 	if (log_debug_writes) {
 		fprintf(stderr,
 	"InnoDB: Scanned group %lu up to log sequence number %lu %lu\n",
@@ -2415,7 +2398,6 @@ recv_group_scan_log_recs(
 				ut_dulint_get_high(*group_scanned_lsn),
 				ut_dulint_get_low(*group_scanned_lsn));
 	}
-#endif /* UNIV_LOG_DEBUG */
 }
 
 /************************************************************
@@ -2739,12 +2721,10 @@ recv_recovery_from_checkpoint_finish(void)
 		recv_apply_hashed_log_recs(TRUE);
 	}
 
-#ifdef UNIV_LOG_DEBUG
 	if (log_debug_writes) {
 		fprintf(stderr,
 		"InnoDB: Log records applied to the database\n");
 	}
-#endif /* UNIV_LOG_DEBUG */
 
 	if (recv_needed_recovery) {
 		trx_sys_print_mysql_master_log_pos();
@@ -3067,7 +3047,6 @@ ask_again:
 			break;
 		}
 	
-#ifdef UNIV_LOG_DEBUG
 		if (log_debug_writes) {
 			fprintf(stderr, 
 "InnoDB: Archive read starting at lsn %lu %lu, len %lu from file %s\n",
@@ -3075,7 +3054,6 @@ ask_again:
 					ut_dulint_get_low(start_lsn),
 					len, name);
 		}
-#endif /* UNIV_LOG_DEBUG */
 
 		fil_io(OS_FILE_READ | OS_FILE_LOG, TRUE,
 			group->archive_space_id, read_offset / UNIV_PAGE_SIZE,
