c5544563acb003aa54b9060bb03e3bad60a97452
guilhem@mysql.com <>
2004-06-19 15:35:41 +0200

    swapping lines to make gcc happy

--- libmysql/libmysql.c
+++ libmysql/libmysql.c
@@ -1287,8 +1287,8 @@ int STDCALL
 mysql_shutdown(MYSQL *mysql, enum enum_shutdown_level shutdown_level)
 {
   uchar level[1];
-  level[0]= (uchar) shutdown_level;
   DBUG_ENTER("mysql_shutdown");
+  level[0]= (uchar) shutdown_level;
   DBUG_RETURN(simple_command(mysql, COM_SHUTDOWN, (char *)level, 1, 0));
 }
 
