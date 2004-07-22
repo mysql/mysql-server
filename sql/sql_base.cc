1159996ee5902ea817e9fc5b565b7ff1cda774d6
unknown <bell@sanja.is.com.ua>
2004-07-22 14:05:00 +0300

    privent crash on temporary table during indexes lookup (BUG#4677)
    mysql-test/r/view.result:
      check 'use index' on view with temporary table
    mysql-test/t/view.test:
      check 'use index' on view with temporary table
    sql/sql_base.cc:
      privent crash on temporary table

--- sql/sql_base.cc
+++ sql/sql_base.cc
@@ -2555,8 +2555,9 @@ bool get_key_map_from_key_list(key_map *map, TABLE *table,
   map->clear_all();
   while ((name=it++))
   {
-    if ((pos= find_type(&table->keynames, name->ptr(), name->length(), 1)) <=
-	0)
+    if (table->keynames.type_names == 0 ||
+        (pos= find_type(&table->keynames, name->ptr(), name->length(), 1)) <=
+        0)
     {
       my_error(ER_KEY_COLUMN_DOES_NOT_EXITS, MYF(0), name->c_ptr(),
 	       table->real_name);
