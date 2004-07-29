5f2750c049fdc6c39ba8fe4d255c705bf453b6dd
pekka@mysql.com <>
2004-07-29 10:44:53 +0200

    ha_ndbcluster.h, ha_ndbcluster.cc:   compile fix for gcc-2.95

--- sql/ha_ndbcluster.h
+++ sql/ha_ndbcluster.h
@@ -184,7 +184,7 @@ class ha_ndbcluster: public handler
 		  uint fieldnr, const byte* field_ptr);
   int set_ndb_value(NdbOperation*, Field *field, uint fieldnr);
   int get_ndb_value(NdbOperation*, Field *field, uint fieldnr);
-  friend int ::get_ndb_blobs_value(NdbBlob *ndb_blob, void *arg);
+  friend int g_get_ndb_blobs_value(NdbBlob *ndb_blob, void *arg);
   int get_ndb_blobs_value(NdbBlob *last_ndb_blob);
   int set_primary_key(NdbOperation *op, const byte *key);
   int set_primary_key(NdbOperation *op);
