d758fc8ec36afce4d63bc86c320d15f5171eb8a8
unknown <lenz@mysql.com>
2004-08-10 16:50:29 +0200

    - Added libmygcc.a to the "devel" RPM subpackage (required to link    applications against the the embedded server libmysqld.a)    (BUG#4921)

--- support-files/mysql.spec.sh
+++ support-files/mysql.spec.sh
@@ -282,7 +282,18 @@ mv sql/mysqld sql/mysqld-max
 nm --numeric-sort sql/mysqld-max > sql/mysqld-max.sym
 
 # Install embedded server library in the build root
-install -m 644 libmysqld/libmysqld.a $RBR%{_libdir}/mysql
+install -m 644 libmysqld/libmysqld.a $RBR%{_libdir}/mysql/
+
+# Include libgcc.a in the devel subpackage (BUG 4921)
+if [ "$CC" = gcc ]
+then
+  libgcc=`$CC --print-libgcc-file`
+  if [ -f $libgcc ]
+  then
+    %define have_libgcc 1
+    install -m 644 $libgcc $RBR%{_libdir}/mysql/libmygcc.a
+  fi
+fi
 
 # Save libraries
 (cd libmysql/.libs; tar cf $RBR/shared-libs.tar *.so*)
@@ -540,6 +551,9 @@ fi
 %{_libdir}/mysql/libdbug.a
 %{_libdir}/mysql/libheap.a
 %{_libdir}/mysql/libmerge.a
+%if %{have_libgcc}
+%{_libdir}/mysql/libmygcc.a
+%endif
 %{_libdir}/mysql/libmyisam.a
 %{_libdir}/mysql/libmyisammrg.a
 %{_libdir}/mysql/libmysqlclient.a
@@ -576,6 +590,11 @@ fi
 # The spec file changelog only includes changes made to the spec file
 # itself
 %changelog 
+* Tue Aug 10 2004 Lenz Grimmer <lenz@mysql.com>
+
+- Added libmygcc.a to the devel subpackage (required to link applications
+  against the the embedded server libmysqld.a) (BUG 4921)
+
 * Mon Aug 09 2004 Lenz Grimmer <lenz@mysql.com>
 
 - Added EXCEPTIONS-CLIENT to the "devel" package
