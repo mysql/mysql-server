db2db3c0884a0ffe4fa10e2b46b463633af3cdd3
unknown <joreland@mysql.com>
2004-08-12 08:54:21 +0200

    testScan -n ScanRandomTable testDict -n FragmentType
    ndb/test/ndbapi/testDict.cpp:
      Drop table before trying to create with special fragmentation
    ndb/test/ndbapi/testScan.cpp:
      testScan -n ScanRandomTable
    ndb/test/src/NDBT_Tables.cpp:
      testScan -n ScanRandomTable

--- ndb/test/ndbapi/testScan.cpp
+++ ndb/test/ndbapi/testScan.cpp
@@ -65,7 +65,7 @@ int runDropAllTablesExceptTestTable(NDBT_Context* ctx, NDBT_Step* step){
     }
 	    
     int res = GETNDB(step)->getDictionary()->dropTable(tab->getName());
-    if(res != -1){
+    if(res == -1){
       return NDBT_FAILED;
     }
   }
