df3c69f444a6f5d9f0a954a50508849425d494a0
joreland@mysql.com <>
2004-08-13 16:11:00 +0200

    New error codes + automatic nulling of tvariables

--- ndb/src/kernel/blocks/dblqh/DblqhInit.cpp
+++ ndb/src/kernel/blocks/dblqh/DblqhInit.cpp
@@ -324,6 +324,31 @@ Dblqh::Dblqh(const class Configuration & conf):
   addRecSignal(GSN_TUX_ADD_ATTRREF, &Dblqh::execTUX_ADD_ATTRREF);
 
   initData();
+
+#ifdef VM_TRACE
+  {
+    void* tmp[] = { 
+      &addfragptr,
+      &attrinbufptr,
+      &databufptr,
+      &fragptr,
+      &gcpPtr,
+      &lcpPtr,
+      &lcpLocptr,
+      &logPartPtr,
+      &logFilePtr,
+      &lfoPtr,
+      &logPagePtr,
+      &pageRefPtr,
+      &scanptr,
+      &tabptr,
+      &tcConnectptr,
+      &tcNodeFailptr,
+    }; 
+    init_globals_list(tmp, sizeof(tmp)/sizeof(tmp[0]));
+  }
+#endif
+  
 }//Dblqh::Dblqh()
 
 Dblqh::~Dblqh() 
