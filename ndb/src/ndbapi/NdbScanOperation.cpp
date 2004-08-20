64fc12f3d23393f2b60dc9bf50a162bcb6ff5b5a
tomas@poseidon.(none) <>
2004-08-20 16:29:35 +0000

    compile error fixes for some platforms

--- ndb/src/ndbapi/NdbScanOperation.cpp
+++ ndb/src/ndbapi/NdbScanOperation.cpp
@@ -14,18 +14,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
-/*****************************************************************************
- * Name:          NdbScanOperation.cpp
- * Include:
- * Link:
- * Author:        UABMASD Martin Sköld INN/V Alzato
- * Date:          2002-04-01
- * Version:       0.1
- * Description:   Table scan support
- * Documentation:
- * Adjust:  2002-04-01  UABMASD   First version.
- ****************************************************************************/
-
+#include <ndb_global.h>
 #include <Ndb.hpp>
 #include <NdbScanOperation.hpp>
 #include <NdbIndexScanOperation.hpp>
