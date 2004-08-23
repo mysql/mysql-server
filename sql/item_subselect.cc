6fcd27e9cde3293f49efdc9ccaef33b7698bf7ae
bell@sanja.is.com.ua <>
2004-08-23 22:31:01 +0300

    after review fixes:  allowed parsing of table fields inside aggregate functions  added new tests of fields resolving in grouping

--- sql/item_subselect.cc
+++ sql/item_subselect.cc
@@ -70,7 +70,14 @@ void Item_subselect::init(st_select_lex *select_lex,
   }
   else
   {
-    parsing_place= unit->outer_select()->parsing_place;
+    SELECT_LEX *outer_select= unit->outer_select();
+    /*
+      do not take into account expression inside aggregate functions because
+      they can access original table fields
+    */
+    parsing_place= (outer_select->in_sum_expr ?
+                    NO_MATTER :
+                    outer_select->parsing_place);
     if (select_lex->next_select())
       engine= new subselect_union_engine(unit, result, this);
     else
