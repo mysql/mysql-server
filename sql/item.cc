acfd7fbf87263989ceeddfe3225858566a84caf5
unknown <pem@mysql.comhem.se>
2004-08-30 11:09:56 +0200

    Fixed return type warning in sql/item.cc.
    sql/item.cc:
      Fixed return type warning.

--- sql/item.cc
+++ sql/item.cc
@@ -2182,7 +2182,7 @@ longlong Item_ref::val_int_result()
   if (result_field)
   {
     if ((null_value= result_field->is_null()))
-      return 0.0;
+      return 0;
     return result_field->val_int();
   }
   return val_int();
