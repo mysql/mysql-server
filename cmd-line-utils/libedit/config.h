aeeccb38f57d13905a0688c144df29329d4aec1c
vva@eagle.mysql.r18.ru <>
2004-08-31 18:29:01 +0500

    fixed two compiling bugs for libedit

--- cmd-line-utils/libedit/config.h
+++ cmd-line-utils/libedit/config.h
@@ -6,3 +6,9 @@
 #define __COPYRIGHT(x)
 #define __RENAME(x)
 #define _DIAGASSERT(x)
+
+#if !defined(__attribute__) && (defined(__cplusplus) || !defined(__GNUC__)  || __GNUC__ == 2 && __GNUC_MINOR__ < 8)
+#define __attribute__(A)
+#endif
+
+
