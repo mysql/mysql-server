88dedcfd6258788f3c3d05209eaecc3155f27589
bar@mysql.com <>
2004-09-01 15:39:15 +0500

    Allow IN to convert arguments into Unicode in some cases.

--- sql/item_func.h
+++ sql/item_func.h
@@ -140,8 +140,11 @@ public:
   Field *tmp_table_field(TABLE *t_arg);
   Item *get_tmp_table_item(THD *thd);
   
-  bool agg_arg_collations(DTCollation &c, Item **items, uint nitems);
-  bool agg_arg_collations_for_comparison(DTCollation &c, Item **items, uint nitems);
+  bool agg_arg_collations(DTCollation &c, Item **items, uint nitems,
+                          bool allow_superset_conversion= FALSE);
+  bool agg_arg_collations_for_comparison(DTCollation &c,
+                                         Item **items, uint nitems,
+                                         bool allow_superset_comversion= FALSE);
 
   bool walk(Item_processor processor, byte *arg);
 };
