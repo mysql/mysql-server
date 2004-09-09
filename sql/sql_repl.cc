df461bdeaba8dddf8056430c48926ac4a614a7cd
brian@brian-akers-computer.local <>
2004-09-08 18:26:19 -0700

    Updating the headers on a few files to include GPL header.

--- sql/sql_repl.cc
+++ sql/sql_repl.cc
@@ -14,8 +14,6 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
-// Sasha Pachev <sasha@mysql.com> is currently in charge of this file
-
 #include "mysql_priv.h"
 #include "sql_repl.h"
 #include "sql_acl.h"
