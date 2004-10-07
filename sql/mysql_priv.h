d2c0e4804a8e8c45fc8cc7bace9e95b5ed058341
monty@mysql.com <>
2004-10-07 10:50:13 +0300

    After merge fixes Some bigger code changes was necessary becasue of the multi-table-update and the new HANDLER code

--- sql/mysql_priv.h
+++ sql/mysql_priv.h
@@ -729,6 +729,7 @@ void wait_for_refresh(THD *thd);
 int open_tables(THD *thd, TABLE_LIST *tables, uint *counter);
 int simple_open_n_lock_tables(THD *thd,TABLE_LIST *tables);
 int open_and_lock_tables(THD *thd,TABLE_LIST *tables);
+void relink_tables_for_derived(THD *thd);
 int lock_tables(THD *thd, TABLE_LIST *tables, uint counter);
 TABLE *open_temporary_table(THD *thd, const char *path, const char *db,
 			    const char *table_name, bool link_in_list);
