b0154e21dc2804788d3a81a2d90ca29afef5bd5d
joerg@mysql.com <>
2004-10-08 15:00:36 +0200

    Solve compile problem - use system types like 'size_t'.

--- sql/sql_class.cc
+++ sql/sql_class.cc
@@ -702,7 +702,7 @@ struct Item_change_record: public ilink
   Item **place;
   Item *old_value;
   /* Placement new was hidden by `new' in ilink (TODO: check): */
-  static void *operator new(unsigned int size, void *mem) { return mem; }
+  static void *operator new(size_t size, void *mem) { return mem; }
 };
 
 
