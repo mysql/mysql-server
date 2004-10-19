9c8436505ddacb7df84962f2c80ce116cae5125c
unknown <ram@gw.mysql.r18.ru>
2004-10-19 11:00:44 +0500

    A fix (bug #6101: mysqldump writes invalid SQL).
    client/mysqldump.c:
      A fix (bug #6101: mysqldump writes invalid SQL).
      Syntax corrected.

--- client/mysqldump.c
+++ client/mysqldump.c
@@ -1797,7 +1797,7 @@ static int init_dumping(char *database)
         MYSQL_ROW row;
         MYSQL_RES *dbinfo;
 
-        sprintf(qbuf,"SHOW CREATE DATABASE WITH IF NOT EXISTS %s",
+        sprintf(qbuf,"SHOW CREATE DATABASE IF NOT EXISTS %s",
 		qdatabase);
 
         if (mysql_query(sock, qbuf) || !(dbinfo = mysql_store_result(sock)))
