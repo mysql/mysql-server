2c574292c49148f6e1d2f8f78bb6cd729c41b16a
heikki@hundin.mysql.fi <>
2004-11-02 09:46:23 +0200

    dict0load.c:   Raise fatal semaphore wait timeout to 2 hours when we are printing the InnoDB table monitor output

--- innobase/dict/dict0load.c
+++ innobase/dict/dict0load.c
@@ -20,6 +20,7 @@ Created 4/24/1996 Heikki Tuuri
 #include "dict0dict.h"
 #include "dict0boot.h"
 #include "srv0start.h"
+#include "srv0srv.h"
 
 /************************************************************************
 Finds the first table name in the given database. */
@@ -123,6 +124,13 @@ dict_print(void)
 	ulint		len;
 	mtr_t		mtr;
 	
+	/* Enlarge the fatal semaphore wait timeout during the InnoDB table
+	monitor printout */
+
+	mutex_enter(&kernel_mutex);
+	srv_fatal_semaphore_wait_threshold += 7200; /* 2 hours */
+	mutex_exit(&kernel_mutex);
+
 	mutex_enter(&(dict_sys->mutex));
 
 	mtr_start(&mtr);
@@ -144,6 +152,12 @@ loop:
 		mtr_commit(&mtr);
 		
 		mutex_exit(&(dict_sys->mutex));
+
+		/* Restore the fatal semaphore wait timeout */
+
+		mutex_enter(&kernel_mutex);
+		srv_fatal_semaphore_wait_threshold -= 7200; /* 2 hours */
+		mutex_exit(&kernel_mutex);
 
 		return;
 	}	
