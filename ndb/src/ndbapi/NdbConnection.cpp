4527110e8758ec82f38f77c9eed8da46f712079e
joreland@mysql.com <>
2004-11-24 16:36:18 +0100

    bug#6775 - ndb Queue scan on real fragment.   Index fragment for range scans   Table fragment for table scans

--- ndb/src/ndbapi/NdbConnection.cpp
+++ ndb/src/ndbapi/NdbConnection.cpp
@@ -1086,8 +1086,11 @@ NdbConnection::getNdbIndexScanOperation(const NdbIndexImpl* index,
     if (indexTable != 0){
       NdbIndexScanOperation* tOp = 
 	getNdbScanOperation((NdbTableImpl *) indexTable);
-      tOp->m_currentTable = table;
-      if(tOp) tOp->m_cursor_type = NdbScanOperation::IndexCursor;
+      if(tOp)
+      {
+	tOp->m_currentTable = table;
+	tOp->m_cursor_type = NdbScanOperation::IndexCursor;
+      }
       return tOp;
     } else {
       setOperationErrorCodeAbort(theNdb->theError.code);
