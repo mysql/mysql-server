5290e580efd8822c6e22060eef572d5dabde3dbb
bell@sanja.is.com.ua <>
2004-11-26 16:30:20 +0200

    portability fix (some printf lack of NULL support in strings)

--- sql/item.cc
+++ sql/item.cc
@@ -187,9 +187,12 @@ void Item_ident::cleanup()
 {
   DBUG_ENTER("Item_ident::cleanup");
   DBUG_PRINT("enter", ("b:%s(%s), t:%s(%s), f:%s(%s)",
-		       db_name, orig_db_name,
-		       table_name, orig_table_name,
-		       field_name, orig_field_name));
+		       db_name ? db_name : "(null)",
+                       orig_db_name ? orig_db_name : "(null)",
+		       table_name ? table_name : "(null)",
+                       orig_table_name ? orig_table_name : "(null)",
+		       field_name ? field_name : "(null)",
+                       orig_field_name ? orig_field_name : "(null)"));
   Item::cleanup();
   db_name= orig_db_name; 
   table_name= orig_table_name;
