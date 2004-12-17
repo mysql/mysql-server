911df28cd8e4221eb7847c5ceb3f08a664d4baed
tomas@poseidon.ndb.mysql.com <>
2004-12-17 10:24:57 +0100

    version.c:   4.1.9 compatible with 4.1.8

--- ndb/src/common/util/version.c
+++ ndb/src/common/util/version.c
@@ -69,6 +69,7 @@ struct NdbUpGradeCompatible {
 
 #ifndef TEST_VERSION
 struct NdbUpGradeCompatible ndbCompatibleTable_full[] = {
+  { MAKE_VERSION(4,1,9), MAKE_VERSION(4,1,8), UG_Exact },
   { MAKE_VERSION(3,5,2), MAKE_VERSION(3,5,1), UG_Exact },
   { 0, 0, UG_Null }
 };
