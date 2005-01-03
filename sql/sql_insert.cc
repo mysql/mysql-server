2299e1eca4230465f3d9b8ca0df49330bec093d2
monty@mysql.com <>
2005-01-03 21:04:33 +0200

    Better handling of ensuring that setup_tables() are not called twice This fixed a bug in prepared statements when used with outher joins Fixed a bug in SUM(DISTINCT) when used with prepared statements. Some safety fixes in test scripts to ensure that previous test failures shouldn't affect other tests

--- sql/sql_insert.cc
+++ sql/sql_insert.cc
@@ -1736,12 +1736,6 @@ bool mysql_insert_select_prepare(THD *thd)
     DBUG_RETURN(TRUE);
 
   /*
-    setup was done in mysql_prepare_insert_check_table, but we have to mark
-    first local table
-  */
-  if (first_select_table)
-    first_select_table->setup_is_done= 1;
-  /*
     exclude first table from leaf tables list, because it belong to
     INSERT
   */
