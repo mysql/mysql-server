74212397870da5196dd43c987291c8050e9355f3
unknown <tomas@poseidon.ndb.mysql.com>
2005-01-10 11:54:03 +0100

    fixed testNdbApi to use new startTransaction interface

--- ndb/test/ndbapi/testNdbApi.cpp
+++ ndb/test/ndbapi/testNdbApi.cpp
@@ -119,6 +119,9 @@ int runTestMaxTransaction(NDBT_Context* ctx, NDBT_Step* step){
     return NDBT_FAILED;
   }
 
+  const NdbDictionary::Table* pTab = ctx->getTab();
+  if (pTab == 0) abort();
+
   while (l < loops && result == NDBT_OK){
     int errors = 0;
     int maxErrors = 5;
@@ -137,7 +140,7 @@ int runTestMaxTransaction(NDBT_Context* ctx, NDBT_Step* step){
 	pCon = pNdb->startTransaction();
 	break;
       case 1:
-	pCon = pNdb->startTransaction(2,
+	pCon = pNdb->startTransaction(pTab,
 				      "DATA",
 				      4);
 	break;
