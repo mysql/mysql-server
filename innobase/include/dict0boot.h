5ca6d3cb46dd6cf99485ed9c234f4cccb1386eef
marko@hundin.mysql.fi <>
2005-01-11 16:28:07 +0200

    InnoDB: Implement fast TRUNCATE TABLE (Bug #7150)

--- innobase/include/dict0boot.h
+++ innobase/include/dict0boot.h
@@ -119,6 +119,7 @@ dict_create(void);
 clustered index */
 #define DICT_SYS_INDEXES_PAGE_NO_FIELD	 8
 #define DICT_SYS_INDEXES_SPACE_NO_FIELD	 7
+#define DICT_SYS_INDEXES_TYPE_FIELD	 6
 					
 /* When a row id which is zero modulo this number (which must be a power of
 two) is assigned, the field DICT_HDR_ROW_ID on the dictionary header page is
