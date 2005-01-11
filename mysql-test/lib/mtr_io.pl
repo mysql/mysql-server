ac606583ca57bf3e6431f6181158c30a8f73255a
unknown <kent@mysql.com>
2005-01-11 01:35:08 +0100

    mtr_process.pl:   Catch more fork() errors.   Moved sleep_until_file_created() here from "mysql-test-run.pl".   Improved debug output. mtr_io.pl:   Improved mtr_get_opts_from_file(), try to mimic some sh. mysql-test-run.pl:   Cleaned up the timeout handling.   Created new function environment_setup().   Corrected time zone handling.   Moved sleep_until_file_created() to "lib/mtr_process.pl".   Improved debug output.
    mysql-test/mysql-test-run.pl:
      Cleaned up the timeout handling.
      Created new function environment_setup().
      Corrected time zone handling.
      Moved sleep_until_file_created() to "lib/mtr_process.pl".
      Improved debug output.
    mysql-test/lib/mtr_io.pl:
      Improved mtr_get_opts_from_file(), try to mimic some sh.
    mysql-test/lib/mtr_process.pl:
      Catch more fork() errors.
      Moved sleep_until_file_created() here from "mysql-test-run.pl".
      Improved debug output.

--- mysql-test/lib/mtr_io.pl
+++ mysql-test/lib/mtr_io.pl
@@ -35,11 +35,70 @@ sub mtr_get_opts_from_file ($) {
   while ( <FILE> )
   {
     chomp;
-    s/\$MYSQL_TEST_DIR/$::glob_mysql_test_dir/g;
-    push(@args, split(' ', $_));
+
+    #    --set-variable=init_connect=set @a='a\\0c'
+    s/^\s+//;                           # Remove leading space
+    s/\s+$//;                           # Remove ending space
+
+    # This is strange, but we need to fill whitespace inside
+    # quotes with something, to remove later. We do this to
+    # be able to split on space. Else, we have trouble with
+    # options like 
+    #
+    #   --someopt="--insideopt1 --insideopt2"
+    #
+    # But still with this, we are not 100% sure it is right,
+    # we need a shell to do it right.
+
+#    print STDERR "\n";
+#    print STDERR "AAA: $_\n";
+
+    s/\'([^\'\"]*)\'/unspace($1,"\x0a")/ge;
+    s/\"([^\'\"]*)\"/unspace($1,"\x0b")/ge;
+    s/\'([^\'\"]*)\'/unspace($1,"\x0a")/ge;
+    s/\"([^\'\"]*)\"/unspace($1,"\x0b")/ge;
+
+#    print STDERR "BBB: $_\n";
+
+#    foreach my $arg (/(--?\w.*?)(?=\s+--?\w|$)/)
+
+    # FIXME ENV vars should be expanded!!!!
+
+    foreach my $arg (split(/[ \t]+/))
+    {
+      $arg =~ tr/\x11\x0a\x0b/ \'\"/;     # Put back real chars
+      # The outermost quotes has to go
+      $arg =~ s/^([^\'\"]*)\'(.*)\'([^\'\"]*)$/$1$2$3/
+        or $arg =~ s/^([^\'\"]*)\"(.*)\"([^\'\"]*)$/$1$2$3/;
+      $arg =~ s/\\\\/\\/g;
+
+      $arg =~ s/\$\{(\w+)\}/envsubst($1)/ge;
+      $arg =~ s/\$(\w+)/envsubst($1)/ge;
+
+#      print STDERR "ARG: $arg\n";
+      push(@args, $arg);
+    }
   }
   close FILE;
   return \@args;
+}
+
+sub envsubst {
+  my $string= shift;
+
+  if ( ! defined $ENV{$string} )
+  {
+    mtr_error("opt file referense \$$string that is unknown");
+  }
+
+  return $ENV{$string};
+}
+
+sub unspace {
+  my $string= shift;
+  my $quote=  shift;
+  $string =~ s/[ \t]/\x11/g;
+  return "$quote$string$quote";
 }
 
 sub mtr_fromfile ($) {
