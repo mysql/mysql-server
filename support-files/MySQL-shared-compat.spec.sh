60ff25cac86cdbfffe1b5dcddb95b46f221202a2
unknown <lenz@mysql.com>
2005-01-13 14:54:18 +0100

    - added the 4.1 shared client libraries to the 5.0 shared-compat RPM   (we should consider dropping 3.23 from this one soon)

--- support-files/MySQL-shared-compat.spec.sh
+++ support-files/MySQL-shared-compat.spec.sh
@@ -26,7 +26,8 @@
 #
 # Change this to match the version of the shared libs you want to include
 #
-%define version41 @MYSQL_NO_DASH_VERSION@
+%define version50 @MYSQL_NO_DASH_VERSION@
+%define version41 4.1.9
 %define version40 4.0.23
 %define version3 3.23.58
 
@@ -37,30 +38,32 @@ License:      GPL
 Group:        Applications/Databases
 URL:          http://www.mysql.com/
 Autoreqprov:  on
-Version:      %{version41}
+Version:      %{version50}
 Release:      0
 BuildRoot:    %{_tmppath}/%{name}-%{version}-build
 Obsoletes:    MySQL-shared, mysql-shared
 Provides:     MySQL-shared
-Summary:      MySQL shared client libraries for MySQL %{version41}, %{version40} and %{version3}
+Summary:      MySQL shared client libraries for MySQL %{version}, %{version41}, %{version40} and %{version3}
 # We simply use the "MySQL-shared" subpackages as input sources instead of
 # rebuilding all from source
-Source0:      MySQL-shared-%{version41}-0.%{_arch}.rpm
-Source1:      MySQL-shared-%{version40}-0.%{_arch}.rpm
-Source2:      MySQL-shared-%{version3}-1.%{_arch}.rpm
+Source0:      MySQL-shared-%{version50}-0.%{_arch}.rpm
+Source1:      MySQL-shared-%{version41}-0.%{_arch}.rpm
+Source2:      MySQL-shared-%{version40}-0.%{_arch}.rpm
+Source3:      MySQL-shared-%{version3}-1.%{_arch}.rpm
 # No need to include the RPMs once more - they can be downloaded seperately
 # if you want to rebuild this package
 NoSource:     0
 NoSource:     1
 NoSource:     2
+NoSource:     3
 BuildRoot:    %{_tmppath}/%{name}-%{version}-build
 
 %description
-This package includes the shared libraries for both MySQL %{version3},
-MySQL %{version40} as well as MySQL %{version41}.
+This package includes the shared libraries for MySQL %{version3},
+MySQL %{version40}, %{version41} as well as MySQL %{version50}.
 Install this package instead of "MySQL-shared", if you have applications
 installed that are dynamically linked against older versions of the MySQL
-client library but you want to upgrade to MySQL 4.1.xx without breaking the
+client library but you want to upgrade to MySQL %{version} without breaking the
 library dependencies.
 
 %install
@@ -70,6 +73,7 @@ cd $RPM_BUILD_ROOT
 rpm2cpio %{SOURCE0} | cpio -iv --make-directories
 rpm2cpio %{SOURCE1} | cpio -iv --make-directories
 rpm2cpio %{SOURCE2} | cpio -iv --make-directories
+rpm2cpio %{SOURCE3} | cpio -iv --make-directories
 /sbin/ldconfig -n $RPM_BUILD_ROOT%{_libdir}
 
 %clean
