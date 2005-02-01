9d548d7f2253ea5dd1f78960f61ba2f0822db177
unknown <joreland@mysql.com>
2005-02-01 17:08:39 +0100

    ndb - Put all output from ndb tools during mysql-test-run into log file
    mysql-test/mysql-test-run.sh:
      Put all output from ndb_tool into log file
    mysql-test/t/ndb_autodiscover.test:
      Put all output from ndb_tool into log file
    mysql-test/t/ndb_restore.test:
      Put all output from ndb_tool into log file

--- mysql-test/mysql-test-run.sh
+++ mysql-test/mysql-test-run.sh
@@ -472,6 +472,7 @@ export MASTER_MYPORT MASTER_MYPORT1 SLAVE_MYPORT MYSQL_TCP_PORT MASTER_MYSOCK MA
 NDBCLUSTER_BASE_PORT=`expr $NDBCLUSTER_PORT + 2`
 NDBCLUSTER_OPTS="--port=$NDBCLUSTER_PORT --port-base=$NDBCLUSTER_BASE_PORT --data-dir=$MYSQL_TEST_DIR/var --ndb_mgm-extra-opts=$NDB_MGM_EXTRA_OPTS --ndb_mgmd-extra-opts=$NDB_MGMD_EXTRA_OPTS --ndbd-extra-opts=$NDBD_EXTRA_OPTS"
 NDB_BACKUP_DIR=$MYSQL_TEST_DIR/var/ndbcluster-$NDBCLUSTER_PORT
+NDB_TOOLS_OUTPUT=$MYSQL_TEST_DIR/var/log/ndb_tools.log
 
 if [ x$SOURCE_DIST = x1 ] ; then
  MY_BASEDIR=$MYSQL_TEST_DIR
@@ -637,6 +638,7 @@ export CLIENT_BINDIR MYSQL_CLIENT_TEST CHARSETSDIR
 export NDB_TOOLS_DIR
 export NDB_MGM
 export NDB_BACKUP_DIR
+export NDB_TOOLS_OUTPUT
 
 MYSQL_TEST_ARGS="--no-defaults --socket=$MASTER_MYSOCK --database=$DB \
  --user=$DBUSER --password=$DBPASSWD --silent -v --skip-safemalloc \
@@ -978,6 +980,7 @@ start_ndbcluster()
 {
   if [ ! -z "$USE_NDBCLUSTER" ]
   then
+  rm -f $NDBAPI_OUTPUT
   if [ -z "$USE_RUNNING_NDBCLUSTER" ]
   then
     echo "Starting ndbcluster"
