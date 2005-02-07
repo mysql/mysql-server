d790e2d42c940cc838f6e09b1d51af2eca6de418
msvensson@neptunus.homeip.net <>
2005-02-07 13:46:13 +0100

    Small changes after Tomas review

--- sql/ha_ndbcluster.cc
+++ sql/ha_ndbcluster.cc
@@ -5711,6 +5711,7 @@ extern "C" pthread_handler_decl(ndb_util_thread_func,
   delete thd;
   DBUG_PRINT("exit", ("ndb_util_thread"));
   my_thread_end();
+  pthread_exit(0);
   DBUG_RETURN(NULL);
 }
 
