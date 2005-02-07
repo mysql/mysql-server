620fb53fd4f0819ccc97c9694c587374a1d07d70
pekka@mysql.com <>
2005-02-07 12:06:55 +0100

    mysqld.cc - remove extra #endif

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -4453,7 +4453,6 @@ Disable with --skip-innodb (will save memory).",
   {"innodb_checksums", OPT_INNODB_CHECKSUMS, "Enable InnoDB checksums validation (enabled by default). \
 Disable with --skip-innodb-checksums.", (gptr*) &innobase_use_checksums,
    (gptr*) &innobase_use_checksums, 0, GET_BOOL, NO_ARG, 1, 0, 0, 0, 0, 0},
-#endif
   {"innodb_data_file_path", OPT_INNODB_DATA_FILE_PATH,
    "Path to individual files and their sizes.",
    0, 0, 0, GET_STR, REQUIRED_ARG, 0, 0, 0, 0, 0, 0},
