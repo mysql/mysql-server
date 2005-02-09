db13afd89db4a415e2a77b0c200a1391ea2f03fe
serg@serg.mylan <>
2005-02-09 20:04:28 +0100

    auto-ROLLBACK if binlog was not closed properly auto-commit on Xid_log_event

--- sql/sql_repl.h
+++ sql/sql_repl.h
@@ -36,7 +36,11 @@ extern I_List<i_string> binlog_do_db, binlog_ignore_db;
 extern int max_binlog_dump_events;
 extern my_bool opt_sporadic_binlog_dump_fail;
 
-#define KICK_SLAVE(thd) { pthread_mutex_lock(&(thd)->LOCK_delete); (thd)->awake(THD::NOT_KILLED); pthread_mutex_unlock(&(thd)->LOCK_delete); }
+#define KICK_SLAVE(thd) do {                                            \
+                          pthread_mutex_lock(&(thd)->LOCK_delete);      \
+                          (thd)->awake(THD::NOT_KILLED);                \
+                          pthread_mutex_unlock(&(thd)->LOCK_delete);    \
+                        } while(0)
 
 int start_slave(THD* thd, MASTER_INFO* mi, bool net_report);
 int stop_slave(THD* thd, MASTER_INFO* mi, bool net_report);
