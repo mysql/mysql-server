1d1470fd0e87678581f74a4952b8cd1b81c1ce6c
jimw@mysql.com <>
2005-02-09 18:17:40 -0800

    Re-enable alter_table test for embedded server by moving grant-related test to grant.test and using replace_column to neuter the Privileges column in SHOW FULL COLUMNS.

--- mysql-test/mysql-test-run.sh
+++ mysql-test/mysql-test-run.sh
@@ -1506,7 +1506,6 @@ run_testcase ()
  # script soon anyway so it is not worth it spending the time
  if [ "x$USE_EMBEDDED_SERVER" = "x1" -a -z "$DO_TEST" ] ; then
    for t in \
-        "alter_table" \
 	"bdb-deadlock" \
 	"connect" \
         "ctype_latin1_de" \
