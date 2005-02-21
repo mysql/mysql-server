20065372a10edbaba9141a4591b16c684a2c9f3c
tulin@mysql.com <>
2005-02-21 10:52:29 +0100

    bug #8611, ndb includes in lib dir instead of include dir

--- scripts/make_binary_distribution.sh
+++ scripts/make_binary_distribution.sh
@@ -278,7 +278,7 @@ if [ x$NDBCLUSTER = x1 ]; then
   $CP $BASE/ndb-stage@bindir@/* $BASE/bin/.
   $CP $BASE/ndb-stage@libexecdir@/* $BASE/bin/.
   $CP $BASE/ndb-stage@pkglibdir@/* $BASE/lib/.
-  $CP -r $BASE/ndb-stage@pkgincludedir@/ndb $BASE/lib/.
+  $CP -r $BASE/ndb-stage@pkgincludedir@/ndb $BASE/include
   $CP -r $BASE/ndb-stage@prefix@/mysql-test/ndb $BASE/mysql-test/. || exit 1
   rm -rf $BASE/ndb-stage
 fi
