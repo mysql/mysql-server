2b5ee94a332c5e97cdb8b642581ddb09743db8b2
gluh@gluh.mysql.r18.ru <>
2005-02-23 18:15:45 +0300

    Fix for bug#6317: string function CHAR, parameter is NULL, wrong result

--- sql/item_strfunc.cc
+++ sql/item_strfunc.cc
@@ -1868,6 +1868,7 @@ String *Item_func_char::val_str(String *str)
   {
     int32 num=(int32) args[i]->val_int();
     if (!args[i]->null_value)
+    {
 #ifdef USE_MB
       if (use_mb(collation.collation))
       {
@@ -1883,6 +1884,7 @@ b1:        str->append((char)(num>>8));
       }
 #endif
       str->append((char)num);
+    }
   }
   str->set_charset(collation.collation);
   str->realloc(str->length());			// Add end 0 (for Purify)
