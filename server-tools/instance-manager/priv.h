8bed4a14e7db732da1658f144f42bb889d525e1e
petr@mysql.com <>
2005-02-28 16:05:18 +0300

    portability fix

--- server-tools/instance-manager/priv.h
+++ server-tools/instance-manager/priv.h
@@ -16,6 +16,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
+#include <sys/types.h>
 #include <unistd.h>
 
 
