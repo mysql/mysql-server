793699f330c009b0a17730e4655ddc45a20e69aa
unknown <stewart@mysql.com>
2005-03-03 19:49:58 +1100

    Add ndb/src/mgmapi/mgmapi_internal.h
    move internal mgmapi functions into it.
    
    should also fix AIX build problem.
    
    
    ndb/include/mgmapi/mgmapi.h:
      Move ndb_mgm_convert_to_transporter to mgmapi_internal.h
      
      Should fix AIX build problem
    ndb/include/mgmapi/mgmapi_debug.h:
      Move ndb_mgm_(get|set)_connection_int_parameter to mgmapi_internal.h
    ndb/src/common/transporter/Makefile.am:
      Add ndb/src/mgmapi to include for common/transporter (TransporterRegistry requires the use of mgmapi_internal.h)
    ndb/src/common/transporter/TransporterRegistry.cpp:
      Use mgmapi_internal.h
    ndb/src/mgmapi/mgmapi.cpp:
      some functions defined in mgmapi_internal.h
    ndb/src/ndbapi/ndb_cluster_connection.cpp:
      use functions from mgmapi_internal.h

---
+++ ndb/src/mgmapi/mgmapi_internal.h
@@ -0,0 +1,77 @@
+/* Copyright (C) 2005 MySQL AB
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+#ifndef MGMAPI_INTERNAL_H
+#define MGMAPI_INTERNAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <NdbTCP.h>
+
+  /**
+   * Set an integer parameter for a connection
+   *
+   * @param handle the NDB management handle.
+   * @param node1 the node1 id
+   * @param node2 the node2 id
+   * @param param the parameter (e.g. CFG_CONNECTION_SERVER_PORT)
+   * @param value what to set it to
+   * @param reply from ndb_mgmd
+   */
+  int ndb_mgm_set_connection_int_parameter(NdbMgmHandle handle,
+					   int node1,
+					   int node2,
+					   int param,
+					   int value,
+					   struct ndb_mgm_reply* reply);
+
+  /**
+   * Get an integer parameter for a connection
+   *
+   * @param handle the NDB management handle.
+   * @param node1 the node1 id
+   * @param node2 the node2 id
+   * @param param the parameter (e.g. CFG_CONNECTION_SERVER_PORT)
+   * @param value where to store the retreived value. In the case of 
+   * error, value is not changed.
+   * @param reply from ndb_mgmd
+   * @return 0 on success. < 0 on error.
+   */
+  int ndb_mgm_get_connection_int_parameter(NdbMgmHandle handle,
+					   int node1,
+					   int node2,
+					   int param,
+					   int *value,
+					   struct ndb_mgm_reply* reply);
+
+  /**
+   * Convert connection to transporter
+   * @param   handle    NDB management handle.
+   *
+   * @return socket
+   *
+   * @note the socket is now able to be used as a transporter connection
+   */
+  NDB_SOCKET_TYPE ndb_mgm_convert_to_transporter(NdbMgmHandle *handle);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
