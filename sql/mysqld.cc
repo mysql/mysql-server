ba05aef9306f3cac9a7280c8197b7eda5fda29c6
unknown <serg@serg.mylan>
2005-03-07 13:59:28 +0100

    Fixed crash if max_connections is exceeded. BUG#8996
    sql/mysqld.cc:
      mark the thread as killed in close_connection
    sql/protocol.cc:
      don't bother remembering warnings if the thread is dying

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -1432,6 +1432,7 @@ void close_connection(THD *thd, uint errcode, bool lock)
 		      errcode ? ER(errcode) : ""));
   if (lock)
     (void) pthread_mutex_lock(&LOCK_thread_count);
+  thd->killed=1;
   if ((vio=thd->net.vio) != 0)
   {
     if (errcode)
