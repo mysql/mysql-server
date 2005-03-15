da911ec8f150102029708f0abd7d1718c475702b
unknown <monty@mysql.com>
2005-03-16 01:15:45 +0200

    Partly reverty back patch (in heap-auto-increment-key detection) to ensure that auto_key and auto_key_type are calculated the same way
    heap/hp_create.c:
      Partly reverty back patch to ensure that auto_key and auto_key_type are calculated the same way
    include/heap.h:
      Partly reverty back patch to ensure that auto_key and auto_key_type are calculated the same way
    mysql-test/mysql-test-run.sh:
      made --skip-ndb a synonyme for --skip-ndbcluster
    sql/ha_heap.cc:
      Partly reverty back patch to ensure that auto_key and auto_key_type are calculated the same way

--- mysql-test/mysql-test-run.sh
+++ mysql-test/mysql-test-run.sh
@@ -426,7 +426,7 @@ while test $# -gt 0; do
       TMP=`$ECHO "$1" | $SED -e "s;--valgrind-options=;;"`
       VALGRIND="$VALGRIND $TMP"
       ;;
-    --skip-ndbcluster)
+    --skip-ndbcluster | --skip-ndb)
       USE_NDBCLUSTER=""
       EXTRA_MASTER_MYSQLD_OPT="$EXTRA_MASTER_MYSQLD_OPT $1"
       EXTRA_SLAVE_MYSQLD_OPT="$EXTRA_SLAVE_MYSQLD_OPT $1"
