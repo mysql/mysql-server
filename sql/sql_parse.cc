1fbb61754b1f96902043d1e5716d6260d42222c1
jani@a193-229-222-105.elisa-laajakaista.fi <>
2005-03-17 08:16:56 +0200

    Fixed two bugs in MySQL ACL. First one is related to Bug#7905. One should not be allowed to create new user with password without UPDATE privilege to MySQL database. Furthermore, executing the same GRANT statement twice would actually crash the server and corrupt privilege database.
    Other bug was that one could update a column, using the existing
    value as basis to calculate the new value (e.g. UPDATE t1 SET a=a+1)
    without SELECT privilege to the field (a in the above example)
    
    Fixed tests grant.pl and grant2, which were wrong.

--- sql/sql_parse.cc
+++ sql/sql_parse.cc
@@ -3626,6 +3626,24 @@ unsent_create_error:
 		     first_table ? 0 : 1, 0))
       goto error;
 
+    if (thd->user)				// If not replication
+    {
+      LEX_USER *user;
+      List_iterator <LEX_USER> user_list(lex->users_list);
+      while ((user=user_list++))
+      {
+	if (user->password.str &&
+	    strcmp(thd->user, user->user.str) ||
+	    user->host.str &&
+	    my_strcasecmp(system_charset_info,
+			  user->host.str, thd->host_or_ip))
+	{
+	  if (check_access(thd, UPDATE_ACL, "mysql", 0, 1, 0))
+	    goto error;
+	  break;			// We are allowed to do changes
+	}
+      }
+    }
     if (specialflag & SPECIAL_NO_RESOLVE)
     {
       LEX_USER *user;
