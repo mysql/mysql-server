fe5384dad8f88c28baece9bb8602d79734e28a02
msvensson@neptunus.(none) <>
2005-04-01 12:32:47 +0200

    BUG#6554 Problem Building MySql on Fedora Core 3  - Remove the local static var

--- sql/ha_berkeley.cc
+++ sql/ha_berkeley.cc
@@ -336,8 +336,10 @@ void berkeley_cleanup_log_files(void)
 ** Berkeley DB tables
 *****************************************************************************/
 
+static const char *bdb_bas_exts[]= { ha_berkeley_ext, NullS };
 const char **ha_berkeley::bas_ext() const
-{ static const char *ext[]= { ha_berkeley_ext, NullS }; return ext; }
+{ return bdb_bas_exts; }
+
 
 
 static int
