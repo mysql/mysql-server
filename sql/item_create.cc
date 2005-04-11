93b4fbea9247aad1ad469a252ef1b67fc8e87bdf
unknown <dlenev@brandersnatch.localdomain>
2005-04-11 10:44:48 +0400

    Merging fix for bug #9796 "Query Cache caches queries with CURRENT_USER() function" into 4.1 tree.

--- sql/item_create.cc
+++ sql/item_create.cc
@@ -306,7 +306,7 @@ Item *create_func_current_user()
   char buff[HOSTNAME_LENGTH+USERNAME_LENGTH+2];
   uint length;
 
-  thd->safe_to_cache_query= 0;
+  thd->lex->safe_to_cache_query= 0;
   length= (uint) (strxmov(buff, thd->priv_user, "@", thd->priv_host, NullS) -
 		  buff);
   return new Item_string(NullS, thd->memdup(buff, length), length,
