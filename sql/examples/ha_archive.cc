4715085db2da8cc45b01874c0f5b7d6f3de5daa8
msvensson@neptunus.(none) <>
2005-04-27 22:20:58 +0200

    BUG#9911 RENAME TABLE of type ARCHIVE fails with .ARN file error  - Different behaviuor in 5.0 pushes a warning when renaming a non existent file. Avoid that by checking that the file exists before renaming.

--- sql/examples/ha_archive.cc
+++ sql/examples/ha_archive.cc
@@ -455,17 +455,22 @@ const char **ha_archive::bas_ext() const
 int ha_archive::rename_table(const char * from, const char * to)
 {
   DBUG_ENTER("ha_archive::rename_table");
+  DBUG_PRINT("enter", ("from: %s, to: %s", from, to));
   for (const char **ext=bas_ext(); *ext ; ext++)
   {
-    if (rename_file_ext(from,to,*ext))
+    // Check if the .arn file exists before rename
+    if (!my_strcasecmp(system_charset_info, *ext, ARN))
     {
-      if (my_errno == ENOENT &&
-          !my_strcasecmp(system_charset_info, *ext, ARN))
+      char name[FN_REFLEN];
+      (void)strxnmov(name, FN_REFLEN, from, ARN, NullS);
+      if (access(name, F_OK))
+      {
+        DBUG_PRINT("info", ("%s does not exist on disk, skipping it", name));
         continue;
-
-      DBUG_RETURN(my_errno);
+      }
     }
-
+    if (rename_file_ext(from,to,*ext))
+      DBUG_RETURN(my_errno);
   }
   DBUG_RETURN(0);
 }
