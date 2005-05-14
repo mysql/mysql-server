18d0e6a73d1dca6b967e8f006012f78e7af74140
unknown <monty@mishka.local>
2005-05-14 19:31:34 +0300

    Don't enable binlogs by default as this gives too many warnings

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -5871,7 +5871,7 @@ static void mysql_init_variables(void)
   opt_skip_slave_start= opt_reckless_slave = 0;
   mysql_home[0]= pidfile_name[0]= log_error_file[0]= 0;
   opt_log= opt_update_log= opt_slow_log= 0;
-  opt_bin_log= 1;                               // Enable binlog by default
+  opt_bin_log= 0;
   opt_disable_networking= opt_skip_show_db=0;
   opt_logname= opt_update_logname= opt_binlog_index_name= opt_slow_logname= 0;
   opt_tc_log_file= (char *)"tc.log";      // no hostname in tc_log file name !
