cd2150993562ca2465a91340d5bd9f2aefd50784
unknown <bell@sanja.is.com.ua>
2005-05-20 16:21:23 +0300

    Macos 10.4 compatibility changes
    include/my_global.h:
      make it compilable on mac os
    ndb/src/kernel/SimBlockList.cpp:
      make it compilable with gcc 4.0

--- include/my_global.h
+++ include/my_global.h
@@ -285,7 +285,7 @@ C_MODE_END
 # endif
 #endif /* TIME_WITH_SYS_TIME */
 #ifdef HAVE_UNISTD_H
-#if defined(HAVE_OPENSSL) && !defined(__FreeBSD__) && !defined(NeXT) && !defined(__OpenBSD__)
+#if defined(HAVE_OPENSSL) && !defined(__FreeBSD__) && !defined(NeXT) && !defined(__OpenBSD__) && !defined(__APPLE__)
 #define crypt unistd_crypt
 #endif
 #include <unistd.h>
