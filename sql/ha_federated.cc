d3d727e32874b2c3392baab8188ebe17da7cc8b9
patg@radha.local <>
2005-05-27 22:07:46 +0200

    Comment and test changes per review request by Timour. All tests pass on production with this code.

--- sql/ha_federated.cc
+++ sql/ha_federated.cc
@@ -1526,7 +1526,11 @@ int ha_federated::index_read_idx(byte *buf, uint index, const byte *key,
     table->status= STATUS_NOT_FOUND;
     DBUG_RETURN(mysql_errno(mysql));
   }
-  /* very important - joins will not work without this! */
+  /* 
+     This basically says that the record in table->record[0] is legal, and that it is
+     ok to use this record, for whatever reason, such as with a join (without it, joins 
+     will not work)
+  */
   table->status=0;
 
   DBUG_RETURN(rnd_next(buf));
