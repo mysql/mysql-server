5e618219442a5c57c8da783ff415e2be8aa55613
ramil@mysql.com <>
2005-06-29 12:03:38 +0500

    a fix (bug #11215: BIGINT: can't set DEFAULT to minimum end-range)

--- sql/item.cc
+++ sql/item.cc
@@ -3573,7 +3573,7 @@ Item *Item_int_with_ref::new_item()
 
 Item_num *Item_uint::neg()
 {
-  Item_decimal *item= new Item_decimal(value, 0);
+  Item_decimal *item= new Item_decimal(value, 1);
   return item->neg();
 }
 
