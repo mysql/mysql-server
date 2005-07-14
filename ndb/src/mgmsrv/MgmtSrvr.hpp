680e7d9deb26868362e8c10aac0ed40720497892
tomas@poseidon.ndb.mysql.com <>
2005-07-14 18:04:44 +0200

    BUG#11898 ndb_mgmd not releasing resources, added "ping" on add_listener     + added close of some fd's     + debug prints

--- ndb/src/mgmsrv/MgmtSrvr.hpp
+++ ndb/src/mgmsrv/MgmtSrvr.hpp
@@ -60,6 +60,7 @@ public:
   }
   
   void add_listener(const Event_listener&);
+  void check_listeners();
   void update_max_log_level(const LogLevel&);
   void update_log_level(const LogLevel&);
   
@@ -764,16 +765,6 @@ private:
   int send(class NdbApiSignal* signal, Uint32 node, Uint32 node_type);
 
   ConfigRetriever *m_config_retriever;
-
-public:
-  /**
-   * This method does not exist
-   */
-  struct Area51 {
-    class TransporterFacade * theFacade;
-    class TransporterRegistry * theRegistry;
-  };
-  Area51 getStuff();
 };
 
 inline
