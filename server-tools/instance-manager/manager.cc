b9fcdfbb0fdf456fd9a083c7fb53aa480f623d9f
petr@mysql.com <>
2005-07-21 14:21:23 +0400

    WL #2713 Change IM behaviour so, that it only reads and alters one config file only. Implemented on brian's request.

--- server-tools/instance-manager/manager.cc
+++ server-tools/instance-manager/manager.cc
@@ -68,7 +68,8 @@ void manager(const Options &options)
   */
 
   User_map user_map;
-  Instance_map instance_map(options.default_mysqld_path, options.first_option);
+  Instance_map instance_map(options.default_mysqld_path,
+                            options.single_defaults_file_option);
   Guardian_thread guardian_thread(thread_registry,
                                   &instance_map,
                                   options.monitoring_interval);
