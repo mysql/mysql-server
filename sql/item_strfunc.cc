95d3bd8bad28cba17a2c7adc36107cb1c77d4ddd
aivanov@mysql.com <>
2005-07-26 20:37:12 +0400

    BUG# <no-such-number> : some fix

--- sql/item_strfunc.cc
+++ sql/item_strfunc.cc
@@ -2130,7 +2130,9 @@ void Item_func_lpad::fix_length_and_dec()
   {
     ulonglong length= ((ulonglong) args[1]->val_int() *
                        collation.collation->mbmaxlen);
-    length= max((ulonglong) args[0]->max_length, length);
+    /*a comment before */
+    length= max((ulonglong)args[0]->max_length, length);
+    /*a comment after */
     if (length >= MAX_BLOB_WIDTH)
     {
       length= MAX_BLOB_WIDTH;
