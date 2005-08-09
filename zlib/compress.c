23f00b61d0d0dfd124bb1c5ca9f2a2dcd436c208
jimw@mysql.com <>
2005-08-09 10:22:48 -0700

    Import zlib-1.2.3 library

--- zlib/compress.c
+++ zlib/compress.c
@@ -1,5 +1,5 @@
 /* compress.c -- compress a memory buffer
- * Copyright (C) 1995-2002 Jean-loup Gailly.
+ * Copyright (C) 1995-2003 Jean-loup Gailly.
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
