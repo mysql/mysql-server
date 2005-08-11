c1a7cfb7d5553ec9d1d2e993e29287b501284129
unknown <jimw@mysql.com>
2005-08-10 18:32:17 -0700

    Fix mysql_info() returning bad data in the results of a multi-statement query that mixed statements that do and do not return info. (Bug #11688)
    mysql-test/r/metadata.result:
      Add new results
    mysql-test/t/metadata.test:
      Add new regression test
    sql-common/client.c:
      Clear mysql->info in free_old_query()

--- sql-common/client.c
+++ sql-common/client.c
@@ -713,8 +713,9 @@ void free_old_query(MYSQL *mysql)
   if (mysql->fields)
     free_root(&mysql->field_alloc,MYF(0));
   init_alloc_root(&mysql->field_alloc,8192,0); /* Assume rowlength < 8192 */
-  mysql->fields=0;
-  mysql->field_count=0;				/* For API */
+  mysql->fields= 0;
+  mysql->field_count= 0;			/* For API */
+  mysql->info= 0;
   DBUG_VOID_RETURN;
 }
 
