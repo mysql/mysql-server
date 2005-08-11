a6975c017ba47b2b4b4ec9261b5d935523b1c202
unknown <acurtis@xiphis.org>
2005-08-11 18:42:22 +0100

    fix after merge
    mysql-test/r/bigint.result:
      fix test after merge
    mysql-test/r/query_cache.result:
      fix test after merge
    mysql-test/r/type_datetime.result:
      fix test after merge

--- sql/sql_union.cc
+++ sql/sql_union.cc
@@ -301,8 +301,8 @@ bool st_select_lex_unit::prepare(THD *thd_arg, select_result *sel_result,
       }
     }
     
-    ulong create_options= (first_select_in_union()->options | thd_arg->options |
-                          TMP_TABLE_ALL_COLUMNS) & ~TMP_TABLE_FORCE_MYISAM;
+    create_options= (first_select_in_union()->options | thd_arg->options |
+                     TMP_TABLE_ALL_COLUMNS) & ~TMP_TABLE_FORCE_MYISAM;
     /*
       Force the temporary table to be a MyISAM table if we're going to use
       fullext functions (MATCH ... AGAINST .. IN BOOLEAN MODE) when reading
