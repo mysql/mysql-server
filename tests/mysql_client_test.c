0dc444ec4efc8d4adcf7124a276c86859a1b3bcb
jimw@mysql.com <>
2005-08-30 12:24:37 -0700

    Fix incorrect spellings of "dropped" in source and tests. (Bug #12828)

--- tests/mysql_client_test.c
+++ tests/mysql_client_test.c
@@ -9177,7 +9177,7 @@ static void test_create_drop()
     rc= mysql_stmt_execute(stmt_drop);
     check_execute(stmt_drop, rc);
     if (!opt_silent)
-      fprintf(stdout, "droped %i\n", i);
+      fprintf(stdout, "dropped %i\n", i);
 
     rc= mysql_stmt_execute(stmt_create_select);
     check_execute(stmt_create, rc);
@@ -9192,7 +9192,7 @@ static void test_create_drop()
     rc= mysql_stmt_execute(stmt_drop);
     check_execute(stmt_drop, rc);
     if (!opt_silent)
-      fprintf(stdout, "droped %i\n", i);
+      fprintf(stdout, "dropped %i\n", i);
   }
 
   mysql_stmt_close(stmt_create);
