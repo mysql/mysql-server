1a9cf5b63b65789652d1c1a01a88c88027710bea
gluh@eagle.intranet.mysql.r18.ru <>
2005-09-09 17:51:13 +0500

    after merge fix

--- sql/item.cc
+++ sql/item.cc
@@ -3627,7 +3627,12 @@ void Item::make_field(Send_field *tmp_field)
 
 void Item_empty_string::make_field(Send_field *tmp_field)
 {
-  init_make_field(tmp_field,FIELD_TYPE_VAR_STRING);
+  enum_field_types type= FIELD_TYPE_VAR_STRING;
+  if (max_length >= 16777216)
+    type= FIELD_TYPE_LONG_BLOB;
+  else if (max_length >= 65536)
+    type= FIELD_TYPE_MEDIUM_BLOB;
+  init_make_field(tmp_field, type);
 }
 
 
