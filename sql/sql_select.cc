0b29417ffa4d4eaa48e52bce635b248794ae396a
monty@mishka.mysql.com <>
2005-09-12 18:48:17 +0300

    Review fixes since last pull Fix for bug #13025; Server crash in filesort because wrong call to handler::position()

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -9307,12 +9307,12 @@ static bool change_group_ref(THD *thd, Item_func *expr, ORDER *group_list,
   if (expr->arg_count)
   {
     Item **arg,**arg_end;
+    bool arg_changed= FALSE;
     for (arg= expr->arguments(),
          arg_end= expr->arguments()+expr->arg_count;
          arg != arg_end; arg++)
     {
       Item *item= *arg;
-      bool arg_changed= FALSE;
       if (item->type() == Item::FIELD_ITEM || item->type() == Item::REF_ITEM)
       {
         ORDER *group_tmp;
@@ -9333,11 +9333,11 @@ static bool change_group_ref(THD *thd, Item_func *expr, ORDER *group_list,
         if (change_group_ref(thd, (Item_func *) item, group_list, &arg_changed))
           return 1;
       }
-      if (arg_changed)
-      {
-        expr->maybe_null= 1;
-        *changed= TRUE;
-      }
+    }
+    if (arg_changed)
+    {
+      expr->maybe_null= 1;
+      *changed= TRUE;
     }
   }
   return 0;
