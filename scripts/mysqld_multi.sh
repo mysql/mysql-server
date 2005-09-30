d1538465e058ba861ca95b5fab8d56ced350a21f
unknown <jani@a193-229-222-105.elisa-laajakaista.fi>
2005-09-30 14:02:31 +0300

    Fix for Bug#12629, "mysqld_multi produces syntax error". Quote here is unneccessary as it should all be handled in quote_opt_arg().

--- scripts/mysqld_multi.sh
+++ scripts/mysqld_multi.sh
@@ -4,7 +4,7 @@ use Getopt::Long;
 use POSIX qw(strftime);
 
 $|=1;
-$VER="2.14";
+$VER="2.15";
 
 $opt_config_file   = undef();
 $opt_example       = 0;
@@ -326,7 +326,6 @@ sub start_mysqlds()
       }
       else
       {
-	$options[$j]=~ s/;/\\;/g;
 	$options[$j]= quote_opt_arg($options[$j]);
 	$tmp.= " $options[$j]";
       }
