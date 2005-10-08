0f60474f290683d2fc468c0081a1058f3f152c16
unknown <serg@serg.mylan>
2005-10-08 16:39:55 +0200

    pthread_handler_decl() changed to be ctags-friendly (and contain extern "C" when necessary)

--- server-tools/instance-manager/guardian.h
+++ server-tools/instance-manager/guardian.h
@@ -31,11 +31,7 @@ class Instance_map;
 class Thread_registry;
 struct GUARD_NODE;
 
-C_MODE_START
-
-pthread_handler_decl(guardian, arg);
-
-C_MODE_END
+pthread_handler_t guardian(void *arg);
 
 struct Guardian_thread_args
 {
