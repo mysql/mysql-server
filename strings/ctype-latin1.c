90664519275454c4c05eefe42eaea096476e7604
bar@mysql.com <>
2005-10-13 19:16:19 +0500

    type_binary.result, type_binary.test:   new file mysql_fix_privilege_tables.sql, mysql_create_system_tables.sh:   Adding true BINARY/VARBINARY: fixing "password" type, not to be 0x00-padding. Many files:   Adding true BINARY/VARBINARY: fixing tests not to output 0x00 bytes.   Adding true BINARY/VARBINARY: new pad_char structure member. ctype-bin.c:   Adding true BINARY/VARBINARY: new pad_char structure member.   New strnxfrm, with two trailing length bytes. field.cc:   Adding true BINARY/VARBINARY.

--- strings/ctype-latin1.c
+++ strings/ctype-latin1.c
@@ -441,6 +441,7 @@ CHARSET_INFO my_charset_latin1=
     1,			/* mbmaxlen  */
     0,			/* min_sort_char */
     255,		/* max_sort_char */
+    ' ',                /* pad char      */
     0,                  /* escape_with_backslash_is_dangerous */
     &my_charset_handler,
     &my_collation_8bit_simple_ci_handler
@@ -740,6 +741,7 @@ CHARSET_INFO my_charset_latin1_german2_ci=
   1,					/* mbmaxlen  */
   0,					/* min_sort_char */
   247,					/* max_sort_char */
+  ' ',                                  /* pad char      */
   0,                                    /* escape_with_backslash_is_dangerous */
   &my_charset_handler,
   &my_collation_german2_ci_handler
@@ -772,6 +774,7 @@ CHARSET_INFO my_charset_latin1_bin=
   1,					/* mbmaxlen  */
   0,					/* min_sort_char */
   255,					/* max_sort_char */
+  ' ',                                  /* pad char      */
   0,                                    /* escape_with_backslash_is_dangerous */
   &my_charset_handler,
   &my_collation_8bit_bin_handler
