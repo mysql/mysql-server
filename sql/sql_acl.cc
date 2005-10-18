5bdb45913cdcc86c5b754a5314ab15904224dceb
bell@sanja.is.com.ua <>
2005-10-18 21:42:26 +0300

    check of ACL initialization in is_acl_user() (BUG#13504)

--- sql/sql_acl.cc
+++ sql/sql_acl.cc
@@ -1487,6 +1487,11 @@ end:
 bool is_acl_user(const char *host, const char *user)
 {
   bool res;
+
+  /* --skip-grants */
+  if (!initialized)
+    return TRUE;
+
   VOID(pthread_mutex_lock(&acl_cache->lock));
   res= find_acl_user(host, user, TRUE) != NULL;
   VOID(pthread_mutex_unlock(&acl_cache->lock));
