07191d88b7cf417370acd90768cafed4ad1be2f5
ramil@mysql.com <>
2005-10-24 16:45:27 +0500

    a fix (bug #14207: strange change of values CHARACTER_OCTET_LENGTH and CHARACTER_MAXIMUM_LENGTH).

--- sql/sql_show.cc
+++ sql/sql_show.cc
@@ -2559,11 +2559,11 @@ static int get_schema_column_record(THD *thd, struct st_table_list *tables,
     is_blob= (field->type() == FIELD_TYPE_BLOB);
     if (field->has_charset() || is_blob)
     {
-      longlong c_octet_len= is_blob ? (longlong) field->max_length() :
+      longlong char_max_len= is_blob ? (longlong) field->max_length() :
         (longlong) field->max_length()/field->charset()->mbmaxlen;
-      table->field[8]->store((longlong) field->max_length(), TRUE);
+      table->field[8]->store(char_max_len, TRUE);
       table->field[8]->set_notnull();
-      table->field[9]->store(c_octet_len, TRUE);
+      table->field[9]->store((longlong) field->max_length(), TRUE);
       table->field[9]->set_notnull();
     }
 
