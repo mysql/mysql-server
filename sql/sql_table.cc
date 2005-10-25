115bf43602424f2bba326efab6d7ec51c66ac4e5
unknown <sergefp@mysql.com>
2005-10-26 00:56:17 +0400

    BUG#14139: When handling "CREATE TABLE(field_X type_spec,...) SELECT smth AS field_X, ...." avoid multiplying length of field_X by charset->mbmaxlen twice when calculating space required for field_X in the new table.
    mysql-test/r/create.result:
      Testcase for BUG#14139
    mysql-test/t/create.test:
      Testcase for BUG#14139
    sql/field.cc:
      BUG#14139: Make create_length_to_internal_length() save length-in-characters in 
      create_field::chars_length.
    sql/field.h:
      BUG#14139: Add create_length::chars_length where we save length-in-characters, added comments.
    sql/sql_table.cc:
      BUG#14139: When handling "CREATE TABLE(field_X type_spec,...) SELECT smth AS field_X, ...."
      we get two instances of create_field: (1) is occurence of field_X in create list, and (2) is
      in select list. If we figure they both refer to the same field, we "join" them according to
      some rule that is not explicitly specified anywhere.
      When we do this "join", create_field::length already contains length-in-bytes for both, so
      when we transfer field length (in characters) from (1) to (2), use length-in-characters that
      we have saved in create_length::chars_length.

--- sql/sql_table.cc
+++ sql/sql_table.cc
@@ -643,8 +643,8 @@ int mysql_prepare_table(THD *thd, HA_CREATE_INFO *create_info,
 	  sql_field->charset=		(dup_field->charset ?
 					 dup_field->charset :
 					 create_info->default_table_charset);
-	  sql_field->length=		dup_field->length;
-	  sql_field->pack_length=	dup_field->pack_length;
+	  sql_field->length=		dup_field->chars_length;
+          sql_field->pack_length=	dup_field->pack_length;
 	  sql_field->create_length_to_internal_length();
 	  sql_field->decimals=		dup_field->decimals;
 	  sql_field->flags=		dup_field->flags;
