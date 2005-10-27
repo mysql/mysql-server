1b574f5bdf347ddfeea3b6b704f1089ebb99ea98
unknown <jani@ua141d10.elisa.omakaista.fi>
2005-10-28 02:36:19 +0300

    Merge 4.1 - 5.0
    mysql-test/r/insert_select.result:
      Merge from 4.1 to 5.0.
    mysql-test/r/select.result:
      Merge from 4.1 to 5.0.
    mysql-test/t/insert_select.test:
      Merge from 4.1 to 5.0.
    mysys/my_handler.c:
      Merge from 4.1 to 5.0.
    sql/item.cc:
      Merge from 4.1 to 5.0.
    sql/item_timefunc.cc:
      Imported bug fix from 4.1 to 5.0. (Bug#14016)
    sql/item_timefunc.h:
      Imported bug fix from 4.1 to 5.0. (Bug#14016)

--- sql/item.cc
+++ sql/item.cc
@@ -755,7 +755,8 @@ bool Item_string::eq(const Item *item, bool binary_cmp) const
   {
     if (binary_cmp)
       return !stringcmp(&str_value, &item->str_value);
-    return !sortcmp(&str_value, &item->str_value, collation.collation);
+    return (collation.collation == item->collation.collation &&
+	    !sortcmp(&str_value, &item->str_value, collation.collation));
   }
   return 0;
 }
