abf4f669a62baf64b8b1490e8682a0fdc5a024e6
unknown <sergefp@mysql.com>
2005-11-01 08:36:49 +0300

    BUG#13126: Post-review fixes: better comments, some function renaming.
    sql/mysql_priv.h:
      BUG#13126: Post-review fixes: better comments

--- sql/mysql_priv.h
+++ sql/mysql_priv.h
@@ -45,8 +45,9 @@ typedef ulonglong table_map;          /* Used for table bits in join */
 typedef Bitmap<64> key_map;           /* Used for finding keys */
 typedef ulong key_part_map;           /* Used for finding key parts */
 /*
-  Used for nested join bits within a scope of a join (applicable to non-unary
-  nested joins that have not been simplified away)
+  Used to identify NESTED_JOIN structures within a join (applicable only to
+  structures that have not been simplified away and embed more the one
+  element)
 */
 typedef ulonglong nested_join_map;
 
