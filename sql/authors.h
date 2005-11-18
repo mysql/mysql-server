c20fe5dffd43edf1ebec8c17b58ff79b4808a83b
konstantin@mysql.com <>
2005-11-18 14:39:44 +0300

    More authors.

--- sql/authors.h
+++ sql/authors.h
@@ -12,7 +12,7 @@ struct show_table_authors_st {
 };
 
 struct show_table_authors_st show_table_authors[]= {
-  { "Brian \"Krow\" Aker", "Seattle, WA. USA", 
+  { "Brian \"Krow\" Aker", "Seattle, WA. USA",
     "Architecture, archive, federated, buncha of little stuff :)" },
   { "David Axmark", "Uppsala, Sweden", "Small stuff long time ago, Monty ripped it out!"},
   { "Omer BarNir", "Sunnyvale, CA. USA", "Testing (sometimes) and general QA stuff"},
@@ -23,5 +23,11 @@ struct show_table_authors_st show_table_authors[]= {
   { "Carsten Segieth (Pino)", "Fredersdorf, Germany", "Testing - Server"},
   { "Punita Srivastava", "Austin, TX. USA", "Testing - Merlin"},
   { "Alexey Stroganov (Ranger)", "Lugansk, Ukraine", "Testing - Benchmarks"},
+  { "Oleksandr Byelkin", "Lugansk, Ukraine", "Query Cache (4.0), Subqueries (4.1), Views (5.0)"},
+  { "Dmitri Lenev", "Moscow, Russia", "Time zones support (4.1), Triggers (5.0)"},
+  { "Konstantin Osipov", "Moscow, Russia", "Prepared statements (4.1), Cursors (5.0)"},
+  { "Petr Chardin", "Moscow, Russia", "Instance Manager (5.0)" },
+  { "Sergey Vojtovich", "Izhevsk, Russia", "Plugins infrastructure (5.1)" },
+  { "Alexander Nozdrin", "Moscow, Russia", "Bugfixing (Stored Procedures, 5.0)" },
   {NULL, NULL, NULL}
 };
