056d7266c7f3b10b2a054cb5bb534981e49408ee
unknown <bell@sanja.is.com.ua>
2005-11-23 12:26:07 +0200

    A name of macro is fixed.

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -2086,7 +2086,7 @@ public:
 class my_var : public Sql_alloc  {
 public:
   LEX_STRING s;
-#ifndef DEBUG_OFF
+#ifndef DBUG_OFF
   /*
     Routine to which this Item_splocal belongs. Used for checking if correct
     runtime context is used for variable handling.
