8c0f465b064fa2be15351ca4bedcc1e9324f7024
msvensson@neptunus.(none) <>
2005-12-07 14:52:37 +0100

    Fix memory leak

--- client/mysqltest.c
+++ client/mysqltest.c
@@ -4015,6 +4015,7 @@ static void free_re(void)
   my_regfree(&ps_re);
   my_regfree(&sp_re);
   my_regfree(&view_re);
+  my_regex_end();
 }
 
 /****************************************************************************/
