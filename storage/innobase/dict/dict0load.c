f8d6d13781d45ecfe2893d9d2a2ff4564da4eb97
unknown <aivanov@mysql.com>
2005-12-08 15:00:18 +0300

    The innodb-5.0-ss52 snapshot changes were erroneously auto-merged  to 5.1. Undoing the merge.
    mysql-test/r/innodb.result:
      Undoing erroneous auto-merge 5.0 -> 5.1
    mysql-test/t/innodb.test:
      Undoing erroneous auto-merge 5.0 -> 5.1
    sql/ha_innodb.cc:
      Undoing erroneous auto-merge 5.0 -> 5.1
    sql/ha_innodb.h:
      Undoing erroneous auto-merge 5.0 -> 5.1
    storage/innobase/btr/btr0sea.c:
      Undoing erroneous auto-merge 5.0 -> 5.1
    storage/innobase/dict/dict0dict.c:
      Undoing erroneous auto-merge 5.0 -> 5.1
    storage/innobase/dict/dict0load.c:
      Undoing erroneous auto-merge 5.0 -> 5.1
    storage/innobase/include/buf0buf.h:
      Undoing erroneous auto-merge 5.0 -> 5.1
    storage/innobase/include/dict0dict.h:
      Undoing erroneous auto-merge 5.0 -> 5.1
    storage/innobase/include/dict0load.h:
      Undoing erroneous auto-merge 5.0 -> 5.1
    storage/innobase/include/rem0cmp.h:
      Undoing erroneous auto-merge 5.0 -> 5.1
    storage/innobase/rem/rem0cmp.c:
      Undoing erroneous auto-merge 5.0 -> 5.1
    storage/innobase/row/row0mysql.c:
      Undoing erroneous auto-merge 5.0 -> 5.1

--- storage/innobase/dict/dict0load.c
+++ storage/innobase/dict/dict0load.c
@@ -1091,7 +1091,7 @@ dict_load_foreign(
 				/* out: DB_SUCCESS or error code */
 	const char*	id,	/* in: foreign constraint id as a
 				null-terminated string */
-	ibool		check_charsets)/* in: TRUE=check charset compatibility */
+	ibool		check_types)/* in: TRUE=check type compatibility */
 {	
 	dict_foreign_t*	foreign;
 	dict_table_t*	sys_foreign;
@@ -1204,7 +1204,7 @@ dict_load_foreign(
 	a new foreign key constraint but loading one from the data
 	dictionary. */
 
-	return(dict_foreign_add_to_cache(foreign, check_charsets));
+	return(dict_foreign_add_to_cache(foreign, check_types));
 }
 
 /***************************************************************************
@@ -1219,8 +1219,7 @@ dict_load_foreigns(
 /*===============*/
 					/* out: DB_SUCCESS or error code */
 	const char*	table_name,	/* in: table name */
-	ibool		check_charsets)	/* in: TRUE=check charset
-					compatibility */
+	ibool		check_types)	/* in: TRUE=check type compatibility */
 {
 	btr_pcur_t	pcur;
 	mem_heap_t* 	heap;
@@ -1320,7 +1319,7 @@ loop:
 
 	/* Load the foreign constraint definition to the dictionary cache */
 	
-	err = dict_load_foreign(id, check_charsets);
+	err = dict_load_foreign(id, check_types);
 
 	if (err != DB_SUCCESS) {
 		btr_pcur_close(&pcur);
