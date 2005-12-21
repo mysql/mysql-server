c3c43e129bfbad98917e5e01b7d926ae69706a21
unknown <acurtis@xiphis.org>
2005-12-21 12:50:50 -0800

    plugin version changes
    include/plugin.h:
      relocate version
    sql/handler.cc:
      no version checks here
    sql/handler.h:
      version is kinda short
    sql/sql_show.cc:
      version is a short, rearrange and show all
    storage/csv/ha_tina.cc:
      rearrange version
    storage/example/ha_example.cc:
      rearrange version

--- include/plugin.h
+++ include/plugin.h
@@ -21,7 +21,7 @@
   Plugin API. Common for all plugin types.
 */
 
-#define MYSQL_PLUGIN_INTERFACE_VERSION 0x0000
+#define MYSQL_PLUGIN_INTERFACE_VERSION 0x0001
 
 /*
   The allowable types of plugins
@@ -51,11 +51,11 @@ struct st_mysql_plugin
   int type;             /* the plugin type (a MYSQL_XXX_PLUGIN value)   */
   void *info;           /* pointer to type-specific plugin descriptor   */
   const char *name;     /* plugin name                                  */
-  uint version;         /* plugin version                               */
   const char *author;   /* plugin author (for SHOW PLUGINS)             */
   const char *descr;    /* general descriptive text (for SHOW PLUGINS ) */
   int (*init)(void);    /* the function to invoke when plugin is loaded */
   int (*deinit)(void);  /* the function to invoke when plugin is unloaded */
+  uint version;         /* plugin version (for SHOW PLUGINS)            */
 };
 
 /*************************************************************************
