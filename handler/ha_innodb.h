6234dd648e2b24b393f114a2c8ffd1685a66d8da
marko <Unknown>
2006-01-19 13:31:02 +0000

    Port r146 from branches/5.0:
    Make innodb_flush_log_at_trx_commit a settable global variable.

--- handler/ha_innodb.h
+++ handler/ha_innodb.h
@@ -208,7 +208,6 @@ class ha_innobase: public handler
 };
 
 extern struct show_var_st innodb_status_variables[];
-extern uint innobase_flush_log_at_trx_commit;
 extern ulong innobase_fast_shutdown;
 extern ulong innobase_large_page_size;
 extern long innobase_mirrored_log_groups, innobase_log_files_in_group;
@@ -244,6 +243,7 @@ extern ulong srv_n_free_tickets_to_enter;
 extern ulong srv_thread_sleep_delay;
 extern ulong srv_thread_concurrency;
 extern ulong srv_commit_concurrency;
+extern ulong srv_flush_log_at_trx_commit;
 }
 
 bool innobase_init(void);
