e3523a0f6cc1938ca188882ce18f0a7051959413
unknown <knielsen@mysql.com>
2006-01-20 13:23:24 +0100

    Portability compilation fixes.
    storage/ndb/src/ndbapi/NdbEventOperationImpl.cpp:
      Fix sol10-amd64-a compile failure, conflict with system headers.
    sql/log.cc:
      Rename macros to fix AIX conflict with system LOG_GENERAL macros.
    sql/mysql_priv.h:
      Rename macros to fix AIX conflict with system LOG_GENERAL macros.
    sql/sql_delete.cc:
      Rename macros to fix AIX conflict with system LOG_GENERAL macros.
    sql/table.cc:
      Rename macros to fix AIX conflict with system LOG_GENERAL macros.
    sql/sql_parse.cc:
      Fix HPUX compile failure, initializing char * with const char *

--- sql/mysql_priv.h
+++ sql/mysql_priv.h
@@ -1189,8 +1189,8 @@ void sql_print_warning(const char *format, ...);
 void sql_print_information(const char *format, ...);
 
 /* type of the log table */
-#define LOG_SLOW 1
-#define LOG_GENERAL 2
+#define QUERY_LOG_SLOW 1
+#define QUERY_LOG_GENERAL 2
 
 int error_log_print(enum loglevel level, const char *format,
                     va_list args);
