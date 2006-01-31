3eb9e1702cd444eaa6e26c989f871cbf242bbe05
reggie@big_geek. <>
2006-01-31 07:52:16 -0600

    initial cmake fileset.  It is not production ready but stable enough to start working with

---
+++ win/configure.js
@@ -0,0 +1,88 @@
+// Configure.js
+
+ForReading = 1;
+ForWriting = 2;
+ForAppending = 8;
+
+try 
+{
+	// first we attempt to open the main configure.in file
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
+    
+	var args = WScript.Arguments
+	
+    var configfile = fso.CreateTextFile("configure.data", true);
+    for (i=0; i < args.Count(); i++)
+    {
+		configfile.WriteLine(args.Item(i));
+    }
+    configfile.Close();
+    
+	fso = null;
+
+    WScript.Echo("done!");
+}
+catch (e)
+{
+    WScript.Echo("Error: " + e.description);
+}
+
+function GetValue(str, key)
+{
+    var pos = str.indexOf(key+'=');
+    if (pos == -1) return null;
+    pos += key.length + 1;
+    var end = str.indexOf("\n", pos);
+    if (str.charAt(pos) == "\"")
+		pos++;
+	if (str.charAt(end-1) == "\"")
+		end--;
+    return str.substring(pos, end);    
+}
+
+function GetVersion(str)
+{
+    var key = "AM_INIT_AUTOMAKE(mysql, ";
+    var pos = str.indexOf(key); //5.0.6-beta)
+    if (pos == -1) return null;
+    pos += key.length;
+    var end = str.indexOf(")", pos);
+    if (end == -1) return null;
+    return str.substring(pos, end);
+}
+
+function GetBaseVersion(version)
+{
+    var dot = version.indexOf(".");
+    if (dot == -1) return null;
+    dot = version.indexOf(".", dot+1);
+    if (dot == -1) dot = version.length;
+    return version.substring(0, dot);
+}
+
+function GetVersionId(version)
+{
+    var dot = version.indexOf(".");
+    if (dot == -1) return null;
+    var major = parseInt(version.substring(0, dot), 10);
+    
+    dot++;
+    var nextdot = version.indexOf(".", dot);
+    if (nextdot == -1) return null;
+    var minor = parseInt(version.substring(dot, nextdot), 10);
+    dot = nextdot+1;
+    
+    var stop = version.indexOf("-", dot);
+    if (stop == -1) stop = version.length;
+    var build = parseInt(version.substring(dot, stop), 10);
+    
+    var id = major;
+    if (minor < 10)
+        id += '0';
+    id += minor;
+    if (build < 10)
+        id += '0';
+    id += build;
+    return id;
+}
+
