e98042268ff409bde6a2d8300ba943944e7e8d59
reggie@big_geek. <>
2006-01-31 08:41:39 -0600

    change win32 to win

--- win/build-vs71.bat
+++ win/build-vs71.bat
@@ -1,7 +1,7 @@
 @echo off
-win32\cmakefiles\deploy
+win\cmakefiles\deploy
 del cmakecache.txt
-copy win32\vs71cache.txt cmakecache.txt
+copy win\vs71cache.txt cmakecache.txt
 cmake -G "Visual Studio 7 .NET 2003"
-copy cmakecache.txt win32\vs71cache.txt
+copy cmakecache.txt win\vs71cache.txt
 
