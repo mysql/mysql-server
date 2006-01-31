b4f1a47643776eea3c81d528f7f4adb3032e899a
reggie@big_geek. <>
2006-01-31 08:41:39 -0600

    change win32 to win

--- win/build-vs8.bat
+++ win/build-vs8.bat
@@ -1,6 +1,6 @@
 @echo off
-win32\cmakefiles\deploy
+win\cmakefiles\deploy
 del cmakecache.txt
-copy win32\vs8cache.txt cmakecache.txt
+copy win\vs8cache.txt cmakecache.txt
 cmake -G "Visual Studio 8 2005"
-copy cmakecache.txt win32\vs8cache.txt
+copy cmakecache.txt win\vs8cache.txt
