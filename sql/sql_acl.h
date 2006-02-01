730a711f931c3801eb9dd88bfdc5be7f7e99be3f
dlenev@mysql.com <>
2006-02-01 21:27:12 +0300

    Addition to the fix for BUG#9412 "Triggers: should have trigger privilege".
    When we add new privilege we should update mysql-test/lib/init_db.sql
    accordingly.

--- sql/sql_acl.h
+++ sql/sql_acl.h
@@ -49,6 +49,7 @@
   1. static struct show_privileges_st sys_privileges[]
   2. static const char *command_array[] and static uint command_lengths[]
   3. mysql_create_system_tables.sh, mysql_fix_privilege_tables.sql
+     and mysql-test/lib/init_db.sql
   4. acl_init() or whatever - to define behaviour for old privilege tables
   5. sql_yacc.yy - for GRANT/REVOKE to work
 */
