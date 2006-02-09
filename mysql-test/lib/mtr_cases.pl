0e9e1f2c5d05310e8c345a28908e5d82d72021e5
msvensson@neptunus.(none) <>
2006-02-09 09:40:48 +0100

    Start cluster only for testcases that really needs it and stop it when not needed by the tescases. This will save test time for those testcases that does not need cluster, but need a restart, as they dont have to wait the extra time it would take for cluster to restart. It will also save time for other testcases, as cluster does not need to be contacted for each table to be dropped or created. Backport from 5.1

--- mysql-test/lib/mtr_cases.pl
+++ mysql-test/lib/mtr_cases.pl
@@ -252,6 +252,26 @@ sub collect_one_test_case($$$$$$$) {
     $tinfo->{'slave_restart'}= 1;
   }
 
+  # Cluster is needed by test case if testname contains ndb
+  if ( ( $::opt_with_ndbcluster or $::glob_use_running_ndbcluster ) and
+       defined mtr_match_substring($tname,"ndb") )
+  {
+    if ( $::opt_skip_ndbcluster )
+    {
+      $tinfo->{'ndb_test'}= 0;
+      $tinfo->{'skip'}= 1;
+      return;
+    }
+    else
+    {
+      $tinfo->{'ndb_test'}= 1;
+    }
+  }
+  else
+  {
+    $tinfo->{'ndb_test'}= 0;
+  }
+
   # FIXME what about embedded_server + ndbcluster, skip ?!
 
   my $master_opt_file= "$testdir/$tname-master.opt";
