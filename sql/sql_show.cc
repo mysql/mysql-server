da94b1662b394c4146e553dd13572e800772f9cf
SergeyV@selena. <>
2006-02-13 19:53:34 +0300

    Fixes bug #15943. resets error flag for show create view command, to allow proper processing of multiple sql statements sent as a single command.

--- sql/sql_show.cc
+++ sql/sql_show.cc
@@ -365,6 +365,13 @@ mysqld_show_create(THD *thd, TABLE_LIST *table_list)
   {
     if (!table_list->view || thd->net.last_errno != ER_VIEW_INVALID)
       DBUG_RETURN(TRUE);
+
+    /* 
+       Need this for proper processing of multiple sql statements        
+       sent as single command     
+    */    
+    thd->net.report_error= 0;
+
     /*
       Clear all messages with 'error' level status and
       issue a warning with 'warning' level status in 
