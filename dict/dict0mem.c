331c8eaada61dc16ee426015e4fdc71a818e3d88
marko <>
2006-02-21 14:43:23 +0000

    branches/zip: Remove temporary hooks for testing BLOB compression.
    dict_mem_table_create(): Initialize table->zip.
    
    btr0cur.c: Distinguish compressed BLOBs based on page_zip or table->zip.

--- dict/dict0mem.c
+++ dict/dict0mem.c
@@ -57,6 +57,7 @@ dict_mem_table_create(
 	table->ibd_file_missing = FALSE;
 	table->tablespace_discarded = FALSE;
 	table->comp = comp;
+	table->zip = 0;
 	table->n_def = 0;
 	table->n_cols = n_cols + DATA_N_SYS_COLS;
 	table->mem_fix = 0;
