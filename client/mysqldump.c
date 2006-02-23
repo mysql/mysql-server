24c44a9bea0f3208e1e28db30e91ca3c49143e75
unknown <msvensson@neptunus.(none)>
2006-02-23 14:06:29 +0100

    Aix52 build fix
    client/mysqldump.c:
      Declare variable in start of function

--- client/mysqldump.c
+++ client/mysqldump.c
@@ -3238,10 +3238,10 @@ static int replace(DYNAMIC_STRING *ds_str,
                    const char *search_str, ulong search_len,
                    const char *replace_str, ulong replace_len)
 {
+  DYNAMIC_STRING ds_tmp;
   const char *start= strstr(ds_str->str, search_str);
   if (!start)
     return 1;
-  DYNAMIC_STRING ds_tmp;
   init_dynamic_string(&ds_tmp, "",
 		      ds_str->length + replace_len, 256);
   dynstr_append_mem(&ds_tmp, ds_str->str, start - ds_str->str);
