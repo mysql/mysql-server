460ac2bc00ef4c0d46475b6aaac71f945587bf30
acurtis@xiphis.org <>
2006-03-16 00:15:23 -0800

    Bug#14575   ¨MySQL server crashes if you try to access to InnoDB table¨   crash caused by schizophrenic mysqld - 2 memory locations for logically same function   with conflicting values.   Fixed by backporting from 5.1 changes to have_xyz_db declarations.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -472,13 +472,10 @@ CHARSET_INFO *system_charset_info, *files_charset_info ;
 CHARSET_INFO *national_charset_info, *table_alias_charset;
 CHARSET_INFO *character_set_filesystem;
 
-SHOW_COMP_OPTION have_berkeley_db, have_innodb, have_isam, have_ndbcluster, 
-  have_example_db, have_archive_db, have_csv_db;
-SHOW_COMP_OPTION have_federated_db;
+SHOW_COMP_OPTION have_isam;
 SHOW_COMP_OPTION have_raid, have_openssl, have_symlink, have_query_cache;
 SHOW_COMP_OPTION have_geometry, have_rtree_keys;
 SHOW_COMP_OPTION have_crypt, have_compress;
-SHOW_COMP_OPTION have_blackhole_db;
 
 /* Thread specific variables */
 
@@ -7394,6 +7391,30 @@ static void create_pid_file()
   sql_perror("Can't start server: can't create PID file");
   exit(1);  
 }
+
+
+/*****************************************************************************
+  Instantiate have_xyx for missing storage engines
+*****************************************************************************/
+#undef have_berkeley_db
+#undef have_innodb
+#undef have_ndbcluster
+#undef have_example_db
+#undef have_archive_db
+#undef have_csv_db
+#undef have_federated_db
+#undef have_partition_db
+#undef have_blackhole_db
+
+SHOW_COMP_OPTION have_berkeley_db= SHOW_OPTION_NO;
+SHOW_COMP_OPTION have_innodb= SHOW_OPTION_NO;
+SHOW_COMP_OPTION have_ndbcluster= SHOW_OPTION_NO;
+SHOW_COMP_OPTION have_example_db= SHOW_OPTION_NO;
+SHOW_COMP_OPTION have_archive_db= SHOW_OPTION_NO;
+SHOW_COMP_OPTION have_csv_db= SHOW_OPTION_NO;
+SHOW_COMP_OPTION have_federated_db= SHOW_OPTION_NO;
+SHOW_COMP_OPTION have_partition_db= SHOW_OPTION_NO;
+SHOW_COMP_OPTION have_blackhole_db= SHOW_OPTION_NO;
 
 
 /*****************************************************************************
