7d529a93f88ae5d1a97f97db87a405724c34034a
marko <>
2006-03-17 08:20:14 +0000

    Merge r328:340 from branches/5.0:
    r340: Fix http://bugs.mysql.com/bug.php?id=18283 in ha_innodb.cc and
    http://bugs.mysql.com/bug.php?id=18238 in InnoDB
    
    r339: Remove disk space leak on update of BLOB columns (Bug #18252).
    
    btr_cur_pessimistic_update(): Invoke rec_get_offsets() after
    rec_set_field_extern_bits().
    
    btr_store_big_rec_extern_fields(): Note that offsets will no longer be
    valid after calling this function.
    
    r338: Fix bug 18238 : check in pessimistic insert and update if the buffer
    pool is exhausted by locks

--- handler/ha_innodb.cc
+++ handler/ha_innodb.cc
@@ -539,11 +539,18 @@ convert_error_code_to_mysql(
 
 		return(HA_ERR_NO_SAVEPOINT);
 	} else if (error == (int) DB_LOCK_TABLE_FULL) {
+ 		/* Since we rolled back the whole transaction, we must
+ 		tell it also to MySQL so that MySQL knows to empty the
+ 		cached binlog for this transaction */
 
-		return(HA_ERR_LOCK_TABLE_FULL);
-	} else {
-		return(-1);			// Unknown error
-	}
+ 		if (thd) {
+ 			ha_rollback(thd);
+ 		}
+
+    		return(HA_ERR_LOCK_TABLE_FULL);
+    	} else {
+    		return(-1);			// Unknown error
+    	}
 }
 
 /*****************************************************************
