556fbe7d68764885cc22a9b37c10bb2775c60f1a
georg@lmy002.wdf.sap.corp <>
2006-03-21 22:02:36 +0100

    fixed merge problem (bk commit - 5.1 tree (anozdrin:1.2163)     (approved by kostja)

--- libmysql/libmysql.c
+++ libmysql/libmysql.c
@@ -2816,10 +2816,11 @@ my_bool STDCALL mysql_stmt_attr_get(MYSQL_STMT *stmt,
 {
   switch (attr_type) {
   case STMT_ATTR_UPDATE_MAX_LENGTH:
+    *(my_bool*) value= stmt->update_max_length;
     break;
   case STMT_ATTR_CURSOR_TYPE:
     *(ulong*) value= stmt->flags;
-      break;
+    break;
   case STMT_ATTR_PREFETCH_ROWS:
     *(ulong*) value= stmt->prefetch_rows;
     break;
