32c697806f04ba0272ebfd55f65cfae18b826037
mats@mysql.com <>
2006-04-06 17:18:12 +0200

    WL#3206 (Adding unit tests): Moving mytap library into unittest/ Adding 'test' target to make and run unit tests. Minor fixes.

--- unittest/mysys/bitmap.t.c
+++ unittest/mysys/bitmap.t.c
@@ -1,10 +1,28 @@
+/* Copyright (C) 2006 MySQL AB
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+   This test was copied from the unit test inside the
+   mysys/my_bitmap.c file and adapted by Mats Kindahl to use the mytap
+   library.
+*/
 
 #include <tap.h>
 
 #include <my_global.h>
-#include "my_bitmap.h"
-
-#include <string.h>
+#include <my_bitmap.h>
 
 static void bitmap_print(MY_BITMAP *map)
 {
