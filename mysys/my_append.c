d4f7518e3d4fe06fd3035fe5b5ec8a1904d2442e
unknown <msvensson@shellback.>
2006-04-11 20:12:57 +0200

    Remove unused defined USES_TYPES

--- mysys/my_append.c
+++ mysys/my_append.c
@@ -14,7 +14,6 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
-#define USES_TYPES				/* sys/types is included */
 #include "mysys_priv.h"
 #include <sys/stat.h>
 #include <m_string.h>
