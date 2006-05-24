68cd158fd2bd1cd737eb7c8ad680672ec05bb379
mikael@c-870ae253.1238-1-64736c10.cust.bredbandsbolaget.se <>
2006-05-24 15:01:03 -0400

    review fix

--- sql/partition_info.cc
+++ sql/partition_info.cc
@@ -442,7 +442,7 @@ bool partition_info::check_engine_mix(handlerton **engine_array, uint no_parts)
       DBUG_RETURN(TRUE);
     }
   } while (++i < no_parts);
-  if (ha_legacy_type(engine_array[0]) == DB_TYPE_MRG_MYISAM)
+  if (engine_array[0] == &myisammrg_hton)
   {
     my_error(ER_PARTITION_MERGE_ERROR, MYF(0));
     DBUG_RETURN(TRUE);
