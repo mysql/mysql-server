dd370c9a23bcdbdef04e005437f2bf5e767fef1a
unknown <jani@a193-229-222-105.elisa-laajakaista.fi>
2006-05-31 11:18:58 +0300

    Merge from 5.0
    mysql-test/extra/rpl_tests/rpl_insert_id.test:
      Manual merge from 5.0. In 5.0 this was in another file.
    sql/field.cc:
      Removed unused variable.

--- sql/field.cc
+++ sql/field.cc
@@ -1223,7 +1223,7 @@ Field::Field(char *ptr_arg,uint32 length_arg,uchar *null_ptr_arg,
    field_name(field_name_arg),
    query_id(0), key_start(0), part_of_key(0), part_of_sortkey(0),
    unireg_check(unireg_check_arg),
-   field_length(length_arg), null_bit(null_bit_arg), dflt_field(0)
+   field_length(length_arg), null_bit(null_bit_arg)
 {
   flags=null_ptr ? 0: NOT_NULL_FLAG;
   comment.str= (char*) "";
