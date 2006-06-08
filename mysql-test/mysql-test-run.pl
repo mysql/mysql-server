a26c9c31a19cc89b9ae7cd5e56d1c46fe9d0edd5
joerg@mysql.com <>
2006-06-08 19:55:22 +0200

    mysql-test/mysql-test-run.pl  :  Output the usage error if one is given. (Found when fixing bug#20303)

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -2459,6 +2459,13 @@ sub valgrind_arguments {
 ##############################################################################
 
 sub usage ($) {
+  my $message= shift;
+
+  if ( $message )
+  {
+    print STDERR "$message \n";
+  }
+
   print STDERR <<HERE;
 
 mysql-test-run [ OPTIONS ] [ TESTCASE ]
