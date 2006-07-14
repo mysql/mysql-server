b170112caee29fc078073785756d8a8a23450c46
igreenhoe/greenman@anubis.greendragongames.com <>
2006-07-14 05:56:30 -0700

    4.1 -> 5.0 merge fixes

--- sql/sql_lex.cc
+++ sql/sql_lex.cc
@@ -124,7 +124,6 @@ void lex_start(THD *thd, uchar *buf,uint length)
   lex->value_list.empty();
   lex->update_list.empty();
   lex->param_list.empty();
-  lex->auxilliary_table_list.empty();
   lex->view_list.empty();
   lex->prepared_stmt_params.empty();
   lex->unit.next= lex->unit.master=
