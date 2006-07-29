cc98d26a2318bd5c3e9169a39eb90cba8db46307
kroki/tomash@moonlight.intranet <>
2006-07-29 13:50:08 +0400

    Fix after manual merge.

--- mysys/my_bitmap.c
+++ mysys/my_bitmap.c
@@ -445,7 +445,7 @@ uint bitmap_get_first(const MY_BITMAP *map)
 {
   uchar *bitmap=map->bitmap;
   uint bit_found = MY_BIT_NONE;
-  uint bitmap_size=map->bitmap_size*8;
+  uint bitmap_size=map->bitmap_size;
   uint i;
 
   DBUG_ASSERT(map->bitmap);
