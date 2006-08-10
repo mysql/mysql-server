e8524352dc79e9231fd2c2d835bf0e479ba6e77a
unknown <guilhem@gbichot3.local>
2006-08-10 17:24:50 +0200

    adding handler flag missing from previous merge, for Maria
    sql/ha_maria.cc:
      flag missing from previous merge

--- sql/ha_maria.cc
+++ sql/ha_maria.cc
@@ -155,7 +155,7 @@ handler(&maria_hton, table_arg), file(0),
 int_table_flags(HA_NULL_IN_KEY | HA_CAN_FULLTEXT | HA_CAN_SQL_HANDLER |
                 HA_DUPLICATE_POS | HA_CAN_INDEX_BLOBS | HA_AUTO_PART_KEY |
                 HA_FILE_BASED | HA_CAN_GEOMETRY | HA_NO_TRANSACTIONS |
-                HA_CAN_INSERT_DELAYED | HA_CAN_BIT_FIELD |
+                HA_CAN_INSERT_DELAYED | HA_CAN_BIT_FIELD | HA_CAN_RTREEKEYS |
                 HA_HAS_RECORDS | HA_STATS_RECORDS_IS_EXACT),
 can_enable_indexes(1)
 {}
