3d085927ba1b9eb1468fca83bd10c9a5b4dc71f9
marko <Unknown>
2006-08-15 14:28:09 +0000

    branches/zip: Disable unused function ut_dulint_sort().

--- ut/ut0byte.c
+++ ut/ut0byte.c
@@ -12,13 +12,14 @@ Created 5/11/1994 Heikki Tuuri
 #include "ut0byte.ic"
 #endif
 
-#include "ut0sort.h"
-
 /* Zero value for a dulint */
 dulint	ut_dulint_zero		= {0, 0};
 
 /* Maximum value for a dulint */
 dulint	ut_dulint_max		= {0xFFFFFFFFUL, 0xFFFFFFFFUL};
+
+#ifdef notdefined /* unused code */
+#include "ut0sort.h"
 
 /****************************************************************
 Sort function for dulint arrays. */
@@ -29,3 +30,4 @@ ut_dulint_sort(dulint* arr, dulint* aux_arr, ulint low, ulint high)
 	UT_SORT_FUNCTION_BODY(ut_dulint_sort, arr, aux_arr, low, high,
 				ut_dulint_cmp);
 }
+#endif /* notdefined */
