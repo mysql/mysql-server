c8d1c498fb4a7298e5c5a7e66c541f2ef7dcd325
tnurnberg@salvation.intern.azundris.com <>
2006-08-18 14:16:11 +0200

    innodb r702 innodb r719

--- innobase/row/row0sel.c
+++ innobase/row/row0sel.c
@@ -3101,7 +3101,7 @@ row_search_for_mysql(
 "InnoDB: Have you deleted the .ibd file from the database directory under\n"
 "InnoDB: the MySQL datadir, or have you used DISCARD TABLESPACE?\n"
 "InnoDB: Look from\n"
-"http://dev.mysql.com/doc/mysql/en/InnoDB_troubleshooting_datadict.html\n"
+"InnoDB: http://dev.mysql.com/doc/refman/5.0/en/innodb-troubleshooting.html\n"
 "InnoDB: how you can resolve the problem.\n",
 				prebuilt->table->name);
 
