e4cc40f88c23ea3d6649c7ca19380696e413be5f
unknown <mskold/marty@mysql.com/linux.site>
2006-09-21 16:54:54 +0200

    Bug #21072  Duplicate key error in NDB references wrong key: wrong indent for code block

--- sql/handler.cc
+++ sql/handler.cc
@@ -1114,10 +1114,10 @@ void handler::print_error(int error, myf errflag)
 	key_unpack(&str,table,(uint) key_nr);
 	uint max_length=MYSQL_ERRMSG_SIZE-(uint) strlen(ER(ER_DUP_ENTRY));
 	if (str.length() >= max_length)
-	  {
+	{
 	    str.length(max_length-4);
 	    str.append("...");
-	  }
+	}
       }
       my_error(ER_DUP_ENTRY,MYF(0),str.c_ptr(),key_nr+1);
       DBUG_VOID_RETURN;
