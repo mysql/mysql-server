0f3c3e2edc556e0870668062396265530e027b5b
unknown <brian@zim.(none)>
2006-10-01 11:17:59 -0700

    This  patch makes ndbcluster_hton not static. I have a second patch for ndbcluster_hton to be static, but I want to talk to Jonas first.
    sql/ha_ndbcluster.cc:
      Can't quite make  it static yet :)
    sql/ha_ndbcluster_binlog.h:
      Moving bad extern replacement

--- sql/ha_ndbcluster_binlog.h
+++ sql/ha_ndbcluster_binlog.h
@@ -31,6 +31,8 @@ extern ulong ndb_extra_logging;
 
 #define NDB_INVALID_SCHEMA_OBJECT 241
 
+extern handlerton *ndbcluster_hton;
+
 /*
   The numbers below must not change as they
   are passed between mysql servers, and if changed
@@ -103,7 +105,6 @@ extern pthread_mutex_t injector_mutex;
 extern pthread_cond_t  injector_cond;
 
 extern unsigned char g_node_id_map[max_ndb_nodes];
-extern handlerton *ndbcluster_hton;
 extern pthread_t ndb_util_thread;
 extern pthread_mutex_t LOCK_ndb_util_thread;
 extern pthread_cond_t COND_ndb_util_thread;
