d9b292808461af13c9c17a6260475524bc60b728
unknown <mats@romeo.(none)>
2006-10-02 15:08:40 +0200

    BUG#19459 (BINLOG RBR command does not lock tables correctly causing crash for, e.g., NDB): Adding new function my_b_copy_to_file() to copy an IO_CACHE to a file.
    include/my_sys.h:
      Adding new function my_b_copy_to_file() to copy an IO_CACHE to a file.
    mysys/mf_iocache2.c:
      Adding new function my_b_copy_to_file() to copy an IO_CACHE to a file.

--- include/my_sys.h
+++ include/my_sys.h
@@ -517,6 +517,7 @@ typedef int (*qsort2_cmp)(const void *, const void *, const void *);
 			 (uint) (*(info)->current_pos - (info)->request_pos))
 
 /* tell write offset in the SEQ_APPEND cache */
+int      my_b_copy_to_file(IO_CACHE *cache, FILE *file);
 my_off_t my_b_append_tell(IO_CACHE* info);
 my_off_t my_b_safe_tell(IO_CACHE* info); /* picks the correct tell() */
 
