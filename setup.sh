4051a5b2a1a073baae3a3115af1a09853651dbff
marko <>
2006-10-03 13:10:20 +0000

    branches/zip: Merge revisions 869:887 from trunk.

--- setup.sh
+++ setup.sh
@@ -3,43 +3,18 @@
 # Prepare the MySQL source code tree for building
 # with checked-out InnoDB Subversion directory.
 
-# This script assumes that the MySQL tree is at .. and that . = ../innodb
+# This script assumes that the current directory is storage/innobase.
 
 set -eu
 
 TARGETDIR=../storage/innobase
 
-rm -fr "$TARGETDIR"
-mkdir "$TARGETDIR"
-
 # link the build scripts
-ln -sf ../innodb/compile-innodb{,-debug} ../BUILD
-
-# create the directories
-for dir in */
-do
-  case "$dir" in
-      mysql-test/) ;;
-      *.svn*) ;;
-      *to-mysql*) ;;
-      *) mkdir "$TARGETDIR/$dir" ;;
-  esac
-done
-
-# create the symlinks to files
-cd "$TARGETDIR"
-for dir in */
-do
-  cd "$dir"
-  ln -s ../../../innodb/"$dir"* .
-  cd ..
-done
-for file in plug.in Makefile.am CMakeLists.txt
-do
-  ln -s ../../innodb/"$file" .
-done
+ln -sf $TARGETDIR/compile-innodb{,-debug} ../../BUILD
 
 cd ../../mysql-test/t
-ln -sf ../../innodb/mysql-test/*.test ../../innodb/mysql-test/*.opt ./
-ln -sf ../../innodb/mysql-test/*.result ../r/
-ln -sf ../../innodb/mysql-test/*.inc ../include/
+ln -sf ../$TARGETDIR/mysql-test/*.test $TARGETDIR/mysql-test/*.opt .
+cd ../r
+ln -sf ../$TARGETDIR/mysql-test/*.result .
+cd ../include
+ln -sf ../$TARGETDIR/mysql-test/*.inc .
