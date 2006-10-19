d262020f44988305f45519b21d3568fad0eb6b9a
unknown <jonas@perch.ndb.mysql.com>
2006-10-19 09:27:58 +0200

    ndb - bug#23200   this changes lock taken during peek, to decrease likelyhood of transaction abort
    sql/ha_ndbcluster.cc:
      use exclusive lock in peek, as peek is used just before insert/update

--- sql/ha_ndbcluster.cc
+++ sql/ha_ndbcluster.cc
@@ -1588,7 +1588,9 @@ int ha_ndbcluster::peek_indexed_rows(const byte *record)
   int res;
   DBUG_ENTER("peek_indexed_rows");
 
-  NdbOperation::LockMode lm= NdbOperation::LM_Read;
+  NdbOperation::LockMode lm=
+    (NdbOperation::LockMode)get_ndb_lock_type(m_lock.type);
+  
   first= NULL;
   if (table->s->primary_key != MAX_KEY)
   {
