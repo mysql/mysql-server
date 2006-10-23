453156598141ac2450544a361f280f95cc2fa33e
kostja@bodhi.local <>
2006-10-23 20:08:00 +0400

    Post-merge fixes.

--- server-tools/instance-manager/instance.cc
+++ server-tools/instance-manager/instance.cc
@@ -166,7 +166,7 @@ static int start_process(Instance_options *instance_options,
     exit(1);
   case -1:
     log_info("cannot create a new process to start instance '%s'.",
-             (const char *) instance_options->instance_name);
+             (const char *) instance_options->instance_name.str);
     return 1;
   }
   return 0;
@@ -312,9 +312,9 @@ void Instance::remove_pid()
   int pid;
   if ((pid= options.get_pid()) != 0)          /* check the pidfile */
     if (options.unlink_pidfile())             /* remove stalled pidfile */
-      log_error("cannot remove pidfile for instance '%s', this might be \
-                since IM lacks permmissions or hasn't found the pidifle",
-                (const char *) options.instance_name);
+      log_error("cannot remove pidfile for instance '%s', this might be "
+                "since IM lacks permmissions or hasn't found the pidifle",
+                (const char *) options.instance_name.str);
 }
 
 
@@ -620,7 +620,7 @@ void Instance::kill_instance(int signum)
         log_error("The instance '%s' is being stopped forcibly. Normally"
                   "it should not happen. Probably the instance has been"
                   "hanging. You should also check your IM setup",
-                  (const char *) options.instance_name);
+                  (const char *) options.instance_name.str);
         /* After sucessful hard kill the pidfile need to be removed */
         options.unlink_pidfile();
       }
