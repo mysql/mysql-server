2edcb14b251da9487480eeb1899272c572b5f466
mskold/marty@mysql.com/linux.site <>
2006-11-15 11:13:49 +0100

    bug #21495  Alter table from x engine to ndb and back can cause issue with drop DB:disable distributed drop database if a database contains local tables

--- sql/sql_show.h
+++ sql/sql_show.h
@@ -10,6 +10,15 @@ struct st_table_list;
 typedef st_ha_create_information HA_CREATE_INFO;
 typedef st_table_list TABLE_LIST;
 
+enum find_files_result {
+  FIND_FILES_OK,
+  FIND_FILES_OOM,
+  FIND_FILES_DIR
+};
+
+find_files_result find_files(THD *thd, List<char> *files, const char *db,
+                             const char *path, const char *wild, bool dir);
+
 int store_create_info(THD *thd, TABLE_LIST *table_list, String *packet,
                       HA_CREATE_INFO  *create_info_arg);
 int view_store_create_info(THD *thd, TABLE_LIST *table, String *buff);
