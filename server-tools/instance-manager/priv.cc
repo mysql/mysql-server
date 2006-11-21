cbfff7304d60f56853bb1312dbe6fe42ebbeffee
unknown <anozdrin/alik@booka.>
2006-11-21 17:31:03 +0300

    Polishing: 1) add support for joinable threads to Thread class; 2) move checking of thread model to Manager from mysqlmanager.cc, because it is needed only for IM-main process.
    server-tools/instance-manager/instance.cc:
      Use Manager::is_linux_threads() instead of global variable.
    server-tools/instance-manager/listener.cc:
      Use Thread::start(DETACHED) instead of Thread::start_detached().
    server-tools/instance-manager/manager.cc:
      1. Use Thread::start(DETACHED) instead of Thread::start_detached();
      2. Move checking of thread model to Manager from mysqlmanager.cc,
      because it is needed only for IM-main process.
    server-tools/instance-manager/manager.h:
      Move checking of thread model to Manager from mysqlmanager.cc,
      because it is needed only for IM-main process.
    server-tools/instance-manager/mysqlmanager.cc:
      Move checking of thread model to Manager from mysqlmanager.cc,
      because it is needed only for IM-main process.
    server-tools/instance-manager/priv.cc:
      Move checking of thread model to Manager from mysqlmanager.cc,
      because it is needed only for IM-main process.
    server-tools/instance-manager/priv.h:
      Move checking of thread model to Manager from mysqlmanager.cc,
      because it is needed only for IM-main process.
    server-tools/instance-manager/thread_registry.cc:
      Add support of joinable threads to Thread class.
    server-tools/instance-manager/thread_registry.h:
      Add support of joinable threads to Thread class.

--- server-tools/instance-manager/priv.cc
+++ server-tools/instance-manager/priv.cc
@@ -22,14 +22,6 @@
 
 #include "log.h"
 
-#ifndef __WIN__
-/*
-  This flag is set if mysqlmanager has detected that it is running on the
-  system using LinuxThreads
-*/
-bool linuxthreads;
-#endif
-
 /*
   The following string must be less then 80 characters, as
   mysql_connection.cc relies on it
