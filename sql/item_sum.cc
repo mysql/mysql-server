a9104e5d18e6c5512eab295e40bc881902d06a11
gkodinov/kgeorge@macbook.gmz <>
2006-11-28 15:44:11 +0200

    BUG#11927: Warnings shown for CAST( chr as signed) but not (chr + 0)  When implicitly converting string fields to numbers the  string-to-number conversion error was not sent to the client.  Added code to send the conversion error as warning.
     We also need to prevent generation of warnings from the places
     where val_xxx() methods are called for the sole purpose of updating
     the Item::null_value flag.
     To achieve that a special function is added (and called) : 
     update_null_value(). This function will set the no_errors flag and
     will call val_xxx(). The warning generation in Field_string::val_xxx()
     will use the flag when generating the conversion warnings.

--- sql/item_sum.cc
+++ sql/item_sum.cc
@@ -1050,7 +1050,7 @@ bool Item_sum_count::add()
     count++;
   else
   {
-    (void) args[0]->val_int();
+    args[0]->update_null_value();
     if (!args[0]->null_value)
       count++;
   }
@@ -1956,7 +1956,7 @@ void Item_sum_count::reset_field()
     nr=1;
   else
   {
-    (void) args[0]->val_int();
+    args[0]->update_null_value();
     if (!args[0]->null_value)
       nr=1;
   }
@@ -2066,7 +2066,7 @@ void Item_sum_count::update_field()
     nr++;
   else
   {
-    (void) args[0]->val_int();
+    args[0]->update_null_value();
     if (!args[0]->null_value)
       nr++;
   }
@@ -2546,7 +2546,7 @@ bool Item_sum_count_distinct::setup(THD *thd)
       return TRUE;                              // End of memory
     if (item->const_item())
     {
-      (void) item->val_int();
+      item->update_null_value();
       if (item->null_value)
 	always_null=1;
     }
