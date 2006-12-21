8b855aecad88b3a653827d5a12e511b74c3fd413
unknown <tsmith/tim@siva.hindu.god>
2006-12-20 17:11:47 -0700

    Post-merge fix (related to bug 24200 changes)
    sql/mysqld.cc:
      Post-merge fix
    sql/set_var.cc:
      Post-merge fix

--- sql/set_var.cc
+++ sql/set_var.cc
@@ -78,7 +78,8 @@ extern my_bool innobase_log_archive,
                innobase_use_doublewrite,
                innobase_use_checksums,
                innobase_file_per_table,
-               innobase_locks_unsafe_for_binlog;
+               innobase_locks_unsafe_for_binlog,
+               innobase_rollback_on_timeout;
 
 extern "C" {
 extern ulong srv_max_buf_pool_modified_pct;
