0792d70524249139deabfd69735fe860a4ea86ab
marko <>
2006-12-29 09:13:57 +0000

    branches/zip: Minor cleanup.
    buf_buddy_get(): New function in buf0buddy.c, to replace the macro
    in buf0buddy.h.
    
    buf_buddy_get_offset(): Remove.
    
    buf_buddy_get_slot(): Remove the prototype from buf0buddy.h.  This function
    is for internal use in buf0buddy.ic.

--- include/buf0buddy.h
+++ include/buf0buddy.h
@@ -18,32 +18,6 @@ Created December 2006 by Marko Makela
 #include "buf0types.h"
 
 /**************************************************************************
-Get the offset of the buddy of a compressed page frame. */
-UNIV_INLINE
-lint
-buf_buddy_get_offset(
-/*=================*/
-				/* out: offset of the buddy relative to page */
-	const void*	page,	/* in: compressed page */
-	ulint		size)	/* in: page size in bytes */
-	__attribute__((nonnull));
-
-/**************************************************************************
-Get the buddy of a compressed page frame.
-Note: "page" should be a pointer to byte or char. */
-#define buf_buddy_get(page,size) ((page) + buf_buddy_get_offset((page),(size)))
-
-/**************************************************************************
-Get the index of buf_pool->zip_free[] for a given block size. */
-UNIV_INLINE
-ulint
-buf_buddy_get_slot(
-/*===============*/
-			/* out: index of buf_pool->zip_free[],
-			or BUF_BUDDY_SIZES */
-	ulint	size);	/* in: block size */
-
-/**************************************************************************
 Allocate a block. */
 UNIV_INLINE
 void*
