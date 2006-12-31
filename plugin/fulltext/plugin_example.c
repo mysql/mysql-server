67868597bbb71e58ded44b479001c5482685d06d
kent@mysql.com/kent-amd64.(none) <>
2006-12-31 02:29:11 +0100

    Many files:   Removed "MySQL Finland AB & TCX DataKonsult AB" from copyright header   Adjusted year(s) in copyright header   Added GPL copyright text my_vle.h, rpl_utility.h, my_vle.c, base64-t.c, rpl_utility.cc:   Changed copyright header formatting some plugin_example.c, daemon_example.c:   Added "Copyright (C) 2006 MySQL AB" to GPL header

--- plugin/fulltext/plugin_example.c
+++ plugin/fulltext/plugin_example.c
@@ -1,8 +1,8 @@
-/*
+/* Copyright (C) 2006 MySQL AB
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
+   the Free Software Foundation; version 2 of the License.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -11,7 +11,7 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
 
 #include <stdlib.h>
 #include <ctype.h>
