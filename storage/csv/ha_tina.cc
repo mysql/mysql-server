c031a21e3b6ccf96c6a450f73cd2cf0c409f6066
brian@zim.(none) <>
2007-01-03 17:26:45 -0800

    Split out the hidden class from the ha_tina file (probably should have just deleted it...) but this is a bit better.

--- storage/csv/ha_tina.cc
+++ storage/csv/ha_tina.cc
@@ -77,54 +77,6 @@ static handler *tina_create_handler(handlerton *hton,
                                     MEM_ROOT *mem_root);
 
 
-off_t Transparent_file::read_next()
-{
-  off_t bytes_read;
-
-  /*
-     No need to seek here, as the file managed by Transparent_file class
-     always points to upper_bound byte
-  */
-  if ((bytes_read= my_read(filedes, buff, buff_size, MYF(0))) == MY_FILE_ERROR)
-    return -1;
-
-  /* end of file */
-  if (!bytes_read)
-    return -1;
-
-  lower_bound= upper_bound;
-  upper_bound+= bytes_read;
-
-  return lower_bound;
-}
-
-
-char Transparent_file::get_value(off_t offset)
-{
-  off_t bytes_read;
-
-  /* check boundaries */
-  if ((lower_bound <= offset) && (offset < upper_bound))
-    return buff[offset - lower_bound];
-  else
-  {
-    VOID(my_seek(filedes, offset, MY_SEEK_SET, MYF(0)));
-    /* read appropriate portion of the file */
-    if ((bytes_read= my_read(filedes, buff, buff_size,
-                             MYF(0))) == MY_FILE_ERROR)
-      return 0;
-
-    lower_bound= offset;
-    upper_bound= lower_bound + bytes_read;
-
-    /* end of file */
-    if (upper_bound == offset)
-      return 0;
-
-    return buff[0];
-  }
-}
-
 /*****************************************************************************
  ** TINA tables
  *****************************************************************************/
