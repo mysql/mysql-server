a3aae5797099c246e0aecf249f508b599309ce2b
kent@mysql.com/kent-amd64.(none) <>
2007-01-31 00:06:42 +0100

    gen_lex_hash.cc:   If inserting a GPL header, include a complete one Makefile.am, mysql.dsw, mysql.sln:   Removed C version of mysql-test-run mysql.spec.sh:   Specify GPL license only in GPL sources .del-my_manage.h:   Delete: mysql-test/my_manage.h mysql.spec.sh:   Added GPL header .del-mysql_test_run_new.c:   Delete: mysql-test/mysql_test_run_new.c .del-mysql_test_run_new.dsp:   Delete: VC++Files/mysql-test/mysql_test_run_new.dsp .del-my_create_tables.c:   Delete: mysql-test/my_create_tables.c .del-mysql_test_run_new_ia64.dsp:   Delete: VC++Files/mysql-test/mysql_test_run_new_ia64.dsp msql2mysql.sh:   Use up-to-date GPL header .del-mysql_test_run_new.vcproj:   Delete: VC++Files/mysql-test/mysql_test_run_new.vcproj .del-my_manage.c:   Delete: mysql-test/my_manage.c

--- scripts/msql2mysql.sh
+++ scripts/msql2mysql.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-# Copyright (C) 1979-1996 TcX AB & Monty Program KB & Detron HB
+# Copyright (C) 1979-2007 MySQL AB
 # 
-# This software is distributed with NO WARRANTY OF ANY KIND.  No author or
-# distributor accepts any responsibility for the consequences of using it, or
-# for whether it serves any particular purpose or works at all, unless he or
-# she says so in writing.  Refer to the Free Public License (the "License")
-# for full details.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
 # 
-# Every copy of this file must include a copy of the License, normally in a
-# plain ASCII text file named PUBLIC.  The License grants you the right to 
-# copy, modify and redistribute this file, but only under certain conditions
-# described in the License.  Among other things, the License requires that
-# the copyright notice and this notice be preserved on all copies.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING. If not, write to the
+# Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston
+# MA  02110-1301  USA.
 
 @bindir@/replace msqlConnect mysql_connect msqlListDBs  mysql_list_dbs msqlNumRows mysql_num_rows msqlFetchRow mysql_fetch_row msqlFetchField mysql_fetch_field msqlFreeResult mysql_free_result msqlListFields mysql_list_fields msqlListTables mysql_list_tables msqlErrMsg 'mysql_error(mysql)' msqlStoreResult mysql_store_result msqlQuery mysql_query msqlField mysql_field msqlSelect mysql_select msqlSelectDB mysql_select_db msqlNumFields mysql_num_fields msqlClose mysql_close msqlDataSeek mysql_data_seek m_field MYSQL_FIELD m_result MYSQL_RES m_row MYSQL_ROW msql mysql mSQL mySQL MSQL MYSQL msqlCreateDB mysql_create_db msqlDropDB mysql_drop_db msqlFieldSeek mysql_field_seek -- $*
