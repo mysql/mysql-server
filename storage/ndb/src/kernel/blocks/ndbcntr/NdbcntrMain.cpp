bc57aa87be13ca937d44081ec7839ef7cefea6b7
tomas@poseidon.mysql.com <>
2007-01-31 14:30:28 +0700

    (recommit from 5.1-wl2325-5.0-drop6) ndb - moved initialization of ndbd fs block first to ensure that it gets enough space for allocation of file system thread stacks

--- storage/ndb/src/kernel/blocks/ndbcntr/NdbcntrMain.cpp
+++ storage/ndb/src/kernel/blocks/ndbcntr/NdbcntrMain.cpp
@@ -91,6 +91,7 @@ static BlockInfo ALL_BLOCKS[] = {
 static const Uint32 ALL_BLOCKS_SZ = sizeof(ALL_BLOCKS)/sizeof(BlockInfo);
 
 static BlockReference readConfigOrder[ALL_BLOCKS_SZ] = {
+  NDBFS_REF, // let it run first to make sure it can start the threads
   CMVMI_REF,
   DBTUP_REF,
   DBACC_REF,
@@ -99,7 +100,6 @@ static BlockReference readConfigOrder[ALL_BLOCKS_SZ] = {
   DBTUX_REF,
   DBDICT_REF,
   DBDIH_REF,
-  NDBFS_REF,
   NDBCNTR_REF,
   QMGR_REF,
   TRIX_REF,
