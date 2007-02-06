549d50a20755ba63bbd1f302314b5b2461d743f9
tomas@poseidon.mysql.com <>
2007-02-06 11:57:56 +0700

    ndb: remove WAITFOR_RESPONSE_TIMEOUT printout - to get cleaner error log in debug mode

--- storage/ndb/src/ndbapi/TransporterFacade.cpp
+++ storage/ndb/src/ndbapi/TransporterFacade.cpp
@@ -1403,7 +1403,7 @@ int PollGuard::wait_for_input_in_loop(int wait_time, bool forceSend)
     }
     if (wait_time == -1)
     {
-#ifdef VM_TRACE
+#ifdef NOT_USED
       ndbout << "Waited WAITFOR_RESPONSE_TIMEOUT, continuing wait" << endl;
 #endif
       continue;
