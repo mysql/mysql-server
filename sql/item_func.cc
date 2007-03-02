6a06b9e501e58dc8544b63687d7870212d7db12c
malff/marcsql@weblab.(none) <>
2007-03-01 19:20:47 -0700

    Bug#26093 (SELECT BENCHMARK() for SELECT statements does not produce valid   results)
    Before this fix, the function BENCHMARK() would fail to evaluate expressions
    like "(select avg(a) from t1)" in debug builds (with an assert),
    or would report a time of zero in non debug builds.
    
    The root cause is that evaluation of DECIMAL_RESULT expressions was not
    supported in Item_func_benchmark::val_int().
    
    This has been fixed by this change.

--- sql/item_func.cc
+++ sql/item_func.cc
@@ -3456,6 +3456,7 @@ longlong Item_func_benchmark::val_int()
   DBUG_ASSERT(fixed == 1);
   char buff[MAX_FIELD_WIDTH];
   String tmp(buff,sizeof(buff), &my_charset_bin);
+  my_decimal tmp_decimal;
   THD *thd=current_thd;
 
   for (ulong loop=0 ; loop < loop_count && !thd->killed; loop++)
@@ -3469,6 +3470,9 @@ longlong Item_func_benchmark::val_int()
       break;
     case STRING_RESULT:
       (void) args[0]->val_str(&tmp);
+      break;
+    case DECIMAL_RESULT:
+      (void) args[0]->val_decimal(&tmp_decimal);
       break;
     case ROW_RESULT:
     default:
