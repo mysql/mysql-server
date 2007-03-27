919b6dcdc6e32c1b2a0a6c6d82565564b285fb4e
marko <Unknown>
2007-03-27 13:17:47 +0000

    Remove ha_innobase::last_query_id and references to thd->query_id.
    MySQL calls external_lock at the beginning and end of a statement
    when it is not calling start_stmt or commit or rollback.  Thus,
    statement boundaries can be (and are already) detected without
    monitoring thd->query_id.
    
    The function innobase_commit() seemingly lacks the call to
    innobase_release_stat_resources(), which should be called at
    the end of every SQL statement.  The call was replaced by
    equivalent statements by Vadim Tkachenko when he implemented
    innodb_commit_concurrency in MySQL 5.0:
    
    http://mysql.bkbits.net:8080/mysql-5.0/?PAGE=patch&REV=1.1886.70.1

--- handler/ha_innodb.h
+++ handler/ha_innodb.h
@@ -44,8 +44,6 @@ class ha_innobase: public handler
 	THD*		user_thd;	/* the thread handle of the user
 					currently using the handle; this is
 					set in external_lock function */
-	query_id_t	last_query_id;	/* the latest query id where the
-					handle was used */
 	THR_LOCK_DATA	lock;
 	INNOBASE_SHARE	*share;
 
