587942a792f5c31d0c5fc900dcacb454a0000e13
unknown <kostja@bodhi.local>
2007-03-27 21:57:33 +0400

    Fix warnings.
    include/typelib.h:
      Fix a compile warning. char *x is actually updated if
      it's found in the typelib (a peculiar way to return
      canonical value in case of a partial match)
    mysys/typelib.c:
      Fix a warning.
    sql/item_func.cc:
      Fix a compile-time warning.

--- include/typelib.h
+++ include/typelib.h
@@ -26,7 +26,7 @@ typedef struct st_typelib {	/* Different types saved here */
   unsigned int *type_lengths;
 } TYPELIB;
 
-extern int find_type(const char *x, const TYPELIB *typelib,
+extern int find_type(char *x, const TYPELIB *typelib,
                      unsigned int full_name);
 extern void make_type(char *to,unsigned int nr,TYPELIB *typelib);
 extern const char *get_type(TYPELIB *typelib,unsigned int nr);
