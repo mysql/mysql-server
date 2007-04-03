0b3dfea79ae0d65ac0a7d88d3154848a51e92802
cmiller@zippy.cornsilk.net <>
2007-04-03 14:08:09 -0400

    Backport of Igor's patch for Bug#27362, March 22 2007.
    Fixed bug #27362: crash at evaluation of IN predicate when one
    of its argument happened to be a decimal expression returning
    the NULL value.
    The crash was due to the fact the function in_decimal::set did
    not take into account that val_decimal() could return 0 if 
    the decimal expression had been evaluated to NULL.

--- sql/item_cmpfunc.cc
+++ sql/item_cmpfunc.cc
@@ -2423,7 +2423,8 @@ void in_decimal::set(uint pos, Item *item)
   dec->len= DECIMAL_BUFF_LENGTH;
   dec->fix_buffer_pointer();
   my_decimal *res= item->val_decimal(dec);
-  if (res != dec)
+  /* if item->val_decimal() is evaluated to NULL then res == 0 */ 
+  if (!item->null_value && res != dec)
     my_decimal2decimal(res, dec);
 }
 
