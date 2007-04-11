b57233d0a851e0786da60622d868827a2656e361
igor@olga.mysql.com <>
2007-04-11 12:45:27 -0700

    Post-merge fix.

--- sql/item_cmpfunc.cc
+++ sql/item_cmpfunc.cc
@@ -95,9 +95,9 @@ static int cmp_row_type(Item* item1, Item* item2)
     return 1;
   for (uint i=0; i<n; i++)
   {
-    if (item2->el(i)->check_cols(item1->el(i)->cols()) ||
-        (item1->el(i)->result_type() == ROW_RESULT &&
-         cmp_row_type(item1->el(i), item2->el(i))))
+    if (item2->element_index(i)->check_cols(item1->element_index(i)->cols()) ||
+        (item1->element_index(i)->result_type() == ROW_RESULT &&
+         cmp_row_type(item1->element_index(i), item2->element_index(i))))
       return 1;
   }
   return 0;
