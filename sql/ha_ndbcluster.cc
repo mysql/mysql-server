41015903e85733e5ebab05d98f30104e2ed5e7eb
serg@janus.mylan <>
2007-04-19 11:05:25 +0200

    mysql-test/t/ndb_dd_basic.test     fixed the test sql/ha_ndbcluster.cc     LOCK_plugin is used in ha_ndbcluster.cc sql/sql_plugin.cc     LOCK_plugin is used in ha_ndbcluster.cc

--- sql/ha_ndbcluster.cc
+++ sql/ha_ndbcluster.cc
@@ -6851,6 +6851,7 @@ static int connect_callback()
 }
 
 extern int ndb_dictionary_is_mysqld;
+extern pthread_mutex_t LOCK_plugin;
 
 static int ndbcluster_init(void *p)
 {
@@ -6859,6 +6860,13 @@ static int ndbcluster_init(void *p)
 
   if (ndbcluster_inited)
     DBUG_RETURN(FALSE);
+
+  /*
+    Below we create new THD's. They'll need LOCK_plugin, but it's taken now by
+    plugin initialization code. Release it to avoid deadlocks.  It's safe, as
+    there're no threads that may concurrently access plugin control structures.
+  */
+  pthread_mutex_unlock(&LOCK_plugin);
 
   pthread_mutex_init(&ndbcluster_mutex,MY_MUTEX_INIT_FAST);
   pthread_mutex_init(&LOCK_ndb_util_thread, MY_MUTEX_INIT_FAST);
@@ -6998,6 +7006,8 @@ static int ndbcluster_init(void *p)
     goto ndbcluster_init_error;
   }
 
+  pthread_mutex_lock(&LOCK_plugin);
+
   ndbcluster_inited= 1;
   DBUG_RETURN(FALSE);
 
@@ -7009,6 +7019,8 @@ ndbcluster_init_error:
     delete g_ndb_cluster_connection;
   g_ndb_cluster_connection= NULL;
   ndbcluster_hton->state= SHOW_OPTION_DISABLED;               // If we couldn't use handler
+
+  pthread_mutex_lock(&LOCK_plugin);
 
   DBUG_RETURN(TRUE);
 }
