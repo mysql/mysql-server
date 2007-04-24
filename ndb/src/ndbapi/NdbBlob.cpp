56cf7a53b626c47d34df1bf75d3c99aae7d76696
knielsen@ymer.(none) <>
2007-04-24 08:15:31 +0200

    BUG#27370: Potential inconsistent blob reads for ReadCommitted reads.
    The old blob implementation had code that attempted to upgrade the lock mode for
    LM_CommittedRead operations, but it did not work properly as it did not recompute
    the operation flags.
    
    As a consequence, reading a blob with LM_CommittedRead could return inconsistent
    data, with different part of the read data being from different commits done by
    other transactions.
    
    The fix is to correctly recompute all necessary flags when upgrading lock mode.

--- ndb/src/ndbapi/NdbBlob.cpp
+++ ndb/src/ndbapi/NdbBlob.cpp
@@ -1157,7 +1157,7 @@ NdbBlob::atPrepare(NdbTransaction* aCon, NdbOperation* anOp, const NdbColumnImpl
     if (isReadOp()) {
       // upgrade lock mode
       if (theNdbOp->theLockMode == NdbOperation::LM_CommittedRead)
-        theNdbOp->theLockMode = NdbOperation::LM_Read;
+        theNdbOp->setReadLockMode(NdbOperation::LM_Read);
       // add read of head+inline in this op
       if (getHeadInlineValue(theNdbOp) == -1)
         DBUG_RETURN(-1);
@@ -1178,7 +1178,7 @@ NdbBlob::atPrepare(NdbTransaction* aCon, NdbOperation* anOp, const NdbColumnImpl
   if (isScanOp()) {
     // upgrade lock mode
     if (theNdbOp->theLockMode == NdbOperation::LM_CommittedRead)
-      theNdbOp->theLockMode = NdbOperation::LM_Read;
+      theNdbOp->setReadLockMode(NdbOperation::LM_Read);
     // add read of head+inline in this op
     if (getHeadInlineValue(theNdbOp) == -1)
       DBUG_RETURN(-1);
