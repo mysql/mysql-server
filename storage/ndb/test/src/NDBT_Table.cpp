210476089ba0e4a8185440257f7dd019f15792ac
tomas@whalegate.ndb.mysql.com <>
2007-04-26 13:36:51 +0200

    WL#1190 preparatory work needed for compatability with future online add column

--- storage/ndb/test/src/NDBT_Table.cpp
+++ storage/ndb/test/src/NDBT_Table.cpp
@@ -33,6 +33,7 @@ operator <<(class NdbOut& ndbout, const NDBT_Table & tab)
   ndbout << "Length of frm data: " << tab.getFrmLength() << endl;
   ndbout << "Row Checksum: " << tab.getRowChecksumIndicator() << endl;
   ndbout << "Row GCI: " << tab.getRowGCIIndicator() << endl;
+  ndbout << "ForceVarPart: " << tab.getForceVarPart() << endl;
 
 
   //<< ((tab.getTupleKey() == TupleId) ? " tupleid" : "") <<endl;
