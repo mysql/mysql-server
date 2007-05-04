8f08a0c430c697805f1c2f9de54fc0f881a37265
tomas@whalegate.ndb.mysql.com <>
2007-05-04 11:58:23 +0200

    bugfix in checksum with force varpart

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -2385,7 +2385,7 @@ sub ndbcluster_start_install ($) {
     else
     {
       $ndb_no_ord=32;
-      $ndb_con_op=5000;
+      $ndb_con_op=10000;
       $ndb_dmem="20M";
       $ndb_imem="1M";
       $ndb_pbmem="4M";
