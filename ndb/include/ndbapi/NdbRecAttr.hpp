7ef22d8d22ee46ca69026563dbdc9bcccf5e7524
unknown <tomas@whalegate.ndb.mysql.com>
2007-05-09 14:31:22 +0200

    Bug #28287 Sign problem in test "ndb_restore_print"   - corrected previous patch   - some platforms do strange things with char... use Int8 to be sure of signedness

--- ndb/include/ndbapi/NdbRecAttr.hpp
+++ ndb/include/ndbapi/NdbRecAttr.hpp
@@ -164,7 +164,14 @@ public:
    *
    * @return  Char value.
    */           
-  Int8  char_value() const;           
+  char  char_value() const;           
+
+  /**
+   * Get value stored in NdbRecAttr object.
+   *
+   * @return  Int8 value.
+   */           
+  Int8  int8_value() const;           
 
   /**
    * Get value stored in NdbRecAttr object.
@@ -200,6 +207,13 @@ public:
    * @return  Unsigned char value.
    */   
   Uint8 u_char_value() const;        
+
+  /**
+   * Get value stored in NdbRecAttr object.
+   *
+   * @return  Uint8 value.
+   */   
+  Uint8 u_8_value() const;
 
   /**
    * Get value stored in NdbRecAttr object.
@@ -340,8 +354,15 @@ NdbRecAttr::short_value() const
 }
 
 inline
-Int8
+char
 NdbRecAttr::char_value() const
+{
+  return *(char*)theRef;
+}
+
+inline
+Int8
+NdbRecAttr::int8_value() const
 {
   return *(Int8*)theRef;
 }
@@ -363,6 +384,13 @@ NdbRecAttr::u_short_value() const
 inline
 Uint8
 NdbRecAttr::u_char_value() const
+{
+  return *(Uint8*)theRef;
+}
+
+inline
+Uint8
+NdbRecAttr::u_8_value() const
 {
   return *(Uint8*)theRef;
 }
