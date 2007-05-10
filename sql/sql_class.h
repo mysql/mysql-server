0d6e93e0c1c6e8a7827650f00448237a6e7cdfa1
kostja@vajra.(none) <>
2007-05-10 18:27:36 +0400

    Follow the coding style with class names.

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -495,7 +495,7 @@ public:
 };
 
 
-class delayed_insert;
+class Delayed_insert;
 class select_result;
 
 #define THD_SENTRY_MAGIC 0xfeedd1ff
@@ -1248,7 +1248,7 @@ public:
   time_t     start_time,time_after_lock,user_time;
   time_t     connect_time,thr_create_time; // track down slow pthread_create
   thr_lock_type update_lock_default;
-  delayed_insert *di;
+  Delayed_insert *di;
 
   /* <> 0 if we are inside of trigger or stored function. */
   uint in_sub_stmt;
