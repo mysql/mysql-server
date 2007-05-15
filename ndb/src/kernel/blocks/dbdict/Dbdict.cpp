b027b84afada9d6339171fb278312bf431f36411
tomas@whalegate.ndb.mysql.com <>
2007-05-15 09:03:00 +0200

    Bug #26386 ndbd wont start after changing schema     backported error code from 5.1     split error check when reading schema file

--- ndb/src/kernel/blocks/dbdict/Dbdict.cpp
+++ ndb/src/kernel/blocks/dbdict/Dbdict.cpp
@@ -1069,14 +1069,24 @@ void Dbdict::readSchemaConf(Signal* signal,
 
   for (Uint32 n = 0; n < xsf->noOfPages; n++) {
     SchemaFile * sf = &xsf->schemaPage[n];
-    bool ok =
-      memcmp(sf->Magic, NDB_SF_MAGIC, sizeof(sf->Magic)) == 0 &&
-      sf->FileSize != 0 &&
-      sf->FileSize % NDB_SF_PAGE_SIZE == 0 &&
-      sf->FileSize == sf0->FileSize &&
-      sf->PageNumber == n &&
-      computeChecksum((Uint32*)sf, NDB_SF_PAGE_SIZE_IN_WORDS) == 0;
-    ndbrequire(ok || !crashInd);
+    bool ok = false;
+    if (memcmp(sf->Magic, NDB_SF_MAGIC, sizeof(sf->Magic)) != 0)
+    { jam(); }
+    else if (sf->FileSize == 0)
+    { jam(); }
+    else if (sf->FileSize % NDB_SF_PAGE_SIZE != 0)
+    { jam(); }
+    else if (sf->FileSize != sf0->FileSize)
+    { jam(); }
+    else if (sf->PageNumber != n)
+    { jam(); }
+    else if (computeChecksum((Uint32*)sf, NDB_SF_PAGE_SIZE_IN_WORDS) != 0)
+    { jam(); }
+    else if (crashInd)
+    { jam(); }
+    else
+      ok = true;
+    ndbrequireErr(ok, NDBD_EXIT_SR_SCHEMAFILE);
     if (! ok) {
       jam();
       ndbrequire(fsPtr.p->fsState == FsConnectRecord::READ_SCHEMA1);
