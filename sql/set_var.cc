945f3c2cc827ca6acc06a1034538f4ec1388ac5b
unknown <msvensson@pilot.blaudden>
2007-05-24 11:21:27 +0200

    Bug#26664 test suite times out on OS X 64bit  - The "mysql client in mysqld"(which is used by    replication and federated) should use alarms instead of setting    socket timeout value if the rest of the server uses alarm. By    always calling 'my_net_set_write_timeout'    or 'my_net_set_read_timeout' when changing the timeout value(s), the    selection whether to use alarms or timeouts will be handled by    ifdef's in those two functions.  - Move declaration of 'vio_timeout' into "vio_priv.h"
    include/mysql_com.h:
      Move the net_set_*_timeout function declarations to mysql_com.h and
      rename to my_net_set_*_timeout to avoid name clashes
    include/violite.h:
      Move declaration of 'vio_timeout' to vio_priv.h (to make
      the function as private as possible)
    libmysql/libmysql.c:
      Use my_net_read_timeout or my_net_write_timeout when setting
      the timeouts. Move the global variables for my_net_read/my_write_timeout
      into the only place where they are used. Thus removing them...
    server-tools/instance-manager/mysql_connection.cc:
      Use my_net_read_timeout or my_net_write_timeout when setting the timeouts
    sql-common/client.c:
      Use my_net_read_timeout or my_net_write_timeout when setting the timeouts
    sql/mysql_priv.h:
      Move the net_set_*_timeout function declarations to mysql_com.h
    sql/net_serv.cc:
      No need to cast the net->write_timeout value from "uint" to "uint"
    sql/set_var.cc:
      Rename net_set_*_timeout to my_net_set_*_timeout
    sql/sql_client.cc:
      Use my_net_read_timeout or my_net_write_timeout when setting the timeouts
    sql/sql_parse.cc:
      Rename net_set_*_timeout to my_net_set_*_timeout
    sql/sql_repl.cc:
      Rename net_set_*_timeout to my_net_set_*_timeout
    vio/vio_priv.h:
      Move declaration of 'vio_timeout' to vio_priv.h
    vio/viosocket.c:
      Cleanup 'vio_timeout'
       - Use "const void*" on POSIX and "const char*" on windows for
         setsockopt
       - Add DBUG_PRINT's
       - Add comment about why we don't have an implementation of
        vio_timeout for platforms not supporting SO_SNDTIMEO or SO_RCVTIMEO

--- sql/set_var.cc
+++ sql/set_var.cc
@@ -1282,14 +1282,14 @@ static int check_completion_type(THD *thd, set_var *var)
 static void fix_net_read_timeout(THD *thd, enum_var_type type)
 {
   if (type != OPT_GLOBAL)
-    net_set_read_timeout(&thd->net, thd->variables.net_read_timeout);
+    my_net_set_read_timeout(&thd->net, thd->variables.net_read_timeout);
 }
 
 
 static void fix_net_write_timeout(THD *thd, enum_var_type type)
 {
   if (type != OPT_GLOBAL)
-    net_set_write_timeout(&thd->net, thd->variables.net_write_timeout);
+    my_net_set_write_timeout(&thd->net, thd->variables.net_write_timeout);
 }
 
 static void fix_net_retry_count(THD *thd, enum_var_type type)
