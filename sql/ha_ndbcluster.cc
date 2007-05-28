f22712788855df6b02026ffc8607416dd52311d6
mats@kindahl-laptop.dnsalias.net <>
2007-05-28 12:50:29 +0200

    WL#3303 (RBR: Engine-controlled logging format):
    Adding support to allow engines to tell what formats they can handle.
    The server will generate an error if it is not possible to log the
    statement according to the logging mode in effect.
    
    Adding flags to several storage engines to state what they can handle.
    
    Changes to NDB handler removing code that forces row-based mode and
    adding flag saying that NDB can only handle row format.
    
    Adding check that binlog flags are only used for real tables that are
    opened for writing.

--- sql/ha_ndbcluster.cc
+++ sql/ha_ndbcluster.cc
@@ -4444,7 +4444,6 @@ int ha_ndbcluster::external_lock(THD *thd, int lock_type)
       DBUG_PRINT("warning", ("ops_pending != 0L"));
     m_ops_pending= 0;
   }
-  thd->set_current_stmt_binlog_row_based_if_mixed();
   DBUG_RETURN(error);
 }
 
@@ -4494,7 +4493,6 @@ int ha_ndbcluster::start_stmt(THD *thd, thr_lock_type lock_type)
   m_active_trans= trans;
   // Start of statement
   m_ops_pending= 0;    
-  thd->set_current_stmt_binlog_row_based_if_mixed();
 
   DBUG_RETURN(error);
 }
@@ -6000,6 +5998,7 @@ void ha_ndbcluster::get_auto_increment(ulonglong offset, ulonglong increment,
                 HA_PRIMARY_KEY_REQUIRED_FOR_DELETE | \
                 HA_PARTIAL_COLUMN_READ | \
                 HA_HAS_OWN_BINLOGGING | \
+                HA_BINLOG_ROW_CAPABLE | \
                 HA_HAS_RECORDS
 
 ha_ndbcluster::ha_ndbcluster(handlerton *hton, TABLE_SHARE *table_arg):
@@ -8788,7 +8787,6 @@ pthread_handler_t ndb_util_thread_func(void *arg __attribute__((unused)))
   my_net_init(&thd->net, 0);
   thd->main_security_ctx.master_access= ~0;
   thd->main_security_ctx.priv_user = 0;
-  thd->current_stmt_binlog_row_based= TRUE;     // If in mixed mode
 
   /* Signal successful initialization */
   ndb_util_thread_running= 1;
