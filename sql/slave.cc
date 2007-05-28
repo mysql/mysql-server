61c0e4c01c06ef8917f09d6fee20026b6e62e5f7
unknown <rafal@quant.(none)>
2007-05-28 21:14:57 +0200

    BUG#21132 (Slave fails to reconnect on update_slave_list)
    This is a one liner which will fix semantics if SHOW SLAVE HOSTS to 
    display the list of slaves currently registered on the host on which
    it was issued.
    
    
    sql/slave.cc:
      Remove call to update_slave_list() which was executing "SHOW SLAVE
      HOSTS" on master and populating slave_list based on the output.

--- sql/slave.cc
+++ sql/slave.cc
@@ -2020,7 +2020,7 @@ connected:
       on with life.
     */
     thd->proc_info = "Registering slave on master";
-    if (register_slave_on_master(mysql) ||  update_slave_list(mysql, mi))
+    if (register_slave_on_master(mysql))
       goto err;
   }
 
