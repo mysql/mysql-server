75fe8f63a17b5802388f37cc2cefb0ea77156222
tomas@whalegate.ndb.mysql.com <>
2007-06-11 17:50:39 +0200

    make function static

--- storage/ndb/src/ndbapi/ndb_internal.hpp
+++ storage/ndb/src/ndbapi/ndb_internal.hpp
@@ -21,6 +21,6 @@ private:
   friend class NdbEventBuffer;
   Ndb_internal() {}
   virtual ~Ndb_internal() {}
-  int send_event_report(Ndb *ndb, Uint32 *data, Uint32 length)
+  static int send_event_report(Ndb *ndb, Uint32 *data, Uint32 length)
     { return ndb->theImpl->send_event_report(data, length); }
 };
