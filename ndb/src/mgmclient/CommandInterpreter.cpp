14e19b1edb009354d74b779861fc00336803973c
lzhou/zhl@dev3-63.(none) <>
2007-06-13 10:42:21 +0000

    BUG#23354 Clear "no start" when nodes restart and add new prompting string when nodes restart

--- ndb/src/mgmclient/CommandInterpreter.cpp
+++ ndb/src/mgmclient/CommandInterpreter.cpp
@@ -2026,7 +2026,7 @@ CommandInterpreter::executeRestart(Vector<BaseString> &command_list,
     return -1;
   }
 
-  if (!nostart)
+  if (nostart)
     ndbout_c("Shutting down nodes with \"-n, no start\" option, to subsequently start the nodes.");
 
   result= ndb_mgm_restart3(m_mgmsrv, no_of_nodes, node_ids,
@@ -2046,7 +2046,15 @@ CommandInterpreter::executeRestart(Vector<BaseString> &command_list,
       ndbout << "Node";
       for (int i= 0; i < no_of_nodes; i++)
         ndbout << " " << node_ids[i];
-      ndbout_c(" is being restarted");
+      ndbout_c(": Is being restarted");
+
+      ndbout << "Node";
+      for (int i= 0; i < no_of_nodes; i++)
+        ndbout << " " << node_ids[i];
+      if (nostart)
+        ndbout_c(": No start");
+      else
+        ndbout_c(": Is rejoining the cluster");
     }
     if(need_disconnect)
       disconnect();
