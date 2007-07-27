74e4da489efe6f6832f4eb5f5faf5bdadd1881d4
cbell/Chuck@mysql_cab_desk. <>
2007-07-27 13:58:37 -0400

    WL#3228 (NDB) : RBR using different table defs on slave/master
    This patch adds the ability to store extra field metadata in the table
    map event. This data can include pack_length() or field_lenght() for
    fields such as CHAR or VARCHAR enabling developers to add code that
    can check for compatibilty between master and slave columns. More 
    importantly, the extra field metadata can be used to store data from the
    master correctly should a VARCHAR field on the master be <= 255 bytes 
    while the same field on the slave is > 255 bytes. '
    
    The patch also includes the needed changes to unpack to ensure that data
    which is smaller on the master can be unpacked correctly on the slave.

--- sql/rpl_record.cc
+++ sql/rpl_record.cc
@@ -16,6 +16,8 @@
 #include "mysql_priv.h"
 #include "rpl_record.h"
 #include "slave.h"                  // Need to pull in slave_print_msg
+#include "rpl_utility.h"
+#include "rpl_rli.h"
 
 /**
    Pack a record of data for a table into a format suitable for
@@ -190,7 +192,9 @@ unpack_row(RELAY_LOG_INFO const *rli,
   unsigned int null_mask= 1U;
   // The "current" null bits
   unsigned int null_bits= *null_ptr++;
-  for (field_ptr= begin_ptr ; field_ptr < end_ptr ; ++field_ptr)
+  uint i= 0;
+  table_def *tabledef= ((RELAY_LOG_INFO*)rli)->get_tabledef(table);
+  for (field_ptr= begin_ptr ; field_ptr < end_ptr && *field_ptr ; ++field_ptr)
   {
     Field *const f= *field_ptr;
 
@@ -219,14 +223,20 @@ unpack_row(RELAY_LOG_INFO const *rli,
         f->set_notnull();
 
         /*
-          We only unpack the field if it was non-null
-        */
-        pack_ptr= f->unpack(f->ptr, pack_ptr);
+          We only unpack the field if it was non-null.
+          Use the master's size information if available else call
+          normal unpack operation.
+         */
+        if (tabledef && tabledef->field_metadata(i))
+          pack_ptr= f->unpack(f->ptr, pack_ptr, tabledef->field_metadata(i));
+        else
+          pack_ptr= f->unpack(f->ptr, pack_ptr);
       }
 
       bitmap_set_bit(rw_set, f->field_index);
       null_mask <<= 1;
     }
+    i++;
   }
 
   /*
