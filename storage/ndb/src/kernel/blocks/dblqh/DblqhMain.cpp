1694d4928b24da1525aeb973e95c2f03aa6c97d5
pekka/stewart@mysql.com[stewart] <>
2007-08-01 14:45:46 +1000

    [PATCH] time GCP protocol on each node (in 3 parts) and send info event if time is "excessive" or once if error 7901 is inserted
    Index: telco-6.1/storage/ndb/src/kernel/blocks/dbdih/DbdihMain.cpp
    ===================================================================

--- storage/ndb/src/kernel/blocks/dblqh/DblqhMain.cpp
+++ storage/ndb/src/kernel/blocks/dblqh/DblqhMain.cpp
@@ -12104,6 +12104,10 @@ void Dblqh::execGCP_SAVEREQ(Signal* signal)
     return;
   }
 
+#ifdef GCP_TIMER_HACK
+  NdbTick_getMicroTimer(&globalData.gcp_timer_save[0]);
+#endif
+
   ccurrentGcprec = 0;
   gcpPtr.i = ccurrentGcprec;
   ptrCheckGuard(gcpPtr, cgcprecFileSize, gcpRecord);
@@ -12325,6 +12329,11 @@ Dblqh::execFSSYNCCONF(Signal* signal)
       return;
     }//if
   }//for
+
+#ifdef GCP_TIMER_HACK
+  NdbTick_getMicroTimer(&globalData.gcp_timer_save[1]);
+#endif
+
   GCPSaveConf * const saveConf = (GCPSaveConf *)&signal->theData[0];
   saveConf->dihPtr = localGcpPtr.p->gcpUserptr;
   saveConf->nodeId = getOwnNodeId();
