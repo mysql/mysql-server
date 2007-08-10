326da930c4273d5aa6de9efba58f06d8d2f32c9b
Bradley C. Kuszmaul <bradley@tokutek.com>
2007-08-10 21:15:17 +0000

    log-test checks to see if we can find log numbers
    git-svn-id: file:///svn/tokudb@106 c7de825b-a66e-492c-adef-691d508d4ae1

--- newbrt/log.c
+++ newbrt/log.c
@@ -1,11 +1,11 @@
 #include "log-internal.h"
-#include "yerror.h"
+#include "memory.h"
 #include <sys/types.h>
 #include <dirent.h>
 #include <errno.h>
 #include <stdio.h>
 
-int find_next_unused_log_file(const char *directory, long long *result) {
+int tokulogger_find_next_unused_log_file(const char *directory, long long *result) {
     DIR *d=opendir(directory);
     long long max=-1;
     struct dirent *de;
@@ -20,19 +20,19 @@ int find_next_unused_log_file(const char *directory, long long *result) {
     return 0;
 }
 
-int create_and_open_logger (const char *directory) {
+int tokulogger_create_and_open_logger (const char *directory) {
     TAGMALLOC(TOKULOGGER, result);
     if (result==0) return -1;
     int r;
     long long nexti;
-    r = find_next_unused_log_file(directory, &nexti);
+    r = tokulogger_find_next_unused_log_file(directory, &nexti);
     if (r!=0) {
     died0:
 	toku_free(result);
 	return nexti;
     }
     result->directory = toku_strdup(directory);
-    if (result->directory!=0) oto died0;
+    if (result->directory!=0) goto died0;
     result->f = 0;
     result->next_log_file_number = nexti;
     result->n_in_buf = 0;
