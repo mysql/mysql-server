9b6b53f0d0d747e47a707d7c579b9e2b66020ed3
monty@mysql.com/narttu.mysql.fi <>
2007-08-16 16:47:31 +0300

    Fixed errors found by pushbuild: Fixed failing func_misc test for embedded server Added casts to avoid compiler warnings Removed Table_locks_immediate as it's depending on log file cacheing Changed type of get_time() to avoid warnings Removed testing if purger master logs succeded as this is not deterministic

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -4162,7 +4162,7 @@ sub stop_all_servers () {
   my $pid;
 
   # Start shutdown of all started masters
-  foreach my $mysqld (@{$master}, @{$slave})
+  foreach my $mysqld (@{$slave}, @{$master})
   {
     if ( $mysqld->{'pid'} )
     {
