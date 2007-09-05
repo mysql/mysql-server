1d54aef5b3c7e30e497a17969e77d544cc1833fc
Bradley C. Kuszmaul <bradley@tokutek.com>
2007-09-05 19:16:50 +0000

    64
    git-svn-id: file:///svn/tokudb@260 c7de825b-a66e-492c-adef-691d508d4ae1

--- buildheader/sample_offsets_64.h
+++ buildheader/sample_offsets_64.h
@@ -34,3 +34,26 @@ struct fieldinfo dbc_fields64[] = {
   {"int (*c_get) (DBC *, DBT *, DBT *, u_int32_t)", 336, 8},
   {0, 0, 432} /* size of whole struct */
 };
+struct fieldinfo db_env_fields64[] = {
+  {"int  (*close) (DB_ENV *, u_int32_t)", 432, 8},
+  {"void (*err) (const DB_ENV *, int, const char *, ...)", 456, 8},
+  {"int  (*open) (DB_ENV *, const char *, u_int32_t, int)", 472, 8},
+  {"int  (*set_data_dir) (DB_ENV *, const char *)", 488, 8},
+  {"void (*set_errcall) (DB_ENV *, void (*)(const char *, char *))", 520, 8},
+  {"void (*set_errpfx) (DB_ENV *, const char *)", 536, 8},
+  {"int  (*set_flags) (DB_ENV *, u_int32_t, int)", 552, 8},
+  {"void (*set_noticecall) (DB_ENV *, void (*)(DB_ENV *, db_notices))", 560, 8},
+  {"int  (*set_tmp_dir) (DB_ENV *, const char *)", 600, 8},
+  {"int  (*set_verbose) (DB_ENV *, u_int32_t, int)", 608, 8},
+  {"int  (*set_lg_bsize) (DB_ENV *, u_int32_t)", 624, 8},
+  {"int  (*set_lg_dir) (DB_ENV *, const char *)", 632, 8},
+  {"int  (*set_lg_max) (DB_ENV *, u_int32_t)", 640, 8},
+  {"int  (*log_archive) (DB_ENV *, char **[], u_int32_t)", 656, 8},
+  {"int  (*log_flush) (DB_ENV *, const DB_LSN *)", 680, 8},
+  {"int  (*set_lk_detect) (DB_ENV *, u_int32_t)", 720, 8},
+  {"int  (*set_lk_max) (DB_ENV *, u_int32_t)", 728, 8},
+  {"int  (*set_cachesize) (DB_ENV *, u_int32_t, u_int32_t, int)", 856, 8},
+  {"int  (*txn_checkpoint) (DB_ENV *, u_int32_t, u_int32_t, u_int32_t)", 1040, 8},
+  {"int  (*txn_stat) (DB_ENV *, DB_TXN_STAT **, u_int32_t)", 1064, 8},
+  {0, 0, 1096} /* size of whole struct */
+};
