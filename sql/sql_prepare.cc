18300001c1dbbfddf9a0adcbaeea68956102bdd0
malff@lambda.weblab <>
2007-09-10 16:10:37 -0600

    WL#4030 (Deprecate RENAME DATABASE: replace with ALTER DATABASE <name>   UPGRADE)
    Bug 17565 (RENAME DATABASE destroys events)
    Bug#28360 (RENAME DATABASE destroys routines)
    
    Removed the
      RENAME DATABASE db1 TO db2
    statement.
    
    Implemented the
      ALTER DATABASE db UPGRADE DATA DIRECTORY NAME
    statement, which has the same function.

--- sql/sql_prepare.cc
+++ sql/sql_prepare.cc
@@ -1799,7 +1799,7 @@ static bool check_prepared_statement(Prepared_statement *stmt,
   case SQLCOM_UNINSTALL_PLUGIN:
   case SQLCOM_CREATE_DB:
   case SQLCOM_DROP_DB:
-  case SQLCOM_RENAME_DB:
+  case SQLCOM_ALTER_DB_UPGRADE:
   case SQLCOM_CHECKSUM:
   case SQLCOM_CREATE_USER:
   case SQLCOM_RENAME_USER:
