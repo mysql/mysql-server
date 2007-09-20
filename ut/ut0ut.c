c1969aaeb5b90a77598012dde5095aa81a2ec0f9
vasil <>
2007-09-20 14:37:03 +0000

    Revert r1850 as MySQL did not approve the addition.
    log for r1850:
    
    Implement this feature request:
    http://bugs.mysql.com/30706
    
    * Add a function that returns the number of microseconds since
      epoch - ut_time_us().
    
    * Add (innodb|innobase|srv)_replication_delay MySQL config parameter.
    
    * Add UT_WAIT_FOR() macro that waits for a specified condition to occur
      until a timeout elapses.
    
    * Using all of the above, handle the replication thread specially in
      srv_conc_enter_innodb().

--- ut/ut0ut.c
+++ ut/ut0ut.c
@@ -128,31 +128,6 @@ ut_usectime(
 }
 
 /**************************************************************
-Returns the number of microseconds since epoch. Similar to
-time(3), the return value is also stored in *tloc, provided
-that tloc is non-NULL. */
-
-ullint
-ut_time_us(
-/*=======*/
-			/* out: us since epoch */
-	ullint*	tloc)	/* out: us since epoch, if non-NULL */
-{
-	struct timeval	tv;
-	ullint		us;
-
-	ut_gettimeofday(&tv, NULL);
-
-	us = (ullint) tv.tv_sec * 1000000 + tv.tv_usec;
-
-	if (tloc != NULL) {
-		*tloc = us;
-	}
-
-	return(us);
-}
-
-/**************************************************************
 Returns the difference of two times in seconds. */
 
 double
