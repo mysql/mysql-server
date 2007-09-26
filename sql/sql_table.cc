24f7a2e78a9cdedc90924f5bd5d50b98009fd61f
mskold/marty@mysql.com/linux.site <>
2007-09-26 11:35:11 +0200

    Added more checks for unsupported online alter table: no support for column re-order, no support for changing storage engine

--- sql/sql_table.cc
+++ sql/sql_table.cc
@@ -5832,6 +5832,11 @@ mysql_prepare_alter_table(THD *thd, TABLE *table,
         TODO: detect the situation in compare_tables, behave based
         on engine capabilities.
       */
+      if (alter_info->build_method == HA_BUILD_ONLINE)
+      {
+        my_error(ER_NOT_SUPPORTED_YET, MYF(0), thd->query);
+        goto err;
+      }
       alter_info->build_method= HA_BUILD_OFFLINE;
     }
   }
@@ -6472,7 +6477,14 @@ view_err:
       || partition_changed
 #endif
      )
+  {
+    if (alter_info->build_method == HA_BUILD_ONLINE)
+    {
+      my_error(ER_NOT_SUPPORTED_YET, MYF(0), thd->query);
+      goto err;
+    }
     alter_info->build_method= HA_BUILD_OFFLINE;
+  }
 
   if (alter_info->build_method != HA_BUILD_OFFLINE)
   {
