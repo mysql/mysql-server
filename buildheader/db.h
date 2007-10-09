ada8d57b8cfd169751ec0fa13c914011a509b064
Bradley C. Kuszmaul <bradley@tokutek.com>
2007-10-09 22:24:25 +0000

    Up
    git-svn-id: file:///svn/tokudb@427 c7de825b-a66e-492c-adef-691d508d4ae1

--- buildheader/db.h
+++ buildheader/db.h
@@ -205,6 +205,7 @@ struct __toku_dbt {
 int db_env_create(DB_ENV **, u_int32_t);
 int db_create(DB **, DB_ENV *, u_int32_t);
 char *db_strerror(int);
+int log_compare (const DB_LSN*, const DB_LSN *);
 #if defined(__cplusplus)
 }
 #endif
