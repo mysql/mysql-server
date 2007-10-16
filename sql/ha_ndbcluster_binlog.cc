53aaf09ace6c13ad8ced243d8e09450a11501bf8
tomas@whalegate.ndb.mysql.com <>
2007-10-16 11:02:09 +0200

    Bug #31618 ndb_binlog_discover test failure - make sure all tables are inited before binlog thread may run

--- sql/ha_ndbcluster_binlog.cc
+++ sql/ha_ndbcluster_binlog.cc
@@ -4230,7 +4230,8 @@ restart:
 
     pthread_mutex_lock(&injector_mutex);
     while (!ndb_schema_share ||
-           (ndb_binlog_running && !ndb_apply_status_share))
+           (ndb_binlog_running && !ndb_apply_status_share) ||
+           !ndb_binlog_tables_inited)
     {
       /* ndb not connected yet */
       struct timespec abstime;
