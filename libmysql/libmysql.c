3d203e55e67946e505f85a7d3a4321794ece51fe
tsmith@ramayana.hindu.god <>
2007-10-17 11:22:41 -0600

    Fix syntax error build problem on Windows (variable was defined in middle of block)

--- libmysql/libmysql.c
+++ libmysql/libmysql.c
@@ -3876,15 +3876,19 @@ static void fetch_float_with_conversion(MYSQL_BIND *param, MYSQL_FIELD *field,
       sprintf(buff, "%.*f", (int) field->decimals, value);
       end= strend(buff);
     }
-    uint length= (uint) (end-buff);
-    if (field->flags & ZEROFILL_FLAG && length < field->length &&
-        field->length < MAX_DOUBLE_STRING_REP_LENGTH-1)
+
     {
-      bmove_upp((char*) buff+field->length,buff+length, length);
-      bfill((char*) buff, field->length - length,'0');
-      length= field->length;
+      size_t length= end - buff;
+      if (field->flags & ZEROFILL_FLAG && length < field->length &&
+          field->length < MAX_DOUBLE_STRING_REP_LENGTH - 1)
+      {
+        bmove_upp((char*) buff + field->length, buff + length, length);
+        bfill((char*) buff, field->length - length, '0');
+        length= field->length;
+      }
+      fetch_string_with_conversion(param, buff, length);
     }
-    fetch_string_with_conversion(param, buff, length);
+
     break;
   }
   }
