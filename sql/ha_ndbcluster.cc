84fd0cae937ee081321bd4b0f86e9b5ed16eeae2
knielsen@ymer.(none) <>
2007-10-26 13:09:35 +0200

    Bug #31874: Failure for NDB update/delete with combined range and equality constraints
    In update/delete with read_multi_range, when mixing pk lookups and range
    scans, sometimes update/delete did not correctly distinguish whether
    the current row was from pk lookup or from scan. This caused query
    failure (or potentially memory corruption and crash).
    
    Fix by setting proper flags in the read_multi_range_next() method.

--- sql/ha_ndbcluster.cc
+++ sql/ha_ndbcluster.cc
@@ -4202,7 +4202,11 @@ int ha_ndbcluster::close_scan()
   NdbScanOperation *cursor= m_active_cursor;
   
   if (!cursor)
-    DBUG_RETURN(0);
+  {
+    cursor = m_multi_cursor;
+    if (!cursor)
+      DBUG_RETURN(0);
+  }
 
   if ((error= scan_handle_lock_tuple(cursor, trans)) != 0)
     DBUG_RETURN(error);
@@ -4222,6 +4226,7 @@ int ha_ndbcluster::close_scan()
   
   cursor->close(m_force_send, TRUE);
   m_active_cursor= NULL;
+  m_multi_cursor= NULL;
   DBUG_RETURN(0);
 }
 
@@ -5011,6 +5016,10 @@ int ha_ndbcluster::external_lock(THD *thd, int lock_type)
     if (m_active_cursor)
       DBUG_PRINT("warning", ("m_active_cursor != NULL"));
     m_active_cursor= NULL;
+
+    if (m_multi_cursor)
+      DBUG_PRINT("warning", ("m_multi_cursor != NULL"));
+    m_multi_cursor= NULL;
 
     if (m_blobs_pending)
       DBUG_PRINT("warning", ("blobs_pending != 0"));
@@ -6797,7 +6806,8 @@ ha_ndbcluster::ha_ndbcluster(handlerton *hton, TABLE_SHARE *table_arg):
   m_force_send(TRUE),
   m_autoincrement_prefetch((ha_rows) 32),
   m_transaction_on(TRUE),
-  m_cond(NULL)
+  m_cond(NULL),
+  m_multi_cursor(NULL)
 {
   int i;
  
@@ -9763,7 +9773,7 @@ ha_ndbcluster::read_multi_range_first(KEY_MULTI_RANGE **found_range_p,
        mask, flags, parallelism, 0);
     if (!scanOp)
       ERR_RETURN(m_active_trans->getNdbError());
-    m_active_cursor= scanOp;
+    m_multi_cursor= scanOp;
 
     /*
       We do not get_blob_values() here, as when using blobs we always
@@ -9779,7 +9789,7 @@ ha_ndbcluster::read_multi_range_first(KEY_MULTI_RANGE **found_range_p,
   }
   else
   {
-    m_active_cursor= 0;
+    m_multi_cursor= 0;
   }
 
   buffer->end_of_used_area= row_buf;
@@ -9820,6 +9830,12 @@ ha_ndbcluster::read_multi_range_next(KEY_MULTI_RANGE ** multi_range_found_p)
       */
       KEY_MULTI_RANGE *old_multi_range_curr= multi_range_curr;
       multi_range_curr= old_multi_range_curr + 1;
+      /*
+        Clear m_active_cursor; it is used as a flag in update_row() /
+        delete_row() to know whether the current tuple is from a scan
+        or pk operation.
+      */
+      m_active_cursor= NULL;
       const NdbOperation *op= m_current_multi_operation;
       m_current_multi_operation= m_active_trans->getNextCompletedOperation(op);
       const uchar *src_row= m_multi_range_result_ptr;
@@ -9893,6 +9909,13 @@ ha_ndbcluster::read_multi_range_next(KEY_MULTI_RANGE ** multi_range_found_p)
             one on the next call.
           */
           m_next_row= 0;
+          /*
+            Set m_active_cursor; it is used as a flag in update_row() /
+            delete_row() to know whether the current tuple is from a scan or
+            pk operation.
+          */
+          m_active_cursor= m_multi_cursor;
+
           DBUG_RETURN(0);
         }
         else if (current_range_no > expected_range_no)
@@ -9941,7 +9964,7 @@ ha_ndbcluster::read_multi_range_next(KEY_MULTI_RANGE ** multi_range_found_p)
 int
 ha_ndbcluster::read_multi_range_fetch_next()
 {
-  NdbIndexScanOperation *cursor= (NdbIndexScanOperation *)m_active_cursor;
+  NdbIndexScanOperation *cursor= (NdbIndexScanOperation *)m_multi_cursor;
 
   if (!cursor)
     return 0;                                   // Scan already done.
@@ -9958,6 +9981,7 @@ ha_ndbcluster::read_multi_range_fetch_next()
       /* We have fetched the last row from the scan. */
       cursor->close(FALSE, TRUE);
       m_active_cursor= 0;
+      m_multi_cursor= 0;
       m_next_row= 0;
       return 0;
     }
