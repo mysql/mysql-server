77911386f36a644ff965539bf33e0fed7d041f77
tomas@whalegate.ndb.mysql.com <>
2007-10-29 22:47:51 +0100

    wl#3686 - server part - number of found records now printed correctly

--- sql/sql_update.cc
+++ sql/sql_update.cc
@@ -854,9 +854,13 @@ int mysql_update(THD *thd,
       also when using BEFORE UPDATE triggers on table and also quite
       hard checks on UPDATE statement. Still it is used very often with
       all those limitations.
+
+      Moreover, since there is no read before update, found == updated,
+      as there is no optimization to remove the update if the new data
+      should equal the old.
     */
     table->file->info(HA_STATUS_WRITTEN_ROWS);
-    updated= table->file->stats.rows_updated;
+    found= updated= table->file->stats.rows_updated;
   }
 
   if (thd->lock)
