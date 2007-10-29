3d59e53a2ccb60739e76244ede066e667723bf4e
kent@mysql.com/kent-amd64.(none) <>
2007-10-29 20:12:44 +0100

    autorun.sh:   Removed innobase from compile-dist, and let it use autorun.sh to avoid   code duplication. Also corrected a problem when searching for one of   "libtoolize" and "glibtoolize", now it honors the PATH setting (Bug#31405)

--- BUILD/autorun.sh
+++ BUILD/autorun.sh
@@ -3,20 +3,29 @@
 
 die() { echo "$@"; exit 1; }
 
-# Added glibtoolize reference to make native OSX autotools work
-if [ -f /usr/bin/glibtoolize ]
+# Added glibtoolize reference to make native OSX autotools work,
+# we pick the first of glibtoolize/libtoolize we find in PATH
+LIBTOOLIZE=libtoolize  # Default
+IFS="${IFS=   }"; save_ifs="$IFS"; IFS=':'
+for dir in $PATH
+do
+  if test -f $dir/glibtoolize
   then
     LIBTOOLIZE=glibtoolize
-  else
-    LIBTOOLIZE=libtoolize
-fi
+    break
+  elif test -f $dir/libtoolize
+  then
+    break
+  fi
+done
+IFS="$save_ifs"
 
 aclocal || die "Can't execute aclocal" 
 autoheader || die "Can't execute autoheader"
 # --force means overwrite ltmain.sh script if it already exists 
-$LIBTOOLIZE --automake --force || die "Can't execute libtoolize"
+$LIBTOOLIZE --automake --force --copy || die "Can't execute libtoolize"
   
 # --add-missing instructs automake to install missing auxiliary files
 # and --force to overwrite them if they already exist
-automake --add-missing --force || die "Can't execute automake"
+automake --add-missing --force  --copy || die "Can't execute automake"
 autoconf || die "Can't execute autoconf"
