a88d94614c565e0805ca95f5f8b651e2c6f1034a
tsmith@ramayana.hindu.god <>
2007-11-06 16:40:50 -0700

    Apply snapshot innodb-5.1-ss2034
    The following bugs are fixed:
    
    Bug #31860: Server crashes after inserting into InnoDB table with auto_increment column
      In the Bug 16979 fix there was an erroneous assertion that
      autoincrement columns can't contain negative values. With the fix, the
      autoincrement table counter is set to 0 if the maximum value read from
      the autoinc column index is negative.

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -1144,7 +1144,6 @@ innobase_query_caching_of_table_permitted(
 	}
 
 	if (trx->has_search_latch) {
-		ut_print_timestamp(stderr);
 		sql_print_error("The calling thread is holding the adaptive "
 				"search, latch though calling "
 				"innobase_query_caching_of_table_permitted.");
@@ -2322,7 +2321,6 @@ ha_innobase::open(
 	ib_table = dict_table_get(norm_name, TRUE);
 
 	if (NULL == ib_table) {
-		ut_print_timestamp(stderr);
 		sql_print_error("Cannot find or open table %s from\n"
 				"the internal data dictionary of InnoDB "
 				"though the .frm file for the\n"
@@ -2346,7 +2344,6 @@ ha_innobase::open(
 	}
 
 	if (ib_table->ibd_file_missing && !thd_tablespace_op(thd)) {
-		ut_print_timestamp(stderr);
 		sql_print_error("MySQL is trying to open a table handle but "
 				"the .ibd file for\ntable %s does not exist.\n"
 				"Have you deleted the .ibd file from the "
@@ -3420,7 +3417,7 @@ no_commit:
 			/*
 			ut_print_timestamp(stderr);
 			fprintf(stderr,
-				"  InnoDB error: ALTER TABLE is holding lock"
+				"  InnoDB: ALTER TABLE is holding lock"
 				" on %lu tables!\n",
 				prebuilt->trx->mysql_n_tables_locked);
 			*/
@@ -5723,7 +5720,6 @@ ha_innobase::info(
 
 		for (i = 0; i < table->s->keys; i++) {
 			if (index == NULL) {
-				ut_print_timestamp(stderr);
 				sql_print_error("Table %s contains fewer "
 						"indexes inside InnoDB than "
 						"are defined in the MySQL "
@@ -5739,7 +5735,6 @@ ha_innobase::info(
 			for (j = 0; j < table->key_info[i].key_parts; j++) {
 
 				if (j + 1 > index->n_uniq) {
-					ut_print_timestamp(stderr);
 					sql_print_error(
 "Index %s of %s has %lu columns unique inside InnoDB, but MySQL is asking "
 "statistics for %lu columns. Have you mixed up .frm files from different "
@@ -5804,7 +5799,6 @@ ha_innobase::info(
 			ret = innobase_read_and_init_auto_inc(&auto_inc);
 
 			if (ret != 0) {
-				ut_print_timestamp(stderr);
 				sql_print_error("Cannot get table %s auto-inc"
 						"counter value in ::info\n",
 						ib_table->name);
@@ -6578,14 +6572,17 @@ ha_innobase::transactional_table_lock(
 
 	if (prebuilt->table->ibd_file_missing && !thd_tablespace_op(thd)) {
 		ut_print_timestamp(stderr);
-		fprintf(stderr, "  InnoDB error:\n"
-"MySQL is trying to use a table handle but the .ibd file for\n"
-"table %s does not exist.\n"
-"Have you deleted the .ibd file from the database directory under\n"
-"the MySQL datadir?"
-"See http://dev.mysql.com/doc/refman/5.1/en/innodb-troubleshooting.html\n"
-"how you can resolve the problem.\n",
-				prebuilt->table->name);
+		fprintf(stderr,
+			"  InnoDB: MySQL is trying to use a table handle"
+			" but the .ibd file for\n"
+			"InnoDB: table %s does not exist.\n"
+			"InnoDB: Have you deleted the .ibd file"
+			" from the database directory under\n"
+			"InnoDB: the MySQL datadir?"
+			"InnoDB: See"
+			" http://dev.mysql.com/doc/refman/5.1/en/innodb-troubleshooting.html\n"
+			"InnoDB: how you can resolve the problem.\n",
+			prebuilt->table->name);
 		DBUG_RETURN(HA_ERR_CRASHED);
 	}
 
@@ -7194,7 +7191,8 @@ ha_innobase::innobase_read_and_init_auto_inc(
 			++auto_inc;
 			dict_table_autoinc_initialize(innodb_table, auto_inc);
 		} else {
-			fprintf(stderr, " InnoDB error (%lu): Couldn't read "
+			ut_print_timestamp(stderr);
+			fprintf(stderr, "  InnoDB: Error: (%lu) Couldn't read "
 				"the max AUTOINC value from the index (%s).\n",
 				error, index->name);
 
@@ -7281,8 +7279,7 @@ ha_innobase::innobase_get_auto_increment(
 		and can be ignored. */
 		} else if (error != DB_DEADLOCK) {
 
-			ut_print_timestamp(stderr);
-			sql_print_error(" InnoDB Error %lu in "
+			sql_print_error("InnoDB: Error: %lu in "
 					"::innobase_get_auto_increment()",
 					error);
 		}
