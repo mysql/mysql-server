0e463512cbf2311f0bd6b698123ec1093dd6e959
Rich Prohaska <prohaska@tokutek.com>
2007-11-15 14:09:40 +0000

    add bdb 4.4 and 4.6
    git-svn-id: file:///svn/tokudb@563 c7de825b-a66e-492c-adef-691d508d4ae1

---
+++ buildheader/sample_offsets_32_4_6.h
@@ -0,0 +1,90 @@
+/* BDB offsets on a 32-bit machine */
+#define DB_VERSION_MAJOR_32 4
+#define DB_VERSION_MINOR_32 6
+#define DB_VERSION_STRING_32 "Berkeley DB Compatability Header 4.6"
+struct fieldinfo db_btree_stat_fields32[] = {
+  {"u_int32_t bt_nkeys", 12, 4},
+  {"u_int32_t bt_ndata", 16, 4},
+  {0, 84, 84} /* size of whole struct */
+};
+struct fieldinfo db_env_fields32[] = {
+  {"void *app_private", 52, 4},
+  {"int  (*close) (DB_ENV *, u_int32_t)", 392, 4},
+  {"void (*err) (const DB_ENV *, int, const char *, ...)", 404, 4},
+  {"int  (*log_archive) (DB_ENV *, char **[], u_int32_t)", 576, 4},
+  {"int  (*log_flush) (DB_ENV *, const DB_LSN *)", 588, 4},
+  {"int  (*open) (DB_ENV *, const char *, u_int32_t, int)", 692, 4},
+  {"int  (*set_cachesize) (DB_ENV *, u_int32_t, u_int32_t, int)", 816, 4},
+  {"int  (*set_data_dir) (DB_ENV *, const char *)", 824, 4},
+  {"void (*set_errcall) (DB_ENV *, void (*)(const char *, char *))", 832, 4},
+  {"void (*set_errpfx) (DB_ENV *, const char *)", 840, 4},
+  {"int  (*set_flags) (DB_ENV *, u_int32_t, int)", 852, 4},
+  {"int  (*set_lg_bsize) (DB_ENV *, u_int32_t)", 864, 4},
+  {"int  (*set_lg_dir) (DB_ENV *, const char *)", 868, 4},
+  {"int  (*set_lg_max) (DB_ENV *, u_int32_t)", 876, 4},
+  {"int  (*set_lk_detect) (DB_ENV *, u_int32_t)", 888, 4},
+  {"int  (*set_tmp_dir) (DB_ENV *, const char *)", 956, 4},
+  {"int  (*set_verbose) (DB_ENV *, u_int32_t, int)", 968, 4},
+  {"int  (*txn_begin) (DB_ENV *, DB_TXN *, DB_TXN **, u_int32_t)", 976, 4},
+  {"int  (*txn_checkpoint) (DB_ENV *, u_int32_t, u_int32_t, u_int32_t)", 980, 4},
+  {"int  (*txn_stat) (DB_ENV *, DB_TXN_STAT **, u_int32_t)", 988, 4},
+  {0, 1016, 1016} /* size of whole struct */
+};
+struct fieldinfo db_key_range_fields32[] = {
+  {"double less", 0, 8},
+  {"double equal", 8, 8},
+  {"double greater", 16, 8},
+  {0, 1016, 1016} /* size of whole struct */
+};
+struct fieldinfo db_lsn_fields32[] = {
+  {0, 8, 8} /* size of whole struct */
+};
+struct fieldinfo db_fields32[] = {
+  {"void *app_private", 20, 4},
+  {"DB_ENV *dbenv", 24, 4},
+  {"int (*close) (DB*, u_int32_t)", 300, 4},
+  {"int (*cursor) (DB *, DB_TXN *, DBC **, u_int32_t)", 308, 4},
+  {"int (*del) (DB *, DB_TXN *, DBT *, u_int32_t)", 312, 4},
+  {"int (*get) (DB *, DB_TXN *, DBT *, DBT *, u_int32_t)", 332, 4},
+  {"int (*key_range) (DB *, DB_TXN *, DBT *, DB_KEY_RANGE *, u_int32_t)", 440, 4},
+  {"int (*open) (DB *, DB_TXN *, const char *, const char *, DBTYPE, u_int32_t, int)", 444, 4},
+  {"int (*put) (DB *, DB_TXN *, DBT *, DBT *, u_int32_t)", 452, 4},
+  {"int (*remove) (DB *, const char *, const char *, u_int32_t)", 456, 4},
+  {"int (*rename) (DB *, const char *, const char *, const char *, u_int32_t)", 460, 4},
+  {"int (*set_bt_compare) (DB *, int (*)(DB *, const DBT *, const DBT *))", 472, 4},
+  {"int (*set_dup_compare) (DB *, int (*)(DB *, const DBT *, const DBT *))", 488, 4},
+  {"int (*set_flags) (DB *, u_int32_t)", 512, 4},
+  {"int (*set_pagesize) (DB *, u_int32_t)", 544, 4},
+  {"int (*stat) (DB *, void *, u_int32_t)", 576, 4},
+  {"int (*verify) (DB *, const char *, const char *, FILE *, u_int32_t)", 596, 4},
+  {0, 636, 636} /* size of whole struct */
+};
+struct fieldinfo db_txn_active_fields32[] = {
+  {"u_int32_t txnid", 0, 4},
+  {"DB_LSN lsn", 16, 8},
+  {0, 224, 224} /* size of whole struct */
+};
+struct fieldinfo db_txn_fields32[] = {
+  {"int (*abort) (DB_TXN *)", 100, 4},
+  {"int (*commit) (DB_TXN*, u_int32_t)", 104, 4},
+  {"u_int32_t (*id) (DB_TXN *)", 116, 4},
+  {0, 140, 140} /* size of whole struct */
+};
+struct fieldinfo db_txn_stat_fields32[] = {
+  {"u_int32_t st_nactive", 36, 4},
+  {"DB_TXN_ACTIVE *st_txnarray", 52, 4},
+  {0, 68, 68} /* size of whole struct */
+};
+struct fieldinfo dbc_fields32[] = {
+  {"int (*c_close) (DBC *)", 244, 4},
+  {"int (*c_del) (DBC *, u_int32_t)", 252, 4},
+  {"int (*c_get) (DBC *, DBT *, DBT *, u_int32_t)", 260, 4},
+  {0, 304, 304} /* size of whole struct */
+};
+struct fieldinfo dbt_fields32[] = {
+  {"void*data", 0, 4},
+  {"u_int32_t size", 4, 4},
+  {"u_int32_t ulen", 8, 4},
+  {"u_int32_t flags", 24, 4},
+  {0, 28, 28} /* size of whole struct */
+};
