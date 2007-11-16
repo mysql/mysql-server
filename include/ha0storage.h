c7d534b59b4c7ee02f0cc83d8a687f88673da054
vasil <>
2007-11-16 13:12:13 +0000

    branches/zip:
    Implement a limit on the memory used by the INNODB_TRX, INNODB_LOCKS and
    INNODB_LOCK_WAITS tables. The maximum allowed memory is defined with the
    macro TRX_I_S_MEM_LIMIT.
    
    Approved by:	Marko (via IM)

--- include/ha0storage.h
+++ include/ha0storage.h
@@ -36,6 +36,31 @@ ha_storage_create(
 					in the hash table */
 
 /***********************************************************************
+Copies data into the storage and returns a pointer to the copy. If the
+same data chunk is already present, then pointer to it is returned.
+Data chunks are considered to be equal if len1 == len2 and
+memcmp(data1, data2, len1) == 0. If "data" is not present (and thus
+data_len bytes need to be allocated) and the size of storage is going to
+become more than "memlim" then "data" is not added and NULL is returned.
+To disable this behavior "memlim" can be set to 0, which stands for
+"no limit". */
+
+const void*
+ha_storage_put_memlim(
+/*==================*/
+					/* out: pointer to the copy */
+	ha_storage_t*	storage,	/* in/out: hash storage */
+	const void*	data,		/* in: data to store */
+	ulint		data_len,	/* in: data length */
+	ulint		memlim);	/* in: memory limit to obey */
+
+/***********************************************************************
+Same as ha_storage_put_memlim() but without memory limit. */
+
+#define ha_storage_put(storage, data, data_len)	\
+	ha_storage_put_memlim((storage), (data), (data_len), 0)
+
+/***********************************************************************
 Copies string into the storage and returns a pointer to the copy. If the
 same string is already present, then pointer to it is returned.
 Strings are considered to be equal if strcmp(str1, str2) == 0. */
@@ -44,17 +69,12 @@ Strings are considered to be equal if strcmp(str1, str2) == 0. */
 	((const char*) ha_storage_put((storage), (str), strlen(str) + 1))
 
 /***********************************************************************
-Copies data into the storage and returns a pointer to the copy. If the
-same data chunk is already present, then pointer to it is returned.
-Data chunks are considered to be equal if len1 == len2 and
-memcmp(data1, data2, len1) == 0. */
+Copies string into the storage and returns a pointer to the copy obeying
+a memory limit. */
 
-const void*
-ha_storage_put(
-/*===========*/
-	ha_storage_t*	storage,	/* in/out: hash storage */
-	const void*	data,		/* in: data to store */
-	ulint		data_len);	/* in: data length */
+#define ha_storage_put_str_memlim(storage, str, memlim)	\
+	((const char*) ha_storage_put_memlim((storage), (str),	\
+					     strlen(str) + 1, (memlim)))
 
 /***********************************************************************
 Empties a hash storage, freeing memory occupied by data chunks.
