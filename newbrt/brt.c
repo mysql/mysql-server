4594d58954d6e1f75309a9777852a956bdf1115a
Rich Prohaska <prohaska@tokutek.com>
2007-11-17 13:23:32 +0000

    serialize pivotflags
    git-svn-id: file:///svn/tokudb@608 c7de825b-a66e-492c-adef-691d508d4ae1

--- newbrt/brt.c
+++ newbrt/brt.c
@@ -176,7 +176,8 @@ void brtnode_flush_callback (CACHEFILE cachefile, DISKOFF nodename, void *brtnod
 int brtnode_fetch_callback (CACHEFILE cachefile, DISKOFF nodename, void **brtnode_pv, long *sizep, void*extraargs, LSN *written_lsn) {
     BRT t =(BRT)extraargs;
     BRTNODE *result=(BRTNODE*)brtnode_pv;
-    int r = deserialize_brtnode_from(cachefile_fd(cachefile), nodename, result, t->nodesize, t->compare_fun);
+    int r = deserialize_brtnode_from(cachefile_fd(cachefile), nodename, result, t->flags, t->nodesize, 
+                                     t->compare_fun, t->dup_compare);
     if (r == 0)
         *sizep = brtnode_size(*result);
     *written_lsn = (*result)->lsn;
@@ -449,6 +450,7 @@ void brt_nonleaf_split (BRT t, BRTNODE node, BRTNODE *nodea, BRTNODE *nodeb, DBT
 	for (i=0; i<n_children_in_a-1; i++) {
 	    A->u.n.childkeys[i] = node->u.n.childkeys[i];
 	    A->u.n.childkeylens[i] = node->u.n.childkeylens[i];
+            A->u.n.pivotflags[i] = node->u.n.pivotflags[i];
 	    A->u.n.totalchildkeylens += node->u.n.childkeylens[i];
 	    node->u.n.totalchildkeylens -= node->u.n.childkeylens[i];
 	    node->u.n.childkeys[i] = 0;
@@ -456,12 +458,14 @@ void brt_nonleaf_split (BRT t, BRTNODE node, BRTNODE *nodea, BRTNODE *nodeb, DBT
 	}
 	splitk->data = (void*)(node->u.n.childkeys[n_children_in_a-1]);
 	splitk->size = node->u.n.childkeylens[n_children_in_a-1];
+        splitk->flags = node->u.n.pivotflags[n_children_in_a-1];
 	node->u.n.totalchildkeylens -= node->u.n.childkeylens[n_children_in_a-1];
 	node->u.n.childkeys[n_children_in_a-1]=0;
 	node->u.n.childkeylens[n_children_in_a-1]=0;
 	for (i=n_children_in_a; i<node->u.n.n_children-1; i++) {
 	    B->u.n.childkeys[i-n_children_in_a] = node->u.n.childkeys[i];
 	    B->u.n.childkeylens[i-n_children_in_a] = node->u.n.childkeylens[i];
+            B->u.n.pivotflags[i-n_children_in_a] = node->u.n.pivotflags[i];
 	    B->u.n.totalchildkeylens += node->u.n.childkeylens[i];
 	    node->u.n.totalchildkeylens -= node->u.n.childkeylens[i];
 	    node->u.n.childkeys[i] = 0;
@@ -661,9 +665,11 @@ static int handle_split_of_child (BRT t, BRTNODE node, int childnum,
     for (cnum=node->u.n.n_children-1; cnum>childnum; cnum--) {
 	node->u.n.childkeys[cnum] = node->u.n.childkeys[cnum-1];
 	node->u.n.childkeylens[cnum] = node->u.n.childkeylens[cnum-1];
+        node->u.n.pivotflags[cnum] = node->u.n.pivotflags[cnum-1];
     }
     node->u.n.childkeys[childnum]= (char*)childsplitk->data;
     node->u.n.childkeylens[childnum]= childsplitk->size;
+    node->u.n.pivotflags[childnum] = childsplitk->flags;
     node->u.n.totalchildkeylens += childsplitk->size;
     node->u.n.n_children++;
 
@@ -1452,6 +1458,7 @@ int brt_init_new_root(BRT brt, BRTNODE nodea, BRTNODE nodeb, DBT splitk, CACHEKE
     //printf("%s:%d Splitkey=%p %s\n", __FILE__, __LINE__, splitkey, splitkey);
     newroot->u.n.childkeys[0] = splitk.data;
     newroot->u.n.childkeylens[0] = splitk.size;
+    newroot->u.n.pivotflags[0] = splitk.flags;
     newroot->u.n.totalchildkeylens=splitk.size;
     newroot->u.n.children[0]=nodea->thisnodename;
     newroot->u.n.children[1]=nodeb->thisnodename;
