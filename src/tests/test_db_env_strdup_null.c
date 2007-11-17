a3610877949ee5a210d6688ab6c01ed9b17d09b3
Bradley C. Kuszmaul <bradley@tokutek.com>
2007-11-17 15:43:58 +0000

    Improve teh bugs
    git-svn-id: file:///svn/tokudb@613 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/test_db_env_strdup_null.c
+++ src/tests/test_db_env_strdup_null.c
@@ -15,11 +15,14 @@ int main (int argc, char *argv[]) {
     r=system("rm -rf " DIR);                    assert(r==0);
     r=mkdir(DIR, 0777);                         assert(r==0);
     r=db_env_create(&env, 0);                   assert(r==0);
+// None of this stuff works with BDB.  TDB does more error checking.
+#ifdef USE_TDB
     r=env->set_data_dir(env, NULL);             assert(r==EINVAL);
     r=env->open(env, NULL, DB_PRIVATE, 0777);   assert(r==EINVAL);
     r=env->open(env, DIR, DB_PRIVATE, 0777);    assert(r==0);
     env->set_errpfx(env, NULL);                 assert(1); //Did not crash.
     r=env->set_tmp_dir(env, NULL);              assert(r==EINVAL);
+#endif
     r=env->close(env, 0);                       assert(r==0);
     return 0;
 }
