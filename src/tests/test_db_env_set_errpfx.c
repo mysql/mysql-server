7fefdb87bf0857caf24c7c3be4fd6440ca0e5ccf
Yoni Fogel <yoni@tokutek.com>
2007-11-20 19:55:48 +0000

    Fixed test_db_env_set_errpfx
    git-svn-id: file:///svn/tokudb@687 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/test_db_env_set_errpfx.c
+++ src/tests/test_db_env_set_errpfx.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <assert.h>
 #include <db.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 // DIR is defined in the Makefile
 
 int main() {
     DB_ENV *dbenv;
     int r;
+
+    system("rm -rf " DIR);
+    mkdir(DIR, 0777);
 
     r = db_env_create(&dbenv, 0);
     assert(r == 0);
@@ -15,6 +21,8 @@ int main() {
 
     r = dbenv->open(dbenv, DIR, DB_CREATE|DB_PRIVATE|DB_INIT_MPOOL, 0);
     assert(r == 0);
+
+    dbenv->set_errpfx(dbenv, "houdy partners: ");
 
     r = dbenv->close(dbenv, 0);
     assert(r == 0);
