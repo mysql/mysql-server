29ba787047b43f64a64243c8996c0b24a473d823
unknown <skozlov/ksm@mysql.com/virtop.localdomain>
2007-11-24 20:34:30 +0300

    WL#3949, Added the optimization of rpl tests if mtr started w/o a combination and --mysqld=--binlog-format=XXX
    mysql-test/lib/mtr_cases.pl:
      Added the optimization of rpl tests

--- mysql-test/lib/mtr_cases.pl
+++ mysql-test/lib/mtr_cases.pl
@@ -479,6 +479,17 @@ sub optimize_cases {
 	}
       }
 
+      # =======================================================
+      # Use dynamic switching of binlog-format if mtr started
+      # w/o --mysqld=--binlog-format=xxx and combinations.
+      # =======================================================
+      if (!defined $tinfo->{'combination'} and 
+          $tinfo->{'skip'} ne 1 and 
+          !defined $::used_binlog_format) 
+      {
+        $test_binlog_format= $tinfo->{'sup_binlog_formats'}->[0];
+      }
+
       # Save binlog format for dynamic switching
       $tinfo->{binlog_format}= $test_binlog_format;
     }
