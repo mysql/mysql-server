da98b27ffeda13084f373302383b67c36d8649b9
Bradley C. Kuszmaul <bradley@tokutek.com>
2007-11-25 14:33:54 +0000

    Improve test infrastructure.  Addresses #27.
    git-svn-id: file:///svn/tokudb@743 c7de825b-a66e-492c-adef-691d508d4ae1

---
+++ src/tests/test_log3.c
@@ -0,0 +1,35 @@
+/* Test to see if we can do logging and recovery. */
+/* This is very specific to TokuDB.  It won't work with Berkeley DB. */
+
+#include <assert.h>
+#include <db.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+// DIR is defined in the Makefile
+
+#define CKERR(r) if (r!=0) fprintf(stderr, "%s:%d error %d %s\n", __FILE__, __LINE__, r, db_strerror(r)); assert(r==0);
+
+static void make_db (void) {
+    DB_ENV *env;
+    DB *db;
+    DB_TXN *tid;
+    int r;
+
+    system("rm -rf " DIR);
+    r=mkdir(DIR, 0777);       assert(r==0);
+    r=db_env_create(&env, 0); assert(r==0);
+    r=env->open(env, DIR, DB_INIT_LOCK|DB_INIT_LOG|DB_INIT_MPOOL|DB_INIT_TXN|DB_CREATE|DB_PRIVATE, 0777); CKERR(r);
+    r=db_create(&db, env, 0); CKERR(r);
+    r=env->txn_begin(env, 0, &tid, 0); assert(r==0);
+    r=db->open(db, tid, "foo.db", 0, DB_BTREE, DB_CREATE, 0777); CKERR(r);
+    r=tid->commit(tid, 0);    assert(r==0);
+    r=db->close(db, 0);       assert(r==0);
+    r=env->close(env, 0);     assert(r==0);
+}
+
+int main (int argc __attribute__((__unused__)), char *argv[] __attribute__((__unused__))) {
+    make_db();
+    return 0;
+}
