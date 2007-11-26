939764b08fe13d63fe565524833d56d8d77122da
msvensson@pilot.mysql.com <>
2007-11-26 19:42:07 +0100

    Revert

--- client/mysql.cc
+++ client/mysql.cc
@@ -2166,7 +2166,7 @@ com_go(String *buffer,char *line __attribute__((unused)))
 {
   char		buff[200], time_buff[32], *pos;
   MYSQL_RES	*result;
-  ulong		timer, warnings= 0;
+  ulong		timer, warnings;
   uint		error= 0;
   int           err= 0;
 
