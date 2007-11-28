64dbec8b77b98c5e462437e1e90042cba598730f
Rich Prohaska <prohaska@tokutek.com>
2007-11-28 20:47:21 +0000

    implement db->del as a lookup followed by a delete to match BDB del return value.  also implemented a tokutek flag that suppresses the return value.  closes #45
    git-svn-id: file:///svn/tokudb@806 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/test_db_delete.c
+++ src/tests/test_db_delete.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 #include <arpa/inet.h>
 #include <sys/stat.h>
 #include <db.h>
@@ -28,9 +29,9 @@ void db_put(DB *db, int k, int v) {
     assert(r == 0);
 }
 
-void expect_db_del(DB *db, int k, int expectr) {
+void expect_db_del(DB *db, int k, int flags, int expectr) {
     DBT key;
-    int r = db->del(db, 0, dbt_init(&key, &k, sizeof k), 0);
+    int r = db->del(db, 0, dbt_init(&key, &k, sizeof k), flags);
     assert(r == expectr);
 }
 
@@ -41,12 +42,12 @@ void expect_db_get(DB *db, int k, int expectr) {
 }
 
 void test_db_delete(int n, int dup_mode) {
-    if (verbose) printf("test_rand_insert:%d %d\n", n, dup_mode);
+    if (verbose) printf("test_db_delete:%d %d\n", n, dup_mode);
 
     DB_ENV * const null_env = 0;
     DB *db;
     DB_TXN * const null_txn = 0;
-    const char * const fname = DIR "/" "test.rand.insert.brt";
+    const char * const fname = DIR "/" "test.db.delete.brt";
     int r;
 
     system("rm -rf " DIR);
@@ -84,12 +85,50 @@ void test_db_delete(int n, int dup_mode) {
         db_put(db, htonl(i), i);
 
     for (i=0; i<n; i++) {
-        expect_db_del(db, htonl(i), 0);
+        expect_db_del(db, htonl(i), 0, 0);
 
         expect_db_get(db, htonl(i), DB_NOTFOUND);
     }
 
-    expect_db_del(db, htonl(n), DB_NOTFOUND);
+    expect_db_del(db, htonl(n), 0, DB_NOTFOUND);
+#if USE_TDB
+    expect_db_del(db, htonl(n), DB_DELETE_ANY, 0);
+#endif
+#if USE_BDB && defined(DB_DELETE_ANY)
+    expect_db_del(db, htonl(n), DB_DELETE_ANY, EINVAL);
+#endif
+
+    r = db->close(db, 0);
+    assert(r == 0);
+}
+
+void test_db_get_datasize0() {
+    if (verbose) printf("test_db_get_datasize0\n");
+
+    DB_ENV * const null_env = 0;
+    DB *db;
+    DB_TXN * const null_txn = 0;
+    const char * const fname = DIR "/" "test.db_delete.brt";
+    int r;
+
+    system("rm -rf " DIR);
+    r=mkdir(DIR, 0777); assert(r==0);
+
+    /* create the dup database file */
+    r = db_create(&db, null_env, 0);
+    assert(r == 0);
+    r = db->set_pagesize(db, 4096);
+    assert(r == 0);
+    r = db->open(db, null_txn, fname, "main", DB_BTREE, DB_CREATE, 0666);
+    assert(r == 0);
+
+    int k = 0;
+    db_put(db, k, 0);
+
+    DBT key, val; 
+    r = db->get(db, 0, dbt_init(&key, &k, sizeof k), dbt_init_malloc(&val), 0);
+    assert(r == 0);
+    free(val.data);
 
     r = db->close(db, 0);
     assert(r == 0);
@@ -97,6 +136,8 @@ void test_db_delete(int n, int dup_mode) {
 
 int main(int argc, const char *argv[]) {
     parse_args(argc, argv);
+
+    test_db_get_datasize0();
 
     test_db_delete(0, 0);
 
