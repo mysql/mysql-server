ccb6b5ff4ee07625a381c6cb263b63adba226fd6
Rich Prohaska <prohaska@tokutek.com>
2007-12-06 12:08:25 +0000

    addresses #125
    git-svn-id: file:///svn/tokudb@958 c7de825b-a66e-492c-adef-691d508d4ae1

---
+++ src/tests/test_insert_memleak.c
@@ -0,0 +1,68 @@
+/* -*- mode: C; c-basic-offset: 4 -*- */
+#ident "Copyright (c) 2007 Tokutek Inc.  All rights reserved."
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <db.h>
+
+#include "test.h"
+
+DBT *dbt_init(DBT *dbt, void *data, u_int32_t size) {
+    memset(dbt, 0, sizeof *dbt);
+    dbt->data = data;
+    dbt->size = size;
+    return dbt;
+}
+
+DBT *dbt_init_malloc(DBT *dbt) {
+    memset(dbt, 0, sizeof *dbt);
+    dbt->flags = DB_DBT_MALLOC;
+    return dbt;
+}
+
+void test_insert(int n, int dup_mode) {
+    if (verbose) printf("test_insert:%d %d\n", n, dup_mode);
+
+    DB_ENV * const null_env = 0;
+    DB *db;
+    DB_TXN * const null_txn = 0;
+    const char * const fname = DIR "/" "test.insert.brt";
+    int r;
+
+    /* create the dup database file */
+    r = db_create(&db, null_env, 0);
+    assert(r == 0);
+    r = db->set_flags(db, dup_mode);
+    assert(r == 0);
+    r = db->set_pagesize(db, 4096);
+    assert(r == 0);
+    r = db->open(db, null_txn, fname, "main", DB_BTREE, DB_CREATE, 0666);
+    assert(r == 0);
+
+    int i;
+    for (i=0; i<n; i++) {
+        int k = htonl(i);
+        DBT key, val;
+        r = db->put(db, null_txn, dbt_init(&key, &k, sizeof k), dbt_init(&val, &i, sizeof i), 0);
+        assert(r == 0);
+    } 
+
+    r = db->close(db, 0);
+    assert(r == 0);
+}
+
+int main(int argc, const char *argv[]) {
+    parse_args(argc, argv);
+
+    system("rm -rf " DIR);
+    int r=mkdir(DIR, 0777); assert(r==0);
+
+    test_insert(256, 0);
+
+    return 0;
+}
