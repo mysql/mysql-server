37a2c4590a7453d5c8e5b67c2f512162323ac738
unknown <kostja@bodhi.(none)>
2007-12-13 23:58:55 +0300

    Fix broken embedded build (broken by the patch for Bug#12713, first part).
    client/mysqltest.c:
      Remove a call to my_message() (server call) in case of a client error.
      It asserts in the embedded build now, since is called after send_ok().
    libmysqld/emb_qcache.cc:
      Use net_send_eof (embedded implementation) in embedded query cache/
      send result to client, since send_eof currently does not
      send anything.
    libmysqld/emb_qcache.h:
      Declare net_send_eof.
    libmysqld/lib_sql.cc:
      Fix the main loop of the embedded server to use the new Diagnostics_area
      API.
    sql/log.cc:
      Silence errors of open_performance_schema_table. This function
      is called for general logging, and it happens after the error
      has been sent to the client, and thus triggers an assert.
    storage/myisam/ha_myisam.cc:
      Remove an old hack that broke repair.test in embedded build:
      unless we clear an error here, the server attempts to send OK after
      ERROR. This currently asserts.

--- libmysqld/emb_qcache.cc
+++ libmysqld/emb_qcache.cc
@@ -447,7 +447,7 @@ int emb_load_querycache_result(THD *thd, Querycache_stream *src)
   *prev_row= NULL;
   data->embedded_info->prev_ptr= prev_row;
 return_ok:
-  send_eof(thd);
+  net_send_eof(thd, thd->server_status, thd->total_warn_count);
   DBUG_RETURN(0);
 err:
   DBUG_RETURN(1);
