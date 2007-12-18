ccd04f13d4e88a64c437e5db5a5e2e024b0a8f53
Bradley C. Kuszmaul <bradley@tokutek.com>
2007-12-18 01:52:52 +0000

    More fiddling around with c++. Addresses #197.
    git-svn-id: file:///svn/tokudb@1179 c7de825b-a66e-492c-adef-691d508d4ae1

--- cxx/db.cpp
+++ cxx/db.cpp
@@ -2,20 +2,21 @@
 
 Db::~Db() {
     if (!the_db) {
-	the_db->close(db, 0);
-	the_db->toku_internal = 0;
-	// Do we need to clean up "private environments"?
+	close(0); // the user should have called close, but we do it here if not done.
     }
 }
 
-int DB::close (u_int32_t flags) {
+int Db::close (u_int32_t flags) {
     if (!the_db) {
 	return EINVAL;
     }
+    the_db->toku_internal = 0;
+
     int ret = the_db->close(flags);
 
-    the_db->toku_internal = 0;
+    the_db = 0;
     // Do we need to clean up "private environments"?
+    // What about cursors?  They should be cleaned up already.
 
     return ret;
 }
