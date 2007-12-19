86757a94dd185e9b02564e21fe8aaa41ca07f93b
Bradley C. Kuszmaul <bradley@tokutek.com>
2007-12-19 21:11:42 +0000

    Add set_errfile to samples.  Addresses #72.
    git-svn-id: file:///svn/tokudb@1256 c7de825b-a66e-492c-adef-691d508d4ae1

--- buildheader/sample_offsets_32_4_1.h
+++ buildheader/sample_offsets_32_4_1.h
@@ -58,6 +58,7 @@ struct fieldinfo db_fields32[] = {
   {"int (*remove) (DB *, const char *, const char *, u_int32_t)", 316, 4},
   {"int (*rename) (DB *, const char *, const char *, const char *, u_int32_t)", 320, 4},
   {"int (*set_dup_compare) (DB *, int (*)(DB *, const DBT *, const DBT *))", 344, 4},
+  {"void (*set_errfile) (DB_ENV *, FILE*)", 356, 4},
   {"int (*set_flags) (DB *, u_int32_t)", 368, 4},
   {"int (*set_pagesize) (DB *, u_int32_t)", 376, 4},
   {"int (*stat) (DB *, void *, u_int32_t)", 384, 4},
