24c2e857ab585c88754397bf02dd1e9d252f924c
Yoni Fogel <yoni@tokutek.com>
2007-12-20 18:34:33 +0000

    Added Dbt.get_ulen and Dbt.set_ulen Modified test1 to test new functions and some of the older ones. Addresses #197
    git-svn-id: file:///svn/tokudb@1284 c7de825b-a66e-492c-adef-691d508d4ae1

--- cxx/tests/test1.cpp
+++ cxx/tests/test1.cpp
@@ -5,8 +5,20 @@
 using namespace std;
 int main()
 {
+    u_int32_t size  = 3;
+    u_int32_t flags = 5;
+    u_int32_t ulen  = 7;
+    void*     data  = &size;
     Dbt dbt;
-    dbt.set_size(3);
+
+    dbt.set_size(size);
+    dbt.set_flags(flags);
+    dbt.set_data(data);
+    dbt.set_ulen(ulen);
+    assert(dbt.get_size()  == size);
+    assert(dbt.get_flags() == flags);
+    assert(dbt.get_data()  == data);
+    assert(dbt.get_ulen()  == ulen);
     cout << "Hello World!" << endl;   cout << "Welcome to C++ Programming" << endl;
     return 0;
 }
