c742a2df733de8e3b050a60eeb0f289fdf8f574e
Bradley C. Kuszmaul <bradley@tokutek.com>
2007-12-21 11:07:29 +0000

    Fix up memory issues in exceptions. Addresses #215.
    git-svn-id: file:///svn/tokudb@1311 c7de825b-a66e-492c-adef-691d508d4ae1

--- cxx/exception.cpp
+++ cxx/exception.cpp
@@ -1,6 +1,14 @@
 #include <db.h>
 #include <db_cxx.h>
 
+static char*cpp_strdup (const char *s)
+{
+    int l=strlen(s)+1;
+    char *r = new char[l];
+    strncpy(r, s, l);
+    return r;
+}
+
 DbException::~DbException() throw()
 {
     if (the_what!=0) {
@@ -18,7 +26,7 @@ DbException::DbException(int err)
 void DbException::FillTheWhat(void)
 {
     if (the_err!=0) {
-	the_what = strdup(db_strerror(the_err));
+	the_what = cpp_strdup(db_strerror(the_err));
     }
 }
 
@@ -40,6 +48,27 @@ DbEnv *DbException::get_env() const
 void DbException::set_env(DbEnv *new_env)
 {
     the_env = new_env;
+}
+
+// Must define a copy constructor so that the delete[] of the same the_what doesn't happen
+DbException::DbException (const DbException &that)
+    :  std::exception(),
+       the_what(cpp_strdup(that.the_what)),
+       the_err(that.the_err),
+       the_env(that.the_env)
+{
+}
+    
+
+DbException &DbException::operator = (const DbException &that)
+{
+	if (this != &that) {
+	    delete [] the_what;
+	    the_what = cpp_strdup(that.the_what);
+	    the_err  = that.the_err;
+	    the_env  = that.the_env;
+	}
+	return (*this);
 }
 
 DbDeadlockException::DbDeadlockException (DbEnv *env)
