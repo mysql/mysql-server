a1dbfac439df54bb6ae4be5e4dac66b406e459c9
Bradley C. Kuszmaul <bradley@tokutek.com>
2007-12-21 15:16:03 +0000

    Add {{DB*dbp}} to {{{DBC}}}.  Addresses #215.
    git-svn-id: file:///svn/tokudb@1314 c7de825b-a66e-492c-adef-691d508d4ae1

--- buildheader/sample_offsets_32_4_6.h
+++ buildheader/sample_offsets_32_4_6.h
@@ -87,6 +87,7 @@ struct fieldinfo db_txn_stat_fields32[] = {
   {0, 68, 68} /* size of whole struct */
 };
 struct fieldinfo dbc_fields32[] = {
+  {"DB *dbp", 0, 4},
   {"int (*c_close) (DBC *)", 244, 4},
   {"int (*c_del) (DBC *, u_int32_t)", 252, 4},
   {"int (*c_get) (DBC *, DBT *, DBT *, u_int32_t)", 260, 4},
