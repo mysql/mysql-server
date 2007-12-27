967748e3d52fba88b29da9f2f419238664e2bb2a
Rich Prohaska <prohaska@tokutek.com>
2007-12-27 21:37:04 +0000

    implement Dbc::del addresses #227
    git-svn-id: file:///svn/tokudb@1394 c7de825b-a66e-492c-adef-691d508d4ae1

--- cxx/dbc.cpp
+++ cxx/dbc.cpp
@@ -15,6 +15,14 @@ int Dbc::get(Dbt* key, Dbt *data, u_int32_t flags) {
     return env->maybe_throw_error(ret);
 }
 
+int Dbc::del(u_int32_t flags) {
+    DBC *dbc = this;
+    int ret = dbc->c_del(dbc, flags);
+    DB_ENV *dbenv_c=dbc->dbp->dbenv;
+    DbEnv *env = (DbEnv*)dbenv_c->api1_internal;
+    return env->maybe_throw_error(ret);
+}
+
 // Not callable, but some compilers require it to be defined anyway.
 Dbc::~Dbc()
 {
