fa71a681b6286d42b20a2fd575926cf5c89c6913
Bradley C. Kuszmaul <bradley@tokutek.com>
2008-01-07 16:29:34 +0000

    Fix the type in set_errcall.  Addresses #246.
    git-svn-id: file:///svn/tokudb@1532 c7de825b-a66e-492c-adef-691d508d4ae1

--- buildheader/sample_offsets_64_4_5.h
+++ buildheader/sample_offsets_64_4_5.h
@@ -19,7 +19,7 @@ struct fieldinfo db_env_fields64[] = {
   {"int  (*open) (DB_ENV *, const char *, u_int32_t, int)", 1200, 8},
   {"int  (*set_cachesize) (DB_ENV *, u_int32_t, u_int32_t, int)", 1424, 8},
   {"int  (*set_data_dir) (DB_ENV *, const char *)", 1432, 8},
-  {"void (*set_errcall) (DB_ENV *, void (*)(DB_ENV *, const char *, const char *))", 1448, 8},
+  {"void (*set_errcall) (DB_ENV *, void (*)(const DB_ENV *, const char *, const char *))", 1448, 8},
   {"void (*set_errfile) (DB_ENV *, FILE*)", 1456, 8},
   {"void (*set_errpfx) (DB_ENV *, const char *)", 1464, 8},
   {"int  (*set_flags) (DB_ENV *, u_int32_t, int)", 1488, 8},
