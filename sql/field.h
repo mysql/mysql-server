5030dd096b4df2477826ee2260064e64de580ac4
gkodinov/kgeorge@macbook.gmz <>
2008-01-08 12:27:15 +0200

    merge of bug 33256 5.0-opt -> 5.1-opt

--- sql/field.h
+++ sql/field.h
@@ -1270,9 +1270,9 @@ public:
 	       unireg_check_arg, field_name_arg, cs)
     {}
   Field_newdate(bool maybe_null_arg, const char *field_name_arg,
-                struct st_table *table_arg, CHARSET_INFO *cs)
-    :Field_str((char*) 0,10, maybe_null_arg ? (uchar*) "": 0,0,
-               NONE, field_name_arg, table_arg, cs) {}
+                CHARSET_INFO *cs)
+    :Field_str((uchar*) 0,10, maybe_null_arg ? (uchar*) "": 0,0,
+               NONE, field_name_arg, cs) {}
   enum_field_types type() const { return MYSQL_TYPE_DATE;}
   enum_field_types real_type() const { return MYSQL_TYPE_NEWDATE; }
   enum ha_base_keytype key_type() const { return HA_KEYTYPE_UINT24; }
