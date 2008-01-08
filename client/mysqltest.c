d610b72159a6cc2f5ae22b7ca27799723f44e680
msvensson@pilot.mysql.com <>
2008-01-08 21:13:25 +0100

    Check for windows patch to convert also in --replace

--- client/mysqltest.c
+++ client/mysqltest.c
@@ -7406,6 +7406,7 @@ void do_get_replace(struct st_command *command)
     if (!*from)
       die("Wrong number of arguments to replace_result in '%s'",
           command->query);
+    IF_WIN(fix_win_paths(to, from - to), 0);
     insert_pointer_name(&from_array,to);
     to= get_string(&buff, &from, command);
     insert_pointer_name(&to_array,to);
@@ -8596,9 +8597,7 @@ void free_pointer_array(POINTER_ARRAY *pa)
 void replace_dynstr_append_mem(DYNAMIC_STRING *ds,
                                const char *val, int len)
 {
-#ifdef __WIN__
-  fix_win_paths(val, len);
-#endif
+  IF_WIN(fix_win_paths(val, len), 0);
 
   if (glob_replace_regex)
   {
