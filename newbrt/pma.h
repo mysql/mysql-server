89985a3175a18b392affd7d4fc687ef61dccfe13
Bradley C. Kuszmaul <bradley@tokutek.com>
2008-01-16 13:50:23 +0000

    Improve logging. Addresses #27.
    git-svn-id: file:///svn/tokudb@1690 c7de825b-a66e-492c-adef-691d508d4ae1

--- newbrt/pma.h
+++ newbrt/pma.h
@@ -155,9 +155,9 @@ int toku_pma_clear_at_index (PMA, unsigned int /*index*/); // If the index is wr
 
 
 // Requires:  No open cursors on the pma.
-int toku_pma_move_indices (PMA pma, INTPAIRARRAY fromto); // Return nonzero if the indices are somehow wrong.
+int toku_pma_move_indices (PMA pma_from, PMA pma_to, INTPAIRARRAY fromto); // Return nonzero if the indices are somehow wrong.
 // Move things backwards according to fromto.
-int toku_pma_move_indices_back (PMA pma, INTPAIRARRAY fromto);
+int toku_pma_move_indices_back (PMA pma_backto, PMA pma_backfrom, INTPAIRARRAY fromto);
 
 void toku_pma_show_stats (void);
 
