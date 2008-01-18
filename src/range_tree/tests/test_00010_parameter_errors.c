0b7e94de8727f383023794bd8f0098e8be5111f2
Yoni Fogel <yoni@tokutek.com>
2008-01-18 22:10:19 +0000

    Addresses #288 First set of test cases for range trees. linear.c is stub implementation so we can compile and write tests before we actually implement the range tree.
    The makefiles were modified just enough to get this to work, and have to
    be cleaned up a lot.
    
    git-svn-id: file:///svn/tokudb@1740 c7de825b-a66e-492c-adef-691d508d4ae1

---
+++ src/range_tree/tests/test_00010_parameter_errors.c
@@ -0,0 +1,133 @@
+/* We are going to test whether create and close properly check their input. */
+
+#include "test.h"
+
+int main(int argc, const char *argv[]) {
+    int r;
+    toku_range_tree *tree = NULL;
+    toku_range range;
+
+    parse_args(argc, argv);
+
+    /* Create tests */
+    r = toku_rt_create(NULL, dummy_cmp, dummy_cmp, FALSE);
+    CKERR2(r, EINVAL);
+
+    r = toku_rt_create(&tree, NULL, dummy_cmp, FALSE);
+    CKERR2(r, EINVAL);
+    
+    assert(tree == NULL);
+
+    r = toku_rt_create(&tree, dummy_cmp, NULL, FALSE);
+    CKERR2(r, EINVAL);
+
+    assert(tree == NULL);
+
+    /* Close tests */
+    r = toku_rt_close(NULL);
+    CKERR2(r, EINVAL);
+
+    /* Insert tests */
+    r = toku_rt_insert(NULL, &range);
+    CKERR2(r, EINVAL);
+    
+    r = toku_rt_create(&tree, dummy_cmp, dummy_cmp, FALSE); CKERR(r);
+    assert(tree != NULL);
+
+    r = toku_rt_insert(tree, NULL);                         CKERR2(r, EINVAL);
+    r = toku_rt_close(tree);                                CKERR(r);
+    
+    tree = NULL;
+
+    /* Delete tests */
+    r = toku_rt_delete(NULL, &range);
+    CKERR2(r, EINVAL);
+    
+    r = toku_rt_create(&tree, dummy_cmp, dummy_cmp, FALSE); CKERR(r);
+    assert(tree != NULL);
+
+    r = toku_rt_delete(tree, NULL);                         CKERR2(r, EINVAL);
+    r = toku_rt_close(tree);                                CKERR(r);
+
+    /* Find tests */
+    toku_range* buf = (toku_range*)toku_malloc(2*sizeof(toku_range));
+    unsigned bufsize = 2;
+    unsigned found;
+    
+    r = toku_rt_create(&tree, dummy_cmp, dummy_cmp, FALSE); CKERR(r);
+    assert(tree != NULL);
+
+    r = toku_rt_find(NULL, &range, 2, &buf, &bufsize, &found);
+    CKERR2(r, EINVAL);
+    
+    r = toku_rt_find(tree, NULL, 2, &buf, &bufsize, &found);
+    CKERR2(r, EINVAL);
+    
+    r = toku_rt_find(tree, &range, 2, NULL, &bufsize, &found);
+    CKERR2(r, EINVAL);
+    
+    r = toku_rt_find(tree, &range, 2, &buf, NULL, &found);
+    CKERR2(r, EINVAL);
+    
+    r = toku_rt_find(tree, &range, 2, &buf, &bufsize, NULL);
+    CKERR2(r, EINVAL);
+    
+    r = toku_rt_close(tree);                                CKERR(r);
+
+    /* Predecessor tests */
+    int foo;
+    BOOL wasfound;
+    r = toku_rt_create(&tree, dummy_cmp, dummy_cmp, FALSE); CKERR(r);
+    assert(tree != NULL);
+
+    r = toku_rt_predecessor(NULL, &foo, &range, &wasfound);
+    CKERR2(r, EINVAL);
+
+    r = toku_rt_predecessor(tree, NULL, &range, &wasfound);
+    CKERR2(r, EINVAL);
+
+    r = toku_rt_predecessor(tree, &foo, NULL, &wasfound);
+    CKERR2(r, EINVAL);
+
+    r = toku_rt_predecessor(tree, &foo, &range, NULL);
+    CKERR2(r, EINVAL);
+
+    r = toku_rt_close(tree);                                CKERR(r);
+
+    r = toku_rt_create(&tree, dummy_cmp, dummy_cmp, TRUE); CKERR(r);
+    assert(tree != NULL);
+
+    r = toku_rt_predecessor(tree, &foo, &range, &wasfound);
+    CKERR2(r, EINVAL);
+
+    r = toku_rt_close(tree);                                CKERR(r);
+
+
+    /* Successor tests */
+    r = toku_rt_create(&tree, dummy_cmp, dummy_cmp, FALSE); CKERR(r);
+    assert(tree != NULL);
+
+    r = toku_rt_successor(NULL, &foo, &range, &wasfound);
+    CKERR2(r, EINVAL);
+
+    r = toku_rt_successor(tree, NULL, &range, &wasfound);
+    CKERR2(r, EINVAL);
+
+    r = toku_rt_successor(tree, &foo, NULL, &wasfound);
+    CKERR2(r, EINVAL);
+
+    r = toku_rt_successor(tree, &foo, &range, NULL);
+    CKERR2(r, EINVAL);
+
+    r = toku_rt_close(tree);                                CKERR(r);
+
+    r = toku_rt_create(&tree, dummy_cmp, dummy_cmp, TRUE); CKERR(r);
+    assert(tree != NULL);
+
+    r = toku_rt_successor(tree, &foo, &range, &wasfound);
+    CKERR2(r, EINVAL);
+
+    r = toku_rt_close(tree);                                CKERR(r);
+
+    return 0;
+}
