c42703fd90ead730dc85756e7aa01382a8b550d8
Vincenzo Liberatore <vincenzo@tokutek.com>
2008-02-05 22:40:05 +0000

    Addresses #337 Modify test cases to support lock bound
    git-svn-id: file:///svn/tokudb@2106 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/lock_tree/tests/test_00000_createclose.c
+++ src/lock_tree/tests/test_00000_createclose.c
@@ -4,12 +4,13 @@ int main() {
     int r;
     toku_lock_tree* lt = NULL;
     DB* db = (DB*)1;
-    size_t mem = 4096 * 1000;
+    u_int32_t mem = 4096 * 1000;
+    u_int32_t memcnt;
     BOOL duplicates;
 
     for (duplicates = 0; duplicates < 2; duplicates++) {
-        r = toku_lt_create(&lt, db, duplicates, dbpanic, mem, dbcmp, dbcmp,
-                           toku_malloc, toku_free, toku_realloc);
+        r = toku_lt_create(&lt, db, duplicates, dbpanic, mem, &memcnt,
+                           dbcmp, dbcmp, toku_malloc, toku_free, toku_realloc);
         CKERR(r);
         assert(lt);
         r = toku_lt_close(lt);
