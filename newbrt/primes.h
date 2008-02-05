3277dbc808c1e1e2fadabf93525ef140a0c38a36
Yoni Fogel <yoni@tokutek.com>
2008-02-05 22:30:01 +0000

    Addresses #293 Addresses #307 A compiler directive to make compilation sweeter
    git-svn-id: file:///svn/tokudb@2105 c7de825b-a66e-492c-adef-691d508d4ae1

--- newbrt/primes.h
+++ newbrt/primes.h
@@ -2,5 +2,5 @@
 
 /* Return the smallest prime >= 2^(idx+1)
  * Only works for idx<30 */
-int toku_get_prime (unsigned int idx);
-void toku_test_primes(void);
+int toku_get_prime (unsigned int idx) __attribute__((const));
+void toku_test_primes(void) __attribute__((const));
