ff9e497d43408bcbde4f3d20def41470ddaf7504
Rich Prohaska <prohaska@tokutek.com>
2008-02-06 13:37:52 +0000

    fix some global namespace pollution
    git-svn-id: file:///svn/tokudb@2118 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tokuglobals.py
+++ src/tokuglobals.py
@@ -21,7 +21,7 @@ def checkglobals(libname, exceptsymbols, verbose):
             type = match.group(2)
             symbol = match.group(3)
             if verbose: print type, symbol
-            match = re.match("^toku_", symbol)
+            match = re.match("^toku_|^__toku", symbol)
             if match == None and not exceptsymbols.has_key(symbol):
                 print "non toku symbol=", symbol
                 badglobals = 1
