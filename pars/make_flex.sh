8de3b674f6481788b6fe687c9dc8b69984cf3987
marko <>
2008-02-07 10:03:54 +0000

    branches/zip: make_flex.sh, lexyy.c: Remove some GCC warnings about unused static symbol definitions.
    yy_scan_buffer, yy_scan_string, yy_scan_bytes: Leave these unused extern
    function declarations alone.
    
    yypush_buffer_state, yypop_buffer_state, yyswitch_to_buffer, yyget*, yyset*,
    yylex_destroy: Add __attribute__((unused)).
    
    This closes Mantis issue #8.

--- pars/make_flex.sh
+++ pars/make_flex.sh
@@ -14,18 +14,20 @@ flex -o $TMPFILE pars0lex.l
 echo '#include "univ.i"' > $OUTFILE
 
 # flex assigns a pointer to an int in one place without a cast, resulting in
-# a warning on Win64.  Add the cast.  Also define some functions as static.
+# a warning on Win64.  Add the cast.  Also define some symbols as static.
 sed -e '
 s/'"$TMPFILE"'/'"$OUTFILE"'/;
 s/\(int offset = \)\((yy_c_buf_p) - (yytext_ptr)\);/\1(int)(\2);/;
-s/\(void \(yyrestart\|yy_\(switch_to\|delete\|flush\)_buffer\)\)/static \1/;
-s/\(void yypush_buffer_state\)/static \1/;
-s/\(void yypop_buffer_state\)/static \1/;
-s/\(YY_BUFFER_STATE yy_\(create_buffer\|scan_\(buffer\|string\|bytes\)\)\)/static \1/;
-s/\(\(int\|void\) yy[gs]et_\)/static \1/;
+s/\(void yy\(restart\|_\(delete\|flush\)_buffer\)\)/static \1/;
+s/\(void yy_switch_to_buffer\)/__attribute__((unused)) static \1/;
+s/\(void yy\(push\|pop\)_buffer_state\)/__attribute__((unused)) static \1/;
+s/\(YY_BUFFER_STATE yy_create_buffer\)/static \1/;
+s/\(\(int\|void\) yy[gs]et_\)/__attribute__((unused)) static \1/;
 s/\(void \*\?yy\(\(re\)\?alloc\|free\)\)/static \1/;
-s/\(extern \)\?\(int yy\(leng\|lex_destroy\|lineno\|_flex_debug\)\)/static \2/;
+s/\(extern \)\?\(int yy\(leng\|lineno\|_flex_debug\)\)/static \2/;
+s/\(int yylex_destroy\)/__attribute__((unused)) static \1/;
 s/\(extern \)\?\(int yylex \)/UNIV_INTERN \2/;
+s/^\(\(FILE\|char\) *\* *yyget\)/__attribute__((unused)) static \1/;
 s/^\(extern \)\?\(\(FILE\|char\) *\* *yy\)/static \2/;
 ' < $TMPFILE >> $OUTFILE
 
