b713f7714bc8659cd28372c70aa0e67a91f515a7
Rich Prohaska <prohaska@tokutek.com>
2008-02-22 01:57:00 +0000

    get it working on bdb. address #431
    git-svn-id: file:///svn/tokudb@2373 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/test_txn_abort5.c
+++ src/tests/test_txn_abort5.c
@@ -18,17 +18,22 @@ void test_txn_abort(int n) {
     r = db_env_create(&env, 0); assert(r == 0);
     r = env->set_data_dir(env, DIR);
     r = env->set_lg_dir(env, DIR);
-    r = env->open(env, ".", DB_INIT_MPOOL + DB_INIT_LOG + DB_INIT_LOCK + DB_INIT_TXN + DB_PRIVATE, 0777); assert(r == 0);
+    r = env->open(env, 0, DB_INIT_MPOOL + DB_INIT_LOG + DB_INIT_LOCK + DB_INIT_TXN + DB_PRIVATE + DB_CREATE, 0777); 
+    if (r != 0) printf("%s:%d:%d:%s\n", __FILE__, __LINE__, r, db_strerror(r));
+    assert(r == 0);
+
+    DB_TXN *txn;
+    r = env->txn_begin(env, 0, &txn, 0); assert(r == 0);
 
     DB *db;
     r = db_create(&db, env, 0); assert(r == 0);
-    r = db->open(db, 0, "test.db", 0, DB_BTREE, DB_CREATE, 0777); assert(r == 0);
-    DB_TXN *txn;
-    r = env->txn_begin(env, 0, &txn, 0); assert(r == 0);
+    r = db->open(db, txn, "test.db", 0, DB_BTREE, DB_CREATE, 0777); assert(r == 0);
     int i;
     for (i=0; i<n; i++) {
         DBT key, val;
-        r = db->put(db, txn, dbt_init(&key, &i, sizeof i), dbt_init(&val, &i, sizeof i), 0); assert(r == 0);
+        r = db->put(db, txn, dbt_init(&key, &i, sizeof i), dbt_init(&val, &i, sizeof i), 0); 
+        if (r != 0) printf("%s:%d:%d:%s\n", __FILE__, __LINE__, r, db_strerror(r));
+        assert(r == 0);
     }
     r = txn->abort(txn); 
 #if 0
