f20fe94b55727fbc69b1cb86af5dc96f216bdd87
unknown <kaa@kaamos.(none)>
2008-03-13 12:14:14 +0300

    Bug#35103 mysql_client_test::test_bug29948 causes sporadic failures
    Disable test case for bug 29948, which is causing sporadically
    failures in other tests inside mysql_client_test.
    
    
    tests/mysql_client_test.c:
      Disable test case.

--- tests/mysql_client_test.c
+++ tests/mysql_client_test.c
@@ -15952,6 +15952,7 @@ static void test_bug27592()
   DBUG_VOID_RETURN;
 }
 
+#if 0
 
 static void test_bug29948()
 {
@@ -16026,6 +16027,8 @@ static void test_bug29948()
   mysql_query(dbc, "DROP TABLE t1");
   mysql_close(dbc);
 }
+
+#endif
 
 /**
   Bug#29306 Truncated data in MS Access with decimal (3,1) columns in a VIEW
@@ -16553,7 +16556,7 @@ static struct my_tests_st my_tests[]= {
   { "test_bug28505", test_bug28505 },
   { "test_bug28934", test_bug28934 },
   { "test_bug27592", test_bug27592 },
-  { "test_bug29948", test_bug29948 },
+  /* { "test_bug29948", test_bug29948 }, Bug#35103 */
   { "test_bug29306", test_bug29306 },
   { "test_bug31669", test_bug31669 },
   { "test_bug32265", test_bug32265 },
