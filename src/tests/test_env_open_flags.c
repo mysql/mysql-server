f6fcc7dc9d871fed542a214b6ed66230c5d47cf6
Bradley C. Kuszmaul <bradley@tokutek.com>
2008-03-13 21:40:57 +0000

    Disable a function call that causes BDB 4.6.21 to segfault.  Fixes #530.
    git-svn-id: file:///svn/tokudb@2798 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/test_env_open_flags.c
+++ src/tests/test_env_open_flags.c
@@ -22,6 +22,7 @@ void test_env_open_flags(int env_open_flags, int expectr) {
 
     r = db_env_create(&env, 0);
     assert(r == 0);
+    env->set_errfile(env, 0);
 
     r = env->open(env, ENVDIR, env_open_flags, 0644);
     if (r != expectr && verbose) printf("env open flags=%x expectr=%d r=%d\n", env_open_flags, expectr, r);
@@ -39,7 +40,10 @@ int main(int argc, const char *argv[]) {
 
     /* test flags */
     test_env_open_flags(0, ENOENT);
+#ifdef TOKUDB
+    // This one segfaults in BDB 4.6.21
     test_env_open_flags(DB_PRIVATE, ENOENT);
+#endif
     test_env_open_flags(DB_PRIVATE+DB_CREATE, 0);
     test_env_open_flags(DB_PRIVATE+DB_CREATE+DB_INIT_MPOOL, 0);
     test_env_open_flags(DB_PRIVATE+DB_RECOVER, EINVAL);
