afcd389e529e620fd45cb42117a0347716de3922
Bradley C. Kuszmaul <bradley@tokutek.com>
2008-03-17 04:12:06 +0000

    test_thread_insert doesn't leak memory any more.  The leak is in {{{pthread_exit()}}}.  If you return from the initial function, the leak isn't there.  Fixes #303.
    git-svn-id: file:///svn/tokudb@2884 c7de825b-a66e-492c-adef-691d508d4ae1

---
+++ src/tests/test_forkjoin.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <assert.h>
+#include <pthread.h>
+
+void *f(void *arg) {
+    //pthread_exit(arg);  // pthread_exit has a memoryh leak.
+    return arg;
+}
+
+int main() {
+    pthread_t t;
+    int r = pthread_create(&t, 0, f, 0); assert(r == 0);
+    void *ret;
+    r = pthread_join(t, &ret); assert(r == 0);
+    return 0;
+}
