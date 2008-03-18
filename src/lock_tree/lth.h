b83b0db80397a4acc3a7c8a824b68b8526066669
Yoni Fogel <yoni@tokutek.com>
2008-03-18 22:15:01 +0000

    addresses #553 create iterator for lth
    git-svn-id: file:///svn/tokudb@2930 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/lock_tree/lth.h
+++ src/lock_tree/lth.h
@@ -3,6 +3,9 @@
 
 #ident "The technology is licensed by the Massachusetts Institute of Technology, Rutgers State University of New Jersey, and the Research Foundation of State University of New York at Stony Brook under United States of America Serial No. 11/760379 and to the patents and/or patent applications resulting from it."
 
+
+#if !defined(TOKU_LTH_H)
+#define TOKU_LTH_H
 /**
    \file  hash_table.h
    \brief Hash table
@@ -14,21 +17,27 @@
 #include <brttypes.h>
 #include <locktree.h>
 
+typedef struct __toku_lth_value toku_lth_value;
+struct __toku_lth_value {
+    toku_lock_tree*          hash_key;
+};
+
 typedef struct __toku_lth_elt toku_lth_elt;
 struct __toku_lth_elt {
-    toku_lock_tree* key;
-    toku_lth_elt*   next;
+    toku_lth_value  value;
+    toku_lth_elt*   next_in_bucket;
+    toku_lth_elt*   next_in_iteration;
+    toku_lth_elt*   prev_in_iteration;
 };
 
 typedef struct __toku_lth toku_lth;
 struct __toku_lth {
-    toku_lth_elt**  table;
+    toku_lth_elt*   buckets;
+    uint32          num_buckets;
     uint32          num_keys;
-    uint32          array_size;
-    uint32          finger_index;
-    toku_lth_elt*   finger_ptr;
-    BOOL            finger_start;
-    BOOL            finger_end;
+    toku_lth_elt    iter_head;
+    toku_lth_elt*   iter_curr;
+    BOOL            iter_is_valid;
     /** The user malloc function */
     void*         (*malloc) (size_t);
     /** The user free function */
@@ -42,14 +51,16 @@ int  toku_lth_create(toku_lth** ptable,
                      void  (*user_free)   (void*),
                      void* (*user_realloc)(void*, size_t));
 
-toku_lock_tree* toku_lth_find      (toku_lth* table, toku_lock_tree* key);
+toku_lock_tree* toku_lth_find       (toku_lth* table, toku_lock_tree* key);
 
-void            toku_lth_start_scan(toku_lth* table);
+void            toku_lth_start_scan (toku_lth* table);
 
-toku_lock_tree* toku_lth_next      (toku_lth* table);
+toku_lock_tree* toku_lth_next       (toku_lth* table);
 
-void            toku_lth_delete    (toku_lth* table, toku_lock_tree* key);
+void            toku_lth_delete     (toku_lth* table, toku_lock_tree* key);
 
-void            toku_lth_close     (toku_lth* table);
+void            toku_lth_close      (toku_lth* table);
 
-int             toku_lth_insert    (toku_lth* table, toku_lock_tree* key);
+int             toku_lth_insert     (toku_lth* table, toku_lock_tree* key);
+#endif
+
