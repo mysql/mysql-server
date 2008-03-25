a1de91e7082816f05bcafc2b321e5f82eb66054b
unknown <istruewing@stella.local>
2008-03-25 21:30:45 +0100

    Bug#35559 - plugin_load.test creates warnings
    plugin_load.test created lines in the warnings files and made
    pushbuild cells yellow. Some pushbuild systems are configured
    so that they don't build the ha_example plugin. Trying to load
    them creates error messages in the error log. These were copied
    to the warnings file.
    
    Fixed by ignoring expected error messages for non-existent
    ha_example.
    
    
    mysql-test/lib/mtr_report.pl:
      Bug#35559 - plugin_load.test creates warnings
      Ignore expected error messages for non-existent ha_example.

--- mysql-test/lib/mtr_report.pl
+++ mysql-test/lib/mtr_report.pl
@@ -279,6 +279,8 @@ sub mtr_report_stats ($) {
 		/Failed to open log/ or
 		/Failed to open the existing master info file/ or
 		/Forcing shutdown of [0-9]* plugins/ or
+                /Can't open shared library .*\bha_example\b/ or
+                /Couldn't load plugin .*\bha_example\b/ or
 		/Got error [0-9]* when reading table/ or
 		/Incorrect definition of table/ or
 		/Incorrect information in file/ or
