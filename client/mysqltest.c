c1918b0464dcc4bc82cc1207b3a9189d3e7e2139
msvensson@pilot.mysql.com <>
2008-05-02 11:22:10 +0200

    Bug#36465 mysqltest should always use the --tmpdir

--- client/mysqltest.c
+++ client/mysqltest.c
@@ -1536,7 +1536,7 @@ int dyn_string_cmp(DYNAMIC_STRING* ds, const char *fname)
   DBUG_ENTER("dyn_string_cmp");
   DBUG_PRINT("enter", ("fname: %s", fname));
 
-  if ((fd= create_temp_file(temp_file_path, NULL,
+  if ((fd= create_temp_file(temp_file_path, TMPDIR,
                             "tmp", O_CREAT | O_SHARE | O_RDWR,
                             MYF(MY_WME))) < 0)
     die("Failed to create temporary file for ds");
