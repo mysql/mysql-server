65a310fe2a594aca6e73308896763933adb59e3e
unknown <gkodinov/kgeorge@magare.gmz>
2008-05-06 11:57:19 +0300

    revert the push of bug 35616.

--- mysys/my_static.c
+++ mysys/my_static.c
@@ -74,8 +74,8 @@ uint sf_malloc_prehunc=0,		/* If you have problem with core- */
      sf_malloc_endhunc=0,		/* dump when malloc-message.... */
 					/* set theese to 64 or 128  */
      sf_malloc_quick=0;			/* set if no calls to sanity */
-size_t sf_malloc_cur_memory= 0L;		/* Current memory usage */
-size_t sf_malloc_max_memory= 0L;		/* Maximum memory usage */
+ulong sf_malloc_cur_memory= 0L;		/* Current memory usage */
+ulong sf_malloc_max_memory= 0L;		/* Maximum memory usage */
 uint  sf_malloc_count= 0;		/* Number of times NEW() was called */
 byte *sf_min_adress= (byte*) ~(unsigned long) 0L,
      *sf_max_adress= (byte*) 0L;
