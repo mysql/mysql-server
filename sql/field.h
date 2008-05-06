1c45081b5285a9ae2c5401cffd1fcfe0a312f4e7
gshchepa/uchum@host.loc <>
2008-05-06 21:43:46 +0500

    Partial rollback of fix for bug #30059: End-space truncation is inconsistent or incorrect.
    For better conformance with standard, truncation procedure of CHAR columns
    has been changed to ignore truncation of trailing whitespace characters
    (note has been removed).
    
    Finally, for columns with non-binary charsets:
    
    1. CHAR(N) columns silently ignore trailing whitespace truncation;
    2. VARCHAR and TEXT columns issue Note about truncation.
    
    BLOBs and other columns with BINARY charset are unaffected.

--- sql/field.h
+++ sql/field.h
@@ -455,7 +455,8 @@ public:
 class Field_longstr :public Field_str
 {
 protected:
-  int report_if_important_data(const char *ptr, const char *end);
+  int report_if_important_data(const char *ptr, const char *end,
+                               bool count_spaces);
 public:
   Field_longstr(char *ptr_arg, uint32 len_arg, uchar *null_ptr_arg,
                 uchar null_bit_arg, utype unireg_check_arg,
