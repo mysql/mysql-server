ff98e5e1dfd8bd66821adebd4ed170a715994bc5
Bradley C. Kuszmaul <bradley@tokutek.com>
2008-05-08 21:43:08 +0000

    Addresses #797.
    git-svn-id: file:///svn/tokudb@3873 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/test_nested.c
+++ src/tests/test_nested.c
@@ -124,7 +124,7 @@ void test_nested (void) {
     r=txn->commit(txn, 0); CKERR(r);
 
     r=env->txn_begin(env, 0, &txn, 0);    CKERR(r);
-    insert(6, 0);
+    insert(6, txn);
     r=txn->commit(txn, 0); CKERR(r);
 
     r=env->txn_begin(env, 0, &txn, 0);    CKERR(r);
