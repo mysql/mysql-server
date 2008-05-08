db1d33188e02db4f34281aa096a789d6de3d0ec4
Bradley C. Kuszmaul <bradley@tokutek.com>
2008-05-08 18:26:28 +0000

    Add some more cursor delete tests, but they didn't find any problems.  Addresses #788, #789.
    git-svn-id: file:///svn/tokudb@3864 c7de825b-a66e-492c-adef-691d508d4ae1

---
+++ src/tests/test_cursor_delete2.c
@@ -0,0 +1,72 @@
+/* -*- mode: C; c-basic-offset: 4 -*- */
+#ident "Copyright (c) 2007,2008 Tokutek Inc.  All rights reserved."
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include <db.h>
+
+#include "test.h"
+
+static DB_ENV *dbenv;
+static DB *db;
+static DB_TXN * txn;
+
+void test_cursor_delete2 () {
+    int r;
+    DBT key,val;
+
+    r = db_env_create(&dbenv, 0);                                                            CKERR(r);
+    r = dbenv->open(dbenv, ENVDIR, DB_PRIVATE|DB_INIT_MPOOL|DB_CREATE|DB_INIT_TXN, 0);       CKERR(r);
+
+    r = db_create(&db, dbenv, 0);                                                            CKERR(r);
+    r = dbenv->txn_begin(dbenv, 0, &txn, 0);                                                 CKERR(r);
+    r = db->open(db, txn, "primary.db", NULL, DB_BTREE, DB_CREATE, 0600);                    CKERR(r);
+    r = txn->commit(txn, 0);                                                                 CKERR(r);
+
+    r = dbenv->txn_begin(dbenv, 0, &txn, 0);                                                 CKERR(r);
+    r = db->put(db, txn, dbt_init(&key, "a", 2), dbt_init(&val, "b", 2), DB_YESOVERWRITE);   CKERR(r);
+    r = txn->commit(txn, 0);                                                                 CKERR(r);
+
+    r = dbenv->txn_begin(dbenv, 0, &txn, 0);                                                 CKERR(r);
+    r = db->del(db, txn, dbt_init(&key, "a", 2), 0);                                         CKERR(r);
+    r = txn->commit(txn, 0);                                                                 CKERR(r);
+
+    r = dbenv->txn_begin(dbenv, 0, &txn, 0);                                                 CKERR(r);
+    r = db->del(db, txn, dbt_init(&key, "a", 2), DB_DELETE_ANY);                             CKERR(r);
+    r = txn->commit(txn, 0);                                                                 CKERR(r);
+
+    r = dbenv->txn_begin(dbenv, 0, &txn, 0);                                                 CKERR(r);
+    r = db->put(db, txn, dbt_init(&key, "a", 2), dbt_init(&val, "c", 2), DB_YESOVERWRITE);   CKERR(r);
+    r = db->del(db, txn, dbt_init(&key, "a", 2), 0);                                         CKERR(r);
+    r = txn->commit(txn, 0);                                                                 CKERR(r);
+
+    r = dbenv->txn_begin(dbenv, 0, &txn, 0);                                                 CKERR(r);
+    r = db->put(db, txn, dbt_init(&key, "a", 2), dbt_init(&val, "c", 2), DB_YESOVERWRITE);   CKERR(r);
+    r = txn->commit(txn, 0);                                                                 CKERR(r);
+
+    r = dbenv->txn_begin(dbenv, 0, &txn, 0);                                                 CKERR(r);
+    r = db->del(db, txn, dbt_init(&key, "a", 2), 0);                                         CKERR(r);
+    r = db->del(db, txn, dbt_init(&key, "a", 2), DB_DELETE_ANY);                             CKERR(r);
+    r = txn->commit(txn, 0);                                                                 CKERR(r);
+
+    r = db->close(db, 0);                                                                    CKERR(r);
+    r = dbenv->close(dbenv, 0);                                                              CKERR(r);
+}
+
+int main(int argc, const char *argv[]) {
+
+    parse_args(argc, argv);
+  
+    system("rm -rf " ENVDIR);
+    mkdir(ENVDIR, 0777);
+    
+    test_cursor_delete2();
+
+    return 0;
+}
