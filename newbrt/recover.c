3d0f87f4d867e4f12212e50dcdc5e1a0c912d6b9
Rich Prohaska <prohaska@tokutek.com>
2008-05-12 18:39:21 +0000

    compile with gcc-4.3 on debian4. addresses #804
    git-svn-id: file:///svn/tokudb@3922 c7de825b-a66e-492c-adef-691d508d4ae1

--- newbrt/recover.c
+++ newbrt/recover.c
@@ -605,7 +605,7 @@ void toku_recover_deleteleafentry (LSN lsn, FILENUM filenum, DISKOFF diskoff, u_
     VERIFY_COUNTS(node);
     node->log_lsn = lsn;
     {
-	OMTVALUE data;
+	OMTVALUE data = 0;
 	r=toku_omt_fetch(node->u.l.buffer, idx, &data);
 	assert(r==0);
 	LEAFENTRY oldleafentry=data;
