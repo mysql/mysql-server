913ceb43d74635dd24fa54f2caf12f849de9b366
Bradley C. Kuszmaul <bradley@tokutek.com>
2008-05-17 01:06:03 +0000

    Create continuable assertion failures.  See #835 for how to use it.  Fixes #835.
    git-svn-id: file:///svn/tokudb@3989 c7de825b-a66e-492c-adef-691d508d4ae1

--- newbrt/toku_assert.c
+++ newbrt/toku_assert.c
@@ -2,9 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int toku_continue_on_assert_failure=0;
+
+void toku_assert_failed_but_continue_anyway (void) __attribute__((noinline));
+
+void toku_assert_failed_but_continue_anyway (void) {
+    printf("Assertion failed, but continuing anyway\n");
+}
+
 void toku_do_assert(int expr,const char* expr_as_string,const char *function,const char*file,int line) {
     if (expr==0) {
 	fprintf(stderr, "%s:%d %s: Assertion `%s' failed\n", file,line,function,expr_as_string);
-	abort();
+	if (!toku_continue_on_assert_failure)
+	    abort();
+	else
+	    toku_assert_failed_but_continue_anyway();
     }
 }
