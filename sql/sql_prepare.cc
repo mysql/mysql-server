77e54c03ac401e7477b2a4ab7831c44f51aebe27
Mattias Jonsson <mattiasj@mysql.com>
2008-06-09 14:39:20 +0200

    Bug#31210: INSERT DELAYED crashes server when used on partitioned tables
    post push patch
    
    push build found tree failing test when using:
    --ps-protocol --mysqld=--binlog-format=row
    Changed to the new error message.

--- sql/sql_prepare.cc
+++ sql/sql_prepare.cc
@@ -1154,9 +1154,9 @@ static bool mysql_test_insert(Prepared_statement *stmt,
     if (table_list->lock_type == TL_WRITE_DELAYED &&
         !(table_list->table->file->ha_table_flags() & HA_CAN_INSERT_DELAYED))
     {
-      my_error(ER_ILLEGAL_HA, MYF(0), (table_list->view ?
-                                       table_list->view_name.str :
-                                       table_list->table_name));
+      my_error(ER_DELAYED_NOT_SUPPORTED, MYF(0), (table_list->view ?
+                                                  table_list->view_name.str :
+                                                  table_list->table_name));
       goto error;
     }
     while ((values= its++))
