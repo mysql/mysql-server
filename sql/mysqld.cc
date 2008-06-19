e7f3c5fc44c168f0a574719722aa3763d22f7668
Davi Arnaut <davi@mysql.com>
2008-06-19 11:02:32 -0300

    Bug#37003 Tests sporadically crashes with embedded server
    Another problem is that the backtrace facility wasn't being
    enabled for non-Linux targets even if the target OS has the
    backtrace functions. Also, the stacktrace functions inside
    mysqltest were being used without proper checks for their
    presence in the build.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -2538,7 +2538,9 @@ static void init_signals(void)
     sigemptyset(&sa.sa_mask);
     sigprocmask(SIG_SETMASK,&sa.sa_mask,NULL);
 
+#ifdef HAVE_STACKTRACE
     my_init_stacktrace();
+#endif
 #if defined(__amiga__)
     sa.sa_handler=(void(*)())handle_segfault;
 #else
