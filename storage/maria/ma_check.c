fdebe2788f96d12640220e959d2ccd006a6579cf
Guilhem Bichot <guilhem@mysql.com>
2008-06-30 11:23:10 +0200

    Fix for BUG#35107 "maria-preload.test is disabled because causes assertion". maria-preload will be re-enabled later because other pieces of it have possibly random output.
    storage/maria/ma_check.c:
      Fix for BUG#35107 "maria-preload.test is disabled because causes assertion".

--- storage/maria/ma_check.c
+++ storage/maria/ma_check.c
@@ -2234,6 +2234,10 @@ static int initialize_variables_for_repair(HA_CHECK *param,
 {
   MARIA_SHARE *share= info->s;
 
+  /* Repair code relies on share->state.state so we have to update it here */
+  if (share->lock.update_status)
+    (*share->lock.update_status)(info);
+
   bzero((char*) sort_info,  sizeof(*sort_info));
   bzero((char*) sort_param, sizeof(*sort_param));
 
