8ff32200fdb9e8d950c7de624f6e05e7c4a1f561
Rich Prohaska <prohaska@tokutek.com>
2008-08-11 13:02:17 +0000

    addresses #1079
    git-svn-id: file:///svn/tokudb@5604 c7de825b-a66e-492c-adef-691d508d4ae1

--- buildheader/sample_offsets_32_4_1.h
+++ buildheader/sample_offsets_32_4_1.h
@@ -76,6 +76,7 @@ struct fieldinfo db_txn_active_fields32[] = {
 };
 struct fieldinfo db_txn_fields32[] = {
   {"DB_ENV *mgrp /*In TokuDB, mgrp is a DB_ENV not a DB_TXNMGR*/", 0, 4},
+  {"DB_TXN *parent", 4, 4},
   {"void *api_internal", 68, 4},
   {"int (*abort) (DB_TXN *)", 76, 4},
   {"int (*commit) (DB_TXN*, u_int32_t)", 80, 4},
