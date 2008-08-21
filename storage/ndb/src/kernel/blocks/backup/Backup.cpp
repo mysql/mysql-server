88f91318c8767a1020383dd4fbce01c6db4895ba
stewart@flamingspork.com <>
2008-08-21 15:06:30 +1000

    [patch 03/64] WL4012 WL1744 (ndb on win32): use my_global insetad of my_configthis includes the right headers for win32

--- storage/ndb/src/kernel/blocks/backup/Backup.cpp
+++ storage/ndb/src/kernel/blocks/backup/Backup.cpp
@@ -13,7 +13,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
-#include <my_config.h>
+#include <my_global.h>
 #include "Backup.hpp"
 
 #include <ndb_version.h>
