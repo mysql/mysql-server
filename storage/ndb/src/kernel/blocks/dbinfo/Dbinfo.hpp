2106f4499ee323c45b9d50403e6ca8db3de0fde1
stewart@flamingspork.com <>
2008-10-05 18:12:42 +1100

    [patch 03/11] Add DBINFO block for kernel side NDB$INFOSkeleton block implementation. Nothing special yet.

--- storage/ndb/src/kernel/blocks/dbinfo/Dbinfo.hpp
+++ storage/ndb/src/kernel/blocks/dbinfo/Dbinfo.hpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2003 MySQL AB
+/* Copyright (C) 2007 MySQL AB
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -13,30 +13,27 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
+#ifndef DBINFO_H
+#define DBINFO_H
 
-#include <BlockNumbers.h>
+#include <SimulatedBlock.hpp>
 
-const BlockName BlockNames[] = {
-  { "CMVMI", CMVMI },
-  { "DBACC", DBACC },
-  { "DBDICT", DBDICT },
-  { "DBDIH", DBDIH },
-  { "DBLQH", DBLQH },
-  { "DBTC", DBTC },
-  { "DBTUP", DBTUP },
-  { "NDBFS", NDBFS },
-  { "NDBCNTR", NDBCNTR },
-  { "QMGR", QMGR },
-  { "TRIX", TRIX },
-  { "BACKUP", BACKUP },
-  { "DBUTIL", DBUTIL },
-  { "SUMA", SUMA },
-  { "DBTUX", DBTUX }
-  ,{ "TSMAN", TSMAN}
-  ,{ "LGMAN", LGMAN }
-  ,{ "PGMAN", PGMAN }
-  ,{ "RESTORE", RESTORE }
-  ,{ "DBINFO", DBINFO }
+#include <NodeBitmask.hpp>
+
+class Dbinfo : public SimulatedBlock
+{
+public:
+  Dbinfo(Block_context& ctx);
+  virtual ~Dbinfo();
+  BLOCK_DEFINES(Dbinfo);
+
+protected:
+
+  void execSTTOR(Signal* signal);
+  void sendSTTORRY(Signal*);
+  void execREAD_CONFIG_REQ(Signal*);
+  void execDUMP_STATE_ORD(Signal* signal);
+
 };
 
-const BlockNumber NO_OF_BLOCK_NAMES = sizeof(BlockNames) / sizeof(BlockName);
+#endif
