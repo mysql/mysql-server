11d68f94de656ccb14f6ee6eb5b87bab3cdd3b11
Stewart Smith <stewart@flamingspork.com>
2008-10-06 21:06:32 +1100

    string to char* conversion warning (change to const char*)

--- storage/ndb/src/kernel/blocks/dbutil/DbUtil.cpp
+++ storage/ndb/src/kernel/blocks/dbutil/DbUtil.cpp
@@ -695,7 +695,7 @@ void DbUtil::execDBINFO_SCANREQ(Signal *signal)
   if(req.tableId == NDBINFO_POOLS_TABLEID)
   {
     struct {
-      char* poolname;
+      const char* poolname;
       Uint32 free;
       Uint32 size;
     } pools[] =
@@ -728,7 +728,7 @@ void DbUtil::execDBINFO_SCANREQ(Signal *signal)
     {
       dbinfo_write_row_init(&r, buf, sizeof(buf));
       dbinfo_write_row_column_uint32(&r, getOwnNodeId());
-      char *blockname= "DBUTIL";
+      const char *blockname= "DBUTIL";
       dbinfo_write_row_column(&r, blockname, strlen(blockname));
       dbinfo_write_row_column(&r, pools[i].poolname, strlen(pools[i].poolname));
       dbinfo_write_row_column_uint32(&r, pools[i].free);
