4ba34f41313d0a67e3f28030c01e6e85e26bef88
Stewart Smith <stewart@flamingspork.com>
2008-10-06 19:04:13 +1100

    fix "deprecated conversion from string constant to char*" warnings in ndbinfo

--- storage/ndb/src/kernel/blocks/dbtup/DbtupDebug.cpp
+++ storage/ndb/src/kernel/blocks/dbtup/DbtupDebug.cpp
@@ -116,7 +116,7 @@ void Dbtup::execDBINFO_SCANREQ(Signal* signal)
   else if(req.tableId == NDBINFO_POOLS_TABLEID)
   {
     struct {
-      char* poolname;
+      const char* poolname;
       Uint32 free;
       Uint32 size;
     } pools[] =
@@ -149,7 +149,7 @@ void Dbtup::execDBINFO_SCANREQ(Signal* signal)
     {
       dbinfo_write_row_init(&r, buf, sizeof(buf));
       dbinfo_write_row_column_uint32(&r, getOwnNodeId());
-      char *blockname= "DBTUP";
+      const char *blockname= "DBTUP";
       dbinfo_write_row_column(&r, blockname, strlen(blockname));
       dbinfo_write_row_column(&r, pools[i].poolname, strlen(pools[i].poolname));
       dbinfo_write_row_column_uint32(&r, pools[i].free);
