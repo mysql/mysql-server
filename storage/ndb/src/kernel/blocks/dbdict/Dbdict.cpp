4f07f10db642f8bd0f4c1e2e27d26d479dbf2a53
Stewart Smith <stewart@flamingspork.com>
2008-10-08 17:18:28 +1100

    uninit local variable: dbinfo_ratelimit_init() call needed

--- storage/ndb/src/kernel/blocks/dbdict/Dbdict.cpp
+++ storage/ndb/src/kernel/blocks/dbdict/Dbdict.cpp
@@ -270,6 +270,8 @@ void Dbdict::execDBINFO_SCANREQ(Signal *signal)
   struct dbinfo_row r;
   struct dbinfo_ratelimit rl;
 
+  dbinfo_ratelimit_init(&rl, &req);
+
   jamEntry();
 
   if(req.tableId == NDBINFO_POOLS_TABLEID)
