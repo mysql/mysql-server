6d54b0443a55142263754e0f7a6a36a5bafc2a21
Georgi Kodinov <kgeorge@mysql.com>
2008-10-08 14:23:53 +0300

    Bug #32124: crash if prepared statements refer to variables in the where clause
    The code to get read the value of a system variable was extracting its value 
    on PREPARE stage and was substituting the value (as a constant) into the parse tree.
    Note that this must be a reversible transformation, i.e. it must be reversed before
    each re-execution.
    Unfortunately this cannot be reliably done using the current code, because there are
    other non-reversible source tree transformations that can interfere with this
    reversible transformation.
    Fixed by not resolving the value at PREPARE, but at EXECUTE (as the rest of the 
    functions operate). Added a cache of the value (so that it's constant throughout
    the execution of the query). Note that the cache also caches NULL values.
    Updated an obsolete related test suite (variables-big) and the code to test the 
    result type of system variables (as per bug 74).
    
    mysql-test/extra/rpl_tests/rpl_insert_id.test:
      Bug #32124: removed ambiguous testcase
    mysql-test/r/innodb_data_home_dir_basic.result:
      Bug #32124: fixed wrong test case
    mysql-test/r/innodb_flush_method_basic.result:
      Bug #32124: fixed wrong test case
    mysql-test/r/ps_11bugs.result:
      Bug #32124: test case
    mysql-test/r/ssl_capath_basic.result:
      Bug #32124: fixed wrong test case
    mysql-test/r/ssl_cipher_basic.result:
      Bug #32124: fixed wrong test case
    mysql-test/r/variables.result:
      Bug #32124: system vars are shown as such in EXPLAIN EXTENDED, not as constants.
    mysql-test/suite/rpl/r/rpl_insert_id.result:
      Bug #32124: removed ambiguous testcase
    mysql-test/t/ps_11bugs.test:
      Bug #32124: test case
    sql/item.cc:
      Bug #32124: placed the code to convert string to longlong or double 
      to a function (so that it can be reused)
    sql/item.h:
      Bug #32124: placed the code to convert string to longlong or double 
      to a function (so that it can be reused)
    sql/item_func.cc:
      Bug #32124: moved the evaluation of system variables at runtime (val_xxx).
    sql/item_func.h:
      Bug #32124: moved the evaluation of system variables at runtime (val_xxx).
    sql/set_var.cc:
      Bug #32124: removed the code that calculated the system variable's value 
      at PREPARE
    sql/set_var.h:
      Bug #32124: removed the code that calculated the system variable's value 
      at PREPARE
    tests/mysql_client_test.c:
      Bug #32124 : removed the reading of the system variable, because its max
      length is depended on the system charset and client charset and can't be
      easily calculated.

--- sql/item.cc
+++ sql/item.cc
@@ -2380,17 +2380,15 @@ void Item_string::print(String *str, enum_query_type query_type)
 }
 
 
-double Item_string::val_real()
+double 
+double_from_string_with_check (CHARSET_INFO *cs, const char *cptr, char *end)
 {
-  DBUG_ASSERT(fixed == 1);
   int error;
-  char *end, *org_end;
+  char *org_end;
   double tmp;
-  CHARSET_INFO *cs= str_value.charset();
 
-  org_end= (char*) str_value.ptr() + str_value.length();
-  tmp= my_strntod(cs, (char*) str_value.ptr(), str_value.length(), &end,
-                  &error);
+  org_end= end;
+  tmp= my_strntod(cs, (char*) cptr, end - cptr, &end, &error);
   if (error || (end != org_end && !check_if_only_end_space(cs, end, org_end)))
   {
     /*
@@ -2400,7 +2398,39 @@ double Item_string::val_real()
     push_warning_printf(current_thd, MYSQL_ERROR::WARN_LEVEL_WARN,
                         ER_TRUNCATED_WRONG_VALUE,
                         ER(ER_TRUNCATED_WRONG_VALUE), "DOUBLE",
-                        str_value.ptr());
+                        cptr);
+  }
+  return tmp;
+}
+
+
+double Item_string::val_real()
+{
+  DBUG_ASSERT(fixed == 1);
+  return double_from_string_with_check (str_value.charset(), str_value.ptr(), 
+                                        (char *) str_value.ptr() + str_value.length());
+}
+
+
+longlong 
+longlong_from_string_with_check (CHARSET_INFO *cs, const char *cptr, char *end)
+{
+  int err;
+  longlong tmp;
+  char *org_end= end;
+
+  tmp= (*(cs->cset->strtoll10))(cs, cptr, &end, &err);
+  /*
+    TODO: Give error if we wanted a signed integer and we got an unsigned
+    one
+  */
+  if (err > 0 ||
+      (end != org_end && !check_if_only_end_space(cs, end, org_end)))
+  {
+    push_warning_printf(current_thd, MYSQL_ERROR::WARN_LEVEL_WARN,
+                        ER_TRUNCATED_WRONG_VALUE,
+                        ER(ER_TRUNCATED_WRONG_VALUE), "INTEGER",
+                        cptr);
   }
   return tmp;
 }
@@ -2413,26 +2443,8 @@ double Item_string::val_real()
 longlong Item_string::val_int()
 {
   DBUG_ASSERT(fixed == 1);
-  int err;
-  longlong tmp;
-  char *end= (char*) str_value.ptr()+ str_value.length();
-  char *org_end= end;
-  CHARSET_INFO *cs= str_value.charset();
-
-  tmp= (*(cs->cset->strtoll10))(cs, str_value.ptr(), &end, &err);
-  /*
-    TODO: Give error if we wanted a signed integer and we got an unsigned
-    one
-  */
-  if (err > 0 ||
-      (end != org_end && !check_if_only_end_space(cs, end, org_end)))
-  {
-    push_warning_printf(current_thd, MYSQL_ERROR::WARN_LEVEL_WARN,
-                        ER_TRUNCATED_WRONG_VALUE,
-                        ER(ER_TRUNCATED_WRONG_VALUE), "INTEGER",
-                        str_value.ptr());
-  }
-  return tmp;
+  return longlong_from_string_with_check(str_value.charset(), str_value.ptr(),
+                             (char *) str_value.ptr()+ str_value.length());
 }
 
 
