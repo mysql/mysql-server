350c8108b2783d3b00fc8d3eb630f1e13680b91e
Stewart Smith <stewart@flamingspork.com>
2008-10-09 19:21:38 +1100

    welcome to the world of include paths.

--- sql/ha_ndbcluster_lock_ext.h
+++ sql/ha_ndbcluster_lock_ext.h
@@ -14,7 +14,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#include <NdbTick.h>
+#include <portlib/NdbTick.h>
 
 /*
   These functions are shared with ndb_restore so that the creating of
