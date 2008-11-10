e85fe79430d74f4ca4f972bf13594a5c1fbcddb2
Build Team <bteam@astra04>
2008-11-10 21:21:49 +0100

    Added "Sun Microsystems, Inc." to copyright headers on files modified since Oct 1st

--- sql/ha_partition.h
+++ sql/ha_partition.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2005 MySQL AB
+/* Copyright 2005-2008 MySQL AB, 2008 Sun Microsystems, Inc.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
