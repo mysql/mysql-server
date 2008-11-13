07f218f3a457250b25fb5236609ec161035206b3
Rich Prohaska <prohaska@tokutek.com>
2008-11-13 14:54:45 +0000

    imp toku_os_ closes #1253
    git-svn-id: file:///svn/toku/tokudb.1032b@7838 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/test_db_version.c
+++ src/tests/test_db_version.c
@@ -1,19 +1,28 @@
 /* -*- mode: C; c-basic-offset: 4 -*- */
 #ident "Copyright (c) 2007 Tokutek Inc.  All rights reserved."
 
+#include "portability.h"
 #include <db.h>
 #include <assert.h>
 
 #include "test.h"
 
-int main (int UU(argc), char UU(*argv[])) {
+int main (int argc, char *argv[]) {
     const char *v;
     int major, minor, patch;
+    int i;
+    for (i=1; i<argc; i++) {
+        char *arg = argv[i];
+        if (strcmp(arg, "-v") == 0)
+            verbose++;
+    }
     v = db_version(0, 0, 0);
     assert(v!=0);
     v = db_version(&major, &minor, &patch);
     assert(major==DB_VERSION_MAJOR);
     assert(minor==DB_VERSION_MINOR);
     assert(patch==DB_VERSION_PATCH);
+    if (verbose)
+        printf("%d.%d.%d %s\n", major, minor, patch, v);
     return 0;
 }
