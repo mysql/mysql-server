eb2b1eab895a7a68a15ae6ad8c28b4697e76b970
marko <>
2008-11-28 14:58:51 +0000

    branches/innodb+: Merge revisions 3180:3312 from branches/zip:
      ------------------------------------------------------------------------
      r3254 | marko | 2008-11-24 18:01:42 +0200 (Mon, 24 Nov 2008) | 4 lines
    
      branches/zip: Note that it is legitimate for a secondary index record not
      to be found during purge.  This tries to address Issue #129.  The comments
      were supplied by Heikki.
      ------------------------------------------------------------------------
      r3286 | marko | 2008-11-26 10:00:28 +0200 (Wed, 26 Nov 2008) | 18 lines
    
      branches/zip: row_merge_drop_temp_indexes(): Replace the WHILE 1 with
      WHILE 1=1 in the SQL procedure, so that the loop will actually be
      entered and temporary indexes be dropped during crash recovery.
      Thanks to Sunny Bains for pointing this out.
    
      Tested as follows:
    
      Set a breakpoint in row_merge_rename_indexes.
    
      CREATE TABLE t(a INT)ENGINE=InnoDB;
      CREATE INDEX a ON t(a);
    
      -- The breakpoint will be reached.  Kill and restart mysqld.
      SHOW CREATE TABLE t;
      -- This shows the MySQL .frm file, without and index.
      CREATE TABLE innodb_table_monitor(a INT)ENGINE=InnoDB;
      -- This will dump the InnoDB dictionary to the error log, without the index.
      ------------------------------------------------------------------------
      r3302 | vasil | 2008-11-27 23:26:39 +0200 (Thu, 27 Nov 2008) | 12 lines
    
      branches/zip:
    
      Fix Mantis issue#130 wdl: does not handle 64-bit address
    
      - Change the call from strtoul() to strtoull()
      - Change "%16X" to "%16llx" when scanning preferred load address
    
      rb://58
    
      Submitted by:   Calvin
      Approved by:    Marko
      ------------------------------------------------------------------------
      r3303 | vasil | 2008-11-27 23:31:18 +0200 (Thu, 27 Nov 2008) | 10 lines
    
      branches/zip:
    
      * Remove a change from win-plugin/win-plugin.diff about time_t because
        MySQL has used VS2005 for building 5.1.30.
    
      * Adjust the line numbers so the patch applies cleanly without fuzz and
        offset messages.
    
      Submitted by:   Calvin
      ------------------------------------------------------------------------
      r3304 | vasil | 2008-11-27 23:33:48 +0200 (Thu, 27 Nov 2008) | 6 lines
    
      branches/zip:
    
      Non-functional change in win-plugin/win-plugin.diff: fix the file name
      before the diff, this is irrelevant but it is nice to be the same as
      the file name on the following line.
      ------------------------------------------------------------------------
      r3312 | marko | 2008-11-28 16:18:43 +0200 (Fri, 28 Nov 2008) | 5 lines
    
      branches/zip: row_undo_mod_del_mark_or_remove_sec_low(): Complain if
      the secondary index entry cannot be found, and this is not an incomplete
      transaction that is being rolled back in crash recovery.  The source code
      comments were suggested by Heikki.
      ------------------------------------------------------------------------

--- row/row0umod.c
+++ row/row0umod.c
@@ -314,8 +314,35 @@ row_undo_mod_del_mark_or_remove_sec_low(
 	search_result = row_search_index_entry(index, entry, mode,
 					       &pcur, &mtr);
 
-	switch (search_result) {
+	switch (UNIV_EXPECT(search_result, ROW_FOUND)) {
+		trx_t*	trx;
 	case ROW_NOT_FOUND:
+		/* In crash recovery, the secondary index record may
+		be missing if the UPDATE did not have time to insert
+		the secondary index records before the crash.  When we
+		are undoing that UPDATE in crash recovery, the record
+		may be missing.  In normal processing, the record
+		SHOULD exist. */
+
+		trx = thr_get_trx(thr);
+
+		if (!trx_is_recv(trx)) {
+			fputs("InnoDB: error in sec index entry del undo in\n"
+			      "InnoDB: ", stderr);
+			dict_index_name_print(stderr, trx, index);
+			fputs("\n"
+			      "InnoDB: tuple ", stderr);
+			dtuple_print(stderr, entry);
+			fputs("\n"
+			      "InnoDB: record ", stderr);
+			rec_print(stderr, btr_pcur_get_rec(&pcur), index);
+			putc('\n', stderr);
+			trx_print(stderr, trx, 0);
+			fputs("\n"
+			      "InnoDB: Submit a detailed bug report"
+			      " to http://bugs.mysql.com\n", stderr);
+		}
+
 		err = DB_SUCCESS;
 		goto func_exit;
 	case ROW_FOUND:
