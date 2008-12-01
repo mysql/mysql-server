1968ad63ef36cd8b557d8d5056176472d8382229
Timothy Smith <timothy.smith@sun.com>
2008-12-01 12:46:57 -0700

    Minor clean up in build system; make BUILD/compile-pentium-debug work with current auto tools (create BUILD/autorun.sh based on 5.0 version).  Remove some generated files from version control.  Ignore some generated files.

---
+++ BUILD/autorun.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Create MySQL autotools infrastructure
+
+die() { echo "$@"; exit 1; }
+
+aclocal || die "Can't execute aclocal"
+autoheader || die "Can't execute autoheader"
+# --force means overwrite ltmain.sh script if it already exists
+# Added glibtoolize reference to make native OSX autotools work
+if test -f /usr/bin/glibtoolize  ; then
+  glibtoolize --automake --force || die "Can't execute glibtoolize"
+else
+  libtoolize --automake --force || die "Can't execute libtoolize"
+fi
+
+# --add-missing instructs automake to install missing auxiliary files
+# and --force to overwrite them if they already exist
+automake --add-missing --force || die "Can't execute automake"
+autoconf || die "Can't execute autoconf"
+(cd bdb/dist && sh s_all)
+(cd innobase && aclocal && autoheader && aclocal && automake && autoconf)
+if [ -d gemini ]
+then
+  (cd gemini && aclocal && autoheader && aclocal && automake && autoconf)
+fi
