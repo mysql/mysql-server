3f14288cbd4b5185a1adf640d4197a94eb81e30a
vasil <>
2008-12-04 16:49:24 +0000

    branches/zip:
    Revert our temporary fix for Bug#40360 Binlog related errors with binlog off
    
    This bug was fixed in MySQL code.
    
    Our fix went into r2944 and r2947, but this patch does not entirely revert
    those revisions because we want to leave the test case that was introduced
    and also r2944 itself reverted r2935 and r2936. So if we completely revert
    r2944 and r2947 then we would loose the test and will restore r2935 and
    r2936.
    
    This resolves Issue#108 We should remove ib_bin_log_is_engaged() once mysql
    add an equivallent, see Bug#40360

--- include/mysql_addons.h
+++ include/mysql_addons.h
@@ -14,34 +14,3 @@ here. In a perfect world this file exists but is empty.
 
 Created November 07, 2007 Vasil Dimov
 *******************************************************/
-
-#include <my_global.h>  /* for my_bool */
-#include <mysql/plugin.h>  /* for MYSQL_THD */
-
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
-/***********************************************************************
-Retrieve THD::thread_id
-http://bugs.mysql.com/30930 */
-
-unsigned long
-ib_thd_get_thread_id(
-/*=================*/
-				/* out: THD::thread_id */
-	const void*	thd);	/* in: THD */
-
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
-
-/* http://bugs.mysql.com/40360 */
-/* http://lists.mysql.com/commits/57450 */
-/**
-  See if the binary log is engaged for a thread, i.e., open and
-  LOG_BIN is set.
-
-  @return @c true if the binlog is active, @c false otherwise.
-*/
-my_bool ib_bin_log_is_engaged(const MYSQL_THD thd);
