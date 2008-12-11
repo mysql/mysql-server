ce8ad64dd2ac2f28557020f5de07c56cc21ecaac
Gleb Shchepa <gshchepa@mysql.com>
2008-12-12 00:57:32 +0400

    Bug #40761: Assert on sum function on             IF(..., CAST(longtext AS UNSIGNED), signed_val)             (was: LEFT JOIN on inline view crashes server)
    Select from a LONGTEXT column wrapped with an expression
    like "IF(..., CAST(longtext_column AS UNSIGNED), smth_signed)"
    failed an assertion or crashed the server. IFNULL function was
    affected too.
    
    LONGTEXT column item has a maximum length of 32^2-1 bytes,
    at the same time this is a maximum possible length of any
    MySQL item. CAST(longtext_column AS UNSIGNED) returns some
    unsigned numeric result of length 32^2-1, so the result of
    IF/IFNULL function of this number and some other signed number
    will have text length of (32^2-1)+1=32^2 (one byte for the
    minus sign) - there is integer overflow, and the length is
    equal to zero. That caused assert/crash.
    
    The bug has been fixed by the same solution as in the CASE
    function implementation.

--- sql/item_cmpfunc.h
+++ sql/item_cmpfunc.h
@@ -759,7 +759,6 @@ public:
   Item *find_item(String *str);
   CHARSET_INFO *compare_collation() { return cmp_collation.collation; }
   void agg_str_lengths(Item *arg);
-  void agg_num_lengths(Item *arg);
 };
 
 
