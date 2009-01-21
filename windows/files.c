938da8df5fadd4097df752222daa0803cb4c7828
Yoni Fogel <yoni@tokutek.com>
2009-01-21 21:22:02 +0000

    Fix windows build
    git-svn-id: file:///svn/toku/tokudb@9112 c7de825b-a66e-492c-adef-691d508d4ae1

--- windows/files.c
+++ windows/files.c
@@ -72,3 +72,27 @@ ftruncate(int fd, int64_t offset) {
     return r;
 }
 
+static ssize_t (*t_pwrite)(int, const void *, size_t, off_t) = 0;
+static ssize_t (*t_write)(int, const void *, size_t) = 0;
+
+int toku_set_func_pwrite (ssize_t (*pwrite_fun)(int, const void *, size_t, off_t)) {
+    t_pwrite = pwrite_fun;
+    return 0;
+}
+
+int toku_set_func_write (ssize_t (*write_fun)(int, const void *, size_t)) {
+    t_write = write_fun;
+    return 0;
+}
+
+
+ssize_t
+toku_os_pwrite (int fd, const void *buf, size_t len, off_t off)
+{
+    if (t_pwrite) {
+	return t_pwrite(fd, buf, len, off);
+    } else {
+	return pwrite(fd, buf, len, off);
+    }
+}
+
