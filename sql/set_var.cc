c891fbd05cf0866bcd7bd5a4999e873401c359cc
Tomas Ulin <tomas.ulin@sun.com>
2009-01-22 13:51:12 +0100

    add options --ndb-log-bin --ndb-log-binlog-index

--- sql/set_var.cc
+++ sql/set_var.cc
@@ -72,6 +72,7 @@ extern ulong ndb_extra_logging;
 #ifdef HAVE_NDB_BINLOG
 extern ulong ndb_report_thresh_binlog_epoch_slip;
 extern ulong ndb_report_thresh_binlog_mem_usage;
+extern my_bool ndb_log_binlog_index;
 #endif
 
 extern CHARSET_INFO *character_set_filesystem;
@@ -667,6 +668,8 @@ sys_ndb_report_thresh_binlog_epoch_slip(&vars, "ndb_report_thresh_binlog_epoch_s
 static sys_var_long_ptr
 sys_ndb_report_thresh_binlog_mem_usage(&vars, "ndb_report_thresh_binlog_mem_usage",
                                        &ndb_report_thresh_binlog_mem_usage);
+static sys_var_bool_ptr
+sys_ndb_log_binlog_index(&vars, "ndb_log_binlog_index", &ndb_log_binlog_index);
 #endif
 static sys_var_thd_bool
 sys_ndb_use_exact_count(&vars, "ndb_use_exact_count", &SV::ndb_use_exact_count);
