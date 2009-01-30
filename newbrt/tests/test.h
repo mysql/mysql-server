ecbaab2797779c6710ac2d6360b1f986dc67d0b0
Yoni Fogel <yoni@tokutek.com>
2009-01-30 20:21:42 +0000

    Addresses #1463 Modified benchmark-test to check for more errors
    git-svn-id: file:///svn/toku/tokudb@9350 c7de825b-a66e-492c-adef-691d508d4ae1

--- newbrt/tests/test.h
+++ newbrt/tests/test.h
@@ -5,6 +5,10 @@
 #include "toku_portability.h"
 #include "brt.h"
 
+#define CKERR(r) do { if (r!=0) fprintf(stderr, "%s:%d error %d %s\n", __FILE__, __LINE__, r, strerror(r)); assert(r==0); } while (0)
+#define CKERR2(r,r2) do { if (r!=r2) fprintf(stderr, "%s:%d error %d %s, expected %d\n", __FILE__, __LINE__, r, strerror(r), r2); assert(r==r2); } while (0)
+#define CKERR2s(r,r2,r3) do { if (r!=r2 && r!=r3) fprintf(stderr, "%s:%d error %d %s, expected %d or %d\n", __FILE__, __LINE__, r, strerror(r), r2,r3); assert(r==r2||r==r3); } while (0)
+
 int verbose=0;
 
 void
