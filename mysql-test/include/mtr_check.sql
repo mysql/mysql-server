aea9bcaa403267617607ee09fc41de890623d3ed
Georgi Kodinov <joro@sun.com>
2009-02-01 11:18:09 +0200

    Fixed internal test check warnings in 5.1-bugteam.

--- mysql-test/include/mtr_check.sql
+++ mysql-test/include/mtr_check.sql
@@ -12,7 +12,7 @@ BEGIN
   -- Dump all global variables except those
   -- that are supposed to change
   SELECT * FROM INFORMATION_SCHEMA.GLOBAL_VARIABLES
-    WHERE variable_name != 'timestamp';
+    WHERE variable_name != 'timestamp' ORDER BY VARIABLE_NAME;
 
   -- Dump all databases, there should be none
   -- except those that was created during bootstrap
