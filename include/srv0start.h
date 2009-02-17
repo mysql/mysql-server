f53e7dc83c60151a1f4e87f114bfd5c71071b919
vasil <Unknown>
2009-02-17 09:39:11 +0000

    branches/zip:
    * Remove old Innobase copyright lines from C source files
    * Add a reference to the GPLv2 license as recommended by the lawyers
    at Oracle Legal
    
    [Step 18/28]

--- include/srv0start.h
+++ include/srv0start.h
@@ -1,11 +1,26 @@
+/*****************************************************************************
+
+Copyright (c) 1995, 2009, Innobase Oy. All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place, Suite 330, Boston, MA 02111-1307 USA
+
+*****************************************************************************/
+
 /******************************************************
 Starts the Innobase database server
 
-(c) 1995-2000 Innobase Oy
-
 Created 10/10/1995 Heikki Tuuri
 *******************************************************/
-
 
 #ifndef srv0start_h
 #define srv0start_h
--- include/trx0i_s.h
+++ include/trx0i_s.h
@@ -1,9 +1,25 @@
+/*****************************************************************************
+
+Copyright (c) 2007, 2009, Innobase Oy. All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place, Suite 330, Boston, MA 02111-1307 USA
+
+*****************************************************************************/
+
 /******************************************************
 INFORMATION SCHEMA innodb_trx, innodb_locks and
 innodb_lock_waits tables cache structures and public
 functions.
-
-(c) 2007 Innobase Oy
 
 Created July 17, 2007 Vasil Dimov
 *******************************************************/
