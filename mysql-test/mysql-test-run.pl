6f06b01ee40e592a63da175020c3da449d7fbbf6
Tomas Ulin <tomas.ulin@sun.com>
2009-03-23 07:27:57 +0100

    smaller testsuite for tracing

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -126,7 +126,8 @@ my $path_config_file;           # The generated config file, var/my.cnf
 # executables will be used by the test suite.
 our $opt_vs_config = $ENV{'MTR_VS_CONFIG'};
 
-my $DEFAULT_SUITES= "ndb,ndb_binlog,rpl_ndb,main,binlog,federated,rpl";
+#my $DEFAULT_SUITES= "ndb,ndb_binlog,rpl_ndb,main,binlog,federated,rpl";
+my $DEFAULT_SUITES= "ndb";
 my $opt_suites;
 
 our $opt_verbose= 0;  # Verbose output, enable with --verbose
