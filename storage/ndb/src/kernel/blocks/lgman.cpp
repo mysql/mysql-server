0adc4c9dfea763d5dd1d0eeaa8f1003600d41426
Tomas Ulin <tomas.ulin@sun.com>
2009-03-24 09:33:25 +0100

    Bug #43825  ndbmt and DD may cause drop table to hang

--- storage/ndb/src/kernel/blocks/lgman.cpp
+++ storage/ndb/src/kernel/blocks/lgman.cpp
@@ -1158,6 +1158,7 @@ Logfile_client::Logfile_client(SimulatedBlock* block,
 {
   Uint32 bno = block->number();
   Uint32 ino = block->instance();
+  m_client_block= block;
   m_block= numberToBlock(bno, ino);
   m_lgman= lgman;
   m_lock = lock;
@@ -1222,8 +1223,8 @@ Logfile_client::sync_lsn(Signal* signal,
                           signal->theData[1],signal->theData[2],signal->theData[3],
                           Uint32(ptr.p->m_last_sync_req_lsn >> 32), Uint32(ptr.p->m_last_sync_req_lsn));
 
-      m_lgman->sendSignalWithDelay(m_lgman->reference(), 
-				   GSN_CONTINUEB, signal, 10, 4);
+      m_client_block->sendSignalWithDelay(m_lgman->reference(), 
+                                          GSN_CONTINUEB, signal, 10, 4);
     }
     return 0;
   }
