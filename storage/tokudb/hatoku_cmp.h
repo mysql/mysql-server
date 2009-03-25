7e130e6ec3ffcebaecf3372382ccc15f46ffce47
Zardosht Kasheff <zardosht@tokutek.com>
2009-03-25 20:25:06 +0000

    addresses #1619 move cmp related functions to new files
    git-svn-id: file:///svn/mysql/tokudb-engine/src@10719 c7de825b-a66e-492c-adef-691d508d4ae1

---
+++ storage/tokudb/hatoku_cmp.h
@@ -0,0 +1,86 @@
+#ifndef _HATOKU_CMP
+#define _HATOKU_CMP
+
+#include "mysql_priv.h"
+
+extern "C" {
+#include "stdint.h"
+}
+
+
+#include <db.h>
+
+
+//
+// for storing NULL byte in keys
+//
+#define NULL_COL_VAL 0
+#define NONNULL_COL_VAL 1
+
+//
+// for storing if rest of key is +/- infinity
+//
+#define COL_NEG_INF 0 
+#define COL_POS_INF 1
+
+//
+// information for hidden primary keys
+//
+#define TOKUDB_HIDDEN_PRIMARY_KEY_LENGTH 8
+
+//
+// function to convert a hidden primary key into a byte stream that can be stored in DBT
+//
+inline void hpk_num_to_char(uchar* to, ulonglong num) {
+    int8store(to, num);
+}
+
+//
+// function that takes a byte stream of a hidden primary key and returns a ulonglong
+//
+inline ulonglong hpk_char_to_num(uchar* val) {
+    return uint8korr(val);
+}
+
+
+
+inline int tokudb_compare_two_hidden_keys(
+    const void* new_key_data, 
+    const u_int32_t new_key_size, 
+    const void*  saved_key_data,
+    const u_int32_t saved_key_size
+    );
+
+int tokudb_compare_two_keys(
+    KEY *key, 
+    const void* new_key_data, 
+    const u_int32_t new_key_size, 
+    const void*  saved_key_data,
+    const u_int32_t saved_key_size,
+    bool cmp_prefix
+    );
+
+int tokudb_cmp_hidden_key(
+    DB* file, 
+    const DBT* new_key, 
+    const DBT* saved_key
+    );
+
+
+int tokudb_compare_two_clustered_keys(
+    KEY *key, 
+    KEY* primary_key, 
+    const DBT * new_key, 
+    const DBT * saved_key
+    );
+
+
+int tokudb_cmp_packed_key(DB *file, const DBT *keya, const DBT *keyb);
+
+int tokudb_cmp_primary_key(DB *file, const DBT *keya, const DBT *keyb);
+    
+//TODO: QQQ Only do one direction for prefix.
+int tokudb_prefix_cmp_packed_key(DB *file, const DBT *keya, const DBT *keyb);
+
+#endif
+
