d66ce7284ad814f58fe54b4372f8e4c46db44430
Jonas Oreland <jonas@mysql.com>
2009-04-04 22:50:24 +0200

    ndb - bug#44015 - apparently all tux triggers should fire first...

--- storage/ndb/src/kernel/blocks/dbtup/Dbtup.hpp
+++ storage/ndb/src/kernel/blocks/dbtup/Dbtup.hpp
@@ -2616,8 +2616,7 @@ private:
   bool do_tup_abort_operation(Signal*, Tuple_header *,
                               Operationrec*,
                               Fragrecord*,
-                              Tablerec*,
-                              Uint32);
+                              Tablerec*);
 
 //------------------------------------------------------------------
 //------------------------------------------------------------------
