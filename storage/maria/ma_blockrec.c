92b2b45e4fa43bdec5de0832040b945f5ff9fb2b
Michael Widenius <monty@mysql.com>
2009-04-08 11:47:30 +0300

    Updated comment

--- storage/maria/ma_blockrec.c
+++ storage/maria/ma_blockrec.c
@@ -2874,7 +2874,7 @@ static my_bool write_block_record(MARIA_HA *info,
         if (cur_block->used & BLOCKUSED_TAIL)
         {
           DBUG_ASSERT(data_length < MAX_TAIL_SIZE(block_size));
-          /* tail written to full tail page */
+          /* tail written to tail page */
           cur_block->used|= BLOCKUSED_USED;
           head_tail_block= cur_block;
         }
