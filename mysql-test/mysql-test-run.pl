add004346bcf0e1da7c3479df17ede260934edf7
Bernhard Ocklin <bo221916@urd01>
2009-04-08 10:47:07 +0200

    make funcs_1 run in PB1

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -271,7 +271,7 @@ sub main {
        "bzr_mysql-5.1-telco-6.2"        => "ndb_team",
        "bzr_mysql-5.1-telco-6.2-merge"  => "ndb_team",
        "bzr_mysql-5.1-telco-6.3"        => "ndb_team",
-       "bzr_mysql-5.1-telco-6.4"        => "ndb_team",
+       "bzr_mysql-5.1-telco-6.4"        => "ndb_team,funcs_1",
        "bzr_mysql-6.0-ndb"              => "ndb_team,rpl_ndb_big,ndb_binlog",
       );
 
