5c5691e8ec0a4c7ffe7990b03008838ae7571d19
Sergey Glukhov <Sergey.Glukhov@sun.com>
2009-04-10 14:25:48 +0500

    Bug#43385 Cannot ALTER DATABASE ... UPGRADE DATA DIRECTORY NAME when Views exist allow 'rename view' for ALTER ...UPGRADE DATA DIRECTORY NAME command. it's safe because a view has valid internal db&table names in this case.

--- sql/sql_rename.cc
+++ sql/sql_rename.cc
@@ -301,8 +301,13 @@ do_rename(THD *thd, TABLE_LIST *ren_table, char *new_db, char *new_table_name,
       }
       break;
     case FRMTYPE_VIEW:
-      /* change of schema is not allowed */
-      if (strcmp(ren_table->db, new_db))
+      /* 
+         change of schema is not allowed
+         except of ALTER ...UPGRADE DATA DIRECTORY NAME command
+         because a view has valid internal db&table names in this case.
+      */
+      if (thd->lex->sql_command != SQLCOM_ALTER_DB_UPGRADE &&
+          strcmp(ren_table->db, new_db))
         my_error(ER_FORBID_SCHEMA_CHANGE, MYF(0), ren_table->db, 
                  new_db);
       else
