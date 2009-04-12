0596c3f85e5b9165b366caf92d76d51c47affdca
Bradley C. Kuszmaul <bradley@tokutek.com>
2009-04-12 19:23:20 +0000

    Refs #1529.
    git-svn-id: file:///svn/toku/tokudb@11223 c7de825b-a66e-492c-adef-691d508d4ae1

--- newbrt/fifo.h
+++ newbrt/fifo.h
@@ -1,6 +1,6 @@
 #ifndef FIFO_H
 #define FIFO_H
-#ident "$Id: brt.c 11200 2009-04-10 22:28:41Z yfogel $"
+#ident "$Id$"
 #ident "Copyright (c) 2007, 2008, 2009 Tokutek Inc.  All rights reserved."
 #ident "The technology is licensed by the Massachusetts Institute of Technology, Rutgers State University of New Jersey, and the Research Foundation of State University of New York at Stony Brook under United States of America Serial No. 11/760379 and to the patents and/or patent applications resulting from it."
 
