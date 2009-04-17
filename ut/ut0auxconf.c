26a7583954a13ccfbc392048c57e18d70c495117
vasil <Unknown>
2009-04-17 21:29:16 +0000

    branches/zip:
    Silence warning about unused variables.

--- ut/ut0auxconf.c
+++ ut/ut0auxconf.c
@@ -7,6 +7,10 @@ main(int argc, char** argv)
 	pthread_t	x2;
 	pthread_t	x3;
 
+	memset(&x1, 0x0, sizeof(x1));
+	memset(&x2, 0x0, sizeof(x2));
+	memset(&x3, 0x0, sizeof(x3));
+
 	__sync_bool_compare_and_swap(&x1, x2, x3);
 
 	return(0);
