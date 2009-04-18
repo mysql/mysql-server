96e7913d687d6e6becd4729511242a0b0bee6c46
Zardosht Kasheff <zardosht@tokutek.com>
2009-04-18 22:08:17 +0000

    addresses BUILD stupid build error on linux, was passing windows
    git-svn-id: file:///svn/mysql/tokudb-engine/src@11431 c7de825b-a66e-492c-adef-691d508d4ae1

--- storage/tokudb/ha_tokudb.h
+++ storage/tokudb/ha_tokudb.h
@@ -402,7 +402,7 @@ public:
     void read_key_only(uchar * buf, uint keynr, DBT const *row, DBT const *found_key);
     void read_primary_key(uchar * buf, uint keynr, DBT const *row, DBT const *found_key);
     int read_row(uchar * buf, uint keynr, DBT const *row, DBT const *found_key);
-    void ha_tokudb::unpack_blobs(
+    void unpack_blobs(
         uchar* record,
         const uchar* from_tokudb_blob,
         u_int32_t num_blob_bytes
