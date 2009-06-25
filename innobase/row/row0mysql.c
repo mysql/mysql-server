0158cafab9275dcba4f718be1c92bbeb923ca26d
Satya B <satya.bn@sun.com>
2009-06-25 15:06:12 +0530

    Applying InnoDB snashot 5.0-ss5406, part 1. Fixes BUG#38479
    BUG#38479 - valgrind warnings in show table status for innodb tables
    
    Detailed revision comments:
    
    r5080 | vasil | 2009-05-22 14:45:34 +0300 (Fri, 22 May 2009) | 6 lines
    branches/5.0:
    
    Fix Bug#38479 valgrind warnings in show table status for innodb tables
    
    by initializing prebuilt->hint_need_to_fetch_extra_cols.

--- innobase/row/row0mysql.c
+++ innobase/row/row0mysql.c
@@ -625,7 +625,9 @@ row_create_prebuilt(
 	prebuilt->ins_node = NULL;
 
 	prebuilt->ins_upd_rec_buff = NULL;
-	
+
+	prebuilt->hint_need_to_fetch_extra_cols = 0;
+
 	prebuilt->upd_node = NULL;
 	prebuilt->ins_graph = NULL;
 	prebuilt->upd_graph = NULL;
