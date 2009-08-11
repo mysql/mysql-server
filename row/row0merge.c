a00ff8de64224f3571ecdab7c186b6dc00fc292f
marko <>
2009-08-11 10:42:37 +0000

    branches/zip: trx_general_rollback_for_mysql(): Remove the redundant parameter partial. If savept==NULL, partial==FALSE.

--- row/row0merge.c
+++ row/row0merge.c
@@ -2129,7 +2129,7 @@ row_merge_rename_tables(
 	if (err != DB_SUCCESS) {
 err_exit:
 		trx->error_state = DB_SUCCESS;
-		trx_general_rollback_for_mysql(trx, FALSE, NULL);
+		trx_general_rollback_for_mysql(trx, NULL);
 		trx->error_state = DB_SUCCESS;
 	}
 
