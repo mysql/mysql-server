6387737306c33b1d57790d31c37793e77d584819
Jonas Oreland <jonas@mysql.com>
2009-08-21 11:26:34 +0200

    ndb -   complete basepath concept, by forcing sender of FsOpenReq to specify basepath   (if using v4-filenames)
      This unifies Filename.set() and removes hard-coded basepath's in Ndbfs
    
      This is a preparation for fix of bug 36072

--- storage/ndb/src/kernel/blocks/lgman.cpp
+++ storage/ndb/src/kernel/blocks/lgman.cpp
@@ -598,6 +598,7 @@ Lgman::open_file(Signal* signal, Ptr<Undofile> ptr, Uint32 requestInfo)
   
   memset(req->fileNumber, 0, sizeof(req->fileNumber));
   FsOpenReq::setVersion(req->fileNumber, 4); // Version 4 = specified filename
+  FsOpenReq::v4_setBasePath(req->fileNumber, FsOpenReq::BP_DD_UF);
 
   req->fileFlags = 0;
   req->fileFlags |= FsOpenReq::OM_READWRITE;
