1fdd9da15a7efbd7a9b5e730280507b6d419beb2
MySQL Build Team <build@mysql.com>
2009-09-22 22:43:48 +0200

    Backport into build-200909221805-5.1.37sp1
    > ------------------------------------------------------------
    > revno: 3028 [merge]
    > revision-id: davi.arnaut@sun.com-20090715182250-ru0l00nqqlcfqe8n
    > parent: ramil@mysql.com-20090715102544-ejid81yz8k39m2fa
    > parent: kostja@sun.com-20090715170034-kjzpmq3px90pedw7
    > committer: Davi Arnaut <Davi.Arnaut@Sun.COM>
    > branch nick: 44495-5.1
    > timestamp: Wed 2009-07-15 15:22:50 -0300
    > message:
    >   Bug#44495: Prepared Statement: CALL p(<x>) - `thd->protocol == &thd->protocol_text' failed
    >   
    >   Merge Konstantin's patch and add a test case.
    > ------------------------------------------------------------
    > Use --include-merges or -n0 to see merged revisions.

--- tests/mysql_client_test.c
+++ tests/mysql_client_test.c
@@ -17940,6 +17940,57 @@ static void test_bug41078(void)
   DBUG_VOID_RETURN;
 }
 
+
+/**
+  Bug#44495: Prepared Statement:
+             CALL p(<x>) - `thd->protocol == &thd->protocol_text' failed
+*/
+
+static void test_bug44495()
+{
+  int rc;
+  MYSQL con;
+  MYSQL_STMT *stmt;
+
+  DBUG_ENTER("test_bug44495");
+  myheader("test_44495");
+
+  rc= mysql_query(mysql, "DROP PROCEDURE IF EXISTS p1");
+  myquery(rc);
+
+  rc= mysql_query(mysql, "CREATE PROCEDURE p1(IN arg VARCHAR(25))"
+                         "  BEGIN SET @stmt = CONCAT('SELECT \"', arg, '\"');"
+                         "  PREPARE ps1 FROM @stmt;"
+                         "  EXECUTE ps1;"
+                         "  DROP PREPARE ps1;"
+                         "END;");
+  myquery(rc);
+
+  DIE_UNLESS(mysql_init(&con));
+
+  DIE_UNLESS(mysql_real_connect(&con, opt_host, opt_user, opt_password,
+                                current_db, opt_port, opt_unix_socket,
+                                CLIENT_MULTI_RESULTS));
+
+  stmt= mysql_simple_prepare(&con, "CALL p1('abc')");
+  check_stmt(stmt);
+
+  rc= mysql_stmt_execute(stmt);
+  check_execute(stmt, rc);
+
+  rc= my_process_stmt_result(stmt);
+  DIE_UNLESS(rc == 1);
+
+  mysql_stmt_close(stmt);
+
+  mysql_close(&con);
+
+  rc= mysql_query(mysql, "DROP PROCEDURE p1");
+  myquery(rc);
+
+  DBUG_VOID_RETURN;
+}
+
 /*
   Read and parse arguments and MySQL options from my.cnf
 */
@@ -18255,6 +18306,7 @@ static struct my_tests_st my_tests[]= {
   { "test_bug36326", test_bug36326 },
 #endif
   { "test_bug41078", test_bug41078 },
+  { "test_bug44495", test_bug44495 },
   { 0, 0 }
 };
 
