7dde009fff6d37921dc7741c3ce17f9f43578348
Georgi Kodinov <joro@sun.com>
2009-09-24 16:19:06 +0300

    added suppressions for existing warnings in the result file.

--- mysql-test/include/mtr_warnings.sql
+++ mysql-test/include/mtr_warnings.sql
@@ -173,6 +173,7 @@ INSERT INTO global_suppressions VALUES
       this error message.
   */
  ("Can't find file: '.\\\\test\\\\\\?{8}.frm'"),
+ ("Slave: Unknown table 't1' Error_code: 1051"),
 
  ("THE_LAST_SUPPRESSION")||
 
