621e98a31b982ca32f288c2b7ff6388a33233bfd
Magnus Blåudd <magnus.blaudd@sun.com>
2009-09-24 16:09:11 +0200

    Bug#47612 mtr - improving the report for valgrind erorrs  - Improve the report produced when a valgrind error is detected

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -1307,6 +1307,9 @@ sub command_line_setup {
     push(@valgrind_args, @default_valgrind_args)
       unless @valgrind_args;
 
+    # Make valgrind run in quiet mode so it only print errors
+    push(@valgrind_args, "--quiet" );
+
     mtr_report("Running valgrind with options \"",
 	       join(" ", @valgrind_args), "\"");
   }
@@ -3588,7 +3591,7 @@ sub extract_warning_lines ($) {
      # qr/^Error:|\[ERROR\]/,
      qr/^Warning:|mysqld: Warning/,
      qr/^Error:/,
-     qr/^==.* at 0x/,
+     qr/^==\d*==/, # valgrind errors
      qr/InnoDB: Warning|InnoDB: Error/,
      qr/^safe_mutex:|allocated at line/,
      qr/missing DBUG_RETURN/,
