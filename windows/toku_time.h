924edf1b38e3f10b5ae778ab1e9d10ef25d16c95
Yoni Fogel <yoni@tokutek.com>
2009-10-05 16:55:03 +0000

    Closes #1792 closes[t:1792] icc or windows has ctime_r function internally, gives bad warning Using internal version instead of overwriting
    git-svn-id: file:///svn/toku/tokudb@14947 c7de825b-a66e-492c-adef-691d508d4ae1

--- windows/toku_time.h
+++ windows/toku_time.h
@@ -30,8 +30,6 @@ static inline float toku_tdiff (struct timeval *a, struct timeval *b) {
     return (a->tv_sec - b->tv_sec) +1e-6*(a->tv_usec - b->tv_usec);
 }
 
-char * ctime_r(const time_t *timep, char *buf);
-
 #ifdef __cplusplus
 };
 #endif
