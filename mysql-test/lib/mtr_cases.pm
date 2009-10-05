f8c5d3a521bcb047263d0d9d390ba46966c2490b
Satya B <satya.bn@sun.com>
2009-10-05 16:39:07 +0530

    Applying InnoDB snapshot 5.1-ss5921, Part 1. Fixes BUG#46000
    1. BUG#46000 - using index called GEN_CLUST_INDEX crashes server
    
    Detailed revision comments:
    
    r5895 | jyang | 2009-09-15 03:39:21 +0300 (Tue, 15 Sep 2009) | 5 lines
    branches/5.1: Disallow creating index with the name of
    "GEN_CLUST_INDEX" which is reserved for the default system
    primary index. (Bug #46000) rb://149 approved by Marko Makela.

--- mysql-test/lib/mtr_cases.pm
+++ mysql-test/lib/mtr_cases.pm
@@ -507,6 +507,8 @@ sub collect_one_suite($)
       next if ($test->{'name'} eq 'sys_vars.innodb_lock_wait_timeout_basic');
       # Diff around innodb_thread_concurrency variable
       next if ($test->{'name'} eq 'sys_vars.innodb_thread_concurrency_basic');
+      # Disable for Innodb Plugin until the fix for Plugin is received
+      next if ($test->{'name'} eq 'main.innodb_bug46000');
       # Copy test options
       my $new_test= My::Test->new();
       while (my ($key, $value) = each(%$test))
