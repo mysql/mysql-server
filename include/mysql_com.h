62672568bd6eaefab8c4d5b3d9701010c394dba3
Konstantin Osipov <kostja@sun.com>
2009-10-09 13:06:41 +0400

    Backport to 5.4 the following changesets:
    revno: 2476.785.24
    committer: kostja@bodhi.(none)
    timestamp: Tue 2007-10-16 20:19:00 +0400
    message:
      Reflect a rename of a member in the client ABI (a compatible change).
    ----------------------------------------------------------
    revno: 2476.423.26
    committer: kostja@bodhi.(none)
    timestamp: Tue 2007-10-16 20:12:37 +0400
    message:
      Update the client ABI to reflect member rename
      (this is a backward-compatible change).
    ----------------------------------------------------------
    revno: 2476.785.22
    committer: kostja@bodhi.(none)
    timestamp: Tue 2007-10-16 19:37:25 +0400
    message:
      Remove some remains of support of 3.22 protocol. This was in fact dead code,
      since the option to talk 3.22 protocol was removed in 4.1 and there
      is no other protocol negotiation mechanism besides this option.

--- include/mysql_com.h
+++ include/mysql_com.h
@@ -254,10 +254,10 @@ typedef struct st_net {
   unsigned int *return_status;
   unsigned char reading_or_writing;
   char save_char;
-  my_bool unused0; /* Please remove with the next incompatible ABI change. */
-  my_bool unused; /* Please remove with the next incompatible ABI change */
-  my_bool compress;
   my_bool unused1; /* Please remove with the next incompatible ABI change. */
+  my_bool unused2; /* Please remove with the next incompatible ABI change */
+  my_bool compress;
+  my_bool unused3; /* Please remove with the next incompatible ABI change. */
   /*
     Pointer to query object in query cache, do not equal NULL (0) for
     queries in cache that have not stored its results yet
@@ -270,8 +270,8 @@ typedef struct st_net {
   unsigned char *query_cache_query;
   unsigned int last_errno;
   unsigned char error; 
-  my_bool unused2; /* Please remove with the next incompatible ABI change. */
-  my_bool return_errno;
+  my_bool unused4; /* Please remove with the next incompatible ABI change. */
+  my_bool unused5; /* Please remove with the next incompatible ABI change. */
   /** Client library error message buffer. Actually belongs to struct MYSQL. */
   char last_error[MYSQL_ERRMSG_SIZE];
   /** Client library sqlstate buffer. Set along with the error message. */
