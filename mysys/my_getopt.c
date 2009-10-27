0ba101d4ea8cc081142288a47bdd4e10d83219e9
Tatiana A. Nurnberg <azundris@mysql.com>
2009-10-27 06:16:02 -0700

    Bug#46586: When using the plugin interface the type "set" for options caused a crash.
    "What do you mean, there's a bug? There isn't even code!"
    
    There was some token code for plug-in variables of the SET type,
    but clearly this never worked, or was subject to massive bit rot
    since. Bug-fixes ... fail-safes ... tests -- fais au mieux, mon chou!
    
    mysys/my_getopt.c:
      SETs set-up should set up a default value, but no min/max bounding.
    mysys/typelib.c:
      fail-safe requested by serg: don't try to skip separator when we're
      already at end of string.
    sql/sql_plugin.cc:
      check_func_set:
      Initialize error_len as find_set() will only update it on error,
      and we're using the value to see whether an error has occurred (!= 0),
      so we'd better not have a random val in there.
      
      value_ptr:
      There's no guarantee we're handed string lengths, so play it safe!
      Use prepared string lengths where possible for minimum speed gain,
      otherwise determine on the fly!

--- mysys/my_getopt.c
+++ mysys/my_getopt.c
@@ -1012,8 +1012,10 @@ static void init_one_value(const struct my_option *option, uchar* *variable,
     *((longlong*) variable)= (longlong) getopt_ll_limit_value((longlong) value, option, NULL);
     break;
   case GET_ULL:
-  case GET_SET:
     *((ulonglong*) variable)= (ulonglong) getopt_ull_limit_value((ulonglong) value, option, NULL);
+    break;
+  case GET_SET:
+    *((ulonglong*) variable)= (ulonglong) value;
     break;
   case GET_DOUBLE:
     *((double*) variable)=  (double) value;
