be04d1a47953188ced936a81824fdf871b8e9656
Tatiana A. Nurnberg <azundris@mysql.com>
2009-10-29 22:06:10 -0700

    Bug#48319: Server crashes on "GRANT/REVOKE ... TO CURRENT_USER"
    CURRENT_USER() in GRANT ... TO CURRENT_USER() only gave us a definer,
    not a full user (i.e., password-element was not initiliazed). Hence
    dereferencing the password led to a crash.
    
    Properly initializes definers now, just so there are no misunderstandings.
    Also does some magic so IDENTIFIED BY ... works with CURRENT_USER().
    
    mysql-test/r/grant2.result:
      Show GRANT ... TO CURRENT_USER() no longer crashes.
      Show it to work with IDENTIFIED BY to boot.
    mysql-test/t/grant2.test:
      Show GRANT ... TO CURRENT_USER() no longer crashes.
      Show it to work with IDENTIFIED BY to boot.
    sql/sql_acl.cc:
      Make IDENTIFIED BY ... work with CURRENT_USER()
    sql/sql_parse.cc:
      Zero password-part of definer just in case somebody mistakes this for
      a complete LEX_USER!

--- sql/sql_parse.cc
+++ sql/sql_parse.cc
@@ -7573,6 +7573,9 @@ void get_default_definer(THD *thd, LEX_USER *definer)
 
   definer->host.str= (char *) sctx->priv_host;
   definer->host.length= strlen(definer->host.str);
+
+  definer->password.str= NULL;
+  definer->password.length= 0;
 }
 
 
@@ -7624,6 +7627,8 @@ LEX_USER *create_definer(THD *thd, LEX_STRING *user_name, LEX_STRING *host_name)
 
   definer->user= *user_name;
   definer->host= *host_name;
+  definer->password.str= NULL;
+  definer->password.length= 0;
 
   return definer;
 }
