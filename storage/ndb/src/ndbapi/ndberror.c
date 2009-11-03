7a73c68502353f362617b5e6c1c64dd5b0e2a124
Jonas Oreland <jonas@mysql.com>
2009-11-03 15:38:26 +0100

    ndb - bug#48441 - fix so that failing seizeChunk sets p-value to 0 (and fix error code)

--- storage/ndb/src/ndbapi/ndberror.c
+++ storage/ndb/src/ndbapi/ndberror.c
@@ -168,7 +168,7 @@ ErrorBundle ErrorCodes[] = {
    * TemporaryResourceError
    */
   { 217,  DMEC, TR, "217" },
-  { 218,  DMEC, TR, "218" },
+  { 218,  DMEC, TR, "Out of LongMessageBuffer" },
   { 219,  DMEC, TR, "219" },
   { 233,  DMEC, TR,
     "Out of operation records in transaction coordinator (increase MaxNoOfConcurrentOperations)" },
