76903202dda9cc6d04f9b66c7f983a3b91c952ac
timothy.smith@sun.com <>
2009-11-05 21:22:17 +0100

    Use C comments in C code

--- mysys/typelib.c
+++ mysys/typelib.c
@@ -184,7 +184,7 @@ my_ulonglong find_typeset(char *x, TYPELIB *lib, int *err)
     i= x;
     while (*x && *x != field_separator)
       x++;
-    if (x[0] && x[1])                            // skip separator if found
+    if (x[0] && x[1])                            /* skip separator if found */
       x++;
     if ((find= find_type(i, lib, 2 | 8) - 1) < 0)
       DBUG_RETURN(0);
