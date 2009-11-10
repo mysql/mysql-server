ed800b5d7710a862d0a1fa18f577b93c5c2227f2
Davi Arnaut <Davi.Arnaut@Sun.COM>
2009-11-10 18:51:14 -0200

    Backport of Bug#45767 to mysql-next-mr ------------------------------------------------------------ revno: 3405 revision-id: davi.arnaut@sun.com-20090626124624-m4wolyo5193j4cu7 parent: luis.soares@sun.com-20090626113019-1j4mn1jos480u9f3 committer: Davi Arnaut <Davi.Arnaut@Sun.COM> branch nick: mysql-pe timestamp: Fri 2009-06-26 09:46:24 -0300 message:   Bug#45767: deprecate/remove Field::pack_key, Field::unpack_key, Field::pack_cmp
      Remove unused and dead code.
      
      Parts of the patch contributed by Zardosht Kasheff

--- sql/sql_string.cc
+++ sql/sql_string.cc
@@ -499,22 +499,6 @@ bool String::append(const char *s,uint32 arg_length, CHARSET_INFO *cs)
   return FALSE;
 }
 
-
-#ifdef TO_BE_REMOVED
-bool String::append(FILE* file, uint32 arg_length, myf my_flags)
-{
-  if (realloc(str_length+arg_length))
-    return TRUE;
-  if (my_fread(file, (uchar*) Ptr + str_length, arg_length, my_flags))
-  {
-    shrink(str_length);
-    return TRUE;
-  }
-  str_length+=arg_length;
-  return FALSE;
-}
-#endif
-
 bool String::append(IO_CACHE* file, uint32 arg_length)
 {
   if (realloc(str_length+arg_length))
