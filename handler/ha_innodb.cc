fcfe811c697c232c5ab6baa95e35e54225173378
marko <>
2009-11-17 08:19:34 +0000

    branches/zip: ha_innobase::change_active_index(): When the history is missing, report it to the client, not to the error log.

--- handler/ha_innodb.cc
+++ handler/ha_innodb.cc
@@ -5241,8 +5241,10 @@ ha_innobase::change_active_index(
 							   prebuilt->index);
 
 	if (UNIV_UNLIKELY(!prebuilt->index_usable)) {
-		sql_print_warning("InnoDB: insufficient history for index %u",
-				  keynr);
+		push_warning_printf(user_thd, MYSQL_ERROR::WARN_LEVEL_WARN,
+				    HA_ERR_TABLE_DEF_CHANGED,
+				    "InnoDB: insufficient history for index %u",
+				    keynr);
 		/* The caller seems to ignore this.  Thus, we must check
 		this again in row_search_for_mysql(). */
 		DBUG_RETURN(2);
