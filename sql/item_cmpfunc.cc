d76b83c4b508e0885fb8a4792baa5937ee91f491
MySQL Build Team <build@mysql.com>
2009-11-25 18:00:46 +0100

    Backport into build-200911241145-5.1.40sp1
    > ------------------------------------------------------------
    > revno: 3148.8.5
    > revision-id: davi.arnaut@sun.com-20091102112139-pztthzy6qj8jzomn
    > parent: svoj@sun.com-20091103091902-vwszwwpfi1f4zrpn
    > committer: Davi Arnaut <Davi.Arnaut@Sun.COM>
    > branch nick: 48370-5.1
    > timestamp: Mon 2009-11-02 09:21:39 -0200
    > message:
    >   Bug#48370: Absolutely wrong calculations with GROUP BY and decimal fields when using IF
    >   Bug#45261: Crash, stored procedure + decimal
    >   
    >   Revert fix for Bug#45261 due to unforeseen bugs.

--- sql/item_cmpfunc.cc
+++ sql/item_cmpfunc.cc
@@ -2191,7 +2191,7 @@ uint Item_func_ifnull::decimal_precision() const
   int arg1_int_part= args[1]->decimal_int_part();
   int max_int_part= max(arg0_int_part, arg1_int_part);
   int precision= max_int_part + decimals;
-  return precision;
+  return min(precision, DECIMAL_MAX_PRECISION);
 }
 
 
@@ -2375,7 +2375,7 @@ uint Item_func_if::decimal_precision() const
   int arg1_prec= args[1]->decimal_int_part();
   int arg2_prec= args[2]->decimal_int_part();
   int precision=max(arg1_prec,arg2_prec) + decimals;
-  return precision;
+  return min(precision, DECIMAL_MAX_PRECISION);
 }
 
 
@@ -2783,7 +2783,7 @@ uint Item_func_case::decimal_precision() const
 
   if (else_expr_num != -1) 
     set_if_bigger(max_int_part, args[else_expr_num]->decimal_int_part());
-  return max_int_part + decimals;
+  return min(max_int_part + decimals, DECIMAL_MAX_PRECISION);
 }
 
 
