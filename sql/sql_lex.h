488269e2a4e2f6c2339dbf5b032c9156461f573b
Tor Didriksen <tor.didriksen@sun.com>
2009-11-27 15:23:20 +0100

    Bug#49165 Remove unused/confusing code/comments in setup_conds()

--- sql/sql_lex.h
+++ sql/sql_lex.h
@@ -659,11 +659,6 @@ public:
   uint select_n_where_fields;
   enum_parsing_place parsing_place; /* where we are parsing expression */
   bool with_sum_func;   /* sum function indicator */
-  /* 
-    PS or SP cond natural joins was alredy processed with permanent
-    arena and all additional items which we need alredy stored in it
-  */
-  bool conds_processed_with_permanent_arena;
 
   ulong table_join_options;
   uint in_sum_expr;
