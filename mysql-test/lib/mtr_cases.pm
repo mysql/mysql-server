7cec35005b729cb7d52ae17f33f8be14f3c1b08b
Satya B <satya.bn@sun.com>
2009-11-30 18:43:34 +0530

    Applying InnoDB Plugin 1.0.6 snapshot, part 8. Fixes BUG#48782
    applied revisions: r6185, r6186, r6189, r6194
    
    r6185 - only code changes incorporated, changesets which change
            innodb tests in the main mysql suite are discarded
    
    r61889 - Fixes BUG#48782
    
    Detailed revision comments:
    
    r6185 | marko | 2009-11-17 16:44:20 +0200 (Tue, 17 Nov 2009) | 16 lines
    branches/zip: Report duplicate table names
    to the client connection, not to the error log.  This change will allow
    innodb-index.test to be re-enabled.  It was previously disabled, because
    mysql-test-run does not like output in the error log.
    
    row_create_table_for_mysql(): Do not output anything to the error log
    when reporting DB_DUPLICATE_KEY.  Let the caller report the error.
    Add a TODO comment that the dict_table_t object is apparently not freed
    when an error occurs.
    
    create_table_def(): Convert InnoDB table names to the character set
    of the client connection for reporting.  Use my_error(ER_WRONG_COLUMN_NAME)
    for reporting reserved column names.  Report my_error(ER_TABLE_EXISTS_ERROR)
    when row_create_table_for_mysql() returns DB_DUPLICATE_KEY.
    
    rb://206
    r6186 | vasil | 2009-11-17 16:48:14 +0200 (Tue, 17 Nov 2009) | 4 lines
    branches/zip:
    
    Add ChangeLog entry for r6185.
    
    r6189 | marko | 2009-11-18 11:36:18 +0200 (Wed, 18 Nov 2009) | 5 lines
    branches/zip: ha_innobase::add_index(): When creating the primary key
    and the table is being locked by another transaction,
    do not attempt to drop the table. (Bug #48782)
    
    Approved by Sunny Bains over IM
    r6194 | vasil | 2009-11-19 09:24:45 +0200 (Thu, 19 Nov 2009) | 5 lines
    branches/zip:
    
    Increment version number from 1.0.5 to 1.0.6 since 1.0.5 was just released
    by MySQL and we will soon release 1.0.6.

--- mysql-test/lib/mtr_cases.pm
+++ mysql-test/lib/mtr_cases.pm
@@ -522,6 +522,8 @@ sub collect_one_suite($)
       next if ($test->{'name'} eq 'main.innodb_bug46000');
       # Fails with innodb plugin
       next if ($test->{'name'} eq 'main.innodb-autoinc');
+      # Fails with innodb plugin: r6185 Testcases changes not included
+      next if ($test->{'name'} eq 'main.innodb_bug44369');
       # Copy test options
       my $new_test= My::Test->new();
       while (my ($key, $value) = each(%$test))
