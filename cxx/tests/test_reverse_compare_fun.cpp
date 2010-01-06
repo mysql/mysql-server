6088dfa47e30ab9044f3599c51244e42b96ec91c
Bradley C. Kuszmaul <bradley@tokutek.com>
2010-01-06 05:13:10 +0000

    test_errpfx was setting two data dirs.  exceptions is broken out to its worn rule (and is failing because of #2284).  exceptions was also operating in the cwd, instead of a subdir.  The reverse_compare_fun was failing because it wasing cwd for some calls instead of the subdir. Refs #2285, #2284.  [t:2285] [t:2284]
    git-svn-id: file:///svn/toku/tokudb@16835 c7de825b-a66e-492c-adef-691d508d4ae1

--- cxx/tests/test_reverse_compare_fun.cpp
+++ cxx/tests/test_reverse_compare_fun.cpp
@@ -58,7 +58,7 @@ void test_reverse_compare(int n, int dup_flags) {
 
     Db *db;
     DbTxn * const null_txn = 0;
-    const char * const fname = DIR "/reverse.compare.db";
+    const char * const fname = "reverse.compare.db";
 
     int r;
     int i;
@@ -68,7 +68,7 @@ void test_reverse_compare(int n, int dup_flags) {
 
     /* create the dup database file */
     DbEnv env(DB_CXX_NO_EXCEPTIONS);
-    r = env.open(".", DB_INIT_MPOOL + DB_CREATE + DB_PRIVATE, 0777); assert(r == 0);
+    r = env.open(DIR, DB_INIT_MPOOL + DB_CREATE + DB_PRIVATE, 0777); assert(r == 0);
     db = new Db(&env, DB_CXX_NO_EXCEPTIONS);
     assert(db);
     r = db->set_flags(dup_flags);
