a64b869f504ce97f10aa0ac9bba625e6249e0f4b
Bradley C. Kuszmaul <bradley@tokutek.com>
2010-01-06 22:04:16 +0000

    Make recovery tests quieter.  Fixes #2293.    close[t:2293]
    git-svn-id: file:///svn/toku/tokudb@16861 c7de825b-a66e-492c-adef-691d508d4ae1

--- newbrt/tests/recovery-no-log.c
+++ newbrt/tests/recovery-no-log.c
@@ -13,6 +13,12 @@ run_test(void) {
     system("rm -rf " TESTDIR);
     r = toku_os_mkdir(TESTDIR, S_IRWXU); assert(r == 0);
 
+    // redirect stderr
+    int devnul = open("/dev/null", O_WRONLY);
+    assert(devnul>=0);
+    r = dup2(devnul, fileno(stderr)); 	    assert(r==fileno(stderr));
+    r = close(devnul);                      assert(r==0);
+
     // run recovery
     r = tokudb_recover(TESTDIR, TESTDIR, 0, 0, 0); 
     assert(r != 0);
