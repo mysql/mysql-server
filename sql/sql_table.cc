4872d2a6e25f93f1516fa4549dcdd45f5f27b128
Martin Skold <Martin.Skold@mysql.com>
2010-01-15 10:58:18 +0100

    Bug #50247 ALTER TABLE cannot change auto_increment: Fixed update_create_info not to read up old auto_increment value during alter of auto_increment, set HA_CHANGE_AUTOINCREMENT_VALUE to simplify check_if_supported_alter

--- sql/sql_table.cc
+++ sql/sql_table.cc
@@ -5550,7 +5550,10 @@ err:
   Copy all changes detected by parser to the HA_ALTER_FLAGS
 */
 
-void setup_ha_alter_flags(Alter_info *alter_info, HA_ALTER_FLAGS *alter_flags)
+void setup_ha_alter_flags(TABLE *table,
+                          HA_CREATE_INFO *create_info,
+                          Alter_info *alter_info,
+                          HA_ALTER_FLAGS *alter_flags)
 {
   uint flags= alter_info->flags;
 
@@ -5588,6 +5591,9 @@ void setup_ha_alter_flags(Alter_info *alter_info, HA_ALTER_FLAGS *alter_flags)
     *alter_flags|= HA_ALTER_PARTITION;
   if (ALTER_FOREIGN_KEY & flags)
     *alter_flags|= HA_ALTER_FOREIGN_KEY;
+  if (create_info->auto_increment_value !=
+      table->file->stats.auto_increment_value)
+    *alter_flags|= HA_CHANGE_AUTOINCREMENT_VALUE;
 }
 
 
@@ -5687,7 +5693,7 @@ compare_tables(THD *thd,
     First we setup ha_alter_flags based on what was detected
     by parser
   */
-  setup_ha_alter_flags(alter_info, alter_flags);
+  setup_ha_alter_flags(table, create_info, alter_info, alter_flags);
 
 #ifndef DBUG_OFF
   {
