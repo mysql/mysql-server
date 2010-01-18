1f074b1c90ef54626271d6187e473258b5bf25c7
Jonas Oreland <jonas@mysql.com>
2010-01-18 12:11:04 +0100

    ndb - bug#49737 - fix correct error message if encountering truncated LCP file

--- storage/ndb/src/kernel/blocks/restore.hpp
+++ storage/ndb/src/kernel/blocks/restore.hpp
@@ -146,6 +146,7 @@ private:
   void parse_error(Signal*, FilePtr, Uint32 line, Uint32 extra);
   int check_file_version(Signal*, Uint32 file_version);
   void restore_lcp_conf(Signal* signal, FilePtr);
+  void crash_during_restore(FilePtr, Uint32 line, Uint32 errCode);
 public:
   
 private:
