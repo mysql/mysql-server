90fbdb612223921a6e4bd7f1c66cfca135959fd8
Vladislav Vaintroub <vvaintroub@mysql.com>
2010-01-26 15:56:50 +0100

    WL#5161, implement Mats' suggeston to move platform specifics into own files under cmake/os. This patch does it for Linux

--- cmake/os/Windows.cmake
+++ cmake/os/Windows.cmake
@@ -1,4 +1,4 @@
-# Copyright (C) 2009 Sun Microsystems, Inc
+# Copyright (C) 2010 Sun Microsystems, Inc
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
