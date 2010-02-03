5c04b4374d7fbf3f0170c10081da1804359b19cd
MySQL Build Team <build@mysql.com>
2010-02-03 16:38:46 +0100

    Backport into build-201002030816-5.0.87sp1
    > ------------------------------------------------------------
    > revno: 2818.1.39
    > revision-id: gshchepa@mysql.com-20091201102444-yw166t3audrojo9s
    > parent: joro@sun.com-20091127160731-6h2fahbh4409i841
    > committer: Gleb Shchepa <gshchepa@mysql.com>
    > branch nick: mysql-5.0-bugteam
    > timestamp: Tue 2009-12-01 14:24:44 +0400
    > message:
    >   Bug #38883 (reopened): thd_security_context is not thread safe, crashes?
    >   
    >   The bug 38816 changed the lock that protects THD::query from
    >   LOCK_thread_count to LOCK_thd_data, but didn't update the associated
    >   InnoDB functions.
    >   
    >   1. The innobase_mysql_prepare_print_arbitrary_thd and the
    >   innobase_mysql_end_print_arbitrary_thd InnoDB functions have been
    >   removed, since now we have a per-thread mutex: now we don't need to wrap
    >   several inter-thread access tries to THD::query with a single global
    >   LOCK_thread_count lock, so we can simplify the code.
    >   
    >   2. The innobase_mysql_print_thd function has been modified to lock
    >   LOCK_thd_data in direct way.

--- innobase/lock/lock0lock.c
+++ innobase/lock/lock0lock.c
@@ -18,31 +18,6 @@ Created 5/7/1996 Heikki Tuuri
 #include "trx0sys.h"
 
 
-/* 2 function prototypes copied from ha_innodb.cc: */
-
-/*****************************************************************
-If you want to print a thd that is not associated with the current thread,
-you must call this function before reserving the InnoDB kernel_mutex, to
-protect MySQL from setting thd->query NULL. If you print a thd of the current
-thread, we know that MySQL cannot modify thd->query, and it is not necessary
-to call this. Call innobase_mysql_end_print_arbitrary_thd() after you release
-the kernel_mutex.
-NOTE that /mysql/innobase/lock/lock0lock.c must contain the prototype for this
-function! */
-
-void
-innobase_mysql_prepare_print_arbitrary_thd(void);
-/*============================================*/
-
-/*****************************************************************
-Relases the mutex reserved by innobase_mysql_prepare_print_arbitrary_thd().
-NOTE that /mysql/innobase/lock/lock0lock.c must contain the prototype for this
-function! */
-
-void
-innobase_mysql_end_print_arbitrary_thd(void);
-/*========================================*/
-
 /* Restricts the length of search we will do in the waits-for
 graph of transactions */
 #define LOCK_MAX_N_STEPS_IN_DEADLOCK_CHECK 1000000
@@ -4231,11 +4206,6 @@ lock_print_info_summary(
 /*====================*/
 	FILE*	file)	/* in: file where to print */
 {
-	/* We must protect the MySQL thd->query field with a MySQL mutex, and
-	because the MySQL mutex must be reserved before the kernel_mutex of
-	InnoDB, we call innobase_mysql_prepare_print_arbitrary_thd() here. */
-
-	innobase_mysql_prepare_print_arbitrary_thd();
 	lock_mutex_enter_kernel();
 
 	if (lock_deadlock_found) {
@@ -4322,7 +4292,6 @@ loop:
 
 	if (trx == NULL) {
 		lock_mutex_exit_kernel();
-		innobase_mysql_end_print_arbitrary_thd();
 
 		ut_ad(lock_validate());
 
@@ -4387,7 +4356,6 @@ loop:
 
  		if (load_page_first) {
 			lock_mutex_exit_kernel();
-			innobase_mysql_end_print_arbitrary_thd();
 
 			mtr_start(&mtr);
 			
@@ -4397,7 +4365,6 @@ loop:
 
 			load_page_first = FALSE;
 
-			innobase_mysql_prepare_print_arbitrary_thd();
 			lock_mutex_enter_kernel();
 
 			goto loop;
