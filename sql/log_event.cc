a0e19f681601979231effab07a2bb6f7e3fbc33c
Luis Soares <luis.soares@sun.com>
2010-02-05 17:48:01 +0000

    BUG#50620: Adding an index to a table prevents slave from logging into slow log
    While processing a statement, down the mysql_parse execution
    stack, the thd->enable_slow_log can be assigned to
    opt_log_slow_admin_statements, depending whether one is executing
    administrative statements, such as ALTER TABLE, OPTIMIZE,
    ANALYZE, etc, or not. This can have an impact on slow logging for
    statements that are executed after an administrative statement
    execution is completed.
          
    When executing statements directly from the user this is fine
    because, the thd->enable_slow_log is reset right at the beginning
    of the dispatch_command function, ie, everytime a new statement
    is set is set to execute.
          
    On the other hand, for slave SQL thread (sql_thd) the story is a
    bit different. When in SBR the sql_thd applies statements by
    calling mysql_parse. Right after, it calls log_slow_statement
    function to log them if they take too long. Calling mysql_parse
    directly is fine, but also means that dispatch_command function
    is bypassed. As a consequence, thd->enable_slow_log does not get
    a chance to be reset before the next statement to be executed by
    the sql_thd. If the statement just executed by the sql_thd was an
    administrative statement and logging of admin statements was
    disabled, this means that sql_thd->enable_slow_log will be set to
    0 (disabled) from that moment on. End result: sql_thd stops
    logging slow statements.
          
    We fix this by resetting the value of sql_thd->enable_slow_log to
    the value of opt_log_slow_slave_statements right after
    log_slow_stement is called by the sql_thd.

--- sql/log_event.cc
+++ sql/log_event.cc
@@ -3170,6 +3170,18 @@ int Query_log_event::do_apply_event(Relay_log_info const *rli,
       const char* found_semicolon= NULL;
       mysql_parse(thd, thd->query(), thd->query_length(), &found_semicolon);
       log_slow_statement(thd);
+
+      /*
+        Resetting the enable_slow_log thd variable.
+
+        We need to reset it back to the opt_log_slow_slave_statements
+        value after the statement execution (and slow logging
+        is done). It might have changed if the statement was an
+        admin statement (in which case, down in mysql_parse execution
+        thd->enable_slow_log is set to the value of
+        opt_log_slow_admin_statements).
+      */
+      thd->enable_slow_log= opt_log_slow_slave_statements;
     }
     else
     {
