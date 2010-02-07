2fd32cad3393780ed3c56e3a701adf156e70103f
Pekka Nousiainen <pekka@mysql.com>
2010-02-07 18:35:44 +0200

    bug#31712 01_bug31712.diff Return error if datafile size is smaller than extent size. The I_S discrepancies are fixed in another patch.

--- storage/ndb/src/ndbapi/ndberror.c
+++ storage/ndb/src/ndbapi/ndberror.c
@@ -437,7 +437,7 @@ ErrorBundle ErrorCodes[] = {
   { 1513, DMEC, IE, "Filegroup not online" },
   { 1514, DMEC, SE, "Currently there is a limit of one logfile group" },
   { 1515, DMEC, SE, "Currently there is a 4G limit of one undo/data-file in 32-bit host" },
-  { 1516, DMEC, SE, "File to small" },
+  { 1516, DMEC, SE, "File too small" },
 
   { 773,  DMEC, SE, "Out of string memory, please modify StringMemory config parameter" },
   { 775,  DMEC, SE, "Create file is not supported when Diskless=1" },
