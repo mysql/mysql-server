dbf972f7ea8de9b2b6e2bc0cdf3b648c0fbcc178
Jorgen Austvik <jorgen.austvik@sun.com>
2010-02-18 14:14:06 +0100

    Don't run tests annotated with @SlowTest unless specified with command line option - to reduce run time in PB2

--- storage/ndb/clusterj/clusterj-test/src/main/java/testsuite/clusterj/AllTests.java
+++ storage/ndb/clusterj/clusterj-test/src/main/java/testsuite/clusterj/AllTests.java
@@ -3,6 +3,7 @@ package testsuite.clusterj;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.jar.JarEntry;
@@ -19,8 +20,10 @@ public class AllTests {
 
     private static String jarFile = "";
 
+    private static boolean onlyRunSlowTests = false; 
+
     private static void usage() {
-        System.out.println("Usage: java -cp <jar file>:... AllTests <jar file>");
+        System.out.println("Usage: java -cp <jar file>:... AllTests <jar file> [--only-run-slow-tests]");
         System.out.println("Will run all tests in the given jar file.");
         System.exit(2);
     }
@@ -29,11 +32,23 @@ public class AllTests {
         return fileName.endsWith("Test.class");
     }
 
+    private static boolean isSlowTestAnnotationPresent(Class candidate) {
+        for (Annotation annotation: candidate.getAnnotations()) {
+            if (annotation.getClass().getName().contains("SlowTest")) {
+                return true;
+            }
+        }
+        return false;
+    } 
+
     private static boolean isTestClass(Class klass) {
-        // TODO: Could check if klass inherits junit.framework.Test in some way,
-        //       but this will also be cought in the cast later
         return klass.getName().endsWith("Test")
-            && !klass.getName().contains("Abstract");
+            && !klass.getName().contains("Abstract")
+            && Test.class.isAssignableFrom(klass);
+    }
+
+    private static boolean isSlowTest(Class klass) {
+	return isSlowTestAnnotationPresent(klass);
     }
 
     private static List<Class> getClasses(File jarFile) throws IOException, ClassNotFoundException {
@@ -74,20 +89,31 @@ public class AllTests {
         List<Class> classes = getClasses(new File(jarFile));
         for (Class klass : classes) {
 	    if (isTestClass(klass)) {
-		suite.addTestSuite(klass);
+		if ((isSlowTest(klass) && onlyRunSlowTests)
+		    || (!isSlowTest(klass) && !onlyRunSlowTests)) {
+		    suite.addTestSuite(klass);
+		}
 	    }
         }
         return suite;
     }
 
     /**
-     * Usage: java -cp ... AllTests file.jar
+     * Usage: java -cp ... AllTests file.jar [--only-run-slow-tests]
+     *
+     * --only-run-slow-tests parameter only run test in classes annotated with @SlowTest,
+     * else these tests will be skipped.
      *
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        if (args.length == 1) {
+        if (args.length > 0 && args.length <= 2) {
             jarFile = args[0];
+	    if (args.length > 1) {
+		if (args[1].equalsIgnoreCase("--only-run-slow-tests")) {
+		    onlyRunSlowTests = true;
+		}
+	    }
             System.out.println("Running all tests in '" + jarFile + "'");
 	    TestSuite suite = (TestSuite) suite();
             System.out.println("Found '" + suite.testCount() + "' tests in jar file.");
