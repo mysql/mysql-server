15bd17b3ff716c98b5b003cea0cf12b4ad4566e5
Andrew Hutchings <Andrew.Hutchings@Sun.COM>
2010-02-20 08:57:57 +0000

    Bug #50599  	ndb_desc does not show total/free extents for BLOB/TEXT columns
    Added BT= to Attributes output to reflect the blob table for a column
    Added --blob-info (-b) to show partition information for blob tables too
    
    6.3 version of fix

--- storage/ndb/tools/desc.cpp
+++ storage/ndb/tools/desc.cpp
@@ -34,6 +34,7 @@ NDB_STD_OPTS_VARS;
 static const char* _dbname = "TEST_DB";
 static int _unqualified = 0;
 static int _partinfo = 0;
+static int _blobinfo = 0;
 
 const char *load_default_groups[]= { "mysql_cluster",0 };
 
@@ -53,6 +54,9 @@ static struct my_option my_long_options[] =
   { "retries", 'r', "Retry every second for # retries",
     (uchar**) &_retries, (uchar**) &_retries, 0,
     GET_INT, REQUIRED_ARG, 0, 0, 0, 0, 0, 0 }, 
+  { "blob-info", 'b', "Show information for hidden blob tables (requires -p)",
+    (uchar**) &_blobinfo, (uchar**) &_blobinfo, 0,
+    GET_BOOL, NO_ARG, 0, 0, 0, 0, 0, 0 },
   { 0, 0, 0, 0, 0, 0, GET_NO_ARG, NO_ARG, 0, 0, 0, 0, 0, 0}
 };
 static void usage()
@@ -281,7 +285,24 @@ int desc_table(Ndb *myndb, char* name)
   ndbout << endl;
 
   if (_partinfo)
+  {
     print_part_info(myndb, pTab);
+    ndbout << endl;
+    if (_blobinfo)
+    {
+      int noOfAttributes = pTab->getNoOfColumns();
+      for (int i = 0; i < noOfAttributes; i++)
+      {
+        const NdbDictionary::Column* column = pTab->getColumn(i);
+        if ((column->getType() == NdbDictionary::Column::Blob) || 
+          (column->getType() == NdbDictionary::Column::Text))
+        {
+          print_part_info(myndb, (NDBT_Table*) column->getBlobTable());
+          ndbout << endl;
+        }
+      }
+    }
+  }
 	
   return 1;
 }
@@ -309,7 +330,12 @@ void print_part_info(Ndb* pNdb, NDBT_Table* pTab)
     { 0, 0, 0 }
   };
 
-  ndbout << "-- Per partition info -- " << endl;
+  ndbout << "-- Per partition info";
+
+  if (_blobinfo && _partinfo)
+    ndbout << " for " << pTab->getName();
+
+  ndbout << " -- " << endl;
   
   const Uint32 codeWords= 1;
   Uint32 codeSpace[ codeWords ];
