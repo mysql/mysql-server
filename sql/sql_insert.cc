51e47e13173c01174c4e367c7ca2640a6a9dd699
Tatiana A. Nurnberg <azundris@mysql.com>
2010-02-22 16:58:56 +0000

    revert 48525

--- sql/sql_insert.cc
+++ sql/sql_insert.cc
@@ -3158,7 +3158,7 @@ bool select_insert::send_data(List<Item> &values)
 
   thd->count_cuted_fields= CHECK_FIELD_WARN;	// Calculate cuted fields
   store_values(values);
-  thd->count_cuted_fields= CHECK_FIELD_ERROR_FOR_NULL;
+  thd->count_cuted_fields= CHECK_FIELD_IGNORE;
   if (thd->is_error())
   {
     table->auto_increment_field_not_null= FALSE;
