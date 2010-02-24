0e7c84153d4fdf03d626ad59215912f2bf8ac030
Sergey Petrunya <psergey@askmonty.org>
2010-02-24 12:33:42 +0100

    Bug#49198 Wrong result for second call of of procedure with view in subselect. Re-worked fix of Tor Didriksen:  The problem was that fix_after_pullout() after semijoin conversion  wasn't propagated from the view to the underlying table.  On subesequent executions of the prepared statement,  we would mark the underlying table as 'dependent' and the predicate  anlysis would lead to a different (and illegal) execution plan.

--- sql/item.cc
+++ sql/item.cc
@@ -6491,11 +6491,9 @@ void Item_outer_ref::fix_after_pullout(st_select_lex *new_parent, Item **ref)
 
 void Item_ref::fix_after_pullout(st_select_lex *new_parent, Item **refptr)
 {
+  (*ref)->fix_after_pullout(new_parent, ref);
   if (depended_from == new_parent)
-  {
-    (*ref)->fix_after_pullout(new_parent, ref);
     depended_from= NULL;
-  }
 }
 
 
