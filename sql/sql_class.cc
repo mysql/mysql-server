638ebaa7a9e30b2b1f01d34acd2b21c449e7e1b9
Luis Soares <luis.soares@sun.com>
2010-03-08 23:25:52 +0000

    WL#5092
    The pending event was not being flushed to disk when 
    affected columns for the table would change. This would
    cause mismatches between write_set bits and the actual
    row data.
    
    Fixed by deploying missing check in the condition that
    is tested while choosing to flush the pending event or not.

--- sql/sql_class.cc
+++ sql/sql_class.cc
@@ -3815,7 +3815,8 @@ THD::binlog_prepare_pending_rows_event(TABLE* table, uint32 serv_id,
       pending->server_id != serv_id || 
       pending->get_table_id() != table->s->table_map_id ||
       pending->get_type_code() != type_code || 
-      pending->get_data_size() + needed > opt_binlog_rows_event_max_size) 
+      pending->get_data_size() + needed > opt_binlog_rows_event_max_size ||
+      !bitmap_cmp(pending->get_cols(), table->write_set)) 
   {
     /* Create a new RowsEventT... */
     Rows_log_event* const
