1a1a23235d7ac62a9055032d4b187d1f05f1a868
Evgeny Potemkin <epotemkin@mysql.com>
2010-03-08 16:36:43 +0300

    BUG#36639: subselect.test crashes on 64 bit pentium4 when compiled for valgrind - Use the compiler's default copy constructor for QUICK_RANGE_SELECT.   bcopy(this, copy, ...) call caused some odd action on gcc-4.1.2 on x86_64

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -5780,8 +5780,7 @@ best_access_path(JOIN      *join,
       }
       else
       {
-        found_constraint= 1;
-        // psergey:
+        found_constraint= test(found_part);
         /*
           Check if InsideOut scan is applicable:
           1. All IN-equalities are either "bound" or "handled"
