95567a75341f81692fa882c73e21518304c5aeaa
vasil <Unknown>
2010-03-13 10:31:17 +0000

    branches/innodb+:
    Say "InnoDB" instead of "InnoDB+" at startup, this is going to be the
    builtin InnoDB in MySQL 5.5.

--- srv/srv0start.c
+++ srv/srv0start.c
@@ -1790,7 +1790,7 @@ innobase_start_or_create_for_mysql(void)
 	if (srv_print_verbose_log) {
 		ut_print_timestamp(stderr);
 		fprintf(stderr,
-			" InnoDB+ %s started; "
+			" InnoDB %s started; "
 			"log sequence number %llu\n",
 			INNODB_VERSION_STR, srv_start_lsn);
 	}
