47d0cf2929536fb7779e97c63a062ccc8511b856
Sergey Petrunya <psergey@askmonty.org>
2010-03-15 09:32:54 +0300

    Apply fix by Roy Lyseng: Bug#48623: Multiple subqueries are optimized incorrectly
    The function setup_semijoin_dups_elimination() has a major loop that
    goes through every table in the JOIN object. Usually, there is a normal
    "plus one" increment in the for loop that implements this, but each semijoin
    nest is treated as one entity and there is another increment that skips past
    the semijoin nest to the next table in the JOIN object. However, when
    combining these two increments, the next joined table is skipped, and if that
    happens to be the start of another semijoin nest, the correct processing
    for that nest will not be carried out.
    
    mysql-test/r/subselect_sj.result:
      Added test results for bug#48623
    mysql-test/r/subselect_sj_jcl6.result:
      Added test results for bug#48623
    mysql-test/t/subselect_sj.test:
      Added test case for bug#48623
    sql/opt_subselect.cc:
      Omitted the "plus one" increment in the for loop, added "plus one"
      in the remaining switch case, fixed coding style issue in remaining
      increment operations.

--- sql/opt_subselect.cc
+++ sql/opt_subselect.cc
@@ -3030,7 +3030,7 @@ int setup_semijoin_dups_elimination(JOIN *join, ulonglong options,
   THD *thd= join->thd;
   DBUG_ENTER("setup_semijoin_dups_elimination");
 
-  for (i= join->const_tables ; i < join->tables ; i++)
+  for (i= join->const_tables ; i < join->tables; )
   {
     JOIN_TAB *tab=join->join_tab + i;
     POSITION *pos= join->best_positions + i;
@@ -3039,7 +3039,7 @@ int setup_semijoin_dups_elimination(JOIN *join, ulonglong options,
       case SJ_OPT_MATERIALIZE:
       case SJ_OPT_MATERIALIZE_SCAN:
         /* Do nothing */
-        i += pos->n_sj_tables;
+        i+= pos->n_sj_tables;
         break;
       case SJ_OPT_LOOSE_SCAN:
       {
@@ -3055,7 +3055,7 @@ int setup_semijoin_dups_elimination(JOIN *join, ulonglong options,
         tab->loosescan_key_len= keylen;
         if (pos->n_sj_tables > 1) 
           tab[pos->n_sj_tables - 1].do_firstmatch= tab;
-        i += pos->n_sj_tables;
+        i+= pos->n_sj_tables;
         break;
       }
       case SJ_OPT_DUPS_WEEDOUT:
@@ -3152,7 +3152,7 @@ int setup_semijoin_dups_elimination(JOIN *join, ulonglong options,
         join->join_tab[first_table].flush_weedout_table= sjtbl;
         join->join_tab[i + pos->n_sj_tables - 1].check_weed_out_table= sjtbl;
 
-        i += pos->n_sj_tables;
+        i+= pos->n_sj_tables;
         break;
       }
       case SJ_OPT_FIRST_MATCH:
@@ -3174,10 +3174,11 @@ int setup_semijoin_dups_elimination(JOIN *join, ulonglong options,
           }
         }
         j[-1].do_firstmatch= jump_to;
-        i += pos->n_sj_tables;
+        i+= pos->n_sj_tables;
         break;
       }
       case SJ_OPT_NONE:
+        i++;
         break;
     }
   }
