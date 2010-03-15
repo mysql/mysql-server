7dca134c47bfc957517b937c9423466837e30944
Davi Arnaut <Davi.Arnaut@Sun.COM>
2010-03-15 09:07:16 -0300

    Bug#51289: double Item_cache_decimal::val_real() is broken
    Fix up function return value, it must return a double.

--- sql/item.cc
+++ sql/item.cc
@@ -7159,7 +7159,7 @@ double Item_cache_decimal::val_real()
   DBUG_ASSERT(fixed);
   double res;
   if (!value_cached && !cache_value())
-    return NULL;
+    return 0.0;
   my_decimal2double(E_DEC_FATAL_ERROR, &decimal_value, &res);
   return res;
 }
