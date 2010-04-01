058fdb11ad60ffc93d23366333ab98639722aa90
Sergey Vojtovich <svoj@sun.com>
2010-04-01 15:51:08 +0400

    Applying InnoDB snapshot
    Detailed revision comments:
    
    r6639 | marko | 2010-02-10 13:11:04 +0200 (Wed, 10 Feb 2010) | 1 line
    branches/zip: trx_rseg_create(): Unused function, remove.

--- storage/innodb_plugin/include/trx0rseg.h
+++ storage/innodb_plugin/include/trx0rseg.h
@@ -114,17 +114,6 @@ trx_rseg_list_and_array_init(
 /*=========================*/
 	trx_sysf_t*	sys_header,	/*!< in: trx system header */
 	mtr_t*		mtr);		/*!< in: mtr */
-/****************************************************************//**
-Creates a new rollback segment to the database.
-@return	the created segment object, NULL if fail */
-UNIV_INTERN
-trx_rseg_t*
-trx_rseg_create(
-/*============*/
-	ulint	space,		/*!< in: space id */
-	ulint	max_size,	/*!< in: max size in pages */
-	ulint*	id,		/*!< out: rseg id */
-	mtr_t*	mtr);		/*!< in: mtr */
 /***************************************************************************
 Free's an instance of the rollback segment in memory. */
 UNIV_INTERN
