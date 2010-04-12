ae88a26f6bf20793906ba39a3fe39c4d0bed7755
Tor Didriksen <tor.didriksen@sun.com>
2010-04-12 14:51:43 +0200

    Post-merge fix of #includes.

--- sql/sql_alloc_error_handler.cc
+++ sql/sql_alloc_error_handler.cc
@@ -13,7 +13,9 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
-#include "mysql_priv.h"
+#include "log.h"
+#include "sql_class.h"
+#include "mysqld.h"
 
 extern "C" void sql_alloc_error_handler(void)
 {
