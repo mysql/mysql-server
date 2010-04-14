02ed15db571d7b6d4be79a5bf0434c1d5c975d6f
Sergei Golubchik <sergii@pisem.net>
2010-04-14 12:29:40 -0700

    don't hide old_password_plugin from embedded, it may be compiled with grant support

--- sql/sql_acl.cc
+++ sql/sql_acl.cc
@@ -162,9 +162,7 @@ static LEX_STRING old_password_plugin_name= {
 LEX_STRING *default_auth_plugin_name= &native_password_plugin_name;
 
 static plugin_ref native_password_plugin;
-#ifndef EMBEDDED_LIBRARY
 static plugin_ref old_password_plugin;
-#endif
 
 /* Classes */
 
