a36f927b4782933306f7d104f863a195bafd18dd
Evgeny Potemkin <epotemkin@mysql.com>
2010-04-20 14:23:13 +0400

    Backporting: remove garbage comments. Original revid: sergefp@mysql.com-20080913015108-gwwtzyw51xktsbrs

--- storage/myisam/ha_myisam.cc
+++ storage/myisam/ha_myisam.cc
@@ -1558,7 +1558,6 @@ C_MODE_START
 my_bool index_cond_func_myisam(void *arg)
 {
   ha_myisam *h= (ha_myisam*)arg;
-  /*if (h->in_range_read)*/
   if (h->end_range)
   {
     if (h->compare_key2(h->end_range) > 0)
@@ -1573,7 +1572,6 @@ C_MODE_END
 int ha_myisam::index_init(uint idx, bool sorted)
 { 
   active_index=idx;
-  //in_range_read= FALSE;
   if (pushed_idx_cond_keyno == idx)
     mi_set_index_cond_func(file, index_cond_func_myisam, this);
   return 0; 
@@ -1697,13 +1695,7 @@ int ha_myisam::read_range_first(const key_range *start_key,
                                 bool sorted /* ignored */)
 {
   int res;
-  //if (!eq_range_arg)
-  //  in_range_read= TRUE;
-
   res= handler::read_range_first(start_key, end_key, eq_range_arg, sorted);
-
-  //if (res)
-  //  in_range_read= FALSE;
   return res;
 }
 
@@ -1711,8 +1703,6 @@ int ha_myisam::read_range_first(const key_range *start_key,
 int ha_myisam::read_range_next()
 {
   int res= handler::read_range_next();
-  //if (res)
-  //  in_range_read= FALSE;
   return res;
 }
 
