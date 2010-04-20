69fb5cef191cf666a9c5211e527294871fa2e904
Vasil Dimov <vasil.dimov@oracle.com>
2010-04-20 18:23:28 +0300

    Import the Persistent Stats code from (svn) branches/innodb+_persistent_stats
    The history of this WIP is irrelevant and is not worth the effort importing
    into BZR, if needed it can still be viewed in SVN.

--- storage/innobase/handler/ha_innodb.h
+++ storage/innobase/handler/ha_innodb.h
@@ -27,6 +27,8 @@ Place, Suite 330, Boston, MA 02111-1307 USA
 #pragma interface			/* gcc class implementation */
 #endif
 
+#include "dict0stats.h"
+
 /* Structure defines translation table between mysql index and innodb
 index structures */
 typedef struct innodb_idx_translate_struct {
@@ -109,6 +111,7 @@ class ha_innobase: public handler
 	ulint innobase_update_autoinc(ulonglong	auto_inc);
 	void innobase_initialize_autoinc();
 	dict_index_t* innobase_get_index(uint keynr);
+	int info_low(uint flag, enum dict_stats_upd_how stats_upd_how);
 
 	/* Init values for the class: */
  public:
