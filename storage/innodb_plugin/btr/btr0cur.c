2b57b0765db00497a6c02c14a69f8535c9b986d9
Vasil Dimov <vasil.dimov@oracle.com>
2010-04-28 11:46:27 +0300

    Fix Bug#53046 dict_update_statistics_low can still be run concurrently on same table
    Protect dict_index_t::stat_n_diff_key_vals[] with an array of
    mutexes.
    
    Testing: tested all code paths under UNIV_SYNC_DEBUG
    for the one in dict_print() one has to enable the InnoDB table monitor:
    CREATE TABLE innodb_table_monitor (a int) ENGINE=INNODB;

--- storage/innodb_plugin/btr/btr0cur.c
+++ storage/innodb_plugin/btr/btr0cur.c
@@ -3356,6 +3356,8 @@ btr_estimate_number_of_different_key_vals(
 	also the pages used for external storage of fields (those pages are
 	included in index->stat_n_leaf_pages) */
 
+	dict_index_stat_mutex_enter(index);
+
 	for (j = 0; j <= n_cols; j++) {
 		index->stat_n_diff_key_vals[j]
 			= ((n_diff[j]
@@ -3384,6 +3386,8 @@ btr_estimate_number_of_different_key_vals(
 
 		index->stat_n_diff_key_vals[j] += add_on;
 	}
+
+	dict_index_stat_mutex_exit(index);
 
 	mem_free(n_diff);
 	if (UNIV_LIKELY_NULL(heap)) {
