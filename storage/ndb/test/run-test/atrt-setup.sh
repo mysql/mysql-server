eb15d4eef56f3d1bf97b1a377e1e71e1057a743e
Jonas Oreland <jonas@mysql.com>
2010-04-30 11:12:28 +0200

    ndb - autotest, Fix regression in windows changes, breaking a my.cnf containing only localhost, also change to verbose only on windows

--- storage/ndb/test/run-test/atrt-setup.sh
+++ storage/ndb/test/run-test/atrt-setup.sh
@@ -3,21 +3,36 @@
 MACHINE=$1
 LOCAL_DIR=$2
 REMOTE_DIR=$3
+verbose=
 
-# Local copy
-if [ `uname -n` = "$MACHINE" ]; then
-  if [ "$REMOTE_DIR" = "$LOCAL_DIR" -o "$REMOTE_DIR/" = "$LOCAL_DIR" ]; then
-    echo "$0: Same directoty on local host. Skipping setup..."
-  else
-    echo "$0: Local machine setup from '$REMOTE_DIR' to '$LOCAL_DIR'..."
-    cp -r "$REMOTE_DIR" "$LOCAL_DIR"
-  fi
-  exit 0;
+if uname | grep -iq cygwin
+then
+    verbose=1
 fi
 
-if uname | grep -iq cygwin; then
-  LOCAL_DIR=`cygpath -u $LOCAL_DIR`
-  REMOTE_DIR=`cygpath -u $REMOTE_DIR`
+# Local copy
+if [ "$MACHINE" = "`uname -n`" -o "$MACHINE" = "localhost" ]
+then
+    if [ "$REMOTE_DIR" = "$LOCAL_DIR" -o "$REMOTE_DIR/" = "$LOCAL_DIR" ]
+    then
+	if [ "$verbose" ]
+	then
+	    echo "$0: Same directory on localhost. Skipping setup..."
+	fi
+    else
+	if [ "$verbose" ]
+	then
+	    echo "$0: Local machine setup from '$REMOTE_DIR' to '$LOCAL_DIR'..."
+	fi
+	cp -r "$REMOTE_DIR" "$LOCAL_DIR"
+    fi
+    exit 0;
+fi
+
+if uname | grep -iq cygwin
+then
+    LOCAL_DIR=`cygpath -u $LOCAL_DIR`
+    REMOTE_DIR=`cygpath -u $REMOTE_DIR`
 fi
 
 set -e
