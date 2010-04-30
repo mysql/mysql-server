60267648d453b43f4c9d48814a0851fe767a8c08
Dave Wells <wells@tokutek.com>
2010-04-30 14:07:58 +0000

    move changes to loader.c and its tests to main re:DB_DBT_REALLOC
    git-svn-id: file:///svn/toku/tokudb@19651 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/loader-reference-test.c
+++ src/tests/loader-reference-test.c
@@ -11,6 +11,7 @@ DB_ENV *env;
 enum {MAX_NAME=128};
 enum {NUM_DBS=1};
 enum {NUM_KV_PAIRS=3};
+int USE_PUTS=0;
 struct kv_pair {
     int64_t key;
     int64_t val;
@@ -47,7 +48,7 @@ static void test_loader(DB **dbs)
         db_flags[i] = DB_NOOVERWRITE; 
         dbt_flags[i] = 0;
     }
-    uint32_t loader_flags = 0;
+    uint32_t loader_flags = USE_PUTS; // set with -p option
 
     // create and initialize loader
     r = env->txn_begin(env, NULL, &txn, 0);                                                               
@@ -170,6 +171,8 @@ static void do_args(int argc, char * const argv[]) {
 	} else if (strcmp(argv[0],"-q")==0) {
 	    verbose--;
 	    if (verbose<0) verbose=0;
+        } else if (strcmp(argv[0], "-p")==0) {
+            USE_PUTS = 1;
 	} else {
 	    fprintf(stderr, "Unknown arg: %s\n", argv[0]);
 	    resultcode=1;
