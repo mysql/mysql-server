bed25036aaf7c68d84ab1930289eb3cb26efa967
Bradley C. Kuszmaul <bradley@tokutek.com>
2010-05-13 20:07:00 +0000

    Merge in the double buffered io abstraction.  It's not in the loader yet.  Refs #2571. [t:2571] {{{ svn merge -r 20089:20155 https://svn.tokutek.com/tokudb/toku/tokudb.2571 }}} .
    git-svn-id: file:///svn/toku/tokudb@20156 c7de825b-a66e-492c-adef-691d508d4ae1

--- newbrt/brtloader-callback.c
+++ newbrt/brtloader-callback.c
@@ -1,5 +1,5 @@
 /* -*- mode: C; c-basic-offset: 4 -*- */
-#ident "$Id: brtloader.c 19913 2010-05-07 01:39:12Z prohaska $"
+#ident "$Id$"
 #ident "Copyright (c) 2007-2010 Tokutek Inc.  All rights reserved."
 #ident "The technology is licensed by the Massachusetts Institute of Technology, Rutgers State University of New Jersey, and the Research Foundation of State University of New York at Stony Brook under United States of America Serial No. 11/760379 and to the patents and/or patent applications resulting from it."
 
