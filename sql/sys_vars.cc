2796d8e1c9bb0b5b9eabb9358aca7f827d07e847
Roy Lyseng <roy.lyseng@sun.com>
2010-05-26 08:07:10 +0200

    WL#5369: Remove --optimizer_search_depth=63
    Remove deprecated value 63 for this switch, and the old function find_best()
    that was controlled by this value.
     
    mysql-test/r/greedy_optimizer.result
      WL#5369: Updated test result.
    
    mysql-test/r/mysqld--help-notwin.result
      WL#5369: Updated test result.
    
    mysql-test/r/mysqld--help-win.result
      WL#5369: Updated test result.
    
    mysql-test/suite/sys_vars/r/optimizer_search_depth_basic.result
      WL#5369: Updated test result.
    
    mysql-test/suite/sys_vars/t/optimizer_search_depth_basic.test
      WL#5369: Updated test file.
    
    mysql-test/t/greedy_optimizer.test
      WL#5369: Updated test file.
    
    sql/sql_select.cc
      Removed test for 'MAX_TABLES+2' in choose_plan(), removed function
      find_best(), ie we are now always calling greedy_search(), unless we
      use straight_join to override the join optimizer.
    
    sql/sql_select.h
      Removed reference to find_best().
    
    sql/sys_vars.cc
      Updated limits and comment for variable optimizer-search-depth.

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -1330,17 +1330,6 @@ static Sys_var_ulong Sys_optimizer_prune_level(
        SESSION_VAR(optimizer_prune_level), CMD_LINE(REQUIRED_ARG),
        VALID_RANGE(0, 1), DEFAULT(1), BLOCK_SIZE(1));
 
-/** Warns about deprecated value 63 */
-static bool fix_optimizer_search_depth(sys_var *self, THD *thd,
-                                       enum_var_type type)
-{
-  SV *sv= type == OPT_GLOBAL ? &global_system_variables : &thd->variables;
-  if (sv->optimizer_search_depth == MAX_TABLES+2)
-    WARN_DEPRECATED(thd, 6, 0, "optimizer-search-depth=63",
-                    "a search depth less than 63");
-  return false;
-}
-
 static Sys_var_ulong Sys_optimizer_search_depth(
        "optimizer_search_depth",
        "Maximum depth of search performed by the query optimizer. Values "
@@ -1348,13 +1337,9 @@ static Sys_var_ulong Sys_optimizer_search_depth(
        "query plans, but take longer to compile a query. Values smaller "
        "than the number of tables in a relation result in faster "
        "optimization, but may produce very bad query plans. If set to 0, "
-       "the system will automatically pick a reasonable value; if set to "
-       "63, the optimizer will switch to the original find_best search. "
-       "NOTE: The value 63 and its associated behaviour is deprecated",
+       "the system will automatically pick a reasonable value",
        SESSION_VAR(optimizer_search_depth), CMD_LINE(REQUIRED_ARG),
-       VALID_RANGE(0, MAX_TABLES+2), DEFAULT(MAX_TABLES+1), BLOCK_SIZE(1),
-       NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(0),
-       ON_UPDATE(fix_optimizer_search_depth));
+       VALID_RANGE(0, MAX_TABLES+1), DEFAULT(MAX_TABLES+1), BLOCK_SIZE(1));
 
 static const char *optimizer_switch_names[]=
 {
