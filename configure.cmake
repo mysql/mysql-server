2857b55a463a288753f2b860b7bfb9bf538d00ed
Tor Didriksen <tor.didriksen@sun.com>
2010-05-27 09:42:48 +0200

    Bug #53445 Build with -Wall and fix warnings that it generates Post-push fix: -Wall implies -Wunused on some platforms, which will generate thousands of warnings about unused parameters.

--- configure.cmake
+++ configure.cmake
@@ -56,7 +56,7 @@ ENDIF()
 
 # Always enable -Wall for gnu C/C++
 IF(CMAKE_COMPILER_IS_GNUCXX)
-  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
+  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-unused-parameter")
 ENDIF()
 IF(CMAKE_COMPILER_IS_GNUCC)
   SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall")
