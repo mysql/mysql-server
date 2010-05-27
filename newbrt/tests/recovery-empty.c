6d4f2104fdaf4b328d17ec24cb57a7becb5de133
Rich Prohaska <prohaska@tokutek.com>
2010-05-27 15:34:07 +0000

    closes[t:2667] quiet recovery-empty
    git-svn-id: file:///svn/toku/tokudb@20719 c7de825b-a66e-492c-adef-691d508d4ae1

--- newbrt/tests/recovery-empty.c
+++ newbrt/tests/recovery-empty.c
@@ -20,6 +20,14 @@ run_test(void) {
     r = toku_logger_open(TESTDIR, logger); assert(r == 0);
     r = toku_logger_close(&logger); assert(r == 0);
 
+    if (!verbose) {
+        // redirect stderr
+        int devnul = open(DEV_NULL_FILE, O_WRONLY);
+        assert(devnul >= 0);
+        r = toku_dup2(devnul, fileno(stderr)); assert(r == fileno(stderr));
+        r = close(devnul); assert(r == 0);
+    }
+
     // run recovery
     r = tokudb_recover(TESTDIR, TESTDIR, 0, 0, NULL, NULL, 0); 
     assert(r == DB_RUNRECOVERY);
@@ -28,7 +36,8 @@ run_test(void) {
 }
 
 int
-test_main(int UU(argc), const char *UU(argv[])) {
+test_main(int argc, const char *argv[]) {
+    default_parse_args(argc, argv);
     int r;
     r = run_test();
     return r;
