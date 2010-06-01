b2c758a96f168f31f1683951b91074b33f752576
Magnus Blåudd <magnus.blaudd@sun.com>
2010-06-01 14:49:58 +0200

    ndb  - rewrite the NDB_CLOSE_SOCKET mess to a inline function

--- storage/ndb/include/portlib/NdbTCP.h
+++ storage/ndb/include/portlib/NdbTCP.h
@@ -25,7 +25,11 @@
 #include <portlib/ndb_socket_poller.h>
 
 #define NDB_SOCKET_TYPE ndb_socket_t
-#define _NDB_CLOSE_SOCKET(x) my_socket_close(x)
+
+static inline
+void NDB_CLOSE_SOCKET(ndb_socket_t s) {
+  my_socket_close(s);
+}
 
 #ifdef	__cplusplus
 extern "C" {
@@ -43,12 +47,6 @@ extern "C" {
  *      inet_addr
  */
 int Ndb_getInAddr(struct in_addr * dst, const char *address);
-
-#ifdef DBUG_OFF
-#define NDB_CLOSE_SOCKET(fd) _NDB_CLOSE_SOCKET(fd)
-#else
-int NDB_CLOSE_SOCKET(NDB_SOCKET_TYPE fd);
-#endif
 
 int Ndb_check_socket_hup(NDB_SOCKET_TYPE sock);
 
