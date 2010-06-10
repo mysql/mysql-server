0f9ddfa9d8bb8d071266bcc63e92813cf18ccd2b
Davi Arnaut <Davi.Arnaut@Sun.COM>
2010-06-10 17:45:22 -0300

    Bug#42733: Type-punning warnings when compiling MySQL --             strict aliasing violations.
    One somewhat major source of strict-aliasing violations and
    related warnings is the SQL_LIST structure. For example,
    consider its member function `link_in_list` which takes
    a pointer to pointer of type T (any type) as a pointer to
    pointer to unsigned char. Dereferencing this pointer, which
    is done to reset the next field, violates strict-aliasing
    rules and might cause problems for surrounding code that
    uses the next field of the object being added to the list.
    
    The solution is to use templates to parametrize the SQL_LIST
    structure in order to deference the pointers with compatible
    types. As a side bonus, it becomes possible to remove quite
    a few casts related to acessing data members of SQL_LIST.
    
    sql/handler.h:
      Use the appropriate template type argument.
    sql/item.cc:
      Remove now-unnecessary cast.
    sql/item_subselect.cc:
      Remove now-unnecessary casts.
    sql/item_sum.cc:
      Use the appropriate template type argument.
      Remove now-unnecessary cast.
    sql/mysql_priv.h:
      Move SQL_LIST structure to sql_list.h
      Use the appropriate template type argument.
    sql/sp.cc:
      Remove now-unnecessary casts.
    sql/sql_delete.cc:
      Use the appropriate template type argument.
      Remove now-unnecessary casts.
    sql/sql_derived.cc:
      Remove now-unnecessary casts.
    sql/sql_lex.cc:
      Remove now-unnecessary casts.
    sql/sql_lex.h:
      SQL_LIST now takes a template type argument which must
      match the type of the elements of the list. Use forward
      declaration when the type is not available, it is used
      in pointers anyway.
    sql/sql_list.h:
      Rename SQL_LIST to SQL_I_List. The template parameter is
      the type of object that is stored in the list.
    sql/sql_olap.cc:
      Remove now-unnecessary casts.
    sql/sql_parse.cc:
      Remove now-unnecessary casts.
    sql/sql_prepare.cc:
      Remove now-unnecessary casts.
    sql/sql_select.cc:
      Remove now-unnecessary casts.
    sql/sql_show.cc:
      Remove now-unnecessary casts.
    sql/sql_table.cc:
      Remove now-unnecessary casts.
    sql/sql_trigger.cc:
      Remove now-unnecessary casts.
    sql/sql_union.cc:
      Remove now-unnecessary casts.
    sql/sql_update.cc:
      Remove now-unnecessary casts.
    sql/sql_view.cc:
      Remove now-unnecessary casts.
    sql/sql_yacc.yy:
      Remove now-unnecessary casts.
    storage/myisammrg/ha_myisammrg.cc:
      Remove now-unnecessary casts.

--- sql/sql_prepare.cc
+++ sql/sql_prepare.cc
@@ -1240,7 +1240,7 @@ static int mysql_test_update(Prepared_statement *stmt,
 
   if (mysql_prepare_update(thd, table_list, &select->where,
                            select->order_list.elements,
-                           (ORDER *) select->order_list.first))
+                           select->order_list.first))
     goto error;
 
 #ifndef NO_EMBEDDED_ACCESS_CHECKS
@@ -1746,11 +1746,10 @@ error:
 static int mysql_insert_select_prepare_tester(THD *thd)
 {
   SELECT_LEX *first_select= &thd->lex->select_lex;
-  TABLE_LIST *second_table= ((TABLE_LIST*)first_select->table_list.first)->
-    next_local;
+  TABLE_LIST *second_table= first_select->table_list.first->next_local;
 
   /* Skip first table, which is the table we are inserting in */
-  first_select->table_list.first= (uchar *) second_table;
+  first_select->table_list.first= second_table;
   thd->lex->select_lex.context.table_list=
     thd->lex->select_lex.context.first_name_resolution_table= second_table;
 
@@ -1787,7 +1786,7 @@ static bool mysql_test_insert_select(Prepared_statement *stmt,
     return 1;
 
   /* store it, because mysql_insert_select_prepare_tester change it */
-  first_local_table= (TABLE_LIST *)lex->select_lex.table_list.first;
+  first_local_table= lex->select_lex.table_list.first;
   DBUG_ASSERT(first_local_table != 0);
 
   res=
@@ -1795,7 +1794,7 @@ static bool mysql_test_insert_select(Prepared_statement *stmt,
                                     &mysql_insert_select_prepare_tester,
                                     OPTION_SETUP_TABLES_DONE);
   /* revert changes  made by mysql_insert_select_prepare_tester */
-  lex->select_lex.table_list.first= (uchar*) first_local_table;
+  lex->select_lex.table_list.first= first_local_table;
   return res;
 }
 
@@ -2339,10 +2338,10 @@ void reinit_stmt_before_use(THD *thd, LEX *lex)
       DBUG_ASSERT(sl->join == 0);
       ORDER *order;
       /* Fix GROUP list */
-      for (order= (ORDER *)sl->group_list.first; order; order= order->next)
+      for (order= sl->group_list.first; order; order= order->next)
         order->item= &order->item_ptr;
       /* Fix ORDER list */
-      for (order= (ORDER *)sl->order_list.first; order; order= order->next)
+      for (order= sl->order_list.first; order; order= order->next)
         order->item= &order->item_ptr;
 
       /* clear the no_error flag for INSERT/UPDATE IGNORE */
@@ -2379,7 +2378,7 @@ void reinit_stmt_before_use(THD *thd, LEX *lex)
     (multi-delete).  We do a full clean up, although at the moment all we
     need to clean in the tables of MULTI-DELETE list is 'table' member.
   */
-  for (TABLE_LIST *tables= (TABLE_LIST*) lex->auxiliary_table_list.first;
+  for (TABLE_LIST *tables= lex->auxiliary_table_list.first;
        tables;
        tables= tables->next_global)
   {
