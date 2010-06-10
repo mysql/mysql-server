c3b4cf4aaa5e99759b254f5f7bbcd1d7bad9e2b6
Inaam Rana <inaam.rana@oracle.com>
2010-06-10 09:58:11 -0400

    Formatting changes

--- storage/innodb_plugin/row/row0purge.c
+++ storage/innodb_plugin/row/row0purge.c
@@ -44,7 +44,7 @@ Created 3/14/1997 Heikki Tuuri
 #include "row0mysql.h"
 #include "log0log.h"
 
-/*********************************************************************//**
+/*************************************************************************
 IMPORTANT NOTE: Any operation that generates redo MUST check that there
 is enough space in the redo log before for that operation. This is
 done by calling log_free_check(). The reason for checking the
