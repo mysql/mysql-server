dacd3e4a77bb4015ca0e2fb55088dbb852f6bd15
MySQL Build Team <build@mysql.com>
2010-06-22 22:53:01 +0200

    Backport into build-201006221614-5.1.46sp1
    > ------------------------------------------------------------
    > revno: 3351.41.1
    > revision-id: alexey.kopytov@sun.com-20100430111048-jdls6ofn4kkmpt09
    > parent: sergey.glukhov@sun.com-20100329134249-03wyhzp5k92dzhcb
    > committer: Alexey Kopytov <Alexey.Kopytov@Sun.com>
    > branch nick: my51-bug48419
    > timestamp: Fri 2010-04-30 15:10:48 +0400
    > message:
    >   Bug #48419: another explain crash..
    >   
    >   WHERE predicates containing references to empty tables in a
    >   subquery were handled incorrectly by the optimizer when
    >   executing EXPLAIN. As a result, the optimizer could try to
    >   evaluate such predicates rather than just stop with
    >   "Impossible WHERE noticed after reading const tables" as 
    >   it would do in a non-subquery case. This led to valgrind 
    >   errors and crashes.
    >   
    >   Fixed the code checking the above condition so that subqueries
    >   are not excluded and hence are handled in the same way as top
    >   level SELECTs.

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -1107,8 +1107,7 @@ JOIN::optimize()
   }
 
   if (conds && const_table_map != found_const_table_map &&
-      (select_options & SELECT_DESCRIBE) &&
-      select_lex->master_unit() == &thd->lex->unit) // upper level SELECT
+      (select_options & SELECT_DESCRIBE))
   {
     conds=new Item_int((longlong) 0,1);	// Always false
   }
