a8c62f2bbc15526f9e17b157efa168eb1d96ee98
Vladislav Vaintroub <vvaintroub@mysql.com>
2010-06-28 02:12:12 +0200

    Fix build with nmake, cleanup Wix packaging stuff

--- packaging/WiX/CPackWixConfig.cmake
+++ packaging/WiX/CPackWixConfig.cmake
@@ -1,10 +1,15 @@
 
 IF(ESSENTIALS)
- MESSAGE("Essentials!")
  SET(CPACK_COMPONENTS_USED "Server;Client;DataFiles")
  SET(CPACK_WIX_UI "WixUI_InstallDir")
- MATH(EXPR bits ${CMAKE_SIZEOF_VOID_P}*8)
- SET(CPACK_PACKAGE_FILE_NAME  "mysql-essentials-${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH}-win${bits}")
+ IF(CPACK_PACKAGE_FILE_NAME MATCHES "mysql-")
+   STRING(REPLACE "mysql" "mysql-essentials" CPACK_PACKAGE_FILE_NAME
+     "${CPACK_PACKAGE_FILE_NAME}")
+ ELSE()
+   MATH(EXPR bits ${CMAKE_SIZEOF_VOID_P}*8)
+   SET(CPACK_PACKAGE_FILE_NAME  
+     "mysql-essentials-${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH}-win${bits}")
+ ENDIF()
 ELSE()
   SET(CPACK_COMPONENTS_USED 
     "Server;Client;DataFiles;Development;SharedLibraries;Embedded;Debuginfo;Documentation;IniFiles;Readme;Server_Scripts")
