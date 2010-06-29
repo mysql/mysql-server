deaba603e4ac60d1c15c13b773a116ee4c49e717
Marko Mäkelä <marko.makela@oracle.com>
2010-06-29 14:32:48 +0300

    Bug#52199 utf32: mbminlen=4, mbmaxlen=4, type->mbminlen=0, type->mbmaxlen=4
    Merge and adjust a forgotten change to fix this bug.
    rb://393 approved by Jimmy Yang
      ------------------------------------------------------------------------
      r3794 | marko | 2009-01-07 14:14:53 +0000 (Wed, 07 Jan 2009) | 18 lines
    
      branches/6.0: Allow the minimum length of a multi-byte character to be
      up to 4 bytes. (Bug #35391)
    
      dtype_t, dict_col_t: Replace mbminlen:2, mbmaxlen:3 with mbminmaxlen:5.
      In this way, the 5 bits can hold two values of 0..4, and the storage size
      of the fields will not cross the 64-bit boundary.  Encode the values as
      DATA_MBMAX * mbmaxlen + mbminlen.  Define the auxiliary macros
      DB_MBMINLEN(mbminmaxlen), DB_MBMAXLEN(mbminmaxlen), and
      DB_MINMAXLEN(mbminlen, mbmaxlen).
    
      Try to trim and pad UTF-16 and UTF-32 with spaces as appropriate.
    
      Alexander Barkov suggested the use of cs->cset->fill(cs, buff, len, 0x20).
      ha_innobase::store_key_val_for_row() now does that, but the added function
      row_mysql_pad_col() does not, because it doesn't have the MySQL TABLE object.
    
      rb://49 approved by Heikki Tuuri
      ------------------------------------------------------------------------

--- storage/innobase/include/dict0mem.h
+++ storage/innobase/include/dict0mem.h
@@ -151,9 +151,9 @@ dict_mem_table_add_col(
 	ulint		prtype,	/*!< in: precise type */
 	ulint		len);	/*!< in: precision */
 /**********************************************************************//**
-This function poplulates a dict_col_t memory structure with
+This function populates a dict_col_t memory structure with
 supplied information. */
-UNIV_INLINE
+UNIV_INTERN
 void
 dict_mem_fill_column_struct(
 /*========================*/
@@ -162,7 +162,7 @@ dict_mem_fill_column_struct(
 	ulint		col_pos,	/*!< in: column position */
 	ulint		mtype,		/*!< in: main data type */
 	ulint		prtype,		/*!< in: precise type */
-	ulint		col_len);	/*!< in: column lenght */
+	ulint		col_len);	/*!< in: column length */
 /**********************************************************************//**
 This function poplulates a dict_index_t index memory structure with
 supplied information. */
@@ -249,10 +249,11 @@ struct dict_col_struct{
 					the string, MySQL uses 1 or 2
 					bytes to store the string length) */
 
-	unsigned	mbminlen:2;	/*!< minimum length of a
-					character, in bytes */
-	unsigned	mbmaxlen:3;	/*!< maximum length of a
-					character, in bytes */
+	unsigned	mbminmaxlen:5;	/*!< minimum and maximum length of a
+					character, in bytes;
+					DATA_MBMINMAXLEN(mbminlen,mbmaxlen);
+					mbminlen=DATA_MBMINLEN(mbminmaxlen);
+					mbmaxlen=DATA_MBMINLEN(mbminmaxlen) */
 	/*----------------------*/
 	/* End of definitions copied from dtype_t */
 	/* @} */
