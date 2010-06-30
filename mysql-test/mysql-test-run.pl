3b832b49c5006e504cd8de0f5686263b28236266
Marc Alff <marc.alff@oracle.com>
2010-06-30 08:05:18 -0600

    Implemented code review comments: fixed FSF address

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+# long with this program; if not, write to the Free Software Foundation,
+# 51 Franklin Street, Suite 500, Boston, MA 02110-1335 USA
 
 #
 ##############################################################################
