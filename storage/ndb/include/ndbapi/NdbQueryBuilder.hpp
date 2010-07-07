ecde58eb0072ca343c352f9cbda517f302e944bd
Jan Wedvik <jan.wedvik@sun.com>
2010-07-07 15:24:23 +0200

    This commit contains an initial version of result handling for NdbQuery with multiple scans. The code has not been tested much. It will not work for (left) outer joins or queries with a lookup root and a scan descendant. This code iterates over results from the lowest scan and uppwards. This may or or may not be rewritten into going from the root and downwards, depending on what appears to give the simplest code and best performance.
    The code may be tested using the following SQL script:
    
    create database spj_ndb;
    use spj_ndb;
    set ndb_join_pushdown = true;
    
    create table t1 (pk int primary key, a int, b int) engine=ndb;
    create index ix1 on t1(b,a);
    
    insert into t1 values (0,10,10);
    insert into t1 values (1,10,10);
    insert into t1 values (2,10,10);
    
    select * from t1 as x join t1 as y on y.b=x.b;
    
    select * from t1 as x join t1 as y join t1 as z on x.a=y.b and y.a=z.b;

--- storage/ndb/include/ndbapi/NdbQueryBuilder.hpp
+++ storage/ndb/include/ndbapi/NdbQueryBuilder.hpp
@@ -32,6 +32,12 @@ class NdbQueryOptionsImpl;
 class NdbQueryOperandImpl;
 class NdbQueryOperationDefImpl;
 
+/** 
+ * This is the maximal number of table access operations there can be in a 
+ * single pushed join.
+ */
+static const uint NdbMaxPushedQueryOps = 32;
+
 /**
  * This is the API interface for building a (composite) query definition,
  * possibly existing of multiple operations linked together (aka 'joined')
