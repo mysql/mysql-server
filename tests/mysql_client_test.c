07f7fa48bf2ed2c52710c70e0a75dbf6f7f90bd3
Alfranio Correia <alfranio.correia@sun.com>
2010-07-13 18:09:01 +0100

    Post-merge fix.
    Reverted the patch from BUG#53907 over tests/mysql_client_test.c
    as it was null-merged into next-mr.
          
    revno: 2875.40.20 [merge]
    revision-id: alexey.kopytov@sun.com-20100527104212-cloj25taqgmyp2of
          
    The other part of the patch that changed sql/sql_parse.cc was
    reverted in the merge.
          
    revno: 3206 [merge]
    revision-id: alfranio.correia@sun.com-20100712112614-vbio0kgi3nh4gl16

--- tests/mysql_client_test.c
+++ tests/mysql_client_test.c
@@ -18092,45 +18092,6 @@ static void test_bug53371()
   myquery(rc);
 }
 
-
-static void test_bug53907()
-{
-  int rc;
-  char buf[] = "\x4test\x14../client_test_db/t1";
-
-  myheader("test_bug53907");
-
-  rc= mysql_query(mysql, "DROP TABLE IF EXISTS t1");
-  myquery(rc);
-  rc= mysql_query(mysql, "DROP DATABASE IF EXISTS bug53907");
-  myquery(rc);
-  rc= mysql_query(mysql, "DROP USER 'testbug'@localhost");
-
-  rc= mysql_query(mysql, "CREATE TABLE t1 (a INT)");
-  myquery(rc);
-  rc= mysql_query(mysql, "CREATE DATABASE bug53907");
-  myquery(rc);
-  rc= mysql_query(mysql, "GRANT SELECT ON bug53907.* to 'testbug'@localhost");
-  myquery(rc);
-
-  rc= mysql_change_user(mysql, "testbug", NULL, "bug53907");
-  myquery(rc);
-
-  rc= simple_command(mysql, COM_TABLE_DUMP, buf, sizeof(buf), 0);
-  fprintf(stderr, ">>>>>>>>> %d\n", mysql_errno(mysql));
-  DIE_UNLESS(mysql_errno(mysql) == 1103); /* ER_WRONG_TABLE_NAME */
-
-  rc= mysql_change_user(mysql, opt_user, opt_password, current_db);
-  myquery(rc);
-  rc= mysql_query(mysql, "DROP TABLE t1");
-  myquery(rc);
-  rc= mysql_query(mysql, "DROP DATABASE bug53907");
-  myquery(rc);
-  rc= mysql_query(mysql, "DROP USER 'testbug'@localhost");
-  myquery(rc);
-}
-
-
 /**
   Bug#42373: libmysql can mess a connection at connect
 */
@@ -18521,7 +18482,6 @@ static struct my_tests_st my_tests[]= {
   { "test_bug20023", test_bug20023 },
   { "test_bug45010", test_bug45010 },
   { "test_bug53371", test_bug53371 },
-  { "test_bug53907", test_bug53907 },
   { "test_bug31418", test_bug31418 },
   { "test_bug31669", test_bug31669 },
   { "test_bug28386", test_bug28386 },
