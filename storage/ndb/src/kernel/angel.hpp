a0d5249e20d60e5df32985f02782e4a6f735fdf1
Magnus Blåudd <magnus.blaudd@sun.com>
2010-08-16 19:06:06 +0200

    ndb  - pass name of the program as first argument to 'angel_run' so it knows    which path to use when respawning(without looking at global    variable my_progname)

--- storage/ndb/src/kernel/angel.hpp
+++ storage/ndb/src/kernel/angel.hpp
@@ -19,7 +19,8 @@
 #include <util/BaseString.hpp>
 
 void
-angel_run(const BaseString& original_args,
+angel_run(const char* progname,
+          const BaseString& original_args,
           const char* connect_str,
           int force_nodeid,
           const char* bind_address,
