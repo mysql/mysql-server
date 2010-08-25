ee9a56bb8d77e01f5015db249b89a1c2c34b63c5
Alexander Nozdrin <alik@ibmvm>
2010-08-25 13:17:45 +0400

    Revert patch for Bug#56120 temporarily.

--- sql/item.cc
+++ sql/item.cc
@@ -7510,14 +7510,9 @@ String *Item_cache_datetime::val_str(String *str)
         return NULL;
       if (cached_field_type == MYSQL_TYPE_TIME)
       {
-        longlong time= int_value;
-        set_zero_time(&ltime, MYSQL_TIMESTAMP_TIME);
-        if (time < 0)
-        {
-          time= -time;
-          ltime.neg= TRUE;
-        }
+        ulonglong time= int_value;
         DBUG_ASSERT(time <= TIME_MAX_VALUE);
+        set_zero_time(&ltime, MYSQL_TIMESTAMP_TIME);
         ltime.second= time % 100;
         time/= 100;
         ltime.minute= time % 100;
