94cfcbd9dffdfe5e38dd29c542738de85beaeb06
Igor Babaev <igor@askmonty.org>
2010-08-31 09:34:21 -0700

    The main patch for MWL#128: Implement Block Nested Loop Hash Join.

--- sql/handler.h
+++ sql/handler.h
@@ -1211,6 +1211,8 @@ typedef struct st_range_seq_if
   bool (*skip_index_tuple) (range_seq_t seq, char *range_info);
 } RANGE_SEQ_IF;
 
+typedef bool (*SKIP_INDEX_TUPLE_FUNC) (range_seq_t seq, char *range_info);
+
 class COST_VECT
 { 
 public:
