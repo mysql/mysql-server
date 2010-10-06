9b2559db9ac385e69f30b644088480ece8ee0e4f
Vasil Dimov <vasil.dimov@oracle.com>
2010-10-06 13:34:46 +0300

    Fix after the merge vasil.dimov@oracle.com-20101001151006-up615xj2i40r4clh
    This changeset fixes the failing tests:
    sys_vars.all_vars
    innodb.innodb_monitor
    sys_vars.innodb_page_hash_mutexes_basic
    
    It restores the code which was deleted by the merge above.

--- storage/innobase/handler/i_s.h
+++ storage/innobase/handler/i_s.h
@@ -33,5 +33,6 @@ extern struct st_mysql_plugin	i_s_innodb_cmp;
 extern struct st_mysql_plugin	i_s_innodb_cmp_reset;
 extern struct st_mysql_plugin	i_s_innodb_cmpmem;
 extern struct st_mysql_plugin	i_s_innodb_cmpmem_reset;
+extern struct st_mysql_plugin	i_s_innodb_metrics;
 
 #endif /* i_s_h */
