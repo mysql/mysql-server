856c6f18b76b7f353e57872ff49d97b603851afc
Sunny Bains <Sunny.Bains@Oracle.Com>
2010-10-13 09:35:02 +1100

    Add UNIV_SYNC_DEBUG code for read view list mutex.

--- storage/innobase/sync/sync0sync.c
+++ storage/innobase/sync/sync0sync.c
@@ -1176,6 +1176,7 @@ sync_thread_add_level(
 	case SYNC_SEARCH_SYS:
 	case SYNC_SEARCH_SYS_CONF:
 	case SYNC_THREADS:
+	case SYNC_READ_VIEW:
 	case SYNC_LOCK_SYS:
 	case SYNC_LOCK_WAIT_SYS:
 	case SYNC_TRX_SYS:
