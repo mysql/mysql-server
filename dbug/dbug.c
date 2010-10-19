a3357b55c2aa7e89c58291618ce99aa347d3f3f2
Tor Didriksen <tor.didriksen@oracle.com>
2010-10-19 09:06:48 +0200

    Bug#52172 post-push fix: init auto-variable to NULL

--- dbug/dbug.c
+++ dbug/dbug.c
@@ -2269,7 +2269,7 @@ static void dbug_flush(CODE_STATE *cs)
 
 void _db_flush_()
 {
-  CODE_STATE *cs;
+  CODE_STATE *cs= NULL;
   get_code_state_or_return;
   (void) fflush(cs->stack->out_file);
 }
