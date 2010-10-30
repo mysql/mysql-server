f69e942bb183e7295b3cc9a4d520e2e11d0084ef
Alexey Botchkov <holyfoot@mysql.com>
2010-10-30 09:48:12 +0500

    merging mistake fixed.

--- sql/spatial.cc
+++ sql/spatial.cc
@@ -1434,9 +1434,8 @@ uint Gis_multi_line_string::init_from_wkb(const char *wkb, uint len,
   const char *wkb_orig= wkb;
 
   if (len < 4 ||
-      (n_line_strings= wkb_get_uint(wkb, bo)< 1))
+      (n_line_strings= wkb_get_uint(wkb, bo))< 1)
     return 0;
-  ;
 
   if (res->reserve(4, 512))
     return 0;
