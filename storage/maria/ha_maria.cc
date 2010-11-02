432c9b20e90064e1b4a03a9c98ad12ee4fc0814a
Igor Babaev <igor@askmonty.org>
2010-11-02 10:07:46 -0700

    Fixed LP bug #669420. This bug in the MRR code for Maria engine caused wrong results when MRR was used to scan ranges for each record.
    Added test cases for bugs 669420 and 669423 (as a duplicate of 669420).

--- storage/maria/ha_maria.cc
+++ storage/maria/ha_maria.cc
@@ -2286,6 +2286,7 @@ int ha_maria::rnd_init(bool scan)
 
 int ha_maria::rnd_end()
 {
+  ds_mrr.dsmrr_close();
   /* Safe to call even if we don't have started a scan */
   maria_scan_end(file);
   return 0;
