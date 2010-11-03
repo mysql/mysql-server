9dda18668a5d55757090a43c159fbe10f7c4c49d
Jonas Oreland <jonas@mysql.com>
2010-11-03 10:40:26 +0100

    ndb - wl#5567 remove extra view that "It is decided that no extra summary view"

--- scripts/mysql_system_tables.sql
+++ scripts/mysql_system_tables.sql
@@ -161,11 +161,6 @@ PREPARE stmt FROM @str;
 EXECUTE stmt;
 DROP PREPARE stmt;
 
-SET @str=IF(@have_ndbinfo,'DROP VIEW IF EXISTS ndbinfo.diskpagebufferusage','SET @dummy = 0');
-PREPARE stmt FROM @str;
-EXECUTE stmt;
-DROP PREPARE stmt;
-
 # Drop any old lookup tables in ndbinfo
 SET @str=IF(@have_ndbinfo,'DROP TABLE IF EXISTS ndbinfo.blocks','SET @dummy = 0');
 PREPARE stmt FROM @str;
@@ -364,12 +359,6 @@ DROP PREPARE stmt;
 
 # ndbinfo.diskpagebuffer
 SET @str=IF(@have_ndbinfo,'CREATE OR REPLACE SQL SECURITY INVOKER VIEW `ndbinfo`.`diskpagebuffer` AS SELECT node_id, block_instance, pages_written, pages_written_lcp, pages_read, log_waits, page_requests_direct_return, page_requests_wait_queue, page_requests_wait_io FROM ndbinfo.ndb$diskpagebuffer','SET @dummy = 0');
-PREPARE stmt FROM @str;
-EXECUTE stmt;
-DROP PREPARE stmt;
-
-# ndbinfo.diskpagebufferusage
-SET @str=IF(@have_ndbinfo,'CREATE OR REPLACE SQL SECURITY INVOKER VIEW `ndbinfo`.`diskpagebufferusage` AS SELECT node_id,   SUM(pages_written) AS pages_written,   SUM(pages_written_lcp) AS pages_written_LCP,   SUM(pages_read) AS pages_read,   SUM(log_waits) AS log_waits,   (SUM(page_requests_direct_return) +    SUM(page_requests_wait_queue) +    SUM(page_requests_wait_io)   ) AS `page_requests_total =`,   SUM(page_requests_direct_return) AS `(PR_direct_return`,   SUM(page_requests_wait_queue) AS ` + PR_wait_queue`,   SUM(page_requests_wait_io)  AS ` + PR_wait_IO)` FROM ndbinfo.ndb$diskpagebuffer GROUP BY node_id','SET @dummy = 0');
 PREPARE stmt FROM @str;
 EXECUTE stmt;
 DROP PREPARE stmt;
