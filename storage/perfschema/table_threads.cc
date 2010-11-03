4e1678daca86af018fb4921a15e87ec28dbaacd1
Marc Alff <marc.alff@oracle.com>
2010-11-03 16:42:33 +0100

    Bug#57609 performance_schema does not work with lower_case_table_names
    Before this fix, the performance schema tables were defined in UPPERCASE.
    
    This was incompatible with the lowercase_table_names option, and caused
    issues with the install / upgrade process, when changing the lower case
    table names setting *after* the install or upgrade.
    
    With this fix, all performance schema tables are exposed with lowercase names.
    As a result, the name of the performance schema table is always lowercase,
    no matter how / if / when the lowercase_table_names setting if changed.

--- storage/perfschema/table_threads.cc
+++ storage/perfschema/table_threads.cc
@@ -52,7 +52,7 @@ table_threads::m_field_def=
 PFS_engine_table_share
 table_threads::m_share=
 {
-  { C_STRING_WITH_LEN("THREADS") },
+  { C_STRING_WITH_LEN("threads") },
   &pfs_readonly_acl,
   &table_threads::create,
   NULL, /* write_row */
