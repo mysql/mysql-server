bffe1f7e87ffab69ad1e4e2612962e50b73d4b84
Sergey Glukhov <sergey.glukhov@oracle.com>
2010-11-08 13:34:27 +0300

    Fix for bug #54575: crash when joining tables with unique set column(backport from 5.1)
    Problem: a flaw (derefencing a NULL pointer) in the LIKE optimization
    code may lead to a server crash in some rare cases.
    
    Fix: check the pointer before its dereferencing.

--- sql/item_cmpfunc.cc
+++ sql/item_cmpfunc.cc
@@ -4220,13 +4220,14 @@ Item_func::optimize_type Item_func_like::select_optimize() const
   if (args[1]->const_item())
   {
     String* res2= args[1]->val_str((String *)&tmp_value2);
+    const char *ptr2;
 
-    if (!res2)
+    if (!res2 || !(ptr2= res2->ptr()))
       return OPTIMIZE_NONE;
 
-    if (*res2->ptr() != wild_many)
+    if (*ptr2 != wild_many)
     {
-      if (args[0]->result_type() != STRING_RESULT || *res2->ptr() != wild_one)
+      if (args[0]->result_type() != STRING_RESULT || *ptr2 != wild_one)
 	return OPTIMIZE_OP;
     }
   }
