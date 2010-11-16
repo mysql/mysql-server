c8ea88f3d1fc03cb1d3ab63b3392c74857df0361
Rich Prohaska <prohaska@tokutek.com>
2010-11-16 18:25:28 +0000

    #2843 remove junk refs[t:2843]
    git-svn-id: file:///svn/toku/tokudb@25670 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/ydb-le-cursor.h
+++
@@ -1,6 +0,0 @@
-#include "le-cursor.h"
-
-static inline int le_cursor_create_db_txn(LE_CURSOR *le_cursor_result, DB *db, DB_TXN *txn) {
-    return le_cursor_create(le_cursor_result, db_struct_i(db)->brt, db_txn_struct_i(txn)->tokutxn);
-}
-
