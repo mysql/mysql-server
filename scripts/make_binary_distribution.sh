16a576cf5ed6460892483d3635883a2785fbe47a
Magnus Blåudd <magnus.blaudd@oracle.com>
2010-11-29 11:12:31 +0100

    BUG#58503 Remove Cluster GPL EXCEPTIONS-CLIENT file  - cherry pick patches for bug#58340 from 5.1-security     - revno:3452.20.19, the original patch     - revno:3530, a follow-up patch     - revno:3532, fix formatting issues in README

--- scripts/make_binary_distribution.sh
+++ scripts/make_binary_distribution.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 # Copyright (C) 2000-2006 MySQL AB
-#  All rights reserved. Use is subject to license terms.
-#
+# 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; version 2 of the License.
-#
+# 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 ##############################################################################
 #
@@ -303,7 +302,9 @@ if [ x"$BASE_SYSTEM" != x"netware" ] ; then
 
   # Copy readme and license files
   cp README Docs/INSTALL-BINARY  $DEST/
-  if [ -f LICENSE.mysql ] ; then
+  if [ -f COPYING ] ; then
+    cp COPYING $DEST/
+  elif [ -f LICENSE.mysql ] ; then
     cp LICENSE.mysql $DEST/
   else
     echo "ERROR: no license files found"
