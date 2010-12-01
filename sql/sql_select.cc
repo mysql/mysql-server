b4b58b7f0d828120bb64c2279f789c07fb647b1b
Ole John Aske <ole.john.aske@oracle.com>
2010-12-01 13:31:27 +0100

    SPJ-scan-scan: Cherry picked fix for bug#58628 Incorrect result for 'WHERE NULL NOT IN (<subquery>)
          create_ref_for_key() allowed any constant part of a REF key to be evaluated
          and stored into the 'key_buff' during ::optimize(). These 'store_key*' was
          *not* kept in ref.key_copy[] as they where constant and we assumed we
          would not have to reevaluate them during JOIN::exec()
    
          However, during execute NULL values in REF key has to be detected as they may
          need special attention - as in 'Full scan on NULL key'. This is done by
          subselect_uniquesubquery_engine::copy_ref_key() which check if any keyparts
          evaluated to a NULL-value.
    
          As we didn't keep a store_key for a constant value, a NULL-constant was not detected
          by subselect_uniquesubquery_engine::copy_ref_key() !
    
          This fixs modifies create_ref_for_key() to check if a NULL-value constant
          was produced - In these cases it keeps the store_key, which then will be
          reevaluated in JOIN::exec() and trigger correct handling of NULL-valued keys.

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -5874,22 +5874,36 @@ static bool create_ref_for_key(JOIN *join, JOIN_TAB *j, KEYUSE *org_keyuse,
       if (keyuse->null_rejecting) 
         j->ref.null_rejecting |= 1 << i;
       keyuse_uses_no_tables= keyuse_uses_no_tables && !keyuse->used_tables;
-      if (!keyuse->used_tables &&
-	  !(join->select_options & SELECT_DESCRIBE))
-      {					// Compare against constant
-	store_key_item tmp(thd, keyinfo->key_part[i].field,
-                           key_buff + maybe_null,
-                           maybe_null ?  key_buff : 0,
-                           keyinfo->key_part[i].length, keyuse->val);
-	if (thd->is_fatal_error)
-	  DBUG_RETURN(TRUE);
-	tmp.copy();
+
+      store_key* key= get_store_key(thd,
+				    keyuse,join->const_table_map,
+				    &keyinfo->key_part[i],
+				    key_buff, maybe_null);
+      if (unlikely(!key || thd->is_fatal_error))
+        DBUG_RETURN(TRUE);
+
+      if (keyuse->used_tables || join->select_options & SELECT_DESCRIBE)
+      {
+	*ref_key++= key; // Always evaluate/explain in JOIN::exec()
       }
       else
-	*ref_key++= get_store_key(thd,
-				  keyuse,join->const_table_map,
-				  &keyinfo->key_part[i],
-				  key_buff, maybe_null);
+      {
+        /* key is constant, copy value now and possibly skip it while ::exec() */
+        enum store_key::store_key_result result= key->copy();
+
+        /* Depending on 'result' it should be reevaluated in ::exec(), if either:
+         *  1) '::copy()' failed, in case we reevaluate - and refail in 
+         *       JOIN::exec() where the error can be handled.
+         *  2)  Constant evaluated to NULL value which we might need to 
+         *      handle as a special case during JOIN::exec()
+         *      (As in : 'Full scan on NULL key')
+         */
+        if (result!=store_key::STORE_KEY_OK  ||    // 1)
+            key->null_key)                         // 2)
+        {
+	  *ref_key++= key;  // Reevaluate in JOIN::exec() 
+        }
+      }
       /*
 	Remember if we are going to use REF_OR_NULL
 	But only if field _really_ can be null i.e. we force JT_REF
