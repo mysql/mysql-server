bad28ea48c0cd6786001916df04ae78d92d9e2d4
Magne Mahre <magne.mahre@oracle.com>
2011-01-04 16:23:59 +0100

    Remove configuration preprocessor symbols 'THREAD' and 'THREAD_SAFE_CLIENT'.
    As of MySQL 5.5, we no longer support non-threaded
    builds.   This patch removes all references to the
    obsolete THREAD and THREAD_SAFE_CLIENT preprocessor
    symbols.  These were used to distinguish between
    threaded and non-threaded builds.

--- sql/filesort.cc
+++ sql/filesort.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2000, 2010, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2000, 2011, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -35,10 +35,6 @@
 #include "bounded_queue.h"
 #include "filesort_utils.h"
 #include "sql_select.h"
-
-#ifndef THREAD
-#define SKIP_DBUG_IN_FILESORT
-#endif
 
 #ifdef HAVE_EXPLICIT_TEMPLATE_INSTANTIATION
 template class Bounded_queue<uchar, uchar>;
