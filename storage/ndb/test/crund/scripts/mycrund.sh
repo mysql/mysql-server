ba3e3fb30f96d9e9146af03d117531f356852b2a
Martin Zaun <martin.zaun@oracle.com>
2011-01-11 08:11:00 -0800

    crund - minor changes

--- storage/ndb/test/crund/scripts/mycrund.sh
+++ storage/ndb/test/crund/scripts/mycrund.sh
@@ -2,8 +2,10 @@
 
 #for f in run.ndbapi.opt run.ndbjtie.opt run.clusterj.opt run.mysql.opt run.openjpa.mysql.opt run.openjpa.clusterj.opt ; do
 #for f in run.ndbapi.opt run.ndbjtie.opt run.clusterj.opt run.mysql.opt ; do
-for f in run.ndbapi.opt ; do
+#for f in run.ndbjtie.opt ; do
+#for f in run.ndbapi.opt ; do
+for f in run.clusterj.opt ; do
   echo testing $f
-  ./mytest.sh ant $f
+  ./mycrundjava.sh $f
   mv -v results/xxx  results/xxx_$f
 done
