129b460e6578df2290849d69ed65c6f3a9d91d48
Tatiana Azundris Nurnberg <azundris@sun.com>
2011-01-20 11:42:03 +0000

    Bug#48387: Log the start and stop of init_file execution
    Parenthesizes init-file results, if any, with the file's name
    in the log so it's more obvious where the statements were read
    from.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -4864,11 +4864,17 @@ static bool read_init_file(char *file_name)
   MYSQL_FILE *file;
   DBUG_ENTER("read_init_file");
   DBUG_PRINT("enter",("name: %s",file_name));
+
+  sql_print_information("Execution of init_file \'%s\' started.", file_name);
+
   if (!(file= mysql_file_fopen(key_file_init, file_name,
                                O_RDONLY, MYF(MY_WME))))
     DBUG_RETURN(TRUE);
   bootstrap(file);
   mysql_file_fclose(file, MYF(MY_WME));
+
+  sql_print_information("Execution of init_file \'%s\' ended.", file_name);
+
   DBUG_RETURN(FALSE);
 }
 
