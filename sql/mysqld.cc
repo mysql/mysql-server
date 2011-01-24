ab02d3463369521a3b277db01087fea11ddec4f4
Mikael Ronstrom <mikael.ronstrom@oracle.com>
2011-01-24 14:57:07 +0100

    BUG#59549, Fix compiler errors on Windows, step 5

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -450,7 +450,7 @@ uint lower_case_table_names;
 ulong tc_heuristic_recover= 0;
 uint volatile thread_count;
 int32 thread_running;
-ulong thread_created;
+MYSQL_PLUGIN_IMPORT ulong thread_created;
 ulong back_log, connect_timeout, concurrency, server_id;
 ulong table_cache_size, table_def_size;
 ulong what_to_log;
@@ -477,7 +477,8 @@ ulong delayed_insert_errors,flush_time;
 ulong specialflag=0;
 ulong binlog_cache_use= 0, binlog_cache_disk_use= 0;
 ulong binlog_stmt_cache_use= 0, binlog_stmt_cache_disk_use= 0;
-ulong max_connections, max_connect_errors;
+MYSQL_PLUGIN_IMPORT ulong max_connections;
+ulong max_connect_errors;
 /**
   Limit of the total number of prepared statements in the server.
   Is necessary to protect the server against out-of-memory attacks.
@@ -580,7 +581,7 @@ Le_creator le_creator;
 MYSQL_FILE *bootstrap_file;
 int bootstrap_error;
 
-I_List<THD> threads;
+MYSQL_PLUGIN_IMPORT I_List<THD> threads;
 Rpl_filter* rpl_filter;
 Rpl_filter* binlog_filter;
 
@@ -629,9 +630,9 @@ mysql_mutex_t LOCK_des_key_file;
 #endif
 mysql_rwlock_t LOCK_grant, LOCK_sys_init_connect, LOCK_sys_init_slave;
 mysql_rwlock_t LOCK_system_variables_hash;
-mysql_cond_t COND_thread_count;
+MYSQL_PLUGIN_IMPORT mysql_cond_t COND_thread_count;
 pthread_t signal_thread;
-pthread_attr_t connection_attrib;
+MYSQL_PLUGIN_IMPORT pthread_attr_t connection_attrib;
 mysql_mutex_t LOCK_server_started;
 mysql_cond_t COND_server_started;
 
@@ -7792,7 +7793,9 @@ static PSI_cond_info all_server_conds[]=
 
 PSI_thread_key key_thread_bootstrap, key_thread_delayed_insert,
   key_thread_handle_manager, key_thread_main,
-  key_thread_one_connection, key_thread_signal_hand;
+  key_thread_signal_hand;
+
+MYSQL_PLUGIN_IMPORT PSI_thread_key key_thread_one_connection;
 
 static PSI_thread_info all_server_threads[]=
 {
