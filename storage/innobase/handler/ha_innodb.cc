af8d7d6b451e676145e4799d63c27b6e33bddc2b
Marko Mäkelä <marko.makela@oracle.com>
2011-01-31 13:56:15 +0200

    Bug#59483 Crash on INSERT/REPLACE in rec_convert_dtuple_to_rec_comp with ICP on
    ha_innobase::reset_template(): When an index condition was used,
    invalidate prebuilt->mysql_template so that it will be rebuilt
    by write_row().

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -2266,9 +2266,14 @@ ha_innobase::reset_template(void)
 
 	prebuilt->keep_other_fields_on_keyread = 0;
 	prebuilt->read_just_key = 0;
-	/* Reset index condition pushdown state */
-	prebuilt->idx_cond = NULL;
-	prebuilt->idx_cond_n_cols = 0;
+	/* Reset index condition pushdown state. */
+	if (prebuilt->idx_cond) {
+		prebuilt->idx_cond = NULL;
+		prebuilt->idx_cond_n_cols = 0;
+		/* Invalidate prebuilt->mysql_template
+		in ha_innobase::write_row(). */
+		prebuilt->template_type = ROW_MYSQL_NO_TEMPLATE;
+	}
 }
 
 /*****************************************************************//**
