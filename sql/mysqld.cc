6c777a622001b291190c89bfcc39627d055a7839
Dmitry Shulga <Dmitry.Shulga@oracle.com>
2011-02-04 10:47:46 +0600

    Fixed bug#58026 - massive recursion and crash in regular expression handling.
    The problem was that parsing of nested regular expression involved
    recursive calls. Such recursion didn't take into account the amount of
    available stack space, which ended up leading to stack overflow crashes.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -3034,6 +3034,19 @@ sizeof(load_default_groups)/sizeof(load_default_groups[0]);
 #endif
 
 
+#ifndef EMBEDDED_LIBRARY
+static
+int
+check_enough_stack_size()
+{
+  uchar stack_top;
+
+  return check_stack_overrun(current_thd, STACK_MIN_SIZE,
+                             &stack_top);
+}
+#endif
+
+
 /**
   Initialize one of the global date/time format variables.
 
@@ -3415,7 +3428,11 @@ static int init_common_variables(const char *conf_file_name, int argc,
 #endif
   mysys_uses_curses=0;
 #ifdef USE_REGEX
-  my_regex_init(&my_charset_latin1);
+#ifndef EMBEDDED_LIBRARY
+  my_regex_init(&my_charset_latin1, check_enough_stack_size);
+#else
+  my_regex_init(&my_charset_latin1, NULL);
+#endif
 #endif
   /*
     Process a comma-separated character set list and choose
