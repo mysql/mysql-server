0d0030ae27415a0e5da4942459787d826720303a
MySQL Build Team <build@mysql.com>
2011-02-09 21:21:06 +0100

    Backport into build-201102032246-5.1.52sp1
    > ------------------------------------------------------------
    > revno: 3452.17.1
    > revision-id: mattias.jonsson@oracle.com-20101005125751-ds92svhhb13ds8ri
    > parent: sergey.glukhov@sun.com-20101004085126-ia6veky4w6j87i43
    > committer: Mattias Jonsson <mattias.jonsson@oracle.com>
    > branch nick: b55091-51-security
    > timestamp: Tue 2010-10-05 14:57:51 +0200
    > message:
    >   Bug#55091: Server crashes on ADD PARTITION after a failed attempt
    >   
    >   In case of failure in ALTER ... PARTITION under LOCK TABLE
    >   the server could crash, due to it had modified the locked
    >   table object, which was not reverted in case of failure,
    >   resulting in a bad table definition used after the failed
    >   command.
    >   
    >   Solved by always closing the LOCKED TABLE, even in case
    >   of error.
    >   
    >   Note: this is a 5.1-only fix, bug#56172 fixed it in 5.5+

--- sql/sql_partition.cc
+++ sql/sql_partition.cc
@@ -5935,6 +5935,12 @@ static void alter_partition_lock_handling(ALTER_PARTITION_PARAM_TYPE *lpt)
   if (lpt->thd->locked_tables)
   {
     /*
+      Close the table if open, to remove/destroy the already altered
+      table->part_info object, so that it is not reused.
+    */
+    if (lpt->table->db_stat)
+      abort_and_upgrade_lock_and_close_table(lpt);
+    /*
       When we have the table locked, it is necessary to reopen the table
       since all table objects were closed and removed as part of the
       ALTER TABLE of partitioning structure.
@@ -6436,7 +6442,20 @@ uint fast_alter_partition_table(THD *thd, TABLE *table,
                                  table, table_list, FALSE, NULL,
                                  written_bin_log));
 err:
-  close_thread_tables(thd);
+  if (thd->locked_tables)
+  {
+    /*
+      table->part_info was altered in prep_alter_part_table and must be
+      destroyed and recreated, since otherwise it will be reused, since
+      we are under LOCK TABLE.
+    */
+    alter_partition_lock_handling(lpt);
+  }
+  else
+  {
+    /* Force the table to be closed to avoid reuse of the table->part_info */
+    close_thread_tables(thd);
+  }
   DBUG_RETURN(TRUE);
 }
 #endif
