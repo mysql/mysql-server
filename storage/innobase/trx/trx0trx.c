89d09c37644ed6fa7341db22b4c9040dddc2367e
Sunny Bains <Sunny.Bains@Oracle.Com>
2011-02-10 12:56:07 +1100

    Make trx_sys_t::latest_rseg static and local to trx_assign_rseg(). It was only being used in this function. There really is no need to protect this variable using an X trx_sys_t::lock. We don't care about overflow because all the work is done using an auto variable i in trx_assign_rseg() and doing a modulus on i ensures that we will not overflow the bounds of the rseg array.
    Reading/Incrementing latest_rseg doesn't need to atomic either. This change
    should help in further reducing the contention on the trx_sys_t::lock.
    
    rb://594 Approved by Jimmy Yang.

--- storage/innobase/trx/trx0trx.c
+++ storage/innobase/trx/trx0trx.c
@@ -595,7 +595,6 @@ trx_lists_init_at_db_start(void)
 
 /******************************************************************//**
 Assigns a rollback segment to a transaction in a round-robin fashion.
-Skips the SYSTEM rollback segment if another is available.
 @return	assigned rollback segment */
 UNIV_INLINE
 trx_rseg_t*
@@ -604,11 +603,12 @@ trx_assign_rseg(void)
 {
 	ulint		i;
 	trx_rseg_t*	rseg;
+	static ulint	latest_rseg = 0;
 
-	/* This breaks true round robin but that should be OK.
-	Our aim is to reduce the contention of the trx_sys_t::lock.  */
-	i = trx_sys->latest_rseg;
-	i %= TRX_SYS_N_RSEGS;
+	/* This breaks true round robin but that should be OK. */
+
+	i = latest_rseg++;
+        i %= TRX_SYS_N_RSEGS;
 
 	do {
 		rseg = trx_sys->rseg_array[i];
@@ -618,13 +618,13 @@ trx_assign_rseg(void)
 
 	} while (rseg == NULL);
 
+#ifdef UNIV_DEBUG
 	rw_lock_x_lock(&trx_sys->lock);
-
-	trx_sys->latest_rseg = i % TRX_SYS_N_RSEGS;
 
 	ut_ad(trx_sys_validate_trx_list());
 
 	rw_lock_x_unlock(&trx_sys->lock);
+#endif /* UNIV_DEBUG */
 
 	return(rseg);
 }
