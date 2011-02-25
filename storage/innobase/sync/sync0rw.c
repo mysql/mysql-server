cc158c963a5990938c3f24aad52a74e5fb17c6fd
Jimmy Yang <jimmy.yang@oracle.com>
2011-02-25 03:21:02 -0800

    Include "ha_prototypes.h" to sync/sync0rw.c for innobase_basename().

--- storage/innobase/sync/sync0rw.c
+++ storage/innobase/sync/sync0rw.c
@@ -39,6 +39,7 @@ Created 9/11/1995 Heikki Tuuri
 #include "mem0mem.h"
 #include "srv0srv.h"
 #include "os0sync.h" /* for INNODB_RW_LOCKS_USE_ATOMICS */
+#include "ha_prototypes.h"
 
 /*
 	IMPLEMENTATION OF THE RW_LOCK
