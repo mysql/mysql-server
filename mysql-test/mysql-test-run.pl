d8100a7cb2910b52d77ad5a562241d8403ac112b
Bjorn Munch <bjorn.munch@oracle.com>
2011-02-25 14:51:25 +0100

    mtr: ignore not finding ctest if in PB2

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -5684,9 +5684,10 @@ sub run_ctest() {
   $ctest_vs= "-C $opt_vs_config" if $opt_vs_config;
 
   # Also silently ignore if we don't have ctest and didn't insist
+  # Special override: also ignore in Pushbuild, some platforms may not have it
   # Now, run ctest and collect output
   my $ctest_out= `ctest $ctest_vs 2>&1`;
-  if ($? == $no_ctest && $opt_ctest == -1) {
+  if ($? == $no_ctest && $opt_ctest == -1 && ! defined $ENV{PB2WORKDIR}) {
     chdir($olddir);
     return;
   }
