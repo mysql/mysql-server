7946f1ba2717235b3c2b786d9fe0d3462e25c0c2
Dmitry Shulga <Dmitry.Shulga@oracle.com>
2011-03-16 00:08:59 +0600

    Fixed a bug in manual merge (dmitry.shulga@oracle.com-20110315130322-2a0zhequpu9q8dyt)

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -1272,13 +1272,6 @@ static Sys_var_ulong Sys_max_length_for_sort_data(
        SESSION_VAR(max_length_for_sort_data), CMD_LINE(REQUIRED_ARG),
        VALID_RANGE(4, 8192*1024L), DEFAULT(1024), BLOCK_SIZE(1));
 
-static Sys_var_harows Sys_sql_max_join_size(
-       "sql_max_join_size", "Alias for max_join_size",
-       SESSION_VAR(max_join_size), NO_CMD_LINE,
-       VALID_RANGE(1, HA_POS_ERROR), DEFAULT(HA_POS_ERROR), BLOCK_SIZE(1),
-       NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(0),
-       ON_UPDATE(fix_max_join_size), DEPRECATED(70000, 0));
-
 static Sys_var_ulong Sys_max_long_data_size(
        "max_long_data_size",
        "The maximum BLOB length to send to server from "
