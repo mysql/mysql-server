967beb46a66c2b5d9860dfc0980e02209a9e273e
Alfranio Correia <alfranio.correia@oracle.com>
2011-03-16 16:38:30 +0000

    BUG#11877568 - RPL_SEMI_SYNC FAILS IN PB
    Currently, rpl_semi_sync is failing in PB due to the warning message:
    
      "Slave SQL: slave SQL thread is being stopped in the middle of "
      "applying of a group having updated a non-transaction table; "
      "waiting for the group completion ..."
    
    The problem started happening after the fix for BUG#11762407 what was
    automatically suppressing some warning messages.
    
    To fix the current issue, we suppress the aforementioned warning message
    and exploit the opportunity to make the sentence clearer.

--- sql/slave.cc
+++ sql/slave.cc
@@ -903,18 +903,18 @@ static bool sql_slave_killed(THD* thd, Relay_log_info* rli)
         && rli->is_in_group())
     {
       char msg_stopped[]=
-        "... The slave SQL is stopped, leaving the current group "
-        "of events unfinished with a non-transaction table changed. "
-        "If the group consists solely of Row-based events, you can try "
-        "restarting the slave with --slave-exec-mode=IDEMPOTENT, which "
+        "... Slave SQL Thread stopped with incomplete event group "
+        "having non-transactional changes. "
+        "If the group consists solely of row-based events, you can try "
+        "to restart the slave with --slave-exec-mode=IDEMPOTENT, which "
         "ignores duplicate key, key not found, and similar errors (see "
         "documentation for details).";
 
       if (rli->abort_slave)
       {
-        DBUG_PRINT("info", ("Slave SQL thread is being stopped in the middle of"
-                            " a group having updated a non-trans table, giving"
-                            " it some grace period"));
+        DBUG_PRINT("info", ("Request to stop slave SQL Thread received while "
+                            "applying a group that has non-transactional "
+                            "changes; waiting for completion of the group ... "));
 
         /*
           Slave sql thread shutdown in face of unfinished group modified 
@@ -938,9 +938,9 @@ static bool sql_slave_killed(THD* thd, Relay_log_info* rli)
         if (ret == 0)
         {
           rli->report(WARNING_LEVEL, 0,
-                      "slave SQL thread is being stopped in the middle "
-                      "of applying of a group having updated a non-transaction "
-                      "table; waiting for the group completion ... ");
+                      "Request to stop slave SQL Thread received while "
+                      "applying a group that has non-transactional "
+                      "changes; waiting for completion of the group ... ");
         }
         else
         {
