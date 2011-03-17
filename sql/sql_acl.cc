45df7799224d4fe4cbfcd438a55fa4e6abab64fb
Georgi Kodinov <Georgi.Kodinov@Oracle.com>
2011-03-17 13:29:59 +0200

    Fixed a post-merge embedded compilation error

--- sql/sql_acl.cc
+++ sql/sql_acl.cc
@@ -8397,6 +8397,7 @@ static bool parse_com_change_user_packet(MPVIO_EXT *mpvio, uint packet_length)
   DBUG_RETURN (0);
 }
 
+#ifndef EMBEDDED_LIBRARY
 /**
   Get a null character terminated string from a user-supplied buffer.
 
@@ -8481,6 +8482,7 @@ char *get_length_encoded_string(char **buffer,
   *buffer+= *string_length + 1;
   return str;
 }
+#endif
 
 
 /* the packet format is described in send_client_reply_packet() */
