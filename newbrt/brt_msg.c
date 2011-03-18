f428b6690019eb6c76e09193a882bf478d8c133e
Barry Perlman <barry@tokutek.com>
2011-03-18 15:24:35 +0000

    [t:3193] Closes #3193 Delete obsolete fifo_msg code.
    git-svn-id: file:///svn/toku/tokudb@29573 c7de825b-a66e-492c-adef-691d508d4ae1

--- newbrt/brt_msg.c
+++ newbrt/brt_msg.c
@@ -8,38 +8,6 @@
 #include "xids.h"
 #include "brt_msg.h"
 
-#if 0
-
-//BRT_MSG internals are in host order
-//XIDS are not 'internal' to BRT_MSG
-
-void
-brt_msg_from_dbts(BRT_MSG brt_msg,
-                  DBT *key, DBT *val,
-                  XIDS xids, enum brt_msg_type type) {
-    brt_msg->u.id.key = key;
-    brt_msg->u.id.val = val;
-    brt_msg->xids   = xids;
-    brt_msg->type   = type;
-}
-
-#endif
-
-//No conversion (from disk to host) is necessary
-//Accessor functions for fifo return host order bytes.
-#if 0
-#include "fifo_msg.h"
-void
-brt_msg_from_fifo_msg(BRT_MSG brt_msg, FIFO_MSG fifo_msg) {
-    brt_msg->keylen = fifo_msg_get_keylen(fifo_msg);
-    brt_msg->vallen = fifo_msg_get_vallen(fifo_msg);
-    brt_msg->vallen = fifo_msg_get_vallen(fifo_msg);
-    brt_msg->key    = fifo_msg_get_key(fifo_msg);
-    brt_msg->val    = fifo_msg_get_val(fifo_msg);
-    brt_msg->xids   = fifo_msg_get_xids(fifo_msg);
-    brt_msg->type   = fifo_msg_get_type(fifo_msg);
-}
-#endif
 
 u_int32_t 
 brt_msg_get_keylen(BRT_MSG brt_msg) {
