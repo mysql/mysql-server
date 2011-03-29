74bb936db17d151c592e80585f6fb3cd03379a62
Sergey Petrunya <psergey@askmonty.org>
2011-03-29 10:08:23 +0400

    optimize_wo_join_buffering(): Put saving/restoring of cur_sj_inner_tables back,   together with explanation why we need it.

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -11541,6 +11541,20 @@ void optimize_wo_join_buffering(JOIN *join, uint first_tab, uint last_tab,
   *outer_rec_count= rec_count;
   for (i= first_tab; i <= last_tab; i++)
     reopt_remaining_tables |= join->positions[i].table->table->map;
+  
+  /*
+    best_access_path() optimization depends on the value of 
+    join->cur_sj_inner_tables. Our goal in this function is to do a
+    re-optimization with disabled join buffering, but no other changes.
+    In order to achieve this, cur_sj_inner_tables needs have the same 
+    value it had during the original invocations of best_access_path. 
+
+    We know that this function, optimize_wo_join_buffering() is called to
+    re-optimize semi-join join order range, which allows to conclude that 
+    the "original" value of cur_sj_inner_tables was 0.
+  */
+  table_map save_cur_sj_inner_tables= join->cur_sj_inner_tables;
+  join->cur_sj_inner_tables= 0;
 
   for (i= first_tab; i <= last_tab; i++)
   {
@@ -11567,6 +11581,7 @@ void optimize_wo_join_buffering(JOIN *join, uint first_tab, uint last_tab,
     if (!rs->emb_sj_nest)
       *outer_rec_count *= pos.records_read;
   }
+  join->cur_sj_inner_tables= save_cur_sj_inner_tables;
 
   *reopt_cost= cost;
 }
