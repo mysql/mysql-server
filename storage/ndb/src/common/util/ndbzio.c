e5a4cc4cb69ef19eff1faac4ddb22e474c0d6588
Magnus Blåudd <magnus.blaudd@oracle.com>
2011-04-01 14:10:59 +0200

    ndbzio  - fix copyright headers so that Oracle is on top, then the original   implementors orignal copyright comments below.  - Add comment describing what ndbzio is.

--- storage/ndb/src/common/util/ndbzio.c
+++ storage/ndb/src/common/util/ndbzio.c
@@ -1,4 +1,27 @@
 /*
+   Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+/*
+  ndbzio is a modified version of azio with a cleaner interface.
+
+    - Magnus Blåudd
+*/
+
+/*
   azio is a modified version of gzio. It  makes use of mysys and removes mallocs.
     -Brian Aker
 */
@@ -11,7 +34,7 @@
  */
 
 /* gzio.c -- IO on .gz files
- * Copyright (c) 2007, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (C) 1995-2005 Jean-loup Gailly.
  * For conditions of distribution and use, see copyright notice in zlib.h
  *
  */
