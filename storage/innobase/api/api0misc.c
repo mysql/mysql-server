c6754fc1f1f0e4aa029a52cb720ae5310f332bd9
Calvin Sun <calvin.sun@oracle.com>
2011-04-03 00:52:40 -0500

    mysql-5.6-labs-innodb-memcached: fix copyright information.

--- storage/innobase/api/api0misc.c
+++ storage/innobase/api/api0misc.c
@@ -1,21 +1,21 @@
-/***********************************************************************
-Copyright (c) 2008 Innobase Oy. All rights reserved.
-Copyright (c) 2008 Oracle. All rights reserved.
+/*****************************************************************************
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; version 2 of the License.
+Copyright (c) 2008, 2011, MySQL AB & Innobase Oy. All Rights Reserved.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; version 2 of the License.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-************************************************************************/
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place, Suite 330, Boston, MA 02111-1307 USA
+
+*****************************************************************************/
+
 #include <errno.h>
 
 #ifdef HAVE_UNISTD_H
