57bbceb373b7f47905308bdc22840886aa04913d
Calvin Sun <calvin.sun@oracle.com>
2011-04-03 00:52:40 -0500

    mysql-5.6-labs-innodb-memcached: fix copyright information.

--- storage/innobase/include/api0misc.h
+++ storage/innobase/include/api0misc.h
@@ -1,9 +1,24 @@
-/******************************************************
-Interface between Innobase and client. This file contains
-the functions that don't have a proper home yet.
+/*****************************************************************************
 
-(c) 2008 Oracle Corpn./Innobase Oy
-*******************************************************/
+Copyright (c) 2008, 2011, MySQL AB & Innobase Oy. All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place, Suite 330, Boston, MA 02111-1307 USA
+
+*****************************************************************************/
+
+/**********************************************************************
+This file contains the functions that don't have a proper home yet.
+************************************************************************/
 
 #include "univ.i"
 #include "os0file.h"
