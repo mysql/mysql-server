2b8312e50c47aa7f186f8e4df5ecd905610273e8
Marko Mäkelä <marko.makela@oracle.com>
2011-04-06 09:22:36 +0300

    Enable __attribute__((cold)) only for GCC 4.3 and later. This attribute was introduced in bzr revision-id marko.makela@oracle.com-20110405073758-b8y733yvkqum940i and caused older GCC versions to emit warnings.

--- storage/innobase/include/ut0dbg.h
+++ storage/innobase/include/ut0dbg.h
@@ -53,7 +53,7 @@ ut_dbg_assertion_failed(
 	const char*	expr,	/*!< in: the failed assertion */
 	const char*	file,	/*!< in: source file containing the assertion */
 	ulint		line)	/*!< in: line number of the assertion */
-	__attribute__((nonnull(2), cold));
+	UNIV_COLD __attribute__((nonnull(2)));
 
 #if defined(__WIN__) || defined(__INTEL_COMPILER)
 # undef UT_DBG_USE_ABORT
