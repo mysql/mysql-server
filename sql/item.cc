e270c0996f0d053ce5f0bdcde9748a5b8d876efc
Dmitry Shulga <Dmitry.Shulga@oracle.com>
2011-04-11 17:28:36 +0700

    follow-up for patch for Bug#11764168: 56976: SEVERE DENIAL OF SERVICE IN PREPARED STATEMENTS
    Removed using of max-long-data-size variable since
    it was marked as deprecated. Use max-allowed-packet instead.
    
    Variable max-long-data-size was introduced as temporal solution 
    in order to make possible send of BLOB with size greater than 1GB.

--- sql/item.cc
+++ sql/item.cc
@@ -2980,12 +2980,12 @@ bool Item_param::set_longdata(const char *str, ulong length)
     (here), and first have to concatenate all pieces together,
     write query to the binary log and only then perform conversion.
   */
-  if (str_value.length() + length > max_long_data_size)
+  if (str_value.length() + length > current_thd->variables.max_allowed_packet)
   {
     my_message(ER_UNKNOWN_ERROR,
                "Parameter of prepared statement which is set through "
                "mysql_send_long_data() is longer than "
-               "'max_long_data_size' bytes",
+               "'max_allowed_packet' bytes",
                MYF(0));
     DBUG_RETURN(true);
   }
