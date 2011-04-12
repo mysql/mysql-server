b91d3a9a61e1a2db0528975fd86fe67bc9a2af38
Magnus Blåudd <magnus.blaudd@sun.com>
2011-04-12 11:33:16 -0400

    ndb  - mark set_range_data, set_list_data and set_up_partition_info as being const

--- sql/ha_ndbcluster.h
+++ sql/ha_ndbcluster.h
@@ -642,9 +642,9 @@ private:
 
   uint set_up_partition_info(partition_info *part_info,
                              TABLE *table,
-                             void *tab);
-  int set_range_data(void *tab, partition_info* part_info);
-  int set_list_data(void *tab, partition_info* part_info);
+                             void *tab) const;
+  int set_range_data(void *tab, partition_info* part_info) const;
+  int set_list_data(void *tab, partition_info* part_info) const;
   int ndb_pk_update_row(THD *thd, 
                         const uchar *old_data, uchar *new_data,
                         uint32 old_part_id);
