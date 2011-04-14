35c3a28352dbc8f883a7617652b740f1963f355e
Jimmy Yang <jimmy.yang@oracle.com>
2011-04-14 06:41:47 -0700

    In dict_load_table(), the assertion about table cannot be corrupted did not consider the posibility of the dict_table_t is not loaded at all, in which case the "table" variable could be NULL. Add the NULL check in the assertion.
    Approved by Marko

--- storage/innobase/dict/dict0load.c
+++ storage/innobase/dict/dict0load.c
@@ -1792,7 +1792,8 @@ err_exit:
 #endif /* 0 */
 	mem_heap_free(heap);
 
-	ut_ad(ignore_err != DICT_ERR_IGNORE_NONE || table->corrupted == FALSE);
+	ut_ad(!table || ignore_err != DICT_ERR_IGNORE_NONE
+	      || !table->corrupted);
 
 	return(table);
 }
