13b0e46e1f09f22e2a74aaee34f7271f191e8e16
Alexander Nozdrin <alexander.nozdrin@oracle.com>
2011-04-15 13:04:21 +0400

    Preliminatry patch for WL#5787 (IPv6-capable INET_ATON and INET_NTOA functions).
    The patch just moves Item_func_inet_aton and Item_func_inet_ntoa
    implementations into new files item_inetfunc.h and
    item_inetfunc.cc. Those new files will be used to hold
    implementations of new functions in the next patch.

--- sql/item_func.h
+++ sql/item_func.h
@@ -1635,17 +1635,6 @@ public:
 };
 
 
-class Item_func_inet_aton : public Item_int_func
-{
-public:
-  Item_func_inet_aton(Item *a) :Item_int_func(a) {}
-  longlong val_int();
-  const char *func_name() const { return "inet_aton"; }
-  void fix_length_and_dec()
-    { decimals= 0; max_length= 21; maybe_null= 1; unsigned_flag= 1;}
-};
-
-
 /* for fulltext search */
 #include <ft_global.h>
 
