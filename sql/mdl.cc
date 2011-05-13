c0c1e0e53d160c510dd3cadbefd461d082eae236
Mikael Ronström <mikael@dator9>
2011-05-13 14:39:59 +0200

    merge fixes

--- sql/mdl.cc
+++ sql/mdl.cc
@@ -972,14 +972,14 @@ MDL_wait::timed_wait(MDL_context_owner *owner, struct timespec *abs_timeout,
 
   old_msg= owner->enter_cond(&m_COND_wait_status, &m_LOCK_wait_status,
                              wait_state_name);
-  thd_wait_begin(thd, THD_WAIT_META_DATA_LOCK);
+  thd_wait_begin(NULL, THD_WAIT_META_DATA_LOCK);
   while (!m_wait_status && !owner->is_killed() &&
          wait_result != ETIMEDOUT && wait_result != ETIME)
   {
     wait_result= mysql_cond_timedwait(&m_COND_wait_status, &m_LOCK_wait_status,
                                       abs_timeout);
   }
-  thd_wait_end(thd);
+  thd_wait_end(NULL);
 
   if (m_wait_status == EMPTY)
   {
