b2270aedd60809e0dab256f03f939513361e976e
Marko Mäkelä <marko.makela@oracle.com>
2011-05-24 11:41:31 +0300

    Backport an InnoDB Bug #58815 (Bug #11765812) work-around from mysql-trunk:
    ------------------------------------------------------------
    revno 2876.244.305
    revision id marko.makela@oracle.com-20110413082211-e6ouhjz5rmqxcqap
    parent  marko.makela@oracle.com-20110413075948-kvytmc37ye1nt7d9
    committer  Marko Mäkelä <marko.makela@oracle.com>
    branch nick 5.6-innodb
    timestamp Wed 2011-04-13 11:22:11 +0300
    message:
      Suppress the Bug #58815 (Bug #11765812) assertion failure.
    
      buf_page_get_gen(): Introduce BUF_GET_POSSIBLY_FREED for suppressing the
      check that the file page must not have been freed.
    
      btr_estimate_n_rows_in_range_on_level(): Pass BUF_GET_POSSIBLY_FREED and
      explain in the comments why this is needed and why it should be mostly
      harmless to ignore the problem. If InnoDB had always initialized all
      unused fields in data files, no problem would exist.
    
      This change does not fix the bug, it just "shoots the messenger".
    
      rb:647 approved by Jimmy Yang

--- storage/innobase/buf/buf0buf.c
+++ storage/innobase/buf/buf0buf.c
@@ -2747,6 +2747,7 @@ buf_page_get_gen(
 	case BUF_GET_IF_IN_POOL:
 	case BUF_PEEK_IF_IN_POOL:
 	case BUF_GET_IF_IN_POOL_OR_WATCH:
+	case BUF_GET_POSSIBLY_FREED:
 		break;
 	default:
 		ut_error;
@@ -3062,7 +3063,10 @@ wait_until_unfixed:
 #endif /* UNIV_DEBUG || UNIV_IBUF_DEBUG */
 
 	buf_block_buf_fix_inc(block, file, line);
-
+#if defined UNIV_DEBUG_FILE_ACCESSES || defined UNIV_DEBUG
+	ut_a(mode == BUF_GET_POSSIBLY_FREED
+	     || !block->page.file_page_was_freed);
+#endif
 	mutex_exit(&block->mutex);
 
 	/* Check if this is the first access to the page */
@@ -3074,10 +3078,6 @@ wait_until_unfixed:
 	if (UNIV_LIKELY(mode != BUF_PEEK_IF_IN_POOL)) {
 		buf_page_set_accessed_make_young(&block->page, access_time);
 	}
-
-#if defined UNIV_DEBUG_FILE_ACCESSES || defined UNIV_DEBUG
-	ut_a(!block->page.file_page_was_freed);
-#endif
 
 #if defined UNIV_DEBUG || defined UNIV_BUF_DEBUG
 	ut_a(++buf_dbg_counter % 5771 || buf_validate());
