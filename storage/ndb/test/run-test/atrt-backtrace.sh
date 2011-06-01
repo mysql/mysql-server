f76fa98a0143f1cc5d6682b18189b0f2f2d70381
jonas oreland <jonas@mysql.com>
2011-06-01 10:11:39 +0200

    ndb - remove usage of non-standard grep options added by autotest for windows project

--- storage/ndb/test/run-test/atrt-backtrace.sh
+++ storage/ndb/test/run-test/atrt-backtrace.sh
@@ -16,7 +16,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
 
 # Does not work on Windows (gcc only)
-if uname | grep -iq cygwin
+if [ `uname | grep -ic cygwin` -ne 0 ]
 then
     exit
 fi
