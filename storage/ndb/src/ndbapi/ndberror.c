07f06e64813511e39ea11c01fcb3848d572c956f
Pekka Nousiainen <pekka.nousiainen@oracle.com>
2011-06-06 15:18:27 +0300

    wl#4124 b05_api.diff ndbapi methods, utils, test

--- storage/ndb/src/ndbapi/ndberror.c
+++ storage/ndb/src/ndbapi/ndberror.c
@@ -540,12 +540,13 @@ ErrorBundle ErrorCodes[] = {
   /*
    * Index stats error codes
    */
-  { 4714, DMEC, AE, "Index stats sys tables " NDB_INDEX_STAT_PREFIX " are invalid" },
+  { 4714, DMEC, AE, "Index stats sys tables " NDB_INDEX_STAT_PREFIX " do not exist" },
   { 4715, DMEC, AE, "Index stats for specified index do not exist" },
   { 4716, DMEC, AE, "Index stats methods usage error" },
   { 4717, DMEC, AE, "Index stats cannot allocate memory" },
   { 4718, DMEC, IE, "Index stats memory cache is corrupted" },
   { 4719, DMEC, IE, "Index stats internal error" },
+  { 4720, DMEC, AE, "Index stats sys tables " NDB_INDEX_STAT_PREFIX " partly missing or invalid" },
   
   /**
    * Still uncategorized
