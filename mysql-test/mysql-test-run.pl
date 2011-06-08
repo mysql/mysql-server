84d72043ec4d4a66b9b6c7c2e8ec2de5876a9e57
magnus.blaudd@oracle.com <>
2011-06-08 16:07:27 +0200

    ndb  - use same set of suites for MySQL Cluster build as in 5.5-cluster

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -2734,7 +2734,7 @@ sub check_ndbcluster_support ($) {
     mtr_report(" - MySQL Cluster");
     # Enable ndb engine and add more test suites
     $opt_include_ndbcluster = 1;
-    $DEFAULT_SUITES.=",ndb";
+    $DEFAULT_SUITES.=",ndb,ndb_binlog,rpl_ndb,ndb_rpl";
   }
 
   if ($opt_include_ndbcluster)
