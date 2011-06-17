b3696af2e66fb4d72815d2d7754740c2fd8043fd
Inaam Rana <inaam.rana@oracle.com>
2011-06-17 16:20:20 -0400

    Bug 12635227 - 61188: DROP TABLE EXTREMELY SLOW
    approved by: Marko
    rb://681
    
    Coalescing of free buf_page_t descriptors can prove to be one severe
    bottleneck in performance of compression. One such workload where it
    hurts badly is DROP TABLE. This patch removes buf_page_t allocations
    from buf_buddy and uses ut_malloc instead.
    In order to further reduce overhead of colaescing we no longer attempt
    to coalesce a block if the corresponding free_list is less than 16 in
    size.

--- storage/innodb_plugin/btr/btr0cur.c
+++ storage/innodb_plugin/btr/btr0cur.c
@@ -3864,7 +3864,7 @@ btr_blob_free(
 	    && buf_block_get_space(block) == space
 	    && buf_block_get_page_no(block) == page_no) {
 
-		if (buf_LRU_free_block(&block->page, all) != BUF_LRU_FREED
+		if (!buf_LRU_free_block(&block->page, all)
 		    && all && block->page.zip.data) {
 			/* Attempt to deallocate the uncompressed page
 			if the whole block cannot be deallocted. */
