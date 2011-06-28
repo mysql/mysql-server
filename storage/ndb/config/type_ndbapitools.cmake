ddeb4d95cb3cb7636b4f4dddc046782af03f3776
jonas oreland <jonas.oreland@oracle.com>
2011-06-28 20:45:09 +0200

    55 - cleanup (?) cmake a bit. Use TARGET_LINK_LIBRARIES instead of LINK_LIBRARIES as it seems that dependencies arent tracked for the later, introducing race in build

--- storage/ndb/config/type_ndbapitools.cmake
+++ storage/ndb/config/type_ndbapitools.cmake
@@ -14,11 +14,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-LINK_LIBRARIES(ndbclient
-               dbug
-               mysys
-               strings
-               )
 IF(WIN32)
 LINK_LIBRARIES(wsock32)
 ENDIF(WIN32)
