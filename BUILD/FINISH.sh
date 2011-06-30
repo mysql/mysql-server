e5ce023f5716212aa1d20c757ebc8c6cecdf055a
Kent Boortz <kent.boortz@oracle.com>
2011-06-30 17:31:31 +0200

    Updated/added copyright headers

--- BUILD/FINISH.sh
+++ BUILD/FINISH.sh
@@ -1,4 +1,5 @@
-# Copyright (C) 2000, 2005 MySQL AB, 2008 Sun Microsystems, Inc.
+# Copyright (c) 2000, 2005 MySQL AB, 2008 Sun Microsystems, Inc.
+# Use is subject to license terms.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Library General Public
@@ -12,8 +13,8 @@
 #
 # You should have received a copy of the GNU Library General Public
 # License along with this library; if not, write to the Free
-# Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-# MA 02111-1307, USA
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+# MA 02110-1301, USA
 
 cflags="$c_warnings $extra_flags"
 cxxflags="$cxx_warnings $base_cxxflags $extra_flags"
