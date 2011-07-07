f4266ed51960c46a4d64a1323aea4e71c2fc6ab9
Davi Arnaut <davi.arnaut@oracle.com>
2011-07-07 08:22:43 -0300

    Bug#12727287: Maintainer mode compilation fails with gcc 4.6
    GCC 4.6 has new -Wunused-but-set-variable flag, which is enabled
    by -Wall, that causes GCC to emit a warning whenever a local variable
    is assigned to, but otherwise unused (aside from its declaration).
    
    Since the maintainer mode uses -Wall and -Werror, source code which
    triggers these warnings will be rejected. That is, these warnings
    become hard errors.
    
    The solution is to fix the code which triggers these specific warnings.
    In most of the cases, this is a welcome cleanup as code which triggers
    this warning is probably dead anyway.

--- sql/protocol.cc
+++ sql/protocol.cc
@@ -368,9 +368,8 @@ bool net_send_error_packet(THD *thd, uint sql_errno, const char *err,
     buff[]: sql_errno:2 + ('#':1 + SQLSTATE_LENGTH:5) + MYSQL_ERRMSG_SIZE:512
   */
   uint error;
-  uchar converted_err[MYSQL_ERRMSG_SIZE];
-  uint32 converted_err_len;
-  uchar buff[2+1+SQLSTATE_LENGTH+MYSQL_ERRMSG_SIZE], *pos;
+  char converted_err[MYSQL_ERRMSG_SIZE];
+  char buff[2+1+SQLSTATE_LENGTH+MYSQL_ERRMSG_SIZE], *pos;
 
   DBUG_ENTER("send_error_packet");
 
@@ -390,19 +389,16 @@ bool net_send_error_packet(THD *thd, uint sql_errno, const char *err,
   {
     /* The first # is to make the protocol backward compatible */
     buff[2]= '#';
-    pos= (uchar*) strmov((char*) buff+3, sqlstate);
+    pos= strmov(buff+3, sqlstate);
   }
 
-  converted_err_len= convert_error_message((char*)converted_err,
-                                           sizeof(converted_err),
-                                           thd->variables.character_set_results,
-                                           err, strlen(err),
-                                           system_charset_info, &error);
-  length= (uint) (strmake((char*) pos, (char*)converted_err,
-                          MYSQL_ERRMSG_SIZE - 1) - (char*) buff);
-  err= (char*) buff;
+  convert_error_message(converted_err, sizeof(converted_err),
+                        thd->variables.character_set_results,
+                        err, strlen(err), system_charset_info, &error);
+  /* Converted error message is always null-terminated. */
+  length= (uint) (strmake(pos, converted_err, MYSQL_ERRMSG_SIZE - 1) - buff);
 
-  DBUG_RETURN(net_write_command(net,(uchar) 255, (uchar*) "", 0, (uchar*) err,
+  DBUG_RETURN(net_write_command(net,(uchar) 255, (uchar*) "", 0, (uchar*) buff,
                                 length));
 }
 
--- tests/mysql_client_test.c
+++ tests/mysql_client_test.c
@@ -18596,11 +18596,8 @@ static void test_bug38486(void)
 static void test_bug33831(void)
 {
   MYSQL *l_mysql;
-  my_bool error;
 
   DBUG_ENTER("test_bug33831");
-  
-  error= 0;
 
   if (!(l_mysql= mysql_client_init(NULL)))
   {
@@ -18623,9 +18620,8 @@ static void test_bug33831(void)
     DIE_UNLESS(0);
   }
 
-
   mysql_close(l_mysql);
-  
+
   DBUG_VOID_RETURN;
 }
 
