aa863670015f410ddaf67c706db0ac1cc9d40665
Guilhem Bichot <guilhem.bichot@oracle.com>
2011-07-19 17:11:15 +0200

    Optimizer trace. By me: WL#5257	 	Create a first API for the Optimizer trace By Jorgen Loland: WL#5594	 	Add optimizer traces to the range optimizer WL#5741	 	Ensure optimizer tracing for subqueries works without JSON syntax errors WL#5581	 	Add --opt-trace-protocol to MTR. Summary below. * This feature produces a trace for any SELECT/INSERT/UPDATE/DELETE/DO/SET/CALL, which contains information about decisions taken by the optimizer during the optimization phase (choice of table access method, various costs, transformations, etc). It should help understand the logic followed by the optimizer for a particular query. Trace is session-local, controlled by the @@optimizer_trace variable and other new variables, and readable by selecting from information_schema.optimizer_trace. See the doxygen comments in opt_trace.h for more info. * By default the tracing code is compiled in, but not enabled at runtime; set @@optimizer_trace to enable it. * Changes to test results: unless otherwise noted they are due to those changes: ** The query printed by EXPLAIN EXTENDED now shows the id of each SELECT, to help interpret the "id" column. ** EXPLAIN EXTENDED now prints triggered conditions as:     trigcond_if(outer_field_is_not_null, <is_not_null_test>(`test`.`t2`.`s1`), true) instead of:     trigcond(<is_not_null_test>(`test`.`t2`.`s1`). * @@optimizer_trace_offset is a signed variable, so I had to add support for this in MySQL (so far all integer system variables were unsigned, it was impossible to set a system variable to a negative value, it would be rounded to 0).

--- sql/sp_head.h
+++ sql/sp_head.h
@@ -581,17 +581,6 @@ public:
   virtual int execute(THD *thd, uint *nextp) = 0;
 
   /**
-    Execute <code>open_and_lock_tables()</code> for this statement.
-    Open and lock the tables used by this statement, as a pre-requisite
-    to execute the core logic of this instruction with
-    <code>exec_core()</code>.
-    @param thd the current thread
-    @param tables the list of tables to open and lock
-    @return zero on success, non zero on failure.
-  */
-  int exec_open_and_lock_tables(THD *thd, TABLE_LIST *tables);
-
-  /**
     Get the continuation destination of this instruction.
     @return the continuation destination
   */
@@ -1349,6 +1338,8 @@ sp_prepare_func_item(THD* thd, Item **it_addr);
 
 bool
 sp_eval_expr(THD *thd, Field *result_field, Item **expr_item_ptr);
+
+bool check_show_routine_access(THD *thd, sp_head *sp, bool *full_access);
 
 /**
   @} (end of group Stored_Routines)
