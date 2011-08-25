fbe3414badab8fbf71bdd14fcfc9c82f3202cf93
Jonas Oreland <jonas.oreland@oracle.com>
2011-08-25 08:25:01 +0200

    ndb - set ndb_index_stat_enable=TRUE in >= 7.2.0

--- sql/ha_ndbcluster.cc
+++ sql/ha_ndbcluster.cc
@@ -164,6 +164,11 @@ static MYSQL_THDVAR_ULONG(
   0                                  /* block */
 );
 
+#if NDB_VERSION_D < NDB_MAKE_VERSION(7,2,0)
+#define DEFAULT_NDB_INDEX_STAT_ENABLE FALSE
+#else
+#define DEFAULT_NDB_INDEX_STAT_ENABLE TRUE
+#endif
 
 static MYSQL_THDVAR_BOOL(
   index_stat_enable,                 /* name */
@@ -171,7 +176,7 @@ static MYSQL_THDVAR_BOOL(
   "Use ndb index statistics in query optimization.",
   NULL,                              /* check func. */
   NULL,                              /* update func. */
-  FALSE                              /* default */
+  DEFAULT_NDB_INDEX_STAT_ENABLE      /* default */
 );
 
 
