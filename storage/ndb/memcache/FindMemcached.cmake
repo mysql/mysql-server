b14bc37d40125f7a253a77f980f0f460fc5b7082
John David Duncan <john.duncan@oracle.com>
2011-09-19 22:13:01 -0700

    Bundle source trees for libevent and memcached under extra/ Modify NDB+memcache to be able to use bundled memcached

--- storage/ndb/memcache/FindMemcached.cmake
+++ storage/ndb/memcache/FindMemcached.cmake
@@ -6,27 +6,37 @@ SET(MEMCACHED_HOME "" CACHE PATH "Path to installed Memcached 1.6")
 # unset(MEMCACHED_INCLUDE_DIR CACHE)
 # unset(MEMCACHED_UTILITIES_LIBRARY CACHE)
 
-find_path(MEMCACHED_ROOT_DIR bin/engine_testapp
-  HINTS 
-    $ENV{MEMCACHED_HOME}
-    ${MEMCACHED_HOME}
-    ${CMAKE_INSTALL_PREFIX}
-  PATHS
-    /usr/local  /usr
-    /opt/local  /opt
-    ~/Library/Frameworks  /Library/Frameworks
-)  
+if(WITH_MEMCACHE) 
+  # Use bundled memcached
+  set(MEMCACHED_ROOT_DIR ${CMAKE_SOURCE_DIR}/extra/memcached)
+  set(MEMCACHED_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/extra/memcached/include)
+  set(MEMCACHED_UTILITIES_LIBRARY memcached_utilities)
 
-find_path(MEMCACHED_INCLUDE_DIR memcached/engine_testapp.h
-  HINTS  ${MEMCACHED_ROOT_DIR}
-  PATH_SUFFIXES include
-)
+else()   
 
-find_library(MEMCACHED_UTILITIES_LIBRARY
-  NAMES memcached_utilities
-  HINTS  ${MEMCACHED_ROOT_DIR}
-  PATH_SUFFIXES lib/memcached lib memcached/lib
-)
+  # Find an installed memcached
+  find_path(MEMCACHED_ROOT_DIR bin/engine_testapp
+    HINTS 
+      $ENV{MEMCACHED_HOME}
+      ${MEMCACHED_HOME}
+      ${CMAKE_INSTALL_PREFIX}
+    PATHS
+      /usr/local  /usr
+      /opt/local  /opt
+      ~/Library/Frameworks  /Library/Frameworks
+  )  
+
+  find_path(MEMCACHED_INCLUDE_DIR memcached/engine_testapp.h
+    HINTS  ${MEMCACHED_ROOT_DIR}
+    PATH_SUFFIXES include
+  )
+
+  find_library(MEMCACHED_UTILITIES_LIBRARY
+    NAMES memcached_utilities
+    HINTS  ${MEMCACHED_ROOT_DIR}
+    PATH_SUFFIXES lib/memcached lib memcached/lib
+  )
+endif()
 
 if(MEMCACHED_ROOT_DIR AND MEMCACHED_INCLUDE_DIR AND MEMCACHED_UTILITIES_LIBRARY) 
   set(MEMCACHED_FOUND TRUE)
