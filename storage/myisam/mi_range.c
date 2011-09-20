22135cf8db0bfdefdbdd9397f22cb5de8eafeb56
magnus.blaudd@oracle.com <>
2011-09-20 12:51:55 +0200

    ndb - erase copyright diffs in storage/

--- storage/myisam/mi_range.c
+++ storage/myisam/mi_range.c
@@ -1,6 +1,7 @@
 /*
    Copyright (c) 2000, 2011, Oracle and/or its affiliates. All rights reserved.
 
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; version 2 of the License.
