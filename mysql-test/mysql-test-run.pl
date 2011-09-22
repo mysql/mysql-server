b2c389b39dfd42ee4ec8d7271a48aa96d7e3a1cf
John David Duncan <john.duncan@oracle.com>
2011-09-22 12:59:38 -0700

    Add ndb_memcache to default list of test suites.

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -2744,7 +2744,7 @@ sub check_ndbcluster_support ($) {
     mtr_report(" - MySQL Cluster");
     # Enable ndb engine and add more test suites
     $opt_include_ndbcluster = 1;
-    $DEFAULT_SUITES.=",ndb,ndb_binlog,rpl_ndb,ndb_rpl";
+    $DEFAULT_SUITES.=",ndb,ndb_binlog,rpl_ndb,ndb_rpl,ndb_memcache";
   }
 
   if ($opt_include_ndbcluster)
