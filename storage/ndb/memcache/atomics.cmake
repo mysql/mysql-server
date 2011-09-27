3a797fab84ed42a3cff6886787c88ba4959ec403
John David Duncan <john.duncan@oracle.com>
2011-09-27 12:31:36 -0700

    storage/ndb/memcache: try "-march=pentium" with GCC atomic builtins.

--- storage/ndb/memcache/atomics.cmake
+++ storage/ndb/memcache/atomics.cmake
@@ -16,7 +16,34 @@
 # Identify support for atomic operations
 IF(NOT MSVC)
 
-# HAVE_GCC_ATOMIC_BUILTINS is already checked by CMake somewhere else
+# HAVE_GCC_ATOMIC_BUILTINS is already checked by CMake, but if it's not defined
+# we're going to retest using "-march=pentium"
+
+
+  if(${CMAKE_COMPILER_IS_GNUCC} AND NOT ${HAVE_GCC_ATOMIC_BUILTINS})
+     set(OLD_FLAGS ${CMAKE_REQUIRED_FLAGS})
+     set(CMAKE_REQUIRED_FLAGS ${OLD_FLAGS} -march=pentium)
+     CHECK_C_SOURCE_RUNS(
+         "int main() {
+          volatile int foo= -10; 
+          volatile int bar= 10;
+          /* operation returns 0 and foo should be 0 */
+          if (!__sync_fetch_and_add(&foo, bar) || foo)
+            return -1;
+          bar= __sync_lock_test_and_set(&foo, bar);
+          /* Now bar is the return value 0 and foo is set to 10 */
+          if (bar || foo != 10)
+            return -1;
+          __sync_val_compare_and_swap(&bar, foo, 15);
+          /* CAS should have failed and bar is still 0 */
+          if (bar)
+            return -1;
+          return 0;
+        }"
+        HAVE_GCC_ATOMICS_WITH_ARCH_FLAG
+      )
+      set(CMAKE_REQUIRED_FLAGS ${OLD_FLAGS})
+  endif()
 
   CHECK_C_SOURCE_RUNS(
     "#include <libkern/OSAtomic.h>
@@ -39,7 +66,11 @@ IF(NOT MSVC)
   )
 ENDIF()
 
-IF(HAVE_GCC_ATOMIC_BUILTINS)
+IF(HAVE_GCC_ATOMICS_WITH_ARCH_FLAG)
+  add_definitions(-march=pentium)
+ENDIF()
+
+IF(HAVE_GCC_ATOMIC_BUILTINS OR HAVE_GCC_ATOMICS_WITH_ARCH_FLAG)
   MESSAGE(STATUS "Using gcc atomic builtins") 
 ELSEIF(HAVE_DARWIN_ATOMICS) 
   MESSAGE(STATUS "Using Darwin OSAtomic") 
