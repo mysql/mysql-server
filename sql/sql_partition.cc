e3d14001859077d077deb65c7602ba057764855c
Jonas Oreland <jonas.oreland@oracle.com>
2011-09-29 07:46:22 +0200

    ndb - another attempt at fixing warning (online alter code)

--- sql/sql_partition.cc
+++ sql/sql_partition.cc
@@ -4651,7 +4651,7 @@ uint prep_alter_part_table(THD *thd, TABLE *table, Alter_info *alter_info,
       if (tab_part_info->part_type != HASH_PARTITION ||
           ((table->s->db_type()->partition_flags() & HA_USE_AUTO_PARTITION) &&
            !tab_part_info->use_default_num_partitions) ||
-          ((!table->s->db_type()->partition_flags() & HA_USE_AUTO_PARTITION) &&
+          ((!(table->s->db_type()->partition_flags() & HA_USE_AUTO_PARTITION))&&
            tab_part_info->use_default_num_partitions))
 #else
       if (tab_part_info->part_type != HASH_PARTITION ||
