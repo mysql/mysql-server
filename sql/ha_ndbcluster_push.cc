726d272863c295448540d5c509ef4ffcd07635c2
magnus.blaudd@oracle.com <>
2011-09-30 12:16:51 +0200

    ndb - explosives not allowed in the air

--- sql/ha_ndbcluster_push.cc
+++ sql/ha_ndbcluster_push.cc
@@ -42,12 +42,6 @@
 
 #include <ndb_version.h>
 
-
-#ifdef ndb_dynamite
-#undef assert
-#define assert(x) do { if(x) break; ::printf("%s %d: assert failed: %s\n", __FILE__, __LINE__, #x); ::fflush(stdout); ::signal(SIGABRT,SIG_DFL); ::abort(); ::kill(::getpid(),6); ::kill(::getpid(),9); } while (0)
-#endif
-
 #define EXPLAIN_NO_PUSH(msgfmt, ...)                              \
 do                                                                \
 {                                                                 \
