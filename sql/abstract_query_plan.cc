95920c1874fdcfa98677956f38bdac3cb96c65a9
magnus.blaudd@oracle.com <>
2011-09-30 12:24:10 +0200

    Merge

--- sql/abstract_query_plan.cc
+++ sql/abstract_query_plan.cc
@@ -16,29 +16,11 @@
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
 */
 
-#ifdef USE_PRAGMA_IMPLEMENTATION
-#pragma implementation				// gcc: Class implementation
-#endif
-
-#ifndef MYSQL_SERVER
-#define MYSQL_SERVER
-#endif
-
 #include "sql_priv.h"
 #include "sql_select.h"
 #include "abstract_query_plan.h"
 
 
-#ifdef NDB_WITHOUT_JOIN_PUSHDOWN
-static bool
-test_if_skip_sort_order(JOIN_TAB *tab,ORDER *order,ha_rows select_limit,
-			bool no_changes, const key_map *map)
-{
-  // Stub used when compiling AQP as part of ndbcluster, never called 
-  assert(false);
-  return false;
-}
-#endif
 
 namespace AQP
 {
