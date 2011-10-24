c9b2339d81cd98b3fbd618b69b5083a01dea66b1
magnus.blaudd@oracle.com <>
2011-10-24 09:37:20 +0200

    ndb schema dist  - remove unused argument "ndb" from 'ndb_binlog_thread_handle_error'

--- sql/ha_ndbcluster_binlog.cc
+++ sql/ha_ndbcluster_binlog.cc
@@ -5714,8 +5714,7 @@ static void ndb_unpack_record(TABLE *table, NdbValue *value,
   Handle error states on events from the storage nodes
 */
 static int
-ndb_binlog_thread_handle_error(Ndb *ndb,
-                               NdbEventOperation *pOp)
+ndb_binlog_thread_handle_error(NdbEventOperation *pOp)
 {
   Ndb_event_data *event_data= (Ndb_event_data *) pOp->getCustomData();
   NDB_SHARE *share= event_data->share;
@@ -7093,7 +7092,7 @@ restart_cluster_failure:
           event_count++;
 #endif
           if (pOp->hasError() &&
-              ndb_binlog_thread_handle_error(i_ndb, pOp) < 0)
+              ndb_binlog_thread_handle_error(pOp) < 0)
             goto err;
 
 #ifndef DBUG_OFF
