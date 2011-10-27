35db550df03be067f7031a299cf7d492429755a6
Magnus Blåudd <magnus.blaudd@oracle.com>
2011-10-27 14:20:56 +0200

    ndbcluster  - Make 'ndbcluster_discover' function static, only called as handlerton callback

--- sql/ha_ndbcluster.h
+++ sql/ha_ndbcluster.h
@@ -42,7 +42,6 @@ class NdbBlob;
 class NdbIndexStat;
 class NdbEventOperation;
 class ha_ndbcluster_cond;
-class Ndb_event_data;
 class NdbQuery;
 class NdbQueryOperation;
 class NdbQueryOperationTypeWrapper;
@@ -814,8 +813,6 @@ private:
   int add_handler_to_open_tables(THD*, Thd_ndb*, ha_ndbcluster* handler);
 };
 
-int ndbcluster_discover(THD* thd, const char* dbname, const char* name,
-                        const void** frmblob, uint* frmlen);
 int ndbcluster_table_exists_in_engine(THD* thd,
                                       const char *db, const char *name);
 void ndbcluster_print_error(int error, const NdbOperation *error_op);
