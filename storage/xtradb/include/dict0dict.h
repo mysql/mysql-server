0efacfcfb24443bbd342e5c4447b166cb569438c
Igor Babaev <igor@askmonty.org>
2011-10-31 01:36:28 -0700

    Fixed a compilation error.

--- storage/xtradb/include/dict0dict.h
+++ storage/xtradb/include/dict0dict.h
@@ -832,7 +832,7 @@ dict_index_get_nth_col_no(
 Looks for column n in an index.
 @return position in internal representation of the index;
 ULINT_UNDEFINED if not contained */
-UNIV_INTERN
+UNIV_INLINE
 ulint
 dict_index_get_nth_col_pos(
 /*=======================*/
