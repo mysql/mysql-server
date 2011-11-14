38272703ba2ff8e7c9e06b0a4eb34171a119932c
magnus.blaudd@oracle.com <>
2011-11-14 11:18:52 +0100

    WL#5881  - remove the MCP patch for BUG#59948 and instead    patch the problem in ha_ndbcluster::print_error    (cherrypick fix from 5.5-cluster)

--- sql/handler.cc
+++ sql/handler.cc
@@ -3325,19 +3325,6 @@ uint handler::get_dup_key(int error)
   DBUG_ASSERT(table_share->tmp_table != NO_TMP_TABLE ||
               m_lock_type != F_UNLCK);
   DBUG_ENTER("handler::get_dup_key");
-#ifndef MCP_BUG59948
-  if (table == NULL || table->file == NULL)
-  {
-    /*
-      This is a sideffect of the ndbcluster_print_error() (called from
-      ndbcluster_commit and ndbcluster_rollback). The function realises that
-      it "knows nothing" so it creates brand new ha_ndbcluster in order to be
-      able to call print_error() function. Unfortunately the handler hasn't
-      been open()ed and thus table pointer is not set.
-     */
-    DBUG_RETURN(-1);
-  }
-#endif
   table->file->errkey  = (uint) -1;
   if (error == HA_ERR_FOUND_DUPP_KEY || error == HA_ERR_FOREIGN_DUPLICATE_KEY ||
       error == HA_ERR_FOUND_DUPP_UNIQUE || error == HA_ERR_NULL_IN_SPATIAL ||
