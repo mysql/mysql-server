69b6241a79876ae98bb0c9dce7c8d8799d6ad273
Inaam Rana <inaam.rana@oracle.com>
2011-12-23 08:17:36 -0500

    [part 1 of WL5655] refactoring dblwr code
    rb://861
    approved by: Jimmy
    
    * Refactor dblwr code in a separate module
    * Cleanup support of pre-4.1 ibuf record format

--- storage/innobase/include/sync0sync.h
+++ storage/innobase/include/sync0sync.h
@@ -109,7 +109,7 @@ extern mysql_pfs_key_t	srv_monitor_file_mutex_key;
 # ifdef UNIV_SYNC_DEBUG
 extern mysql_pfs_key_t	sync_thread_mutex_key;
 # endif /* UNIV_SYNC_DEBUG */
-extern mysql_pfs_key_t	trx_doublewrite_mutex_key;
+extern mysql_pfs_key_t	buf_dblwr_mutex_key;
 extern mysql_pfs_key_t	trx_undo_mutex_key;
 extern mysql_pfs_key_t	trx_mutex_key;
 extern mysql_pfs_key_t	lock_sys_mutex_key;
