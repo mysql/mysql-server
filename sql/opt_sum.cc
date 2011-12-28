31805e621495e33d4dcf4e4b2180d42367f90894
Igor Babaev <igor@askmonty.org>
2011-12-27 19:13:53 -0800

    Fixed LP bug #879860. The MIN/MAX optimization cannot be applied to a subquery if its WHERE clause contains a conjunctive condition depending on an outer reference.

--- sql/opt_sum.cc
+++ sql/opt_sum.cc
@@ -608,6 +608,10 @@ static bool matching_cond(bool max_fl, TABLE_REF *ref, KEY *keyinfo,
   if (!cond)
     DBUG_RETURN(TRUE);
   Field *field= field_part->field;
+  if (cond->used_tables() & OUTER_REF_TABLE_BIT)
+  { 
+    DBUG_RETURN(FALSE);
+  } 
   if (!(cond->used_tables() & field->table->map) &&
       test(cond->used_tables() & ~PSEUDO_TABLE_BITS))
   {
