43a802e71bda937acf4179d45fbd876651051602
Roy Lyseng <roy.lyseng@oracle.com>
2012-01-20 16:30:14 +0100

    Bug#13596176: Missing row on select with nested in clause when matr=on               and bnl=off + MyISAM
    This is a case where Materialize-scan semi-join strategy is selected,
    and ref access is used to look up a row in the outer table.
    The referenced field in the outer table belongs in a multiple equality
    together with three fields from the inner tables.
    
    setup_sj_materialization() sets up objects to copy fields from the
    materialized tables back to original field objects. For a multiple
    equality, a "best" field is picked for this, the remaining fields
    are not copied. This best field coincides with fields used by regular
    conditions, but not necessarily with fields used for ref access.
    
    The reason for this is that before fields are substituted within
    regular conditions (see substitute_for_best_equal_field()), multiple
    equality objects are sorted so that fields come in table order.
    This sorting has not been done when calling set_access_methods(),
    which sets up ref access.
    
    The solution to the problem is to execute set_access_methods() after
    the multiple equalities have been sorted, and use the same algorithm
    as setup_sj_materialization() uses to pick the proper field for
    ref access.
    
    A pleasant side effect of this fix is also that
    make_cond_for_table_from_pred() now does a better job in replacing
    equality predicates that are also covered by ref accesses.
    
    mysql-test/include/subquery_sj.inc
      Added test case for bug#13596176.
    
    mysql-test/r/derived.result
    mysql-test/r/greedy_optimizer.result
    mysql-test/r/join_cache_bka.result
    mysql-test/r/join_cache_bka_nixbnl.result
    mysql-test/r/join_cache_bkaunique.result
    mysql-test/r/join_cache_bnl.result
    mysql-test/r/join_cache_nojb.result
    mysql-test/r/join_nested.result
    mysql-test/r/join_nested_bka.result
    mysql-test/r/join_nested_bka_nixbnl.result
    mysql-test/suite/opt_trace/r/general2_no_prot.result
    mysql-test/suite/opt_trace/r/general2_ps_prot.result
      Updated plans. Mostly these are removed "Using where" and
      "Using index condition", which means that a condition has been
      correctly identified as ref access and removed.
    
    mysql-test/r/subquery_sj_all.result
    mysql-test/r/subquery_sj_all_bka.result
    mysql-test/r/subquery_sj_all_bka_nixbnl.result
    mysql-test/r/subquery_sj_all_bkaunique.result
    mysql-test/r/subquery_sj_dupsweed.result
    mysql-test/r/subquery_sj_dupsweed_bka.result
    mysql-test/r/subquery_sj_dupsweed_bka_nixbnl.result
    mysql-test/r/subquery_sj_dupsweed_bkaunique.result
    mysql-test/r/subquery_sj_firstmatch.result
    mysql-test/r/subquery_sj_firstmatch_bka.result
    mysql-test/r/subquery_sj_firstmatch_bka_nixbnl.result
    mysql-test/r/subquery_sj_firstmatch_bkaunique.result
    mysql-test/r/subquery_sj_loosescan.result
    mysql-test/r/subquery_sj_loosescan_bka.result
    mysql-test/r/subquery_sj_loosescan_bka_nixbnl.result
    mysql-test/r/subquery_sj_loosescan_bkaunique.result
    mysql-test/r/subquery_sj_mat.result
    mysql-test/r/subquery_sj_mat_bka.result
    mysql-test/r/subquery_sj_mat_bka_nixbnl.result
    mysql-test/r/subquery_sj_mat_bkaunique.result
    mysql-test/r/subquery_sj_none.result
    mysql-test/r/subquery_sj_none_bka.result
    mysql-test/r/subquery_sj_none_bka_nixbnl.result
    mysql-test/r/subquery_sj_none_bkaunique.result
      Added test case results for bug#13596176, plus some updated plans.
    
    sql/sql_optimizer.cc
      Added function get_best_field() that picks the "best" field from
      a multiple equality.
      Calls set_access_methods() after multiple equalities have been sorted.
      Calls drop_unused_derived_keys() after ref access has been set up.
      Calls update_depend_map() after ref access has been set up.
      Calls initialize_tables() after ref access has been set up.
    
    sql/sql_optimizer.h
      Added definition for function get_best_field().
    
    sql/sql_select.cc
      Calls get_best_field() in create_ref_for_key() and
      setup_sj_materialization() to get the "best" field.
      Call to set_access_methods() and update_depend_map() moved to a more
      appropriate place.

--- sql/sql_optimizer.cc
+++ sql/sql_optimizer.cc
@@ -362,8 +362,6 @@ JOIN::optimize()
     DBUG_RETURN(1);
   }
 
-  drop_unused_derived_keys();
-
   if (rollup.state != ROLLUP::STATE_NONE)
   {
     if (rollup_process_const_fields())
@@ -378,12 +376,6 @@ JOIN::optimize()
     select_distinct= select_distinct && (const_tables != tables);
   }
 
-  THD_STAGE_INFO(thd, stage_preparing);
-  if (result->initialize_tables(this))
-  {
-    DBUG_PRINT("error",("Error: initialize_tables() failed"));
-    DBUG_RETURN(1);				// error == -1
-  }
   if (const_table_map != found_const_table_map &&
       !(select_options & SELECT_DESCRIBE) &&
       (!conds ||
@@ -438,8 +430,7 @@ JOIN::optimize()
   }
 
   /*
-    Permorm the the optimization on fields evaluation mentioned above
-    for all on expressions.
+    Perform the same optimization on field evaluation for all join conditions.
   */ 
   for (JOIN_TAB *tab= join_tab + const_tables; tab < join_tab + tables ; tab++)
   {
@@ -462,6 +453,26 @@ JOIN::optimize()
       (select_options & SELECT_DESCRIBE))
   {
     conds=new Item_int((longlong) 0,1);	// Always false
+  }
+
+  if (set_access_methods())
+  {
+    error= 1;
+    DBUG_PRINT("error",("Error from set_access_methods"));
+    DBUG_RETURN(1);
+  }
+
+  // We need all derived keys until access methods have been set.
+  drop_unused_derived_keys();
+
+  // Update table dependencies after assigning ref access fields
+  update_depend_map(this);
+
+  THD_STAGE_INFO(thd, stage_preparing);
+  if (result->initialize_tables(this))
+  {
+    DBUG_PRINT("error",("Error: initialize_tables() failed"));
+    DBUG_RETURN(1);				// error == -1
   }
 
   if (make_join_select(this, conds))
@@ -984,7 +995,33 @@ finish:
   return item;
 }
 
-  
+
+/**
+  Get the best field substitution for a given field.
+
+  If the field is member of a multiple equality, look up that equality
+  and return the most appropriate field. Usually this is the equivalenced
+  field belonging to the outer-most table in the join order, but
+  @see Item_field::get_subst_item() for details.
+  Otherwise, return the same field.
+
+  @param item_field The field that we are seeking a substitution for.
+  @param cond_equal multiple equalities to search in
+
+  @return The substituted field.
+*/
+
+Item_field *get_best_field(Item_field *item_field, COND_EQUAL *cond_equal)
+{
+  bool dummy;
+  Item_equal *item_eq= find_item_equal(cond_equal, item_field->field, &dummy);
+  if (!item_eq)
+    return item_field;
+
+  return item_eq->get_subst_item(item_field);
+}
+
+
 /**
   Check whether an equality can be used to build multiple equalities.
 
@@ -2593,8 +2630,8 @@ void update_depend_map(JOIN *join)
     uint i;
     for (i=0 ; i < ref->key_parts ; i++,item++)
       depend_map|=(*item)->used_tables();
-    ref->depend_map=depend_map & ~OUTER_REF_TABLE_BIT;
-    depend_map&= ~OUTER_REF_TABLE_BIT;
+    depend_map&= ~PSEUDO_TABLE_BITS;
+    ref->depend_map= depend_map;
     for (JOIN_TAB **tab=join->map2table;
 	 depend_map ;
 	 tab++,depend_map>>=1 )
