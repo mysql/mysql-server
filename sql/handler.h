a6580977cd424b7de3304d63ab6efd9421f66294
Jon Olav Hauglid <jon.hauglid@oracle.com>
2012-02-02 10:12:43 +0100

    WL#5534 Online ALTER, Phase 1
    Follow-up patch: Update copyright messages for
    touched source code files to 2012.

--- sql/handler.h
+++ sql/handler.h
@@ -2,7 +2,7 @@
 #define HANDLER_INCLUDED
 
 /*
-   Copyright (c) 2000, 2011, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2000, 2012, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
