7f291e62fb9d7245126a122caacf9c6348c884a4
Mayank Prasad <mayank.prasad@oracle.com>
2012-02-02 21:27:35 +0530

    WL#5767 : Performance Schema, statements digest
    Details:
    - Modified printed reduced token for digest text as per new
      specification. Modified code comments as well.
    - Removed TOK_PFS_ROW_POSSIBLE_SINGLE_VALUE,
      TOK_PFS_ROW_POSSIBLE_MULTIPLE_VALUE
    - Made READ/STORE(_TOKEN) & READ/STORE(_IDENTIFIER) etc to
      inline function.
    - Modified structure PFS_statements_digest_stat as per comments.

--- storage/perfschema/pfs_digest.h
+++ storage/perfschema/pfs_digest.h
@@ -55,13 +55,14 @@ struct {
 struct PFS_statements_digest_stat
 {
   /**
-    Digest Storage.
-  */
-  PFS_digest_storage m_digest_storage;
-  /**
     Digest hash/LF Hash search key.
   */
   PFS_digest_hash m_md5_hash;
+
+  /**
+    Digest Storage.
+  */
+  PFS_digest_storage m_digest_storage;
 
   /**
     Statement stat.
@@ -74,8 +75,6 @@ struct PFS_statements_digest_stat
   ulonglong m_first_seen;
   ulonglong m_last_seen;
 };
-
-
 
 int init_digest(unsigned int digest_sizing);
 void cleanup_digest();
@@ -102,5 +101,114 @@ void pfs_digest_add_token_v1(PSI_digest_locker *locker,
                              char *yytext,
                              int yylen);
 void pfs_digest_end_v1(PSI_digest_locker *locker);
+
+
+
+/** 
+  Function to read a single token from token array.
+*/
+inline void read_token(uint *dest, int *index, char *src)
+{
+  unsigned short sh;
+  sh= ((0x00ff & src[*index + 1])<<8) | (0x00ff & src[*index]);
+  *dest= (uint)(sh);
+  *index= *index + PFS_SIZE_OF_A_TOKEN;
+}
+
+/**
+  Function to store a single token in token array.
+*/
+inline void store_token(PFS_digest_storage* digest_storage, uint token)
+{
+  char* dest= digest_storage->m_token_array;
+  int* index= &digest_storage->m_byte_count;
+  unsigned short sh= (unsigned short)token;
+
+  dest[*index]= (sh) & 0xff;
+  *index= *index + 1;
+  dest[*index]= (sh>>8) & 0xff;
+  *index= *index + 1;
+}
+
+/**
+  Function to read an identifier from token array.
+*/
+inline void read_identifier(char **dest, int *index, char *src)
+{
+  uint length;
+  int remaining_bytes= PFS_MAX_DIGEST_STORAGE_SIZE - *index;
+  DBUG_ASSERT(remaining_bytes >= 0);
+  /*
+    Read ID's length.
+    Make sure that space, to read ID's length, is available.
+  */
+  if(remaining_bytes >= PFS_SIZE_OF_A_TOKEN)
+  {
+    read_token(&length, index, src);
+    /*
+      While storing ID length, it has already been stored
+      in a way that ID doesn't go beyond the storage size,
+      so no need to check length here.
+    */
+    strncpy(*dest, src + *index, length);
+    *index= *index + length;
+    *dest= *dest + length;
+  }
+}
+
+/**
+  Function to store an identifier in token array.
+*/
+inline void store_identifier(PFS_digest_storage* digest_storage,
+                             uint id_length, char *id_name)
+{
+  char* dest= digest_storage->m_token_array;
+  int* index= &digest_storage->m_byte_count;
+  int remaining_bytes= PFS_MAX_DIGEST_STORAGE_SIZE - *index;
+  DBUG_ASSERT(remaining_bytes >= 0);
+
+  /*
+    Store ID's length.
+    Make sure that space, to store ID's length, is available.
+  */
+  if(remaining_bytes >= PFS_SIZE_OF_A_TOKEN)
+  {
+    /*
+       Make sure to store ID length/ID as per the space available.
+    */
+    remaining_bytes-= PFS_SIZE_OF_A_TOKEN;
+    id_length= id_length>(uint)remaining_bytes?(uint)remaining_bytes:id_length;
+    store_token(digest_storage, id_length);
+    strncpy(dest + *index, id_name, id_length);
+    *index= *index + id_length;
+  }
+}
+
+/**
+  Function to read last two tokens from token array. If an identifier
+  is found, do not look for token after that.
+*/
+inline void read_last_two_tokens(PFS_digest_storage* digest_storage,
+                                 uint *t1, uint *t2)
+{
+  int last_token_index;
+  int last_id_index= digest_storage->m_last_id_index;
+  int byte_count= digest_storage->m_byte_count;
+
+  if(last_id_index <= byte_count - PFS_SIZE_OF_A_TOKEN)
+  {
+    /* Take last token. */
+    last_token_index= byte_count - PFS_SIZE_OF_A_TOKEN;
+    DBUG_ASSERT(last_token_index >= 0);
+    read_token(t1, &last_token_index, digest_storage->m_token_array);
+  }
+  if(last_id_index <= byte_count - 2*PFS_SIZE_OF_A_TOKEN)
+  {
+    /* Take 2nd token from last. */
+    last_token_index= byte_count - 2*PFS_SIZE_OF_A_TOKEN;
+    DBUG_ASSERT(last_token_index >= 0);
+    read_token(t2, &last_token_index, digest_storage->m_token_array);
+  }
+}
 
 #endif
