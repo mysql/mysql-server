f7180e1f839f0beffedb95934be730212e79b3ab
Zardosht Kasheff <zardosht@tokutek.com>
2012-02-23 19:51:41 +0000

    [t:4563], move txn code to own file
    git-svn-id: file:///svn/toku/tokudb@40238 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/ydb-internal.h
+++ src/ydb-internal.h
@@ -257,13 +257,6 @@ void env_note_zombie_db_closed(DB_ENV *env, DB *db);
 int toku_env_dbremove(DB_ENV * env, DB_TXN *txn, const char *fname, const char *dbname, u_int32_t flags);
 int toku_env_dbrename(DB_ENV *env, DB_TXN *txn, const char *fname, const char *dbname, const char *newname, u_int32_t flags);
 
-
-int toku_txn_begin_internal(DB_ENV *env, DB_TXN * stxn, DB_TXN ** txn, u_int32_t flags, bool internal, bool holds_ydb_lock);
-int toku_txn_commit(DB_TXN * txn, u_int32_t flags, TXN_PROGRESS_POLL_FUNCTION, void*, bool release_multi_operation_client_lock);
-int toku_txn_abort(DB_TXN * txn, TXN_PROGRESS_POLL_FUNCTION, void*, bool release_multi_operation_client_lock);
-int locked_txn_commit(DB_TXN *txn, u_int32_t flags);
-int locked_txn_abort(DB_TXN *txn);
-
 #if defined(__cplusplus)
 }
 #endif
