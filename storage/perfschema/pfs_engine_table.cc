b44299498ee3191df92cab436888cb6ee512be1a
Christopher Powers <chris.powers@oracle.com>
2012-02-23 19:25:29 -0600

    WL#5767 PERFORMANCE_SCHEMA statement digests
    Fixed timestamp bug pfs_engine_table

--- storage/perfschema/pfs_engine_table.cc
+++ storage/perfschema/pfs_engine_table.cc
@@ -511,8 +511,8 @@ void PFS_engine_table::set_field_enum(Field *f, ulonglong value)
 void PFS_engine_table::set_field_timestamp(Field *f, ulonglong value)
 {
   struct timeval tm;
-  tm.tv_sec= (long) value / 1000000;
-  tm.tv_usec= (long) value % 1000000;
+  tm.tv_sec= (long)(value / 1000000);
+  tm.tv_usec= (long)(value % 1000000);
   DBUG_ASSERT(f->real_type() == MYSQL_TYPE_TIMESTAMP2);
   Field_timestampf *f2= (Field_timestampf*) f;
   f2->store_timestamp(& tm);
