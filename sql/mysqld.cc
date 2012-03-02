3ecf3da4bdab5ca0525840279f91e98d534f9b9a
Sven Sandberg <sven.sandberg@oracle.com>
2012-03-02 21:12:56 +0100

    BUG#13779291: RACE CONDITION AROUND ROTATE RELAY LOGS & FLUSH LOGS This is part 5 of the patch. It only cleans up redundant junk from the source files. The member variable MYSQL_BIN_LOG::no_auto_events and the parameter no_auto_events in MYSQL_BIN_LOG::open_binlog(char*,...) are always false. So they can be removed. As a result MYSQL_BIN_LOG::init, which is called in only one place, contains only a single assignment. So we remove it and replace the call by the assignment.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -4484,7 +4484,7 @@ a file name for --log-bin-index option", opt_binlog_index_name);
     mysql_bin_log.set_previous_gtid_set(
       const_cast<Gtid_set*>(gtid_state.get_logged_gtids()));
     if (mysql_bin_log.open_binlog(opt_bin_logname, LOG_BIN, 0,
-                                  WRITE_CACHE, 0, max_binlog_size, 0,
+                                  WRITE_CACHE, max_binlog_size, 0,
                                   true/*need mutex*/, true/*need sid_lock*/,
                                   NULL))
       unireg_abort(1);
