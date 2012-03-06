0d69c30512a091126823fe322ce7af7004fce701
Sunny Bains <Sunny.Bains@Oracle.Com>
2012-03-06 13:37:28 +1100

    WL#5522 - Don't rename the .ibd file if the tablespace is discarded.

--- storage/innobase/dict/dict0dict.cc
+++ storage/innobase/dict/dict0dict.cc
@@ -1409,7 +1409,7 @@ dict_table_rename_in_cache(
 	/* If the table is stored in a single-table tablespace, rename the
 	.ibd file */
 
-	if (table->space != 0) {
+	if (!dict_table_is_discarded(table) && table->space != TRX_SYS_SPACE) {
 		if (table->dir_path_of_temp_table != NULL) {
 			ut_print_timestamp(stderr);
 			fputs("  InnoDB: Error: trying to rename a"
