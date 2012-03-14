7254553a9158cad1366a1777582e1579add140b4
Sunny Bains <Sunny.Bains@Oracle.Com>
2012-03-14 17:56:19 +1100

    Bug 13830371 - NORMAL SHUTDOWN HANGS WITH INNODB-FORCE-RECOVERY>=2, WAITING FOR PURGE THREAD
    Fix the test for bug# 13628249. It should have used --send_shutdown instead of
    --shutdown_server 10. The latter sends a SIGKILL after 10 seconds. Due to the
    SIGKILL the test did not hang as it should have. Additionally, add a new purge
    state "PURGE_STATE_DISABLED".
    
    rb://973 Approved by Jimmy Yang.

--- storage/innobase/include/trx0purge.h
+++ storage/innobase/include/trx0purge.h
@@ -108,7 +108,8 @@ enum purge_state_t {
 	PURGE_STATE_INIT,		/*!< Purge instance created */
 	PURGE_STATE_RUN,		/*!< Purge should be running */
 	PURGE_STATE_STOP,		/*!< Purge should be stopped */
-	PURGE_STATE_EXIT		/*!< Purge has been shutdown */
+	PURGE_STATE_EXIT,		/*!< Purge has been shutdown */
+	PURGE_STATE_DISABLED		/*!< Purge was never started */
 };
 
 /*******************************************************************//**
