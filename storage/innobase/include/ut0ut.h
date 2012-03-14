0bc028b81b427b82612b8c59dac01ee99961fe4d
Sunny Bains <Sunny.Bains@Oracle.Com>
2012-03-15 10:15:39 +1100

    WL#5522 - Get rid of the OpenSolaris version of vasprintf(), use my_vnsprintf() instead on platforms that don't have vasprintf().

--- storage/innobase/include/ut0ut.h
+++ storage/innobase/include/ut0ut.h
@@ -436,11 +436,6 @@ a limited buffer. */
 # define ut_snprintf	snprintf
 #endif /* __WIN__ */
 
-#ifndef HAVE_VASPRINTF
-int vasprintf(char **str, const char *format, va_list ap);
-int asprintf(char **str, const char *format, ...);
-#endif /* !HAVE_VASPRINTF */
-
 /*************************************************************//**
 Convert an error number to a human readable text message. The
 returned string is static and should not be freed or modified.
