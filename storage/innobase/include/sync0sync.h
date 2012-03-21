6d6eb56cfddfdac5438e912da72c317b3aecfe00
Marko Mäkelä <marko.makela@oracle.com>
2012-03-21 12:45:12 +0200

    Add a feature test HAVE_IB_GCC_ATOMIC_BUILTINS_64 for GCC-supported 64-bit built-in atomic memory access. GCC 4.3 does not support 64-bit atomics on 32-bit x86. 4.4 and later do. The 64-bit atomics are needed in MONITOR_ATOMIC_INC, MONITOR_ATOMIC_DEC.

--- storage/innobase/include/sync0sync.h
+++ storage/innobase/include/sync0sync.h
@@ -119,8 +119,10 @@ extern mysql_pfs_key_t	srv_sys_mutex_key;
 extern mysql_pfs_key_t	srv_sys_tasks_mutex_key;
 #ifndef HAVE_ATOMIC_BUILTINS
 extern mysql_pfs_key_t	srv_conc_mutex_key;
-extern mysql_pfs_key_t	monitor_mutex_key;
 #endif /* !HAVE_ATOMIC_BUILTINS */
+#ifndef HAVE_ATOMIC_BUILTINS_64
+extern mysql_pfs_key_t	monitor_mutex_key;
+#endif /* !HAVE_ATOMIC_BUILTINS_64 */
 extern mysql_pfs_key_t	event_os_mutex_key;
 extern mysql_pfs_key_t	ut_list_mutex_key;
 extern mysql_pfs_key_t	os_mutex_key;
