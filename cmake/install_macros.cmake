9f54ae519e284d63734f5d0ced74d678da515f0f
Vladislav Vaintroub <wlad@montyprogram.com>
2012-03-22 15:37:52 +0100

    Fix CMake code to work with older cmake version, such as 2.6.2

--- cmake/install_macros.cmake
+++ cmake/install_macros.cmake
@@ -40,17 +40,23 @@ FUNCTION (INSTALL_DEBUG_SYMBOLS)
     IF(CMAKE_GENERATOR MATCHES "Visual Studio")
       STRING(REPLACE "${CMAKE_CFG_INTDIR}" "\${CMAKE_INSTALL_CONFIG_NAME}" pdb_location ${pdb_location})
     ENDIF()
-    IF(ARG_COMPONENT STREQUAL "Server" AND (target MATCHES "mysqld" OR type MATCHES "MODULE"))
-      #MESSAGE("PDB: ${targets}")
-      SET(comp Server)
-    ELSEIF(ARG_COMPONENT MATCHES Development 
-      OR ARG_COMPONENT MATCHES SharedLibraries 
-      OR ARG_COMPONENT MATCHES Embedded)
+    IF(ARG_COMPONENT STREQUAL "Server")
+      IF(target MATCHES "mysqld" OR type MATCHES "MODULE")
+        #MESSAGE("PDB: ${targets}")
+        SET(comp Server)
+      ENDIF()
+    ENDIF()
+ 
+    IF(NOT comp MATCHES Server)
+      IF(ARG_COMPONENT MATCHES Development
+        OR ARG_COMPONENT MATCHES SharedLibraries
+        OR ARG_COMPONENT MATCHES Embedded)
+        SET(comp Debuginfo)
+      ELSE()
+        SET(comp Debuginfo_archive_only) # not in MSI
+      ENDIF()	  
+    ENDIF()
 
-      SET(comp Debuginfo)
-    ELSE()
-      SET(comp Debuginfo_archive_only) # not in MSI
-    ENDIF()	  
     INSTALL(FILES ${pdb_location} DESTINATION ${ARG_INSTALL_LOCATION} COMPONENT ${comp})
   ENDFOREACH()
   ENDIF()
