5352a76134dcd421ef346ca2f9697188ec93c352
Inaam Rana <inaam.rana@oracle.com>
2012-03-26 09:45:01 -0400

    Bug#13704145: ELIMINATE LRU SCAN WHEN DROPPING A TABLE
    rb://942
    approved by: Marko Makela
    
    We don't need to scan LRU for dropping AHI entries when DROPing a table.
    AHI entries are already removed when we free up extents for the btree.

--- storage/innobase/include/buf0types.h
+++ storage/innobase/include/buf0types.h
@@ -63,6 +63,15 @@ enum buf_io_fix {
 					the flush_list */
 };
 
+/** Algorithm to remove the pages for a tablespace from the buffer pool.
+@See buf_LRU_flush_or_remove_pages(). */
+enum buf_remove_t {
+	BUF_REMOVE_ALL_NO_WRITE,	/*!< Remove all pages from the buffer
+					pool, don't write or sync to disk */
+	BUF_REMOVE_FLUSH_NO_WRITE,	/*!< Remove only, from the flush list,
+					don't write or sync to disk */
+};
+
 /** Parameters of binary buddy system for compressed pages (buf0buddy.h) */
 /* @{ */
 #define BUF_BUDDY_LOW_SHIFT	PAGE_ZIP_MIN_SIZE_SHIFT
