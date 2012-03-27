40737d5ee23d78bd1f0227f9217dc7dc4cf42b6a
Sunny Bains <Sunny.Bains@Oracle.Com>
2012-03-28 08:45:30 +1100

    Bug#13792503 - INNODB.INNODB_BUG60049 TEST FAILS ON TRUNK SPORADICALLY
    Force the purge coordinator to make one more pass at purging when
    the server state changes to shutdown.
    
    Add a truncate parameter to trx_purge() so that the purge coordinator
    thread (the caller) can decide when to truncate.
    
    rb://966 Approved by Jimmy Yang.  Ported from mysql-trunk:
    revision-id: sunny.bains@oracle.com-20120307050609-edb7al2wjs7tvesc

--- storage/innobase/trx/trx0purge.cc
+++ storage/innobase/trx/trx0purge.cc
@@ -1173,13 +1173,7 @@ void
 trx_purge_truncate(void)
 /*====================*/
 {
-	static ulint	count;
-
 	ut_ad(trx_purge_check_limit());
-
-	if (++count % TRX_SYS_N_RSEGS) {
-		return;
-	}
 
 	if (purge_sys->limit.trx_no == 0) {
 		trx_purge_truncate_history(&purge_sys->iter, purge_sys->view);
@@ -1197,8 +1191,9 @@ trx_purge(
 /*======*/
 	ulint	n_purge_threads,	/*!< in: number of purge tasks
 					to submit to the work queue */
-	ulint	batch_size)		/*!< in: the maximum number of records
+	ulint	batch_size,		/*!< in: the maximum number of records
 					to purge in one batch */
+	bool	truncate)		/*!< in: truncate history if true */
 {
 	que_thr_t*	thr = NULL;
 	ulint		n_pages_handled;
@@ -1263,11 +1258,9 @@ run_synchronously:
 		}
 	}
 
-	/* When shutdown is triggered it is possible for the worker threads
-        to have already exited via os_event_wait(). We only truncate the UNDO
-	log when we are 100% sure that all submitted tasks have completed. */
+	ut_a(purge_sys->n_submitted == purge_sys->n_completed);
 
-	if (purge_sys->n_submitted == purge_sys->n_completed) {
+	if (truncate) {
 		trx_purge_truncate();
 	}
 
