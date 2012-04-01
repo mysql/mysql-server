7c6f71cef11f345bc406a2e808dbe0a257fe17c9
Bradley C. Kuszmaul <bradley@tokutek.com>
2012-04-01 18:54:56 +0000

    close[t:4671] Annotate every malloc with {{{ANNOTATE_NEW_MEMORY()}}} and get rid of the {{{--check-stack-var-yes}}}.  This fixes for valgrind 3.7.0 but not for valgrind 3.6.1.  Closes #4671.
    git-svn-id: file:///svn/toku/tokudb@41430 c7de825b-a66e-492c-adef-691d508d4ae1

--- linux/memory.c
+++ linux/memory.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <malloc.h>
 #include <dlfcn.h>
+#include <valgrind/helgrind.h>
 #include "memory.h"
 #include "toku_assert.h"
 
@@ -108,6 +109,7 @@ void *
 toku_malloc(size_t size) {
     void *p = t_malloc ? t_malloc(size) : os_malloc(size);
     if (p) {
+	ANNOTATE_NEW_MEMORY(p, size);
         if (toku_memory_do_stats) {
             size_t used = my_malloc_usable_size(p);
             __sync_add_and_fetch(&status.malloc_count, 1);
@@ -118,7 +120,7 @@ toku_malloc(size_t size) {
     } else {
         __sync_add_and_fetch(&status.malloc_fail, 1);
     }
-    return p;
+  return p;
 }
 
 void *
@@ -185,6 +187,7 @@ toku_xmalloc(size_t size) {
     void *p = t_xmalloc ? t_xmalloc(size) : os_malloc(size);
     if (p == NULL)  // avoid function call in common case
         resource_assert(p);
+    ANNOTATE_NEW_MEMORY(p, size);
     if (toku_memory_do_stats) {
         size_t used = my_malloc_usable_size(p);
         __sync_add_and_fetch(&status.malloc_count, 1);
