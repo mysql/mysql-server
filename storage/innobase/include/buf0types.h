96e9dd03eac40d1a0abe32e62df39a41f83e79d3
Inaam Rana <inaam.rana@oracle.com>
2012-04-03 09:45:10 -0400

    Fix Sun Studio compiler warnings due to extra ',' introduced in rev_id: inaam.rana@oracle.com-20120326134501-23554a8vhjg0wqbm

--- storage/innobase/include/buf0types.h
+++ storage/innobase/include/buf0types.h
@@ -68,7 +68,7 @@ enum buf_io_fix {
 enum buf_remove_t {
 	BUF_REMOVE_ALL_NO_WRITE,	/*!< Remove all pages from the buffer
 					pool, don't write or sync to disk */
-	BUF_REMOVE_FLUSH_NO_WRITE,	/*!< Remove only, from the flush list,
+	BUF_REMOVE_FLUSH_NO_WRITE	/*!< Remove only, from the flush list,
 					don't write or sync to disk */
 };
 
