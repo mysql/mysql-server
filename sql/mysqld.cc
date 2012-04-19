edfcb97a61a04ccfe41eb5cd837f1a2c88175e87
Sergei Golubchik <sergii@pisem.net>
2012-04-19 04:02:28 +0200

    MDEV-220 MariaDB server 5.5 GA candidate has default storage engine MyISAM

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -3491,10 +3491,10 @@ static int init_common_variables()
     (except in the embedded server, where the default continues to
     be MyISAM)
   */
-#ifndef WITH_INNOBASE_STORAGE_ENGINE
-  default_storage_engine= const_cast<char *>("MyISAM");
-#else
+#if defined(WITH_INNOBASE_STORAGE_ENGINE) || defined(WITH_XTRADB_STORAGE_ENGINE)
   default_storage_engine= const_cast<char *>("InnoDB");
+#else
+  default_storage_engine= const_cast<char *>("MyISAM");
 #endif
 
   /*
