4a5a51b3aed6aeab496c747d7722df8155a0b0a9
Marko Mäkelä <marko.makela@oracle.com>
2012-05-02 16:09:58 +0300

    Non-functional changes.
    dict_mem_table_create(): Remove bogus comment about clustered tables,
    which were never implemented in InnoDB.
    
    create_table_def(): Remove unnecessary type casts.

--- storage/innobase/dict/dict0mem.cc
+++ storage/innobase/dict/dict0mem.cc
@@ -61,9 +61,7 @@ dict_mem_table_create(
 /*==================*/
 	const char*	name,	/*!< in: table name */
 	ulint		space,	/*!< in: space where the clustered index of
-				the table is placed; this parameter is
-				ignored if the table is made a member of
-				a cluster */
+				the table is placed */
 	ulint		n_cols,	/*!< in: number of columns */
 	ulint		flags,	/*!< in: table flags */
 	ulint		flags2)	/*!< in: table flags2 */
@@ -117,8 +115,6 @@ dict_mem_table_create(
 		table->fts = fts_create(table);
 		table->fts->cache = fts_cache_create(table);
 		fts_optimize_add_table(table);
-	} else {
-		table->fts = NULL;
 	}
 #endif /* !UNIV_HOTBACKUP */
 
