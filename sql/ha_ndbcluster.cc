f12cb4c0a9482237f1fc13061db9d7c0e0042fbb
Ole John Aske <ole.john.aske@oracle.com>
2012-05-03 12:42:49 +0200

    Backported some WL5940 related changes to 7.2-cluster:
      The handler extension ::test_push_flag() has now been removed.
      The two remaining HA_PUSH_<foo>-flags related to this extension
      has been made obsolete by:
    
      - HA_PUSH_BLOCK_CONST_TABLE is replaced with the 'test_flag'
        HA_BLOCK_CONST_TABLE. Furthermore it was identified that
        usage of this flag was obsolete at one of the places it was used.
        (As 'system' table optimization is not possible for handlers
         with non-EXACT statistics)
    
      - HA_PUSH_MULTIPLE_DEPENDENCY has been entirely removed as this
        covered a corner case with questionable advantage of being pushed.
        Previous changes to join_no_more_records() has been reverted.

--- sql/ha_ndbcluster.cc
+++ sql/ha_ndbcluster.cc
@@ -12480,6 +12480,19 @@ ulonglong ha_ndbcluster::table_flags(void) const
   */
   if (thd->variables.binlog_format == BINLOG_FORMAT_STMT)
     f= (f | HA_BINLOG_STMT_CAPABLE) & ~HA_HAS_OWN_BINLOGGING;
+
+  /**
+   * To maximize join pushability we want const-table optimization
+   * blocked if table is possibly pushable, that is:
+   *  - Variable 'ndb_join_pushdown= on'
+   *  - Lock mode is LM_CommittedRead
+   */
+  if (THDVAR(thd, join_pushdown) &&
+      get_ndb_lock_mode(m_lock.type) == NdbOperation::LM_CommittedRead)
+  {
+    f= f | HA_BLOCK_CONST_TABLE;
+  }
+
   return f;
 }
 
@@ -14664,57 +14677,6 @@ ha_ndbcluster::parent_of_pushed_join() const
   }
   return NULL;
 }
-
-bool
-ha_ndbcluster::test_push_flag(enum ha_push_flag flag) const
-{
-  DBUG_ENTER("test_push_flag");
-  switch (flag) {
-  case HA_PUSH_BLOCK_CONST_TABLE:
-  {
-    /**
-     * We don't support join push down if...
-     *   - not LM_CommittedRead
-     *   - uses blobs
-     */
-    THD *thd= current_thd;
-    if (unlikely(!THDVAR(thd, join_pushdown)))
-      DBUG_RETURN(false);
-
-    if (table->read_set != NULL && uses_blob_value(table->read_set))
-    {
-      DBUG_RETURN(false);
-    }
-
-    NdbOperation::LockMode lm= get_ndb_lock_mode(m_lock.type);
-
-    if (lm != NdbOperation::LM_CommittedRead)
-    {
-      DBUG_RETURN(false);
-    }
-
-    DBUG_RETURN(true);
-  }
-  case HA_PUSH_MULTIPLE_DEPENDENCY:
-    /**
-     * If any child operation within this pushed join refer 
-     * column values (paramValues), the pushed join has dependencies
-     * in addition to the root operation itself.
-     */
-    if (m_pushed_join_operation==PUSHED_ROOT &&
-        m_pushed_join_member->get_field_referrences_count() > 0)  // Childs has field refs
-    {
-      DBUG_RETURN(true);
-    }
-    DBUG_RETURN(false);
-
-  default:
-    DBUG_ASSERT(0);
-    DBUG_RETURN(false);
-  }
-  DBUG_RETURN(false);
-}
-
 
 /**
   @param[in] comment  table comment defined by user
