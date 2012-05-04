1037bc8f09d5673aa7534bab679e45395df79ed8
Rich Prohaska <prohaska@tokutek.com>
2012-05-04 22:04:44 +0000

    fix debug build
    git-svn-id: file:///svn/mysql/tokudb-engine/tokudb-engine@43074 c7de825b-a66e-492c-adef-691d508d4ae1

--- storage/tokudb/ha_tokudb.cc
+++ storage/tokudb/ha_tokudb.cc
@@ -6618,6 +6618,7 @@ row_format_to_row_type(srv_row_format_t row_format)
     case SRV_ROW_FORMAT_DEFAULT:
         return ROW_TYPE_DEFAULT;
     }
+    assert(0); return ROW_TYPE_DEFAULT;;
 }
 
 //
