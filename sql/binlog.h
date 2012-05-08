78839dff2cf3d2d48575fee4ce0124becca37cf5
Nuno Carvalho <nuno.carvalho@oracle.com>
2012-05-08 17:33:07 +0100

    BUG#13779291: RACE CONDITION AROUND ROTATE RELAY LOGS & FLUSH LOGS This is part 8 of the patch. Refactored MYSQL_BIN_LOG::write_incident(), added a new argument do_flush_and_sync.

--- sql/binlog.h
+++ sql/binlog.h
@@ -266,8 +266,10 @@ public:
 
   void set_write_error(THD *thd, bool is_transactional);
   bool check_write_error(THD *thd);
-  bool write_incident(THD *thd, bool lock);
-  bool write_incident(Incident_log_event *ev, bool lock);
+  bool write_incident(THD *thd, bool need_lock_log,
+                      bool do_flush_and_sync= true);
+  bool write_incident(Incident_log_event *ev, bool need_lock_log,
+                      bool do_flush_and_sync= true);
 
   void start_union_events(THD *thd, query_id_t query_id_param);
   void stop_union_events(THD *thd);
