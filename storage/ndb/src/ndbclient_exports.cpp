efe3d5ada07a78f7d41eae1588191aef8dcfe134
magnus.blaudd@oracle.com <>
2012-05-09 09:20:11 +0200

    ndb  - Change ndbclient_exports.cpp to use a function instead of 'array of    function pointers' since this is more flexible  - Add copyright header and comment

--- storage/ndb/src/ndbclient_exports.cpp
+++ storage/ndb/src/ndbclient_exports.cpp
@@ -1,9 +1,32 @@
+/*
+   Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+/*
+  This source file is compiled into the shared ndbclient
+  in order to tell the linker which functions to export.
+
+  NOTE! _ndbclient_exports() is not intended to be called
+*/
+
 #include "../include/ndbapi/NdbApi.hpp"
 
-typedef void (Ndb_cluster_connection::* fptr)(const char*);
-
-NDB_EXPORT fptr functions[] = {
-  (fptr)&Ndb_cluster_connection::set_name,
-  (fptr)&NdbScanFilter::isfalse,
-  0
-};
+void
+_ndbclient_exports(void)
+{
+  Ndb_cluster_connection cluster_connection;
+  NdbScanFilter scan_filter((NdbOperation*)0);
+}
