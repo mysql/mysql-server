6e26739734b2d155221e51187d5b16df32db2300
Zardosht Kasheff <zardosht@tokutek.com>
2012-05-22 12:42:33 +0000

    [t:4890], remove release_multi_operation_client_unlock code from TOKUTXN layer and handle it at ydb layer
    git-svn-id: file:///svn/toku/tokudb@43746 c7de825b-a66e-492c-adef-691d508d4ae1

--- ft/recover.c
+++ ft/recover.c
@@ -656,8 +656,7 @@ static int toku_recover_xcommit (struct logtype_xcommit *l, RECOVER_ENV renv) {
 
     // commit the transaction
     r = toku_txn_commit_with_lsn(txn, TRUE, recover_yield, NULL, l->lsn,
-				 NULL, NULL,
-				 false);
+				 NULL, NULL);
     assert(r == 0);
 
     // close the transaction
@@ -704,7 +703,7 @@ static int toku_recover_xabort (struct logtype_xabort *l, RECOVER_ENV renv) {
     assert(txn!=NULL);
 
     // abort the transaction
-    r = toku_txn_abort_with_lsn(txn, recover_yield, NULL, l->lsn, NULL, NULL, false);
+    r = toku_txn_abort_with_lsn(txn, recover_yield, NULL, l->lsn, NULL, NULL);
     assert(r == 0);
 
     // close the transaction
@@ -1252,7 +1251,7 @@ static void recover_abort_live_txns(RECOVER_ENV renv) {
 	int r = find_an_unprepared_txn (renv, &txn);
 	if (r==0) {
 	    // abort the transaction
-	    r = toku_txn_abort_txn(txn, recover_yield, NULL, NULL, NULL, false);
+	    r = toku_txn_abort_txn(txn, recover_yield, NULL, NULL, NULL);
 	    assert(r == 0);
 	    
 	    // close the transaction
