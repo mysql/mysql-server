08f360703f52f451a5985acb8411140adbff6105
Annamalai Gurusami <annamalai.gurusami@oracle.com>
2012-06-01 14:12:57 +0530

    Bug #13933132: [ERROR] GOT ERROR -1 WHEN READING TABLE APPEARED WHEN KILLING
    Suppose there is a query waiting for a lock.  If the user kills
    this query, then "Got error -1 when reading table" error message
    must not be logged in the server log file.  Since this is a user
    requested interruption, no spurious error message must be logged
    in the server log.  This patch will remove the error message from
    the log.
    
    approved by joh and tatjana

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -11791,7 +11791,8 @@ int report_error(TABLE *table, int error)
     Locking reads can legally return also these errors, do not
     print them to the .err log
   */
-  if (error != HA_ERR_LOCK_DEADLOCK && error != HA_ERR_LOCK_WAIT_TIMEOUT)
+  if (error != HA_ERR_LOCK_DEADLOCK && error != HA_ERR_LOCK_WAIT_TIMEOUT
+      && !table->in_use->killed)
     sql_print_error("Got error %d when reading table '%s'",
 		    error, table->s->path.str);
   table->file->print_error(error,MYF(0));
