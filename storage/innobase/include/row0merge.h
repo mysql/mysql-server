5c05bbb472405b86bcd3246b01f3943aa872c6c2
Marko Mäkelä <marko.makela@oracle.com>
2012-06-03 19:46:12 +0300

    WL#6255 cleanup in row0*.cc: Remove unused parameter altered_table.

--- storage/innobase/include/row0merge.h
+++ storage/innobase/include/row0merge.h
@@ -290,11 +290,9 @@ row_merge_build_indexes(
 	dict_index_t**	indexes,	/*!< in: indexes to be created */
 	const ulint*	key_numbers,	/*!< in: MySQL key numbers */
 	ulint		n_indexes,	/*!< in: size of indexes[] */
-	struct TABLE*	table,		/*!< in/out: MySQL table, for
+	struct TABLE*	table)		/*!< in/out: MySQL table, for
 					reporting erroneous key value
 					if applicable */
-	struct TABLE*	altered_table)	/*!< in/out: new MySQL table
-					definition */
 	__attribute__((nonnull, warn_unused_result));
 /********************************************************************//**
 Write a buffer to a block. */
