53b80730cb00f52ca644c7ed081e8afb2c879b45
Joerg Bruehe <joerg.bruehe@oracle.com>
2012-06-14 12:26:27 +0200

    Copyright text in the spec file for ULN RPMs.

--- SPECIFIC-ULN/mysql.spec.sh
+++ SPECIFIC-ULN/mysql.spec.sh
@@ -1,5 +1,24 @@
-# Copyright (c)    CORRECT TEXT TO BE DETERMINED  -  TODO / FIXME
 #
+#   This file was modified by Oracle in 2011 and later.
+#   Details of the modifications are described in the "changelog" section.
+#
+#   Modifications copyright (c) 2011, 2012, Oracle and/or its
+#   affiliates. All rights reserved.
+#
+#   This program is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation; version 2 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+#   02110-1301  USA
+
 
 %define mysql_version   @VERSION@
 %define release         1
