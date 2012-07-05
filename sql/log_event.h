298202f2fdf9a0dad88fb206d3012cf0608b8c98
Andrei Elkin <andrei.elkin@oracle.com>
2012-07-05 23:53:07 +0300

    merge bug14275000 fixes to 5.5: sql/log_event.h.

--- sql/log_event.h
+++ sql/log_event.h
@@ -2562,23 +2562,15 @@ public:
   Item_result type;
   uint charset_number;
   bool is_null;
-<<<<<<< TREE
   uchar flags;
 #ifdef MYSQL_SERVER
-=======
-#ifndef MYSQL_CLIENT
   bool deferred;
->>>>>>> MERGE-SOURCE
   User_var_log_event(THD* thd_arg, char *name_arg, uint name_len_arg,
                      char *val_arg, ulong val_len_arg, Item_result type_arg,
 		     uint charset_number_arg, uchar flags_arg)
     :Log_event(), name(name_arg), name_len(name_len_arg), val(val_arg),
     val_len(val_len_arg), type(type_arg), charset_number(charset_number_arg),
-<<<<<<< TREE
-    flags(flags_arg)
-=======
-    deferred(false)
->>>>>>> MERGE-SOURCE
+    flags(flags_arg), deferred(false)
     { is_null= !val; }
   void pack_info(Protocol* protocol);
 #else
