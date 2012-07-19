4c727297a9ec21d4964c855a95ad9f5fc107ebe6
Craig L Russell <Craig.Russell@oracle.com>
2012-07-18 17:33:21 -0700

    Implement Annotations methods:   strict   mapAllClasses   mapClass Add name to column mapping

--- Adapter/api/mynode.js
+++ Adapter/api/mynode.js
@@ -22,20 +22,85 @@ var spi = require("../impl/SPI.js");
 
 
 function Annotations() {
+  this.strictValue = undefined;
+  this.mapAllTablesValue = true;
+  this.mappings = [];
 }
+
+exports.Annotations = function() {
+  return new Annotations();
+};
+
+/** A Mapping holds the mapping for a single class */
+Mapping = function(proto, mapping) {
+  this.proto = proto;
+  this.mapping = mapping;
+};
+
+/** In strict mode, all parameters of mapping functions must be valid */
+Annotations.prototype.strict = function(value) {
+  if (value == true || value == false) {
+    this.strictValue = value;
+  }
+};
+
+/** Map all tables */
+Annotations.prototype.mapAllTables = function(value) {
+  if (value == true || value == false) {
+    this.mapAllTablesValue = value;
+  }
+};
+
+/** Map tables */
+Annotations.prototype.mapClass = function(proto, mapping) {
+  if (this.strictValue != false) {
+    for (x in mapping) {
+      switch (x) {
+      case 'table':
+      case 'schema':
+      case 'database':
+      case 'autoIncrementBatchSize':
+      case 'mapAllColumns':
+        break;
+      case 'field':
+      case 'fields':
+        // look inside the fields
+        var fields = mapping[x];
+        if (typeof(fields) != 'array') {
+          fields = [fields];
+        }
+        fields.forEach(function(field, index, mapping) {
+          for (x in field) {
+            switch(x) {
+            case 'name':
+            case 'nullValue':
+            case 'column':
+            case 'notPersistent':
+            case 'converter':
+              break;
+            default:
+              var err = new Error('bad property ' + x);
+              throw err;
+            }
+          }
+        });
+        break;
+      default:
+        var err = new Error('bad property ' + x);
+        throw err;
+      }
+    }
+  }
+  this.mappings.push(new Mapping(proto, mapping));
+};
 
 function spi_connect_sync(properties) {
   var db = spi.getDBServiceProvider(properties.implementation);
   return db.connectSync(properties);  
 }
 
-
-exports.Annotations = function() {
-  return new Annotations();
-};
-
-
 exports.ConnectionProperties = function(name) {
   var sp = spi.getDBServiceProvider(name);
   return sp.getDefaultConnectionProperties();
 };
+
