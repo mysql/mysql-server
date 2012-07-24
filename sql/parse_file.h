dfbcb045075797ba323a66b28cb03e290ba5b405
Praveenkumar Hulakund <praveenkumar.hulakund@oracle.com>
2012-07-24 12:32:51 +0530

    Bug#13819275 - Huge performance bottleneck because views definitions aren't cached
    Analysis:
    ----------
    Whenever view is opened, the view definition is read from
    the .frm file of view. And then this definition is used
    by the File_parser.
    Reading definition from .frm file every time is one of the 
    main reason of performance bottleneck.
    
    Fix:
    ----------
    When view is opened for the first time, TABLE_SHARE is
    created for it (But no open count is mentioned for it).
    The TABLE_SHARE for view is available in the table share 
    cache till there is enough space. Till its in the table 
    share cache, same TABLE_SHARE of view is used.
    
    To avoid reading view definition from the file every time,
    added new member in TABLE_SHARE to cache File_parser object
    for the view's .FRM file. So that next access to the same
    TABLE_SHARE need not have to read the definition from .FRM,
    but instead can reuse definition stored in the File_parser
    object.
    
    To verify the improvement in the performance, I used the
    same stored procedure provided in this report. Executed
    p1(100000) with view "v1" (say A)  and p1(100000) with 
    table "t1" (say B), with and without this fix.
    
    Delay in execution of A compared to B 
                      without fix= ~3.99 secs
                      with fix= ~1.66 secs
    
    Improvement in performance = ~58.39%
    
    We also tried to cache results of a later stage of view
    processing, i.e. cache results of File_parser::parse() call, 
    but straight forward implementation of this approach didn't 
    bring any noticiable improvement

--- sql/parse_file.h
+++ sql/parse_file.h
@@ -57,8 +57,8 @@ class Unknown_key_hook
 public:
   Unknown_key_hook() {}                       /* Remove gcc warning */
   virtual ~Unknown_key_hook() {}              /* Remove gcc warning */
-  virtual bool process_unknown_string(char *&unknown_key, uchar* base,
-                                      MEM_ROOT *mem_root, char *end)= 0;
+  virtual bool process_unknown_string(const char *&unknown_key, uchar* base,
+                                      MEM_ROOT *mem_root, const char *end)= 0;
 };
 
 
@@ -68,18 +68,20 @@ class File_parser_dummy_hook: public Unknown_key_hook
 {
 public:
   File_parser_dummy_hook() {}                 /* Remove gcc warning */
-  virtual bool process_unknown_string(char *&unknown_key, uchar* base,
-                                      MEM_ROOT *mem_root, char *end);
+  virtual bool process_unknown_string(const char *&unknown_key, uchar* base,
+                                      MEM_ROOT *mem_root, const char *end);
 };
 
 extern File_parser_dummy_hook file_parser_dummy_hook;
 
-bool get_file_options_ulllist(char *&ptr, char *end, char *line,
-                              uchar* base, File_option *parameter,
+bool get_file_options_ulllist(const char *&ptr, const char *end,
+                              const char *line, uchar* base,
+                              File_option *parameter,
                               MEM_ROOT *mem_root);
 
-char *
-parse_escaped_string(char *ptr, char *end, MEM_ROOT *mem_root, LEX_STRING *str);
+const char *
+parse_escaped_string(const char *ptr, const char *end, MEM_ROOT *mem_root,
+                     LEX_STRING *str);
 
 class File_parser;
 File_parser *sql_parse_prepare(const LEX_STRING *file_name,
@@ -95,18 +97,18 @@ my_bool rename_in_schema_file(THD *thd,
 
 class File_parser: public Sql_alloc
 {
-  char *buff, *start, *end;
+  const char *start, *end;
   LEX_STRING file_type;
   my_bool content_ok;
 public:
-  File_parser() :buff(0), start(0), end(0), content_ok(0)
+  File_parser() :start(0), end(0), content_ok(0)
     { file_type.str= 0; file_type.length= 0; }
 
   my_bool ok() { return content_ok; }
-  LEX_STRING *type() { return &file_type; }
+  const LEX_STRING *type() const { return &file_type; }
   my_bool parse(uchar* base, MEM_ROOT *mem_root,
 		struct File_option *parameters, uint required,
-                Unknown_key_hook *hook);
+                Unknown_key_hook *hook) const;
 
   friend File_parser *sql_parse_prepare(const LEX_STRING *file_name,
 					MEM_ROOT *mem_root,
