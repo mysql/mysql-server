c65932be49747c48e6489c36835a5b755cf5a2c6
Annamalai Gurusami <annamalai.gurusami@oracle.com>
2012-07-25 13:51:39 +0530

    Bug #13113026 INFORMATION_SCHEMA.INNODB_BUFFER_PAGE_LRUFROM 5.6 BACKPORT
    Backporting the WL#5716, "Information schema table for InnoDB 
    buffer pool information". Backporting revisions 2876.244.113, 
    2876.244.102 from mysql-trunk.
    
    rb://1175 approved by Jimmy Yang.

--- storage/innodb_plugin/include/log0log.h
+++ storage/innodb_plugin/include/log0log.h
@@ -41,6 +41,9 @@ Created 12/9/1995 Heikki Tuuri
 #include "sync0rw.h"
 #endif /* !UNIV_HOTBACKUP */
 
+/* Type used for all log sequence number storage and arithmetics */
+typedef ib_uint64_t		lsn_t;
+
 /** Redo log buffer */
 typedef struct log_struct	log_t;
 /** Redo log group */
