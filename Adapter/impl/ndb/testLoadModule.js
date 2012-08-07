c1dea2dde84c58a1a9ebaefae6b49fd02bb800fd
John David Duncan <john.duncan@oracle.com>
2012-08-06 17:08:15 -0700

    Add NdbDictionary.js; implement more of NdbCOnnectionPool and NdbSession

--- Adapter/impl/ndb/testLoadModule.js
+++ Adapter/impl/ndb/testLoadModule.js
@@ -1,5 +1,10 @@
 require("../../api/mynode");
 var dbdl = require("../build/Release/common/debug_dlopen");
 
-console.log(dbdl.debug_dlopen("../build/Release/ndb/ndbapi.node"));
+console.log(dbdl.debug_dlopen("../build/Release/ndb/ndb_adapter.node"));
+
+var ada = require("../build/Release/ndb/ndb_adapter.node");
+
+console.dir(ada);
+ 
 
