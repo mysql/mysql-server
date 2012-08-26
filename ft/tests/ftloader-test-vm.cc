d4a333643e0a3a15baa6cbf6d1c79f3a528cbe85
Zardosht Kasheff <zardosht@tokutek.com>
2012-08-26 18:08:01 +0000

    refs #5406, remove tabs from file
    git-svn-id: file:///svn/toku/tokudb@47205 c7de825b-a66e-492c-adef-691d508d4ae1

--- ft/tests/ftloader-test-vm.cc
+++ ft/tests/ftloader-test-vm.cc
@@ -13,31 +13,31 @@ bool verbose=false;
 static void test_cachetable_reservation (long size) {
     CACHETABLE ct;
     {
-	int r = toku_create_cachetable(&ct, size, ZERO_LSN, NULL);
-	assert(r==0);
+        int r = toku_create_cachetable(&ct, size, ZERO_LSN, NULL);
+        assert(r==0);
     }
     {
-	uint64_t r0 = toku_cachetable_reserve_memory(ct, 0.5);
-	uint64_t r0_bound = size/2 + size/16;
-	uint64_t r1 = toku_cachetable_reserve_memory(ct, 0.5);
-	uint64_t r1_bound = r0_bound/2;
-	uint64_t r2 = toku_cachetable_reserve_memory(ct, 0.5);
-	uint64_t r2_bound = r1_bound/2;
-	if (verbose) printf("%10ld: r0=%10" PRIu64 " r1=%10" PRIu64 " r2=%10" PRIu64 "\n", size, r0, r1, r2);
-	assert(r0 <= r0_bound);
-	assert(r1 <= r1_bound);
-	assert(r2 <= r2_bound);
-	assert(r1 <= r0);
-	assert(r2 <= r1);
+        uint64_t r0 = toku_cachetable_reserve_memory(ct, 0.5);
+        uint64_t r0_bound = size/2 + size/16;
+        uint64_t r1 = toku_cachetable_reserve_memory(ct, 0.5);
+        uint64_t r1_bound = r0_bound/2;
+        uint64_t r2 = toku_cachetable_reserve_memory(ct, 0.5);
+        uint64_t r2_bound = r1_bound/2;
+        if (verbose) printf("%10ld: r0=%10" PRIu64 " r1=%10" PRIu64 " r2=%10" PRIu64 "\n", size, r0, r1, r2);
+        assert(r0 <= r0_bound);
+        assert(r1 <= r1_bound);
+        assert(r2 <= r2_bound);
+        assert(r1 <= r0);
+        assert(r2 <= r1);
 
-	long unreservable_part = size * 0.25;
-	assert(r0 <= (size - unreservable_part)*0.5);
-	assert(r1 <= (size - unreservable_part - r0)*0.5);
-	assert(r2 <= (size - unreservable_part - r0 -1)*0.5);
+        long unreservable_part = size * 0.25;
+        assert(r0 <= (size - unreservable_part)*0.5);
+        assert(r1 <= (size - unreservable_part - r0)*0.5);
+        assert(r2 <= (size - unreservable_part - r0 -1)*0.5);
     }
     {
-	int r = toku_cachetable_close(&ct);
-	assert(r==0);
+        int r = toku_cachetable_close(&ct);
+        assert(r==0);
     }
     
 }
