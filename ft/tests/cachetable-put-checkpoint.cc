6ad69c2c1a51847a25893f913122e89b76bb923c
Zardosht Kasheff <zardosht@tokutek.com>
2012-08-27 01:42:36 +0000

    refs #5407, modify a test, no progress on actual bug yet
    git-svn-id: file:///svn/toku/tokudb@47208 c7de825b-a66e-492c-adef-691d508d4ae1

--- ft/tests/cachetable-put-checkpoint.cc
+++ ft/tests/cachetable-put-checkpoint.cc
@@ -143,7 +143,7 @@ static void move_number_to_child(
         &v1,
         &s1,
         wc, fetch, def_pf_req_callback, def_pf_callback,
-        PL_WRITE_EXPENSIVE,
+        PL_WRITE_CHEAP,
         NULL,
         1, //num_dependent_pairs
         &f1,
@@ -190,7 +190,7 @@ static void *move_numbers(void *arg) {
             &v1,
             &s1,
             wc, fetch, def_pf_req_callback, def_pf_callback,
-            PL_WRITE_EXPENSIVE,
+            PL_WRITE_CHEAP,
             NULL,
             0, //num_dependent_pairs
             NULL,
@@ -256,7 +256,7 @@ static void merge_and_split_child(
         &v1,
         &s1,
         wc, fetch, def_pf_req_callback, def_pf_callback,
-        PL_WRITE_EXPENSIVE,
+        PL_WRITE_CHEAP,
         NULL,
         1, //num_dependent_pairs
         &f1,
@@ -290,7 +290,7 @@ static void merge_and_split_child(
         &v1,
         &s1,
         wc, fetch, def_pf_req_callback, def_pf_callback,
-        PL_WRITE_EXPENSIVE,
+        PL_WRITE_CHEAP,
         NULL,
         2, //num_dependent_pairs
         cfs,
@@ -368,7 +368,7 @@ static void *merge_and_split(void *arg) {
             &v1,
             &s1,
             wc, fetch, def_pf_req_callback, def_pf_callback,
-            PL_WRITE_EXPENSIVE,
+            PL_WRITE_CHEAP,
             NULL,
             0, //num_dependent_pairs
             NULL,
