674db3a65aec8dbe52f08c4ac05e4321589725a7
Sunny Bains <Sunny.Bains@Oracle.Com>
2012-08-29 09:58:58 +1000

    WL#6445 - Disable opening of RBS when --innodb-read-only is set. Ignore flush to disk requests from MySQL server layer when --innodb-read-only is set. Add some more assertions to catch requests for writes when in read-only-mode. Don't attempt to fix the .isl information during tablespace open if read-only mode is set.

--- storage/innobase/srv/srv0start.cc
+++ storage/innobase/srv/srv0start.cc
@@ -2405,7 +2405,8 @@ innobase_start_or_create_for_mysql(void)
 
 	if (srv_available_undo_logs == ULINT_UNDEFINED) {
 		/* Can only happen if force recovery is set. */
-		ut_a(srv_force_recovery >= SRV_FORCE_NO_TRX_UNDO);
+		ut_a(srv_force_recovery >= SRV_FORCE_NO_TRX_UNDO
+		     || srv_read_only_mode);
 		srv_undo_logs = ULONG_UNDEFINED;
 	}
 
