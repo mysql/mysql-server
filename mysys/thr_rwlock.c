d92649592f542f6939d9ca5fe29fef67113de096
Marc Alff <marc.alff@oracle.com>
2012-09-01 00:49:28 +0200

    Bug#13915200 - SET GLOBAL DEBUG CAUSES CRASHES IN DBUG.C, RACE CONDITIONS
    Backport from mysql-trunk to mysql-5.6

--- mysys/thr_rwlock.c
+++ mysys/thr_rwlock.c
@@ -317,8 +317,13 @@ int my_rw_unlock(my_rw_lock_t *rwp)
     return srw_unlock(rwp);
 #endif
 
-  DBUG_PRINT("rw_unlock",
-	     ("state: %d waiters: %d", rwp->state, rwp->waiters));
+  /*
+    The DBUG api uses rw locks to protect global debug settings. Calling into
+    the DBUG api from here can cause a deadlock.
+  
+    DBUG_PRINT("rw_unlock", ("state: %d waiters: %d",
+               rwp->state, rwp->waiters));
+  */
   pthread_mutex_lock(&rwp->lock);
 
   DBUG_ASSERT(rwp->state != 0);
