9cc6c5c295f3c1ea56bd256b6c8478465e3cb574
Sunny Bains <Sunny.Bains@Oracle.Com>
2012-09-14 13:15:17 +1000

    WL#6445 - On Windows always default to segment 0 when read-only is set.

--- storage/innobase/os/os0file.cc
+++ storage/innobase/os/os0file.cc
@@ -4790,7 +4790,9 @@ os_aio_windows_handle(
 	DWORD		len;
 	BOOL		retry		= FALSE;
 
-	if (!srv_read_only_mode && segment == ULINT_UNDEFINED) {
+	if (srv_read_only_mode) {
+		segment = os_aio_get_array_and_local_segment(&array, 0);
+	} else if (segment == ULINT_UNDEFINED) {
 		segment = 0;
 		array = os_aio_sync_array;
 	} else {
