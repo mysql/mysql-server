2a9fe06eb0800e77d281f5b965474e219b3e4720
John David Duncan <john.duncan@oracle.com>
2012-09-16 09:26:00 -0700

    Logging/Debug output redesign, step 1. Now the JavaScript layer sends the messages to stdout and can run without any native code modules.

--- Adapter/impl/src/unified_debug.cpp
+++ Adapter/impl/src/unified_debug.cpp
@@ -26,62 +26,28 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#include <v8.h>
+
+#include "js_wrapper_macros.h"
 #include "adapter_global.h"
 
 /* Undefine UNIFIED_DEBUG here so macros are not expanded 
    and uni_debug is not declared as an extern
 */
-int uni_debug;
+int uni_debug        = 0;
+int udeb_level       = 0;
+int udeb_initialized = 0;
+
 #undef UNIFIED_DEBUG
 #include "unified_debug.h"
 
+using namespace v8;
 
-/* These static variables are initialized to zero */
-int udeb_mode;    // initially UDEB_ALL, i.e. zero
-int udeb_level = UDEB_DEBUG;
-int debug_fd = STDERR_FILENO;
-unsigned char bit_index[UDEB_SOURCE_FILE_BITMASK_BYTES];
-const char * levelstr[4] = {"INFO", "DEBUG", "DETAIL", "META"};
+Persistent<Function> JSLoggerFunction;
 
-int udeb_lookup(const char *);
-void udeb_internal_print(const char *fmt, va_list ap);
-void udeb_private_print(const char *fmt, ...);
 
-void udeb_switch(int i) {
-  switch(i) {
-    case 0:
-    case 1:
-      uni_debug = i;
-      break;
-    
-    case UDEB_INFO:
-    case UDEB_DEBUG:
-    case UDEB_DETAIL:
-    case UDEB_META:
-      udeb_level = i;
-      if(uni_debug) {
-        const char * lstr = levelstr[i - UDEB_INFO];
-        udeb_private_print("Setting debug output level to %s", lstr);
-      }
-      break;
-    
-    default:
-      break;
-  }
-}
+/////  Internal Utility Functions
 
-/* uni_dbg() is used by the dynamically loaded v8 module, 
-   which may fail to load if it tries to directly access 
-   uni_debug as an int.
-*/
-int uni_dbg() {
-  return uni_debug;
-}
-
-/* libc's basename(3) is not thread-safe, so we implement a version here.
-   This one is essentially a strlen() function that also remembers the final
-   path separator
-*/
 inline const char * udeb_basename(const char *path) {
   const char * last_sep = 0;
   if(path) {  
@@ -98,89 +64,6 @@ inline const char * udeb_basename(const char *path) {
 }
 
 
-/* udeb_internal_print is our fprintf() 
-*/
-inline void udeb_internal_print(const char *fmt, va_list args) {
-  int sz = 0;
-  char message[UDEB_MSG_BUF];
-
-  sz += vsnprintf(message + sz, UDEB_MSG_BUF - sz, fmt, args);
-  sprintf(message + sz++, "\n");
-  write(debug_fd, message, sz);
-}
-
-
-inline void udeb_private_print(const char *fmt, ...) {
-  va_list args;
-  va_start(args, fmt);
-  udeb_internal_print(fmt, args);
-  va_end(args);
-}
-
-
-/* udeb_print() is used by macros in the public API 
-*/ 
-void udeb_print(const char *src_path, int level, const char *fmt, ...) {
-  va_list args;
-  va_start(args, fmt);
-  
-  const char * src_file = udeb_basename(src_path);
-  if(udeb_level >= level && udeb_lookup(src_file))
-    udeb_internal_print(fmt, args);
-  
-  va_end(args);  
-}
-
-
-void udeb_enter(int level, const char *src_path, const char *function, int line) {
-  if(udeb_level >= level) {
-    const char *src_file = udeb_basename(src_path);
-    if(udeb_lookup(src_file)) {
-      udeb_private_print("Enter: %27s - line %d - %s", function, line, src_file);
-    }
-  }
-}
-
-
-void udeb_trace(const char *src_path, int line) {
-  if(udeb_level >= UDEB_DETAIL) {
-    const char *src_file = udeb_basename(src_path);
-    if(udeb_lookup(src_file)) {
-      udeb_private_print("  Trace: %27s line %d", ".....", line);
-    }
-  }
-}
-
-
-void udeb_leave(int level, const char *src_path, const char *function) {
-  if(udeb_level >= level) {
-    const char *src_file = udeb_basename(src_path);
-    if(udeb_lookup(src_file)) {
-      udeb_private_print("  Leave: %25s", function);
-    }
-  }
-}
-
-
-void unified_debug_destination(const char * out_file) {
-  int fd = open(out_file, O_APPEND | O_CREAT, 0644);
-  
-  if(fd < 0) {
-    fd = STDERR_FILENO;     /* Print to previous destination: */
-    udeb_private_print("Unified Debug: failed to open \"%s\" for output: %s",
-                        out_file, strerror(errno));
-  }
-  debug_fd = fd;
-}
-
-
-void unified_debug_close() {
-  if(debug_fd != STDERR_FILENO) {
-    close(debug_fd);
-  }
-}
-
-
 // Bernstein hash
 inline int udeb_hash(const char *name) {
   const unsigned char *p;
@@ -191,66 +74,75 @@ inline int udeb_hash(const char *name) {
 
   h = h % UDEB_SOURCE_FILE_BITMASK_BITS;
 
-  if(udeb_level == UDEB_META) 
-    udeb_private_print("udeb_hash: %s --> %d", name, h);
-
   return h;
 }
 
 
-inline int index_read(unsigned int bit_number) {
-  unsigned short byte = bit_number / 8;
-  unsigned char  mask = 1 << ( bit_number % 8);
-  return bit_index[byte] & mask;
-}
+////// The Logging API for C:  udeb_print() and udeb_enter() 
 
-inline void index_set(unsigned int bit_number) {
-  unsigned short byte = bit_number / 8;
-  unsigned char  mask = 1 << ( bit_number % 8);
-  bit_index[byte] |= mask;
-}
-
-inline void index_clear(unsigned int bit_number) {
-  unsigned short byte = bit_number / 8;
-  unsigned char  mask = ((1 << ( bit_number % 8)) ^ 0xFF);
-  bit_index[byte] &= mask;
-}
-
-void udeb_select(const char *file, int cmd) {
-  if(udeb_level == UDEB_META) 
-    udeb_private_print("udeb_select: %s %d", file ? file : "NULL", cmd);
-
-  if(file) {
-    if(cmd == UDEB_ADD)       index_set(udeb_hash(udeb_basename(file)));
-    else if(cmd == UDEB_DROP) index_clear(udeb_hash(udeb_basename(file)));
-    else abort();
-  }
-  else udeb_mode = cmd;
- 
-  return; 
-}
-
-inline int udeb_lookup(const char *key) {
-  int response;
+/* udeb_print() is used by macros in the public API 
+*/ 
+void udeb_print(const char *src_path, int level, const char *fmt, ...) {
+  int sz = 0;
+  char message[UDEB_MSG_BUF];
+  va_list args;
+  va_start(args, fmt);
   
-  switch(udeb_mode) {
-    case UDEB_NONE: 
-      response = false;
-      break;
-    case UDEB_ALL_BUT_SELECTED:
-      response = ! (index_read(udeb_hash(key)));
-      break;
-    case UDEB_NONE_BUT_SELECTED:
-      response =  index_read(udeb_hash(key));
-      break;
-    default:
-      response = true;
-      break;
-  }
+  const char * src_file = udeb_basename(src_path);
+  if(udeb_initialized && udeb_level >= level) {
+    /* Construct the message */
+    sz += vsnprintf(message + sz, UDEB_MSG_BUF - sz, fmt, args);
+    sprintf(message + sz++, "\n");  // also writes the null terminator.
+  
+    /* Send it to JavaScript */
+    Handle<Value> jsArgs[3];
+    jsArgs[0] = Number::New(level);
+    jsArgs[1] = String::New(src_file);
+    jsArgs[2] = String::New(message, sz);
+    JSLoggerFunction->Call(Context::GetCurrent()->Global(), 3, jsArgs);    
+  }  
+  va_end(args);  
+}
 
-  if(udeb_level == UDEB_META) 
-    udeb_private_print("udeb_lookup: %s --> %s", key, response ? "T" : "F");
 
-  return response;
+/************************* The JavaScript API ***********************
+ * setLevel():   JS tells C the global state and level.
+ * setLogger():  JS introduces itself to C and provides a logging function
+ ***************/
+/* libc's basename(3) is not thread-safe, so we implement a version here.
+   This one is essentially a strlen() function that also remembers the final
+   path separator
+*/
+
+Handle<Value> udeb_setLogger(const Arguments &args) {
+  HandleScope scope;
+  
+  Local<Function> f = Function::Cast(* (args[0]));
+  JSLoggerFunction = Persistent<Function>::New(f);
+  udeb_initialized = 1;
+
+  Handle<Value> argv[3];
+  argv[0] = Number::New(UDEB_INFO);
+  argv[1] = String::New("unified_debug.cpp");
+  argv[2] = String::New("C++ unified_debug enabled");
+
+  JSLoggerFunction->Call(Context::GetCurrent()->Global(), 3, argv);
+  return scope.Close(True());
+}
+
+
+Handle<Value> udeb_setLevel(const Arguments &args) {
+  HandleScope scope;
+  
+  udeb_level = args[0]->Int32Value();
+  // C code cannot log below UDEB_INFO
+  uni_debug = (udeb_level > UDEB_NOTICE) ? 1 : 0;  
+  return scope.Close(True());
+}
+
+
+void udebug_initOnLoad(Handle<Object> target) {
+  DEFINE_JS_FUNCTION(target, "setLogger", udeb_setLogger);
+  DEFINE_JS_FUNCTION(target, "setLevel" , udeb_setLevel );
 }
 
