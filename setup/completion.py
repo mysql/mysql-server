4f702f2815425e72a1a829e56e350325ec6b0ac8
John David Duncan <john.duncan@oracle.com>
2012-09-18 18:26:11 -0700

    Tab-completion of filenames now works during setup

--- setup/completion.py
+++ setup/completion.py
@@ -4,33 +4,35 @@ import readline
 
 class Completer(object):
 
-    def _listdir(self, root):
-        "List directory 'root' appending the path separator to subdirs."
-        res = []
-        for name in os.listdir(root):
-            path = os.path.join(root, name)
-            if os.path.isdir(path):
-                name += os.sep
-            res.append(name)
-        return res
+  def __init__(self):
+    readline.set_completer_delims(' \t\n;')
+    readline.parse_and_bind("tab: complete")
 
-    def _complete_path(self, path=None):
-        "Perform completion of filesystem path."
-        if not path:
-            return self._listdir('/')
-        dirname, rest = os.path.split(path)
-        tmp = dirname if dirname else '.'
-        res = [os.path.join(dirname, p)
-                for p in self._listdir(tmp) if p.startswith(rest)]
-        # more than one match, or single match which does not exist (typo)
-        if len(res) > 1 or not os.path.exists(path):
-            return res
-        # resolved to a single directory, so return list of files below it
-        if os.path.isdir(path):
-            return [os.path.join(path, p) for p in self._listdir(path)]
-        # exact file match terminates this completion
-        return [path + ' ']
+  def ls(self, dir):
+    list = []
+    for name in os.listdir(dir):
+      path = os.path.join(dir, name)
+      if os.path.isdir(path):
+        name += os.sep
+      list.append(name)
+    return list
 
-    def complete(self, text, state):
-        "readline completion entry point."
- 
+  def complete_path(self, path=None):
+    if not path:
+      return self.ls('.')
+    dirname, rest = os.path.split(path)
+    tmp = dirname if dirname else '.'
+    result = [os.path.join(dirname, p)
+              for p in self.ls(tmp) if p.startswith(rest)]
+    # more than one match, or single match which does not exist (typo)
+    if len(result) > 1 or not os.path.exists(path):
+      return result
+    # resolved to a single directory, so return list of its files
+    if os.path.isdir(path):
+      return [os.path.join(path, p) for p in self.ls(path)]
+    # exact match
+    return [path + ' ']
+
+  def complete(self, text, state):
+    "readline completion entry point."
+    return self.complete_path(text)[state]
