22c5ffde30008c8f9127db60a99812cd311860ab
Sergei Golubchik <sergii@pisem.net>
2012-09-25 20:23:01 +0200

    a simple pam user mapper module

--- plugin/auth_pam/testing/pam_mariadb_mtr.c
+++ plugin/auth_pam/testing/pam_mariadb_mtr.c
@@ -10,7 +10,7 @@
   Create /etc/pam.d/mariadb_mtr with
 =========================================================
 auth            required        pam_mariadb_mtr.so pam_test
-account         required        pam_mariadb_mtr.so
+account         required        pam_permit.so
 =========================================================
 */
 
@@ -21,9 +21,8 @@ account         required        pam_mariadb_mtr.so
 
 #define N 3
 
-PAM_EXTERN int
-pam_sm_authenticate(pam_handle_t *pamh, int flags,
-    int argc, const char *argv[])
+int pam_sm_authenticate(pam_handle_t *pamh, int flags,
+                        int argc, const char *argv[])
 {
   struct pam_conv *conv;
   struct pam_response *resp = 0;
@@ -69,17 +68,8 @@ ret:
   return retval;
 }
 
-PAM_EXTERN int
-pam_sm_setcred(pam_handle_t *pamh, int flags,
-    int argc, const char *argv[])
-{
-
-    return PAM_SUCCESS;
-}
-
-PAM_EXTERN int
-pam_sm_acct_mgmt(pam_handle_t *pamh, int flags,
-    int argc, const char *argv[])
+int pam_sm_setcred(pam_handle_t *pamh, int flags,
+                   int argc, const char *argv[])
 {
 
     return PAM_SUCCESS;
