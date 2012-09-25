2f7d7c9f7f7b8da68de4bed21f167e316c600a0e
unknown <sanja@montyprogram.com>
2012-09-25 13:45:11 +0300

    makes mi_test_all.sh & ma_test_all.sh working (MDEV-285)

--- storage/maria/ma_test_all.sh
+++ storage/maria/ma_test_all.sh
@@ -7,13 +7,22 @@
 #
 #
 
+PRG='unittest/ma_test_all-t'
+UTST='../../unittest/unit.pl'
+
+if [ ! -x $PRG ] ; then
+  DIR=`dirname $0`
+  PRG="$DIR/unittest/ma_test_all-t"
+  UTST="$DIR/../../unittest/unit.pl"
+fi
+
 if test -n "$1"; then
 
   # unit.pl can't pass options to ma_test_all-t, so if anything
   # was passed as an argument, assume the purpose was to pass
   # them to ma_test_all-t and call it directly
 
-  unittest/ma_test_all-t $@
+  $PRG $@
 else
-  perl ../../unittest/unit.pl run unittest/ma_test_all-t
+  perl $UTST run $PRG
 fi
--- storage/myisam/mi_test_all.sh
+++ storage/myisam/mi_test_all.sh
@@ -99,7 +99,7 @@ if test -f mi_test1$MACH ; then suffix=$MACH ; else suffix=""; fi
 # check of myisampack / myisamchk
 ./myisampack$suffix --force -s test1
 # Ignore error for index file
-./myisamchk$suffix -es test1 2>&1 >& /dev/null
+./myisamchk$suffix -es test1 2>&1 > /dev/null
 ./myisamchk$suffix -rqs test1
 ./myisamchk$suffix -es test1
 ./myisamchk$suffix -rs test1
