781ea074908481adfdf79396509fa9becd51973e
Martin Hansson <martin.hansson@oracle.com>
2012-10-11 16:12:29 +0200

    Worklog #6073: Preliminary patch
    Fixes a bug in the (empty) initialization of the error message
    array. The loop failed to set the last error message's string. This
    will cause a core dump if we want to unit test code that raises the
    last error (with code ER_ERROR_LAST). In unit testing we don't read
    the error messages so we rely on the empty initialization.

---
+++ unittest/gunit/thd-t.cc
@@ -0,0 +1,45 @@
+/* Copyright (c) 2011, 2012, Oracle and/or its affiliates. All rights reserved.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+// First include (the generated) my_config.h, to get correct platform defines.
+#include "my_config.h"
+#include <gtest/gtest.h>
+
+#include "test_utils.h"
+#include "sql_class.h"
+
+namespace {
+
+using my_testing::Server_initializer;
+using my_testing::Mock_error_handler;
+
+// Unit test fixture to test the initialization of the THD object
+class ThdTest : public ::testing::Test
+{
+protected:
+  virtual void SetUp() { initializer.SetUp(); }
+  virtual void TearDown() { initializer.TearDown(); }
+
+  THD *thd() { return initializer.thd(); }
+
+  Server_initializer initializer;
+};
+
+TEST_F(ThdTest, errorMessages)
+{
+  EXPECT_STREQ("", thd()->variables.lc_messages->errmsgs->errmsgs[ER_ERROR_LAST - ER_ERROR_FIRST]);
+}
+
+}
