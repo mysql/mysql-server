0b63c6719df32869acb214244d2bcb00e0ffc03c
Norvald H. Ryeng <norvald.ryeng@oracle.com>
2012-10-15 10:12:21 +0200

    Bug#14601664 ASSERTION FAILED: *REF && (*REF)->FIXED, FILE ITEM.CC
    Problem: A query with a subquery with an outer reference and a
    subquery with the same outer reference causes an assertion to fire.
    
    The outermost query has an Item_field that is referred to by the inner
    queries. The outer reference in the middle query resolves to this
    field, and the Item_field in the middle query is replaced by an
    Item_outer_ref. This Item_outer_ref is added to the inner_refs_list by
    Item_field::fix_outer_field() so that it can be fixed later by
    fix_inner_refs(). The outer reference in the innermost query resolves
    to the middle query's Item_outer_ref, but an assertion fires in
    Item_field::fix_outer_field() or Item_ref::fix_fields(), depending on
    the type of the item in the innermost query, since the referenced item
    is not fixed yet.
    
    Fix: Open up assertions to allow references to unfixed
    Item_outer_refs, similar to what was done in
    resolve_ref_in_select_and_group() by patch for bug #13735050.

--- sql/item.cc
+++ sql/item.cc
@@ -4797,6 +4797,28 @@ static Item** find_field_in_group_list(Item *find_item, ORDER *group_list)
 
 
 /**
+  Check if an Item is fixed or is an Item_outer_ref.
+
+  @param ref the reference to check
+
+  @return Whether or not the item is a fixed item or an Item_outer_ref
+*/
+static bool is_fixed_or_outer_ref(Item *ref)
+{
+  /*
+    The requirements are that the Item pointer
+    1)  is not NULL, and
+    2a) points to a fixed Item, or
+    2b) points to an Item_outer_ref.
+  */
+  return (ref != NULL &&                     // 1
+          (ref->fixed ||                     // 2a
+           (ref->type() == Item::REF_ITEM && // 2b
+            static_cast<Item_ref *>(ref)->ref_type() == Item_ref::OUTER_REF)));
+}
+
+
+/**
   Resolve a column reference in a sub-select.
 
   Resolve a column reference (usually inside a HAVING clause) against the
@@ -4897,10 +4919,7 @@ resolve_ref_in_select_and_group(THD *thd, Item_ident *ref, SELECT_LEX *select)
        Assert if its an incorrect reference . We do not assert if its a outer
        reference, as they get fixed later in fix_innner_refs function.
       */
-      DBUG_ASSERT((*select_ref)->fixed ||
-                  ((*select_ref)->type() == Item::REF_ITEM &&
-                   ((Item_ref *)(*select_ref))->ref_type() ==
-                   Item_ref::OUTER_REF));
+      DBUG_ASSERT(is_fixed_or_outer_ref(*select_ref));
 
       return &select->ref_pointer_array[counter];
     }
@@ -5101,7 +5120,11 @@ Item_field::fix_outer_field(THD *thd, Field **from_field, Item **reference)
         return -1; /* Some error occurred (e.g. ambiguous names). */
       if (ref != not_found_item)
       {
-        DBUG_ASSERT(*ref && (*ref)->fixed);
+        /*
+          Either the item we found is already fixed, or it is an outer
+          reference that will be fixed later in fix_inner_refs().
+        */
+        DBUG_ASSERT(is_fixed_or_outer_ref(*ref));
         prev_subselect_item->used_tables_cache|= (*ref)->used_tables();
         prev_subselect_item->const_item_cache&= (*ref)->const_item();
         break;
@@ -5144,7 +5167,7 @@ Item_field::fix_outer_field(THD *thd, Field **from_field, Item **reference)
     Item_ref *rf;
 
     /* Should have been checked in resolve_ref_in_select_and_group(). */
-    DBUG_ASSERT(*ref && (*ref)->fixed);
+    DBUG_ASSERT(is_fixed_or_outer_ref(*ref));
     /*
       Here, a subset of actions performed by Item_ref::set_properties
       is not enough. So we pass ptr to NULL into Item_[direct]_ref
@@ -7151,7 +7174,7 @@ bool Item_ref::fix_fields(THD *thd, Item **reference)
             goto error; /* Some error occurred (e.g. ambiguous names). */
           if (ref != not_found_item)
           {
-            DBUG_ASSERT(*ref && (*ref)->fixed);
+            DBUG_ASSERT(is_fixed_or_outer_ref(*ref));
             prev_subselect_item->used_tables_cache|= (*ref)->used_tables();
             prev_subselect_item->const_item_cache&= (*ref)->const_item();
             break;
@@ -7283,7 +7306,7 @@ bool Item_ref::fix_fields(THD *thd, Item **reference)
         goto error;
       }
       /* Should be checked in resolve_ref_in_select_and_group(). */
-      DBUG_ASSERT(*ref && (*ref)->fixed);
+      DBUG_ASSERT(is_fixed_or_outer_ref(*ref));
       mark_as_dependent(thd, last_checked_context->select_lex,
                         context->select_lex, this, this);
       /*
