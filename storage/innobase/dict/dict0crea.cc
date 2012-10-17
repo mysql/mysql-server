0ba93a6e28c891f3978865d3e395e874fbb495a9
Krunal Bauskar krunal.bauskar@oracle.com <>
2012-10-17 15:59:15 +0530

    missed out change during conflict resolution

--- storage/innobase/dict/dict0crea.cc
+++ storage/innobase/dict/dict0crea.cc
@@ -287,7 +287,6 @@ dict_build_tablespace(
 
 	ut_ad(mutex_own(&(dict_sys->mutex)));
 
-	table = node->table;
 	use_tablespace = DICT_TF2_FLAG_IS_SET(table, DICT_TF2_USE_TABLESPACE);
 
 	dict_hdr_get_new_id(table, &table->id, NULL, NULL);
