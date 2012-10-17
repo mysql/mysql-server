404e88a52bd63397931f8a5c76097db607f9ee68
Vasil Dimov <vasil.dimov@oracle.com>
2012-10-17 14:15:30 +0300

    Fix Bug#14705287 ASSERTION FAILURE IN THREAD 140369867454208 IN FILE DATA0TYPE.IC LINE 602
    Deny the ALTER TABLE command if we are going to end up with too many
    columns in the table.
    
    Also lift the limit from the artificial 1000 (and remove the hard coded
    magic number 1000), to the actual limit
    of REC_MAX_N_FIELDS - DATA_N_SYS_COLS (1020) columns, add a new macro
    REC_MAX_N_USER_FIELDS for that.
    
    Approved by:	Marko, Joh (rb:1393)

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -59,6 +59,7 @@ this program; if not, write to the Free Software Foundation, Inc.,
 
 #include "trx0sys.h"
 #include "mtr0mtr.h"
+#include "rem0types.h"
 #include "row0ins.h"
 #include "row0mysql.h"
 #include "row0sel.h"
@@ -9369,11 +9370,8 @@ ha_innobase::create(
 	DBUG_ASSERT(thd != NULL);
 	DBUG_ASSERT(create_info != NULL);
 
-	if (form->s->fields > 1000) {
-		/* The limit probably should be REC_MAX_N_FIELDS - 3 = 1020,
-		but we play safe here */
-
-		DBUG_RETURN(HA_ERR_TO_BIG_ROW);
+	if (form->s->fields > REC_MAX_N_USER_FIELDS) {
+		DBUG_RETURN(HA_ERR_TOO_MANY_FIELDS);
 	} else if (srv_read_only_mode) {
 		DBUG_RETURN(HA_ERR_TABLE_READONLY);
 	}
