867a8733df041baee317cc7ec450b2e0e5195ff9
Tatjana Azundris Nuernberg <tatjana.nuernberg@oracle.com>
2012-10-22 09:08:04 +0100

    Bug#14236124: WRONG CONNECTION ID (THREAD ID) IN THE GENERAL AND SLOW QUER
    Optional add-on to previous patch which corrected logging to file)
    wherein we change width of thread-ID field in slow and general log
    tables from INT to BIGINT (4 to 8 bytes), for correct logging of
    very large thread-IDs.
    
    Thread-IDs can get very large; patch adjusts general-logging
    and slow query logging to reflect that and log those IDs correctly.
    
    Backport to 5.6.

--- sql/log.h
+++ sql/log.h
@@ -339,7 +339,7 @@ public:
   MYSQL_QUERY_LOG() : last_time(0) {}
   void reopen_file();
   bool write(time_t event_time, const char *user_host,
-             uint user_host_len, int thread_id,
+             uint user_host_len, my_thread_id thread_id,
              const char *command_type, uint command_type_len,
              const char *sql_text, uint sql_text_len);
   bool write(THD *thd, time_t current_time, time_t query_start_arg,
@@ -386,7 +386,7 @@ public:
   virtual bool log_error(enum loglevel level, const char *format,
                          va_list args)= 0;
   virtual bool log_general(THD *thd, time_t event_time, const char *user_host,
-                           uint user_host_len, int thread_id,
+                           uint user_host_len, my_thread_id thread_id,
                            const char *command_type, uint command_type_len,
                            const char *sql_text, uint sql_text_len,
                            const CHARSET_INFO *client_cs)= 0;
@@ -413,7 +413,7 @@ public:
   virtual bool log_error(enum loglevel level, const char *format,
                          va_list args);
   virtual bool log_general(THD *thd, time_t event_time, const char *user_host,
-                           uint user_host_len, int thread_id,
+                           uint user_host_len, my_thread_id thread_id,
                            const char *command_type, uint command_type_len,
                            const char *sql_text, uint sql_text_len,
                            const CHARSET_INFO *client_cs);
@@ -445,7 +445,7 @@ public:
   virtual bool log_error(enum loglevel level, const char *format,
                          va_list args);
   virtual bool log_general(THD *thd, time_t event_time, const char *user_host,
-                           uint user_host_len, int thread_id,
+                           uint user_host_len, my_thread_id thread_id,
                            const char *command_type, uint command_type_len,
                            const char *sql_text, uint sql_text_len,
                            const CHARSET_INFO *client_cs);
