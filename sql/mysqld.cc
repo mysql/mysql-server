01a560abf5a77fe086722c16c7afbbe23f2644dc
Jon Olav Hauglid <jon.hauglid@oracle.com>
2012-10-24 09:50:30 +0200

    Bug#14790408: ONLINE ALTER TABLE DOES NOT HAVE A DESCRIPTIVE STATUS
    The problem was that no thread states were set during
    execution of in-place ALTER TABLE. This made it difficult
    to monitor progress using e.g. SHOW PROCESSLIST.
    
    This patch fixes the problem by adding three new thread states:
    - "preparing for alter table"
    - "altering table"
    - "committing alter table to storage engine"
    
    These are set before calling corresponding handler API functions.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -9339,6 +9339,9 @@ static PSI_file_info all_server_files[]=
 
 PSI_stage_info stage_after_create= { 0, "After create", 0};
 PSI_stage_info stage_allocating_local_table= { 0, "allocating local table", 0};
+PSI_stage_info stage_alter_inplace_prepare= { 0, "preparing for alter table", 0};
+PSI_stage_info stage_alter_inplace= { 0, "altering table", 0};
+PSI_stage_info stage_alter_inplace_commit= { 0, "committing alter table to storage engine", 0};
 PSI_stage_info stage_changing_master= { 0, "Changing master", 0};
 PSI_stage_info stage_checking_master_version= { 0, "Checking master version", 0};
 PSI_stage_info stage_checking_permissions= { 0, "checking permissions", 0};
@@ -9444,6 +9447,9 @@ PSI_stage_info *all_server_stages[]=
 {
   & stage_after_create,
   & stage_allocating_local_table,
+  & stage_alter_inplace_prepare,
+  & stage_alter_inplace,
+  & stage_alter_inplace_commit,
   & stage_changing_master,
   & stage_checking_master_version,
   & stage_checking_permissions,
