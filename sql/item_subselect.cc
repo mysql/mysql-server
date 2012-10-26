974abc7ad832131e59d310730722bb4ecdb460e8
unknown <timour@askmonty.org>
2012-10-27 00:56:14 +0300

    MDEV-3812
    This patch undoes the removal of enum store_key_result by the previous patch for mdev-3812.

--- sql/item_subselect.cc
+++ sql/item_subselect.cc
@@ -3321,13 +3321,13 @@ bool subselect_uniquesubquery_engine::copy_ref_key(bool skip_constants)
 
   for (store_key **copy= tab->ref.key_copy ; *copy ; copy++)
   {
-    bool store_res;
+    enum store_key::store_key_result store_res;
     if (skip_constants && (*copy)->store_key_is_const())
       continue;
     store_res= (*copy)->copy();
     tab->ref.key_err= store_res;
 
-    if (store_res)
+    if (store_res == store_key::STORE_KEY_FATAL)
     {
       /*
        Error converting the left IN operand to the column type of the right
