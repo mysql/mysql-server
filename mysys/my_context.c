38e3fa961bb746d6d18576bd53e4d0a71187a022
Vladislav Vaintroub <wlad@montyprogram.com>
2012-11-04 22:20:04 +0100

    MDEV-3830 - fix compilation for Intel compiler, avoid .cfi_escape , 32 bit code.

--- mysys/my_context.c
+++ mysys/my_context.c
@@ -456,7 +456,7 @@ my_context_spawn(struct my_context *c, void (*f)(void *), void *d)
     (
      "movl %%esp, (%[save])\n\t"
      "movl %[stack], %%esp\n\t"
-#if __GNUC__ >= 4 && __GNUC_MINOR__ >= 4
+#if __GNUC__ >= 4 && __GNUC_MINOR__ >= 4 && !defined(__INTEL_COMPILER)
      /*
        This emits a DWARF DW_CFA_undefined directive to make the return address
        undefined. This indicates that this is the top of the stack frame, and
