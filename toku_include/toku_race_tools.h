870fcc2c73c424bca17a298b07f3dae098fb9ac2
John Esmet <esmet@tokutek.com>
2012-11-13 20:43:59 +0000

    refs #5351 put this in the race tools header, make sure it does not break a release build
    git-svn-id: file:///svn/toku/tokudb@49925 c7de825b-a66e-492c-adef-691d508d4ae1

--- toku_include/toku_race_tools.h
+++ toku_include/toku_race_tools.h
@@ -19,6 +19,28 @@
 # define TOKU_DRD_IGNORE_VAR(v) DRD_IGNORE_VAR(v)
 # define TOKU_DRD_STOP_IGNORING_VAR(v) DRD_STOP_IGNORING_VAR(v)
 
+/*
+ * How to make helgrind happy about tree rotations and new mutex orderings:
+ *
+ * // Tell helgrind that we unlocked it so that the next call doesn't get a "destroyed a locked mutex" error.
+ * // Tell helgrind that we destroyed the mutex.
+ * VALGRIND_HG_MUTEX_UNLOCK_PRE(&locka);
+ * VALGRIND_HG_MUTEX_DESTROY_PRE(&locka);
+ *
+ * // And recreate it.  It would be better to simply be able to say that the order on these two can now be reversed, because this code forgets all the ordering information for this mutex.
+ * // Then tell helgrind that we have locked it again.
+ * VALGRIND_HG_MUTEX_INIT_POST(&locka, 0);
+ * VALGRIND_HG_MUTEX_LOCK_POST(&locka);
+ *
+ * When the ordering of two locks changes, we don't need tell Helgrind about do both locks.  Just one is good enough.
+ */
+
+# define TOKU_VALGRIND_RESET_MUTEX_ORDERING_INFO(mutex)  \
+    VALGRIND_HG_MUTEX_UNLOCK_PRE(mutex); \
+    VALGRIND_HG_MUTEX_DESTROY_PRE(mutex); \
+    VALGRIND_HG_MUTEX_INIT_POST(mutex, 0); \
+    VALGRIND_HG_MUTEX_LOCK_POST(mutex);
+
 #else
 
 # define NVALGRIND 1
@@ -27,6 +49,7 @@
 # define TOKU_VALGRIND_HG_DISABLE_CHECKING(p, size) ((void) 0)
 # define TOKU_DRD_IGNORE_VAR(v)
 # define TOKU_DRD_STOP_IGNORING_VAR(v)
+# define TOKU_VALGRIND_RESET_MUTEX_ORDERING_INFO(mutex)
 
 #endif
 
