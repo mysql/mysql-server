eb5f06c7553361e7041fe29856362bc17ae6f518
kevin.lewis@oracle.com <>
2012-11-21 09:07:28 -0600

    Bug#15882860 - WINDOWS: DEBUG BUILD CAN'T HANDLE LARGE INNODB_LOG_FILE_SIZE
    The change for WL6494 (InnoDB: Handle redo log file size mismatch),
    exposed an existing problem on Windows 64-bit platforms where any _stat()
    call will fail on any file larger than 4Gb.  This became evident when
    setting -innodb-log-file-size=4G or larger.  The patch mentioned above
    started using os_file_rename_func() for redo log files.  That function
    makes calls to os_file_status() in debug using ut_ad() to make sure that
    the source name exists and the destination name does not.
    An assert, ut_ad(exists), failed because the call to _stat failed, since
    the file was too big to store the size in a 4 byte integer.
    
    The fix is to exchange _stat with _stat64 in two places where _stat() is
    called by Windows-only code.  The function _stat64() works on both 32-bit
    and 64-bit versions of Windows and the compatibility list goes all the way
    back to Windows 95.
    
    Approved at http://rb.no.oracle.com/rb/r/1583/ by Marko

--- storage/innobase/os/os0file.cc
+++ storage/innobase/os/os0file.cc
@@ -3003,9 +3003,9 @@ os_file_status(
 {
 #ifdef __WIN__
 	int		ret;
-	struct _stat	statinfo;
+	struct _stat64	statinfo;
 
-	ret = _stat(path, &statinfo);
+	ret = _stat64(path, &statinfo);
 	if (ret && (errno == ENOENT || errno == ENOTDIR)) {
 		/* file does not exist */
 		*exists = FALSE;
@@ -3078,9 +3078,9 @@ os_file_get_status(
 	int		ret;
 
 #ifdef __WIN__
-	struct _stat	statinfo;
+	struct _stat64	statinfo;
 
-	ret = _stat(path, &statinfo);
+	ret = _stat64(path, &statinfo);
 
 	if (ret && (errno == ENOENT || errno == ENOTDIR)) {
 		/* file does not exist */
