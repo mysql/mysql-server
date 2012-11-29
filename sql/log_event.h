490285bd7da16979704ec80fa2866113e1b0c250
Luis Soares <luis.soares@oracle.com>
2012-11-29 09:17:16 +0530

    BUG#15912728: MYSQLBINLOG + -D + GTIDS => MAY OUTPUT NON-TERMINATED TRANSACTIONS
    Addressed review comments.
    
    Added RESET MASTER to the test case to always start from 
    a clean state.

--- sql/log_event.h
+++ sql/log_event.h
@@ -825,8 +825,8 @@ typedef struct st_print_event_info
   /* Indicate if the body cache has unflushed events */
   bool have_unflushed_events;
 
-  /* 
-     True if an event was skipped while printing the events of 
+  /*
+     True if an event was skipped while printing the events of
      a transaction and no COMMIT statement or XID event was ever
      output (ie, was filtered out as well). This can be triggered
      by the --database option of mysqlbinlog.
