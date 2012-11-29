d2ec670be66d3f3cbbef37822a87360a0aff1f7a
Craig L Russell <Craig.Russell@oracle.com>
2012-11-29 11:15:41 -0800

    Implement persist, save, update, and remove   with table name and values; and constructor and values   for Session and Batch
    Session.js, Batch.js:
      For persist, save, and remove:
      Use first parameter as table indicator; it is a mapped domain object,
      or a table name, or a constructor
      If first parameter is a mapped domain object, there is only one parameter plus callback
      If first parameter is a table name or constructor, there is an extra parameter
      For update, there are two extra parameters: keys and values
    
    UserContext.js:
      For persist, save, update, and remove:
      Analyze first parameter and allow domain object, table name, or constructor
      Use cached table handler in case of multiple threads obtaining table metadata
    
    MySQLDictionary.js:
      Add a bit of debugging
    
    InsertTest.js:
      Add variants of persist: table name and constructor
    
    SaveTest.js:
      Remove illegal argument tests, which are now legal
      Add variants of save: table name and constructor
    
    UpdateTest.js:
      Remove illegal argument tests, which are now legal
      Add variants of update: table name and constructor
    
    UnmappedDomainObjectTest.js:
      Test unmapped domain object with persist, save, update, remove, and load
    
    driver.js:
      Fix lint errors
      Change --timer to --timeout; default to no timeout
    
    harness.js:
      Fix lint errors

--- test/t_basic/UpdateTest.js
+++ test/t_basic/UpdateTest.js
@@ -179,34 +179,171 @@ t4.run = function() {
   });
 };
 
-/***** Update illegal argument ***/
-var t5 = new harness.ConcurrentTest("testUpdateIllegalArgumentNumber");
+/***** Update ***/
+var t5 = new harness.ConcurrentTest("testUpdateTableName");
 t5.run = function() {
   var testCase = this;
+  // load the domain object 5220
+  var object = new global.t_basic(5220, 'Employee 5220', 5220, 5220);
+  var object2;
   fail_openSession(testCase, function(session) {
-    session.update(1, function(err, session2) {
+    // persist object 4020
+    session.persist(object, function(err, session2) {
       if (err) {
-        testCase.pass();
+        testCase.fail(err);
         return;
       }
-      testCase.fail(new Error('UpdateTest.t5 illegal argument should fail.'));
-    });
+      // now update the object with the same primary key but different magic
+      object2 = new global.t_basic(5220, 'Employee 5220', 5220, 5229);
+      session2.update('t_basic', {id: 5220}, object2, function(err, session3) {
+        if (err) {
+          testCase.fail(err);
+          return;
+        }
+        session3.find('t_basic', 5220, function(err, object3) {
+          // verify that object3 has updated magic field from object2
+          testCase.errorIfNotEqual('testSaveUpdate mismatch on magic', 5229, object3.magic);
+          testCase.failOnError();
+        });
+      }, session2);
+    }, session);
   });
 };
 
-/***** Update illegal argument ***/
-var t6 = new harness.ConcurrentTest("testUpdateIllegalArgumentString");
+/***** Batch update ***/
+var t6 = new harness.ConcurrentTest("testBatchUpdateTableName");
 t6.run = function() {
   var testCase = this;
+  var start_number = 5230;
+  var number_of_objects = 10;
+  // create objects
+  
   fail_openSession(testCase, function(session) {
-    session.update('t_basic', function(err, session2) {
+    var batch = session.createBatch();
+    for (i = start_number; i < start_number + number_of_objects; ++i) {
+      batch.persist(new global.t_basic(i, 'Employee ' + (i + 100), i + 100, i + 100), function(err) {
+        if (err) {
+          testCase.appendErrorMessage(err);
+        }
+      });
+    }
+    batch.execute(function(err, session2) {
       if (err) {
-        testCase.pass();
-        return;
+        testCase.appendErrorMessage(err);
       }
-      testCase.fail(new Error('UpdateTest.t6 illegal argument should fail.'));
-    });
+      var batch2 = session2.createBatch();
+      // update all fields in the objects
+      for (i = start_number; i < start_number + number_of_objects; ++i) {
+        var object = new global.t_basic(i, 'Employee ' + i, i, i);
+        batch2.update('t_basic', object, object, function(err) {
+          if (err) {
+            testCase.appendErrorMessage(err);
+          }
+        });
+      }
+      batch2.execute(function(err, session3) {
+        if (err) {
+         testCase.appendErrorMessage(err); 
+        }
+        var batch3 = session3.createBatch();
+        // find all objects and verify they have the proper state
+        for (i = start_number; i < start_number + number_of_objects; ++i) {
+          batch3.find(global.t_basic, i, global.verify_t_basic, i, testCase, true);
+        }
+        batch3.execute(function(err) {
+          if (err) {
+            testCase.appendErrorMessage(err);
+          }
+          testCase.failOnError();
+        });
+      }, session2);
+    }, session);
   });
 };
 
-module.exports.tests = [t1, t2, t3, t4, t5, t6];
+/***** Update ***/
+var t7 = new harness.ConcurrentTest("testPartialUpdateConstructor");
+t7.run = function() {
+  var testCase = this;
+  // load the domain object 5240
+  var object = new global.t_basic(5240, 'Employee 5240', 5240, 5240);
+  var object2;
+  fail_openSession(testCase, function(session) {
+    session.persist(object, function(err, session2) {
+      if (err) {
+        testCase.fail(err);
+        return;
+      }
+      // now update the object with the same primary key but different name
+      object2 = new global.t_basic(5240, 'Employee 5249');
+      delete object2.id;
+      session2.update(global.t_basic, {id:5240}, object2, function(err, session3) {
+        if (err) {
+          testCase.fail(err);
+          return;
+        }
+        session3.find('t_basic', 5240, function(err, object3) {
+          // verify that object3 has updated name field from object2
+          testCase.errorIfNotEqual('testPartialUpdate mismatch on name', 'Employee 5249', object3.name);
+          testCase.failOnError();
+        });
+      }, session2);
+    }, session);
+  });
+};
+
+/***** Batch update ***/
+var t8 = new harness.ConcurrentTest("testBatchPartialUpdateConstructor");
+t8.run = function() {
+  var testCase = this;
+  var start_number = 5250;
+  var number_of_objects = 10;
+  // create objects
+  
+  fail_openSession(testCase, function(session) {
+    var batch = session.createBatch();
+    for (i = start_number; i < start_number + number_of_objects; ++i) {
+      batch.persist(new global.t_basic(i, 'Employee ' + (i + 100), i, i), function(err) {
+        if (err) {
+          testCase.appendErrorMessage(err);
+        }
+      });
+    }
+    batch.execute(function(err, session2) {
+      if (err) {
+        testCase.appendErrorMessage(err);
+      }
+      var batch2 = session2.createBatch();
+      // update only the name
+      for (i = start_number; i < start_number + number_of_objects; ++i) {
+        // update(constructor, key, value, callback)
+        var key = new global.t_basic(i);
+        var value = new global.t_basic(i, 'Employee ' + i);
+        delete value.id;
+        batch2.update(global.t_basic, key, value, function(err) {
+          if (err) {
+            testCase.appendErrorMessage(err);
+          }
+        });
+      }
+      batch2.execute(function(err, session3) {
+        if (err) {
+         testCase.appendErrorMessage(err); 
+        }
+        var batch3 = session3.createBatch();
+        // find all objects and verify they have the proper state
+        for (i = start_number; i < start_number + number_of_objects; ++i) {
+          batch3.find(global.t_basic, i, global.verify_t_basic, i, testCase, true);
+        }
+        batch3.execute(function(err) {
+          if (err) {
+            testCase.appendErrorMessage(err);
+          }
+          testCase.failOnError();
+        });
+      }, session2);
+    }, session);
+  });
+};
+
+module.exports.tests = [t1, t2, t3, t4, t5, t6, t7, t8];
