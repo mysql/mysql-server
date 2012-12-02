1d122a5c24ccf91054149559d2453527f46991d1
Leif Walsh <leif@tokutek.com>
2012-12-02 17:26:26 +0000

    increase join_timeout for upgrade stress tests
    git-svn-id: file:///svn/toku/tokudb@50632 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/run_upgrade_stress_test.sh
+++ src/tests/run_upgrade_stress_test.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-if [[ $# -lt 5 ]]; then exit 1; fi
+if [[ $# -lt 6 ]]; then exit 1; fi
 
 bin=$1; shift
 saveddir=$1; shift
 envdir=$1; shift
 size=$1; shift
 time=$1; shift
+jointime=$1; shift
 
 rm -rf $envdir
 cp -r $saveddir $envdir
-$bin --only_stress --num_elements $size --num_seconds $time
+$bin --only_stress --num_elements $size --num_seconds $time --join_timeout $jointime
