280d20b013d13430db6a6e07bd26202464dd0214
Leif Walsh <leif@tokutek.com>
2012-12-06 14:48:28 +0000

    refs #5127 try to ignore build warnings in jemalloc source
    git-svn-id: file:///svn/toku/tokudb@50813 c7de825b-a66e-492c-adef-691d508d4ae1

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -226,7 +226,7 @@ list(APPEND CTEST_CUSTOM_WARNING_EXCEPTION
   "clang: warning: unknown warning option"
   # don't complain about warnings in jemalloc source
   "jemalloc-3.2.0/src"
-  "jemalloc/internal/jemalloc_internal.h"
+  "jemalloc/internal"
   # don't complain about ranlib or libtool on empty archive
   "has no symbols"
   "the table of contents is empty"
