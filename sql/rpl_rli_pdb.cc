35d50bfd731729daa99243e8c72c3a0ad5a3b108
Nuno Carvalho <nuno.carvalho@oracle.com>
2012-12-14 15:31:30 +0000

    BUG#15956714: REPLICATION DOESN'T WORK IN MYSQL 5.6.80-RC IF STARTED WITH SKIP-INNODB
    When a slave was started with --skip-innodb and info file repositories
    (default values), replication was incorrectly stopped. If slave is not
    doing a migration between info repositories and is using file
    repositories, replication can work flawless.
    
    When server is started with info file repositories and it is not doing a
    live migration, ignore errors when trying to open table info
    repositories.

--- sql/rpl_rli_pdb.cc
+++ sql/rpl_rli_pdb.cc
@@ -132,6 +132,7 @@ Slave_worker::~Slave_worker()
 int Slave_worker::init_worker(Relay_log_info * rli, ulong i)
 {
   DBUG_ENTER("Slave_worker::init_worker");
+  DBUG_ASSERT(!rli->info_thd->is_error());
   uint k;
   Slave_job_item empty= {NULL};
 
@@ -252,7 +253,8 @@ void Slave_worker::end_info()
   if (!inited)
     DBUG_VOID_RETURN;
 
-  handler->end_info();
+  if (handler)
+    handler->end_info();
 
   if (inited)
   {
