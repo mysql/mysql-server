a95dbea93c6a09f7c80499a2a793789ebe9c1b58
Krunal Bauskar krunal.bauskar@oracle.com <>
2012-12-14 13:56:05 +0530

    - WL#6469: updated comment to make it more cleared with an example

--- storage/innobase/dict/dict0boot.cc
+++ storage/innobase/dict/dict0boot.cc
@@ -81,14 +81,25 @@ dict_hdr_get_new_id(
 
 	mtr_start(&mtr);
 
-	/* Turning off REDO logging; Is this a problem as id is persisted to
-	system-tablespace header ?
-	No. Here is the reason why ?
-	- let's say REDO logging is done for the id and crash happens then
-	using the REDO logging new updated id can be restored.
-	- applying this case for temp-table. On crash-restore, temp-table
-	don't exist and so even if id is not restored that is fine as there
-	is no object corresponding to that id. */
+	/* Server started and let's say space-id = x
+	- table created with file-per-table
+	- space-id = x + 1
+	- crash
+	Case 1: If it was redo logged then we know that it will be
+		restored to x + 1
+	Case 2: if not redo-logged
+		Header will have the old space-id = x
+		This is OK because on restart there is no object with
+		space id = x + 1
+	Case 3:
+		space-id = x (on start)
+		space-id = x+1 (temp-table allocation) - no redo logging
+		space-id = x+2 (non-temp-table allocation), this get's
+			   redo logged.
+		If there is a crash there will be only 2 entries
+		x (original) and x+2 (new) and disk hdr will be updated
+		to reflect x + 2 entry.
+		We cannot allocate the same space id to different objects. */
 	if (is_temp_table) {
 		mtr_set_log_mode(&mtr, MTR_LOG_NO_REDO);
 	}
