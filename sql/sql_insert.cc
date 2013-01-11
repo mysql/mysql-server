928e8bcdf0c8340b672f4c7cc695be38d3b01c3d
Mattias Jonsson <mattias.jonsson@oracle.com>
2013-01-12 00:45:39 +0100

    Bug#16076275: HANDLER::HA_END_BULK_IN CREATE TABLE .. SELECT, STATEMENT BINLOGGING
    Regression from wl#4443.
    
    Due to reorganization of select_create::prepare2()/create_table_from_items.
    
    If locking fails, the temporary table is dropped, but the pointer 'table' is
    not reset. So during select_insert::abort_result_set() since table is not NULL
    it tries to use the pointer which is no longer valid.
    
    Fixed by resetting table to NULL after it was dropped.

--- sql/sql_insert.cc
+++ sql/sql_insert.cc
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2000, 2012, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2000, 2013, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -4127,6 +4127,7 @@ select_create::prepare2()
       extra_lock= 0;
     }
     drop_open_table(thd, table, create_table->db, create_table->table_name);
+    table= 0;
     DBUG_RETURN(1);
   }
   if (extra_lock)
