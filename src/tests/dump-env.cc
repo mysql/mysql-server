dfe8a5600b95640504da09de8cf485bb10e800d6
Leif Walsh <leif@tokutek.com>
2013-01-17 16:26:29 +0000

    refs #5902 svn keywords for dump-env
    git-svn-id: file:///svn/toku/tokudb@52191 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/dump-env.cc
+++ src/tests/dump-env.cc
@@ -1,6 +1,6 @@
 /* -*- mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
 // vim: ft=cpp:expandtab:ts=8:sw=4:softtabstop=4:
-#ident "$Id: cursor-more-than-a-leaf-provdel.cc 49851 2012-11-12 00:43:22Z esmet $"
+#ident "$Id$"
 #ident "Copyright (c) 2007 Tokutek Inc.  All rights reserved."
 #include "test.h"
 #include <db.h>
