784947cdd72e8795007c763faaca6a7c1fbda06e
Marko Mäkelä <marko.makela@oracle.com>
2013-01-28 13:18:49 +0200

    Bug#16227539 INCORRECT CHECK FOR INNODB_READ_ONLY IN ALTER TABLE
    At the start of ha_innobase::prepare_inplace_alter_table() and
    ha_innobase::inplace_alter_table() there is a check for
    srv_read_only_mode. If InnoDB is in read-only mode, these methods
    would return a successful status and do nothing.
    
    But, the srv_read_only_mode=true should have been blocked already in
    ha_innobase::check_if_supported_inplace_alter().
    
    Replace the bogus conditions with debug assertions similar to that in
    ha_innobase::commit_inplace_alter_table().

--- storage/innobase/handler/handler0alter.cc
+++ storage/innobase/handler/handler0alter.cc
@@ -3261,10 +3261,7 @@ ha_innobase::prepare_inplace_alter_table(
 	DBUG_ENTER("prepare_inplace_alter_table");
 	DBUG_ASSERT(!ha_alter_info->handler_ctx);
 	DBUG_ASSERT(ha_alter_info->create_info);
-
-	if (srv_read_only_mode) {
-		DBUG_RETURN(false);
-	}
+	DBUG_ASSERT(!srv_read_only_mode);
 
 	MONITOR_ATOMIC_INC(MONITOR_PENDING_ALTER_TABLE);
 
@@ -3833,10 +3830,7 @@ ha_innobase::inplace_alter_table(
 	dberr_t	error;
 
 	DBUG_ENTER("inplace_alter_table");
-
-	if (srv_read_only_mode) {
-		DBUG_RETURN(false);
-	}
+	DBUG_ASSERT(!srv_read_only_mode);
 
 #ifdef UNIV_SYNC_DEBUG
 	ut_ad(!rw_lock_own(&dict_operation_lock, RW_LOCK_EX));
