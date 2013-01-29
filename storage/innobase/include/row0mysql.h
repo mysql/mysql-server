98d72ef5719f397150d37abbb62d1ec53802247b
Sunny Bains <Sunny.Bains@Oracle.Com>
2013-01-30 01:36:45 +1100

    WL#6047 - Minor non-functional changes.

--- storage/innobase/include/row0mysql.h
+++ storage/innobase/include/row0mysql.h
@@ -870,7 +870,7 @@ struct row_prebuilt_t {
 	const rec_t*	innodb_api_rec;	/*!< InnoDB API search result */
 	/*----------------------*/
 	ulint		n_rows_read;	/*!< Number of rows read */
-	/*----------------------*/
+
 	ulint		magic_n2;	/*!< this should be the same as
 					magic_n */
 };
