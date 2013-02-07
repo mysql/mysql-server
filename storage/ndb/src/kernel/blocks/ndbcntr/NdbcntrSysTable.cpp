7b096c2e6fc1c4094cf038f51dfd0b23a6846a05
Mauritz Sundell <mauritz.sundell@oracle.com>
2013-02-07 10:56:23 +0100

    ndb - fixed narrowing compiler warnings

--- storage/ndb/src/kernel/blocks/ndbcntr/NdbcntrSysTable.cpp
+++ storage/ndb/src/kernel/blocks/ndbcntr/NdbcntrSysTable.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2011, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2011, 2013, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -39,7 +39,7 @@ Ndbcntr::g_sysTable_SYSTAB_0 = {
   arrayLength(column_SYSTAB_0), column_SYSTAB_0,
   DictTabInfo::SystemTable,
   DictTabInfo::HashMapPartition,
-  true, ~0, ~0
+  true, UINT_MAX32, UINT_MAX32
 };
 
 // NDB$EVENTS_0
@@ -90,7 +90,7 @@ Ndbcntr::g_sysTable_NDBEVENTS_0 = {
   arrayLength(column_NDBEVENTS_0), column_NDBEVENTS_0,
   DictTabInfo::SystemTable,
   DictTabInfo::HashMapPartition,
-  true, ~0, ~0
+  true, UINT_MAX32, UINT_MAX32
 };
 
 // all
