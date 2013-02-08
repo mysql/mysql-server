e958fe3dcb5822fc3181d7ce5c099db280d6b612
John David Duncan <john.duncan@oracle.com>
2013-02-07 20:38:53 -0800

    Fix compiler warnings in Stockholm scheduler.

--- storage/ndb/memcache/src/schedulers/Stockholm.cc
+++ storage/ndb/memcache/src/schedulers/Stockholm.cc
@@ -205,13 +205,17 @@ ENGINE_ERROR_CODE Scheduler_stockholm::schedule(workitem *newitem) {
     case op_not_supported:
       response_code = ENGINE_ENOTSUP;
       break;
-    case op_failed:
-      response_code = ENGINE_FAILED;
-      break;
-    case op_overflow:
+   case op_overflow:
       response_code = ENGINE_E2BIG;  // ENGINE_FAILED ?
       break;
-  }
+    case op_bad_key:
+      response_code = ENGINE_EINVAL;
+      break;
+    case op_failed:
+    default:
+      response_code = ENGINE_FAILED;
+      break;
+   }
 
   return response_code;
 }
