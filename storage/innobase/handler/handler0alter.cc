ddaa5b5ed957ba81cdd7aa913d98cc4a55b0c00b
Marko Mäkelä <marko.makela@oracle.com>
2013-02-14 17:15:07 +0200

    Bug#16310273 BUG WITH ALTER TABLE AUTO_INCREMENT
    To be compatible with ALGORITHM=COPY, InnoDB has to mimic the
    behaviour in ALGORITHM=INPLACE.  Instead of setting the AUTO_INCREMENT
    value to the user-specified one, it has to set it to the maximum of
    the user-specified value and MAX(auto_increment_column)+1.
    
    commit_get_autoinc(): Get the maximum value.
    
    rb#2005 approved by Kevin Lewis

--- storage/innobase/handler/handler0alter.cc
+++ storage/innobase/handler/handler0alter.cc
@@ -4491,6 +4491,8 @@ commit_get_autoinc(
 	const TABLE*		old_table,
 	dict_table_t*		user_table)
 {
+	ulonglong			max_autoinc;
+
 	ha_innobase_inplace_ctx*	ctx
 		= static_cast<ha_innobase_inplace_ctx*>
 		(ha_alter_info->handler_ctx);
@@ -4500,30 +4502,41 @@ commit_get_autoinc(
 	if (!altered_table->found_next_number_field) {
 		/* There is no AUTO_INCREMENT column in the table
 		after the ALTER operation. */
-		DBUG_RETURN(0);
+		max_autoinc = 0;
 	} else if (ctx && ctx->add_autoinc != ULINT_UNDEFINED) {
 		/* An AUTO_INCREMENT column was added. Get the last
 		value from the sequence, which may be based on a
 		supplied AUTO_INCREMENT value. */
-		DBUG_RETURN(ctx->sequence.last());
+		max_autoinc = ctx->sequence.last();
 	} else if ((ha_alter_info->handler_flags
 		    & Alter_inplace_info::CHANGE_CREATE_OPTION)
 		   && (ha_alter_info->create_info->used_fields
 		       & HA_CREATE_USED_AUTO)) {
 		/* An AUTO_INCREMENT value was supplied, but the table
-		was not rebuilt. Get the user-supplied value. */
-		DBUG_RETURN(ha_alter_info->create_info->auto_increment_value);
+		was not rebuilt. Get the user-supplied value or the
+		last value from the sequence. */
+		ut_ad(old_table->found_next_number_field);
+
+		max_autoinc = ha_alter_info->create_info->auto_increment_value;
+		if (ctx && max_autoinc < ctx->sequence.last()) {
+			max_autoinc = ctx->sequence.last();
+		}
+
+		dict_table_autoinc_lock(user_table);
+		if (max_autoinc < user_table->autoinc) {
+			max_autoinc = user_table->autoinc;
+		}
+		dict_table_autoinc_unlock(user_table);
 	} else {
 		/* An AUTO_INCREMENT value was not specified.
 		Read the old counter value from the table. */
-		ulonglong	max_autoinc;
-
 		ut_ad(old_table->found_next_number_field);
 		dict_table_autoinc_lock(user_table);
-		max_autoinc = dict_table_autoinc_read(user_table);
+		max_autoinc = user_table->autoinc;
 		dict_table_autoinc_unlock(user_table);
-		DBUG_RETURN(max_autoinc);
 	}
+
+	DBUG_RETURN(max_autoinc);
 }
 
 /** Add or drop foreign key constraints to the data dictionary tables,
@@ -5588,6 +5601,7 @@ foreign_fail:
 @param thd - the session
 @param start_value - the lower bound
 @param max_value - the upper bound (inclusive) */
+UNIV_INTERN
 ib_sequence_t::ib_sequence_t(
 	THD*		thd,
 	ulonglong	start_value,
@@ -5624,6 +5638,7 @@ ib_sequence_t::ib_sequence_t(
 /**
 Postfix increment
 @return the next value to insert */
+UNIV_INTERN
 ulonglong
 ib_sequence_t::operator++(int) UNIV_NOTHROW
 {
