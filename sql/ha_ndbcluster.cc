ce5c1240f3e70896783613e542102f1140d7004f
magnus.blaudd@oracle.com <>
2013-02-21 14:18:37 +0100

    ndbcluster
     - cleanup comments
     - remove unnecessary variable

--- sql/ha_ndbcluster.cc
+++ sql/ha_ndbcluster.cc
@@ -4965,23 +4965,20 @@ int ha_ndbcluster::ndb_write_row(uchar *record,
   if (m_use_write &&
       !haveConflictFunction) // Conflict detection must use normal insert()
   {
-    /* Should we use the supplied table writeset or not?
-     * For a REPLACE command, we should ignore it, and write
-     * all columns to get correct REPLACE behaviour.
-     * For applying Binlog events, we need to use the writeset
-     * to avoid trampling unchanged columns when an update is
-     * logged as a WRITE
-     */
-    bool useWriteSet= applying_binlog(thd);
     uchar* mask;
 
-    if (useWriteSet)
+    if (applying_binlog(thd))
     {
+      /*
+        Use write_set when applying binlog to avoid trampling
+        unchanged columns
+      */
       user_cols_written_bitmap= table->write_set;
       mask= (uchar *)(user_cols_written_bitmap->bitmap);
     }
     else
     {
+      /* Ignore write_set for REPLACE command */
       user_cols_written_bitmap= NULL;
       mask= NULL;
     }
