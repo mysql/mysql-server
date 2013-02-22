c261698bb5317b91e962f69694296928b3ca108e
John David Duncan <john.duncan@oracle.com>
2013-02-22 11:36:44 -0800

    Support jshint instead of jslint as a linter

--- test/lint/LintTest.js
+++ test/lint/LintTest.js
@@ -22,10 +22,13 @@
 
 "use strict";
 
-var linter, skipTests;
+var skipTests = false;
+var linter, lintName;
 
-try      { skipTests = false; linter = require("jslint/lib/linter"); }
-catch(e) { skipTests = true; }
+try      { lintName = "jslint"; linter = require("jslint/lib/linter").lint;  }
+catch(e) { try       { lintName = "jshint"; linter = require("jshint").JSHINT; }
+           catch(f)  { skipTests = true;  }
+         }
 
 var jslintOptions = {
   "vars"      : true,     // allow multiple var declarations
@@ -36,6 +39,12 @@ var jslintOptions = {
   "nomen"     : true,     // allow dangling underscore
 };
 
+var jshintOptions = {
+  "node"      : true,     // node.js globals
+  "latedef"   : true      // check for variables used before defined
+};
+
+var lintOptions = lintName === "jslint" ? jslintOptions : jshintOptions;
 
 function lintTest(basePath, sourceFile) {
   var t = new harness.SerialTest(path.basename(sourceFile));
@@ -46,18 +55,31 @@ function lintTest(basePath, sourceFile) {
 
     var e, i, n=0;
     var data = fs.readFileSync(this.sourceFile, "utf8");  
-    var result = linter.lint(data, jslintOptions);
+    var result = linter(data, lintOptions);
+    var ok, errors, msg;
 
-    if(! result.ok) {
+    /* Adapt to differing APIs of jslint and jshint */
+    if(typeof result === 'boolean') {
+      /* We are using jshint */
+      ok = result;
+      errors = linter.errors;
+    }
+    else {
+      ok = result.ok;
+      errors = result.errors;
+    }
+
+    if(! ok) {
       console.log("Errors for "+ this.name +":");
-      for (i = 0; i < result.errors.length; i += 1) {
-        e = result.errors[i];
+      for (i = 0; i < errors.length; i += 1) {
+        e = errors[i];
         if(e) {
           n += 1;
           console.log(' * Line %d[%d]: %s', e.line, e.character, e.reason);
         }
-      }    
-      this.appendErrorMessage(util.format("%d jslint error%s", n, n===1 ? '':'s'));
+      }
+      msg = util.format("%d %s error%s", n, lintName, n===1 ? '':'s');
+      this.appendErrorMessage(msg);
     }
     return true;
   };
@@ -103,6 +125,7 @@ checkSource("impl/mysql/MySQLConnection.js");
 //checkSource("impl/mysql/MySQLDictionary.js");
 
 checkSource("impl/ndb/ndb_service_provider.js");
+checkSource("impl/ndb/NdbConnection.js");
 checkSource("impl/ndb/NdbConnectionPool.js");
 checkSource("impl/ndb/NdbSession.js");
 checkSource("impl/ndb/NdbOperation.js");
