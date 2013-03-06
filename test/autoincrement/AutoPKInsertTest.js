019c041ded002d076c70adc3506549d434462d94
Craig L Russell <Craig.Russell@oracle.com>
2013-03-06 11:54:58 -0800

    Improve autoincrement tests to allow key to be set explicitly by the user If the autoincrement column is explicitly set, pass it to the database If the autoincrement column is not set, let the database figure out the key

--- test/autoincrement/AutoPKInsertTest.js
+++ test/autoincrement/AutoPKInsertTest.js
@@ -109,5 +109,99 @@ t5.run = function() {
   });
 };
 
+/***** Persist with domain object and explicit primary key ***/
+var t6 = new harness.ConcurrentTest("testPersistDomainObjectExplicitPK");
+t6.run = function() {
+  var testCase = this;
+  // create the domain object 4075
+  var object = new global.autopk('Employee 4075', 4075, 4075);
+  object.id = 4075;
+  fail_openSession(testCase, function(session) {
+    session.persist(object, function(err) {
+      if (err) {
+        testCase.fail(err);
+        return;
+      }
+      // key and testCase are passed to fail_verify_autopk as extra parameters
+      var key = new global.autopk_magic_key(4075);
+      session.find(global.autopk, key, fail_verify_autopk, 4075, testCase, true);
+    });
+  });
+};
+
+/***** Persist with constructor and domain object and explicit primary key ***/
+var t7 = new harness.ConcurrentTest("testPersistConstructorAndObjectExplicitPK");
+t7.run = function() {
+  var testCase = this;
+  // create the domain object 4076
+  var object = new global.autopk('Employee 4076', 4076, 4076);
+  object.id = 4076;
+  fail_openSession(testCase, function(session) {
+    session.persist(global.autopk, object, function(err, session2) {
+      if (err) {
+        testCase.fail(err);
+        return;
+      }
+      // key and testCase are passed to fail_verify_autopk as extra parameters
+      session2.find(global.autopk, {magic: 4076}, fail_verify_autopk, 4076, testCase, true);
+    }, session);
+  });
+};
+
+/***** Persist with table name and domain object and explicit primary key ***/
+var t8 = new harness.ConcurrentTest("testPersistTableNameAndObjectExplicitPK");
+t8.run = function() {
+  var testCase = this;
+  // create the domain object 4077
+  var object = new global.autopk('Employee 4077', 4077, 4077);
+  object.id = 4077;
+  fail_openSession(testCase, function(session) {
+    session.persist('autopk', object, function(err, session2) {
+      if (err) {
+        testCase.fail(err);
+        return;
+      }
+      // key and testCase are passed to fail_verify_autopk as extra parameters
+      session2.find(global.autopk, {magic: 4077}, fail_verify_autopk, 4077, testCase, true);
+    }, session);
+  });
+};
+
+/***** Persist with constructor and literal and explicit primary key ***/
+var t9 = new harness.ConcurrentTest("testPersistConstructorAndLiteralExplicitPK");
+t9.run = function() {
+  var testCase = this;
+  // create the literal 4078
+  var object = {id: 4078, name:'Employee 4078', age:4078, magic:4078};
+  fail_openSession(testCase, function(session) {
+    session.persist(global.autopk, object, function(err, session2) {
+      if (err) {
+        testCase.fail(err);
+        return;
+      }
+      // key and testCase are passed to fail_verify_autopk as extra parameters
+      session2.find(global.autopk, {magic: 4078}, fail_verify_autopk, 4078, testCase, true);
+    }, session);
+  });
+};
+
+/***** Persist with table name and literal and explicit primary key ***/
+var t10 = new harness.ConcurrentTest("testPersistTableNameAndLiteralExplicitPK");
+t10.run = function() {
+  var testCase = this;
+  // create the literal 4079
+  var object = {id: 4079, name:'Employee 4079', age:4079, magic:4079};
+  fail_openSession(testCase, function(session) {
+    session.persist('autopk', object, function(err, session2) {
+      if (err) {
+        testCase.fail(err);
+        return;
+      }
+      // key and testCase are passed to fail_verify_autopk as extra parameters
+      session2.find(global.autopk, {magic: 4079}, fail_verify_autopk, 4079, testCase, true);
+    }, session);
+  });
+};
+
 /*************** EXPORT THE TOP-LEVEL GROUP ********/
-module.exports.tests = [t1, t2, t3, t4, t5];
+module.exports.tests = [t1, t2, t3, t4, t5, t6, t7, t8, t9, t10];
