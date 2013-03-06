b249117363ef5c74221930bb63fd131706a84bce
Balasubramanian Kandasamy <balasubramanian.kandasamy@oracle.com>
2013-03-06 16:33:26 +0100

    Added fix for Bug#16445097

--- support-files/mysql.spec.sh
+++ support-files/mysql.spec.sh
@@ -286,10 +286,13 @@ documentation and the manual for more information.
 Summary:	MySQL: a very fast and reliable SQL database server
 Group:		Applications/Databases
 Requires:	%{distro_requires}
+Obsoletes:      MySQL < %{version}-%{release}
+Obsoletes:      MySQL-server < %{version}-%{release}
+Obsoletes:      MySQL-server-advanced < %{version}-%{release}
 Obsoletes:	mysql mysql-server mysql-advanced mysql-server-advanced
 Obsoletes:	MySQL-server-classic MySQL-server-community MySQL-server-enterprise
 Obsoletes:	MySQL-server-advanced-gpl MySQL-server-enterprise-gpl
-Provides:	msqlormysql MySQL MySQL-server
+Provides:	msqlormysql MySQL MySQL-server MySQL-server-advanced
 Provides:	mysql 
 
 %description -n MySQL-server%{product_suffix}
@@ -321,9 +324,11 @@ package "MySQL-client%{product_suffix}" as well!
 Summary:	MySQL - Client
 Group:		Applications/Databases
 Obsoletes:	mysql mysql-advanced
+Obsoletes:      MySQL-client < %{version}-%{release} 
+Obsoletes:      MySQL-client-advanced < %{version}-%{release} 
 Obsoletes:	MySQL-client-classic MySQL-client-community MySQL-client-enterprise
 Obsoletes:	MySQL-client-advanced-gpl MySQL-client-enterprise-gpl
-Provides:	MySQL-client
+Provides:	MySQL-client MySQL-client-advanced
 Provides:	mysql 
 
 %description -n MySQL-client%{product_suffix}
@@ -338,9 +343,11 @@ Group:		Applications/Databases
 Requires:	MySQL-client perl
 Conflicts:	mysql-test mysql-test-advanced
 Obsoletes:	mysql-bench MySQL-bench
+Obsoletes:      MySQL-test < %{version}-%{release}
+Obsoletes:      MySQL-test-advanced < %{version}-%{release}
 Obsoletes:	MySQL-test-classic MySQL-test-community MySQL-test-enterprise
 Obsoletes:	MySQL-test-advanced-gpl MySQL-test-enterprise-gpl
-Provides:	MySQL-test
+Provides:	MySQL-test MySQL-test-advanced
 AutoReqProv:	no
 
 %description -n MySQL-test%{product_suffix}
@@ -353,9 +360,11 @@ For a description of MySQL see the base MySQL RPM or http://www.mysql.com/
 Summary:	MySQL - Development header files and libraries
 Group:		Applications/Databases
 Conflicts:	mysql-devel mysql-embedded-devel mysql-devel-advanced mysql-embedded-devel-advanced
+Obsoletes:      MySQL-devel < %{version}-%{release}
+Obsoletes:      MySQL-devel-advanced < %{version}-%{release}
 Obsoletes:	MySQL-devel-classic MySQL-devel-community MySQL-devel-enterprise
 Obsoletes:	MySQL-devel-advanced-gpl MySQL-devel-enterprise-gpl
-Provides:	MySQL-devel
+Provides:	MySQL-devel MySQL-devel-advanced
 
 %description -n MySQL-devel%{product_suffix}
 This package contains the development header files and libraries necessary
@@ -369,10 +378,12 @@ Summary:	MySQL - Shared libraries
 Group:		Applications/Databases
 Obsoletes:	MySQL-shared-standard MySQL-shared-pro
 Obsoletes:	MySQL-shared-pro-cert MySQL-shared-pro-gpl
+Obsoletes:      MySQL-shared < %{version}-%{release} 
+Obsoletes:      MySQL-shared-advanced < %{version}-%{release}
 Obsoletes:	MySQL-shared-pro-gpl-cert 
 Obsoletes:	MySQL-shared-classic MySQL-shared-community MySQL-shared-enterprise
 Obsoletes:	MySQL-shared-advanced-gpl MySQL-shared-enterprise-gpl
-Provides:	MySQL-shared
+Provides:	MySQL-shared MySQL-shared-advanced
 
 %description -n MySQL-shared%{product_suffix}
 This package contains the shared libraries (*.so*) which certain languages
@@ -385,9 +396,11 @@ Group:		Applications/Databases
 Requires:	MySQL-devel
 Conflicts:	mysql-embedded mysql-embedded-advanced
 Obsoletes:	MySQL-embedded-pro
+Obsoletes:      MySQL-embedded < %{version}-%{release} 
+Obsoletes:      MySQL-embedded-advanced < %{version}-%{release}
 Obsoletes:	MySQL-embedded-classic MySQL-embedded-community MySQL-embedded-enterprise
 Obsoletes:	MySQL-embedded-advanced-gpl MySQL-embedded-enterprise-gpl
-Provides:	MySQL-embedded
+Provides:	MySQL-embedded MySQL-embedded-advanced
 
 %description -n MySQL-embedded%{product_suffix}
 This package contains the MySQL server as an embedded library.
