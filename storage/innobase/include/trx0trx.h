5b7b92e548d332c5c1c97f62375373fb6c1c9d7d
Vasil Dimov <vasil.dimov@oracle.com>
2013-03-06 08:15:04 +0200

    Reword a text in a comment.
    Suggested by:	Kevin (rb:2091)

--- storage/innobase/include/trx0trx.h
+++ storage/innobase/include/trx0trx.h
@@ -662,7 +662,7 @@ struct trx_lock_t {
 
 /** Type used to store the list of tables that are modified by a given
 transaction. We store pointers to the table objects in memory because
-we know that a table object will not be destroyed until a transaction
+we know that a table object will not be destroyed while a transaction
 that modified it is running. */
 typedef std::set<dict_table_t*>	trx_mod_tables_t;
 
