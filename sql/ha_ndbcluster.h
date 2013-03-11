2412358c7d1546c9b3d7feac56eeaa26dc8b6a3d
Ole John Aske <ole.john.aske@oracle.com>
2013-03-11 12:53:14 +0100

    Fix for Bug#16020945 HA_NDBCLUSTER ADAPTION TO 5.6 RBWR
    RBWR (Read Before Write Removal) previously relied on 
    the optimizer calculating the 'read_set' for which rows 
    requiring to be read as part of an update or delete 
    operation. The handler::table_flags() returned
    HA_PRIMARY_KEY_REQUIRED_FOR_DELETE which caused the
    optimizer to add primary key columns to the read_set
    for update and delete operations.
    
    The problem with this approach is that the optimizer
    has insufficient information about how the handler
    will perform the read & update/delete operation at
    this stage. A conservative strategy was thus used which 
    in several cases added the primary key column even when
    not required. This later prevented RBWR to 'remove' the
    read where this would have been possible.
    
    This fix removes returning HA_PRIMARY_KEY_REQUIRED_FOR_DELETE
    from ha_ndbcluster::table_flags(). Thus the RBWR optimizer
    will only see the read_set required by the runtime to evaluate
    any conditions and the new updated values. Any additional 
    columns required by ha_ndbcluster in order to update or delete
    the rows and BLOB columns are now added to read_set inside
    the handler after RBWR has been decided.

--- sql/ha_ndbcluster.h
+++ sql/ha_ndbcluster.h
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2000, 2011, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2000, 2013, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -515,6 +515,8 @@ private:
                                NdbOperation::GetValueSpec gets[2]);
   void get_hidden_fields_scan(NdbScanOperation::ScanOptions *options,
                               NdbOperation::GetValueSpec gets[2]);
+  void get_read_set(bool use_cursor, uint idx);
+
   void eventSetAnyValue(THD *thd,
                         NdbOperation::OperationOptions *options) const;
   bool check_index_fields_in_write_set(uint keyno);
