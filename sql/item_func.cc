2ede76c1d7144ab6a2881055f2bce0355ad40048
Vladislav Vaintroub <wlad@montyprogram.com>
2013-03-12 20:09:49 +0100

    MDEV-4224 : func_math test fails, when clang 3.0  compiler is used.
    The reason for the problem was  negation of signed longlong value  LONGLON
    G_MIN in  Item_func_neg::int_op() - the result of this operation is not defined
    (in C/C++ standard).
    
    With this patch, LONGLONG_MIN is handled as special value, and negation is
    avoided.

--- sql/item_func.cc
+++ sql/item_func.cc
@@ -1907,6 +1907,14 @@ longlong Item_func_neg::int_op()
   if (args[0]->unsigned_flag &&
       (ulonglong) value > (ulonglong) LONGLONG_MAX + 1)
     return raise_integer_overflow();
+
+  if (value == LONGLONG_MIN)
+    if (args[0]->unsigned_flag != unsigned_flag)
+      /* negation of LONGLONG_MIN is LONGLONG_MIN. */
+      return LONGLONG_MIN; 
+    else
+      return raise_integer_overflow();
+
   return check_integer_overflow(-value, !args[0]->unsigned_flag && value < 0);
 }
 
