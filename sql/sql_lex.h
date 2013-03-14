5319cfb60af2cf50584ed5b722433e1a29c9ba30
Tor Didriksen <tor.didriksen@oracle.com>
2013-03-14 15:33:25 +0100

    Bug#16359402 CRASH WITH AGGREGATES: ASSERTION FAILED: N < M_SIZE
    We need to take 'n_sum_items' into the calculation
    when allocating the ref_ptr_array.

--- sql/sql_lex.h
+++ sql/sql_lex.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2000, 2012, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2000, 2013, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -731,6 +731,7 @@ public:
   Item *select_limit, *offset_limit;  /* LIMIT clause parameters */
   // Arrays of pointers to top elements of all_fields list
   Item **ref_pointer_array;
+  size_t ref_pointer_array_size; // Number of elements in array.
 
   /*
     number of items in select_list and HAVING clause used to get number
