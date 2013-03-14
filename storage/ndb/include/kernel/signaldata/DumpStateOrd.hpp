d1f5ced294514c5a02f21301ee6e94b3d7b2d015
Frazer Clement <frazer.clement@oracle.com>
2013-03-14 12:37:02 +0000

    Bug #15878085     DETERMINING THE NUMBER OF TRANSACTION OBJECTS
    This patch adds a new DUMP code, 2514, which shows the number of
    allocated, seized, stateless, statefull, and scanning 'transaction'
    objects, per Api node.
    
    This information is useful when debugging ApiConnectRecord 'seizing'
    behaviour by multiple Api nodes, and looking for 'leaks'.
    
    Ideally it should be exposed via the NdbInfo Api.

--- storage/ndb/include/kernel/signaldata/DumpStateOrd.hpp
+++ storage/ndb/include/kernel/signaldata/DumpStateOrd.hpp
@@ -1,6 +1,5 @@
 /*
-   Copyright (c) 2003-2008 MySQL AB, 2009 Sun Microsystems, Inc.
-   Use is subject to license terms.
+   Copyright (c) 2003, 2013, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -124,6 +123,7 @@ public:
     StartPeriodicTcTimer = 2511,
     TcStartDumpIndexOpCount = 2512,
     TcDumpIndexOpCount = 2513,
+    TcDumpApiConnectRecSummary = 2514,
     TcDumpPoolLevels = 2555,
     CmvmiDumpConnections = 2600,
     CmvmiDumpLongSignalMemory = 2601,
