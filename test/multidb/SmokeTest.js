ac36fe19ccd57f3d321ff47d0e30fdb48b9bcbeb
Craig L Russell <Craig.Russell@oracle.com>
2013-03-16 12:16:52 -0700

    Implement multiple database support   allow user to specify table, defaulting database to session factory,     for both tables to pre-load and mynode.TableMapping   allow array of {table}, {constructor} for pre-load metadata
    Refactor mynode to get rid of callback(... extra1, extra2, extra3, extra4)
    
    Implement pre-load table and domain object metadta
      allow user to specify list of tables for which to pre-load metadata
      allow user to specify list of constructors for which to pre-load metadata
    
    SessionFactory
      refactor allocate session slot to be a member function
      refactor newConnection to allocate a new connection and keep track of it
      refactor dbConnectionPool close
        mynode now closes it when the last factory is closed
    
    mynode
      refactor connect to use UserContext
      refactor openSession to use UserContext
      close db connection pool when last session factory using it is closed
    
    SPI-documentation/DBConnectionPool
      update documentation for getDBSession to include session index
    
    SPI-documentation/NDB_Properties
      use mysql_user: root until we sort the grants issues
    
    mysql_service_provider
      use mysql_user: root until we sort the grants issues
    
    UserContext
      refactor resolve properties for connect and openConnection to allow null or string
      refactor getTableHandler to use tableSpecification
        tableSpecification resolves default database, explicit database, table name
      refactor getSessionFactory from mynode for connect and openSession
      implement resolve mappings (formerly the unimplemented annotations parameter)
      improve undefined argument handling for find
    
    MySQLDictionary
      fix bug that allowed only default database to be used for tables
    
    autoincrement/SmokeTest
      request pre-loading of mappings
    
    test/driver.js
      support pre-loading of mappings
      quiet the post reporting except when trace is on
    
    integraltypes/SmokeTest
      request pre-loading of mappings
    
    t_basic/SmokeTest
      request pre-loading of mappings

--- test/multidb/SmokeTest.js
+++ test/multidb/SmokeTest.js
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2012, Oracle and/or its affiliates. All rights
+ Copyright (c) 2013, Oracle and/or its affiliates. All rights
  reserved.
  
  This program is free software; you can redistribute it and/or
@@ -18,30 +18,29 @@
  02110-1301  USA
  */
 
-/** This is the smoke test for the integraltypes suite.
+/** This is the smoke test for the multidb suite.
  */
 
-require("./lib.js");
+/*global assert, spi_module, harness */
+
+"use strict";
+
+try {
+  require("./suite_config.js");
+} catch (e) {}
 
 var test = new harness.SmokeTest("SmokeTest");
 
 test.run = function() {
-  var t = this;
+  var testCase = this;
   harness.SQL.create(this.suite, function(error) {
     if (error) {
-      t.fail('createSQL failed: ' + error);
+      testCase.fail('createSQL failed: ' + error);
     } else {
-      var props = new mynode.ConnectionProperties(global.adapter);
-      t.mappings = global.integraltypes;
-      global.fail_openSession(t, function(session) {
-        if (session) {
-          t.pass();
-        } else {
-          t.fail();
-        }
-      });
+      testCase.pass();
     }
   });
 };
 
-module.exports.tests = [test];
+
+exports.tests = [test];
---
+++ test/multidb/drop.sql
@@ -0,0 +1,9 @@
+drop table if exists test1.tbl1;
+drop table if exists test2.tbl2;
+drop table if exists test3.tbl3;
+drop table if exists test4.tbl4;
+drop table if exists test5.tbl5;
+drop table if exists test6.tbl6;
+drop table if exists test7.tbl7;
+drop table if exists test8.tbl8;
+
