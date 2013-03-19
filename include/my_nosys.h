d20a70fb55114bd858754a67d863cce573b8333d
Murthy Narkedimilli <murthy.narkedimilli@oracle.com>
2013-03-19 13:29:12 +0100

    Bug 16395495 - OLD FSF ADDRESS IN GPL HEADER

--- include/my_nosys.h
+++ include/my_nosys.h
@@ -11,7 +11,7 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA */
 
 /*
   Header to remove use of my_functions in functions where we need speed and
--- strings/longlong2str-x86.s
+++ strings/longlong2str-x86.s
@@ -10,7 +10,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
 # Optimized longlong2str function for Intel 80x86  (gcc/gas syntax) 
 # Some set sequences are optimized for pentuimpro II 
