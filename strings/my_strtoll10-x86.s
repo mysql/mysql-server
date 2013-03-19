fe85f546405b16259ab97721610c9b5a64c53e22
Murthy Narkedimilli <murthy.narkedimilli@oracle.com>
2013-03-19 13:29:12 +0100

    Bug 16395495 - OLD FSF ADDRESS IN GPL HEADER

--- strings/my_strtoll10-x86.s
+++ strings/my_strtoll10-x86.s
@@ -11,7 +11,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307	USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA
 
 # Implemention of my_strtoll():  Converting a string to a 64 bit integer.
 # For documentation, check my_strtoll.c
