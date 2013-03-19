eecfa8805ea5e56cc36544b461e704cad1b59d17
Murthy Narkedimilli <murthy.narkedimilli@oracle.com>
2013-03-19 13:29:12 +0100

    Bug 16395495 - OLD FSF ADDRESS IN GPL HEADER

--- cmd-line-utils/readline/rlmbutil.h
+++ cmd-line-utils/readline/rlmbutil.h
@@ -18,7 +18,7 @@
    The GNU General Public License is often shipped with GNU software, and
    is generally kept in a file called COPYING or LICENSE.  If you do not
    have a copy of the license, write to the Free Software Foundation,
-   59 Temple Place, Suite 330, Boston, MA 02111 USA. */
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. */
 
 #if !defined (_RL_MBUTIL_H_)
 #define _RL_MBUTIL_H_
--- storage/innobase/pars/pars0grm.c
+++ storage/innobase/pars/pars0grm.c
@@ -15,8 +15,7 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA */
 
 /* As a special exception, when this file is copied by Bison into a
    Bison output file, you may use that output file without restriction.
