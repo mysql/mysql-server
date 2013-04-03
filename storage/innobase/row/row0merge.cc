de9ae24d6d3780456e02cea1064cbdcb5e0ec5b5
Marko Mäkelä <marko.makela@oracle.com>
2013-04-03 10:26:04 +0300

    Bug#16544143 ENABLE PARTIAL ROLLBACK DURING ONLINE ALTER TABLE
    During table-copying online ALTER TABLE (such as ADD COLUMN, DROP
    COLUMN, ADD PRIMARY KEY, changing the ROW_FORMAT), we keep a log of
    concurrent INSERT and DELETE operations that modify the table data.
    Off-page columns (BLOBs) are not logged; instead, we just log the
    pointers as they are in the clustered index records.
    
    In order to avoid accessing stale BLOB pointers when applying the log,
    we used to suspend the purge of records that would free BLOBs, and keep
    track of user transactions that were rolled back.
    
    The bug is that a partial rollback is wrongly treated as a full
    rollback. That is, the remaining changes of a partially rolled back
    transaction (one that involved a statement rollback, or an explicit
    ROLLBACK TO SAVEPOINT) could be lost (rolled back) during the online
    ALTER TABLE.
    
    row_log_table_rollback(), row_log_t::trx_rb: Remove. We no longer keep
    track of transactions. We keep track of BLOB start pages instead.
    
    row_log_table_is_rollback(): Remove. The call in
    row_merge_insert_index_tuples() was made redundant in the fix of
    Bug#14712710 BOGUS DUPLICATE KEY ERROR DURING ONLINE ALTER TABLE
    (rb#1438) when row_merge_read_clustered_index() started to use
    REPEATABLE READ instead of READ UNCOMMITTED.
    
    row_log_t::blobs: A new member, a map from BLOB page numbers to byte
    offsets in the log. This keeps track of BLOBs freed and allocated during
    ALTER TABLE. If a previously freed BLOB page is allocated, the log apply
    must trust the BLOB page number starting from the log record of the new
    allocation.
    
    Example: A transaction inserted a BLOB starting at page 123. There was
    a partial rollback that undoes the insert, but the transaction remains
    active. So, we will insert (123,BLOB_FREED) to log->blobs.
    Later, another BLOB will be inserted at page 123, when log->tail.total=4567.
    We will replace the (123,BLOB_FREED) with (123,4567) in the log->blobs.
    The log record for inserting this BLOB would start at offset 4567.
    
    row_log_table_blob_free(), row_log_table_blob_alloc(): New functions,
    for updating row_log_t::blobs.
    
    row_log_table_close_func(), row_log_table_apply_op():
    Update the new field row_log_buf_t::total.
    
    row_log_table_delete(): Add the parameter 'bool purge', to specify
    whether to log BLOB prefixes for updating secondary indexes.
    The flag will be set during a partial or full rollback.
    
    row_log_table_apply_convert_mrec(): Before accessing any BLOB data,
    check row_log_t::blobs if it is safe to do so.
    
    row_purge_remove_clust_if_poss_low(), row_purge_remove_clust_if_poss(),
    row_purge_del_mark(), row_purge_upd_exist_or_extern(): Do not suspend
    the purge if it would free BLOBs during online ALTER TABLE. Rely on
    the row_log_t::blobs.
    
    rec_print_comp(): Display the BLOB pointer when truncating a column.
    
    rb#2203 approved by Jimmy Yang

--- storage/innobase/row/row0merge.cc
+++ storage/innobase/row/row0merge.cc
@@ -1402,7 +1402,8 @@ end_of_index:
 			will not see a newer state of the table when
 			applying the log.  This is mainly to prevent
 			false duplicate key errors, because the log
-			will identify records by the PRIMARY KEY. */
+			will identify records by the PRIMARY KEY,
+			and also to prevent unsafe BLOB access. */
 			ut_ad(trx->read_view);
 
 			if (!read_view_sees_trx_id(
@@ -2161,23 +2162,6 @@ row_merge_sort(
 }
 
 /*************************************************************//**
-Set blob fields empty */
-static __attribute__((nonnull))
-void
-row_merge_set_blob_empty(
-/*=====================*/
-	dtuple_t*	tuple)	/*!< in/out: data tuple */
-{
-	for (ulint i = 0; i < dtuple_get_n_fields(tuple); i++) {
-		dfield_t*	field = dtuple_get_nth_field(tuple, i);
-
-		if (dfield_is_ext(field)) {
-			dfield_set_data(field, NULL, 0);
-		}
-	}
-}
-
-/*************************************************************//**
 Copy externally stored columns to the data tuple. */
 static __attribute__((nonnull))
 void
@@ -2302,53 +2286,37 @@ row_merge_insert_index_tuples(
 				mrec, index, offsets, &n_ext, tuple_heap);
 
 			if (!n_ext) {
-				/* There are no externally stored columns. */
-			} else if (!dict_index_is_online_ddl(old_index)) {
+				/* There are no externally stored columns.
+				If we are creating secondary indexes only,
+				row_merge_read_clustered_index() will
+				essentially perform READ UNCOMMITTED.
+				This does not matter, because
+				row_log_apply() is idempotent. */
+			} else {
 				ut_ad(dict_index_is_clust(index));
-				/* Modifications to the table are
-				blocked while we are not rebuilding it
-				or creating indexes. Off-page columns
-				can be fetched safely. */
+				/* Off-page columns can be fetched safely
+				when concurrent modifications to the table
+				are disabled. (Purge can process delete-marked
+				records, but row_merge_read_clustered_index()
+				would have skipped them.)
+
+				When concurrent modifications are enabled,
+				row_merge_read_clustered_index() will
+				only see rows from transactions that were
+				committed before the ALTER TABLE started
+				(REPEATABLE READ).
+
+				Any modifications after the
+				row_merge_read_clustered_index() scan
+				will go through row_log_table_apply().
+				Any modifications to off-page columns
+				will be tracked by
+				row_log_table_blob_alloc() and
+				row_log_table_blob_free(). */
 				row_merge_copy_blobs(
 					mrec, offsets,
 					dict_table_zip_size(old_table),
 					dtuple, tuple_heap);
-			} else {
-				ut_ad(dict_index_is_clust(index));
-
-				ulint	offset = index->trx_id_offset;
-
-				if (!offset) {
-					offset = row_get_trx_id_offset(
-						index, offsets);
-				}
-
-				/* Copy the off-page columns while
-				holding old_index->lock, so
-				that they cannot be freed by
-				a rollback of a fresh insert. */
-				rw_lock_s_lock(&old_index->lock);
-
-				if (row_log_table_is_rollback(
-					    old_index,
-					    trx_read_trx_id(mrec + offset))) {
-					/* The row and BLOB could
-					already be freed. They
-					will be deleted by
-					row_undo_ins_remove_clust_rec
-					when rolling back a fresh
-					insert. So, no need to retrieve
-					the off-page column. */
-					row_merge_set_blob_empty(
-						dtuple);
-				} else {
-					row_merge_copy_blobs(
-						mrec, offsets,
-						dict_table_zip_size(old_table),
-						dtuple, tuple_heap);
-				}
-
-				rw_lock_s_unlock(&old_index->lock);
 			}
 
 			ut_ad(dtuple_validate(dtuple));
