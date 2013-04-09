4ad004c2b4d9d5abf1c135678eba1b1af39b9b97
Nirbhay Choubey <nirbhay.choubey@oracle.com>
2013-04-09 14:00:05 +0530

    Backporting patch for bug#15852074.

--- support-files/mysql.server.sh
+++ support-files/mysql.server.sh
@@ -416,7 +416,13 @@ case "$mode" in
     else
       # Try to find appropriate mysqld process
       mysqld_pid=`pidof $libexecdir/mysqld`
-      if test -z $mysqld_pid ; then 
+
+      # test if multiple pids exist
+      pid_count=`echo $mysqld_pid | wc -w`
+      if test $pid_count -gt 1 ; then
+        log_failure_msg "Multiple MySQL running but PID file could not be found ($mysqld_pid)"
+        exit 5
+      elif test -z $mysqld_pid ; then 
         if test "$use_mysqld_safe" = "0" ; then 
           lockfile=/var/lock/subsys/mysqlmanager
         else
