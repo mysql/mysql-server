e939c36f097556dc3b4996a1853eb66ffb4b50a4
Leif Walsh <leif@tokutek.com>
2013-04-16 18:43:44 +0000

    refs #6566 add copyright/license #ident strings to files that didn't have it
    git-svn-id: file:///svn/toku/tokudb@55568 c7de825b-a66e-492c-adef-691d508d4ae1

--- cxx/tests/test_env_set_flags.cpp
+++ cxx/tests/test_env_set_flags.cpp
@@ -1,5 +1,8 @@
 /* -*- mode: C; c-basic-offset: 4; indent-tabs-mode: nil -*- */
 // vim: expandtab:ts=8:sw=4:softtabstop=4:
+#ident "Copyright (c) 2007-2013 Tokutek Inc.  All rights reserved."
+#ident "The technology is licensed by the Massachusetts Institute of Technology, Rutgers State University of New Jersey, and the Research Foundation of State University of New York at Stony Brook under United States of America Serial No. 11/760379 and to the patents and/or patent applications resulting from it."
+
 #include <assert.h>
 #include <db_cxx.h>
 #include <errno.h>
