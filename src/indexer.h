b00607df251842d82d42a69998eae4b0a7778808
Leif Walsh <leif@tokutek.com>
2013-04-16 18:21:06 +0000

    refs #6566 move comment copyright/license headers to #ident strings
    git-svn-id: file:///svn/toku/tokudb@55566 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/indexer.h
+++ src/indexer.h
@@ -1,17 +1,11 @@
 /* -*- mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
 // vim: ft=cpp:expandtab:ts=8:sw=4:softtabstop=4:
+#ident "Copyright (c) 2007-2012 Tokutek Inc.  All rights reserved."
+#ident "The technology is licensed by the Massachusetts Institute of Technology, Rutgers State University of New Jersey, and the Research Foundation of State University of New York at Stony Brook under United States of America Serial No. 11/760379 and to the patents and/or patent applications resulting from it."
 #ident "$Id$"
+
 #ifndef TOKU_INDEXER_H
 #define TOKU_INDEXER_H
-
-/* Copyright (c) 2007-2012 Tokutek Inc.  All rights reserved.
- *
- * The technology is licensed by the Massachusetts Institute of Technology, 
- * Rutgers State University of New Jersey, and the Research Foundation of 
- * State University of New York at Stony Brook under United States of America 
- * Serial No. 11/760379 and to the patents and/or patent applications resulting from it.
- */
-
 
 
 // locking and unlocking functions to synchronize cursor position with
