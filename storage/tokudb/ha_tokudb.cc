497e07b7bf067ec26c6bc03f8160d12e9955f03f
Zardosht Kasheff <zardosht@tokutek.com>
2013-04-17 00:01:46 -0400

    addresses #1392 enable binary logging for tokudb. Work is not done yet, need to do investigation to make sure that there are no unwanted side effects.
    git-svn-id: file:///svn/mysql/tokudb-engine/src@8809 c7de825b-a66e-492c-adef-691d508d4ae1

--- storage/tokudb/ha_tokudb.cc
+++ storage/tokudb/ha_tokudb.cc
@@ -184,7 +184,7 @@ ha_tokudb::ha_tokudb(handlerton * hton, TABLE_SHARE * table_arg)
     handler(hton, table_arg), alloc_ptr(0), rec_buff(0),
     // flags defined in sql\handler.h
     int_table_flags(HA_REC_NOT_IN_SEQ | HA_FAST_KEY_READ | HA_NULL_IN_KEY | HA_CAN_INDEX_BLOBS | HA_PRIMARY_KEY_IN_READ_INDEX | 
-                    HA_FILE_BASED | HA_AUTO_PART_KEY | HA_TABLE_SCAN_ON_INDEX), 
+                    HA_FILE_BASED | HA_AUTO_PART_KEY | HA_TABLE_SCAN_ON_INDEX |HA_BINLOG_ROW_CAPABLE | HA_BINLOG_STMT_CAPABLE), 
     added_rows(0), deleted_rows(0), last_dup_key((uint) - 1), using_ignore(0), last_cursor_error(0),range_lock_grabbed(false), primary_key_offsets(NULL) {
     transaction = NULL;
 }
