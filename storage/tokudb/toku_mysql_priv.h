34df9b0b9e2f86993283759aa41a6271cd3997a2
John Esmet <esmet@tokutek.com>
2013-04-17 00:02:08 -0400

    [t:4445] dead code accidentally commited, reverting
    git-svn-id: file:///svn/mysql/tokudb-engine/tokudb-engine@39338 c7de825b-a66e-492c-adef-691d508d4ae1

--- storage/tokudb/toku_mysql_priv.h
+++ storage/tokudb/toku_mysql_priv.h
@@ -1,16 +1,5 @@
 #if !defined(TOKU_MYSQL_PRIV_H)
 #define TOKU_MYSQL_PRIV_H
-
-#include <sys/time.h>
-#include <stdio.h>
-static long current_time_usec(void)
-{
-    (void) current_time_usec;
-    struct timeval t;
-    gettimeofday(&t, NULL);
-    return t.tv_sec * 1000000 + t.tv_usec;
-}
-
 #include "mysql_version.h"
 #if MYSQL_VERSION_ID < 50506
 #include "mysql_priv.h"
