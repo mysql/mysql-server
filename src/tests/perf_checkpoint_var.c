65e16ca2389f6a9c3cf21c5e67e56debae0d0333
Zardosht Kasheff <zardosht@tokutek.com>
2013-04-17 00:00:08 -0400

    [t:4028], small changes to perf test
    git-svn-id: file:///svn/toku/tokudb@39282 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/perf_checkpoint_var.c
+++ src/tests/perf_checkpoint_var.c
@@ -95,6 +95,9 @@ stress_table(DB_ENV* env, DB** dbp, struct cli_args *cli_args) {
 int
 test_main(int argc, char *const argv[]) {
     struct cli_args args = get_default_args_for_perf();
+    args.env_args.checkpointing_period = 30;
+    args.num_DBs = 4;
+    args.num_ptquery_threads = 4;
     parse_stress_test_args(argc, argv, &args);
     stress_test_main(&args);
     return 0;
