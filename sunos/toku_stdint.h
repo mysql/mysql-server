81f649eb9bb58863f3ab52d4ad7a1d9ef633d92f
Rich Prohaska <prohaska@tokutek.com>
2013-04-16 23:57:54 -0400

    solaris port. addresses #1789
    git-svn-id: file:///svn/toku/tokudb@12570 c7de825b-a66e-492c-adef-691d508d4ae1

---
+++ sunos/toku_stdint.h
@@ -0,0 +1,13 @@
+#ifndef TOKU_STDINT_H
+#define TOKU_STDINT_H
+
+#include <stdint.h>
+#include <inttypes.h>
+
+typedef uint8_t u_int8_t;
+typedef uint16_t u_int16_t;
+typedef uint32_t u_int32_t;
+typedef uint64_t u_int64_t;
+
+#endif
+
