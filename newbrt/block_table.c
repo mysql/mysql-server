5dc29af42c40e1ed98200b2ed57d1e71654d2546
Yoni Fogel <yoni@tokutek.com>
2013-04-16 23:57:47 -0400

    Addresses #1510 windows 32bit port of [10889]
    git-svn-id: file:///svn/toku/tokudb@10914 c7de825b-a66e-492c-adef-691d508d4ae1

--- newbrt/block_table.c
+++ newbrt/block_table.c
@@ -557,14 +557,14 @@ static void
 dump_translation(FILE *f, struct translation *t) {
     if (t->block_translation) {
         BLOCKNUM b = make_blocknum(RESERVED_BLOCKNUM_TRANSLATION);
-        fprintf(f, " length_of_array[%"PRIu64"]", t->length_of_array);
+        fprintf(f, " length_of_array[%"PRId64"]", t->length_of_array);
         fprintf(f, " smallest_never_used_blocknum[%"PRId64"]", t->smallest_never_used_blocknum.b);
         fprintf(f, " blocknum_free_list_head[%"PRId64"]", t->blocknum_freelist_head.b);
-        fprintf(f, " size_on_disk[%"PRIu64"]", t->block_translation[b.b].size);
-        fprintf(f, " location_on_disk[%"PRIu64"]", t->block_translation[b.b].u.diskoff);
+        fprintf(f, " size_on_disk[%"PRId64"]", t->block_translation[b.b].size);
+        fprintf(f, " location_on_disk[%"PRId64"]", t->block_translation[b.b].u.diskoff);
         int64_t i;
         for (i=0; i<t->length_of_array; i++) {
-            fprintf(f, " %"PRIu64": %"PRId64" %"PRId64"", i, t->block_translation[i].u.diskoff, t->block_translation[i].size);
+            fprintf(f, " %"PRId64": %"PRId64" %"PRId64"", i, t->block_translation[i].u.diskoff, t->block_translation[i].size);
         }
         fprintf(f, "\n");
     }
