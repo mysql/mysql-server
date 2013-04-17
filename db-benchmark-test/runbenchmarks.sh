be655050759b6004e86a61f2fdef4b856b5eb392
Yoni Fogel <yoni@tokutek.com>
2013-04-16 23:57:30 -0400

    Uncommented lines
    git-svn-id: file:///svn/toku/tokudb.1032b@8267 c7de825b-a66e-492c-adef-691d508d4ae1

--- db-benchmark-test/runbenchmarks.sh
+++ db-benchmark-test/runbenchmarks.sh
@@ -15,43 +15,43 @@ alias flatteningtxnscan='(dbtxn 2>&1 >/dev/null) && scan'
 (flatteningscan)2>&1 >/dev/null # Cache binaries/etc.
 
 
-#echo db-benchmark-test no transactions
-#db
-#echo Time for 5 runs:
-#time for (( i = 0; i < 5; i++ )); do (db) 2>&1 >/dev/null; done
-#echo
-#echo ============================
-#echo db-benchmark-test single transactions
-#dbtxn
-#echo Time for 5 runs:
-#time for (( i = 0; i < 5; i++ )); do (dbtxn) 2>&1 >/dev/null; done
-#echo
-#echo ============================
-#echo db-benchmark-test single transactions ABORT at end
-#dbabort
-#echo Time for 5 runs:
-#time for (( i = 0; i < 5; i++ )); do (dbabort) 2>&1 >/dev/null; done
-#echo
-#echo ============================
-#echo flattening scanscan
-#flatteningscan
-#echo Time for 5 runs:
-#time for (( i = 0; i < 5; i++ )); do (flatteningscan) 2>&1 >/dev/null; done
-#echo
-#echo ============================
-#echo flattening scanscan on txn db
-#flatteningtxnscan
-#echo Time for 5 runs:
-#time for (( i = 0; i < 5; i++ )); do (flatteningtxnscan) 2>&1 >/dev/null; done
-#echo
-#echo ============================
-#echo pre-flattened scanscan
-#(flatteneddb) 2>&1 >/dev/null
-#scan
-#echo Time for 5 runs:
-#time for (( i = 0; i < 5; i++ )); do (scan) 2>&1 >/dev/null; done
-#echo
-#echo ============================
+echo db-benchmark-test no transactions
+db
+echo Time for 5 runs:
+time for (( i = 0; i < 5; i++ )); do (db) 2>&1 >/dev/null; done
+echo
+echo ============================
+echo db-benchmark-test single transactions
+dbtxn
+echo Time for 5 runs:
+time for (( i = 0; i < 5; i++ )); do (dbtxn) 2>&1 >/dev/null; done
+echo
+echo ============================
+echo db-benchmark-test single transactions ABORT at end
+dbabort
+echo Time for 5 runs:
+time for (( i = 0; i < 5; i++ )); do (dbabort) 2>&1 >/dev/null; done
+echo
+echo ============================
+echo flattening scanscan
+flatteningscan
+echo Time for 5 runs:
+time for (( i = 0; i < 5; i++ )); do (flatteningscan) 2>&1 >/dev/null; done
+echo
+echo ============================
+echo flattening scanscan on txn db
+flatteningtxnscan
+echo Time for 5 runs:
+time for (( i = 0; i < 5; i++ )); do (flatteningtxnscan) 2>&1 >/dev/null; done
+echo
+echo ============================
+echo pre-flattened scanscan
+(flatteneddb) 2>&1 >/dev/null
+scan
+echo Time for 5 runs:
+time for (( i = 0; i < 5; i++ )); do (scan) 2>&1 >/dev/null; done
+echo
+echo ============================
 echo pre-flattened scanscan on txn db
 (flattenedtxndb) 2>&1 >/dev/null
 scan
