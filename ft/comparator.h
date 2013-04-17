6469cc006e0826cc3f0ab500bd8f1a8d980bd508
Leif Walsh <leif@tokutek.com>
2013-04-17 00:01:35 -0400

    refs #6566 add copyright/license #ident strings to files that didn't have it
    git-svn-id: file:///svn/toku/tokudb@55568 c7de825b-a66e-492c-adef-691d508d4ae1

--- ft/comparator.h
+++ ft/comparator.h
@@ -1,9 +1,10 @@
-/**
- * comparator.h
- */
+/* -*- mode: C; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+// vim: ft=cpp:expandtab:ts=8:sw=4:softtabstop=4:
+#ident "Copyright (c) 2007-2013 Tokutek Inc.  All rights reserved."
+#ident "The technology is licensed by the Massachusetts Institute of Technology, Rutgers State University of New Jersey, and the Research Foundation of State University of New York at Stony Brook under United States of America Serial No. 11/760379 and to the patents and/or patent applications resulting from it."
 
-#ifndef COMPARATOR_H
-#define COMPARATOR_H
+
+#pragma once
 
 #include <db.h>
 #include <string.h>
@@ -43,5 +44,3 @@ private:
 };
 
 } /* namespace toku */
-
-#endif /* COMPARATOR_H */
--- portability/huge_page_detection.h
+++ portability/huge_page_detection.h
@@ -1,3 +1,11 @@
+/* -*- mode: C; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+// vim: ft=cpp:expandtab:ts=8:sw=4:softtabstop=4:
+#ident "Copyright (c) 2007-2013 Tokutek Inc.  All rights reserved."
+#ident "The technology is licensed by the Massachusetts Institute of Technology, Rutgers State University of New Jersey, and the Research Foundation of State University of New York at Stony Brook under United States of America Serial No. 11/760379 and to the patents and/or patent applications resulting from it."
+
+
+#pragma once
+
 extern "C" bool complain_and_return_true_if_huge_pages_are_enabled(void);
 // Effect: Return true if huge pages appear to be enabled.  If so, print some diagnostics to stderr.
 //  If environment variable TOKU_HUGE_PAGES_OK is set, then don't complain.
