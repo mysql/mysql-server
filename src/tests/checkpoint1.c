48865d08cf7c83a53b2a1c46af4ee92f4dfc2781
Bradley C. Kuszmaul <bradley@tokutek.com>
2013-04-16 23:59:45 -0400

    [t:3581] Make checkpoint1 use redzone. Refs #3581
    git-svn-id: file:///svn/toku/tokudb@33040 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/checkpoint1.c
+++ src/tests/checkpoint1.c
@@ -32,6 +32,7 @@ checkpoint1 (void)
     toku_os_mkdir(ENVDIR, S_IRWXU+S_IRWXG+S_IRWXO);
 
     r = db_env_create(&env, 0);                                                     CKERR(r);
+    r = env->set_redzone(env, 0);                                                   CKERR(r);
 
     r = env->open(env, ENVDIR, DB_INIT_LOCK|DB_INIT_LOG|DB_INIT_MPOOL|DB_INIT_TXN|DB_CREATE|DB_PRIVATE, S_IRWXU+S_IRWXG+S_IRWXO); CKERR(r);
     r = db_create(&db, env, 0);                                                     CKERR(r);
