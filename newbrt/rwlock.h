ff57e4c6170597cd2d27c071e090f3ac02351264
Barry Perlman <barry@tokutek.com>
2013-04-16 23:57:52 -0400

    Fixes #1735 Merge from tokudb.1735 to main with command: svn merge -r11656:HEAD ../tokudb.1735 (executed in main sandbox)
    git-svn-id: file:///svn/toku/tokudb@11714 c7de825b-a66e-492c-adef-691d508d4ae1

--- newbrt/rwlock.h
+++ newbrt/rwlock.h
@@ -71,9 +71,7 @@ static inline void rwlock_read_lock(RWLOCK rwlock, toku_pthread_mutex_t *mutex) 
 }
 
 
-
-
-// TODO 1398  Get rid of this hack.
+// TODO: #1398  Get rid of this hack.
 #ifdef  BRT_LEVEL_STRADDLE_CALLBACK_LOGIC_NOT_READY
 
 // preferentially obtain a read lock (ignore request for write lock)
@@ -86,7 +84,6 @@ static inline void rwlock_prefer_read_lock(RWLOCK rwlock, toku_pthread_mutex_t *
 	rwlock_read_lock(rwlock, mutex);
 }
 #endif
-
 
 
 // release a read lock
