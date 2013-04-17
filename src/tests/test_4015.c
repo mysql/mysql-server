f020fe5361db870f08daf73e31742e9ed00e4a88
Bradley C. Kuszmaul <bradley@tokutek.com>
2013-04-16 23:59:59 -0400

    [t:4222] ignore dlsym memory leak on Fedora Core 12 ({{{svn merge -c 37819 ../tokudb.4015}}}). Refs #4222.
    git-svn-id: file:///svn/toku/tokudb@37820 c7de825b-a66e-492c-adef-691d508d4ae1

---
+++ src/tests/test_4015.c
@@ -0,0 +1,12 @@
+/* -*- mode: C; c-basic-offset: 4 -*- */
+#ident "Copyright (c) 2011 Tokutek Inc.  All rights reserved."
+#ident "$Id$"
+
+
+#include "test.h"
+
+int test_main(int argc, char * const argv[]) {
+    parse_args(argc, argv);
+
+    return 0;
+}
