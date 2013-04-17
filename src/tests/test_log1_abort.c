aae91f17acbb419ef31d15db1aa5c3f36274c1b3
Rich Prohaska <prohaska@tokutek.com>
2013-04-16 23:58:52 -0400

    fix some bdb tests closes[t:2175]
    git-svn-id: file:///svn/toku/tokudb@15826 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/test_log1_abort.c
+++ src/tests/test_log1_abort.c
@@ -41,9 +41,16 @@ test_main (int UU(argc), char UU(*argv[])) {
 	r=db->put(db, tid, &key, &data, 0);
 	CKERR(r);
     }
-    r=db->close(db, 0);       assert(r==0);
-    r=tid->abort(tid);    assert(r==0);
-    r=env->close(env, 0);     assert(r==0);
+    r=db->close(db, 0);       
+    assert(r==0);
+    r=tid->abort(tid);    
+    assert(r==0);
+    r=env->close(env, 0);
+#if USE_BDB
+    assert(r==ENOENT);
+#else
+    assert(r==0);
+#endif
     {
 	toku_struct_stat statbuf;
 	r = toku_stat(ENVDIR "/foo.db", &statbuf);
