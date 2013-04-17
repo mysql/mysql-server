8a6d5b9ba34835dc117a5442970db9d20bdfcd79
Leif Walsh <leif@tokutek.com>
2013-04-17 00:01:11 -0400

    refs #5537 fix abortrecover test runner to handle other failure cases
    git-svn-id: file:///svn/toku/tokudb@48384 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/run_abortrecover_test.sh
+++ src/tests/run_abortrecover_test.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ $# -lt 2 ]]; then exit 1; fi
+if [[ $# -ne 1 ]]; then exit 1; fi
 
 bin=$1; shift
-abortcode=$1; shift
 
 $bin --test
-if [[ $? -ne $abortcode ]]
+if [[ $? -eq 0 ]]
 then
-    echo $bin --test did not return $abortcode
+    echo $bin --test did not crash
     exit 1
 else
     set -e
