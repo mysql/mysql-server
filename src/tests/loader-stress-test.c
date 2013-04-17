52f886958b3a605938dabc7a7403734381ae0fbf
Dave Wells <wells@tokutek.com>
2013-04-16 23:59:21 -0400

    remove unnecessary printing
    git-svn-id: file:///svn/toku/tokudb@22936 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/loader-stress-test.c
+++ src/tests/loader-stress-test.c
@@ -239,13 +239,13 @@ static void check_results(DB **dbs)
             assert((unsigned int)pkey_for_db_key == (unsigned int)pkey_for_val(v, j));
 //            printf(" DB[%d] key = %10u, val = %10u, pkey_for_db_key = %10u, pkey_for_val=%10d\n", j, v, k, pkey_for_db_key, pkey_for_val(v, j));
         }
-        {printf("."); fflush(stdout);}
+        if ( verbose ) {printf("."); fflush(stdout);}
         r = cursor->c_close(cursor);
         CKERR(r);
         r = txn->commit(txn, 0);
         CKERR(r);
     }
-    printf("\nCheck OK\n");
+    if ( verbose ) printf("\nCheck OK\n");
 }
 
 static void *expect_poll_void = &expect_poll_void;
@@ -332,9 +332,9 @@ static void test_loader(DB **dbs)
         dbt_init(&val, &v, sizeof(unsigned int));
         r = loader->put(loader, &key, &val);
         CKERR(r);
-        if ( CHECK_RESULTS || verbose) { if((i%10000) == 0){printf("."); fflush(stdout);} }
+        if ( verbose) { if((i%10000) == 0){printf("."); fflush(stdout);} }
     }
-    if( CHECK_RESULTS || verbose ) {printf("\n"); fflush(stdout);}        
+    if ( verbose ) {printf("\n"); fflush(stdout);}        
         
     poll_count=0;
 
