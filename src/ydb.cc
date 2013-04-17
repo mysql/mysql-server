cbf1b66ad983b56c8552c09b7ec0a81bb18000e7
Leif Walsh <leif@tokutek.com>
2013-04-17 00:01:36 -0400

    refs #6566 make toku_patent_string a copy of the full license
    git-svn-id: file:///svn/toku/tokudb@55582 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/ydb.cc
+++ src/ydb.cc
@@ -88,7 +88,7 @@ PATENT RIGHTS GRANT:
 #ident "The technology is licensed by the Massachusetts Institute of Technology, Rutgers State University of New Jersey, and the Research Foundation of State University of New York at Stony Brook under United States of America Serial No. 11/760379 and to the patents and/or patent applications resulting from it."
 #ident "$Id$"
 
-const char *toku_patent_string = "The technology is licensed by the Massachusetts Institute of Technology, Rutgers State University of New Jersey, and the Research Foundation of State University of New York at Stony Brook under United States of America Serial No. 11/760379 and to the patents and/or patent applications resulting from it.";
+extern const char *toku_patent_string;
 const char *toku_copyright_string = "Copyright (c) 2007-2013 Tokutek Inc.  All rights reserved.";
 
 #include <toku_portability.h>
