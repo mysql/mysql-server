8df87816e280aa14bce795971d222ce6563cbe1a
John Esmet <esmet@tokutek.com>
2013-04-17 00:01:08 -0400

    refs #5504 step back
    git-svn-id: file:///svn/toku/tokudb@48095 c7de825b-a66e-492c-adef-691d508d4ae1

--- ft/tokuconst.h
+++ ft/tokuconst.h
@@ -9,6 +9,16 @@
 #ident "Copyright (c) 2007-2012 Tokutek Inc.  All rights reserved."
 #ident "The technology is licensed by the Massachusetts Institute of Technology, Rutgers State University of New Jersey, and the Research Foundation of State University of New York at Stony Brook under United States of America Serial No. 11/760379 and to the patents and/or patent applications resulting from it."
 
+/* The number of transaction ids stored in the xids structure is 
+ * represented by an 8-bit value.  The value 255 is reserved. 
+ * The constant MAX_NESTED_TRANSACTIONS is one less because
+ * one slot in the packed leaf entry is used for the implicit
+ * root transaction (id 0).
+ */
+
+
+enum {MAX_NESTED_TRANSACTIONS = 253};
+enum {MAX_TRANSACTION_RECORDS = MAX_NESTED_TRANSACTIONS + 1};
 
 
 #endif
