897964d945cb00222bed21c153e701a04e1f3218
Rich Prohaska <prohaska@tokutek.com>
2013-04-17 00:02:17 -0400

    refs #5947 rename to vlq
    git-svn-id: file:///svn/mysql/tokudb-engine/tokudb-engine@52810 c7de825b-a66e-492c-adef-691d508d4ae1

--- storage/tokudb/tests/vlq_test.cc
+++ storage/tokudb/tests/vlq_test.cc
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <assert.h>
-#include <tokudb_base128.h>
+#include <tokudb_vlq.h>
 
 int main(void) {
     uint32_t n;
@@ -11,41 +11,41 @@ int main(void) {
 
     printf("%u\n", 0);
     for (uint32_t v = 0; v < (1<<7); v++) {
-        out_s = tokudb::base128_encode_uint32(v, b, sizeof b);
+        out_s = tokudb::vlq_encode_uint32(v, b, sizeof b);
         assert(out_s == 1);
-        in_s = tokudb::base128_decode_uint32(&n, b, out_s);
+        in_s = tokudb::vlq_decode_uint32(&n, b, out_s);
         assert(in_s == 1 && n == v);
     }
 
     printf("%u\n", 1<<7);
     for (uint32_t v = (1<<7); v < (1<<14); v++) {
-        out_s = tokudb::base128_encode_uint32(v, b, sizeof b);
+        out_s = tokudb::vlq_encode_uint32(v, b, sizeof b);
         assert(out_s == 2);
-        in_s = tokudb::base128_decode_uint32(&n, b, out_s);
+        in_s = tokudb::vlq_decode_uint32(&n, b, out_s);
         assert(in_s == 2 && n == v);
     }
 
     printf("%u\n", 1<<14);
     for (uint32_t v = (1<<14); v < (1<<21); v++) {
-        out_s = tokudb::base128_encode_uint32(v, b, sizeof b);
+        out_s = tokudb::vlq_encode_uint32(v, b, sizeof b);
         assert(out_s == 3);
-        in_s = tokudb::base128_decode_uint32(&n, b, out_s);
+        in_s = tokudb::vlq_decode_uint32(&n, b, out_s);
         assert(in_s == 3 && n == v);
     }
 
     printf("%u\n", 1<<21);
     for (uint32_t v = (1<<21); v < (1<<28); v++) {
-        out_s = tokudb::base128_encode_uint32(v, b, sizeof b);
+        out_s = tokudb::vlq_encode_uint32(v, b, sizeof b);
         assert(out_s == 4);
-        in_s = tokudb::base128_decode_uint32(&n, b, out_s);
+        in_s = tokudb::vlq_decode_uint32(&n, b, out_s);
         assert(in_s == 4 && n == v);
     }
 
     printf("%u\n", 1<<28);
     for (uint32_t v = (1<<28); v != 0; v++) {
-        out_s = tokudb::base128_encode_uint32(v, b, sizeof b);
+        out_s = tokudb::vlq_encode_uint32(v, b, sizeof b);
         assert(out_s == 5);
-        in_s = tokudb::base128_decode_uint32(&n, b, out_s);
+        in_s = tokudb::vlq_decode_uint32(&n, b, out_s);
         assert(in_s == 5 && n == v);
     }
 
