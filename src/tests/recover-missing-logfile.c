441c408d301c143f2f889dd28e707aee238c5cb8
Yoni Fogel <yoni@tokutek.com>
2013-04-16 23:58:53 -0400

    Addresses #652 refs[t:652] fixed recover test (changed expectation)
    git-svn-id: file:///svn/toku/tokudb@15857 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/recover-missing-logfile.c
+++ src/tests/recover-missing-logfile.c
@@ -66,7 +66,7 @@ static void run_recover (void) {
 
     r = db_env_create(&env, 0);                                                             CKERR(r);
     r = env->open(env, ENVDIR, envflags + DB_RECOVER, S_IRWXU+S_IRWXG+S_IRWXO);
-    assert(r == DB_RUNRECOVERY);
+    CKERR2(r, ENOENT);
 
     r = system("rm -rf " ENVDIR "/*.tokulog");
     CKERR(r);
