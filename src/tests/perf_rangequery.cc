48a7a41f2b4c8e231b71ad9ff38b5c11851e44ba
John Esmet <esmet@tokutek.com>
2013-04-17 00:01:28 -0400

    fixes #5924 add perf range query. the range query limit is specified via the command line.
    git-svn-id: file:///svn/toku/tokudb@52432 c7de825b-a66e-492c-adef-691d508d4ae1

---
+++ src/tests/perf_rangequery.cc
@@ -0,0 +1,37 @@
+/* -*- mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+// vim: ft=cpp:expandtab:ts=8:sw=4:softtabstop=4:
+#ident "Copyright (c) 2007 Tokutek Inc.  All rights reserved."
+#ident "$Id: perf_rangequery.cc 52406 2013-01-24 00:45:19Z esmet $"
+#include "test.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <toku_pthread.h>
+#include <unistd.h>
+#include <memory.h>
+#include <sys/stat.h>
+#include <db.h>
+
+#include "threaded_stress_test_helpers.h"
+
+static void
+stress_table(DB_ENV* env, DB** dbp, struct cli_args *cli_args) {
+    if (verbose) printf("starting creation of pthreads\n");
+    const int num_threads = cli_args->num_ptquery_threads;
+    struct arg myargs[num_threads];
+    for (int i = 0; i < num_threads; i++) {
+        arg_init(&myargs[i], dbp, env, cli_args);
+        myargs[i].operation = rangequery_op;
+        myargs[i].txn_flags |= DB_TXN_READ_ONLY;
+    }
+    run_workers(myargs, num_threads, cli_args->num_seconds, false, cli_args);
+}
+
+int
+test_main(int argc, char *const argv[]) {
+    struct cli_args args = get_default_args_for_perf();
+    parse_stress_test_args(argc, argv, &args);
+    perf_test_main(&args);
+    return 0;
+}
