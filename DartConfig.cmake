08449ab58b1b6554d14b9d37b41857288d5ba0ac
Leif Walsh <leif@tokutek.com>
2013-04-17 00:00:26 -0400

    [t:4814] don't error on __libc_freeres with valgrind
    git-svn-id: file:///svn/toku/tokudb@43248 c7de825b-a66e-492c-adef-691d508d4ae1

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -1,4 +1,4 @@
 if(BUILD_TESTING)
-  set(MEMORYCHECK_COMMAND_OPTIONS "--gen-suppressions=no --num-callers=20 --leak-check=full --show-reachable=yes --trace-children=yes --trace-children-skip=sh,*/sh,basename,*/basename,dirname,*/dirname,rm,*/rm,cp,*/cp,mv,*/mv,cat,*/cat,diff,*/diff,test,*/tokudb_dump* --trace-children-skip-by-arg=--only_create,--test,--no-shutdown" CACHE INTERNAL "options for valgrind")
+  set(MEMORYCHECK_COMMAND_OPTIONS "--gen-suppressions=no --num-callers=20 --leak-check=full --show-reachable=yes --run-libc-freeres=no --trace-children=yes --trace-children-skip=sh,*/sh,basename,*/basename,dirname,*/dirname,rm,*/rm,cp,*/cp,mv,*/mv,cat,*/cat,diff,*/diff,test,*/tokudb_dump* --trace-children-skip-by-arg=--only_create,--test,--no-shutdown" CACHE INTERNAL "options for valgrind")
   set(MEMORYCHECK_SUPPRESSIONS_FILE "${CMAKE_CURRENT_BINARY_DIR}/valgrind.suppressions" CACHE INTERNAL "suppressions file for valgrind")
 endif()
