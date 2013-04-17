mysql/mysql-server

commit 2befbca5e83c96f7650bd4cff47632d40b378b99

Addresses #2042 refs[t:2042] Fix race condition at lower level.
git-svn-id: file:///svn/toku/tokudb@14802 c7de825b-a66e-492c-adef-691d508d4ae1

newbrt/checkpoint.c
@@ -143,8 +143,10 @@ toku_multi_operation_client_unlock(void) {
 
 void 
 toku_checkpoint_safe_client_lock(void) {
+    toku_multi_operation_client_lock();
     int r = toku_pthread_rwlock_rdlock(&checkpoint_safe_lock);   
     assert(r == 0);
+    toku_multi_operation_client_unlock();
 }
 
 void 

commit 08559dad60a103b27fcc820535f290ba88f3db8f

Closes #2055 closes[t:2055] Addresses #2042 refs[t:2042] Fix race condition caused by r14802 A client (read lock) must hold the multi-operation lock during the entire duration of holding the checkpoint-safe lock. The multi-operation lock was being released too early.
git-svn-id: file:///svn/toku/tokudb@14845 c7de825b-a66e-492c-adef-691d508d4ae1

newbrt/checkpoint.c
@@ -152,13 +152,13 @@ toku_checkpoint_safe_client_lock(void) {
     toku_multi_operation_client_lock();
     int r = toku_pthread_rwlock_rdlock(&checkpoint_safe_lock);   
     assert(r == 0);
-    toku_multi_operation_client_unlock();
 }
 
 void 
 toku_checkpoint_safe_client_unlock(void) {
     int r = toku_pthread_rwlock_rdunlock(&checkpoint_safe_lock); 
     assert(r == 0);
+    toku_multi_operation_client_unlock();
 }
 
 