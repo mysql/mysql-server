373892cbdae8cde6f3ea38a1ef82a59cd50d0489
Yoni Fogel <yoni@tokutek.com>
2013-04-16 23:57:30 -0400

    Closes #1295 Merged in 1.08.1 fixes as well as some minor changes required to get them to work/Makefiles
    git-svn-id: file:///svn/toku/tokudb.1032b@8148 c7de825b-a66e-492c-adef-691d508d4ae1

---
+++ src/tests/helgrind1.c
@@ -0,0 +1,24 @@
+// The helgrind1.tdbrun test should fail.  This is merely a check to verify that helgrind actually notices a race.
+#include <assert.h>
+#include <pthread.h>
+#include "test.h"
+int x;
+
+void *starta(void* ignore __attribute__((__unused__))) {
+    x++;
+    return 0;
+}
+void *startb(void* ignore __attribute__((__unused__))) {
+    x++;
+    return 0;
+}
+
+int main (int argc, const char *argv[]) {
+    parse_args(argc, argv);
+    pthread_t a,b;
+    { int x_l = pthread_create(&a, NULL, starta, NULL); assert(x_l==0); }
+    { int x_l = pthread_create(&b, NULL, startb, NULL); assert(x_l==0); }
+    { int x_l = pthread_join(a, NULL);           assert(x_l==0); }
+    { int x_l = pthread_join(b, NULL);           assert(x_l==0); }
+    return 0;
+}
