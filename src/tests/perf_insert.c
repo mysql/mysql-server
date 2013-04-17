4b373ecd8842e95c130ea084005d63cae656a238
Leif Walsh <leif@tokutek.com>
2013-04-17 00:00:48 -0400

    [t:5027] compile fix
    git-svn-id: file:///svn/toku/tokudb@44383 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/perf_insert.c
+++ src/tests/perf_insert.c
@@ -33,7 +33,7 @@ stress_table(DB_ENV* env, DB** dbp, struct cli_args *cli_args) {
         arg_init(&myargs[i], dbp, env, cli_args);
         myargs[i].operation = put_op;
         if (cli_args->serial_insert) {
-            myargs[i].operation_extra = spe[i];
+            myargs[i].operation_extra = &spe[i];
         }
     }
     run_workers(myargs, num_threads, cli_args->time_of_test, false, cli_args);
