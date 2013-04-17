7a06c949f052f68cc22fb4efe507a78b5f7d2b25
Leif Walsh <leif@tokutek.com>
2013-04-17 00:00:52 -0400

    [t:4871] remove the need for the pre-dumped file for test1426, since we removed it from the repo
    git-svn-id: file:///svn/toku/tokudb@44836 c7de825b-a66e-492c-adef-691d508d4ae1

--- src/tests/run_test1426.sh
+++ src/tests/run_test1426.sh
@@ -8,11 +8,9 @@ tdbbin=$1; shift
 bdbbin=$1; shift
 tdbdump=$1; shift
 bdbdump=$1; shift
-original_bdbdump=$1; shift
 
 $bdbbin
 $bdbdump -p -h dir.test1426.c.bdb main > dump.bdb.1426
-diff dump.bdb.1426 $original_bdbdump
 
 $tdbbin
 $tdbdump -x -p -h dir.test1426.c.tdb main > dump.tdb.1426
