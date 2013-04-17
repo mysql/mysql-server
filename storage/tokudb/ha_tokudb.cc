f629cdcd6aa164b525eb610b5e1932f564d63f42
John Esmet <esmet@tokutek.com>
2013-04-17 00:02:06 -0400

    close[t:3990] atis-test no longer reports invalid frees
    git-svn-id: file:///svn/mysql/tokudb-engine/tokudb-engine@35223 c7de825b-a66e-492c-adef-691d508d4ae1

--- storage/tokudb/ha_tokudb.cc
+++ storage/tokudb/ha_tokudb.cc
@@ -2146,10 +2146,16 @@ int ha_tokudb::__close(int mutex_is_locked) {
     my_free(alloc_ptr, MYF(MY_ALLOW_ZERO_PTR));
     my_free(range_query_buff, MYF(MY_ALLOW_ZERO_PTR));
     for (u_int32_t i = 0; i < sizeof(mult_rec_dbt)/sizeof(mult_rec_dbt[0]); i++) {
-        if (mult_rec_dbt[i].flags == DB_DBT_REALLOC && mult_rec_dbt[i].data != NULL) my_free(mult_rec_dbt[i].data, MYF(MY_ALLOW_ZERO_PTR));
+        if (mult_rec_dbt[i].flags == DB_DBT_REALLOC &&
+                mult_rec_dbt[i].data != NULL) {
+            free(mult_rec_dbt[i].data);
+        }
     }
     for (u_int32_t i = 0; i < sizeof(mult_key_dbt)/sizeof(mult_key_dbt[0]); i++) {
-        if (mult_key_dbt[i].flags == DB_DBT_REALLOC && mult_key_dbt[i].data != NULL) my_free(mult_key_dbt[i].data, MYF(MY_ALLOW_ZERO_PTR));
+        if (mult_key_dbt[i].flags == DB_DBT_REALLOC &&
+                mult_key_dbt[i].data != NULL) {
+            free(mult_key_dbt[i].data);
+        }
     }
     rec_buff = NULL;
     rec_update_buff = NULL;
