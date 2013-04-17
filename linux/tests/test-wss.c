6c03cb61509b89e6382142cc05fc98678f47c74a
Rich Prohaska <prohaska@tokutek.com>
2013-04-16 23:57:43 -0400

    make the linux portability tests slightly better. addresses #1524
    git-svn-id: file:///svn/toku/tokudb@9875 c7de825b-a66e-492c-adef-691d508d4ae1

--- linux/tests/test-wss.c
+++ linux/tests/test-wss.c
@@ -6,23 +6,40 @@
 #include <inttypes.h>
 #include <toku_os.h>
 
+const int nbuffers = 1000;
+const int buffersize = 1024*1024;
+
 static void do_mallocs(void) {
     int i;
-    for (i=0; i<1000; i++) {
-        int nbytes = 1024*1024;
+    for (i=0; i<nbuffers; i++) {
+        int nbytes = buffersize;
         void *vp = malloc(nbytes);
         memset(vp, 0, nbytes);
     }
 }
 
-int main(void) {
-    int64_t rss;
+int main(int argc, char *argv[]) {
+    int verbose = 0;
+    int i;
+    for (i=1; i<argc; i++) {
+        if (strcmp(argv[i], "-v") == 0) {
+            verbose = 1;
+            continue;
+        }
+        if (strcmp(argv[i], "-q") == 0) {
+            verbose = 0;
+            continue;
+        }
+    }
 
+    int64_t rss;
     toku_os_get_max_rss(&rss);
-    printf("%"PRId64"\n", rss);
+    if (verbose) printf("%"PRId64"\n", rss);
+    assert(rss < nbuffers*buffersize);
     do_mallocs();
     toku_os_get_max_rss(&rss);
-    printf("%"PRId64"\n", rss);
+    if (verbose) printf("%"PRId64"\n", rss);
+    assert(rss > nbuffers*buffersize);
 
     return 0;
 }
