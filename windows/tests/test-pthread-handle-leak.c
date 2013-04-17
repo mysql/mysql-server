79a7937a2fcc15b71fe4078377e6039bc9a5f129
Rich Prohaska <prohaska@tokutek.com>
2013-04-16 23:57:52 -0400

    merge changeset 11870 from the 2.0.0 branch to main. addresses #1750
    git-svn-id: file:///svn/toku/tokudb@11878 c7de825b-a66e-492c-adef-691d508d4ae1

---
+++ windows/tests/test-pthread-handle-leak.c
@@ -0,0 +1,31 @@
+// test for a pthread handle leak
+
+#include "toku_portability.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <assert.h>
+#include <unistd.h>
+#include <toku_pthread.h>
+
+static void *mythreadfunc(void *arg) {
+    return arg;
+}
+
+int main(void) {
+#define N 1000000
+
+    int i;
+    for (i=0; i<N; i++) {
+	int r;
+	toku_pthread_t tid;
+        r = toku_pthread_create(&tid, NULL, mythreadfunc, (void *)i);
+	assert(r == 0);
+        void *ret;
+        r = toku_pthread_join(tid, &ret);
+        assert(r == 0 && ret == (void*)i);
+    }
+    printf("ok\n"); fflush(stdout);
+    return 0;
+}
+
