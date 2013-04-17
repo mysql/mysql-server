bee27669f7712d2046727c24d738adacef300c74
Yoni Fogel <yoni@tokutek.com>
2013-04-16 23:57:31 -0400

    Addresses #1321 Fixed bug from [8370], reimplemented native windows calls
    git-svn-id: file:///svn/toku/tokudb.1032b@8376 c7de825b-a66e-492c-adef-691d508d4ae1

--- windows/files.c
+++ windows/files.c
@@ -4,28 +4,52 @@
 #include <unistd.h>
 #include <windows.h>
 
-// Note: pread and pwrite are not thread safe on the same fildes as they
-// rely on the file offset
-
 int64_t
 pread(int fildes, void *buf, size_t nbyte, int64_t offset) {
-    int64_t r = _lseeki64(fildes, offset, SEEK_SET);
-    if (r>=0) {
-        assert(r==offset);
-        r = read(fildes, buf, nbyte);
+    HANDLE     filehandle;
+    OVERLAPPED win_offset = {0}; 
+    filehandle = (HANDLE)_get_osfhandle(fildes);
+    int64_t r;
+    if (filehandle==INVALID_HANDLE_VALUE) {
+        r = errno; assert(r!=0);
+        goto cleanup;
     }
+    win_offset.Offset     = offset % (1LL<<32LL);
+    win_offset.OffsetHigh = offset / (1LL<<32LL);
+
+    DWORD bytes_read;
+    r = ReadFile(filehandle, buf, nbyte, &bytes_read, &win_offset);
+    if (!r) {
+        r = GetLastError();
+        if (r==ERROR_HANDLE_EOF) r = bytes_read;
+    }
+    else    r = bytes_read;
+
     // printf("%s: %d %p %u %I64d %I64d\n", __FUNCTION__, fildes, buf, nbyte, offset, r); fflush(stdout);
+cleanup:
     return r;
 }
 
 int64_t
 pwrite(int fildes, const void *buf, size_t nbyte, int64_t offset) {
-    int64_t r = _lseeki64(fildes, offset, SEEK_SET);
-    if (r>=0) {
-        assert(r==offset);
-        r = write(fildes, buf, nbyte);
+    HANDLE     filehandle;
+    OVERLAPPED win_offset = {0}; 
+    filehandle = (HANDLE)_get_osfhandle(fildes);
+    int64_t r;
+    if (filehandle==INVALID_HANDLE_VALUE) {
+        r = errno; assert(r!=0);
+        goto cleanup;
     }
+    win_offset.Offset     = offset % (1LL<<32LL);
+    win_offset.OffsetHigh = offset / (1LL<<32LL);
+
+    DWORD bytes_written;
+    r = WriteFile(filehandle, buf, nbyte, &bytes_written, &win_offset);
+    if (!r) r = GetLastError();
+    else    r = bytes_written;
+
     // printf("%s: %d %p %u %I64d %I64d\n", __FUNCTION__, fildes, buf, nbyte, offset, r); fflush(stdout);
+cleanup:
     return r;
 }
 
