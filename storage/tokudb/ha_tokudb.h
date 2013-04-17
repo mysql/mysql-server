23a56da73746a1b008bb7ddb097cbe688577d788
Zardosht Kasheff <zardosht@tokutek.com>
2013-04-17 00:02:03 -0400

    [t:3014], get rid of remove_keys from .h file, should have been part of last checkin
    git-svn-id: file:///svn/mysql/tokudb-engine/tokudb-engine@25294 c7de825b-a66e-492c-adef-691d508d4ae1

--- storage/tokudb/ha_tokudb.h
+++ storage/tokudb/ha_tokudb.h
@@ -295,7 +295,6 @@ private:
     DBT* create_dbt_key_for_lookup(DBT * key, KEY* key_info, uchar * buff, const uchar * record, bool* has_null, int key_length = MAX_KEY_LENGTH);
     DBT *pack_key(DBT * key, uint keynr, uchar * buff, const uchar * key_ptr, uint key_length, int8_t inf_byte);
     int remove_key(DB_TXN * trans, uint keynr, const uchar * record, DBT * prim_key);
-    int remove_keys(DB_TXN * trans, const uchar * record, DBT * prim_key);
     int key_cmp(uint keynr, const uchar * old_row, const uchar * new_row);
     int handle_cursor_error(int error, int err_to_return, uint keynr);
     DBT *get_pos(DBT * to, uchar * pos);
