73e060e3bc7d46346538ca5298d3bdc99e2459fa
Rich Prohaska <prohaska@tokutek.com>
2013-04-16 23:57:55 -0400

    merge tokudb.1856 13208:HEAD to main. addresses #1856
    git-svn-id: file:///svn/toku/tokudb@13278 c7de825b-a66e-492c-adef-691d508d4ae1

--- newbrt/brt.c
+++ newbrt/brt.c
@@ -2298,7 +2298,8 @@ brt_merge_child (BRT t, BRTNODE node, int childnum_to_merge, BOOL *did_io, BOOL 
         verify_local_fingerprint_nonleaf(childa);
         if (childa->height>0) { int i; for (i=0; i+1<childa->u.n.n_children; i++) assert(childa->u.n.childkeys[i]); }
         //(toku_verify_counts(childa), toku_verify_estimates(t,childa));
-        *did_react = did_merge;
+        // the tree did react if a merge (did_merge) or rebalance (new spkit key) occurred
+        *did_react = did_merge || (splitk_kvpair != 0);
         if (did_merge) assert(!splitk_kvpair); else assert(splitk_kvpair);
         if (r!=0) goto return_r;
 
