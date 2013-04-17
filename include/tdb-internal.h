534fcc0652f7d354eb8a9db62d12a7c3cf922b86
Bradley C. Kuszmaul <bradley@tokutek.com>
2013-04-16 23:58:57 -0400

    Fixes #2276.  Closes[t:2276]
    git-svn-id: file:///svn/toku/tokudb@16733 c7de825b-a66e-492c-adef-691d508d4ae1

--- include/tdb-internal.h
+++ include/tdb-internal.h
@@ -3,9 +3,14 @@
 
 #ident "Copyright (c) 2007 Tokutek Inc.  All rights reserved."
 
-#include "toku_list.h"
 // Included by db.h, defines some internal structures.  These structures are inlined in some versions of db.h
 // the types DB_TXN and so forth have been defined.
+
+// This list structure is repeated here (from toku_list.h) so that the db.h file will be standalone.  Any code that depends on this list matching the structure in toku_list.h
+// will get flagged by the compiler if someone changes one but not the other.
+struct toku_list {
+    struct toku_list *next, *prev;
+};
 
 struct simple_dbt {
     u_int32_t len;
