416dad85feadb7c5a86f5083c4e299300a414645
Bradley C. Kuszmaul <bradley@tokutek.com>
2013-04-17 00:00:59 -0400

    Fix #5161.  Partitioned statistics implemented.
    git-svn-id: file:///svn/toku/tokudb@45847 c7de825b-a66e-492c-adef-691d508d4ae1

---
+++ ft/partitioned_counter.c
@@ -0,0 +1,134 @@
+/* -*- mode: C; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+// vim: expandtab:ts=8:sw=4:softtabstop=4:
+#include "partitioned_counter.h"
+#include "memory.h"
+#include <pthread.h>
+#include <valgrind/helgrind.h>
+
+struct local_counter {
+    unsigned long        sum;
+    struct local_counter *prev, *next;
+    PARTITIONED_COUNTER   owner;
+};
+
+struct partitioned_counter {
+    unsigned long   sum_of_dead;
+    pthread_key_t   key;
+    struct local_counter *first, *last;
+};
+
+// We have a single mutex for all the counters because
+//  (a) the mutex is obtained infrequently, and
+//  (b) it helps us avoid race conditions when destroying the counters.
+static pthread_mutex_t partitioned_counter_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+
+static void pc_lock (void) {
+    int r = pthread_mutex_lock(&partitioned_counter_mutex);
+    assert(r==0);
+}
+static void pc_unlock (void) {
+    int r = pthread_mutex_unlock(&partitioned_counter_mutex);
+    assert(r==0);
+}
+
+static void local_destroy_counter (void *counterp) {
+    pc_lock();
+    struct local_counter *lc = (struct local_counter*)counterp;
+    PARTITIONED_COUNTER owner = lc->owner;
+    // Save the sum
+    owner->sum_of_dead += lc->sum;
+    // Remove from linked list.
+    if (lc->prev) {
+	lc->prev->next = lc->next;
+    } else {
+	owner->first = lc->next;
+    }
+    if (lc->next) {
+	lc->next->prev = lc->prev;
+    } else {
+	owner->last = lc->prev;
+    }
+    
+    // Free the local part of the counter and return.
+    toku_free(lc);
+    {
+	int r = pthread_setspecific(owner->key, NULL);
+	assert(r==0);
+    }
+    pc_unlock();
+}
+
+
+PARTITIONED_COUNTER create_partitioned_counter(void) 
+// Effect: Create a counter, initialized to zero.
+{
+    PARTITIONED_COUNTER MALLOC(result);
+    result->sum_of_dead = 0;
+    {
+	int r = pthread_key_create(&result->key, local_destroy_counter);
+	assert(r==0);
+    }
+    result->first = NULL;
+    result->last  = NULL;
+    return result;
+}
+
+void destroy_partitioned_counter (PARTITIONED_COUNTER pc)
+// Effect: Destroy the counter.  No operations on that counter are permitted after this.
+// Implementation note: Since we have a global lock, we can destroy all the key-specific versions as well.
+{
+    pc_lock();
+    while (pc->first) {
+	struct local_counter *next = pc->first->next;
+	assert(pc->first->owner==pc);
+	toku_free(pc->first);
+	pc->first = next;
+    }
+    {
+	int r = pthread_key_delete(pc->key);
+	assert(r==0);
+    }
+    toku_free(pc);
+    pc_unlock();
+}
+
+void increment_partitioned_counter (PARTITIONED_COUNTER pc, unsigned long amount)
+// Effect: Increment the counter by amount.
+// Requires: No overflows.  This is a 64-bit unsigned counter.
+// Requires: You may not increment this after a destroy has occured.
+{
+    struct local_counter *lc = (struct local_counter *) pthread_getspecific(pc->key);
+    if (lc==NULL) {
+	pc_lock();
+	MALLOC(lc);
+	lc->sum = 0;
+	VALGRIND_HG_DISABLE_CHECKING(&lc->sum, sizeof(lc->sum)); // the counter increment is kind of racy.
+	lc->prev = pc->last;
+	lc->next = NULL;
+	lc->owner = pc;
+	if (pc->first==NULL) {
+	    pc->first = lc;
+	} else {
+	    pc->last->next = lc;
+	}
+	pc->last = lc;
+	if (pc->first==NULL) pc->first=lc;
+	int r = pthread_setspecific(pc->key, lc);
+	assert(r==0);
+	pc_unlock();
+    }
+    lc->sum += amount;
+}
+
+unsigned long read_partitioned_counter (PARTITIONED_COUNTER pc)
+// Effect: Return the current value of the counter.
+{
+    pc_lock();
+    unsigned long sum = pc->sum_of_dead;
+    for (struct local_counter *lc = pc->first; lc; lc=lc->next) {
+	sum += lc->sum;
+    }
+    pc_unlock();
+    return sum;
+}
---
+++ ft/partitioned_counter.h
@@ -0,0 +1,39 @@
+#ifndef THREAD_LOCAL_COUNTER_H
+#define THREAD_LOCAL_COUNTER_H
+
+// Overview: A partitioned_counter provides a counter that can be incremented and the running sum can be read at any time.
+//  We assume that increments are frequent, whereas reading is infrequent.
+// Implementation hint: Use thread-local storage so each thread increments its own data.  The increment does not require a lock or atomic operation.
+//  Reading the data can be performed by iterating over the thread-local versions, summing them up.
+//  The data structure also includes a sum for all the threads that have died.
+//  Use a pthread_key to create the thread-local versions.  When a thread finishes, the system calls pthread_key destructor which can add that thread's copy
+//  into the sum_of_dead counter.
+// Rationale: For statistics such as are found in engine status, we need a counter that requires no cache misses to increment.  We've seen significant
+//  performance speedups by removing certain counters.  Rather than removing those statistics, we would like to just make the counter fast.
+//  We generally increment the counters frequently, and want to fetch the values infrequently.
+//  The counters are monotonic.
+//  The counters can be split into many counters, which can be summed up at the end.
+//  We don't care if we get slightly out-of-date counter sums when we read the counter.  We don't care if there is a race on reading the a counter
+//   variable and incrementing.
+//  See tests/test_partitioned_counter.c for some performance measurements.
+// Operations:
+//   create_partitioned_counter    Create a counter initialized to zero.
+//   destroy_partitioned_counter   Destroy it.
+//   increment_partitioned_counter Increment it.  This is the frequent operation.
+//   read_partitioned_counter      Get the current value.  This is infrequent.
+
+typedef struct partitioned_counter *PARTITIONED_COUNTER;
+PARTITIONED_COUNTER create_partitioned_counter(void);
+// Effect: Create a counter, initialized to zero.
+
+void destroy_partitioned_counter (PARTITIONED_COUNTER);
+// Effect: Destroy the counter.  No operations on that counter are permitted after this.
+
+void increment_partitioned_counter (PARTITIONED_COUNTER, unsigned long amount);
+// Effect: Increment the counter by amount.
+// Requires: No overflows.  This is a 64-bit unsigned counter.
+
+unsigned long read_partitioned_counter (PARTITIONED_COUNTER);
+// Effect: Return the current value of the counter.
+
+#endif
