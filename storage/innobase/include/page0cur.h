629c4decaa6c7832cdde2095fbebbea0dad4cf32
Marko Mäkelä <marko.makela@oracle.com>
2013-04-18 12:26:56 +0300

    Bug#16684523 INFORMATION_SCHEMA.INNODB_LOCKS UNNECESSARILY FETCHES BLOCK FOR INFIMUM,SUPREMUM
    The function fill_locks_row(), which is responsible for providing the
    data for the INFORMATION_SCHEMA.INNODB_LOCKS table, tries to look up
    the B-tree page in the buffer pool even for the pre-determined infimum
    and supremum records, which have pre-determined heap_no 0 and 1. This
    will generate some unnecessary contention on the buffer pool, and will
    unnecessarily omit some information when the page does not happen to
    be available in the buffer pool.
    
    Approved by Sunny Bains

--- storage/innobase/include/page0cur.h
+++ storage/innobase/include/page0cur.h
@@ -376,7 +376,9 @@ page_delete_rec(
 /** Index page cursor */
 
 struct page_cur_t{
-	byte*		rec;	/*!< pointer to a record on page */
+	const dict_index_t*	index;
+	rec_t*		rec;	/*!< pointer to a record on page */
+	ulint*		offsets;
 	buf_block_t*	block;	/*!< pointer to the block containing rec */
 };
 
