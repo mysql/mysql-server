a90539079ebdf3b98df7fd97dd577b2b0b878afa
Marko Mäkelä <marko.makela@oracle.com>
2013-04-19 09:57:31 +0300

    Bug#16690463 REMOVE SOME DEAD CODE
    lock_rec_other_has_expl_req() cleanup:
    Rename a parameter to "bool wait" and correct the comment.
    wait=LOCK_WAIT used to mean the opposite what the comment said.
    Remove the constant parameter gap=0.
    
    page_cur_invalidate(), btr_cur_invalidate():  Unused functions, remove.
    
    Approved by Sunny Bains

--- storage/innobase/include/page0cur.h
+++ storage/innobase/include/page0cur.h
@@ -136,13 +136,6 @@ page_cur_position(
 					the record */
 	page_cur_t*		cur);	/*!< out: page cursor */
 /**********************************************************//**
-Invalidates a page cursor by setting the record pointer NULL. */
-UNIV_INLINE
-void
-page_cur_invalidate(
-/*================*/
-	page_cur_t*	cur);	/*!< out: page cursor */
-/**********************************************************//**
 Moves the cursor to the next record on page. */
 UNIV_INLINE
 void
