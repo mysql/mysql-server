1f4d4c860d4c3c436c8eb7c18eb4b9bb4a76d813
Marko Mäkelä <marko.makela@oracle.com>
2013-04-22 15:40:28 +0300

    WL#6871 cursor code cleanup. Remove the unused mode PAGE_CUR_LE_OR_EXTENDS. It was both introduced and disabled in MySQL 4.0:
      revno: 1416.1.150
      revision-id: sp1r-heikki@hundin.mysql.fi-20030207154409-50631
      parent: sp1r-heikki@hundin.mysql.fi-20030207153315-50621
      committer: heikki@hundin.mysql.fi
      timestamp: Fri 2003-02-07 17:44:09 +0200
      message:
        page0cur.c:
          Disable PAGE_CUR_LE_OR_EXTENDS because it does not work for
          non-latin1 char sets now
    
      revno: 1352.2.13
      revision-id: sp1r-heikki@hundin.mysql.fi-20021029211646-41337
      parent: sp1r-Sinisa@sinisa.nasamreza.org-20021029122123-37723
      committer: heikki@hundin.mysql.fi
      timestamp: Tue 2002-10-29 23:16:46 +0200
      message:
        Many files:
          Merge InnoDB-4.0.5: new isolation levels READ COMMITTED and
          READ UNCOMMITTED now supported, selective deadlock resolution
        mysqld.cc:
          Change MySQL default isolation level to REPEATABLE READ; note
          that InnoDB has always had that default, and BDB and MyISAM
          always run at SERIALIZABLE level anyway

--- storage/innobase/include/page0cur.h
+++ storage/innobase/include/page0cur.h
@@ -44,10 +44,6 @@ Created 10/4/1994 Heikki Tuuri
 #define	PAGE_CUR_GE	2
 #define	PAGE_CUR_L	3
 #define	PAGE_CUR_LE	4
-/*#define PAGE_CUR_LE_OR_EXTENDS 5*/ /* This is a search mode used in
-				 "column LIKE 'abc%' ORDER BY column DESC";
-				 we have to find strings which are <= 'abc' or
-				 which extend it */
 #ifdef UNIV_SEARCH_DEBUG
 # define PAGE_CUR_DBG	6	/* As PAGE_CUR_LE, but skips search shortcut */
 #endif /* UNIV_SEARCH_DEBUG */
