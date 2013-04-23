0f04c6877323777e1b65042424fc031146a7df60
Marko Mäkelä <marko.makela@oracle.com>
2013-04-23 16:18:32 +0300

    WL#6871 cleanup.
    page_cur_search(): Remove the constant parameter mode=PAGE_CUR_LE.

--- storage/innobase/include/page0cur.h
+++ storage/innobase/include/page0cur.h
@@ -269,9 +269,6 @@ page_cur_search(
 	const buf_block_t*	block,	/*!< in: buffer block */
 	const dict_index_t*	index,	/*!< in: record descriptor */
 	const dtuple_t*		tuple,	/*!< in: data tuple */
-	ulint			mode,	/*!< in: PAGE_CUR_L,
-					PAGE_CUR_LE, PAGE_CUR_G, or
-					PAGE_CUR_GE */
 	page_cur_t*		cursor);/*!< out: page cursor */
 /****************************************************************//**
 Searches the right position for a page cursor. */
