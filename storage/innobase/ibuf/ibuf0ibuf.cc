e645422de7f30a5406e331233cc8cd0355f35c76
Marko Mäkelä <marko.makela@oracle.com>
2013-04-23 16:18:32 +0300

    WL#6871 cleanup.
    page_cur_search(): Remove the constant parameter mode=PAGE_CUR_LE.

--- storage/innobase/ibuf/ibuf0ibuf.cc
+++ storage/innobase/ibuf/ibuf0ibuf.cc
@@ -4066,8 +4066,7 @@ dump:
 		return;
 	}
 
-	low_match = page_cur_search(block, index, entry,
-				    PAGE_CUR_LE, &page_cur);
+	low_match = page_cur_search(block, index, entry, &page_cur);
 
 	heap = mem_heap_create(
 		sizeof(upd_t)
@@ -4180,8 +4179,7 @@ ibuf_set_del_mark(
 	ut_ad(ibuf_inside(mtr));
 	ut_ad(dtuple_check_typed(entry));
 
-	low_match = page_cur_search(
-		block, index, entry, PAGE_CUR_LE, &page_cur);
+	low_match = page_cur_search(block, index, entry, &page_cur);
 
 	if (low_match == dtuple_get_n_fields(entry)) {
 		rec_t*		rec;
@@ -4246,8 +4244,7 @@ ibuf_delete(
 	ut_ad(ibuf_inside(mtr));
 	ut_ad(dtuple_check_typed(entry));
 
-	low_match = page_cur_search(
-		block, index, entry, PAGE_CUR_LE, &page_cur);
+	low_match = page_cur_search(block, index, entry, &page_cur);
 
 	if (low_match == dtuple_get_n_fields(entry)) {
 		page_zip_des_t*	page_zip= buf_block_get_page_zip(block);
