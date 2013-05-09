e1774fe1ef77736f6e0b910395bf775ca77e30ba
Nisha Gopalakrishnan <nisha.gopalakrishnan@oracle.com>
2013-05-09 09:37:49 +0530

    BUG#16315351 - ALTER TABLE DROP COLUMN: NO WARNING IF DUPLICATE                INDEX FOR THE SAME COLUMN
    Analysis
    -------
    
    When a column associated with a multi-column index is dropped,
    a NOTE is not reported in case a duplicate index exist on the
    table after the DROP COLUMN operation.
    
    EX: CREATE TABLE t1( fld1 INT, fld2 INT, KEY(fld1, fld2),    
                         KEY(fld2));
        ALTER TABLE t1 DROP COLUMN fld1;
    
    The duplicate index check was not invoked in the above mentioned
    case.
    
    Fix:
    ----
    The flag 'check_for_duplicate_indexes' is set, to trigger the 
    duplicate index check when the column associated with an index 
    is dropped.

--- sql/handler.h
+++ sql/handler.h
@@ -1359,7 +1359,8 @@ typedef struct st_key_create_information
   /**
     A flag to determine if we will check for duplicate indexes.
     This typically means that the key information was specified
-    directly by the user (set by the parser).
+    directly by the user (set by the parser) or a column
+    associated with it was dropped.
   */
   bool check_for_duplicate_indexes;
 } KEY_CREATE_INFO;
