2561aa5bde96e278a80b19cc6d8f98b73bb5ff98
Craig L Russell <Craig.Russell@oracle.com>
2013-05-09 18:05:45 -0700

    Clusterj time2 types Support new mysql 5.6 time with fractional seconds Support is for milliseconds as implemented by java.util.Date Future plans to support microseconds for java.sql.Timestamp These changes are being pushed to mysql 5.1 cluster 7.1 because they are not specific to any version
    clusterj-api ColumnType.java
    added types corresponding to new types Datetime2, Time2, Timestamp2
    
    clusterj-core DomainFieldHandlerImpl.java
    support new time types the same as existing time types
    
    clusterj-test AbstractClusterJModelTest.java
    avoid setting Calendar DATE for time types
    
    clusterj-test TimestampAsSqlTimestampTypesTest.java
    enable this test
    
    clusterj-test TimestampAsUtilDateTypesTest.java
    enable this test
    
    clusterj-tie ColumnImpl.java
    support new time types
    improve error message to log error in addition to throwing exception
    
    clusterj-tie NdbRecordImpl.java
    support new time types aligned on 8 byte boundary
    
    clusterj-tie Utility.java
    support new time types as long (milliseconds per java.util.Date representation)
    
    ndbjtie NdbDictionary.java
    support new time types

--- storage/ndb/clusterj/clusterj-test/src/main/java/testsuite/clusterj/AbstractClusterJModelTest.java
+++ storage/ndb/clusterj/clusterj-test/src/main/java/testsuite/clusterj/AbstractClusterJModelTest.java
@@ -106,10 +106,10 @@ public abstract class AbstractClusterJModelTest extends AbstractClusterJTest {
         return result;
     }
 
-    /** Convert days, hours, minutes, and seconds into milliseconds after the Epoch, UCT.
-     * Date is index origin 1 so add one to the number of days. Default year and month,
+    /** Convert hours, minutes, and seconds into milliseconds after the Epoch, UCT.
+     * Default year, month, and day,
      * as these are assumed by Calendar to be the Epoch.
-     * @param day the number of days
+     * @param day the number of days: ignored
      * @param hour the hour (or number of hours)
      * @param minute the minute (or number of minutes)
      * @param second the second (or number of seconds)
@@ -118,7 +118,6 @@ public abstract class AbstractClusterJModelTest extends AbstractClusterJTest {
     protected static long getMillisFor(int days, int hour, int minute, int second) {
         Calendar calendar = Calendar.getInstance();
         calendar.clear();
-        calendar.set(Calendar.DATE, days + 1);
         calendar.set(Calendar.HOUR, hour);
         calendar.set(Calendar.MINUTE, minute);
         calendar.set(Calendar.SECOND, second);
