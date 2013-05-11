ced9292eb87b061fb7b8ac2190f01e9dec18f3d7
Libing Song <libing.song@oracle.com>
2013-05-12 07:17:29 +0800

    WL#5721 Refactor replication dump thread
    PROBLEMS
    ========
    * Dump threads acquired LOCK_log when reading each event. In fact, it
      is not necessary in most of the cases.
    
    * The code was too complex and mysql_binlog_send() was too huge. It was
      very difficult to maintain.
    
    * There was some code never used.
    
    * Deadlock on semi-synchronous replication
      WL#6355 introduces a feature that semi-synchronous waits for ACK from
      slave when the transaction's events is written to disc but before the
      transaction is committed.
    
      It introduced a three way deadlock.
    
        SESSION 1(Rotate)              SESSION 2(Trx Commit) DUMP THREAD
        ---------                      ---------             -----------
      1                                Flush Stage
                                       Signal update
      2 Rotate(hold LOCK_log)
        WAITING for Session2 to commit
      3                                Sync Stage            WAITING for Session1
                                                             to release LOCK_log
      4                                semisync
                                       WAITING for ACK
    
      Session1 had to hold LOCK_log to block other sessions appending new events.
      It also blocked dump threads reading events.
    
    DESIGN AND IMPLEMENTATION
    =========================
    Binary events can only be appended at the end of the active file. So it is safe
    to read both inactive and active binlog files without lock. The only problem
    is that dump thread may read an incomplete event at the end of active log file.
    
    To solve the problem, MYSQL_BIN_LOG needs a variable which records the end
    position of the last event in active file.
    
    * MYSQL_BIN_LOG::binlog_end_pos
      The end position of active file. It is safe to read all events before it
      without any lock. It should be updated after appending events but before
      broadcasting update signal.
    
    * MYSQL_BIN_LOG::LOCK_binlog_end_pos
      It is used to protect binlog_end_pos. The lock is required when both writing
      and reading binlog_end_pos.
    
    * Binlog_sender class
      The class is designed to make the code of mysql_binlog_send() has better
      modularity and readability. All code of original mysql_binlog_send is
      moved to the class. Now mysql_binlog_send just need to call Binlog_sender.

--- sql/binlog.cc
+++ sql/binlog.cc
@@ -2362,6 +2362,7 @@ void MYSQL_BIN_LOG::cleanup()
     mysql_mutex_destroy(&LOCK_index);
     mysql_mutex_destroy(&LOCK_commit);
     mysql_mutex_destroy(&LOCK_sync);
+    mysql_mutex_destroy(&LOCK_binlog_end_pos);
     mysql_mutex_destroy(&LOCK_xids);
     mysql_cond_destroy(&update_cond);
     my_atomic_rwlock_destroy(&m_prep_xids_lock);
@@ -2380,6 +2381,8 @@ void MYSQL_BIN_LOG::init_pthread_objects()
   mysql_mutex_init(m_key_LOCK_index, &LOCK_index, MY_MUTEX_INIT_SLOW);
   mysql_mutex_init(m_key_LOCK_commit, &LOCK_commit, MY_MUTEX_INIT_FAST);
   mysql_mutex_init(m_key_LOCK_sync, &LOCK_sync, MY_MUTEX_INIT_FAST);
+  mysql_mutex_init(m_key_LOCK_binlog_end_pos, &LOCK_binlog_end_pos,
+                   MY_MUTEX_INIT_FAST);
   mysql_mutex_init(m_key_LOCK_xids, &LOCK_xids, MY_MUTEX_INIT_FAST);
   mysql_cond_init(m_key_update_cond, &update_cond, 0);
   my_atomic_rwlock_init(&m_prep_xids_lock);
@@ -3409,6 +3412,7 @@ bool MYSQL_BIN_LOG::open_binlog(const char *log_name,
   close_purge_index_file();
 #endif
 
+  update_binlog_end_pos();
   DBUG_RETURN(0);
 
 err:
@@ -4977,7 +4981,8 @@ int MYSQL_BIN_LOG::new_file_impl(bool need_lock_log, Format_description_log_even
     log rotation should give the waiting thread a signal to
     discover EOF and move on to the next log.
   */
-  signal_update();
+  flush_io_cache(&log_file);
+  update_binlog_end_pos();
 
   old_name=name;
   name=0;				// Don't free name
@@ -5847,7 +5852,7 @@ bool MYSQL_BIN_LOG::write_incident(Incident_log_event *ev, bool need_lock_log,
     if (!error && !(error= flush_and_sync()))
     {
       bool check_purge= false;
-      signal_update();
+      update_binlog_end_pos();
       error= rotate(true, &check_purge);
       if (!error && check_purge)
         purge();
@@ -6044,9 +6049,9 @@ int MYSQL_BIN_LOG::wait_for_update_bin_log(THD* thd,
   DBUG_ENTER("wait_for_update_bin_log");
 
   if (!timeout)
-    mysql_cond_wait(&update_cond, &LOCK_log);
+    mysql_cond_wait(&update_cond, &LOCK_binlog_end_pos);
   else
-    ret= mysql_cond_timedwait(&update_cond, &LOCK_log,
+    ret= mysql_cond_timedwait(&update_cond, &LOCK_binlog_end_pos,
                               const_cast<struct timespec *>(timeout));
   DBUG_RETURN(ret);
 }
@@ -6083,7 +6088,8 @@ void MYSQL_BIN_LOG::close(uint exiting)
                   relay_log_checksum_alg != BINLOG_CHECKSUM_ALG_UNDEF);
       s.write(&log_file);
       bytes_written+= s.data_written;
-      signal_update();
+      flush_io_cache(&log_file);
+      update_binlog_end_pos();
     }
 #endif /* HAVE_REPLICATION */
 
@@ -6167,15 +6173,6 @@ void MYSQL_BIN_LOG::set_max_size(ulong max_size_arg)
   if (is_open())
     max_size= max_size_arg;
   mysql_mutex_unlock(&LOCK_log);
-  DBUG_VOID_RETURN;
-}
-
-
-void MYSQL_BIN_LOG::signal_update()
-{
-  DBUG_ENTER("MYSQL_BIN_LOG::signal_update");
-  signal_cnt++;
-  mysql_cond_broadcast(&update_cond);
   DBUG_VOID_RETURN;
 }
 
@@ -6936,7 +6933,7 @@ static inline int call_after_sync_hook(THD *queue_head)
   DBUG_ASSERT(queue_head != NULL);
   for (THD *thd= queue_head; thd != NULL; thd= thd->next_to_commit)
     if (likely(thd->commit_error == THD::CE_NONE))
-      thd->get_trans_pos(&log_file, &pos);
+      thd->get_trans_fixed_pos(&log_file, &pos);
 
   if (DBUG_EVALUATE_IF("simulate_after_sync_hook_error", 1, 0) ||
       RUN_HOOK(binlog_storage, after_sync, (queue_head, log_file, pos)))
@@ -7079,7 +7076,7 @@ int MYSQL_BIN_LOG::ordered_commit(THD *thd, bool all, bool skip_commit)
       flush_error= ER_ERROR_ON_WRITE;
     }
 
-    signal_update();
+    update_binlog_end_pos();
     DBUG_EXECUTE_IF("crash_commit_after_log", DBUG_SUICIDE(););
   }
 
