2812634b6c45b96bf3a0def5a6df4267304e8dca
Neeraj Bisht <neeraj.x.bisht@oracle.com>
2013-05-13 17:15:25 +0530

    Bug#12328597 - MULTIPLE COUNT(DISTINCT) IN SAME SELECT FALSE                WITH COMPOSITE KEY COLUMNS
    Problem:-
    While running a SELECT query with several AGGR(DISTINCT) function 
    and these are referring to different field of same composite key, 
    Returned incorrect value.
    
    Analysis:-
    
    In a table, where we have composite key like (a,b,c)
    and when we give a query like
    
    select COUNT(DISTINCT b), SUM(DISTINCT a) from ....
    
    here, we first make a list of items in Aggr(distinct) function
    (which is a, b), where order of item doesn't matter. 
    and then we see, whether we have a composite key where the prefix 
    of index columns matches the items of the aggregation function.
    (in this case we have a,b,c).
    
    if yes, so we can use loose index scan and we need not perform 
    duplicate removal to distinct in our aggregate function.
    
    In our table, we traverse column marked with <-- and get the result as
    (a,b,c)      count(distinct b)           sum(distinct a)
                 treated as count b          treated as sum(a)
    (1,1,2)<--              1                      1		
    (1,2,2)<--              1++=2                  1+1=2
    (1,2,3)		
    (2,1,2)<--              2++=3                  1+1+2=4
    (2,2,2)<--              3++=4                  1+1+2+2=6
    (2,2,3)
    
    result will be 4,6, but it should be (2,3)
    
    As in this case, our assumption is incorrect. If we have
    query like 
    select count(distinct a,b), sum(distinct a,b)from ..
    then we can use loose index scan
    
    Solution:-
    In our query, when we have more then one aggr(distinct) function 
    then they should refer to same  fields like
    
    select count(distinct a,b), sum(distinct a,b) from .. 
    
    -->we can use loose scan index as both aggr(distinct) refer to same fields a,b.
    
    If they are referring to different field like
    
    select count(distinct a), sum(distinct b) from .. 
    
    -->will not use loose scan index as both aggr(distinct) refer to different fields.

--- sql/sql_bitmap.h
+++ sql/sql_bitmap.h
@@ -71,6 +71,7 @@ public:
   my_bool is_subset(const Bitmap& map2) const { return bitmap_is_subset(&map, &map2.map); }
   my_bool is_overlapping(const Bitmap& map2) const { return bitmap_is_overlapping(&map, &map2.map); }
   my_bool operator==(const Bitmap& map2) const { return bitmap_cmp(&map, &map2.map); }
+  my_bool operator!=(const Bitmap& map2) const { return !(*this == map2); }
   char *print(char *buf) const
   {
     char *s=buf;
