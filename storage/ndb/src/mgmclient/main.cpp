a0c7f903cbf119aa6bb12d85cf40888d2e4306c2
magnus.blaudd@oracle.com <>
2013-05-13 12:10:50 +0200

    ndb - backport win define fixes also to 7.0

--- storage/ndb/src/mgmclient/main.cpp
+++ storage/ndb/src/mgmclient/main.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2013, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 
 // copied from mysql.cc to get readline
 extern "C" {
-#if defined( __WIN__)
+#if defined(_WIN32)
 #include <conio.h>
 #elif !defined(__NETWARE__)
 #include <readline/readline.h>
