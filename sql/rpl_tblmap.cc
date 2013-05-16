1e28b58e339a84368197578e0849d3d25338199e
Jon Olav Hauglid <jon.hauglid@oracle.com>
2013-05-16 13:42:49 +0200

    Bug#16768511: HAVE_REPLICATION SHOULD BE SET BY CMAKE
    This patch sets HAVE_REPLICATION from CMake rather than in my_global.h.
    This means that HAVE_REPLICATION is not dependent on my_global.h
    already having been included.
    
    The patch also moves #ifdef HAVE_REPLICATION in front of #includes,
    thereby reducing the number of files that have to be read when compiling
    embedded server.

--- sql/rpl_tblmap.cc
+++ sql/rpl_tblmap.cc
@@ -13,11 +13,9 @@
    along with this program; if not, write to the Free Software Foundation,
    51 Franklin Street, Suite 500, Boston, MA 02110-1335 USA */
 
-#include "sql_priv.h"
-#include "my_global.h"                          // HAVE_REPLICATION
-
 #ifdef HAVE_REPLICATION
-
+#include "sql_priv.h"
+#include "my_global.h"
 #include "rpl_tblmap.h"
 #ifndef MYSQL_CLIENT
 #include "table.h"
