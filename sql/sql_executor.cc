7d4775f7ee28abcd01a4c12d73b50f039cbe3e23
Tor Didriksen <tor.didriksen@oracle.com>
2013-05-16 10:50:44 +0200

    Bug#69119 Bug#16760474 Wrong FOUND_ROWS() on MySQL 5.6.11 Bug#69144 Bug#16764220 sql_calc_found_rows, found_rows() in 5.6.11 differs from 5.6.10 Bug#69166 Bug#16775557 Wrong result of FOUND_ROWS() in MySQL 5.6
    This is a regression caused by the patch for
    Bug#68458 Wrong count from FOUND_ROWS() on MySQL 5.6
    That patch restricted the setting of "thd->limit_found_rows"
    to the case where filesort had actually used a Priority Queue.
    
    The correct solution is to restrict the setting to the case
    where filesort *could* have used a priority queue
    (i.e. whenever we have "order by with limit")
    See code in check_if_pq_applicable() which returns false
    if it thinks that quicksort will be cheaper that PQ.

--- sql/sql_executor.cc
+++ sql/sql_executor.cc
@@ -934,9 +934,7 @@ do_select(JOIN *join)
 
   join->thd->limit_found_rows= join->send_records;
   /*
-    Use info provided by filesort for "order by with limit":
-
-    When using a Priority Queue, we cannot rely on send_records, but need
+    For "order by with limit", we cannot rely on send_records, but need
     to use the rowcount read originally into the join_tab applying the
     filesort. There cannot be any post-filtering conditions, nor any
     following join_tabs in this case, so this rowcount properly represents
@@ -958,8 +956,9 @@ do_select(JOIN *join)
       sort_tab= join_tab + const_tables;
     }
     if (sort_tab->filesort &&
-        sort_tab->filesort->using_pq)
+        sort_tab->filesort->limit != HA_POS_ERROR)
     {
+      DBUG_ASSERT(sort_tab->filesort->sortorder);
       join->thd->limit_found_rows= sort_tab->records;
     }
   }
