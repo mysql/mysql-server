72f823de4537f5322db6ca0debaea20d29f3e859
Praveenkumar Hulakund <praveenkumar.hulakund@oracle.com>
2013-05-24 15:26:03 +0530

    Bug#13418638 - CREATE TABLE IF NOT EXISTS METADATA LOCK IS                TOO RESTRICTIVE
    Analysis:
    ---------
    Creating table using "CREATE TABLE [IF NOT EXISTS]" hangs
    if table with same name already exists and if any other MDL
    lock is taken on the lock object of the table.
    
    While creating the table, we try to acquire "X" MDL lock on 
    the lock object of the table. Lock will be acquired on table,
    before even checking the existence of the table. Since "X"
    is not compatible with any other MDL locks, if some other
    lock is granted on the lock object of table then "X' lock
    will not be granted and it will hang for lock timeout
    period. 
    
    Fix:
    ----
    Acquiring "X" MDL lock before even checking for the existence
    of table is resulting in the issue mentioned above. So, as 
    a fix for this bug, modified a logic to acquire "S" lock and
    check for the existence of the table. If the table does not
    exists then upgrade lock to "X" and continue. If table exists
    then,
          for "CREATE TABLE" - report an error.
          for "CREATE TABLE IF NOT EXISTS" - Push a warning.
    and return.

--- sql/sql_base.cc
+++ sql/sql_base.cc
@@ -2802,7 +2802,8 @@ bool open_table(THD *thd, TABLE_LIST *table_list, Open_table_context *ot_ctx)
   hash_value= my_calc_hash(&table_def_cache, (uchar*) key, key_length);
 
 
-  if (table_list->open_strategy == TABLE_LIST::OPEN_IF_EXISTS)
+  if (table_list->open_strategy == TABLE_LIST::OPEN_IF_EXISTS ||
+      table_list->open_strategy == TABLE_LIST::OPEN_FOR_CREATE)
   {
     bool exists;
 
@@ -2810,7 +2811,31 @@ bool open_table(THD *thd, TABLE_LIST *table_list, Open_table_context *ot_ctx)
       DBUG_RETURN(TRUE);
 
     if (!exists)
+    {
+      if (table_list->open_strategy == TABLE_LIST::OPEN_FOR_CREATE &&
+          ! (flags & (MYSQL_OPEN_FORCE_SHARED_MDL |
+                      MYSQL_OPEN_FORCE_SHARED_HIGH_PRIO_MDL)))
+      {
+        MDL_deadlock_handler mdl_deadlock_handler(ot_ctx);
+
+        thd->push_internal_handler(&mdl_deadlock_handler);
+
+        DEBUG_SYNC(thd, "before_upgrading_lock_from_S_to_X_for_create_table");
+        bool wait_result= thd->mdl_context.upgrade_shared_lock(
+                                 table_list->mdl_request.ticket,
+                                 MDL_EXCLUSIVE,
+                                 thd->variables.lock_wait_timeout);
+
+        thd->pop_internal_handler();
+        DEBUG_SYNC(thd, "after_upgrading_lock_from_S_to_X_for_create_table");
+
+        /* Deadlock or timeout occurred while upgrading the lock. */
+        if (wait_result)
+          DBUG_RETURN(TRUE);
+      }
+
       DBUG_RETURN(FALSE);
+    }
 
     /* Table exists. Let us try to open it. */
   }
@@ -4658,8 +4683,11 @@ extern "C" uchar *schema_set_get_key(const uchar *record, size_t *length,
 
 /**
   Acquire upgradable (SNW, SNRW) metadata locks on tables used by
-  LOCK TABLES or by a DDL statement. Under LOCK TABLES, we can't take
-  new locks, so use open_tables_check_upgradable_mdl() instead.
+  LOCK TABLES or by a DDL statement.
+  Acquire lock "S" on table being created in CREATE TABLE statement.
+
+  @note  Under LOCK TABLES, we can't take new locks, so use
+         open_tables_check_upgradable_mdl() instead.
 
   @param thd               Thread context.
   @param tables_start      Start of list of tables on which upgradable locks
@@ -4688,7 +4716,8 @@ lock_table_names(THD *thd,
   for (table= tables_start; table && table != tables_end;
        table= table->next_global)
   {
-    if (table->mdl_request.type < MDL_SHARED_UPGRADABLE ||
+    if ((table->mdl_request.type < MDL_SHARED_UPGRADABLE &&
+         table->open_strategy != TABLE_LIST::OPEN_FOR_CREATE) ||
         table->open_type == OT_TEMPORARY_ONLY ||
         (table->open_type == OT_TEMPORARY_OR_BASE && is_temporary_table(table)))
     {
@@ -4925,7 +4954,8 @@ restart:
       for (table= *start; table && table != thd->lex->first_not_own_table();
            table= table->next_global)
       {
-        if (table->mdl_request.type >= MDL_SHARED_UPGRADABLE)
+        if (table->mdl_request.type >= MDL_SHARED_UPGRADABLE ||
+            table->open_strategy == TABLE_LIST::OPEN_FOR_CREATE)
           table->mdl_request.ticket= NULL;
       }
     }
