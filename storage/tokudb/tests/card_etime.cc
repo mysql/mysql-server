1b0261cc5c87fe1a68a301d88cb092b82e35258a
Rich Prohaska <prohaska@tokutek.com>
2013-05-24 10:59:07 -0400

    try to retain cardinality data after add/drop index

--- storage/tokudb/tests/card_etime.cc
+++ storage/tokudb/tests/card_etime.cc
@@ -20,27 +20,7 @@
 typedef unsigned long long ulonglong;
 #include "tokudb_status.h"
 #include "tokudb_buffer.h"
-// Provide some mimimal MySQL classes just to compile the tokudb cardinality functions
-class KEY_INFO {
-public:
-    uint flags;
-    uint64_t *rec_per_key;
-};
-#define HA_NOSAME 1
-class TABLE_SHARE {
-public:
-    uint primary_key;
-    uint keys;
-};
-class TABLE {
-public:
-    TABLE_SHARE *s;
-    KEY_INFO *key_info;
-};
-uint get_key_parts(KEY_INFO *key_info) {
-    assert(key_info);
-    return 0;
-}
+#include "fake_mysql.h"
 #if __APPLE__
 typedef unsigned long ulong;
 #endif
