a78429e43357283b2b9ac33b60bbf294c91cf566
John David Duncan <john.duncan@oracle.com>
2013-05-29 22:25:45 -0700

    Expect 22008 on write, not on read

--- test/temporaltypes/TypeTest.js
+++ test/temporaltypes/TypeTest.js
@@ -60,8 +60,16 @@ function ValueVerifier(testCase, field, value) {
 
 function ReadFunction(testCase, session) { 
   return function onPersist(err) {
-    testCase.errorIfError(err);
-    session.find(TestData, testCase.data.id, testCase.verifier.run);
+    if(testCase.insertErrorVerifier) {
+      testCase.insertErrorVerifier.run(err);  
+    }
+    else if(err) {
+      testCase.appendErrorMessage(err);
+      testCase.failOnError();
+    }
+    else {
+      session.find(TestData, testCase.data.id, testCase.verifier.run);
+    }
   }
 }
 
@@ -134,7 +142,7 @@ t6.run = function() {
   var data = new TestData(6);
   var date1969 = new Date(-10000);
   data.cNullableTimestamp = date1969;
-  this.verifier = new ErrorVerifier(this, "22008");
+  this.insertErrorVerifier = new ErrorVerifier(this, "22008");
   fail_openSession(this, InsertFunction(data));
 }
 
