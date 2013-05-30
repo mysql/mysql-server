170e1670c8df046c50273a8ebbce2bd8c7b7666c
Allen lai <zheng.lai@oracle.com>
2013-05-30 09:34:18 +0800

    Bug#15963619 GETTING "TABLE IS READ ONLY" ERROR EVEN WHILE READING WITH --INNODB-READ-ONLY
    rb://2439 Approved by Jimmy
    
    This fix is based on the Sunny's patch in the bug report.
    
    When InnoDB is in read-only mode, we prevent all lock operation in
    ha_innodbase::external_lock as MySQL want to create table.
    In fact, we only need to prevent write lock operation when MySQL is
    doing create table.

--- sql/log_event.cc
+++ sql/log_event.cc
@@ -177,6 +177,7 @@ static const char *HA_ERR(int i)
   case HA_ERR_LOGGING_IMPOSSIBLE: return "HA_ERR_LOGGING_IMPOSSIBLE";
   case HA_ERR_CORRUPT_EVENT: return "HA_ERR_CORRUPT_EVENT";
   case HA_ERR_ROWS_EVENT_APPLY : return "HA_ERR_ROWS_EVENT_APPLY";
+  case HA_ERR_INNODB_READ_ONLY: return "HA_ERR_INNODB_READ_ONLY";
   }
   return "No Error!";
 }
