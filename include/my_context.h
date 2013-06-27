d058863d8e0162aa3a4ea571416c03903c92687b
Vladislav Vaintroub <wlad@montyprogram.com>
2013-06-27 14:19:04 +0200

    MDEV-4720 : fix my_context.h for use with x32 ABI. Do not use x64  assembler implementation in  x32.

--- include/my_context.h
+++ include/my_context.h
@@ -27,7 +27,7 @@
 
 #ifdef __WIN__
 #define MY_CONTEXT_USE_WIN32_FIBERS 1
-#elif defined(__GNUC__) && __GNUC__ >= 3 && defined(__x86_64__)
+#elif defined(__GNUC__) && __GNUC__ >= 3 && defined(__x86_64__) && !defined(__ILP32__)
 #define MY_CONTEXT_USE_X86_64_GCC_ASM
 #elif defined(__GNUC__) && __GNUC__ >= 3 && defined(__i386__)
 #define MY_CONTEXT_USE_I386_GCC_ASM
