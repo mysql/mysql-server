85a9a091436f71f42fba765f55d77bda74160f7d
Sunny Bains <Sunny.Bains@Oracle.Com>
2013-06-27 20:41:57 +1000

    WL#6044 - Move srv_use_native_conditions to os0event.cc.

--- storage/innobase/srv/srv0srv.cc
+++ storage/innobase/srv/srv0srv.cc
@@ -141,21 +141,6 @@ use simulated aio we build below with threads.
 Currently we support native aio on windows and linux */
 my_bool	srv_use_native_aio = TRUE;
 
-#ifdef _WIN32
-/* Windows native condition variables. We use runtime loading / function
-pointers, because they are not available on Windows Server 2003 and
-Windows XP/2000.
-
-We use condition for events on Windows if possible, even if os_event
-resembles Windows kernel event object well API-wise. The reason is
-performance, kernel objects are heavyweights and WaitForSingleObject() is a
-performance killer causing calling thread to context switch. Besides, InnoDB
-is preallocating large number (often millions) of os_events. With kernel event
-objects it takes a big chunk out of non-paged pool, which is better suited
-for tasks like IO than for storing idle event objects. */
-bool	srv_use_native_conditions = false;
-#endif /* _WIN32 */
-
 /*------------------------- LOG FILES ------------------------ */
 char*	srv_log_group_home_dir	= NULL;
 
