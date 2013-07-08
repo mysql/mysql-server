229b11ed8216880b3c01911338da9daa1cf1d4f0
Krunal Bauskar krunal.bauskar@oracle.com <>
2013-07-08 13:11:33 +0530

    - WL#6501: To make InnoDB truncate table atomic
      - Fixed compilation issue for 32 bits ubuntu system.
        Not sure why the same issue is not present in el6-x86-32bit enviornment
        on pb2/mysql-trunk-stage.

--- storage/innobase/row/row0trunc.cc
+++ storage/innobase/row/row0trunc.cc
@@ -1265,8 +1265,8 @@ row_truncate_table_for_mysql(
 	/*-----------------------------------------------------------------*/
 
 	ib_logf(IB_LOG_LEVEL_INFO,
-		"Truncating table %s (table id = %lu) residing in space %u",
-		table->name, table->id, table->space);
+		"Truncating table %s (table id = %llu) residing in space %lu",
+		table->name, (ullint) table->id, (ulong) table->space);
 
 	/* Step-1: Perform intiial sanity check to ensure table can be
 	truncated. This would include check for tablespace discard status,
