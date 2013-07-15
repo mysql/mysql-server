0956325f3dd2e6dae75164d5062ef8b45d5e2c87
Balasubramanian Kandasamy <balasubramanian.kandasamy@oracle.com>
2013-07-15 13:41:27 +0200

    Removed random passwords feature for Bugfix#17160741 (not applicable for 5.5.X)

--- packaging/solaris/postinstall-solaris.sh
+++ packaging/solaris/postinstall-solaris.sh
@@ -81,7 +81,6 @@ if [ -n "$INSTALL" ] ; then
     cd "$mybasedir"
     scripts/mysql_install_db \
 	  --rpm \
-	  --random-passwords \
 	  --user=mysql \
 	  --basedir="$mybasedir" \
 	  --datadir=$mydatadir
