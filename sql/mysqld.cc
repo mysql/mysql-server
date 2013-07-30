80995f297bd3c0b7da0c881ebe79198cafd95047
mayank prasad <mayank.prasad@oracle.com>
2013-07-30 21:33:05 +0530

    Bug#17182801:NESTING_EVENT_TYPE/ID ARE STILL NULL FOR EVENT NESTED STATEMENTS.
    Issue :
     - NESTED_EVENT_/TYPE/LEVEL was NULL/0 for events.
    
    Reason :
     - Evetns are not executed as part of some statement i.e. there
       is no parent statement for a event. Events are scheduled based
       on timing and are executed when time comes.
    
    Fix :
     - A new instrument statement/scheduler/event is added to make
       sure all executed events are recorded by PS. This instrument
       would be the parent for events executed.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -112,6 +112,7 @@
 #include "sp_cache.h"
 #include "sql_reload.h"  // reload_acl_and_cache
 #include "sp_head.h"  // init_sp_psi_keys
+#include "event_data_objects.h"
 
 #ifdef HAVE_POLL_H
 #include <poll.h>
@@ -9919,6 +9920,8 @@ void init_server_psi_keys(void)
   mysql_statement_register(category, sql_statement_info, count);
 
   init_sp_psi_keys();
+
+  init_scheduler_psi_keys();
 
   category= "com";
   init_com_statement_info();
