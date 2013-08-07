6742fde11cf7614f5161a956ad25d9955863803e
Sunny Bains <Sunny.Bains@Oracle.Com>
2013-08-07 13:00:55 +1000

    WL#6578 - Follow up fix. Test for a valid read view before printing.

--- storage/innobase/lock/lock0lock.cc
+++ storage/innobase/lock/lock0lock.cc
@@ -5046,8 +5046,10 @@ lock_trx_print_wait_and_mvcc_state(
 
 	trx_print_latched(file, trx, 600);
 
-	if (trx->read_view != NULL) {
-		trx->read_view->print_limits(file);
+	ReadView*	read_view = trx_get_read_view(trx->read_view);
+
+	if (read_view != NULL) {
+		read_view->print_limits(file);
 	}
 
 	if (trx->lock.que_state == TRX_QUE_LOCK_WAIT) {
