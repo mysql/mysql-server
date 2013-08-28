1261600865b7553242f63d088d57ec843adaa01f
Jon Olav Hauglid <jon.hauglid@oracle.com>
2013-08-28 13:53:54 +0200

    Bug#17332056: REMOVE WINXP SPECIFIC IMPLEMENTATION OF CONDITION VARIABLES
    This patch removes the Win XP & Win Server 2003 specific implementation
    of condition variables. Starting with Windows Vista & Windows Server 2008
    we can use the native implementation of condition variables.
    
    Win XP & Win Server 2003 are no longer supported as of MySQL 5.6.

--- include/my_pthread.h
+++ include/my_pthread.h
@@ -50,30 +50,9 @@ typedef struct st_pthread_link {
 
 /**
   Implementation of Windows condition variables.
-  We use native conditions on Vista and later, and fallback to own 
-  implementation on earlier OS version.
+  We use native conditions as they are available on Vista and later.
 */
-typedef union
-{
-  /* Native condition (used on Vista and later) */
-  CONDITION_VARIABLE native_cond;
-
-  /* Own implementation (used on XP) */
-  struct
-  { 
-    uint32 waiting;
-    CRITICAL_SECTION lock_waiting;
-    enum 
-    {
-      SIGNAL= 0,
-      BROADCAST= 1,
-      MAX_EVENTS= 2
-    } EVENTS;
-    HANDLE events[MAX_EVENTS];
-    HANDLE broadcast_block_event;
-  };
-} pthread_cond_t;
-
+typedef CONDITION_VARIABLE pthread_cond_t;
 
 typedef int pthread_mutexattr_t;
 #define pthread_self() GetCurrentThreadId()
