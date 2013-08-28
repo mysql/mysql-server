341c7880ad2c11fc073c2914eada0ff3c480c85f
Bjorn Munch <bjorn.munch@oracle.com>
2013-08-28 12:21:47 +0200

    Bug #17341731, CS 6388 from trunk

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -16258,30 +16258,6 @@ ha_innobase::multi_range_read_info(
 }
 
 /**
-@brief Determine whether an error is fatal or not. 
-    
-A deadlock error is not a fatal error.
-    
-@param error	error code received from the handler interface (HA_ERR_...)
-@param flags	indicate whether duplicate key errors should be ignorable
-    
-@return   whether the error is fatal or not
-@retval true  the error is fatal
-@retval false the error is not fatal */
-
-bool
-ha_innobase::is_fatal_error(int error, uint flags)
-{
-	if (!handler::is_fatal_error(error, flags)
-	    || error == HA_ERR_LOCK_DEADLOCK) {
-
-		return(false);
-	}
-
-	return(true);
-}
-
-/**
  * Index Condition Pushdown interface implementation
  */
 
