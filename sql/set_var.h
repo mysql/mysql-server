57bc36d4a69048e33b5bd82659cff18e33defd9a
Nisha Gopalakrishnan <nisha.gopalakrishnan@oracle.com>
2013-09-05 09:14:58 +0530

    BUG#16032946 - PLEASE GIVE A MESSAGE FOR "THREAD_CONCURRENCY DOESN'T                DO WHAT YOU EXPECT"
    Fix info:
    --------
    
    Backport of the deprecation bug fix (WL#5265) for global variable
    'THREAD_CONCURRENCY' from mysql-5.6 to mysql-5.5
    
    Note: With this backport, certain additional deprecation warnings
          would be reported under error conditions while setting the
          global/session variables.

--- sql/set_var.h
+++ sql/set_var.h
@@ -143,6 +143,7 @@ public:
     return (option.id != -1) && (m_parse_flag & parse_flags) &&
            insert_dynamic(array, (uchar*)&option);
   }
+  void do_deprecated_warning(THD *thd);
 
 private:
   virtual bool do_check(THD *thd, set_var *var) = 0;
@@ -156,7 +157,7 @@ private:
   virtual void global_save_default(THD *thd, set_var *var) = 0;
   virtual bool session_update(THD *thd, set_var *var) = 0;
   virtual bool global_update(THD *thd, set_var *var) = 0;
-  void do_deprecated_warning(THD *thd);
+
 protected:
   /**
     A pointer to a value of the variable for SHOW.
