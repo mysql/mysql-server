451b3959ecf9d935da0035daf6e3b05fb11da46c
Rich Prohaska <prohaska@tokutek.com>
2013-09-26 09:53:50 -0400

    #104 write frm for existing tables before the txn is committed

--- storage/tokudb/ha_tokudb_alter_55.cc
+++ storage/tokudb/ha_tokudb_alter_55.cc
@@ -98,29 +98,6 @@ bool ha_tokudb::try_hot_alter_table() {
     DBUG_RETURN(!disable_hot_alter);
 }
 
-void ha_tokudb::prepare_for_alter() {
-    TOKUDB_DBUG_ENTER("prepare_for_alter");
-
-    // this is here because mysql commits the transaction before prepare_for_alter is called.
-    // we need a transaction to add indexes, drop indexes, and write the new frm data, so we
-    // create one.  this transaction will be retired by mysql alter table when it commits
-    //
-    // if we remove the commit before prepare_for_alter, then this is not needed.
-    transaction = NULL;
-    THD *thd = ha_thd();
-    tokudb_trx_data *trx = (tokudb_trx_data *) thd_data_get(thd, tokudb_hton->slot);
-    assert(trx);
-    // for partitioned tables, a transaction may already exist, 
-    // as we call prepare_for_alter on all partitions
-    if (!trx->sub_sp_level) {
-        int error = create_txn(thd, trx);
-        assert(error == 0);
-        assert(thd->in_sub_stmt == 0);
-    }
-    transaction = trx->sub_sp_level;
-    DBUG_VOID_RETURN;
-}
-
 int ha_tokudb::new_alter_table_frm_data(const uchar *frm_data, size_t frm_len) {
     return write_frm_data(frm_data, frm_len);
 }
