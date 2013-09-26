c8693bea40c8d0d3f678e263c474ee82b193f62e
Vladislav Vaintroub <wlad@montyprogram.com>
2013-09-26 23:48:38 +0200

    MDEV-5076 : Build on FreeBSD - when looking for execinfo library, and execinfo.h header, allow user-defined EXECINFO_ROOT prefix, in case library and header are not placed under /usr/local . This change was requested by FreeBSD maintainer.

--- cmake/os/FreeBSD.cmake
+++ cmake/os/FreeBSD.cmake
@@ -24,9 +24,10 @@
 # ADD_DEFINITIONS(-DHAVE_BROKEN_REALPATH)
 
 # Find libexecinfo (library that contains backtrace_symbols etc)
-INCLUDE_DIRECTORIES(/usr/local/include)
-SET(CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} /usr/local/include )
-SET(ENV{LIB} "$ENV{LIB}:/usr/local/lib")
+SET(EXECINFO_ROOT /usr/local CACHE INTERNAL "Where to find execinfo library and header")
+INCLUDE_DIRECTORIES(${EXECINFO_ROOT}/include)
+SET(CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} ${EXECINFO_ROOT}/include)
+SET(ENV{LIB} "$ENV{LIB}:${EXECINFO_ROOT}/lib")
 FIND_LIBRARY(EXECINFO NAMES execinfo)
 IF(EXECINFO)
  SET(LIBEXECINFO ${EXECINFO})
