5cd103e979a74d0c3aae31cddd6cad643f56b5e7
Evgeny Potemkin <evgeny.potemkin@oracle.com>
2013-10-07 13:45:43 +0400

    Bug#17434385: SHOW PROCEDURE CODE ASSERTION FAILED: AT SQL_LEX.CC:2831
    After WL#6369 all explainable statements are supposed to be not cleaned till
    the end of the statement in order to provide plan for EXPLAIN FOR CONNECTION.
    To ensure that an assert at sql_lex.cc:2831 was added. SHOW PROCEDURE CODE
    can't be explained, so it's ok to skip this assert.
    Also, "<already_cleaned_up>" isn't printed on a clena unit since it's essentially
    useless: if unit is clean all select list items are reverted to initial state and
    don't point to a tmp table anymore, so it's safe to print them.

--- sql/sql_lex.cc
+++ sql/sql_lex.cc
@@ -2822,16 +2822,13 @@ void st_select_lex::print(THD *thd, String *str, enum_query_type query_type)
   else
     str->append(STRING_WITH_LEN("select "));
 
-  if (master_unit()->cleaned == SELECT_LEX_UNIT::UC_CLEAN)
-  {
-    /*
-     In order to provide inf for EXPLAIN FOR CONNECTION units aren't
-     completely cleaned till the end of the query.
-    */
-    DBUG_ASSERT(false); /* purecov: inspected */
-    str->append(STRING_WITH_LEN("<already_cleaned_up>"));
-    return;
-  }
+  /*
+   In order to provide info for EXPLAIN FOR CONNECTION units shouldn't
+   be completely cleaned till the end of the query. This is valid only for
+   explainable commands.
+  */
+  DBUG_ASSERT(!(master_unit()->cleaned == SELECT_LEX_UNIT::UC_CLEAN &&
+                is_explainable_query(thd->lex->sql_command)));
 
   /* First add options */
   if (options & SELECT_STRAIGHT_JOIN)
