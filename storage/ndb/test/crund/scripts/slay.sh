d9ce5c0e0b4d8956d9cd5af110a8a10ba68af9f6
Murthy Narkedimilli <murthy.narkedimilli@oracle.com>
2013-10-09 16:47:06 +0530

    Bug# 16919882: Changing the copyright header format

--- storage/ndb/test/crund/scripts/slay.sh
+++ storage/ndb/test/crund/scripts/slay.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2010, 2012, Oracle and/or its affiliates. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
