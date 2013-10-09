d0dcce0a717666d3911af6101439f172e7155f5f
Murthy Narkedimilli <murthy.narkedimilli@oracle.com>
2013-10-09 16:15:58 +0530

    Bug# 16919882: Updating the copyright header format for 5.6 files.

--- storage/innobase/include/srv0conc.h
+++ storage/innobase/include/srv0conc.h
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
-Copyright (c) 2011, Oracle and/or its affiliates. All Rights Reserved.
+Copyright (c) 2011, 2012, Oracle and/or its affiliates. All Rights Reserved.
 
 Portions of this file contain modifications contributed and copyrighted by
 Google, Inc. Those modifications are gratefully acknowledged and are described
