81623bdc357e3a6c9bd5582d2e2d038a0bb2d093
Igor Babaev <igor@askmonty.org>
2013-10-14 10:29:24 -0700

    Fixed bug mdev-5135. The patch for bug mdev-5105 incorrectly counted conditions in nested joins.

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -4703,6 +4703,33 @@ static void add_key_fields_for_nj(JOIN *join, TABLE_LIST *nested_join_table,
 }
 
 
+void count_cond_for_nj(SELECT_LEX *sel, TABLE_LIST *nested_join_table)
+{
+  List_iterator<TABLE_LIST> li(nested_join_table->nested_join->join_list);
+  List_iterator<TABLE_LIST> li2(nested_join_table->nested_join->join_list);
+  bool have_another = FALSE;
+  TABLE_LIST *table;
+
+  while ((table= li++) || (have_another && (li=li2, have_another=FALSE,
+                                            (table= li++))))
+  if (table->nested_join)
+  {
+    if (!table->on_expr)
+    {
+      /* It's a semi-join nest. Walk into it as if it wasn't a nest */
+      have_another= TRUE;
+      li2= li;
+      li= List_iterator<TABLE_LIST>(table->nested_join->join_list); 
+    }
+    else
+      count_cond_for_nj(sel, table); 
+  }
+  if (nested_join_table->on_expr)
+    nested_join_table->on_expr->walk(&Item::count_sargable_conds, 
+                                     0, (uchar*) sel);
+    
+}
+
 /**
   Update keyuse array with all possible keys we can use to fetch rows.
   
@@ -4751,12 +4778,7 @@ update_ref_and_keys(THD *thd, DYNAMIC_ARRAY *keyuse,JOIN_TAB *join_tab,
     while ((table= li++))
     {
       if (table->nested_join)
-      {
-        if (table->on_expr)
-          table->on_expr->walk(&Item::count_sargable_conds, 0, (uchar*) sel);
-        if (table->sj_on_expr)
-          table->sj_on_expr->walk(&Item::count_sargable_conds, 0, (uchar*) sel);
-      }
+        count_cond_for_nj(sel, table);
     }
   }
   
