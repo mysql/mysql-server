ec7d2d171d3bf1ff2ff19acaa8511befff66988a
Lakshmi Narayanan Sreethar <lakshmi.narayanan.sreethar@oracle.com>
2013-10-17 12:51:08 +0530

    BUG#11746263 : REMOVING OR REPLACING THE 'MASTER'-WORD FROM SHOW OUTPUT
    Modified the output of 'ndb_mgm -e show' command to print '*' instead of the word 'Master'.
    Also updated a test case to reflect the changes.

--- storage/ndb/src/mgmclient/CommandInterpreter.cpp
+++ storage/ndb/src/mgmclient/CommandInterpreter.cpp
@@ -1705,7 +1705,7 @@ print_nodes(ndb_mgm_cluster_state *state, ndb_mgm_configuration_iterator *it,
           }
           if (node_state->node_group >= 0 || node_state->node_group == (int)RNIL)
 	    if (master_id && node_state->dynamic_id == master_id)
-	      ndbout << ", Master";
+	      ndbout << ", *";
         }
 	ndbout << ")" << endl;
       } else {
