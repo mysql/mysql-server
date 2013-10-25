9b8ae05ca802f0ab5d8f90756dc113120a780ac3
Bharathy Satish <bharathy.x.satish@oracle.com>
2013-10-25 16:54:07 +0530

    Bug#17653288 MYSQL_RESET_CONNECTION DOES NOT RESET LAST_INSERT_ID
    Problem: mysql_reset_connection as part of this resetting session state
    it should reset last_insert_id value which it does not.
    Fix: Fix is to reset all the variables associated with last_insert_id
     in THD class.

--- sql/sql_parse.cc
+++ sql/sql_parse.cc
@@ -1199,6 +1199,7 @@ bool dispatch_command(enum enum_server_command command, THD *thd,
   {
     thd->status_var.com_other++;
     thd->cleanup_connection();
+    thd->reset_first_successful_insert_id();
     my_ok(thd);
     break;
   }
