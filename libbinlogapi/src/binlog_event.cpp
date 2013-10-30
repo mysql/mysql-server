90bbafa41f73fb26383817e2b2ce1a17092a9333
Shubhangi Garg <shubhangi.garg@oracle.com>
2013-10-30 13:52:03 +0530

    post push fix

--- libbinlogapi/src/binlog_event.cpp
+++ libbinlogapi/src/binlog_event.cpp
@@ -20,8 +20,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 
 #include "binlog_event.h"
 #include "transitional_methods.h"
-#include <cstdlib>
-#include <cstring>
+#include <stdlib.h>
+#include <string.h>
 #include <iostream>
 typedef unsigned long ulong;
 
