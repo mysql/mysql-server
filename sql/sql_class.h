4a2989af405ee71ece274f3860ea6ea1d3a795a4
Sivert Sorumgard <sivert.sorumgaard@oracle.com>
2013-11-01 10:41:05 +0100

    Bug#16041903 CONTINUE HANDLER NOT INVOKED IN A STORED FUNCTION AFTER A LOCK WAIT TIMEOUT
    Overview
    --------
    When the SQL layer is executing a sub statement (stored
    function or trigger), some errors cannot be safely recovered
    until we leave sub statement mode. This is the case for
    e.g. ER_LOCK_WAIT_TIMEOUT. When this error is caught, the
    handler does a callback to "mark_transaction_for_rollback()"
    in the SQL layer, which makes the transaction be rolled back
    implicitly. In "mark_transaction_for_rollback()", the flag
    "THD::is_fatal_sub_stmt_error" is set. This flag is used to
    effectievly disable the execution of a condition handler in
    a stored function while handling the error. The flag is
    reset when leaving sub statement mode, in
    "restore_sub_statement_state()", if leaving the last level
    of sub statements (i.e., nested sub statements are handled
    by keeping the value of the flag when going to the previous
    "stack frame"). 
    
    The problem here is that the flag is set in
    "mark_transaction_for_rollback()" even if the SQL layer is
    not in sub statement mode. If this happens, and the
    transaction afterwards calls a stored function with a
    condition handler, the handler will be disabled since the
    flag is already set when calling the function.
    
    
    Suggested fix
    -------------
    The suggested fix is to set "is_fatal_sub_stmt_error"
    in "mark_transaction_for_rollback()" only in the case where
    the SQL layer is currently executing a sub statement
    ("in_sub_stmt==true"). If not in a sub statement,
    setting the "is_fatal_sub_stmt_error" flag really has
    no meaning. Additionally, when the flag is set when the SQL
    layer is not actually in a sub statement, the flag is never
    cleared since this happens only in
    "THD::restore_sub_statement_state()" when leaving sub
    statement mode.

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -637,9 +637,6 @@ typedef struct system_status_var
 
 #define last_system_status_var questions
 
-void mark_transaction_to_rollback(THD *thd, bool all);
-
-
 /**
   Get collation by name, send error to client on failure.
   @param name     Collation name
@@ -3913,6 +3910,8 @@ public:
   LEX_STRING get_invoker_host() { return invoker_host; }
   bool has_invoker() { return invoker_user.length > 0; }
 
+  void mark_transaction_to_rollback(bool all);
+
 #ifndef DBUG_OFF
 private:
   int gis_debug; // Storage for "SELECT ST_GIS_DEBUG(param);"
@@ -5252,7 +5251,6 @@ void add_to_status(STATUS_VAR *to_var, STATUS_VAR *from_var);
 
 void add_diff_to_status(STATUS_VAR *to_var, STATUS_VAR *from_var,
                         STATUS_VAR *dec_var);
-void mark_transaction_to_rollback(THD *thd, bool all);
 
 /* Inline functions */
 
