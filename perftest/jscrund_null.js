ca0a17724cf5f979a2af028e30db5bf8ab435166
John David Duncan <john.duncan@oracle.com>
2013-11-02 15:42:38 -0700

    In jscrund null adapter, add option to measure performance of udebug.log() messages

--- perftest/jscrund_null.js
+++ perftest/jscrund_null.js
@@ -28,13 +28,17 @@ function implementation() {
 
 implementation.prototype = {
   inBatchMode : false,
-  batch       :  null
+  batch       : null,
+  properties  : null,
+  debug_msg   : "debug message "
 };
 
 implementation.prototype.getDefaultProperties = function() {
   return {
-    mysql_user : "root",   /* For CREATE TABLE */
-    database   : "test"
+    mysql_user           : "root",  // For CREATE TABLE
+    database             : "test",
+    debug_messages       : 0,       // log_debug messages per operation
+    debug_message_length : 20,      // length of debug messages
   };
 };
 
@@ -43,10 +47,19 @@ implementation.prototype.close = function(callback) {
 };
 
 implementation.prototype.initialize = function(options, callback) {
+  var n;
+  this.properties = options.properties;
+  while(this.debug_msg.length < this.properties.debug_message_length) {
+    this.debug_msg += "x";
+  }
   callback(null);
 };
 
 implementation.prototype.execOneOperation = function(callback, value) {
+  for(var n = 0 ; n < this.properties.debug_messages ; n++) {
+    udebug.log(this.debug_msg);
+  }
+
   if(this.inBatchMode) {
     this.batch.push({ 'cb': callback, 'val': value });
   }
@@ -60,7 +73,6 @@ implementation.prototype.persist = function(parameters, callback) {
 };
 
 implementation.prototype.find = function(parameters, callback) {
-  udebug.log_detail('find key:', parameters.key);
   var object = {
     id :      parameters.key.id,
     cint:     parameters.key.id,
