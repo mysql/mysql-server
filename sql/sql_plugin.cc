e10d42d93b373080ec9acb28f9ed29857000c0ae
Sergei Golubchik <sergii@pisem.net>
2013-11-11 09:31:09 +0100

    MDEV-5022 Strange message or wrong errno on mismatching versions of plugin and server
    1. use an appropriate errno code
    2. put a comma between the errno and the error message text

--- sql/sql_plugin.cc
+++ sql/sql_plugin.cc
@@ -644,7 +644,7 @@ static my_bool read_maria_plugin_info(struct st_plugin_dl *plugin_dl,
       (plugin_dl->mariaversion >> 8) > (MARIA_PLUGIN_INTERFACE_VERSION >> 8))
   {
     free_plugin_mem(plugin_dl);
-    report_error(report, ER_CANT_OPEN_LIBRARY, dlpath, 0,
+    report_error(report, ER_CANT_OPEN_LIBRARY, dlpath, ENOEXEC,
                  "plugin interface version mismatch");
     DBUG_RETURN(TRUE);
   }
@@ -789,7 +789,7 @@ static st_plugin_dl *plugin_dl_add(const LEX_STRING *dl, int report)
         my_snprintf(buf, sizeof(buf),
                     "service '%s' interface version mismatch",
                     list_of_services[i].name);
-        report_error(report, ER_CANT_OPEN_LIBRARY, dlpath, 0, buf);
+        report_error(report, ER_CANT_OPEN_LIBRARY, dlpath, ENOEXEC, buf);
         DBUG_RETURN(0);
       }
       *(void**)sym= list_of_services[i].service;
@@ -1083,7 +1083,7 @@ static bool plugin_add(MEM_ROOT *tmp_root,
                  plugin_type_names[plugin->type].str,
                  " plugin ", tmp.name.str,
                  " not supported by this version of the server", NullS);
-        report_error(report, ER_CANT_OPEN_LIBRARY, dl->str, 0, buf);
+        report_error(report, ER_CANT_OPEN_LIBRARY, dl->str, ENOEXEC, buf);
         goto err;
       }
       if (plugin_maturity_map[plugin->maturity] < plugin_maturity)
@@ -1095,7 +1095,7 @@ static bool plugin_add(MEM_ROOT *tmp_root,
                  " is prohibited by --plugin-maturity=",
                  plugin_maturity_names[plugin_maturity],
                  NullS);
-        report_error(report, ER_CANT_OPEN_LIBRARY, dl->str, 0, buf);
+        report_error(report, ER_CANT_OPEN_LIBRARY, dl->str, EPERM, buf);
         goto err;
       }
       tmp.plugin= plugin;
