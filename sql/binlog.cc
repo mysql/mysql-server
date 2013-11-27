d8faf1f1a346463768bc771275af80b1c8d563fa
Luis Soares <luis.soares@oracle.com>
2013-11-27 09:19:19 +0000

    BUG#17509011: DDL QUERIES INCREASES RPL_SEMI_SYNC_MASTER_YES_TX BY A VALUE OF 2
    The problem is that the after_commit hook is being called twice during
    the DDL execution: first on the COMMIT stage of the binlog group
    commit procedure (MYSQL_BIN_LOG::process_after_commit_stage_queue) and
    a second time during the transaction implicit commit
    (trans_commit_implicit). And this happens even though the first time
    the hook is called, the flag transaction->run_hooks is set to false.
    
    The smallest and quickest fix for this issue is to deploy checks when
    we call the after_commit hook, to verify that we have not called the
    hook already during this "transaction" lifecycle.

--- sql/binlog.cc
+++ sql/binlog.cc
@@ -6493,6 +6493,12 @@ MYSQL_BIN_LOG::process_after_commit_stage_queue(THD *thd, THD *first)
     if (head->transaction.flags.run_hooks &&
         head->commit_error == THD::CE_NONE)
     {
+
+      /*
+        TODO: This hook here should probably move outside/below this
+              if and be the only after_commit invocation left in the
+              code.
+      */
       excursion.try_to_attach_to(head);
       bool all= head->transaction.flags.real_commit;
       (void) RUN_HOOK(transaction, after_commit, (head, all));
@@ -6646,8 +6652,12 @@ MYSQL_BIN_LOG::finish_commit(THD *thd)
       dec_prep_xids(thd);
     /*
       If commit succeeded, we call the after_commit hook
+
+      TODO: This hook here should probably move outside/below this
+            if and be the only after_commit invocation left in the
+            code.
     */
-    if (thd->commit_error == THD::CE_NONE)
+    if ((thd->commit_error == THD::CE_NONE) && thd->transaction.flags.run_hooks)
     {
       (void) RUN_HOOK(transaction, after_commit, (thd, all));
       thd->transaction.flags.run_hooks= false;
