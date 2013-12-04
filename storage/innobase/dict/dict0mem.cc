5724f608ffc89b2f5a11fffe40b533979354bd95
Vasil Dimov <vasil.dimov@oracle.com>
2013-12-04 08:56:07 +0200

    Followup to vasil.dimov@oracle.com-20131203163459-tlkyqdq93jysk9z0:
    fix compilation failure when perfschema is enabled.
    
    The key is called dict_table_stats_latch_key in mysql-5.6, but
    dict_table_stats_key in mysql-trunk.

--- storage/innobase/dict/dict0mem.cc
+++ storage/innobase/dict/dict0mem.cc
@@ -99,7 +99,7 @@ dict_mem_table_create(
 	ut_d(table->magic_n = DICT_TABLE_MAGIC_N);
 
 	table->stats_latch = new rw_lock_t;
-	rw_lock_create(dict_table_stats_latch_key, table->stats_latch,
+	rw_lock_create(dict_table_stats_key, table->stats_latch,
 		       SYNC_INDEX_TREE);
 
 #ifndef UNIV_HOTBACKUP
