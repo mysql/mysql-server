a053f2aac2c6508e8d53a121c35ab03ba40bc19d
Vasil Dimov <vasil.dimov@oracle.com>
2013-12-06 13:49:49 +0200

    Backport from mysql-trunk to mysql-5.6:
      ** revision-id: vasil.dimov@oracle.com-20131203155950-qm0okr731tms81sy
      ** committer: Vasil Dimov <vasil.dimov@oracle.com>
      ** branch nick: mysql-trunk
      ** timestamp: Tue 2013-12-03 17:59:50 +0200
      ** message:
      **   Fix Bug#17193801 DICT_TABLE_SCHEMA_CHECK CALLS DTYPE_SQL_NAME
      **   NEEDLESSLY - WASTING A LOT OF CPU
      **
      **   Avoid the calls to dtype_sql_name() if the results are not going to
      **   be used (they are used only in an event of an error).
      **
      **   Reviewed by:	Kevin (rb:3625)

--- storage/innobase/dict/dict0dict.cc
+++ storage/innobase/dict/dict0dict.cc
@@ -5930,6 +5930,17 @@ dict_table_check_for_dup_indexes(
 }
 #endif /* UNIV_DEBUG */
 
+/** Auxiliary macro used inside dict_table_schema_check(). */
+#define CREATE_TYPES_NAMES() \
+	dtype_sql_name((unsigned) req_schema->columns[i].mtype, \
+		       (unsigned) req_schema->columns[i].prtype_mask, \
+		       (unsigned) req_schema->columns[i].len, \
+		       req_type, sizeof(req_type)); \
+	dtype_sql_name(table->cols[j].mtype, \
+		       table->cols[j].prtype, \
+		       table->cols[j].len, \
+		       actual_type, sizeof(actual_type))
+
 /*********************************************************************//**
 Checks whether a table exists and whether it has the given structure.
 The table must have the same number of columns with the same names and
@@ -5949,6 +5960,8 @@ dict_table_schema_check(
 	size_t			errstr_sz)	/*!< in: errstr size */
 {
 	char		buf[MAX_FULL_NAME_LEN];
+	char		req_type[64];
+	char		actual_type[64];
 	dict_table_t*	table;
 	ulint		i;
 
@@ -6000,9 +6013,6 @@ dict_table_schema_check(
 	for (i = 0; i < req_schema->n_cols; i++) {
 		ulint	j;
 
-		char	req_type[64];
-		char	actual_type[64];
-
 		/* check if i'th column is the same in both arrays */
 		if (innobase_strcasecmp(req_schema->columns[i].name,
 			       dict_table_get_col_name(table, i)) == 0) {
@@ -6044,18 +6054,10 @@ dict_table_schema_check(
 		/* we found a column with the same name on j'th position,
 		compare column types and flags */
 
-		dtype_sql_name(req_schema->columns[i].mtype,
-			       req_schema->columns[i].prtype_mask,
-			       req_schema->columns[i].len,
-			       req_type, sizeof(req_type));
-
-		dtype_sql_name(table->cols[j].mtype,
-			       table->cols[j].prtype,
-			       table->cols[j].len,
-			       actual_type, sizeof(actual_type));
-
 		/* check length for exact match */
 		if (req_schema->columns[i].len != table->cols[j].len) {
+
+			CREATE_TYPES_NAMES();
 
 			ut_snprintf(errstr, errstr_sz,
 				    "Column %s in table %s is %s "
@@ -6070,6 +6072,8 @@ dict_table_schema_check(
 
 		/* check mtype for exact match */
 		if (req_schema->columns[i].mtype != table->cols[j].mtype) {
+
+			CREATE_TYPES_NAMES();
 
 			ut_snprintf(errstr, errstr_sz,
 				    "Column %s in table %s is %s "
@@ -6087,6 +6091,8 @@ dict_table_schema_check(
 		    && (table->cols[j].prtype
 			& req_schema->columns[i].prtype_mask)
 		       != req_schema->columns[i].prtype_mask) {
+
+			CREATE_TYPES_NAMES();
 
 			ut_snprintf(errstr, errstr_sz,
 				    "Column %s in table %s is %s "
