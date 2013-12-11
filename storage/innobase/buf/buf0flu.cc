fea7607a10ac4a2dd7724c65ba13c26b3400346b
Anil Toshniwal <anil.toshniwal@oracle.com>
2013-12-11 23:47:39 +0530

    Bug#17935793 UT_GET_NAMEL() IS NOT THREAD SAFE Problem: Static variable is used in ut_get_namel(), which is not thread safe.          Also replace the call of ut_print_timestamp() by ib_logf(), wherever in          InnoDB if possible.
    Fixed: Remove static identifier from ut_get_namel().
           Also fixed ut_print_timestamp().
    
    Approved by Kevin (rb#4151).

--- storage/innobase/buf/buf0flu.cc
+++ storage/innobase/buf/buf0flu.cc
@@ -743,9 +743,8 @@ buf_flush_init_for_writing(
 			return;
 		}
 
-		ut_print_timestamp(stderr);
-		fputs("  InnoDB: ERROR: The compressed page to be written"
-		      " seems corrupt:", stderr);
+		ib_logf(IB_LOG_LEVEL_ERROR,
+			"The compressed page to be written seems corrupt:");
 		ut_print_buf(stderr, page, zip_size);
 		fputs("\nInnoDB: Possibly older version of the page:", stderr);
 		ut_print_buf(stderr, page_zip->data, zip_size);
