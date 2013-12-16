5c147cc943a1d0c9b1bc87312df1443c597cb182
Rich Prohaska <prohaska@tokutek.com>
2013-12-16 07:47:36 -0500

    Tokutek/mysql56#42 turn on HA_PRIMARY_KEY_REQUIRED_FOR_POSITION to fix failing partition tests on mysql 5.6

--- storage/tokudb/ha_tokudb.cc
+++ storage/tokudb/ha_tokudb.cc
@@ -1186,8 +1186,8 @@ ha_tokudb::ha_tokudb(handlerton * hton, TABLE_SHARE * table_arg):handler(hton, t
     // flags defined in sql\handler.h
 {
     share = NULL;
-    int_table_flags = HA_REC_NOT_IN_SEQ  | HA_NULL_IN_KEY | HA_CAN_INDEX_BLOBS | HA_PRIMARY_KEY_IN_READ_INDEX | 
-                    HA_FILE_BASED | HA_AUTO_PART_KEY | HA_TABLE_SCAN_ON_INDEX | HA_CAN_WRITE_DURING_OPTIMIZE;
+    int_table_flags = HA_REC_NOT_IN_SEQ  | HA_NULL_IN_KEY | HA_CAN_INDEX_BLOBS | HA_PRIMARY_KEY_IN_READ_INDEX | HA_PRIMARY_KEY_REQUIRED_FOR_POSITION | 
+        HA_FILE_BASED | HA_AUTO_PART_KEY | HA_TABLE_SCAN_ON_INDEX | HA_CAN_WRITE_DURING_OPTIMIZE;
     alloc_ptr = NULL;
     rec_buff = NULL;
     rec_update_buff = NULL;
