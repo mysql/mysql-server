93de39ccd7cd6c9e519762472efb4eddafc7e884
Annamalai Gurusami <annamalai.gurusami@oracle.com>
2013-12-19 13:20:50 +0530

    Bug #17335427 INNODB CAN NOT USE THE DOUBLEWRITE BUFFER PROPERLY
    Problem:
    
    If the first page (page 0) of the single table tablespace is corrupted in the
    data file then our recovery doesn't progress even if there is a clean copy of
    the same available in the double write buffer.  
    
    Analysis:
    
    During recovery, our first step is to process the double write buffer.  We look
    at the pages in the double write buffer and determine its (space_id, page_no)
    details.  Each of the page in the double write buffer corresponds to a page in
    the .ibd data file.  Using the space_id information we need to map the page in
    the double write buffer to the corresponding ibd file.  This is done by reading
    the space_id information from the first page of the single table tablespace.
    If the first page of the single table tablespace is corrupted, then we are
    unable to determine the data file to which a particular page in the double
    write buffer belongs to.  So we need to explore and see if we can determine the
    space_id in other means.
    
    Solution:
    
    Assume a particular page size.  Read N number of pages from the ibd file.
    Ignore the corrupted pages and determine the (space_id, page_size and zip_size)
    information.  Repeat this for all supported page sizes.  Using this approach
    determine the correct (space_id, page_size and zip_size) of the ibd file.
    
    rb#4025 approved by Yasufumi.

--- storage/innobase/fil/fil0fil.cc
+++ storage/innobase/fil/fil0fil.cc
@@ -4015,6 +4015,165 @@ fil_make_ibbackup_old_name(
 }
 #endif /* UNIV_HOTBACKUP */
 
+
+/*******************************************************************//**
+Determine the space id of the given file descriptor by reading a few
+pages from the beginning of the .ibd file.
+@return true if space id was successfully identified, or false. */
+static
+bool
+fil_user_tablespace_find_space_id(
+/*==============================*/
+	fsp_open_info*	fsp)	/* in/out: contains file descriptor, which is
+				used as input.  contains space_id, which is
+				the output */
+{
+	bool		st;
+	os_offset_t	file_size;
+
+	file_size = os_file_get_size(fsp->file);
+
+	if (file_size == (os_offset_t) -1) {
+		ib_logf(IB_LOG_LEVEL_ERROR, "Could not get file size: %s",
+			fsp->filepath);
+		return(false);
+	}
+
+	/* Assuming a page size, read the space_id from each page and store it
+	in a map.  Find out which space_id is agreed on by majority of the
+	pages.  Choose that space_id. */
+	for (ulint page_size = UNIV_ZIP_SIZE_MIN;
+	     page_size <= UNIV_PAGE_SIZE_MAX; page_size <<= 1) {
+
+		/* map[space_id] = count of pages */
+		std::map<ulint, ulint> verify;
+
+		ulint page_count = 64;
+		ulint valid_pages = 0;
+
+		/* Adjust the number of pages to analyze based on file size */
+		while ((page_count * page_size) > file_size) {
+			--page_count;
+		}
+
+		ib_logf(IB_LOG_LEVEL_INFO, "Page size:%lu Pages to analyze:"
+			"%lu", page_size, page_count);
+
+		byte* buf = static_cast<byte*>(ut_malloc(2*page_size));
+		byte* page = static_cast<byte*>(ut_align(buf, page_size));
+
+		for (ulint j = 0; j < page_count; ++j) {
+
+			st = os_file_read(fsp->file, page, (j* page_size), page_size);
+
+			if (!st) {
+				ib_logf(IB_LOG_LEVEL_INFO,
+					"READ FAIL: page_no:%lu", j);
+				continue;
+			}
+
+			if (!buf_page_is_corrupted(false, page, 0)
+			    || !buf_page_is_corrupted(false, page,
+						      page_size)) {
+
+				ulint space_id = mach_read_from_4(page
+					+ FIL_PAGE_SPACE_ID);
+
+				if (space_id > 0) {
+					ib_logf(IB_LOG_LEVEL_INFO,
+						"VALID: space:%lu "
+						"page_no:%lu page_size:%lu",
+						space_id, j, page_size);
+					verify[space_id]++;
+					++valid_pages;
+				}
+			}
+		}
+
+		ut_free(buf);
+
+		ib_logf(IB_LOG_LEVEL_INFO, "Page size: %lu, Possible space_id "
+			"count:%lu", page_size, verify.size());
+
+		const ulint pages_corrupted = 3;
+		for (ulint missed = 0; missed <= pages_corrupted; ++missed) {
+
+			for (std::map<ulint, ulint>::iterator
+			     m = verify.begin(); m != verify.end(); ++m ) {
+
+				ib_logf(IB_LOG_LEVEL_INFO, "space_id:%lu, "
+					"Number of pages matched: %lu/%lu "
+					"(%lu)", m->first, m->second,
+					valid_pages, page_size);
+
+				if (m->second == (valid_pages - missed)) {
+
+					ib_logf(IB_LOG_LEVEL_INFO,
+						"Chosen space:%lu\n", m->first);
+
+					fsp->id = m->first;
+					return(true);
+				}
+			}
+
+		}
+	}
+
+	return(false);
+}
+
+/*******************************************************************//**
+Finds the page 0 of the given space id from the double write buffer, and
+copies it to the corresponding .ibd file.
+@return true if copy was successful, or false. */
+static
+bool
+fil_user_tablespace_restore_page0(
+/*==============================*/
+	fsp_open_info*	fsp)	/* in: contains space id and .ibd file
+				information */
+{
+	bool	err;
+	ulint	flags;
+	ulint	zip_size;
+	ulint	page_no;
+	ulint	page_size;
+	ulint	buflen;
+	byte*	page;
+
+	ib_logf(IB_LOG_LEVEL_INFO, "Restoring first page of tablespace %lu",
+		fsp->id);
+
+	if (fsp->id == 0) {
+		err = false;
+		goto out;
+	}
+
+	// find if double write buffer has page0 of given space id
+	page = recv_sys->dblwr.find_first_page(fsp->id);
+
+	if (!page) {
+		err = false;
+		goto out;
+	}
+
+        flags = mach_read_from_4(FSP_HEADER_OFFSET + FSP_SPACE_FLAGS + page);
+	zip_size = fsp_flags_get_zip_size(flags);
+	page_no = page_get_page_no(page);
+	page_size = fsp_flags_get_page_size(flags);
+
+	ut_ad(page_no == 0);
+
+	buflen = zip_size ? zip_size: page_size;
+
+	ib_logf(IB_LOG_LEVEL_INFO, "Writing %lu bytes into file: %s",
+		buflen, fsp->filepath);
+
+	err = os_file_write(fsp->filepath, fsp->file, page, 0, buflen);
+out:
+	return(err);
+}
+
 /********************************************************************//**
 Opens an .ibd file and adds the associated single-table tablespace to the
 InnoDB fil0fil.cc data structures.
@@ -4026,6 +4185,10 @@ fil_validate_single_table_tablespace(
 	const char*	tablename,	/*!< in: database/tablename */
 	fsp_open_info*	fsp)		/*!< in/out: tablespace info */
 {
+	bool restore_attempted = false;
+
+check_first_page:
+	fsp->success = TRUE;
 	if (const char* check_msg = fil_read_first_page(
 		    fsp->file, FALSE, &fsp->flags, &fsp->id,
 #ifdef UNIV_LOG_ARCHIVE
@@ -4036,6 +4199,19 @@ fil_validate_single_table_tablespace(
 			"%s in tablespace %s (table %s)",
 			check_msg, fsp->filepath, tablename);
 		fsp->success = FALSE;
+	}
+
+	if (!fsp->success) {
+		if (!restore_attempted) {
+			if (!fil_user_tablespace_find_space_id(fsp)) {
+				return;
+			}
+			restore_attempted = true;
+			if (!fil_user_tablespace_restore_page0(fsp)) {
+				return;
+			}
+			goto check_first_page;
+		}
 		return;
 	}
 
@@ -4153,7 +4329,7 @@ fil_load_single_table_tablespace(
 	/* Try to open the tablespace in the datadir. */
 	def.file = os_file_create_simple_no_error_handling(
 		innodb_file_data_key, def.filepath, OS_FILE_OPEN,
-		OS_FILE_READ_ONLY, &def.success);
+		OS_FILE_READ_WRITE, &def.success);
 
 	/* Read the first page of the remote tablespace */
 	if (def.success) {
