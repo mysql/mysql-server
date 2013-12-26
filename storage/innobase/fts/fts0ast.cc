4d1073467553494a5ec9ea1a2027d56066ed0e5b
Shaohua Wang <shaohua.wang@oracle.com>
2013-12-26 17:47:04 +0800

    BUG#17978763 - SEGV IN FTSPARSE() BOOLEAN MODE QUERY AGAINST '\"\"@117'
    We check whether node is null in fts_ast_term_set_distance, and do nothing
    if the node is null.
    
    rb://4230 approved by Jimmy.Yang

--- storage/innobase/fts/fts0ast.cc
+++ storage/innobase/fts/fts0ast.cc
@@ -396,6 +396,10 @@ fts_ast_term_set_distance(
 	ulint		distance)		/*!< in: the text proximity
 						distance */
 {
+	if (node == NULL) {
+		return;
+	}
+
 	ut_a(node->type == FTS_AST_TEXT);
 	ut_a(node->text.distance == ULINT_UNDEFINED);
 
