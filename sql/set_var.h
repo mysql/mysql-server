bd5a3ca8d0f4c9bccbb444a8a01749d40c13a778
Murthy Narkedimilli <murthy.narkedimilli@oracle.com>
2014-01-06 10:53:19 +0530

    Updated/added copyright headers

--- sql/set_var.h
+++ sql/set_var.h
@@ -1,6 +1,6 @@
 #ifndef SET_VAR_INCLUDED
 #define SET_VAR_INCLUDED
-/* Copyright (c) 2002, 2013 Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2002, 2013, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
