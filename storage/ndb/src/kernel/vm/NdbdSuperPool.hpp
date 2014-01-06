c92223e19860bdef8ed9d8958b31a17fdb6173aa
Murthy Narkedimilli <murthy.narkedimilli@oracle.com>
2014-01-06 10:52:35 +0530

    Updated/added copyright headers

--- storage/ndb/src/kernel/vm/NdbdSuperPool.hpp
+++ storage/ndb/src/kernel/vm/NdbdSuperPool.hpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2003 MySQL AB
+/* Copyright (c) 2003, 2006 MySQL AB
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
