0335536930f803a0224e1c8c6813c237fd8f5bc4
Pekka Nousiainen <pekka.nousiainen@oracle.com>
2014-01-10 18:10:30 +0200

    bug#18041636 fixdict-x1.diff put jam()s in the switch

--- storage/ndb/src/kernel/blocks/dbdict/Dbdict.cpp
+++ storage/ndb/src/kernel/blocks/dbdict/Dbdict.cpp
@@ -26438,8 +26438,10 @@ Dbdict::dropFK_fromDropTrigger(Signal* signal, Uint32 op_key, Uint32 ret)
     ndbrequire(find_object(fk_ptr, impl_req->fkId));
     switch(dropFKRecPtr.p->m_sub_drop_trigger) {
     case 0:
+      jam();
       break;
     case 1:
+      jam();
       break;
     default:
       ndbrequire(false);
