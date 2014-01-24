fe147e92bd355ba8deb16cdc8485d83eafb3179a
Tor Didriksen <tor.didriksen@oracle.com>
2014-01-24 17:15:30 +0100

    Bug#17922198 REMOVE OBSOLETE IFDEF HAVE_PURIFY CODE.
    Patch #13
    Remove some obsolete HAVE purify/valgrind code from ha_ndbcluster/opt_range
    Approved by the cluster team.

--- sql/opt_range.cc
+++ sql/opt_range.cc
@@ -10659,14 +10659,6 @@ int QUICK_RANGE_SELECT::reset()
     mrr_buf_desc->buffer= mrange_buff;
     mrr_buf_desc->buffer_end= mrange_buff + buf_size;
     mrr_buf_desc->end_of_used_area= mrange_buff;
-#if defined(HAVE_VALGRIND) && defined(WITH_NDBCLUSTER_STORAGE_ENGINE)
-    /*
-      We need this until ndb will use the buffer efficiently
-      (Now ndb stores  complete row in here, instead of only the used fields
-      which gives us valgrind warnings in compare_record[])
-    */
-    memset(mrange_buff, 0, buf_size);
-#endif
   }
 
   if (!mrr_buf_desc)
