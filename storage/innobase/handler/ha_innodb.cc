90b16f313c4c2719df63fbf4ca9cd3ddd52e924d
Thirunarayanan B <thirunarayanan.balathandayuth@oracle.com>
2014-02-10 10:13:35 +0530

    Bug #14049391   INNODB MISCALCULATES AUTO-INCREMENT AFTER DECREASING                         AUTO_INCREMENT_INCREMENT Problem: ======= When auto_increment_increment system variable decreases, immediate next value of auto increment column is not affected.
    Solution:
    ========
    	Get the previous inserted value of auto increment column by
    subtracting the previous auto_increment_increment from next
    auto increment value. After that calculate the current autoinc value
    using newly changed auto_increment_increment variable.
    
    	Approved by Sunny [rb#4394]

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -10235,6 +10235,21 @@ ha_innobase::get_auto_increment(
 
 		current = *first_value > col_max_value ? autoinc : *first_value;
 
+		/* If the increment step of the auto increment column
+		decreases then it is not affecting the immediate
+		next value in the series. */
+		if (prebuilt->autoinc_increment > increment) {
+
+			current = autoinc - prebuilt->autoinc_increment;
+
+			current = innobase_next_autoinc(
+				current, 1, increment, 1, col_max_value);
+
+			dict_table_autoinc_initialize(prebuilt->table, current);
+
+			*first_value = current;
+		}
+
 		/* Compute the last value in the interval */
 		next_value = innobase_next_autoinc(
 			current, *nb_reserved_values, increment, offset,
