c01761128024eeefaaadc670237a0df74db20235
Shivji Jha <shivji.jha@oracle.com>
2014-02-11 17:51:54 +0530

    Bug#18185490: IF MASTER_HEARTBEAT_PERIOD OPTION IS NOT PROVIDED               IT IS AUTO SETTING TO DEFAULT
    Problem:
    Change master was resetting heartbeat_period to default
    and received_heartbeat to 0 on every change master command.
    Its unintuitive behavior to change heartbeat_period unless
    this option is explicitly given. Similarly, setting
    received_heartbeat to 0 on change master is an unexpected
    behavior.
    
    Fix:
    - dont set heartbeat_period to default on change master.
    - dont reset received_heartbeat to 0 on change master.
    - Updated tests.

--- sql/rpl_slave.cc
+++ sql/rpl_slave.cc
@@ -8452,10 +8452,7 @@ bool change_master(THD* thd, Master_info* mi)
     mi->retry_count = lex_mi->retry_count;
   if (lex_mi->heartbeat_opt != LEX_MASTER_INFO::LEX_MI_UNCHANGED)
     mi->heartbeat_period = lex_mi->heartbeat_period;
-  else
-    mi->heartbeat_period= min<float>(SLAVE_MAX_HEARTBEAT_PERIOD,
-                                     (slave_net_timeout/2.0));
-  mi->received_heartbeats= LL(0); // counter lives until master is CHANGEd
+
   /*
     reset the last time server_id list if the current CHANGE MASTER 
     is mentioning IGNORE_SERVER_IDS= (...)
