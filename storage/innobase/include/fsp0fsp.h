f458d301a03685b2107f4b85fd9cf269e1702e99
Marko Mäkelä <marko.makela@oracle.com>
2014-02-14 22:11:21 +0200

    Bug#18251675 CLEAN UP SOME REDO LOG CODE
    InnoDB used to have disabled functionality for archiving redo logs
    (UNIV_LOG_ARCHIVE) and for multiple redo log copies (log groups). Most of
    this was removed, but some of it remain.
    
    Also, there are some dead variables and parameters that could be removed.
    
    innodb_gis.innodb_gis_bug17057168: Enable the test on embedded and non-debug.
    
    fil_space_create(): Remove some redundant initialization. The memory
    is already initialized by ut_zalloc().
    
    fil_load_single_table_tablespace(): Fix a syntax error inside #if code
    
    log_group_read_log_seg(): Remove the parameter type=LOG_RECOVER
    
    recv_sys_t::limit_lsn: Remove.
    
    LOG_RECOVER, LOG_ARCHIVE, LOG_ARCHIVE_BUF_SIZE,
    LOG_ARCHIVE_EXTRA_MARGIN, LOG_ARCHIVE_RATIO_ASYNC:
    LOG_ARCHIVE_READ, LOG_ARCHIVE_WRITE: Remove.
    
    log_calc_max_ages(): Do not compute smallest_archive_margin.
    
    recv_log_scan_is_startup_type: Remove (it always evaluates to 1 in the check).
    This should probably have been removed when UNIV_LOG_ARCHIVE was removed.
    
    recv_previous_parsed_rec_type: Change the type to mlog_id_t.
    
    recv_find_max_checkpoint(), recv_report_corrupt_log(),
    recv_parse_log_recs(): Use ULINTPF in format strings.
    
    recv_parse_log_rec(): Make type the first parameter, to be in line
    with some other functions. Add Valgrind instrumentation.
    
    rb#4691 approved by Kevin Lewis

--- storage/innobase/include/fsp0fsp.h
+++ storage/innobase/include/fsp0fsp.h
@@ -70,7 +70,7 @@ descriptor page, but used only in the first. */
 					< 64 pages, this number is 64, i.e.,
 					we have initialized the space
 					about the first extent, but have not
-					physically allocted those pages to the
+					physically allocated those pages to the
 					file */
 #define	FSP_SPACE_FLAGS		16	/* fsp_space_t.flags, similar to
 					dict_table_t::flags */
@@ -246,6 +246,7 @@ Initializes the file space system. */
 void
 fsp_init(void);
 /*==========*/
+
 /**********************************************************************//**
 Gets the size of the system tablespace from the tablespace header.  If
 we do not have an auto-extending data file, this should be equal to
