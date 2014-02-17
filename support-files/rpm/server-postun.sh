cb324d91ad9ea656a5597a95c681e5400cbd3d49
Sergei Golubchik <sergii@pisem.net>
2014-02-17 11:10:03 +0100

    MDEV-5579 rpm postun scriptlet leaks exit code to rpm

--- support-files/rpm/server-postun.sh
+++ support-files/rpm/server-postun.sh
@@ -1,7 +1,8 @@
 if [ $1 -ge 1 ]; then
   if [ -x %{_sysconfdir}/init.d/mysql ] ; then
     # only restart the server if it was alredy running
-    %{_sysconfdir}/init.d/mysql status > /dev/null 2>&1 && \
-    %{_sysconfdir}/init.d/mysql restart
+    if %{_sysconfdir}/init.d/mysql status > /dev/null 2>&1; then
+      %{_sysconfdir}/init.d/mysql restart
+    fi
   fi
 fi
