42bfa908743f9c6c0347073d49ca807606575aff
Murthy Narkedimilli <murthy.narkedimilli@oracle.com>
2014-02-17 18:19:04 +0530

    Updated/added copyright header. Added line "use is subject to license terms" to copyright header.

--- storage/ndb/include/util/ConfigValues.hpp
+++ storage/ndb/include/util/ConfigValues.hpp
@@ -1,4 +1,5 @@
 /* Copyright (C) 2004-2006 MySQL AB
+   Use is subject to license terms
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
