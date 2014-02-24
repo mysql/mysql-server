2a35710bd87640a561675fa523eb2dac88db86d8
Annamalai Gurusami <annamalai.gurusami@oracle.com>
2014-02-24 15:20:56 +0530

    Bug #18161853 SEGV IN LOCK_REC_OTHER_TRX_HOLDS_EXPL()
    Problem:
    
    In the function lock_rec_other_trx_holds_expl(), the transaction list
    trx_sys->rw_trx_list is traversed end to end without taking the
    transaction subsystem mutex (trx_sys->mutex).  Because of this
    segmentation faults are possible.
    
    Solution:
    
    Acquire the transaction subsystem mutex, and then traverse the transaction
    list trx_sys->rw_trx_list.
    
    approved by Sunny over IM.

--- storage/innobase/lock/lock0lock.cc
+++ storage/innobase/lock/lock0lock.cc
@@ -1705,6 +1705,8 @@ lock_rec_other_trx_holds_expl(
 	trx_t* holds = NULL;
 
 	lock_mutex_enter();
+	mutex_enter(&trx_sys->mutex);
+
 	if (trx_t *impl_trx = trx_rw_is_active(trx_id, NULL)) {
 		ulint heap_no = page_rec_get_heap_no(rec);
 		for (trx_t* t = UT_LIST_GET_FIRST(trx_sys->rw_trx_list);
@@ -1722,6 +1724,8 @@ lock_rec_other_trx_holds_expl(
 			}
 		}
         }
+
+	mutex_exit(&trx_sys->mutex);
 	lock_mutex_exit();
 
 	return(holds);
