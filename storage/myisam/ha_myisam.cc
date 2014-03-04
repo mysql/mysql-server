52edd8b20bc67488c94bda151f8731193dd32d5b
Jon Olav Hauglid <jon.hauglid@oracle.com>
2014-03-04 08:45:27 +0100

    Bug#18259108: REMOVE THREAD_SAFE_INCREMENT/DECREMENT
    This patch removes the redundant thread_safe_increment/decrement
    functions and replaces their usage with my_atomic_* functions or 
    explicit mutex usage (where appropriate).
    
    The patch also removes some dead code from my_pthread.h

--- storage/myisam/ha_myisam.cc
+++ storage/myisam/ha_myisam.cc
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2000, 2013, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2000, 2014, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -2111,8 +2111,7 @@ int ha_myisam::ft_read(uchar *buf)
   if (!ft_handler)
     return -1;
 
-  thread_safe_increment(table->in_use->status_var.ha_read_next_count,
-                        &LOCK_status); // why ?
+  ha_statistic_increment(&SSV::ha_read_next_count);
 
   error=ft_handler->please->read_next(ft_handler,(char*) buf);
 
