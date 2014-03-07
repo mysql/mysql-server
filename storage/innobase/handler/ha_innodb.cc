c0d758f0ab6521a85d05cad69875ecde8ac8442b
Vasil Dimov <vasil.dimov@oracle.com>
2014-03-07 13:15:41 +0200

    WL#6068 Remove InnoDB rec_per_keys hack
    Revert the patch that ships n_distinct[] to the MySQL/Optimizer layer.
    Instead we will ship a floating point rec_per_key[] array.

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -11041,23 +11041,16 @@ ha_innobase::info_low(
 				break;
 			}
 
-			KEY*	key = &table->key_info[i];
+			for (j = 0; j < table->key_info[i].actual_key_parts; j++) {
 
-			if (key->flags & HA_FULLTEXT) {
-				/* The whole concept has no validity
-				for FTS indexes. */
-				for (j = 0; j < key->actual_key_parts; j++) {
-					key->rec_per_key[j] = 1;
-					this->set_n_distinct(i, j, stats.records);
+				if (table->key_info[i].flags & HA_FULLTEXT) {
+					/* The whole concept has no validity
+					for FTS indexes. */
+					table->key_info[i].rec_per_key[j] = 1;
+					continue;
 				}
-				continue;
-			}
 
-			ulint	n_uniq = dict_index_get_n_unique(index);
-
-			for (j = 0; j < key->actual_key_parts; j++) {
-
-				if (j + 1 > n_uniq) {
+				if (j + 1 > index->n_uniq) {
 					sql_print_error(
 						"Index %s of %s has %lu columns"
 					        " unique inside InnoDB, but "
@@ -11070,7 +11063,7 @@ ha_innobase::info_low(
 						index->name,
 						ib_table->name,
 						(unsigned long)
-						n_uniq, j + 1);
+						index->n_uniq, j + 1);
 					break;
 				}
 
@@ -11088,14 +11081,10 @@ ha_innobase::info_low(
 					rec_per_key = 1;
 				}
 
-				key->rec_per_key[j] =
+				table->key_info[i].rec_per_key[j] =
 				  rec_per_key >= ~(ulong) 0 ? ~(ulong) 0 :
 				  (ulong) rec_per_key;
 			}
-
-			ut_a(key->actual_key_parts == n_uniq);
-
-			this->set_n_distinct(i, index->stat_n_diff_key_vals);
 		}
 
 		if (!(flag & HA_STATUS_NO_LOCK)) {
@@ -16648,17 +16637,6 @@ ha_innobase::idx_cond_push(
 	in_range_check_pushed_down = TRUE;
 	/* We will evaluate the condition entirely */
 	DBUG_RETURN(NULL);
-}
-
-uint64_t
-ha_innobase::get_n_distinct(
-	uint key_nr,
-	uint prefix)
-{
-	ut_a(key_nr < this->table->s->keys);
-	ut_a(prefix < this->table->key_info[key_nr].actual_key_parts);
-
-	return(this->table->key_info[key_nr].n_distinct[prefix]);
 }
 
 /******************************************************************//**
