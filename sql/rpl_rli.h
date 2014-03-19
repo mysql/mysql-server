2c117d4822738a6f142c27c23c1b4fe6dfb45836
Libing Song <libing.song@oracle.com>
2014-03-19 13:13:28 +0800

    WL#6813: Intra-schema MTS: ordered commits (sequential consistency)
    DESCRIPTION
    ===========
    This worklog ensures that the commits by slave worker threads running in
    parallel are done in the same order as on the master. This also means that
    the slave will never externalize a database state which was never externalized
    by the master.
    
    DESIGN
    ======
    - slave_preserve_commit_order
      The behavior is optional. Users can control it through a new system variable
      named slave_preserve_commit_order.
    
    - Commit_order_manager
      We implement a commit order manager which maintains a queue of transactions
      which are applying by workers.
    
      Coordinator registers them into the queue when dispatching them to a worker.
      So the transactions in the queue have the order as they are in the relay log.
    
      All the transactions should go into the flush stage(see Binlog Group commit)
      in the queue sequence. When a worker commits its transaction(before the
      transaction goes into flush stage), it must wait until it becomes the queue
      head. It does means all transactions before it have entered into flush stage.
    
      Once the transaction enters flush stage, It is popped from the queue
      and signals the next transaction to go ahead.
    
    The feature is for only intra-schema multi-threaded slave, but not database
    partitioned multi-threaded slave. And it is not supported when log_bin or
    log_slave_updates is OFF.

--- sql/rpl_rli.h
+++ sql/rpl_rli.h
@@ -30,6 +30,7 @@
 struct RPL_TABLE_LIST;
 class Master_info;
 class Mts_submode;
+class Commit_order_manager;
 extern uint sql_slave_skip_counter;
 
 /*******************************************************************************
@@ -973,10 +974,26 @@ public:
   void adapt_to_master_version(Format_description_log_event *fdle);
   uchar slave_version_split[3]; // bytes of the slave server version
 
+  Commit_order_manager *get_commit_order_manager()
+  {
+    return commit_order_mngr;
+  }
+
+  void set_commit_order_manager(Commit_order_manager *mngr)
+  {
+    commit_order_mngr= mngr;
+  }
+
 protected:
   Format_description_log_event *rli_description_event;
 
 private:
+  /*
+    Commit order manager to order commits made by its workers. In context of
+    Multi Source Replication each worker will be ordered by the coresponding
+    corrdinator's order manager.
+   */
+  Commit_order_manager* commit_order_mngr;
 
   /**
     Delay slave SQL thread by this amount, compared to master (in
