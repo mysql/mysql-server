1558c0c257750c0a2e9709fd2abcc518daebb1e8
Jan Wedvik <jan.wedvik@oracle.com>
2014-03-26 09:18:05 +0100

    This commit implements a subset of WL#6562 ("Improve LongMessageBuffer stats and shortage behaviour"). It does three things:
    1) If the LMB pool is emptied, it prints an informative message in the ndb_X_out.log file, that explains what happened and what might be done about it.
    
    2) It increases the default size of the pool from 4MB to 64MB.
    
    3) It makes the LMB pool status visible through ndbinfo.

--- storage/ndb/src/mgmsrv/ConfigInfo.cpp
+++ storage/ndb/src/mgmsrv/ConfigInfo.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2013, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2014, Oracle and/or its affiliates. All rights reserved.
 
 
    This program is free software; you can redistribute it and/or modify
@@ -927,7 +927,7 @@ const ConfigInfo::ParamInfo ConfigInfo::m_ParamInfo[] = {
     ConfigInfo::CI_USED,
     false,
     ConfigInfo::CI_INT,
-    "4M",
+    "64M",
     "512k",
     STR_VALUE(MAX_INT_RNIL)},
 
