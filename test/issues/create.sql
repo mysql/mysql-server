111ca07eb9d6c29799c7a2487115b2b75aa77111
John David Duncan <john.duncan@oracle.com>
2014-03-26 15:56:14 -0700

    unused table in test suite

--- test/issues/create.sql
+++ test/issues/create.sql
@@ -5,8 +5,3 @@ CREATE TABLE if not exists `towns` (
   `county` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`town`)
 );
-
-CREATE TABLE if not exists a (
-  id int not null primary key,
-  cint int
-);
