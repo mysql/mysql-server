eec54e5a3bb7ad7b536aa7a31b1b39c5d11c491c
Marko Mäkelä <marko.makela@oracle.com>
2014-03-28 12:08:30 +0200

    WL#7142: Add a helper function fil_path_to_space_name().
    HAVE_MEMRCHR: Feature macro for memrchr().
    
    fil_load_single_table_tablespace(): Make the filename const again.
    Make use of the new helper function fil_path_to_space_name().

--- storage/innobase/include/fil0fil.h
+++ storage/innobase/include/fil0fil.h
@@ -677,8 +677,7 @@ enum fil_load_status {
 
 /** Open a tablespace file and add it to the InnoDB data structures.
 @param[in]	space_id	tablespace ID
-@param[in,out]	filename	databasename/tablename.ibd;
-will be normalized to use '/' between databasename and tablename
+@param[in]	filename	path/to/databasename/tablename.ibd
 @param[in]	filename_len	the length of the filename, in bytes
 @param[out]	space		the tablespace, or NULL on error
 @return status of the operation */
@@ -686,7 +685,7 @@ will be normalized to use '/' between databasename and tablename
 enum fil_load_status
 fil_load_single_table_tablespace(
 	ulint		space_id,
-	char*		filename,
+	const char*	filename,
 	ulint		filename_len,
 	fil_space_t*&	space)
 	__attribute__((warn_unused_result));
