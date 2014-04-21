99c4517b296733250325c2b8b3d9e54a6a01a1f5
Rich Prohaska <prohaska@tokutek.com>
2014-04-21 10:35:03 -0400

    #110 turn off replace into and insert ignore optimizations when binlog format != statement

--- storage/tokudb/ha_tokudb.cc
+++ storage/tokudb/ha_tokudb.cc
@@ -396,11 +396,7 @@ static inline bool do_ignore_flag_optimization(THD* thd, TABLE* table, bool opt_
         if (is_replace_into(thd) || is_insert_ignore(thd)) {
             uint pk_insert_mode = get_pk_insert_mode(thd);
             if ((!table->triggers && pk_insert_mode < 2) || pk_insert_mode == 0) {
-                // if bin log row replication is on then ignore the session variable
-                if (mysql_bin_log.is_open() && 
-                    (thd->variables.binlog_format != BINLOG_FORMAT_STMT && thd->variables.binlog_format != BINLOG_FORMAT_MIXED)) {
-                    do_opt = false;
-                } else {
+                if (mysql_bin_log.is_open() && thd->variables.binlog_format == BINLOG_FORMAT_STMT) {
                     do_opt = true;
                 }
             }
