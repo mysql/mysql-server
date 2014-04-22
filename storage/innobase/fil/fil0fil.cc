21536b189f1db7f9b47535337e63b8eb58b0f531
kevin.lewis@oracle.com <>
2014-04-22 16:52:54 -0500

    Fix more Windows 32 and 64 bit compiler warnings introduced by revision 7874 WL6968  InnoDB GIS: R-tree index support. WL6609: InnoDB GIS: Support Predicate Locking for GIS index WL6745: InnoDB GIS: Support DML for InnoDB GIS index

--- storage/innobase/fil/fil0fil.cc
+++ storage/innobase/fil/fil0fil.cc
@@ -4656,7 +4656,7 @@ fil_write_zeros(
 
 		offset += n_bytes;
 
-		n_bytes = ut_min(n_bytes, end - offset);
+		n_bytes = ut_min(n_bytes, static_cast<ulint>(end - offset));
 
 		DBUG_EXECUTE_IF("ib_crash_during_tablespace_extension",
 				DBUG_SUICIDE(););
@@ -4743,7 +4743,8 @@ retry:
 	ut_a(node_start != (os_offset_t) -1);
 
 	/* Number of physical pages in the node/file */
-	ulint		n_node_physical_pages = node_start / page_size;
+	ulint		n_node_physical_pages;
+	n_node_physical_pages = static_cast<int>(node_start) / page_size;
 
 	/* Number of pages to extend in the node/file */
 	lint		n_node_extend;
@@ -4785,7 +4786,8 @@ retry:
 
 		if (success) {
 			success = fil_write_zeros(
-				node, page_size, node_start, len);
+				node, page_size, node_start,
+				static_cast<ulint>(len));
 
 			if (!success) {
 				ib_logf(IB_LOG_LEVEL_WARN,
@@ -4801,7 +4803,7 @@ retry:
 
 		os_has_said_disk_full = !(success = (end == node_start + len));
 
-		pages_added = (end - node_start) / page_size;
+		pages_added = static_cast<ulint>(end - node_start) / page_size;
 
 	} else {
 		success = true;
