2423723b9e1ca5d300a7a74b7052e940550f7618
Tor Didriksen <tor.didriksen@oracle.com>
2014-04-22 09:47:30 +0200

    Bug#18557876 MOVE COMMON CODE OUT OF BYTE_ORDER_GENERIC[_X86].H
    Several of the functions in
      include/byte_order_generic.h
      include/byte_order_generic_x86.h
    are identical.
    Move them to include/my_byteorder.h
    
    Also: remove some un-necessary casting.

--- include/byte_order_generic.h
+++ include/byte_order_generic.h
@@ -20,96 +20,50 @@
 static inline int16 sint2korr(const uchar *A)
 {
   return
-    (int16) (((int16) ((uchar) (A)[0])) +
-             ((int16) ((int16) (A)[1]) << 8))
-    ;
-}
-
-static inline int32 sint3korr(const uchar *A)
-{
-  return
-    ((int32) ((((uchar) (A)[2]) & 128) ?
-              (((uint32) 255L << 24) |
-               (((uint32) (uchar) (A)[2]) << 16) |
-               (((uint32) (uchar) (A)[1]) << 8) |
-               ((uint32) (uchar) (A)[0])) :
-              (((uint32) (uchar) (A)[2]) << 16) |
-              (((uint32) (uchar) (A)[1]) << 8) |
-              ((uint32) (uchar) (A)[0])))
+    (int16) (((int16) (A[0])) +
+             ((int16) (A[1]) << 8))
     ;
 }
 
 static inline int32 sint4korr(const uchar *A)
 {
   return
-    (int32) (((int32) ((uchar) (A)[0])) +
-             (((int32) ((uchar) (A)[1]) << 8)) +
-             (((int32) ((uchar) (A)[2]) << 16)) +
-             (((int32) ((int16) (A)[3]) << 24)))
+    (int32) (((int32) (A[0])) +
+             (((int32) (A[1]) << 8)) +
+             (((int32) (A[2]) << 16)) +
+             (((int32) (A[3]) << 24)))
     ;
 }
 
 static inline uint16 uint2korr(const uchar *A)
 {
   return
-    (uint16) (((uint16) ((uchar) (A)[0])) +
-              ((uint16) ((uchar) (A)[1]) << 8))
-    ;
-}
-
-static inline uint32 uint3korr(const uchar *A)
-{
-  return
-    (uint32) (((uint32) ((uchar) (A)[0])) +
-              (((uint32) ((uchar) (A)[1])) << 8) +
-              (((uint32) ((uchar) (A)[2])) << 16))
+    (uint16) (((uint16) (A[0])) +
+              ((uint16) (A[1]) << 8))
     ;
 }
 
 static inline uint32 uint4korr(const uchar *A)
 {
   return
-    (uint32) (((uint32) ((uchar) (A)[0])) +
-              (((uint32) ((uchar) (A)[1])) << 8) +
-              (((uint32) ((uchar) (A)[2])) << 16) +
-              (((uint32) ((uchar) (A)[3])) << 24))
-    ;
-}
-
-static inline ulonglong uint5korr(const uchar *A)
-{
-  return
-    ((ulonglong)(((uint32) ((uchar) (A)[0])) +
-                 (((uint32) ((uchar) (A)[1])) << 8) +
-                 (((uint32) ((uchar) (A)[2])) << 16) +
-                 (((uint32) ((uchar) (A)[3])) << 24)) +
-     (((ulonglong) ((uchar) (A)[4])) << 32))
-    ;
-}
-
-static inline ulonglong uint6korr(const uchar *A)
-{
-  return
-    ((ulonglong)(((uint32)    ((uchar) (A)[0]))          +
-                 (((uint32)    ((uchar) (A)[1])) << 8)   +
-                 (((uint32)    ((uchar) (A)[2])) << 16)  +
-                 (((uint32)    ((uchar) (A)[3])) << 24)) +
-     (((ulonglong) ((uchar) (A)[4])) << 32) +
-     (((ulonglong) ((uchar) (A)[5])) << 40))
+    (uint32) (((uint32) (A[0])) +
+              (((uint32) (A[1])) << 8) +
+              (((uint32) (A[2])) << 16) +
+              (((uint32) (A[3])) << 24))
     ;
 }
 
 static inline ulonglong uint8korr(const uchar *A)
 {
   return
-    ((ulonglong)(((uint32) ((uchar) (A)[0])) +
-                 (((uint32) ((uchar) (A)[1])) << 8) +
-                 (((uint32) ((uchar) (A)[2])) << 16) +
-                 (((uint32) ((uchar) (A)[3])) << 24)) +
-     (((ulonglong) (((uint32) ((uchar) (A)[4])) +
-                    (((uint32) ((uchar) (A)[5])) << 8) +
-                    (((uint32) ((uchar) (A)[6])) << 16) +
-                    (((uint32) ((uchar) (A)[7])) << 24))) <<
+    ((ulonglong)(((uint32) (A[0])) +
+                 (((uint32) (A[1])) << 8) +
+                 (((uint32) (A[2])) << 16) +
+                 (((uint32) (A[3])) << 24)) +
+     (((ulonglong) (((uint32) (A[4])) +
+                    (((uint32) (A[5])) << 8) +
+                    (((uint32) (A[6])) << 16) +
+                    (((uint32) (A[7])) << 24))) <<
       32))
     ;
 }
@@ -121,49 +75,23 @@ static inline longlong  sint8korr(const uchar *A)
 
 static inline void int2store(uchar *T, uint16 A)
 {
-  uint def_temp= (uint) (A) ;
-  *((uchar*) (T))=  (uchar)(def_temp);
-  *((uchar*) (T)+1)=(uchar)((def_temp >> 8));
-}
-
-static inline void int3store(uchar *T, uint A)
-{
-  *((uchar*)(T))=(uchar) ((A));
-  *((uchar*) (T)+1)=(uchar) (((A) >> 8));
-  *((uchar*)(T)+2)=(uchar) (((A) >> 16));
+  uint def_temp= A ;
+  *(T)=   (uchar)(def_temp);
+  *(T+1)= (uchar)(def_temp >> 8);
 }
 
 static inline void int4store(uchar *T, uint32 A)
 {
-  *((char *)(T))=(char) ((A));
-  *(((char *)(T))+1)=(char) (((A) >> 8));
-  *(((char *)(T))+2)=(char) (((A) >> 16));
-  *(((char *)(T))+3)=(char) (((A) >> 24));
-}
-
-static inline void int5store(uchar *T, ulonglong A)
-{
-  *((char *)(T))=     (char)((A));
-  *(((char *)(T))+1)= (char)(((A) >> 8));
-  *(((char *)(T))+2)= (char)(((A) >> 16));
-  *(((char *)(T))+3)= (char)(((A) >> 24));
-  *(((char *)(T))+4)= (char)(((A) >> 32));
-}
-
-static inline void int6store(uchar *T, ulonglong A)
-{
-  *((char *)(T))=     (char)((A));
-  *(((char *)(T))+1)= (char)(((A) >> 8));
-  *(((char *)(T))+2)= (char)(((A) >> 16));
-  *(((char *)(T))+3)= (char)(((A) >> 24));
-  *(((char *)(T))+4)= (char)(((A) >> 32));
-  *(((char *)(T))+5)= (char)(((A) >> 40));
+  *(T)=  (uchar) (A);
+  *(T+1)=(uchar) (A >> 8);
+  *(T+2)=(uchar) (A >> 16);
+  *(T+3)=(uchar) (A >> 24);
 }
 
 static inline void int8store(uchar *T, ulonglong A)
 {
-  uint def_temp= (uint) (A),
-       def_temp2= (uint) ((A) >> 32);
-  int4store((T),def_temp);
-  int4store((T+4),def_temp2);
+  uint def_temp= (uint) A,
+       def_temp2= (uint) (A >> 32);
+  int4store(T,  def_temp);
+  int4store(T+4,def_temp2);
 }
