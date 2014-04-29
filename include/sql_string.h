9405cf3d1f7f52f7826811f491b5d406503c70be
Jon Olav Hauglid <jon.hauglid@oracle.com>
2014-04-29 13:47:26 +0200

    Bug#18525978: COMBINE CLIENT/SQL_STRING AND SQL/SQL_STRING
    This patch removes client/sql_string.cc and moves sql/sql_string.cc to
    sql-common so that it can be used by both client and server.
    
    This removes code duplication. It turns out that the differences between
    client/sql_string.cc and sql/sql_string.cc were mostly due to bugfixes only
    applied to the latter (Bug#56492, Bug#44793, WL#1213, Bug#24293).

--- include/sql_string.h
+++ include/sql_string.h
@@ -1,7 +1,7 @@
-#ifndef CLIENT_SQL_STRING_INCLUDED
-#define CLIENT_SQL_STRING_INCLUDED
+#ifndef SQL_STRING_INCLUDED
+#define SQL_STRING_INCLUDED
 
-/* Copyright (c) 2000, 2013, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2000, 2014, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -18,10 +18,106 @@
 
 /* This file is originally from the mysql distribution. Coded by monty */
 
-#include "m_ctype.h"
-#include "my_sys.h"
+#include "m_ctype.h"                            /* my_charset_bin */
+#include "my_sys.h"              /* alloc_root, my_free, my_realloc */
+#include "m_string.h"                           /* TRASH */
+
+#ifdef MYSQL_SERVER
+extern PSI_memory_key key_memory_String_value;
+#define STRING_PSI_MEMORY_KEY key_memory_String_value
+#else
+#define STRING_PSI_MEMORY_KEY PSI_NOT_INSTRUMENTED
+#endif
+
+/**
+  A wrapper class for null-terminated constant strings.
+  Constructors make sure that the position of the '\0' terminating byte
+  in m_str is always in sync with m_length.
+
+  This class must stay as small as possible as we often 
+  pass it and its descendants (such as Name_string) into functions
+  using call-by-value evaluation.
+
+  Don't add new members or virual methods into this class!
+*/
+class Simple_cstring
+{
+private:
+  const char *m_str;
+  size_t m_length;
+protected:
+  /**
+    Initialize from a C string whose length is already known.
+  */
+  void set(const char *str_arg, size_t length_arg)
+  {
+    // NULL is allowed only with length==0
+    DBUG_ASSERT(str_arg || length_arg == 0);
+    // For non-NULL, make sure length_arg is in sync with '\0' terminator.
+    DBUG_ASSERT(!str_arg || str_arg[length_arg] == '\0');
+    m_str= str_arg;
+    m_length= length_arg;
+  }
+public:
+  Simple_cstring()
+  {
+    set(NULL, 0);
+  }
+  Simple_cstring(const char *str_arg, size_t length_arg)
+  {
+    set(str_arg, length_arg);
+  }
+  Simple_cstring(const LEX_STRING arg)
+  {
+    set(arg.str, arg.length);
+  }
+  void reset()
+  {
+    set(NULL, 0);
+  }
+  /**
+    Set to a null-terminated string.
+  */
+  void set(const char *str)
+  {
+    set(str, str ? strlen(str) : 0);
+  }
+  /**
+    Return string buffer.
+  */
+  const char *ptr() const { return m_str; }
+  /**
+    Check if m_ptr is set.
+  */
+  bool is_set() const { return m_str != NULL; }
+  /**
+    Return name length.
+  */
+  size_t length() const { return m_length; }
+  /**
+    Compare to another Simple_cstring.
+  */
+  bool eq_bin(const Simple_cstring other) const
+  {
+    return m_length == other.m_length &&
+           memcmp(m_str, other.m_str, m_length) == 0;
+  }
+  /**
+    Copy to the given buffer
+  */
+  void strcpy(char *buff) const
+  {
+    memcpy(buff, m_str, m_length);
+    buff[m_length]= '\0';
+  }
+};
+
 
 class String;
+typedef struct charset_info_st CHARSET_INFO;
+typedef struct st_io_cache IO_CACHE;
+typedef struct st_mem_root MEM_ROOT;
+
 int sortcmp(const String *a,const String *b, const CHARSET_INFO *cs);
 String *copy_if_not_alloced(String *a,String *b,uint32 arg_length);
 inline uint32 copy_and_convert(char *to, uint32 to_length,
@@ -31,6 +127,19 @@ inline uint32 copy_and_convert(char *to, uint32 to_length,
 {
   return my_convert(to, to_length, to_cs, from, from_length, from_cs, errors);
 }
+uint32 well_formed_copy_nchars(const CHARSET_INFO *to_cs,
+                               char *to, uint to_length,
+                               const CHARSET_INFO *from_cs,
+                               const char *from, uint from_length,
+                               uint nchars,
+                               const char **well_formed_error_pos,
+                               const char **cannot_convert_error_pos,
+                               const char **from_end_pos);
+uint convert_to_printable(char *to, size_t to_len,
+                          const char *from, size_t from_len,
+                          const CHARSET_INFO *from_cs, size_t nbytes= 0);
+
+uint bin_to_hex_str(char *to, size_t to_len, char *from, size_t from_len);
 
 class String
 {
@@ -89,7 +198,7 @@ public:
   inline uint32 alloced_length() const { return Alloced_length;}
   inline char& operator [] (uint32 i) const { return Ptr[i]; }
   inline void length(uint32 len) { str_length=len ; }
-  inline bool is_empty() { return (str_length == 0); }
+  inline bool is_empty() const { return (str_length == 0); }
   inline void mark_as_const() { Alloced_length= 0;}
   inline const char *ptr() const { return Ptr; }
   inline char *c_ptr()
@@ -115,6 +224,11 @@ public:
       (void) realloc(str_length);
     return Ptr;
   }
+  LEX_STRING lex_string() const
+  {
+    LEX_STRING lex_string = { (char*) ptr(), length() };
+    return lex_string;
+  }
 
   void set(String &str,uint32 offset,uint32 arg_length)
   {
@@ -127,6 +241,16 @@ public:
       Alloced_length=0;
     str_charset=str.str_charset;
   }
+
+
+  /**
+     Points the internal buffer to the supplied one. The old buffer is freed.
+     @param str Pointer to the new buffer.
+     @param arg_length Length of the new buffer in characters, excluding any 
+            null character.
+     @param cs Character set to use for interpreting string data.
+     @note The new buffer will not be null terminated.
+  */
   inline void set(char *str,uint32 arg_length, const CHARSET_INFO *cs)
   {
     free();
@@ -148,9 +272,12 @@ public:
     }
     str_charset=cs;
   }
-  bool set(longlong num, const CHARSET_INFO *cs);
-  bool set(ulonglong num, const CHARSET_INFO *cs);
-  bool set(double num,uint decimals, const CHARSET_INFO *cs);
+  bool set_int(longlong num, bool unsigned_flag, const CHARSET_INFO *cs);
+  bool set(longlong num, const CHARSET_INFO *cs)
+  { return set_int(num, false, cs); }
+  bool set(ulonglong num, const CHARSET_INFO *cs)
+  { return set_int((longlong)num, true, cs); }
+  bool set_real(double num,uint decimals, const CHARSET_INFO *cs);
 
   /*
     PMG 2004.11.12
@@ -176,7 +303,9 @@ public:
   */
   inline void chop()
   {
-    Ptr[str_length--]= '\0'; 
+    str_length--;
+    Ptr[str_length]= '\0';
+    DBUG_ASSERT(strlen(Ptr) == str_length);
   }
 
   inline void free()
@@ -207,7 +336,7 @@ public:
     if (arg_length < Alloced_length)
     {
       char *new_ptr;
-      if (!(new_ptr=(char*) my_realloc(PSI_NOT_INSTRUMENTED,
+      if (!(new_ptr=(char*) my_realloc(STRING_PSI_MEMORY_KEY,
                                        Ptr,arg_length,MYF(0))))
       {
 	Alloced_length = 0;
@@ -237,14 +366,41 @@ public:
     }
     return *this;
   }
+  /**
+    Takeover the buffer owned by another string.
+    "this" becames the owner of the buffer and
+    is further responsible to free it.
+    The string "s" is detouched from the buffer (cleared).
+
+    @param s - a String object to steal buffer from.
+  */
+  inline void takeover(String &s)
+  {
+    DBUG_ASSERT(this != &s);
+    // Make sure buffers of the two Strings do not overlap
+    DBUG_ASSERT(!s.uses_buffer_owned_by(this));
+    free();
+    Ptr= s.Ptr;
+    str_length= s.str_length;
+    Alloced_length= s.Alloced_length;
+    alloced= s.alloced;
+    str_charset= s.str_charset;
+    s.Ptr= NULL;
+    s.Alloced_length= 0;
+    s.str_length= 0;
+    s.alloced= 0;
+  }
 
   bool copy();					// Alloc string if not alloced
   bool copy(const String &s);			// Allocate new string
-  /// Allocate new string
+  // Allocate new string
   bool copy(const char *s,uint32 arg_length, const CHARSET_INFO *cs);
   static bool needs_conversion(uint32 arg_length,
   			       const CHARSET_INFO *cs_from, const CHARSET_INFO *cs_to,
 			       uint32 *offset);
+  static bool needs_conversion_on_storage(uint32 arg_length,
+                                          const CHARSET_INFO *cs_from,
+                                          const CHARSET_INFO *cs_to);
   bool copy_aligned(const char *s, uint32 arg_length, uint32 offset,
 		    const CHARSET_INFO *cs);
   bool set_or_copy_aligned(const char *s, uint32 arg_length,
@@ -253,11 +409,21 @@ public:
 	    const CHARSET_INFO *csto, uint *errors);
   bool append(const String &s);
   bool append(const char *s);
-  bool append(const char *s,uint32 arg_length);
-  bool append(const char *s,uint32 arg_length, const CHARSET_INFO *cs);
+  bool append(LEX_STRING *ls)
+  {
+    return append(ls->str, (uint32) ls->length);
+  }
+  bool append(Simple_cstring str)
+  {
+    return append(str.ptr(), static_cast<uint>(str.length()));
+  }
+  bool append(const char *s, uint32 arg_length);
+  bool append(const char *s, uint32 arg_length, const CHARSET_INFO *cs);
+  bool append_ulonglong(ulonglong val);
   bool append(IO_CACHE* file, uint32 arg_length);
   bool append_with_prefill(const char *s, uint32 arg_length, 
 			   uint32 full_length, char fill_char);
+  bool append_parenthesized(long nr, int radix= 10);
   int strstr(const String &search,uint32 offset=0); // Returns offset to substring or -1
   int strrstr(const String &search,uint32 offset=0); // Returns offset to substring or -1
   bool replace(uint32 offset,uint32 arg_length,const char *to,uint32 length);
@@ -289,7 +455,6 @@ public:
     return realloc(str_length + space_needed);
   }
   int reserve(uint32 space_needed, uint32 grow_by);
-
   /*
     The following append operations do NOT check alloced memory
     q_*** methods writes values of parameters itself
@@ -369,6 +534,76 @@ public:
   {
     return (s->alloced && Ptr >= s->Ptr && Ptr < s->Ptr + s->str_length);
   }
+  bool is_ascii() const
+  {
+    if (length() == 0)
+      return TRUE;
+    if (charset()->mbminlen > 1)
+      return FALSE;
+    for (const char *c= ptr(), *end= c + length(); c < end; c++)
+    {
+      if (!my_isascii(*c))
+        return FALSE;
+    }
+    return TRUE;
+  }
+  /**
+    Make a zero-terminated copy of our value,allocated in the specified MEM_ROOT
+
+    @param root         MEM_ROOT to allocate the result
+
+    @return allocated string or NULL
+  */
+  char *dup(MEM_ROOT *root) const
+  {
+    if (str_length > 0 && Ptr[str_length - 1] == 0)
+      return static_cast<char *>(memdup_root(root, Ptr, str_length));
+
+    char *ret= static_cast<char*>(alloc_root(root, str_length + 1));
+    if (ret != NULL)
+    {
+      memcpy(ret, Ptr, str_length);
+      ret[str_length]= 0;
+    }
+    return ret;
+  }
 };
 
-#endif /* CLIENT_SQL_STRING_INCLUDED */
+
+/**
+  String class wrapper with a preallocated buffer of size buff_sz
+
+  This class allows to replace sequences of:
+     char buff[12345];
+     String str(buff, sizeof(buff));
+     str.length(0);
+  with a simple equivalent declaration:
+     StringBuffer<12345> str;
+*/
+
+template<size_t buff_sz>
+class StringBuffer : public String
+{
+  char buff[buff_sz];
+
+public:
+  StringBuffer() : String(buff, buff_sz, &my_charset_bin) { length(0); }
+  explicit StringBuffer(const CHARSET_INFO *cs) : String(buff, buff_sz, cs)
+  {
+    length(0);
+  }
+  StringBuffer(const char *str, size_t length, const CHARSET_INFO *cs)
+    : String(buff, buff_sz, cs)
+  {
+    set(str, length, cs);
+  }
+};
+
+
+static inline bool check_if_only_end_space(const CHARSET_INFO *cs, char *str, 
+                                           char *end)
+{
+  return str+ cs->cset->scan(cs, str, end, MY_SEQ_SPACES) == end;
+}
+
+#endif /* SQL_STRING_INCLUDED */
