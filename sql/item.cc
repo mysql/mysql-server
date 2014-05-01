285160dee270abbbfef9b7c1fb72ec4040c42262
unknown <sanja@askmonty.org>
2014-05-01 17:19:17 +0300

    MDEV-5981: name resolution issues with views and multi-update in ps-protocol
    It is triple bug with one test suite:
    1. Incorrect outer table detection
    2. Incorrect leaf table processing for multi-update (should be full like for usual updates and inserts)
    3. ON condition fix_fields() fould be called for all tables of the query.

--- sql/item.cc
+++ sql/item.cc
@@ -4336,6 +4336,10 @@ bool is_outer_table(TABLE_LIST *table, SELECT_LEX *select)
   DBUG_ASSERT(table->select_lex != select);
   TABLE_LIST *tl;
 
+  if (table->belong_to_view &&
+      table->belong_to_view->select_lex == select)
+    return FALSE;
+
   for (tl= select->master_unit()->derived;
        tl && tl->is_merged_derived();
        select= tl->select_lex, tl= select->master_unit()->derived)
