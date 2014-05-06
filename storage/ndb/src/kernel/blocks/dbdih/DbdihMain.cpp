6b692ef8a4af7c8f6517b4883f72459f3f334956
Frazer Clement <frazer.clement@oracle.com>
2014-05-06 15:27:28 +0100

    Bug #18455971 	CLUSTER CRASH CAUSED BY ERROR: 2301 ILLEGAL SIGNAL RECEIVED (GSN 40 NOT ADDED)
          During data node failure handling, the taking-over TC will gather all the
          known state for the failed TC instance(s) transactions, and determine
          a state for each transaction - committed or aborted.
          
          Once this has been determined, it will inform the API node(s) involved so that
          they can report the status accurately to clients.
          
          This is done by the taking over TC instance sending TCKEY_FAILREF or TCKEY_FAILCONF
          signals to the API nodes.
          
          However it is possible that the taking over TC instance does not have
          a direct connection to the API node.  Perhaps a connection was not
          successfully made at the point of the node failure.
          
          In this case, a best-effort attempt to deliver the signal is made
          by *routing* it via another node.  A node in the same nodegroup as the
          failing TC is chosen, and a GSN_TCKEY_FAILREFCONF_R signal is sent
          to TC block instance 0 in that node.
          
          With multi-TC, the TC block instance 0 will be an instance of the TC
          proxy block.
          
          The TC proxy block does not have a signal handler for TCKEY_FAILREFCONF_R,
          and so will crash.
          
          This patch adds a handler to the TC proxy block.  The behaviour is to 
          forward the signal to one of the local TC worker instances, which will
          behave as today (forward on to the Api if possible).
          
          A general 'forward signal' and 'round robin forward signal' mechanism
          is added to the LocalProxy superclass.
    
          Additionally, some bugs with EXECUTE_DIRECT into DIH to find a nodegroup peer
          were fixed.

--- storage/ndb/src/kernel/blocks/dbdih/DbdihMain.cpp
+++ storage/ndb/src/kernel/blocks/dbdih/DbdihMain.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2010, 2011, 2013, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2014, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -16438,7 +16438,7 @@ void Dbdih::makeNodeGroups(Uint32 nodeArray[])
  */
 void Dbdih::execCHECKNODEGROUPSREQ(Signal* signal)
 {
-  jamEntry();
+  jamNoBlock();
   CheckNodeGroups* sd = (CheckNodeGroups*)&signal->theData[0];
 
   bool direct = (sd->requestType & CheckNodeGroups::Direct);
@@ -16446,25 +16446,25 @@ void Dbdih::execCHECKNODEGROUPSREQ(Signal* signal)
   switch(sd->requestType & ~CheckNodeGroups::Direct){
   case CheckNodeGroups::ArbitCheck:{
     ok = true;
-    jam();
+    jamNoBlock();
     unsigned missall = 0;
     unsigned haveall = 0;
     for (Uint32 i = 0; i < cnoOfNodeGroups; i++) {
-      jam();
+      jamNoBlock();
       NodeGroupRecordPtr ngPtr;
       ngPtr.i = c_node_groups[i];
       ptrAss(ngPtr, nodeGroupRecord);
       Uint32 count = 0;
       for (Uint32 j = 0; j < ngPtr.p->nodeCount; j++) {
-	jam();
+	jamNoBlock();
 	Uint32 nodeId = ngPtr.p->nodesInGroup[j];
 	if (sd->mask.get(nodeId)) {
-	  jam();
+	  jamNoBlock();
 	  count++;
 	}//if
       }//for
       if (count == 0) {
-	jam();
+	jamNoBlock();
 	missall++;
       }//if
       if (count == ngPtr.p->nodeCount) {
@@ -16473,13 +16473,13 @@ void Dbdih::execCHECKNODEGROUPSREQ(Signal* signal)
     }//for
 
     if (missall) {
-      jam();
+      jamNoBlock();
       sd->output = CheckNodeGroups::Lose;
     } else if (haveall) {
-      jam();
+      jamNoBlock();
       sd->output = CheckNodeGroups::Win;
     } else {
-      jam();
+      jamNoBlock();
       sd->output = CheckNodeGroups::Partitioning;
     }//if
   }
@@ -16505,17 +16505,17 @@ void Dbdih::execCHECKNODEGROUPSREQ(Signal* signal)
     ngPtr.i = ng;
     if (ngPtr.i != RNIL)
     {
-      jam();
+      jamNoBlock();
       ptrAss(ngPtr, nodeGroupRecord);
       for (Uint32 j = 0; j < ngPtr.p->nodeCount; j++) {
-        jam();
+        jamNoBlock();
         sd->mask.set(ngPtr.p->nodesInGroup[j]);
       }
     }
     break;
   }
   case CheckNodeGroups::GetDefaultFragments:
-    jam();
+    jamNoBlock();
     ok = true;
     sd->output = (cnoOfNodeGroups + sd->extraNodeGroups)
       * getFragmentsPerNode() * cnoReplicas;
