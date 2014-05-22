985f445c8d80f1ede803414496cd32ba9ca67759
Thirunarayanan B <thirunarayanan.balathandayuth@oracle.com>
2014-05-22 16:15:37 +0530

    Bug #17657223   EXCESSIVE TEMPORARY FILE USAGE IN ALTER TABLE         Reverting the patch for the above bug due to regression.

--- storage/innobase/row/row0log.cc
+++ storage/innobase/row/row0log.cc
@@ -197,29 +197,6 @@ struct row_log_t {
 };
 
 
-/** Allocate the file for online log.
-@param[in,out]	log	online rebuild log
-@return TRUE if success, false if not */
-static __attribute__((warn_unused_result))
-bool
-row_log_file_create(
-	row_log_t*	log)
-{
-	DBUG_ENTER("row_log_file_create");
-	if (log->fd == -1) {
-		log->fd = row_merge_file_create_low();
-		DBUG_EXECUTE_IF("simulate_row_log_file_creation_failure",
-				if (log->fd != -1)
-					row_merge_file_destroy_low(log->fd);
-				log->fd = -1;);
-		if (log->fd < 0) {
-			DBUG_RETURN(false);
-		}
-		MONITOR_ATOMIC_INC(MONITOR_ALTER_TABLE_LOG_FILES);
-	}
-	DBUG_RETURN(true);
-}
-
 /** Allocate the memory for the log buffer.
 @param[in,out]	log_buf	Buffer used for log operation
 @return TRUE if success, false if not */
@@ -363,12 +340,6 @@ row_log_online_op(
 			       log->tail.buf, avail_size);
 		}
 		UNIV_MEM_ASSERT_RW(log->tail.block, srv_sort_buf_size);
-
-		if (!row_log_file_create(log)) {
-			log->error = DB_OUT_OF_MEMORY;
-			goto err_exit;
-		}
-
 		ret = os_file_write(
 			"(modification log)",
 			OS_FILE_FROM_FD(log->fd),
@@ -479,11 +450,6 @@ row_log_table_close_func(
 			       log->tail.buf, avail);
 		}
 		UNIV_MEM_ASSERT_RW(log->tail.block, srv_sort_buf_size);
-
-		if (!row_log_file_create(log)) {
-			log->error = DB_OUT_OF_MEMORY;
-			goto err_exit;
-		}
 		ret = os_file_write(
 			"(modification log)",
 			OS_FILE_FROM_FD(log->fd),
@@ -503,7 +469,6 @@ write_failed:
 
 	log->tail.total += size;
 	UNIV_MEM_INVALID(log->tail.buf, sizeof log->tail.buf);
-err_exit:
 	mutex_exit(&log->mutex);
 }
 
@@ -2533,8 +2498,7 @@ corruption:
 		if (index->online_log->head.blocks) {
 #ifdef HAVE_FTRUNCATE
 			/* Truncate the file in order to save space. */
-			if (index->online_log->fd != -1
-			    && ftruncate(index->online_log->fd, 0) == -1) {
+			if (ftruncate(index->online_log->fd, 0) == -1) {
 				perror("ftruncate");
 			}
 #endif /* HAVE_FTRUNCATE */
@@ -2870,7 +2834,11 @@ row_log_allocate(
 		DBUG_RETURN(false);
 	}
 
-	log->fd = -1;
+	log->fd = row_merge_file_create_low();
+	if (log->fd < 0) {
+		ut_free(log);
+		DBUG_RETURN(false);
+	}
 	mutex_create(index_online_log_key, &log->mutex,
 		     SYNC_INDEX_ONLINE_LOG);
 	log->blobs = NULL;
@@ -3362,8 +3330,7 @@ corruption:
 		if (index->online_log->head.blocks) {
 #ifdef HAVE_FTRUNCATE
 			/* Truncate the file in order to save space. */
-			if (index->online_log->fd != -1
-			    && ftruncate(index->online_log->fd, 0) == -1) {
+			if (ftruncate(index->online_log->fd, 0) == -1) {
 				perror("ftruncate");
 			}
 #endif /* HAVE_FTRUNCATE */
