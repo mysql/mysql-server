1e96de926c02e183050e1fc057af7c0b86042c56
Alexey Botchkov <holyfoot@askmonty.org>
2014-05-28 16:51:19 +0500

    MDEV-6216 sys_vars.completion_type_func fails in --embedded.
    The bug was in the mysqltest.c. It didn't check the send_query result
    properly, so when that fails and the thread gets disconnected,
    it goes unnoticed, so the consequitive read_result call crashes.
    Fixed by checking the sernd_query return.

--- client/mysqltest.cc
+++ client/mysqltest.cc
@@ -942,9 +942,13 @@ static int do_read_query_result(struct st_connection *cn)
 {
   DBUG_ASSERT(cn->has_thread);
   wait_query_thread_done(cn);
+  if (cn->result)
+    goto exit_func;
+
   signal_connection_thd(cn, EMB_READ_QUERY_RESULT);
   wait_query_thread_done(cn);
 
+exit_func:
   return cn->result;
 }
 
