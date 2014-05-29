da6a4a72f3194f28e711952b9531481fcf4b9d01
John David Duncan <john.duncan@oracle.com>
2014-05-29 16:21:09 -0700

    Don't try immediate startTransaction() unless using async api.

--- Adapter/impl/ndb/NdbTransactionHandler.js
+++ Adapter/impl/ndb/NdbTransactionHandler.js
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2013, Oracle and/or its affiliates. All rights
+ Copyright (c) 2014, Oracle and/or its affiliates. All rights
  reserved.
  
  This program is free software; you can redistribute it and/or
@@ -100,7 +100,7 @@ function run(self, operationSet, execMode, abortFlag, callback) {
 
     if(this.txIsOpen) {
       canStartImmediate = true;  // Transaction already started
-    } else { 
+    } else if(this.tx.asyncContext) { 
       canStartImmediate = this.operations.tryImmediateStartTransaction();
     }
 
