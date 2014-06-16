ac377047c71ce3da4510f48a12b4346ff984c466
Tor Didriksen <tor.didriksen@oracle.com>
2014-06-16 15:09:21 +0200

    Bug#18920203 GET RID OF DYNAMIC_ARRAY IN SERVER_IDS::DYNAMIC_I
    Our array abstractions should be removed, and substituted with modern data
    structures.
      
    Templatized vectors are:
     - type safe
     - easier to read/maintain
     - faster
    
    Also: remove some dead code.
    The class Database_ids is unused, which means we can remove it,
    and the base class Dynamic_ids.
    The member function Server_ids::do_search_id is also unused,
    and can be removed.
    
    The macro sort_dynamic() is no longer in use, and can be removed.
    
    There was a bug in change_receive_options():
    If we are inserting multiple ids into mi->ignore_server_ids
    then we need to re-sort before doing a binary_search.
    The old code worked "by accident", bsearch() found an element,
    even though the array was no longer sorted after an insert_dynamic()
    
    The fix for the sort/bsearch() bug: implement insertion-sort in Prealloced_array,
    and use insert_unique() to keep the array sorted at all times.
    This is a short array of ints, with pre-allocation, so insert-sort should be fairly efficient.

--- sql/rpl_info_file.h
+++ sql/rpl_info_file.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010, 2012, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2010, 2014, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -99,7 +99,7 @@ private:
   bool do_set_info(const int pos, const int value);
   bool do_set_info(const int pos, const ulong value);
   bool do_set_info(const int pos, const float value);
-  bool do_set_info(const int pos, const Dynamic_ids *value);
+  bool do_set_info(const int pos, const Server_ids *value);
   bool do_get_info(const int pos, char *value, const size_t size,
                    const char *default_value);
   bool do_get_info(const int pos, uchar *value, const size_t size,
@@ -110,8 +110,8 @@ private:
                    const ulong default_value);
   bool do_get_info(const int pos, float *value,
                    const float default_value);
-  bool do_get_info(const int pos, Dynamic_ids *value,
-                   const Dynamic_ids *default_value);
+  bool do_get_info(const int pos, Server_ids *value,
+                   const Server_ids *default_value);
   char* do_get_description_info();
   uint do_get_rpl_info_type();
 
