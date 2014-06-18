1701bc072d2b7790f5fd32ed346cf1581ca3a0cd
Krunal Bauskar krunal.bauskar@oracle.com <>
2014-06-18 09:29:19 +0530

    - WL#6965: Truncate UNDO logs.   - Fixed decrement of history size as we now used optimized way     to free the purge segment if the segment is due for truncate.

--- storage/innobase/trx/trx0purge.cc
+++ storage/innobase/trx/trx0purge.cc
@@ -616,7 +616,18 @@ loop:
 		Instead update the free page count that will be used
 		to adjust the rseg->curr_size. */
 		if (rseg->skip_allocation) {
+			mutex_enter(&(rseg->mutex));
 			rseg->pages_marked_freed += seg_size;
+#ifdef HAVE_ATOMIC_BUILTINS
+			os_atomic_decrement_ulint(
+				&trx_sys->rseg_history_len, n_removed_logs);
+#else
+			trx_sys_mutex_enter();
+			trx_sys->rseg_history_len -= n_removed_logs;
+			trx_sys_mutex_exit();
+#endif /* HAVE_ATOMIC_BUILTINS */
+			rseg->curr_size -= seg_size;
+			mutex_exit(&(rseg->mutex));
 		} else {
 			trx_purge_free_segment(
 				rseg, hdr_addr, n_removed_logs, noredo);
@@ -940,6 +951,11 @@ trx_purge_rseg_get_next_history_log(
 
 	mutex_enter(&(rseg->mutex));
 
+	purge_sys->iter.trx_no = rseg->last_trx_no + 1;
+	purge_sys->iter.undo_no = 0;
+	purge_sys->iter.undo_rseg_space = ULINT_UNDEFINED;
+	purge_sys->next_stored = FALSE;
+
 	if (rseg->last_page_no == FIL_NULL) {
 		/* if rseg is added to history list but we have truncated
 		the undo logs then rseg will not have any record to mark
@@ -948,11 +964,6 @@ trx_purge_rseg_get_next_history_log(
 		would have not qualified for purge. */
 		return;
 	}
-
-	purge_sys->iter.trx_no = rseg->last_trx_no + 1;
-	purge_sys->iter.undo_no = 0;
-	purge_sys->iter.undo_rseg_space = ULINT_UNDEFINED;
-	purge_sys->next_stored = FALSE;
 
 	mtr_start(&mtr);
 
