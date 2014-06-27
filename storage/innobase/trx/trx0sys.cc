865f8d2e8363238b07703b7073b99c267ef24361
Anil Toshniwal <anil.toshniwal@oracle.com>
2014-06-27 13:59:51 +0530

    Bug#13939924 MYSQLBACKUP REFERENCES IBBACKUP IN MYSQL ERROR LOG
    Fixed:The reference of "ibbackup" has to be replace by "mysqlbackup".
    Approved by: Shaohua(rb#5821)

--- storage/innobase/trx/trx0sys.cc
+++ storage/innobase/trx/trx0sys.cc
@@ -952,7 +952,7 @@ trx_sys_print_mysql_binlog_offset_from_page(
 	    == TRX_SYS_MYSQL_LOG_MAGIC_N) {
 
 		fprintf(stderr,
-			"ibbackup: Last MySQL binlog file position %lu %lu,"
+			"mysqlbackup: Last MySQL binlog file position %lu %lu,"
 			" file name %s\n",
 			(ulong) mach_read_from_4(
 				sys_header + TRX_SYS_MYSQL_LOG_INFO
@@ -1003,9 +1003,9 @@ trx_sys_read_file_format_id(
 		ut_print_timestamp(stderr);
 
 		fprintf(stderr,
-			"  ibbackup: Error: trying to read system tablespace "
-			"file format,\n"
-			"  ibbackup: but could not open the tablespace "
+			"  mysqlbackup: Error: trying to read system "
+			"tablespace file format,\n"
+			"  mysqlbackup: but could not open the tablespace "
 			"file %s!\n", pathname);
 		return(FALSE);
 	}
@@ -1022,9 +1022,9 @@ trx_sys_read_file_format_id(
 		ut_print_timestamp(stderr);
 
 		fprintf(stderr,
-			"  ibbackup: Error: trying to read system tablespace "
-			"file format,\n"
-			"  ibbackup: but failed to read the tablespace "
+			"  mysqlbackup: Error: trying to read system "
+			"tablespace file format,\n"
+			"  mysqlbackup: but failed to read the tablespace "
 			"file %s!\n", pathname);
 
 		os_file_close(file);
@@ -1083,9 +1083,9 @@ trx_sys_read_pertable_file_format_id(
 		ut_print_timestamp(stderr);
 
 		fprintf(stderr,
-			"  ibbackup: Error: trying to read per-table "
+			"  mysqlbackup: Error: trying to read per-table "
 			"tablespace format,\n"
-			"  ibbackup: but could not open the tablespace "
+			"  mysqlbackup: but could not open the tablespace "
 			"file %s!\n", pathname);
 
 		return(FALSE);
@@ -1102,9 +1102,9 @@ trx_sys_read_pertable_file_format_id(
 		ut_print_timestamp(stderr);
 
 		fprintf(stderr,
-			"  ibbackup: Error: trying to per-table data file "
+			"  mysqlbackup: Error: trying to per-table data file "
 			"format,\n"
-			"  ibbackup: but failed to read the tablespace "
+			"  mysqlbackup: but failed to read the tablespace "
 			"file %s!\n", pathname);
 
 		os_file_close(file);
