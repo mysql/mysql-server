5e74e225fdef9b488e3577269e42c4d6d3d9b30d
Nuno Carvalho <nuno.carvalho@oracle.com>
2014-06-30 17:02:59 +0100

    WL#6835 - Change thd_tx_priority and tx_priority from bool to int.

--- sql/rpl_slave.cc
+++ sql/rpl_slave.cc
@@ -3001,7 +3001,7 @@ void set_slave_thread_options(THD* thd)
   */
   if (thd->system_thread == SYSTEM_THREAD_SLAVE_SQL ||
       thd->system_thread == SYSTEM_THREAD_SLAVE_WORKER)
-    thd->thd_tx_priority= true;
+    thd->thd_tx_priority= 1;
 
   DBUG_VOID_RETURN;
 }
