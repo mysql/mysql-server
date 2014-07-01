59f82dbc0613f72a5124ac01d3c9129a694ca802
Krunal Bauskar krunal.bauskar@oracle.com <>
2014-07-01 14:11:36 +0530

    - WL#6965: Truncate UNDO logs.
      Fixed a race condition. Let's understand with an example:
    
      user-thrd: select rseg, trx->rseg = rseg
      purge-thrd: rseg->skip_allocation = true, rseg->curr_size ==1 
      continue with truncate.
      user-thrd: allocate page from trx->rseg and write undo-record.
      purge-thrd: truncate the tablespace.
      Oops! we lost the undo record of active transaction.
    
      To avoid this scenario we introduced a trx_ref_count that
      will keep track of active trx for the assigned rseg.
      While checking if rseg is free for truncate we also ensure it
      doesn't have any active transaction.

--- storage/innobase/trx/trx0undo.cc
+++ storage/innobase/trx/trx0undo.cc
@@ -2149,6 +2149,7 @@ trx_undo_truncate_tablespace(
 
 		ut_ad(rseg->curr_size == 1);
 
+		rseg->trx_ref_count = 0;
 		rseg->skip_allocation = false;
 		rseg->last_page_no = FIL_NULL;
 		rseg->last_offset = 0;
