6783358644d72521890a34ac08cb4ebdd3154113
David Zhao <david.zhao@oracle.com>
2014-07-14 20:08:40 +0800

    Bug #19133043 REPRESENTATION OF THE SAME POINT GEOMETRY ISSUE WITH SPATIAL ANALYSIS FUNCTIONS When output to WKT, make coordinates of -0 as 0.

--- sql/spatial.h
+++ sql/spatial.h
@@ -130,11 +130,6 @@ struct MBR
     double x, y;
     float8get(&x, px);
     float8get(&y, py);
-    if (x == -0)
-      x= 0;
-    if (y == -0)
-      y= 0;
-
     add_xy(x,y);
   }
   void add_mbr(const MBR *mbr)
