1e2ed03ce8b129fac4d9195e0a98fe545b234bfc
Venkatesh Duggirala <venkatesh.duggirala@oracle.com>
2014-07-28 18:41:22 +0530

    Bug#18860225 HASH_SCAN SEEMS BROKEN: CAN'T FIND RECORD IN 'T1' ERROR_CODE: 1032
    Problem: In RBR, on slave setting slave_search_algorithm to HASH_SCAN causes "Can't find
    record" error even though that record exists in storage layer.
    
    Analysis: When slave's slave_search_algorithm is set to Hash_scan, it prepares
    a unique key list in add_key_to_distinct_keyset() for all the rows in a
    particular Row_event.
    For eg: Lets say a table 't1(c1,c2, key(c2))' contains 4 tuples
    (1,10), (2,20), (3,10), (4,40). And lets say on Master a delete
    is executed to delete all these four tuples. Received Rows_log_event on slave
    contains all these 4 tuples. Slave should prepare a unique key list(i.e., 10,20,40).
    Later, the same unique key list will be used to retrieve all tuples associated
    to each key in the list from storage engine. In the old code, there was a problem
    while preparing this unique key list in  add_key_to_distinct_keyset. Before
    inserting an element in to the unique list, instead of searching the entire list whether an
    element is there or not, it was comparing only with the last inserted element.
    This can create problem when the processing list is not a sorted array.
    For eg: While processing 10,20,10,40 list, add_key_to_distinct_keyset() list
    thinks that the third 10 is unique as it compares with last inserted key which
    is 20. Hence unique key list prepared will be 10,20,10,40 instead of 10,20,40.
    
    Later in Rows_log_event::do_apply_event, we retrieve all the tuples with the first
    key in the list (10) and will apply the event on those tuples (which is delete in
    this example). When the logic retrieves '10' again from wrongly formed unique list,
    it tries to find the tuples from storage engine using the key value '10'. Thus
    storage engine returns 'ER_RECORD_NOT_FOUND' error.
    
    Fix: Correcting add_key_to_distinct_keyset() logic to maintain unique key list
    i.e., instead of comparing the newly inserted value with only last inserted key,
    now the list (unique list) will be maintained by std::set. To compare two keys,
    We have a compare class which will be supplied to std::set. This compare class
    compares two given keys using key_cmp2 function which is added in key.cc file.

--- sql/log_event.h
+++ sql/log_event.h
@@ -31,6 +31,7 @@
 #include <my_bitmap.h>
 #include "rpl_constants.h"
 #include "table_id.h"
+#include <set>
 
 #ifdef MYSQL_CLIENT
 #include "sql_const.h"
@@ -45,6 +46,7 @@
 #include "sql_class.h"                          /* THD */
 #include "rpl_utility.h"                        /* Hash_slave_rows */
 #include "rpl_filter.h"
+#include "key.h"                                /* key_copy, compare_keys */
 #endif
 
 /* Forward declarations */
@@ -4247,10 +4249,39 @@ protected:
   const uchar *m_curr_row;     /* Start of the row being processed */
   const uchar *m_curr_row_end; /* One-after the end of the current row */
   uchar    *m_key;      /* Buffer to keep key value during searches */
-  uchar    *last_hashed_key;
   uint     m_key_index;
-  List<uchar> m_distinct_key_list;
-  List_iterator_fast<uchar> m_itr;
+  KEY      *m_key_info; /* Points to description of index #m_key_index */
+  class Key_compare
+  {
+public:
+    /**
+       @param  ki  Where to find KEY description
+       @note m_distinct_keys is instantiated when Rows_log_event is constructed; it
+       stores a Key_compare object internally. However at that moment, the
+       index (KEY*) to use for comparisons, is not yet known. So, at
+       instantiation, we indicate the Key_compare the place where it can
+       find the KEY* when needed (this place is Rows_log_event::m_key_info),
+       Key_compare remembers the place in member m_key_info.
+       Before we need to do comparisons - i.e. before we need to insert
+       elements, we update Rows_log_event::m_key_info once for all.
+    */
+    Key_compare(KEY **ki= NULL) : m_key_info(ki) {}
+    bool operator()(uchar *k1, uchar *k2) const
+    {
+      return key_cmp2((*m_key_info)->key_part,
+                      k1, (*m_key_info)->key_length,
+                      k2, (*m_key_info)->key_length) < 0 ;
+    }
+private:
+    KEY **m_key_info;
+  };
+  std::set<uchar *, Key_compare> m_distinct_keys;
+  std::set<uchar *>::iterator m_itr;
+  /**
+    A spare buffer which will be used when saving the distinct keys
+    for doing an index scan with HASH_SCAN search algorithm.
+  */
+  uchar *m_distinct_key_spare_buf;
 
   // Unpack the current row into m_table->record[0]
   int unpack_current_row(const Relay_log_info *const rli,
@@ -4408,14 +4439,13 @@ private:
 
   /**
     Initializes scanning of rows. Opens an index and initailizes an iterator
-    over a list of distinct keys (m_distinct_key_list) if it is a HASH_SCAN
+    over a list of distinct keys (m_distinct_keys) if it is a HASH_SCAN
     over an index or the table if its a HASH_SCAN over the table.
   */
   int open_record_scan();
 
   /**
     Does the cleanup
-    - deallocates all the elements in m_distinct_key_list if any
     - closes the index if opened by open_record_scan
     - closes the table if opened for scanning.
   */
@@ -4436,7 +4466,7 @@ private:
   int next_record_scan(bool first_read);
 
   /**
-    Populates the m_distinct_key_list with unique keys to be modified
+    Populates the m_distinct_keys with unique keys to be modified
     during HASH_SCAN over keys.
     @return_value -0 success
                   -Err_code
