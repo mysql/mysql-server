95f841f456df4917049abf48a6f6a240938fee2d
Ole John Aske <ole.john.aske@oracle.com>
2014-08-11 11:31:11 +0200

    Fix for Bug#19414511:
      ::sendSignalNoRelease() does not report send failure before crashing
          
    Replace ndbrequire() with calls to either ::handle_out_of_longsignal_memory()
    or ::handle_send_failure() which will print a crash reason before 
    crashing.
    
    This is similar to code already existing in the 'normal' ::sendSignal().

--- storage/ndb/src/kernel/vm/SimulatedBlock.cpp
+++ storage/ndb/src/kernel/vm/SimulatedBlock.cpp
@@ -1,5 +1,5 @@
-/* Copyright (c) 2003-2007 MySQL AB
-
+/*
+   Copyright (c) 2003, 2014, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -1242,8 +1242,11 @@ SimulatedBlock::sendSignalNoRelease(BlockReference ref,
     for (Uint32 sec=0; sec < noOfSections; sec++)
     {
       Uint32 secCopy;
-      bool ok= ::dupSection(SB_SP_ARG secCopy, sections->m_ptr[sec].i);
-      ndbrequire (ok);
+      if (unlikely(! ::dupSection(SB_SP_ARG secCopy, sections->m_ptr[sec].i)))
+      {
+        handle_out_of_longsignal_memory(signal);
+        return;
+      }
       * dst ++ = secCopy;
     }
 
@@ -1289,7 +1292,12 @@ SimulatedBlock::sendSignalNoRelease(BlockReference ref,
                                                sections->m_ptr);
 #endif
 
-    ndbrequire(ss == SEND_OK || ss == SEND_BLOCKED || ss == SEND_DISCONNECTED);
+    if (unlikely(! (ss == SEND_OK ||
+                    ss == SEND_BLOCKED ||
+                    ss == SEND_DISCONNECTED)))
+    {
+      handle_send_failed(ss, signal);
+    }
   }
 
   signal->header.m_noOfSections = 0;
@@ -1365,8 +1373,11 @@ SimulatedBlock::sendSignalNoRelease(NodeReceiverGroup rg,
     for (Uint32 sec=0; sec < noOfSections; sec++)
     {
       Uint32 secCopy;
-      bool ok= ::dupSection(SB_SP_ARG secCopy, sections->m_ptr[sec].i);
-      ndbrequire (ok);
+      if (unlikely(! ::dupSection(SB_SP_ARG secCopy, sections->m_ptr[sec].i)))
+      {
+        handle_out_of_longsignal_memory(signal);
+        return;
+      }
       * dst ++ = secCopy;
     }
 
@@ -1420,7 +1431,12 @@ SimulatedBlock::sendSignalNoRelease(NodeReceiverGroup rg,
                                                sections->m_ptr);
 #endif
 
-    ndbrequire(ss == SEND_OK || ss == SEND_BLOCKED || ss == SEND_DISCONNECTED);
+    if (unlikely(! (ss == SEND_OK ||
+                    ss == SEND_BLOCKED ||
+                    ss == SEND_DISCONNECTED)))
+    {
+      handle_send_failed(ss, signal);
+    }
   }
 
   signal->header.m_noOfSections = 0;
