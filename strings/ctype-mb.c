63b2258148d7296ba26a2e9bb48aa380df56c935
mithun <mithun.c.y@oracle.com>
2014-08-12 17:16:51 +0530

    Bug #11755818 : LIKE DOESN'T MATCH WHEN CP932_BIN/SJIS_BIN                 COLLATIONS ARE USED.
    ISSUE :
    -------
    Code points of HALF WIDTH KATAKANA in SJIS/CP932 range from
    A1 to DF. In function my_wildcmp_mb_bin_impl while comparing
    such single byte code points, there is a code which compares
    signed character with unsigned character. Because of this,
    comparisons of two same code points representing a HALF
    WIDTH KATAKANA character always fails.
    
    Solution:
    ---------
    A code point of HALF WIDTH KATAKANA at-least need 8 bits.
    Promoting the variable from uchar to int will fix the issue.

--- strings/ctype-mb.c
+++ strings/ctype-mb.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2002, 2013, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2002, 2014, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -1044,7 +1044,7 @@ static int my_wildcmp_mb_bin_impl(CHARSET_INFO *cs,
     }
     if (*wildstr == w_many)
     {						/* Found w_many */
-      uchar cmp;
+      int cmp;
       const char* mb = wildstr;
       int mb_len=0;
       
