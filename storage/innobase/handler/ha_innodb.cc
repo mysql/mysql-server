684cb41a99294db574a0482dc6d689335a5cbe8f
Vasil Dimov <vasil.dimov@oracle.com>
2014-08-12 11:57:03 +0300

    WL#7777 Integrate PFS memory instrumentation with InnoDB
    Register the dict_stats thread for PFS accounting.

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -380,6 +380,7 @@ performance schema instrumented if "UNIV_PFS_THREAD"
 is defined */
 static PSI_thread_info	all_innodb_threads[] = {
 	PSI_KEY(buf_dump_thread),
+	PSI_KEY(dict_stats_thread),
 	PSI_KEY(io_handler_thread),
 	PSI_KEY(io_ibuf_thread),
 	PSI_KEY(io_log_thread),
