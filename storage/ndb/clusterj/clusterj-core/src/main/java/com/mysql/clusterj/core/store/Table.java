def159e1a10b7480db70f714ddb32fd9b738b39c
Craig L Russell <Craig.Russell@oracle.com>
2014-08-16 08:38:39 -0700

    Clusterj support for autoincrement columns http://wl.no.oracle.com/worklog/NDB-RawIdeaBin/index.pl?tid=8027
    Metadata: detect autoincrement columns; allow specification of
    autoincrement batch size
    
    Operation: when inserting row with autoincrement column, see if the
    column was set by user. If not, get autoincrement value from session factory
    and mark the column as set.
    
    SessionFactory: 
    
    schema.sql: add four tables with autoincrement pk columns
    drop table if exists autopkint;
    create table autopkint (
      id int primary key auto_increment,
      val int
    ) ENGINE=ndb;
    
    drop table if exists autopkbigint;
    create table autopkbigint (
      id bigint primary key auto_increment,
      val bigint
    ) ENGINE=ndb;
    
    drop table if exists autopksmallint;
    create table autopksmallint (
      id smallint primary key auto_increment,
      val smallint
    ) ENGINE=ndb;
    
    drop table if exists autopktinyint;
    create table autopktinyint (
      id tinyint primary key auto_increment,
      val tinyint
    ) ENGINE=ndb;
    
    mysql-test/suite/ndb/t/clusterj.test: drop tables after test
    
    Constants.java: Add three constant that users can set:
    PROPERTY_CLUSTER_CONNECT_AUTO_INCREMENT_BATCH_SIZE = "com.mysql.clusterj.connect.autoincrement.batchsize"
    PROPERTY_CLUSTER_CONNECT_AUTO_INCREMENT_STEP = "com.mysql.clusterj.connect.autoincrement.increment"
    PROPERTY_CLUSTER_CONNECT_AUTO_INCREMENT_START = "com.mysql.clusterj.connect.autoincrement.offset"
    
    SessionFactoryImpl.java: Set values for autoincrement constants
      initialize session factory with autoincrement constants
    
    ClusterConnection.java: define initialization for autoincrement constants
    
    Table.java: define get autoincrement column
    
    AutoPKTest.java: test autoincrement for four pk types: int, bigint, smallint, tinyint
      test insert with no primary key set and verify a unique value was used for the column
      test delete by key and verify proper values were inserted
    
    ClusterConnectionImpl.java: when initialized, pass autoincrement constants to DbImpl
    
    DbImpl.java: handle autoincrement
      initialize autoincrement constants
      when needed, get autoincrement value from wrapped Ndb object
    
    NdbRecordImpl.java: handle setting autoincrement value
      initialize autoincrement value setter based on column type
      set autoincrement value into NdbRecord buffer
    
    NdbRecordOperationImpl.java:
      handle autoincrement on insert operation when autoincrement column was not set by user
    
    TableImpl.java: 
      return autoincrement column if it is defined in the NdbTable
    
    Bundle.properties: new error messages for internal and user errors
    
    ndbjtie/NdbApiWrapper.hpp: define new methods
      Ndb__getAutoIncrementValue: get a new autoincrement value from Ndb
      NdbDictionary__Column__getAutoIncrement: return if a column is autoincrement
    
    ndbjtie/ndbapi/Ndb.java: implement Ndb:getAutoIncrementValue
    
    ndbjtie/ndbapi/NdbDictionary.java: implement Column:getAutoIncrement
    
    ndbjtie/ndbapi_jtie.hpp: implement JNI delegation to native method

--- storage/ndb/clusterj/clusterj-core/src/main/java/com/mysql/clusterj/core/store/Table.java
+++ storage/ndb/clusterj/clusterj-core/src/main/java/com/mysql/clusterj/core/store/Table.java
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2010, 2014, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -40,5 +40,8 @@ public interface Table {
 
     /** Get the names of all columns for this table */
     String[] getColumnNames();
+
+    /** Get the autoincrement column; null if no autoincrement column defined */
+    Column getAutoIncrementColumn();
 
 }
