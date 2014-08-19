91207143d377c0446f48cf05a1d564cd9f80320b
Rich Prohaska <prohaska@tokutek.com>
2014-08-19 17:09:28 -0400

    DB-708 move test scripts

--- scripts/atc.ontime/q6.sql
+++
@@ -1,1 +0,0 @@
-SELECT t.carrier, c, c2, c*1000/c2 as c3 FROM (SELECT carrier, count(*) AS c FROM ontime WHERE DepDelay>10 AND Year BETWEEN 2000 and 2008 GROUP BY carrier) t JOIN (SELECT carrier, count(*) AS c2 FROM ontime WHERE Year=2007 GROUP BY carrier) t2 ON (t.Carrier=t2.Carrier) ORDER BY c3 DESC;
