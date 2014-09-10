68eaf4d3bbcfa29f2d2fb49c6f1217a65a4252a2
Rich Prohaska <prohaska@tokutek.com>
2014-09-10 18:18:53 -0400

    DB-548 generate dirty upgrade test cases with old rollback nodes

--- src/tests/recover-rollback.cc
+++ src/tests/recover-rollback.cc
@@ -29,7 +29,7 @@ COPYING CONDITIONS NOTICE:
 
 COPYRIGHT NOTICE:
 
-  TokuFT, Tokutek Fractal Tree Indexing Library.
+  TokuDB, Tokutek Fractal Tree Indexing Library.
   Copyright (C) 2007-2013 Tokutek, Inc.
 
 DISCLAIMER:
@@ -89,105 +89,174 @@ PATENT RIGHTS GRANT:
 #ident "Copyright (c) 2007-2013 Tokutek Inc.  All rights reserved."
 #ident "The technology is licensed by the Massachusetts Institute of Technology, Rutgers State University of New Jersey, and the Research Foundation of State University of New York at Stony Brook under United States of America Serial No. 11/760379 and to the patents and/or patent applications resulting from it."
 
-// Test that recovery works correctly on a recovery log in a log directory.
+// Test dirty upgrade.
+// Generate a rollback log that requires recovery.
 
 #include "test.h"
-#include <libgen.h>
 
-static void run_recovery(const char *testdir) {
+// Insert max_rows key/val pairs into the db
+static void do_inserts(DB_TXN *txn, DB *db, uint64_t max_rows, size_t val_size) {
+    char val_data[val_size]; memset(val_data, 0, val_size);
     int r;
 
-    int log_version;
-    char shutdown[32+1];
-    r = sscanf(testdir, "upgrade-recovery-logs-%d-%32s", &log_version, shutdown);
-    assert(r == 2);
+    for (uint64_t i = 0; i < max_rows; i++) {
+        // pick a sequential key but it does not matter for this test.
+        uint64_t k[2] = {
+            htonl(i), random64(),
+        };
 
-    char **logfiles = nullptr;
-    int n_logfiles = 0;
-    r = toku_logger_find_logfiles(testdir, &logfiles, &n_logfiles);
-    CKERR(r);
-    assert(n_logfiles > 0);
-
-    FILE *f = fopen(logfiles[n_logfiles-1], "r");
-    assert(f);
-    uint32_t real_log_version;
-    r = toku_read_logmagic(f, &real_log_version);
-    CKERR(r);
-    assert((uint32_t)log_version == (uint32_t)real_log_version);
-    r = fclose(f);
-    CKERR(r);
-
-    toku_logger_free_logfiles(logfiles, n_logfiles);
-
-    // test needs recovery
-    r = tokuft_needs_recovery(testdir, false);
-    if (strcmp(shutdown, "clean") == 0) {
-        CKERR(r); // clean does not need recovery
-    } else if (strncmp(shutdown, "dirty", 5) == 0) {
-        CKERR2(r, 1); // dirty needs recovery
-    } else {
-        CKERR(EINVAL);
-    }
-
-    // test maybe upgrade log
-    LSN lsn_of_clean_shutdown;
-    bool upgrade_in_progress;
-    r = toku_maybe_upgrade_log(testdir, testdir, &lsn_of_clean_shutdown, &upgrade_in_progress);
-    if (strcmp(shutdown, "dirty") == 0 && log_version <= 24) {
-        CKERR2(r, TOKUDB_UPGRADE_FAILURE); // we dont support dirty upgrade from versions <= 24
-        return;
-    } else {
-        CKERR(r);
-    }
-
-    if (!verbose) {
-        // redirect stderr
-        int devnul = open(DEV_NULL_FILE, O_WRONLY);
-        assert(devnul >= 0);
-        int rr = toku_dup2(devnul, fileno(stderr));
-        assert(rr == fileno(stderr));
-        rr = close(devnul);
-        assert(rr == 0);
-    }
-
-    // run recovery
-    if (r == 0) {
-        r = tokuft_recover(NULL,
-                           NULL_prepared_txn_callback,
-                           NULL_keep_cachetable_callback,
-                           NULL_logger, testdir, testdir, 0, 0, 0, NULL, 0);
+        DBT key = { .data = k, .size = sizeof k };
+        DBT val = { .data = val_data, .size = (uint32_t) val_size };
+        r = db->put(db, txn, &key, &val, 0);
         CKERR(r);
     }
 }
 
-int test_main(int argc, const char *argv[]) {
-    int i = 0;
-    for (i = 1; i < argc; i++) {
+static void run_test(uint64_t num_rows, size_t val_size, bool do_crash) {
+    int r;
+
+    DB_ENV *env = nullptr;
+    r = db_env_create(&env, 0);
+    CKERR(r);
+    r = env->set_cachesize(env, 8, 0, 1);
+    CKERR(r);
+    r = env->open(env, TOKU_TEST_FILENAME,
+                  DB_INIT_MPOOL|DB_CREATE|DB_THREAD |DB_INIT_LOCK|DB_INIT_LOG|DB_INIT_TXN|DB_PRIVATE,
+                  S_IRWXU+S_IRWXG+S_IRWXO);
+    CKERR(r);
+
+    DB *db = nullptr;
+    r = db_create(&db, env, 0);
+    CKERR(r);
+    r = db->open(db, nullptr, "foo.db", 0, DB_BTREE, DB_CREATE, S_IRWXU+S_IRWXG+S_IRWXO);
+    CKERR(r);
+
+    r = env->txn_checkpoint(env, 0, 0, 0);
+    CKERR(r);
+
+    DB_TXN *txn = nullptr;
+    r = env->txn_begin(env, nullptr, &txn, 0);
+    CKERR(r);
+
+    do_inserts(txn, db, num_rows, val_size);
+
+    r = env->txn_checkpoint(env, 0, 0, 0);
+    CKERR(r);
+
+    r = txn->commit(txn, 0);
+    CKERR(r);
+
+    if (do_crash)
+        assert(0); // crash on purpose
+
+    r = db->close(db, 0);
+    CKERR(r);
+
+    r = env->close(env, 0);
+    CKERR(r);
+}
+
+static void do_verify(DB_ENV *env, DB *db, uint64_t num_rows, size_t val_size UU()) {
+    int r;
+    DB_TXN *txn = nullptr;
+    r = env->txn_begin(env, nullptr, &txn, 0);
+    CKERR(r);
+
+    DBC *c = nullptr;
+    r = db->cursor(db, txn, &c, 0);
+    CKERR(r);
+
+    uint64_t i = 0;
+    while (1) {
+        DBT key = {};
+        DBT val = {};
+        r = c->c_get(c, &key, &val, DB_NEXT);
+        if (r == DB_NOTFOUND)
+            break;
+        CKERR(r);
+        assert(key.size == 16);
+        uint64_t k[2];
+        memcpy(k, key.data, key.size);
+        assert(htonl(k[0]) == i);
+        assert(val.size == val_size);
+        i++;
+    }
+    assert(i == num_rows);
+
+    r = c->c_close(c);
+    CKERR(r);
+
+    r = txn->commit(txn, 0);
+    CKERR(r);
+}
+
+static void run_recover(uint64_t num_rows, size_t val_size) {
+    int r;
+
+    DB_ENV *env = nullptr;
+    r = db_env_create(&env, 0);
+    CKERR(r);
+    r = env->set_cachesize(env, 8, 0, 1);
+    CKERR(r);
+    r = env->open(env, TOKU_TEST_FILENAME,
+                  DB_INIT_MPOOL|DB_CREATE|DB_THREAD |DB_INIT_LOCK|DB_INIT_LOG|DB_INIT_TXN|DB_PRIVATE | DB_RECOVER,
+                  S_IRWXU+S_IRWXG+S_IRWXO);
+    CKERR(r);
+
+    DB *db = nullptr;
+    r = db_create(&db, env, 0);
+    CKERR(r);
+    r = db->open(db, nullptr, "foo.db", 0, DB_BTREE, 0, S_IRWXU+S_IRWXG+S_IRWXO);
+    CKERR(r);
+
+    do_verify(env, db, num_rows, val_size);
+
+    r = db->close(db, 0);
+    CKERR(r);
+
+    r = env->close(env, 0);
+    CKERR(r);
+}
+
+int test_main (int argc, char *const argv[]) {
+    bool do_test = false;
+    bool do_recover = false;
+    bool do_crash = true;
+    uint64_t num_rows = 1;
+    size_t val_size = 1;
+
+    for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-v") == 0) {
             verbose++;
             continue;
         }
         if (strcmp(argv[i], "-q") == 0) {
-            if (verbose > 0)
-                verbose--;
+            if (verbose > 0) verbose--;
             continue;
         }
-        break;
-    }
-    if (i < argc) {
-        const char *full_test_dir = argv[i];
-        const char *test_dir = basename((char *)full_test_dir);
-        if (strcmp(full_test_dir, test_dir) != 0) {
-            int r;
-            char cmd[32 + strlen(full_test_dir) + strlen(test_dir)];
-            sprintf(cmd, "rm -rf %s", test_dir);
-            r = system(cmd);
-            CKERR(r);
-            sprintf(cmd, "cp -r %s %s", full_test_dir, test_dir);
-            r = system(cmd);
-            CKERR(r);
+        if (strcmp(argv[i], "--test") == 0) {
+            do_test = true;
+            continue;
         }
-        run_recovery(test_dir);
+        if (strcmp(argv[i], "--recover") == 0) {
+            do_recover = true;
+            continue;
+        }
+        if (strcmp(argv[i], "--crash") == 0 && i+1 < argc) {
+            do_crash = atoi(argv[++i]);
+            continue;
+        }
     }
+    if (do_test) {
+        // init the env directory
+        toku_os_recursive_delete(TOKU_TEST_FILENAME);
+        int r = toku_os_mkdir(TOKU_TEST_FILENAME, S_IRWXU+S_IRWXG+S_IRWXO);
+        CKERR(r);
+        run_test(num_rows, val_size, do_crash);
+    }
+    if (do_recover) {
+        run_recover(num_rows, val_size);
+    }
+
     return 0;
 }
