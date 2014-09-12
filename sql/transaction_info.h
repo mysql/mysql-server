896a0dd95ac650ff9c3820ade24dfa511639dfe6
Andrei Elkin <andrei.elkin@oracle.com>
2014-09-12 11:17:10 +0300

    WL#7165 Optimizing MTS scheduling by increasing the parallelization window on master
    The patch implements the WL agenda. That includes logical timestamping
    transactions on the master side with two timestamps one of which
    enumerates the transaction in the binary log, and the other identifies
    an earlier transaction on which the current one may depend on.
    
    The slave applier tracks dependencies to schedule a transaction only
    when the dependency parent transaction is committed.
    To activate the logical clock scheduler one needs to specify
    
      mysql> set @@global.slave_parallel_type=logical_clock;
    
    Values of the two timestamps aka `last_committed' and
    `sequence_number' can be seen in binlog events through
    
      shell> mysqlbinlog binlog_file | grep 'last_committed\|sequence_number'.
    
    The timestamp values are valid within a single binlong. They "rotate"
    along with the binlog files.
    
    A sane (recommended) mumber of workers is that that slightly exceed
    the size of a parallelization window as defined as an average of
    a subtract of
    
       sequence_number - last_committed
    
    across transactions in the binlog.

--- sql/transaction_info.h
+++ sql/transaction_info.h
@@ -35,7 +35,6 @@ class Ha_trx_info;
 
 #define FLAGSTR(V,F) ((V)&(F)?#F" ":"")
 
-
 /**
   Either statement transaction or normal transaction - related
   thread-specific storage engine data.
@@ -339,6 +338,33 @@ public:
     bool ready_preempt;             // internal in MYSQL_BIN_LOG::ordered_commit
 #endif
   } m_flags;
+  /* Binlog-specific logical timestamps. */
+  /*
+    Store for the transaction's commit parent sequence_number.
+    The value specifies this transaction dependency with a "parent"
+    transaction.
+    The member is assigned, when the transaction is about to commit
+    in binlog to a value of the last committed transaction's sequence_number.
+    This and last_committed as numbers are kept ever incremented
+    regardless of binary logs being rotated.
+    However the logger to the binary log may convert them
+    according to its specification.
+  */
+  int64 last_committed;
+  /*
+    The transaction's private logical timestamp assigned at the
+    transaction prepare phase. The timestamp enumerates transactions
+    in the binary log. The value is gained through incrementing (stepping) a
+    global clock.
+    Eventually the value is considered to increase max_committed_transaction
+    system clock when the transaction has committed.
+  */
+  int64 sequence_number;
+
+  void store_commit_parent(int64 last_arg)
+  {
+    last_committed= last_arg;
+  }
 
   Transaction_ctx();
   ~Transaction_ctx()
