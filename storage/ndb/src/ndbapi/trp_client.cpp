4f81455952a67b8fc35c9481a0e4ae7db5b49fd6
Mauritz Sundell <mauritz.sundell@oracle.com>
2014-09-15 14:37:31 +0200

    Repush of WL#7544 after 7.4.1 clone tag.
    ------------------------------------------------------------
    revno: 4458
    revision-id: mikael.ronstrom@oracle.com-20140915090314-4feki0sr3fcmsflr
    parent: mikael.ronstrom@oracle.com-20140912185859-6417bvvey4ih4blu
    committer: Mikael Ronstrom <mikael.ronstrom@oracle.com>
    branch nick: push_wl7544
    timestamp: Mon 2014-09-15 11:03:14 +0200
    message:
      WL#7544: Optimisations of the NDB API receiver thread in various manners. 1) Optimising method to unpack field data from TRANSID_AI signal, 2) optimised handling of trp_client locking check, 3) reorganised code in NDB API receiver thread for better performance and more readability, 4) Optimisation signal receive handler

--- storage/ndb/src/ndbapi/trp_client.cpp
+++ storage/ndb/src/ndbapi/trp_client.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2010, 2013, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2010, 2014, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -24,6 +24,7 @@ trp_client::trp_client()
 {
   m_mutex = NdbMutex_Create();
 
+  m_locked_for_poll = false;
   m_send_nodes_cnt = 0;
   m_send_buffers = new TFBuffer[MAX_NODES];
 }
@@ -43,6 +44,7 @@ trp_client::~trp_client()
   m_mutex = NULL;
 
   assert(m_send_nodes_cnt == 0);
+  assert(m_locked_for_poll == false);
   delete [] m_send_buffers;
 }
 
