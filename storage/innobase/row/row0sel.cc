a39f8b2e05ebb2149c56cb50bdc03833e6e461de
Mattias Jonsson <mattias.jonsson@oracle.com>
2014-10-01 17:34:24 +0200

    Changing prebuilt_t::pcur and prebuilt_t::clust_pcur to pointer instead of direct attached struct.
    Non-functional change.
    
    Refactor broken out from InnoDB Native Partitioning WL#6035
    to easier switch pcur between partitions without having to copy them
    or change prebuilt.
    
    rb#6873 approved by Vasil

--- storage/innobase/row/row0sel.cc
+++ storage/innobase/row/row0sel.cc
@@ -3296,19 +3296,19 @@ row_sel_get_clust_rec_for_mysql(
 
 	btr_pcur_open_with_no_init(clust_index, prebuilt->clust_ref,
 				   PAGE_CUR_LE, BTR_SEARCH_LEAF,
-				   &prebuilt->clust_pcur, 0, mtr);
+				   prebuilt->clust_pcur, 0, mtr);
 
-	clust_rec = btr_pcur_get_rec(&prebuilt->clust_pcur);
+	clust_rec = btr_pcur_get_rec(prebuilt->clust_pcur);
 
-	prebuilt->clust_pcur.trx_if_known = trx;
+	prebuilt->clust_pcur->trx_if_known = trx;
 
 	/* Note: only if the search ends up on a non-infimum record is the
 	low_match value the real match to the search tuple */
 
 	if (!page_rec_is_user_rec(clust_rec)
-	    || btr_pcur_get_low_match(&prebuilt->clust_pcur)
+	    || btr_pcur_get_low_match(prebuilt->clust_pcur)
 	    < dict_index_get_n_unique(clust_index)) {
-		btr_cur_t*	btr_cur = btr_pcur_get_btr_cur(&prebuilt->pcur);
+		btr_cur_t*	btr_cur = btr_pcur_get_btr_cur(prebuilt->pcur);
 
 		/* If this is a spatial index scan, and we are reading
 		from a shadow buffer, the record could be already
@@ -3318,7 +3318,7 @@ row_sel_get_clust_rec_for_mysql(
 		     && btr_cur->rtr_info->matches
 		     && (page_align(rec)
 			== btr_cur->rtr_info->matches->block.frame
-			|| rec != btr_pcur_get_rec(&prebuilt->pcur))) {
+			|| rec != btr_pcur_get_rec(prebuilt->pcur))) {
 #ifdef UNIV_DEBUG
 			rtr_info_t*	rtr_info = btr_cur->rtr_info;
 			mutex_enter(&rtr_info->matches->rtr_match_mutex);
@@ -3342,7 +3342,7 @@ row_sel_get_clust_rec_for_mysql(
 				goto func_exit;
 			}
 
-			if (rec != btr_pcur_get_rec(&prebuilt->pcur)) {
+			if (rec != btr_pcur_get_rec(prebuilt->pcur)) {
 				clust_rec = NULL;
 
                                 err = DB_SUCCESS;
@@ -3351,7 +3351,7 @@ row_sel_get_clust_rec_for_mysql(
 
 			ulint		page_no = page_get_page_no(
 						btr_pcur_get_page(
-							&prebuilt->pcur));
+							prebuilt->pcur));
 
 			page_id_t	page_id(dict_index_get_space(sec_index),
 						page_no);
@@ -3422,7 +3422,7 @@ row_sel_get_clust_rec_for_mysql(
 		we set a LOCK_REC_NOT_GAP type lock */
 
 		err = lock_clust_rec_read_check_and_lock(
-			0, btr_pcur_get_block(&prebuilt->clust_pcur),
+			0, btr_pcur_get_block(prebuilt->clust_pcur),
 			clust_rec, clust_index, *offsets,
 			static_cast<lock_mode>(prebuilt->select_lock_type),
 			LOCK_REC_NOT_GAP,
@@ -3500,7 +3500,7 @@ func_exit:
 		/* We may use the cursor in update or in unlock_row():
 		store its position */
 
-		btr_pcur_store_position(&prebuilt->clust_pcur, mtr);
+		btr_pcur_store_position(prebuilt->clust_pcur, mtr);
 	}
 
 err_exit:
@@ -3799,7 +3799,7 @@ row_sel_try_search_shortcut_for_mysql(
 {
 	dict_index_t*	index		= prebuilt->index;
 	const dtuple_t*	search_tuple	= prebuilt->search_tuple;
-	btr_pcur_t*	pcur		= &prebuilt->pcur;
+	btr_pcur_t*	pcur		= prebuilt->pcur;
 	trx_t*		trx		= prebuilt->trx;
 	const rec_t*	rec;
 
@@ -3992,7 +3992,7 @@ row_search_no_mvcc(
 {
 	dict_index_t*	index		= prebuilt->index;
 	const dtuple_t*	search_tuple	= prebuilt->search_tuple;
-	btr_pcur_t*	pcur		= &prebuilt->pcur;
+	btr_pcur_t*	pcur		= prebuilt->pcur;
 
 	const rec_t*	result_rec	= NULL;
 	const rec_t*	clust_rec	= NULL;
@@ -4268,7 +4268,7 @@ row_search_mvcc(
 	dict_index_t*	index		= prebuilt->index;
 	ibool		comp		= dict_table_is_comp(index->table);
 	const dtuple_t*	search_tuple	= prebuilt->search_tuple;
-	btr_pcur_t*	pcur		= &prebuilt->pcur;
+	btr_pcur_t*	pcur		= prebuilt->pcur;
 	trx_t*		trx		= prebuilt->trx;
 	dict_index_t*	clust_index;
 	que_thr_t*	thr;
@@ -5836,7 +5836,7 @@ row_count_rtree_recs(
 	dict_index_t*	index		= prebuilt->index;
 	dberr_t		err		= DB_SUCCESS;
 	mtr_t		mtr;
-	btr_pcur_t*	pcur		= &prebuilt->pcur;
+	btr_pcur_t*	pcur		= prebuilt->pcur;
 	const rec_t*	rec;
 	ibool		comp		= dict_table_is_comp(index->table);
 	bool		move;
