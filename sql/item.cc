68354ef272087d589f07ee8892301cd8b9ff6eba
Alexander Barkov <bar@mnogosearch.org>
2014-10-03 15:07:53 +0400

    MDEV-6592 Assertion `ltime->day == 0' failed with TIMESTAMP, MAKETIME

--- sql/item.cc
+++ sql/item.cc
@@ -9015,17 +9015,11 @@ bool Item_cache_temporal::get_date(MYSQL_TIME *ltime, ulonglong fuzzydate)
 
 int Item_cache_temporal::save_in_field(Field *field, bool no_conversions)
 {
-  int error;
-  if (!has_value())
-    return set_field_to_null_with_conversions(field, no_conversions);
-
-  field->set_notnull();
- 
   MYSQL_TIME ltime;
-  unpack_time(value, &ltime);
-  ltime.time_type= mysql_type_to_time_type(field_type());
-  error= field->store_time_dec(&ltime, decimals);
- 
+  if (get_date(&ltime, 0))
+    return set_field_to_null_with_conversions(field, no_conversions);
+  field->set_notnull();
+  int error= field->store_time_dec(&ltime, decimals);
   return error ? error : field->table->in_use->is_error() ? 1 : 0;
 }
 
