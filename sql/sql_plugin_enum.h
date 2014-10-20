50107b47427b137431e92f965f5b249bd90f75aa
Jon Olav Hauglid <jon.hauglid@oracle.com>
2014-10-20 15:22:38 +0200

    Bug#19854885: REMOVE SHOW_ALWAYS_LAST TO AVOID HEADER DEPENDENCY
    sql_plugin.h currenctly defines a macro "SHOW_always_last" which adds
    extra server-only enum values to the "enum_mysql_show_type" externally
    visible enum defined in plugin.h. This requires all server code to include
    sql_plugin.h before plugin.h to get the correct enum definition. This is
    both bug-prone and problematic for refactoring.
    
    This patch removes SHOW_always_last and instead puts the server-only
    values in separate server header file which is included in plugin.h protected
    by #ifdef MYSQL_SERVER. This removes the awkward dependency between
    sql_plugin.h and plugin.h.
    
    By removing the bogus SHOW_always_last enum from the plugin.h
    defintion, this patch breaks ABI compatibility. Hence the patch also updates
    the required .pp files. As we have already bumped the libmysqlclient
    version, this should not be a problem (See Bug#19729266).

---
+++ sql/sql_plugin_enum.h
@@ -0,0 +1,32 @@
+/* Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   51 Franklin Street, Suite 500, Boston, MA 02110-1335 USA */
+
+/*
+  These are server-only members of enum_mysql_show_type defined in plugin.h.
+  The values are only meant for internal use and their use by plugins is
+  not supported.
+*/
+, SHOW_KEY_CACHE_LONG,
+SHOW_KEY_CACHE_LONGLONG,
+SHOW_LONG_STATUS,
+SHOW_DOUBLE_STATUS,
+SHOW_HAVE,
+SHOW_MY_BOOL,
+SHOW_HA_ROWS,
+SHOW_SYS,
+SHOW_LONG_NOFLUSH,
+SHOW_LONGLONG_STATUS,
+SHOW_LEX_STRING,
+SHOW_SIGNED_LONG
