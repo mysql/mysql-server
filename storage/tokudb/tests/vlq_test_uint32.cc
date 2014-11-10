607cb96b0a4cc9f4c9a7f18dab6200c95ad6a5a8
Rich Prohaska <prohaska@tokutek.com>
2014-11-10 16:34:55 -0500

    speed up tokudb handler unit tests

--- storage/tokudb/tests/vlq_test_uint32.cc
+++ storage/tokudb/tests/vlq_test_uint32.cc
@@ -101,50 +101,59 @@ namespace tokudb {
     template size_t vlq_decode_ui(uint64_t *np, void *p, size_t s);
 };
 
-static void test_vlq_uint32_error(void) {
-    uint32_t n;
-    unsigned char b[5];
-    size_t out_s, in_s;
+static void test_vlq_uint32(void) {
+    printf("%u\n", 0);
+    for (uint32_t v = 0; v < (1<<7); v++) {
+        unsigned char b[5];
+        size_t out_s = tokudb::vlq_encode_ui<uint32_t>(v, b, sizeof b);
+        assert(out_s == 1);
+        uint32_t n;
+        size_t in_s = tokudb::vlq_decode_ui<uint32_t>(&n, b, out_s);
+        assert(in_s == 1 && n == v);
+    }
 
-    out_s = tokudb::vlq_encode_ui<uint32_t>(128, b, 0);
-    assert(out_s == 0);
-    out_s = tokudb::vlq_encode_ui<uint32_t>(128, b, 1);
-    assert(out_s == 0);
-    out_s = tokudb::vlq_encode_ui<uint32_t>(128, b, 2);
-    assert(out_s == 2);
-    in_s = tokudb::vlq_decode_ui<uint32_t>(&n, b, 0);
-    assert(in_s == 0);
-    in_s = tokudb::vlq_decode_ui<uint32_t>(&n, b, 1);
-    assert(in_s == 0);
-    in_s = tokudb::vlq_decode_ui<uint32_t>(&n, b, 2);
-    assert(in_s == 2 && n == 128);
+    printf("%u\n", 1<<7);
+    for (uint32_t v = (1<<7); v < (1<<14); v++) {
+        unsigned char b[5];
+        size_t out_s = tokudb::vlq_encode_ui<uint32_t>(v, b, sizeof b);
+        assert(out_s == 2);
+        uint32_t n;
+        size_t in_s = tokudb::vlq_decode_ui<uint32_t>(&n, b, out_s);
+        assert(in_s == 2 && n == v);
+    }
+
+    printf("%u\n", 1<<14);
+    for (uint32_t v = (1<<14); v < (1<<21); v++) {
+        unsigned char b[5];
+        size_t out_s = tokudb::vlq_encode_ui<uint32_t>(v, b, sizeof b);
+        assert(out_s == 3);
+        uint32_t n;
+        size_t in_s = tokudb::vlq_decode_ui<uint32_t>(&n, b, out_s);
+        assert(in_s == 3 && n == v);
+    }
+
+    printf("%u\n", 1<<21);
+    for (uint32_t v = (1<<21); v < (1<<28); v++) {
+        unsigned char b[5];
+        size_t out_s = tokudb::vlq_encode_ui<uint32_t>(v, b, sizeof b);
+        assert(out_s == 4);
+        uint32_t n;
+        size_t in_s = tokudb::vlq_decode_ui<uint32_t>(&n, b, out_s);
+        assert(in_s == 4 && n == v);
+    }
+
+    printf("%u\n", 1<<28);
+    for (uint32_t v = (1<<28); v != 0; v++) {
+        unsigned char b[5];
+        size_t out_s = tokudb::vlq_encode_ui<uint32_t>(v, b, sizeof b);
+        assert(out_s == 5);
+        uint32_t n;
+        size_t in_s = tokudb::vlq_decode_ui<uint32_t>(&n, b, out_s);
+        assert(in_s == 5 && n == v);
+    }
 }
-
-static void test_80000000(void) {
-    uint64_t n;
-    unsigned char b[10];
-    size_t out_s, in_s;
-    uint64_t v = 0x80000000;
-    out_s = tokudb::vlq_encode_ui<uint64_t>(v, b, sizeof b);
-    assert(out_s == 5);
-    in_s = tokudb::vlq_decode_ui<uint64_t>(&n, b, out_s);
-    assert(in_s == 5 && n == v);
-}
-
-static void test_100000000(void) {
-    uint64_t n;
-    unsigned char b[10];
-    size_t out_s, in_s;
-    uint64_t v = 0x100000000;
-    out_s = tokudb::vlq_encode_ui<uint64_t>(v, b, sizeof b);
-    assert(out_s == 5);
-    in_s = tokudb::vlq_decode_ui<uint64_t>(&n, b, out_s);
-    assert(in_s == 5 && n == v);
-}   
 
 int main(void) {
-    test_vlq_uint32_error();
-    test_80000000();
-    test_100000000();
+    test_vlq_uint32();
     return 0;
 }
