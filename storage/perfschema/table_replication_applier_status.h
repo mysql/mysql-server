eeaa514a0f4d3a929ef98f7f604c07f42925747c
Shivji Jha <shivji.jha@oracle.com>
2014-11-14 16:06:30 +0530

    BUG#20001250: RPL P_S: CHANGE ALL USAGE OF EXECUTE TO               APPLY IN THE TABLE NAMES
    MySQL-5.7.2 introduced 6 replication P_S tables namely
    
    (1) replication_connection_configuration Table
    (2) replication_connection_status Table
    (3) replication_execute_configuration Table
    (4) replication_execute_status Table
    (5) replication_execute_status_by_coordinator Table
    (6) replication_execute_status_by_worker Table
    
    In this patch we replace all usage of the word
    "execute" by "apply" in tables (3) to (6).
    
    So the table names become:
    
    (3) replication_applier_configuration Table
    (4) replication_applier_status Table
    (5) replication_applier_status_by_coordinator Table
    (6) replication_applier_status_by_worker Table
    
    - Made changes in test and result files accordingly

--- storage/perfschema/table_replication_applier_status.h
+++ storage/perfschema/table_replication_applier_status.h
@@ -15,12 +15,12 @@
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 
 
-#ifndef TABLE_REPLICATION_EXECUTE_STATUS_BY_COORDINATOR_H
-#define TABLE_REPLICATION_EXECUTE_STATUS_BY_COORDINATOR_H
+#ifndef TABLE_REPLICATION_APPLIER_STATUS_H
+#define TABLE_REPLICATION_APPLIER_STATUS_H
 
 /**
-  @file storage/perfschema/table_replication_execute_applier_by_coordinator.h
-  Table replication_execute_status_by_coordinator(declarations).
+  @file storage/perfschema/table_replication_applier_status.h
+  Table replication_applier_status (declarations).
 */
 
 #include "pfs_column_types.h"
@@ -39,31 +39,25 @@ class Master_info;
 
 #ifndef ENUM_RPL_YES_NO
 #define ENUM_RPL_YES_NO
-/** enum values for Service_State of coordinator thread */
+/** enum values for Service_State field*/
 enum enum_rpl_yes_no {
-  PS_RPL_YES= 1, /* Service_State= on */
-  PS_RPL_NO /* Service_State= off */
+  PS_RPL_YES= 1,
+  PS_RPL_NO
 };
 #endif
 
-/*
-  A row in coordinator's table. The fields with string values have an
-  additional length field denoted by <field_name>_length.
-*/
-struct st_row_coordinator {
+/** A row in the table. */
+struct st_row_applier_status {
   char channel_name[CHANNEL_NAME_LENGTH];
   uint channel_name_length;
-  ulonglong thread_id;
-  bool thread_id_is_null;
   enum_rpl_yes_no service_state;
-  uint last_error_number;
-  char last_error_message[MAX_SLAVE_ERRMSG];
-  uint last_error_message_length;
-  ulonglong last_error_timestamp;
+  uint remaining_delay;
+  bool remaining_delay_is_set;
+  ulong count_transactions_retries;
 };
 
-/** Table PERFORMANCE_SCHEMA.replication_execute_status_by_coordinator */
-class table_replication_execute_status_by_coordinator: public PFS_engine_table
+/** Table PERFORMANCE_SCHEMA.replication_applier_status */
+class table_replication_applier_status: public PFS_engine_table
 {
 private:
   void make_row(Master_info *mi);
@@ -73,7 +67,7 @@ private:
   /** Fields definition. */
   static TABLE_FIELD_DEF m_field_def;
   /** Current row */
-  st_row_coordinator m_row;
+  st_row_applier_status m_row;
   /** True is the current row exists. */
   bool m_row_exists;
   /** Current position. */
@@ -95,10 +89,10 @@ protected:
                               Field **fields,
                               bool read_all);
 
-  table_replication_execute_status_by_coordinator();
+  table_replication_applier_status();
 
 public:
-  ~table_replication_execute_status_by_coordinator();
+  ~table_replication_applier_status();
 
   /** Table share. */
   static PFS_engine_table_share m_share;
