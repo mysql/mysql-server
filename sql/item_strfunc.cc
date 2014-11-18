cc2c296309def442de0ebbe101f1fd61cb1acc0a
Sergei Golubchik <sergii@pisem.net>
2014-11-18 15:42:48 +0100

    MDEV-4513 Valgrind warnings (Conditional jump or move depends on uninitialised value) in inflate on UNCOMPRESS

--- sql/item_strfunc.cc
+++ sql/item_strfunc.cc
@@ -3720,7 +3720,7 @@ String *Item_func_uncompress::val_str(String *str)
     goto err;
 
   if ((err= uncompress((Byte*)buffer.ptr(), &new_size,
-		       ((const Bytef*)res->ptr())+4,res->length())) == Z_OK)
+		       ((const Bytef*)res->ptr())+4,res->length()-4)) == Z_OK)
   {
     buffer.length((uint32) new_size);
     return &buffer;
