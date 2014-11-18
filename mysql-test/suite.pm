496fda66fdc34b447ef4dec26d1250b034a321e3
Sergei Golubchik <sergii@pisem.net>
2014-11-19 00:19:52 +0100

    openssl-poodle_6975.test: don't run it for older OpenSSL versions

--- mysql-test/suite.pm
+++ mysql-test/suite.pm
@@ -39,6 +39,9 @@ sub skip_combinations {
   }
   $skip{'include/check_ipv6.inc'} = 'No IPv6' unless ipv6_ok();
 
+  $skip{'t/openssl-poodle_6975.test'} = 'no or too old openssl'
+    unless ! IS_WINDOWS and ! system "openssl ciphers TLSv1.2 2>&1 >/dev/null";
+
   %skip;
 }
 
