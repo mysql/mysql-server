c4439503828ccad00d32108fbbddd1bc341035a0
V S Murthy Sidagam <venkata.sidagam@oracle.com>
2014-11-26 16:59:58 +0530

    Bug #16869534 QUERYING SUBSET OF COLUMNS DOESN'T USE TABLE CACHE; OPENED_TABLES I
    Description: When querying a subset of columns from the information_schema.TABLES
    
    Analysis: When information about tables is collected for statements like
    "SELECT ENGINE FROM I_S.TABLES" we do not perform full-blown table opens
    in SE, instead we only use information from table shares from the Table
    Definition Cache or .FRMs. Still in order to simplify I_S implementation
    mock TABLE objects are created from TABLE_SHARE during this process.
    This is done by calling open_table_from_share() function with special
    arguments. Since this function always increments "Opened_tables" counter,
    calls to it can be mistakingly interpreted as full-blown table opens in SE.
    
    Note that claim that "'SELECT ENGINE FROM I_S.TABLES' statement doesn't
    use Table Cache" is nevertheless factually correct. But it misses the
    point, since such statements a) don't use full-blown TABLE objects and
    therefore don't do table opens b) still use Table Definition Cache.
    
    Fix: We are now incrementing the counter when db_stat(i.e open flags for ha_open(
    
    we have considered an optimization which would use TABLE objects from
    Table Cache when available instead of constructing mock TABLE objects,
    but found it too intrusive for stable releases.

--- sql/table.cc
+++ sql/table.cc
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2000, 2012, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2000, 2014, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -2138,7 +2138,9 @@ partititon_err:
   outparam->no_replicate= outparam->file &&
                           test(outparam->file->ha_table_flags() &
                                HA_HAS_OWN_BINLOGGING);
-  thd->status_var.opened_tables++;
+  /* Increment the opened_tables counter, only when open flags set. */
+  if (db_stat)
+    thd->status_var.opened_tables++;
 
   DBUG_RETURN (0);
 
