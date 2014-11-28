78dc86208d3d3faca0b30eb0aeb65acec1418e84
Martin Skold <Martin.Skold@oracle.com>
2014-11-28 17:05:16 +0100

    Bug#20029843 7.4.2 REGRESSION: UPGRADE_TRAFFIC FAILURES FOR 7.4 -> 7.3: Fixed incorrect conditional expressions

--- storage/ndb/test/src/HugoTransactions.cpp
+++ storage/ndb/test/src/HugoTransactions.cpp
@@ -492,7 +492,7 @@ restart:
 	  closeTransaction(pNdb);
 	  return NDBT_FAILED;
 	}
-	const int updates = calc.getUpdatesValue(&row) + (m_empty_update)? 0 : 1;
+	const int updates = calc.getUpdatesValue(&row) + (m_empty_update? 0 : 1);
 	const int r = calc.getIdValue(&row);
         
   	for(a = 0; a<tab.getNoOfColumns(); a++){
@@ -1204,7 +1204,7 @@ HugoTransactions::pkUpdateRecords(Ndb* pNdb,
               return NDBT_FAILED;
             }
             
-            int updates = calc.getUpdatesValue(rows[0]) + (m_empty_update)? 0 : 1;
+            int updates = calc.getUpdatesValue(rows[0]) + (m_empty_update? 0 : 1);
             
             /* Rows may not arrive in the order they were requested
              * (When multiple partitions scanned without ORDERBY)
@@ -1253,7 +1253,7 @@ HugoTransactions::pkUpdateRecords(Ndb* pNdb,
 	  return NDBT_FAILED;
 	}
 	
-	int updates = calc.getUpdatesValue(rows[b]) + (m_empty_update)? 0 : 1;
+	int updates = calc.getUpdatesValue(rows[b]) + (m_empty_update? 0 : 1);
 	
 	if(pkUpdateRecord(pNdb, r+b, 1, updates) != NDBT_OK)
 	{
@@ -1389,7 +1389,7 @@ HugoTransactions::pkInterpretedUpdateRecords(Ndb* pNdb,
       return NDBT_FAILED;
     }
 
-    int updates = calc.getUpdatesValue(&row) + (m_empty_update)? 0 : 1;
+    int updates = calc.getUpdatesValue(&row) + (m_empty_update? 0 : 1);
 
     NdbOperation* pUpdOp;
     pUpdOp = pTrans->getNdbOperation(tab.getName());	
@@ -2216,7 +2216,7 @@ HugoTransactions::indexUpdateRecords(Ndb* pNdb,
 	return NDBT_FAILED;
       }
       
-      int updates = calc.getUpdatesValue(rows[b]) + (m_empty_update)? 0 : 1;
+      int updates = calc.getUpdatesValue(rows[b]) + (m_empty_update? 0 : 1);
       
       NdbOperation* pUpdOp;
       if(!ordered){
