743e2ae433ca981bab5b2f74438e05d275245c8a
Nirbhay Choubey <nirbhay@mariadb.com>
2014-12-12 17:10:51 -0500

    MDEV-6891: Addendum, update company name in copyright notice

--- include/welcome_copyright_notice.h
+++ include/welcome_copyright_notice.h
@@ -25,6 +25,6 @@
 */
 #define ORACLE_WELCOME_COPYRIGHT_NOTICE(first_year) \
   "Copyright (c) " first_year ", " COPYRIGHT_NOTICE_CURRENT_YEAR \
-   ", Oracle, Monty Program Ab and others.\n"
+   ", Oracle, MariaDB Corporation Ab and others.\n"
 
 #endif /* _welcome_copyright_notice_h_ */
