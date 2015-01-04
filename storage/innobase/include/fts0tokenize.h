43230c2ec732a4fb5d6da70fa6ebf1a09a4fdd97
Shaohua Wang <shaohua.wang@oracle.com>
2015-01-04 14:57:42 +0800

    Follow-up to WL#6607 : InnoDB FULLTEXT SEARCH: CJK support
    Fix compilation errors in fts0tokenize.h with '-DWITH_PERFSCHEMA_STORAGE_ENGINE=0'
    
    Reviewed-by: Jimmy Yang <jimmy.yang@oracle.com>

--- storage/innobase/include/fts0tokenize.h
+++ storage/innobase/include/fts0tokenize.h
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
-Copyright (c) 2014, Oracle and/or its affiliates. All Rights Reserved.
+Copyright (c) 2014, 2015, Oracle and/or its affiliates. All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -25,6 +25,7 @@ Created 2014/11/17 Shaohua Wang
 
 #include "ha_prototypes.h"
 #include "ft_global.h"
+#include "m_ctype.h"
 
 /* Macros and structs below are from ftdefs.h in MyISAM */
 /** Check a char is true word */
