cebaf2d4defbaa0a13602b14ddc5f0130c88c91a
Tor Didriksen <tor.didriksen@oracle.com>
2015-01-07 15:34:40 +0100

    Bug#20317639 REMOVE #INCLUDE OF SET_VAR.H
    After recent cleanup of header dependencies, we can remove even more
    includes of set_var.h

--- sql/parse_tree_helpers.cc
+++ sql/parse_tree_helpers.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013, 2014, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2013, 2015, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -17,7 +17,6 @@
 #include "parse_tree_helpers.h"
 
 #include "sql_class.h"
-#include "set_var.h"
 #include "sp_head.h"
 #include "sp_instr.h"
 #include "auth/auth_common.h"
