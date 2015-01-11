b0d9582e273219e2a0260deee943de5225d16c43
Craig L Russell <Craig.Russell@oracle.com>
2015-01-11 09:48:44 -0800

    Improve garbage collection of clusterj artifacts
    A user object obtained via query or session.newInstance is a proxy that
    holds a strong reference to an NdbRecordSmartValueHandlerImpl, which 
    in turn holds a strong reference to an NdbRecordOperationImpl, which 
    holds strong references to ByteBuffers which wrap native byte arrays. 
    
    When the user object goes out of scope, it is garbage collected but 
    the objects that are now unreferenced take a long time to be collected.
    
    This patch immediately releases objects that are referenced by user objects 
    when the user object is collected. This can improve the ability of the 
    garbage collector to release the space used by the objects.
    
    DynamicObject.java:
      implement finalize method for dynamically created objects
    
    DynamicObjectDelegate.java:
      add finalize to the methods to be implemented by the dynamic object
    
    DomainTypeHandlerImpl.java:
      require that the proxy (user object) implement finalize
    
    InvocationHandlerImpl.java:
      implement release
    
    KeyValueHandlerImpl.java:
      implement release
    
    NdbRecordBlobImpl.java:
      implement release() to release unneeded objects
    
    NdbRecordOperationImpl.java:
      implement release() to release unneeded objects
    
    NdbRecordSmartValueHandlerImpl.java:
      implement finalize() which forwards to release() to release unneeded objects
    
    NdbOpenJPAValueHandler.java:
      implement release
    
    NoPublicConstructorClusterConnectionService.java:
      fix bug in test

--- storage/ndb/clusterj/clusterj-core/src/main/java/com/mysql/clusterj/core/metadata/InvocationHandlerImpl.java
+++ storage/ndb/clusterj/clusterj-core/src/main/java/com/mysql/clusterj/core/metadata/InvocationHandlerImpl.java
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2010, 2012, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2010, 2015, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -86,6 +86,14 @@ public class InvocationHandlerImpl<T> implements InvocationHandler,
         properties = new Object[numberOfFields];
         modifiedFields = new BitSet(numberOfFields);
         domainTypeHandler.initializePrimitiveFields(this);
+    }
+
+    public void release() {
+        this.properties = null;
+        this.typemap = null;
+        this.domainTypeHandler = null;
+        this.proxy = null;
+        this.objectManager = null;
     }
 
     public void setProxy(Object proxy) {
