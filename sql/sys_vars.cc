50d903d54d841f6bfb0aefa8641e47d8eba6da24
Libing Song <libing.song@oracle.com>
2015-01-29 12:35:29 +0800

    BUG#20400518 SQL_SLAVE_SKIP_COUNTER WITH CHANNELS CAN BRING               INCONSISTENCY IN RPL
    When setting sql_slave_skip_counter, the value is applied to all
    channels. So when starting channels, all started appliers will
    go into skip process. And the appliers will decrease the global
    sql_slave_skip_counter after skipping a event. The logic is not
    correct and it will cause problem if two or more channels are
    starting at the same time.
    
    To fix it, the be haviour of skipping event is change to:
    - Setting sql_slave_skip_counter doesn't effect any running applier.
    - Value of sql_slave_skip_counter only effects the first applier
      starting after 'SET GLOBAL sql_slave_skip_counter.
    - The applier copies the value to a local variable and just decrease
      the local variable.
    - The value of sql_slave_skip_counter will be reset to 0 after
      the applier copies it. So if users need to skip some events
      on another channel, then they they need to set sql_slave_skip_counter
      again.

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -4543,13 +4543,7 @@ static bool check_slave_skip_counter(sys_var *self, THD *thd, set_var *var)
   /* slave_skip_counter becomes per channel in multisource replication*/
   mysql_mutex_lock(&LOCK_msr_map);
 
-  if (is_any_slave_channel_running(SLAVE_SQL))
-  {
-    my_message(ER_SLAVE_SQL_THREAD_MUST_STOP,
-               ER(ER_SLAVE_SQL_THREAD_MUST_STOP), MYF(0));
-    result= true;
-  }
-  if (gtid_mode == 3)
+  if (gtid_mode == GTID_MODE_ON)
   {
     my_message(ER_SQL_SLAVE_SKIP_COUNTER_NOT_SETTABLE_IN_GTID_MODE,
                ER(ER_SQL_SLAVE_SKIP_COUNTER_NOT_SETTABLE_IN_GTID_MODE),
@@ -4560,52 +4554,14 @@ static bool check_slave_skip_counter(sys_var *self, THD *thd, set_var *var)
   mysql_mutex_unlock(&LOCK_msr_map);
   return result;
 }
-static bool fix_slave_skip_counter(sys_var *self, THD *thd, enum_var_type type)
-{
-  Master_info *mi;
 
-  /* set for all replication channels in multisource replication */
-
-  /*
-   To understand the below two unlock statments, please see comments in
-    fix_slave_net_timeout function above
-   */
-  mysql_mutex_unlock(&LOCK_sql_slave_skip_counter);
-  mysql_mutex_unlock(&LOCK_global_system_variables);
-  mysql_mutex_lock(&LOCK_msr_map);
-
-  for (mi_map::iterator it = msr_map.begin(); it!=msr_map.end(); it++)
-  {
-    mi= it->second;
-    if (mi != NULL)
-    {
-      mysql_mutex_lock(&mi->rli->run_lock);
-      /*
-       The following test should normally never be true as we test this
-       in the check function;  To be safe against multiple
-       SQL_SLAVE_SKIP_COUNTER request, we do the check anyway
-      */
-      if (!mi->rli->slave_running)
-      {
-        mysql_mutex_lock(&mi->rli->data_lock);
-        mi->rli->slave_skip_counter= sql_slave_skip_counter;
-        mysql_mutex_unlock(&mi->rli->data_lock);
-      }
-      mysql_mutex_unlock(&mi->rli->run_lock);
-    }
-  }
-  mysql_mutex_unlock(&LOCK_msr_map);
-  mysql_mutex_lock(&LOCK_global_system_variables);
-  mysql_mutex_lock(&LOCK_sql_slave_skip_counter);
-  return 0;
-}
 static PolyLock_mutex PLock_sql_slave_skip_counter(&LOCK_sql_slave_skip_counter);
 static Sys_var_uint Sys_slave_skip_counter(
        "sql_slave_skip_counter", "sql_slave_skip_counter",
        GLOBAL_VAR(sql_slave_skip_counter), NO_CMD_LINE,
        VALID_RANGE(0, UINT_MAX), DEFAULT(0), BLOCK_SIZE(1),
        &PLock_sql_slave_skip_counter, NOT_IN_BINLOG,
-       ON_CHECK(check_slave_skip_counter), ON_UPDATE(fix_slave_skip_counter));
+       ON_CHECK(check_slave_skip_counter));
 
 static Sys_var_charptr Sys_slave_skip_errors(
        "slave_skip_errors", "Tells the slave thread to continue "
