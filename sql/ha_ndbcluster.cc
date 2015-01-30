1b67af79a49415f706c2715605645199c94ccc9a
Magnus Blåudd <magnus.blaudd@oracle.com>
2015-01-30 14:36:23 +0100

    Bug#20234994 JOINS ARE NOT PUSHED AFTER WL6042
     - The QEP was not properly modified to use the special
       'linked'(aka. pushed) read functions since the order of
       ha_make_pushed_join() and pick_table_access_method() had
       been changed.
     - Create a new Abstract_query_plan function which the handler
       implementation can call to modify the table access method
       used by QEP_TAB.
     - Modify ndbcluster_make_pushed_join() to call the new function
       after the pushed joins has been defined. Thus the
       'linked' read functions will be called when the QEP is executed.
     - Improve the comments describing ha_make_pushed_join() and the
       responsibility of the handler.
     - Remove double call to plan_is_const()
     - Add new ndbcluster.pushed_join test which checks that the
       integration of pushed join functionality works as expected
       when MySQL Server is compiled with the NDB storage engine.
    
    (cherry picked from commit 36056bc2d9c67c54fe85193e6f699226d76b54e2)

--- sql/ha_ndbcluster.cc
+++ sql/ha_ndbcluster.cc
@@ -15181,6 +15181,7 @@ int ndbcluster_make_pushed_join(handlerton *hton,
       // Check for online upgrade/downgrade.
       ndb_join_pushdown(g_ndb_cluster_connection->get_min_db_version()))
   {
+    bool pushed_something = false;
     ndb_pushed_builder_ctx pushed_builder(*plan);
 
     for (uint i= 0; i < plan->get_access_count()-1; i++)
@@ -15214,6 +15215,18 @@ int ndbcluster_make_pushed_join(handlerton *hton,
           handler->print_error(error, MYF(0));
           DBUG_RETURN(error);
         }
+        // Something was pushed and the QEP need to be modified
+        pushed_something = true;
+      }
+    }
+
+    if (pushed_something)
+    {
+      // Modify the QEP_TAB's to use the 'linked' read functions
+      // for those parts of the join which have been pushed down.
+      for (uint i= 0; i < plan->get_access_count(); i++)
+      {
+        plan->get_table_access(i)->set_pushed_table_access_method();
       }
     }
   }
