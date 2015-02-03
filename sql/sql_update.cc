b8362f8f51a90ceadc3e0e6f8063db090108f8ae
Roy Lyseng <roy.lyseng@oracle.com>
2015-02-03 10:46:07 +0100

    Bug#20460208: !table || (!table->read_set || bitmap_is_set)
    The optimizer relies on thd->mark_used_columns to have the value
    MARK_COLUMNS_READ for columns in WHERE conditions, etc.
    If this is not the case, fields are sometimes erroneously marked for
    write or not marked at all. In particular, if a failed statement of
    some "wrong" type preceded a correct statement, this problem might hit.
    In the bug report, a failed INSERT statement is followed by a DELETE
    statement.
    
    This is a quick fix that should provide safeguards for DELETE and
    UPDATE statements. It sets mark_used_columns to MARK_COLUMNS_READ
    when needed and restores the old value after use.
    
    A more thorough fix with complete safeguard will follow later.

--- sql/sql_update.cc
+++ sql/sql_update.cc
@@ -1112,6 +1112,8 @@ bool mysql_prepare_update(THD *thd, const TABLE_LIST *update_table_ref,
     DBUG_RETURN(true);
 
   thd->want_privilege= SELECT_ACL;
+  enum enum_mark_columns mark_used_columns_saved= thd->mark_used_columns;
+  thd->mark_used_columns= MARK_COLUMNS_READ;
 #ifndef NO_EMBEDDED_ACCESS_CHECKS
   table_list->set_want_privilege(SELECT_ACL);
 #endif
@@ -1159,6 +1161,8 @@ bool mysql_prepare_update(THD *thd, const TABLE_LIST *update_table_ref,
 
   if (setup_fields(thd, Ref_ptr_array(), lex->value_list, SELECT_ACL, 0, 0))
     DBUG_RETURN(true);                          /* purecov: inspected */
+
+  thd->mark_used_columns= mark_used_columns_saved;
 
   if (setup_ftfuncs(select))
     DBUG_RETURN(true);                          /* purecov: inspected */
