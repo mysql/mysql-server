0eaaa4e1456666641b2bc219d506cf1d5534e251
Nuno Carvalho <nuno.carvalho@oracle.com>
2015-02-03 22:09:01 +0000

    BUG#20455917: GROUP REPLICATION APPLIER CHANNEL IS NOT LISTED ON ALL REPLICATION P_S TABLES
    Group Replication applier channel was not being listed on all
    replication performance schema tables, what was causing some status
    values not to be accessible.
    This was happening because Group Replication applier channel doesn't
    have a regular Master_info object, since it doesn't have a IO
    thread. Applier data comes from group communication toolkit and not
    from a point-to-point connection from a master.
    
    On this patch we ensure that Group Replication applier channel is
    listed on all replication performance schema tables by adding the
    channel with a fake master hostname "<NULL>".

--- include/mysql/plugin_group_replication.h
+++ include/mysql/plugin_group_replication.h
@@ -27,12 +27,6 @@ enum enum_member_state {
   MEMBER_STATE_RECOVERING
 };
 
-enum enum_applier_status {
-  APPLIER_STATE_RUNNING= 1,
-  APPLIER_STATE_STOP,
-  APPLIER_STATE_ERROR
-};
-
 typedef struct st_group_replication_connection_status_info
 {
   char* channel_name;
@@ -103,7 +97,7 @@ struct st_mysql_group_replication
     @note The caller is responsible to free memory from the info structure and
           from all its fields.
   */
-  bool (*get_group_members_info)(uint index, GROUP_REPLICATION_GROUP_MEMBERS_INFO *info);
+  bool (*get_group_members_info)(unsigned int index, GROUP_REPLICATION_GROUP_MEMBERS_INFO *info);
 
   /*
     This function is used to fetch information for group replication members statistics.
@@ -118,7 +112,7 @@ struct st_mysql_group_replication
   /*
     Get number of group replication members.
   */
-  uint (*get_members_number_info)();
+  unsigned int (*get_members_number_info)();
 };
 
 #endif
