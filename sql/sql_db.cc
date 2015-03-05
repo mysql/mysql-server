049f1d42e58823fe5b05db6481ed349536cda8fd
Magnus Blåudd <magnus.blaudd@oracle.com>
2015-03-05 13:59:21 +0100

    Bug#20480035 REMOVE MCP_BUG44529
     - The mysqld participating in schema distribution
       of DROP DATABASE failed to remove the database
       when there was a .ndb file in the directory
       which didn't have any corresponding table in NDB.
     - The problem is generic and really not related to schema distribution
       participant, it can be reproduced with only one mysqld
     - Fix by adding an additional check for leftover .ndb
       files in ndbcluster's DROP DATABASE implementation. The
       fix is a rewrite and thus intentionally mimics the beaviour
       of find_db_tables_and_rm_known_files() which previously
       deleted these files.
     - Add testcase reproducing with one mysqld
     - Add testcase reproducing with two mysql(s), same test as for
       bug44529 but with comments.

--- sql/sql_db.cc
+++ sql/sql_db.cc
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2000, 2014, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2000, 2015, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -46,11 +46,7 @@
 
 #define MAX_DROP_TABLE_Q_LEN      1024
 
-#ifdef MCP_BUG44529
 const char *del_exts[]= {".frm", ".BAK", ".TMD", ".opt", ".OLD", NullS};
-#else
-const char *del_exts[]= {".frm", ".BAK", ".TMD", ".opt", ".OLD", ".ndb", NullS};
-#endif
 
 static TYPELIB deletable_extentions=
 {array_elements(del_exts)-1,"del_exts", del_exts, NULL};
