a6eed3a7fe61effa7f49cc3990adc44fba9c56a4
Venkatesh Duggirala <venkatesh.duggirala@oracle.com>
2015-03-10 12:32:15 +0530

    Bug#20574628: SEMI-SYNC REPLICATION PERFORMANCE DEGRADES WITH A HIGH NUMBER OF THREADS Problem: when semi-sync replication is enabled and when the number of threads running is increasing (going beyond a threshold), performance of the system is going drastically down.
    Analysis:
    A transaction in semi-sync replication waits for the ACK
    inside ReplSemiSyncMaster::commitTrx on a condition variable
    COND_binlog_send_. In reportReplyBinlog function, when server
    receives an ACK, it sends condition broadcast to all the
    threads that are waiting on COND_binlog_send_.
    
    Each transaction thread waiting an ACK does the follow (simplified):
    - hold a lock LOCK_binlog_
    - in a loop ( while (is_on()) ) :
      - compare the ACK position with its own position;
         - if the ACK is still behind, it waits on a condition variable COND_binlog_send_
         - if the ACK is ahead or equal, it exits this loop and
            proceeds with the next step
    - unlock the lock LOCK_binlog_
    
    The problem here with this design is that all the threads wake up and while it
    is likely that only few will return to the application and most of them will
    go back to wait on the same condition variable. This creates unnecessary context
    switches and a lot of LLC cache thrashing in multi-cpu systems
    which reduces the throughput of the system.
    
    Fix: The approach splits the *one* condvar used for all threads
    to wait into *several* condvars. Each of the condvars is associated
    with a position range. Thence only those that fall threads that are waiting
    for a given acknowledged range are awaken when the ACK comes back
    (as opposed to waking up all transactions, regardless whether the ACK affects
    them or not).

--- plugin/semisync/semisync_master.h
+++ plugin/semisync/semisync_master.h
@@ -1,6 +1,5 @@
 /* Copyright (C) 2007 Google Inc.
-   Copyright (c) 2008 MySQL AB, 2009 Sun Microsystems, Inc.
-   Use is subject to license terms.
+   Copyright (c) 2008, 2015, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -30,7 +29,9 @@ extern PSI_stage_info stage_waiting_for_semi_sync_ack_from_slave;
 
 struct TranxNode {
   char             log_name_[FN_REFLEN];
-  my_off_t          log_pos_;
+  my_off_t         log_pos_;
+  mysql_cond_t     cond;
+  int              n_waiters;
   struct TranxNode *next_;            /* the next node in the sorted list */
   struct TranxNode *hash_next_;    /* the next node during hash collision */
 };
@@ -128,6 +129,7 @@ public:
     trx_node->log_pos_= 0;
     trx_node->next_= 0;
     trx_node->hash_next_= 0;
+    trx_node->n_waiters= 0;
     return trx_node;
   }
 
@@ -246,6 +248,12 @@ private:
       /* New Block is always the current_block */
       current_block= block;
       ++block_num;
+
+      for (int i=0; i< BLOCK_TRANX_NODES; i++)
+        mysql_cond_init(key_ss_cond_COND_binlog_send_,
+                        &current_block->nodes[i].cond,
+                        NULL);
+
       return 0;
     }
     return 1;
@@ -257,6 +265,8 @@ private:
    */
   void free_block(Block *block)
   {
+    for (int i=0; i< BLOCK_TRANX_NODES; i++)
+      mysql_cond_destroy(&block->nodes[i].cond);
     my_free(block);
     --block_num;
   }
@@ -330,6 +340,11 @@ private:
   }
 
 public:
+  int signal_waiting_sessions_all();
+  int signal_waiting_sessions_up_to(const char *log_file_name,
+                                    my_off_t log_file_pos);
+  TranxNode* find_active_tranx_node(const char *log_file_name,
+                                    my_off_t log_file_pos);
   ActiveTranx(mysql_mutex_t *lock, unsigned long trace_level);
   ~ActiveTranx();
 
@@ -375,11 +390,6 @@ class ReplSemiSyncMaster
 
   /* True when initObject has been called */
   bool init_done_;
-
-  /* This cond variable is signaled when enough binlog has been sent to slave,
-   * so that a waiting trx can return the 'ok' to the client for a commit.
-   */
-  mysql_cond_t  COND_binlog_send_;
 
   /* Mutex that protects the following state variables and the active
    * transaction list.
@@ -434,8 +444,6 @@ class ReplSemiSyncMaster
 
   void lock();
   void unlock();
-  void cond_broadcast();
-  int  cond_timewait(struct timespec *wait_time);
 
   /* Is semi-sync replication on? */
   bool is_on() {
