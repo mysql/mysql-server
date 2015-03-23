d2a041eea62b96530e49a053f2c18c54ad9be8c0
Libing Song <libing.song@oracle.com>
2015-03-23 14:25:18 +0800

    Bug#20464737 SEMI_SYNC: SLAVE IO FAILS WITH ER1236 'SLAVE HAS MORE              GTIDS THAN THE MASTER HAS'
    Slaves, especially semisync slaves, encountered ER1236 when
    connecting to their masters. When connecting to the master
    with COM_GTID_DUMP request, the slave sends all replicated
    gtids to master and master checks if all the gtids(only
    the gtids have master's uuid) are included in its gtid_executed.
    However, there is a chance that a gtid is already in slave's
    gtid_executed, but not in master's gtid_executed. Gtid is
    added into gtid_executed after it is binlogged, so it is
    possible that a transaction is replicated and applied on
    slave, but not committed on master yet.
    
    To fix it, master will check if all gtids are in the set of
    (gtid_executed, owned_gtids).

--- sql/rpl_binlog_sender.cc
+++ sql/rpl_binlog_sender.cc
@@ -679,9 +679,20 @@ int Binlog_sender::check_start_file()
     global_sid_lock->wrlock();
     const rpl_sid &server_sid= gtid_state->get_server_sid();
     rpl_sidno subset_sidno= slave_sid_map->sid_to_sidno(server_sid);
-    if (!m_exclude_gtid->is_subset_for_sid(gtid_state->get_executed_gtids(),
-                                                gtid_state->get_server_sidno(),
-                                                subset_sidno))
+    Gtid_set
+      gtid_executed_and_owned(gtid_state->get_executed_gtids()->get_sid_map());
+
+    // gtids = executed_gtids & owned_gtids
+    if (gtid_executed_and_owned.add_gtid_set(gtid_state->get_executed_gtids())
+        != RETURN_STATUS_OK)
+    {
+      DBUG_ASSERT(0);
+    }
+    gtid_state->get_owned_gtids()->get_gtids(gtid_executed_and_owned);
+
+    if (!m_exclude_gtid->is_subset_for_sid(&gtid_executed_and_owned,
+                                           gtid_state->get_server_sidno(),
+                                           subset_sidno))
     {
       errmsg= ER(ER_SLAVE_HAS_MORE_GTIDS_THAN_MASTER);
       global_sid_lock->unlock();
