406fc50ec89ec501c2ded5f7bd9dbe8004304d69
Tor Didriksen <tor.didriksen@oracle.com>
2015-03-24 10:37:54 +0100

    Bug#20756770 MYSQL SERVER DOESN'T COMPILE AGAINST OPENSSL 1.0.1M
    The #define in openssl/opensslv.h contains an extra space in 1.0.1M
    Fix the regexp that is used for parsing the version number.

--- cmake/ssl.cmake
+++ cmake/ssl.cmake
@@ -166,7 +166,7 @@ MACRO (MYSQL_CHECK_SSL)
     # Encoded as MNNFFPPS: major minor fix patch status
     FILE(STRINGS "${OPENSSL_INCLUDE_DIR}/openssl/opensslv.h"
       OPENSSL_VERSION_NUMBER
-      REGEX "^#define[\t ]+OPENSSL_VERSION_NUMBER[\t ]+0x[0-9].*"
+      REGEX "^#[ ]*define[\t ]+OPENSSL_VERSION_NUMBER[\t ]+0x[0-9].*"
     )
     STRING(REGEX REPLACE
       "^.*OPENSSL_VERSION_NUMBER[\t ]+0x([0-9]).*$" "\\1"
