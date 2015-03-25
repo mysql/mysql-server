6002d255b7a2b10b6e5e15658c0415d9224a719c
Annamalai Gurusami <annamalai.gurusami@oracle.com>
2015-03-25 14:03:02 +0530

    Bug #20713559 ASSERT !HAS_DONE_RESERVATION, FSEG_ALLOC_FREE_PAGE_LOW()
    Problem:
    
    When a BLOB is stored in InnoDB, there is calculation to determine
    the number of pages and then the number of extents.  Once the correct
    number of extents are calculated, then that many extents are reserved.
    But during the calculation of number of extents, we are not taking
    into account the compressed pages.
    
    Solution:
    
    Calculate the number of extents, by taking into account whether the
    pages are compressed or not.
    
    rb#8402 approved by Sunny.

--- storage/innobase/dict/dict0dict.cc
+++ storage/innobase/dict/dict0dict.cc
@@ -6906,3 +6906,52 @@ dict_tablespace_is_empty(
 	return(!found);
 }
 #endif /* !UNIV_HOTBACKUP */
+
+/** Determine the extent size (in pages) for the given table
+@param[in]	table	the table whose extent size is being
+			calculated.
+@return extent size in pages (256, 128 or 64) */
+ulint
+dict_table_extent_size(
+	const dict_table_t*	table)
+{
+	const ulint	mb_1 = 1024 * 1024;
+	const ulint	mb_2 = 2 * mb_1;
+	const ulint	mb_4 = 4 * mb_1;
+
+	page_size_t	page_size = dict_table_page_size(table);
+	ulint	pages_in_extent = FSP_EXTENT_SIZE;
+
+	if (page_size.is_compressed()) {
+
+		ulint	disk_page_size	= page_size.physical();
+
+		switch (disk_page_size) {
+		case 1024:
+			pages_in_extent = mb_1/1024;
+			break;
+		case 2048:
+			pages_in_extent = mb_1/2048;
+			break;
+		case 4096:
+			pages_in_extent = mb_1/4096;
+			break;
+		case 8192:
+			pages_in_extent = mb_1/8192;
+			break;
+		case 16384:
+			pages_in_extent = mb_1/16384;
+			break;
+		case 32768:
+			pages_in_extent = mb_2/32768;
+			break;
+		case 65536:
+			pages_in_extent = mb_4/65536;
+			break;
+		default:
+			ut_ad(0);
+		}
+	}
+
+	return(pages_in_extent);
+}
