c3a1ba54c295ee3afb15bfcb221aac4608364048
Mikael Ronström <mikael.ronstrom@oracle.com>
2015-04-14 16:15:02 +0200

    Fix ndbapi example programs to work on Mac OS X Yosemite

--- storage/ndb/ndbapi-examples/ndbapi_scan/ndbapi_scan.cpp
+++ storage/ndb/ndbapi-examples/ndbapi_scan/ndbapi_scan.cpp
@@ -1,6 +1,6 @@
 
 /*
-   Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2005, 2015, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -76,6 +76,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <config.h>
+#ifdef HAVE_SYS_SELECT_H
+#include <sys/select.h>
+#endif
 
 /**
  * Helper sleep function
