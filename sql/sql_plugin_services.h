c870e9eb8199fa6a41d544a00f96d0acd3a0e19e
Martin Hansson <martin.hansson@oracle.com>
2015-04-17 09:23:19 +0200

    Bug#20856729: QUERY REWRITE: WRONG IFDEF SYMBOL IN SERVICE_PARSER.H
    The #ifdef'd symbol MYSQL_DYNAMIC_PLUGIN was misspelled, causing the code
    using a struct mysql_parser_service_st to disappear. When this was fixed, it
    became evident that this code hadn't kept up with the development of the
    service API, the function mysql_parser_get_statement_type() was missing. The
    naming was also inconsistent, so it has now been changed to this:
    
    - The function pointers inside the struct have names starting with mysql_.
    - The free functions pointed to have names starting with mysql_parser_.
    
    The public API changes, but it was incorrect before.

--- sql/sql_plugin_services.h
+++ sql/sql_plugin_services.h
@@ -87,6 +87,7 @@ static struct mysql_parser_service_st parser_handler=
   mysql_parser_join_thread,
   mysql_parser_set_current_database,
   mysql_parser_parse,
+  mysql_parser_get_statement_type,
   mysql_parser_get_statement_digest,
   mysql_parser_get_number_params,
   mysql_parser_extract_prepared_params,
