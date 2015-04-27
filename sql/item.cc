5fd0088356090bed2375cb9a128d51d973dab3eb
Oleksandr Byelkin <sanja@mariadb.com>
2015-04-27 15:31:12 +0200

    MDEV-8058: funcs_1.innodb_views and funcs_1.memory_views fail
    The double call of ref used tables fixed.

--- sql/item.cc
+++ sql/item.cc
@@ -9806,12 +9806,14 @@ table_map Item_direct_view_ref::used_tables() const
     return OUTER_REF_TABLE_BIT;
 
   if (view->is_merged_derived() || view->merged || !view->table)
-    return ((*ref)->used_tables() ?
-            (*ref)->used_tables() :
+  {
+    table_map used= (*ref)->used_tables();
+    return (used ?
+            used :
             ((null_ref_table != NO_NULL_TABLE) ?
              null_ref_table->map :
              (table_map)0 ));
-
+  }
   return view->table->map;
 }
 
