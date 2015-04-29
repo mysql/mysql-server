9ef936eeae9c6d44fb1f0e421b8861509af71ebd
Mayank Prasad <mayank.prasad@oracle.com>
2015-04-29 10:55:09 +0530

    Bug #19887143 : THREAD/SQL/MAIN DOESN'T CHANGE STATE/INFO AFTER STARTUP
    Issue:
      State and Info of main thread was not changing. It was because of
      not resetting its state/info accordingly while doing some startup
      operation when mysql server starts.
    
    Fix:
      Reset the state/info for main thread so that it shows correct state.

--- sql/sql_table.cc
+++ sql/sql_table.cc
@@ -1730,6 +1730,7 @@ void execute_ddl_log_recovery()
   (void) mysql_file_delete(key_file_global_ddl_log, file_name, MYF(0));
   global_ddl_log.recovery_phase= FALSE;
   mysql_mutex_unlock(&LOCK_gdl);
+  thd->reset_query();
   delete thd;
   /* Remember that we don't have a THD */
   my_pthread_setspecific_ptr(THR_THD,  0);
