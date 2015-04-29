5b7889c6c5c6ac8ce9584da5b6252fecd20bae47
Marc Alff <marc.alff@oracle.com>
2015-04-29 23:58:43 +0200

    Bug#20963147 DECOUPLE DIGEST SIZE FROM SERVER AND PFS
    Before this fix, the server variable 'max_digest_size'
    was used in two places.
    
    1)
    
    During query parsing, the server collects the statement digest text,
    and store the digest in a buffer of size 'max_digest_size'.
    
    Only 1 digest text per session is needed at the same time,
    which limits the memory consumption.
    
    2)
    
    In the performance schema, the digests collected in 1) are
    copied in various tables, and the memory allocated to
    keep digest historical or aggregated data also depends on 'max_digest_size'.
    
    Many digests text per session are preserved at the same time,
    so that the total memory consumption is a multiple of 'max_digest_size',
    making this parameter sensitive.
    
    The problem is that for some deployments,
    namely when using either the firewall plugin or the query rewrite plugin,
    DBA typically need to have at the same time:
    - a big value for 1), to avoid truncation
    - a lower value for 2), to limit memory consumption,
      even if truncations can occur in the recorded performance schema data.
    
    The solution is to keep variable 'max_digest_size' for part 1),
    and create a separate variable 'performance_schema_max_digest_size' for part
    2), so that sizing for the firewall/query rewrite can be independent of the
    performance schema.

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -606,6 +606,14 @@ static Sys_var_long Sys_pfs_events_transactions_history_size(
        DEFAULT(PFS_AUTOSIZE_VALUE),
        BLOCK_SIZE(1), PFS_TRAILING_PROPERTIES);
 
+static Sys_var_long Sys_pfs_max_digest_length(
+       "performance_schema_max_digest_length",
+       "Maximum length considered for digest text, when stored in performance_schema tables.",
+       READ_ONLY GLOBAL_VAR(pfs_param.m_max_digest_length),
+       CMD_LINE(REQUIRED_ARG), VALID_RANGE(0, 1024 * 1024),
+       DEFAULT(1024),
+       BLOCK_SIZE(1), PFS_TRAILING_PROPERTIES);
+
 static Sys_var_long Sys_pfs_connect_attrs_size(
        "performance_schema_session_connect_attrs_size",
        "Size of session attribute string buffer per thread."
@@ -2248,14 +2256,7 @@ static Sys_var_long Sys_max_digest_length(
        READ_ONLY GLOBAL_VAR(max_digest_length),
        CMD_LINE(REQUIRED_ARG), VALID_RANGE(0, 1024 * 1024),
        DEFAULT(1024),
-       BLOCK_SIZE(1),
-       NO_MUTEX_GUARD,
-       NOT_IN_BINLOG,
-       ON_CHECK(0),
-       ON_UPDATE(0),
-       NULL,
-       /* max_digest_length is used as a sizing hint by the performance schema. */
-       sys_var::PARSE_EARLY);
+       BLOCK_SIZE(1));
 
 static bool check_max_delayed_threads(sys_var *self, THD *thd, set_var *var)
 {
