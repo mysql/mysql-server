4471c1eb1942cbaa77d0f34aac63e172e202d7a0
Krunal Bauskar <krunal.bauskar@oracle.com>
2015-05-11 08:55:25 +0530

    - Bug#21046781: WHILE TRUNCATE UNDO-TABLESPACE FILE COULD BE CLOSED IN BACKGROUND
      If number of undo-logs < undo-tablespaces then few trailing undo-tablespaces
      are not made active (and in turn are no open). "undo-truncate" logic was even
      considering these un-opened tablespaces for truncate which lead to an assert.
      Fixed by keeping track of number of active undo tablespaces.
    
      Reviewed by: Sunny Bains (sunny.bains@oracle.com)
      RB:8447

--- storage/innobase/srv/srv0srv.cc
+++ storage/innobase/srv/srv0srv.cc
@@ -105,10 +105,15 @@ char*	srv_data_home	= NULL;
 char*	srv_undo_dir = NULL;
 
 /** The number of tablespaces to use for rollback segments. */
-ulong	srv_undo_tablespaces = 8;
+ulong	srv_undo_tablespaces = 0;
 
 /** The number of UNDO tablespaces that are open and ready to use. */
-ulint	srv_undo_tablespaces_open = 8;
+ulint	srv_undo_tablespaces_open = 0;
+
+/** The number of UNDO tablespaces that are active (hosting some rollback
+segment). It is quite possible that some of the tablespaces doesn't host
+any of the rollback-segment based on configuration used. */
+ulint	srv_undo_tablespaces_active = 0;
 
 /* The number of rollback segments to use */
 ulong	srv_undo_logs = 1;
