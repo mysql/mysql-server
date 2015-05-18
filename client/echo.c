09d112d1aff37668008bacd8e6ca614120b3b4f8
master-vic <victor_neville@linux.com>
2015-05-18 12:16:25 -0400

    Update echo.c
    Adding an empty space for the first fprintf and removing the if statement. The purpose is to eliminate argc executions of the if statement and argc-1 calls of fprintf. The echo result will be the same as before but the produced machine code will have less instructions and less function calls for more performance.

--- client/echo.c
+++ client/echo.c
@@ -35,11 +35,8 @@ int main(int argc, char **argv)
 {
   int i;
   for (i= 1; i < argc; i++)
-  {
-    fprintf(stdout, "%s", argv[i]);
-    if (i < argc - 1)
-      fprintf(stdout, " ");
-  }
+    fprintf(stdout, "%s ", argv[i]);
+  
   fprintf(stdout, "\n");
   return 0;
 }
