4bce94d1871c663a4934973ee2fd22e3b2908697
Magnus Blåudd <magnus.blaudd@oracle.com>
2015-06-17 11:53:53 +0200

    Bug#21270190 REMOVE UNUSED AND DANGEROUS NDBHOST_GETHOSTNAME()
     - remove function NdbHost_GetHostName()
     - remove some old windows specific hacks in NdbHost.h
     - fix copyright headers
     - remove usage of NdbHost_GetHostName() from mainAsyncGenerator.cpp
       which doesn't really need to print the name of current host
       using a portability layer function.

--- storage/ndb/src/common/portlib/NdbHost.c
+++ storage/ndb/src/common/portlib/NdbHost.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2015, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -18,13 +18,6 @@
 
 #include <ndb_global.h>
 #include "NdbHost.h"
-
-int NdbHost_GetHostName(char* buf)
-{
-  if(gethostname(buf, MAXHOSTNAMELEN))
-    return -1;
-  return 0;
-}
 
 int NdbHost_GetProcessId(void)
 {
