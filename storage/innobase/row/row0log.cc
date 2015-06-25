4f5ba3aff0702ca29b93f685243f819d5a8997c6
Michal Hrusecky <mhrusecky@suse.com>
2015-06-25 10:33:07 +0200

    Rename srv_buf_size variable bug#70047
    Variable was probably renamed sometime in the past but error was not triggered
    because ifdef was not satisfied.

--- storage/innobase/row/row0log.cc
+++ storage/innobase/row/row0log.cc
@@ -2684,7 +2684,7 @@ all_done:
 		and be ignored when the operation is unsupported. */
 		fallocate(index->online_log->fd,
 			  FALLOC_FL_PUNCH_HOLE | FALLOC_FL_KEEP_SIZE,
-			  ofs, srv_buf_size);
+			  ofs, srv_sort_buf_size);
 #endif /* FALLOC_FL_PUNCH_HOLE */
 
 		next_mrec = index->online_log->head.block;
@@ -3521,7 +3521,7 @@ all_done:
 		and be ignored when the operation is unsupported. */
 		fallocate(index->online_log->fd,
 			  FALLOC_FL_PUNCH_HOLE | FALLOC_FL_KEEP_SIZE,
-			  ofs, srv_buf_size);
+			  ofs, srv_sort_buf_size);
 #endif /* FALLOC_FL_PUNCH_HOLE */
 
 		next_mrec = index->online_log->head.block;
