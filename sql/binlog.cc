36a27833ddfc85696e63d3126dfa44706ed544ae
Libing Song <libing.song@oracle.com>
2015-06-29 15:02:05 +0800

    BUG#19641963 RPL.RPL_SEMI_SYNC_GROUP_COMMIT_DEADLOCK FAILS WITH              ASSERT IN MTS CODE
    It was an sporadically assertion failure on:
    lwm_estim + 1 == rli->gaq->get_job_group(rli->gaq->entry)->sequence_number);
    
    In slave_worker_ends_group(), mts_submode->min_waited_timestamp is checked.
    if it is not SEQ_UNINIT then the worker should call get_lwm_timestamp and
    signal coordinator. But the checking is not protected by mts_gaq_LOCK,
    coordinator's status may already changed when the worker calls get_lwm_timestamp.
    And the status should not appear when get_lwm_timestamp is being called.
    
    To fix it, mts_submode->min_waited_timestamp is checked again after the
    worker gets mts_gaq_LOCK.

--- sql/binlog.cc
+++ sql/binlog.cc
@@ -10843,6 +10843,7 @@ inline int64 Logical_clock::get_timestamp()
 inline int64 Logical_clock::step()
 {
   compile_time_assert(SEQ_UNINIT == 0);
+  DBUG_EXECUTE_IF("logical_clock_step_2", ++state;);
   return ++state;
 }
 
