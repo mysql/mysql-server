cc4a4d8b3323a950ac5442a18678484301902927
Abhishek Ranjan <abhishek.ar.ranjan@oracle.com>
2015-06-29 13:53:30 +0530

    Bug#20444737 STRING::CHOP ASSERTS ON NAUGHTY TABLE NAMES
    DESCRIPTION:
    
    This is a regression from the fix of Bug#16066637.
    Bug#16066637 introduced a character set conversion while writing
    to binlog. While calculating the length of the converted string,
    system default charset was being used instead of the new charset.
    This led to assert in debug build and crash in asan release build.
    
    FIX :
    
    Use the new charset to which string is converted to calculate the
    length of the string.

--- sql/sql_show.cc
+++ sql/sql_show.cc
@@ -1156,68 +1156,14 @@ static const char *require_quotes(const char *name, size_t name_length)
 }
 
 
-/*
-  Quote the given identifier if needed and append it to the target string.
-  If the given identifier is empty, it will be quoted.
-
-  SYNOPSIS
-  append_identifier()
-  thd                   thread handler
-  packet                target string
-  name                  the identifier to be appended
-  name_length           length of the appending identifier
-*/
-
-void
-append_identifier(THD *thd, String *packet, const char *name, size_t length)
-{
-  const char *name_end;
-  char quote_char;
-  int q;
-  q= thd ? get_quote_char_for_identifier(thd, name, length) : '`';
-
-  if (q == EOF)
-  {
-    packet->append(name, length, packet->charset());
-    return;
-  }
-
-  /*
-    The identifier must be quoted as it includes a quote character or
-   it's a keyword
-  */
-
-  (void) packet->reserve(length*2 + 2);
-  quote_char= (char) q;
-  packet->append(&quote_char, 1, system_charset_info);
-
-  for (name_end= name+length ; name < name_end ; name+= length)
-  {
-    uchar chr= (uchar) *name;
-    length= my_mbcharlen(system_charset_info, chr);
-    /*
-      my_mbcharlen can return 0 on a wrong multibyte
-      sequence. It is possible when upgrading from 4.0,
-      and identifier contains some accented characters.
-      The manual says it does not work. So we'll just
-      change length to 1 not to hang in the endless loop.
-    */
-    if (!length)
-      length= 1;
-    if (length == 1 && chr == (uchar) quote_char)
-      packet->append(&quote_char, 1, system_charset_info);
-    packet->append(name, length, system_charset_info);
-  }
-  packet->append(&quote_char, 1, system_charset_info);
-}
-
 /**
-  Convert the given identifier into to_charset if needed
-  and append it to target string.
-  @param thd                   thread handler
-  @param packet                target string
-  @param name                  the identifier to be appended
-  @param length                length of the input identifier
+  Convert and quote the given identifier if needed and append it to the
+  target string. If the given identifier is empty, it will be quoted.
+
+  @thd                         thread handler
+  @packet                      target string
+  @name                        the identifier to be appended
+  @length                      length of the appending identifier
   @param from_cs               Charset information about the input string
   @param to_cs                 Charset information about the target string
 */
@@ -1226,10 +1172,61 @@ void
 append_identifier(THD *thd, String *packet, const char *name, size_t length,
                   const CHARSET_INFO *from_cs, const CHARSET_INFO *to_cs)
 {
-        String to_name(name,length, from_cs);
-        if (from_cs != to_cs)
-            thd->convert_string(&to_name, from_cs, to_cs);
-        append_identifier(thd, packet, to_name.c_ptr(), to_name.length());
+  const char *name_end;
+  char quote_char;
+  int q;
+
+  const CHARSET_INFO *cs_info= system_charset_info;
+  const char *to_name= name;
+  size_t to_length= length;
+  String to_string(name,length, from_cs);
+
+  if (from_cs != NULL && to_cs != NULL && from_cs != to_cs)
+    thd->convert_string(&to_string, from_cs, to_cs);
+
+  if (to_cs != NULL)
+  {
+    to_name= to_string.c_ptr();
+    to_length= to_string.length();
+    cs_info= to_cs;
+  }
+
+  q= thd != NULL ? get_quote_char_for_identifier(thd, to_name, to_length) :
+                   '`';
+
+  if (q == EOF)
+  {
+    packet->append(to_name, to_length, packet->charset());
+    return;
+  }
+
+  /*
+    The identifier must be quoted as it includes a quote character or
+   it's a keyword
+  */
+
+  (void) packet->reserve(to_length*2 + 2);
+  quote_char= (char) q;
+  packet->append(&quote_char, 1, system_charset_info);
+
+  for (name_end= to_name+to_length ; to_name < name_end ; to_name+= to_length)
+  {
+    uchar chr= static_cast<uchar>(*to_name);
+    to_length= my_mbcharlen(cs_info, chr);
+    /*
+      my_mbcharlen can return 0 on a wrong multibyte
+      sequence. It is possible when upgrading from 4.0,
+      and identifier contains some accented characters.
+      The manual says it does not work. So we'll just
+      change length to 1 not to hang in the endless loop.
+    */
+    if (!to_length)
+      to_length= 1;
+    if (to_length == 1 && chr == static_cast<uchar>(quote_char))
+      packet->append(&quote_char, 1, system_charset_info);
+    packet->append(to_name, to_length, system_charset_info);
+  }
+  packet->append(&quote_char, 1, system_charset_info);
 }
 
 /*
