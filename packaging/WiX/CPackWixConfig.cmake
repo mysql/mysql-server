8d34e8018fea2658076591f65d6f9e584e3ca3ca
Sreedhar Sreedhargadda <sreedhar.sreedhargadda@oracle.com>
2015-07-14 16:48:06 +0200

    Fix to resolve msi build issue ENDIF is missing

--- packaging/WiX/CPackWixConfig.cmake
+++ packaging/WiX/CPackWixConfig.cmake
@@ -30,7 +30,8 @@ ELSE()
     SET(CPACK_COMPONENTS_USED
       "${CPACK_COMPONENTS_USED};ClusterTools;ClusterDataNode;ClusterManagementServer;ClusterManagementClient;ClusterJ;nodejs")
   ENDIF()
-ENDIF()
+ENDIF
+
 
 # Some components like Embedded are optional
 # We will build MSI without embedded if it was not selected for build
