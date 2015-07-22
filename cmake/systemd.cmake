57990d5f39183f63c9b2932f2bddd255e7c15edf
Erlend Dahl <erlend.dahl@oracle.com>
2015-07-22 13:50:45 +0200

    BUG#21483948 - INCOMPLETE GPL COPYRIGHT IN FILES
    Approved by Balasubramanian Kandasamy  <balasubramanian.kandasamy@oracle.com> over IM.

--- cmake/systemd.cmake
+++ cmake/systemd.cmake
@@ -9,6 +9,9 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 # systemd support files shall be installed on linux if the build host machine
 # has systemd package installed.  make install will install the systemd
