24d9c45cd8dc2bb2556ae244bba91fea67dc39ba
Martin Hansson <martin.hansson@oracle.com>
2015-08-21 11:01:37 +0200

    Bug#21625929: ASSERTION FAILED: FALSE IN CREATE_TMP_FIELD WITH UNKNOWN OUTER FIELD
    Some assertions in sql_tmp_table.cc were added in a merge
    from 5.6. They ruled out the possibility that a temporary
    table column could be created on the basis of a prepared
    statement parameter.
    
    Fixed by reverting the behavior back to 5.6 in this
    particular case: I.e. not creating columns for parameters
    during PREPARE. During EXECUTE we don't have this problem,
    obviously, since at that time the parameters have been
    substituted by constant expressions.

--- sql/sql_tmp_table.cc
+++ sql/sql_tmp_table.cc
@@ -242,10 +242,10 @@ static Field *create_tmp_field_for_schema(THD *thd, Item *item, TABLE *table)
                        If modify_item is 0 then fill_record() will update
                        the temporary table
 
-  @retval
-    NULL		on error
-  @retval
-    new_created field
+  @retval NULL On error. This also happens if the item is a prepared statement
+  parameter.
+
+  @retval new_created field
 */
 
 Field *create_tmp_field(THD *thd, TABLE *table,Item *item, Item::Type type,
@@ -390,6 +390,8 @@ Field *create_tmp_field(THD *thd, TABLE *table,Item *item, Item::Type type,
       break;
     result->set_derivation(item->collation.derivation);
     break;
+  case Item::PARAM_ITEM:
+    return NULL;
   default:					// Dosen't have to be stored
     DBUG_ASSERT(false);
     break;
@@ -954,6 +956,8 @@ create_tmp_table(THD *thd, Temp_table_param *param, List<Item> &fields,
 
       if (!new_field)
       {
+        if (type == Item::PARAM_ITEM)
+          goto update_hidden;
         DBUG_ASSERT(thd->is_fatal_error);
         goto err;				// Got OOM
       }
