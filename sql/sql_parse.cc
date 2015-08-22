42ea35f1a216e18a5ad288d51c2869c0d1d3bcfe
Nisha Gopalakrishnan <nisha.gopalakrishnan@oracle.com>
2015-08-22 18:26:28 +0530

    BUG#21473602: REDUNDANT CODE IN THD::RESET_FOR_NEXT_COMMAND()
    Note
    ====
    This a code cleanup to remove the redundant lines of
    code to reset the binary log position in the function
    'THD::reset_for_next_command()'.

--- sql/sql_parse.cc
+++ sql/sql_parse.cc
@@ -6057,9 +6057,6 @@ void THD::reset_for_next_command()
   thd->reset_current_stmt_binlog_format_row();
   thd->binlog_unsafe_warning_flags= 0;
 
-  thd->m_trans_end_pos= 0;
-  thd->m_trans_log_file= NULL;
-  thd->m_trans_fixed_log_file= NULL;
   thd->commit_error= THD::CE_NONE;
   thd->durability_property= HA_REGULAR_DURABILITY;
   thd->set_trans_pos(NULL, 0);
