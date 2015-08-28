db041d1a5546e88c9d16f0160231f2713dc91ad6
V S Murthy Sidagam <venkata.sidagam@oracle.com>
2015-08-28 17:47:11 +0530

    Bug#11757169: MyISAM share list scalability problems
    As the number of open tables is increased, table lookup
    (testing if a table is already open) and in particular
    the case when a table is not open, became increasingly more
    expensive.
    
    The problem was caused by the open table lookup mechanism,
    which was based on traversing a linked list comparing the
    file names.
    
    Fixed by storing a pointer to the MYISAM_SHARE on
    the designated handler share area, so it could skip
    traversing the linked list on open.
    And that there are additional changes for have_rtree,
    which are not related to the bug.
    
    Now with the patch we can see good performance improvement
    in SELECT on huge number of tables for bigger values for
    TABLE_OPEN_CACHE and TABLE_DEFINITION_CACHE.

--- storage/myisam/mi_open.c
+++ storage/myisam/mi_open.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2000, 2014, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2000, 2015, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -77,21 +77,22 @@ MI_INFO *test_if_reopen(char *filename)
   have an open count of 0.
 ******************************************************************************/
 
-MI_INFO *mi_open(const char *name, int mode, uint open_flags)
+MI_INFO *mi_open_share(const char *name, MYISAM_SHARE *old_share, int mode,
+                       uint open_flags)
 {
-  int lock_error,kfile,open_mode,save_errno,have_rtree=0, realpath_err;
+  int lock_error,kfile,open_mode,save_errno, realpath_err;
   uint i,j,len,errpos,head_length,base_pos,offset,info_length,keys,
     key_parts,unique_key_parts,fulltext_keys,uniques;
   uint internal_table= open_flags & HA_OPEN_INTERNAL_TABLE;
   char name_buff[FN_REFLEN], org_name[FN_REFLEN], index_name[FN_REFLEN],
        data_name[FN_REFLEN];
   uchar *disk_cache, *disk_pos, *end_pos;
-  MI_INFO info, *m_info, *old_info= NULL;
+  MI_INFO info, *m_info;
   MYISAM_SHARE share_buff,*share;
   ulong rec_per_key_part[HA_MAX_POSSIBLE_KEY*MI_MAX_KEY_SEG];
   my_off_t key_root[HA_MAX_POSSIBLE_KEY],key_del[MI_MAX_KEY_BLOCK_SIZE];
   ulonglong max_key_file_length, max_data_file_length;
-  DBUG_ENTER("mi_open");
+  DBUG_ENTER("mi_open_share");
 
   m_info= NULL;
   kfile= -1;
@@ -112,10 +113,15 @@ MI_INFO *mi_open(const char *name, int mode, uint open_flags)
   if (!internal_table)
   {
     mysql_mutex_lock(&THR_LOCK_myisam);
-    old_info= test_if_reopen(name_buff);
+    if (!old_share && ! (open_flags & HA_OPEN_FROM_SQL_LAYER))
+    {
+      MI_INFO *old_info= test_if_reopen(name_buff);
+      if (old_info)
+        old_share= old_info->s;
+    }
   }
 
-  if (!old_info)
+  if (!old_share)
   {
     share= &share_buff;
     memset(&share_buff, 0, sizeof(share_buff));
@@ -335,7 +341,7 @@ MI_INFO *mi_open(const char *name, int mode, uint open_flags)
         disk_pos_assert(disk_pos + share->keyinfo[i].keysegs * HA_KEYSEG_SIZE,
  			end_pos);
         if (share->keyinfo[i].key_alg == HA_KEY_ALG_RTREE)
-          have_rtree=1;
+          share->have_rtree=1;
 	set_if_smaller(share->blocksize,share->keyinfo[i].block_length);
 	share->keyinfo[i].seg=pos;
 	for (j=0 ; j < share->keyinfo[i].keysegs; j++,pos++)
@@ -530,7 +536,7 @@ MI_INFO *mi_open(const char *name, int mode, uint open_flags)
 			   HA_OPTION_COMPRESS_RECORD |
 			   HA_OPTION_TEMP_COMPRESS_RECORD)) ||
 	 (open_flags & HA_OPEN_TMP_TABLE) ||
-	 have_rtree) ? 0 : 1;
+	 share->have_rtree) ? 0 : 1;
       if (share->concurrent_insert)
       {
 	share->lock.get_status=mi_get_status;
@@ -551,16 +557,15 @@ MI_INFO *mi_open(const char *name, int mode, uint open_flags)
   }
   else
   {
-    share= old_info->s;
+    share= old_share;
     if (mode == O_RDWR && share->mode == O_RDONLY)
     {
       my_errno=EACCES;				/* Can't open in write mode */
       goto err;
     }
-    if (mi_open_datafile(&info, share, name, old_info->dfile))
+    if (mi_open_datafile(&info, share, name, -1))
       goto err;
     errpos=5;
-    have_rtree= old_info->rtree_recursion_state != NULL;
   }
 
   /* alloc and set up private structure parts */
@@ -574,12 +579,12 @@ MI_INFO *mi_open(const char *name, int mode, uint open_flags)
                        &info.rnext_same_key, share->base.max_key_length,
 		       &info.first_mbr_key, share->base.max_key_length,
 		       &info.filename,strlen(name)+1,
-		       &info.rtree_recursion_state,have_rtree ? 1024 : 0,
+		       &info.rtree_recursion_state,share->have_rtree ? 1024 : 0,
 		       NullS))
     goto err;
   errpos=6;
 
-  if (!have_rtree)
+  if (!share->have_rtree)
     info.rtree_recursion_state= NULL;
 
   my_stpcpy(info.filename,name);
@@ -673,7 +678,7 @@ err:
     /* fall through */
   case 5:
     (void) mysql_file_close(info.dfile, MYF(0));
-    if (old_info)
+    if (old_share)
       break;					/* Don't remove open table */
     /* fall through */
   case 4:
@@ -696,7 +701,7 @@ err:
     mysql_mutex_unlock(&THR_LOCK_myisam);
   my_errno=save_errno;
   DBUG_RETURN (NULL);
-} /* mi_open */
+} /* mi_open_share */
 
 
 uchar *mi_alloc_rec_buff(MI_INFO *info, ulong length, uchar **buf)
