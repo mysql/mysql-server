f5bbcb19559e849aeb141255a4bd6a940d93dcc4
Marko Mäkelä <marko.makela@oracle.com>
2015-09-10 13:04:48 +0300

    Bug#21801423 INNODB REDO LOG DOES NOT INDICATE WHEN FILES ARE CREATED
    As part of WL#7142 Simplify tablespace discovery during crash recovery,
    InnoDB removed code to deal with the MLOG_FILE_CREATE2 redo log record.
    
    The thinking was that external backup tools could parse MLOG_FILE_NAME
    records to discover created files. However, MLOG_FILE_NAME usually gets
    emitted for something else than file creation, and the MLOG_FILE_CREATE2
    record included some flags that are not present in the MLOG_FILE_NAME
    records.
    
    So, it seems that we must bring back the record, for the benefit of
    external backup tools that parse the redo log.
    
    fil_op_write_log(): Add the parameter "flags" for writing MLOG_FILE_CREATE2.
    
    fil_ibd_create(): Write a MLOG_FILE_CREATE2 record before MLOG_FILE_NAME.
    
    fil_name_parse(): Parse but ignore the MLOG_FILE_CREATE2 record.
    
    RB: 10180
    Reviewed-by: Shaohua Wang <shaohua.wang@oracle.com>
    Reviewed-by: Debarun Banerjee <debarun.banerjee@oracle.com>

--- storage/innobase/log/log0recv.cc
+++ storage/innobase/log/log0recv.cc
@@ -323,8 +323,8 @@ fil_name_process(
 @param[in]	end		end of the redo log buffer
 @param[in]	space_id	the tablespace ID
 @param[in]	first_page_no	first page number in the file
-@param[in]	type		MLOG_FILE_NAME or MLOG_FILE_RENAME2
-or MLOG_FILE_DELETE
+@param[in]	type		MLOG_FILE_NAME or MLOG_FILE_DELETE
+or MLOG_FILE_CREATE2 or MLOG_FILE_RENAME2
 @param[in]	apply		whether to apply the record
 @return pointer to next redo log record
 @retval NULL if this log record was truncated */
@@ -338,6 +338,20 @@ fil_name_parse(
 	mlog_id_t	type,
 	bool		apply)
 {
+#ifdef UNIV_HOTBACKUP
+	ulint		flags	= 0;
+#endif /* UNIV_HOTBACKUP */
+
+	if (type == MLOG_FILE_CREATE2) {
+		if (end < ptr + 4) {
+			return(NULL);
+		}
+#ifdef UNIV_HOTBACKUP
+		flags = mach_read_from_4(ptr);
+#endif /* UNIV_HOTBACKUP */
+		ptr += 4;
+	}
+
 	if (end < ptr + 2) {
 		return(NULL);
 	}
@@ -384,6 +398,11 @@ fil_name_parse(
 				space_id, BUF_REMOVE_FLUSH_NO_WRITE);
 			ut_a(err == DB_SUCCESS);
 		}
+#endif /* UNIV_HOTBACKUP */
+		break;
+	case MLOG_FILE_CREATE2:
+#ifdef UNIV_HOTBACKUP
+		/* if needed, invoke fil_ibd_create() with flags */
 #endif /* UNIV_HOTBACKUP */
 		break;
 	case MLOG_FILE_RENAME2:
@@ -1241,6 +1260,7 @@ recv_parse_or_apply_log_rec_body(
 	switch (type) {
 	case MLOG_FILE_NAME:
 	case MLOG_FILE_DELETE:
+	case MLOG_FILE_CREATE2:
 	case MLOG_FILE_RENAME2:
 		ut_ad(block == NULL);
 		/* Collect the file names when parsing the log,
@@ -1703,6 +1723,7 @@ recv_add_to_hash_table(
 	recv_addr_t*	recv_addr;
 
 	ut_ad(type != MLOG_FILE_DELETE);
+	ut_ad(type != MLOG_FILE_CREATE2);
 	ut_ad(type != MLOG_FILE_RENAME2);
 	ut_ad(type != MLOG_FILE_NAME);
 	ut_ad(type != MLOG_DUMMY_RECORD);
@@ -2740,8 +2761,9 @@ loop:
 			}
 			break;
 		case MLOG_FILE_NAME:
-		case MLOG_FILE_RENAME2:
 		case MLOG_FILE_DELETE:
+		case MLOG_FILE_CREATE2:
+		case MLOG_FILE_RENAME2:
 		case MLOG_TRUNCATE:
 			/* These were already handled by
 			recv_parse_log_rec() and
@@ -2891,8 +2913,9 @@ loop:
 				break;
 #endif /* UNIV_LOG_LSN_DEBUG */
 			case MLOG_FILE_NAME:
-			case MLOG_FILE_RENAME2:
 			case MLOG_FILE_DELETE:
+			case MLOG_FILE_CREATE2:
+			case MLOG_FILE_RENAME2:
 			case MLOG_INDEX_LOAD:
 			case MLOG_TRUNCATE:
 				/* These were already handled by
@@ -4149,6 +4172,9 @@ get_mlog_string(mlog_id_t type)
 
 	case MLOG_COMP_PAGE_REORGANIZE:
 		return("MLOG_COMP_PAGE_REORGANIZE");
+
+	case MLOG_FILE_CREATE2:
+		return("MLOG_FILE_CREATE2");
 
 	case MLOG_ZIP_WRITE_NODE_PTR:
 		return("MLOG_ZIP_WRITE_NODE_PTR");
