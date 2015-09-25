b4326582831c538119d3a149f6e3aca10b45a60c
Jimmy Yang <jimmy.yang@oracle.com>
2015-09-25 23:52:16 +0800

    Bug #21869656 	UNDO LOG DOES NOT CONTAIN ENOUGH INFORMATION ON INDEXED VIRTUAL COLUMNS
    Reviewed-by: Marko Makela <marko.makela@oracle.com>

--- storage/innobase/handler/handler0alter.cc
+++ storage/innobase/handler/handler0alter.cc
@@ -3424,6 +3424,8 @@ prepare_inplace_add_virtual(
 		ctx->add_vcol[j].v_pos = ctx->old_table->n_v_cols
 					 - ctx->num_to_drop_vcol + j;
 
+		/* No need to track the list */
+		ctx->add_vcol[j].v_indexes = NULL;
 		innodb_base_col_setup(ctx->old_table, field, &ctx->add_vcol[j]);
 		j++;
 	}
@@ -7144,57 +7146,83 @@ commit_cache_rebuild(
 
 /** Store the column number of the columns in a list belonging
 to indexes which are not being dropped.
-@param[in]      ctx             In-place ALTER TABLE context
-@param[in, out] drop_col_list   list which will be set, containing columns
-				which is part of index being dropped */
+@param[in]	ctx		In-place ALTER TABLE context
+@param[in, out]	drop_col_list	list which will be set, containing columns
+				which is part of index being dropped
+@param[in, out]	drop_v_col_list	list which will be set, containing
+				virtual columns which is part of index
+				being dropped */
 static
 void
 get_col_list_to_be_dropped(
-        ha_innobase_inplace_ctx*        ctx,
-        std::set<ulint>&                drop_col_list)
+	const ha_innobase_inplace_ctx*	ctx,
+	std::set<ulint>&		drop_col_list,
+	std::set<ulint>&		drop_v_col_list)
 {
-        for (ulint index_count = 0; index_count < ctx->num_to_drop_index;
-             index_count++) {
-                dict_index_t* index = ctx->drop_index[index_count];
+	for (ulint index_count = 0; index_count < ctx->num_to_drop_index;
+	     index_count++) {
+		const dict_index_t* index = ctx->drop_index[index_count];
 
-                for (ulint col = 0; col < index->n_user_defined_cols; col++) {
-                        ulint col_no = dict_index_get_nth_col_no(index, col);
-                        drop_col_list.insert(col_no);
-                }
-        }
+		for (ulint col = 0; col < index->n_user_defined_cols; col++) {
+			const dict_col_t*	idx_col
+				= dict_index_get_nth_col(index, col);
+
+			if (dict_col_is_virtual(idx_col)) {
+				const dict_v_col_t*	v_col
+					= reinterpret_cast<
+						const dict_v_col_t*>(idx_col);
+				drop_v_col_list.insert(v_col->v_pos);
+
+			} else {
+				ulint col_no = dict_col_get_no(idx_col);
+				drop_col_list.insert(col_no);
+			}
+		}
+	}
 }
 
 /** For each column, which is part of an index which is not going to be
 dropped, it checks if the column number of the column is same as col_no
 argument passed.
-@param[in]      table   table object
-@param[in]      col_no  column number of the column which is to be checked
+@param[in]	table	table object
+@param[in]	col_no	column number of the column which is to be checked
+@param[in]	is_v	if this is a virtual column
 @retval true column exists
 @retval false column does not exist. */
 static
 bool
 check_col_exists_in_indexes(
-        const dict_table_t*	table,
-        ulint			col_no)
+	const dict_table_t*	table,
+	ulint			col_no,
+	bool			is_v)
 {
-        for (dict_index_t* index = dict_table_get_first_index(table); index;
-             index = dict_table_get_next_index(index)) {
+	for (dict_index_t* index = dict_table_get_first_index(table); index;
+	     index = dict_table_get_next_index(index)) {
 
-                if (index->to_be_dropped) {
-                        continue;
-                }
+		if (index->to_be_dropped) {
+			continue;
+		}
 
-                for (ulint col = 0; col < index->n_user_defined_cols; col++) {
+		for (ulint i = 0; i < index->n_user_defined_cols; i++) {
+			const dict_col_t* idx_col
+				= dict_index_get_nth_col(index, i);
 
-                        ulint index_col_no = dict_index_get_nth_col_no(
-                                                index, col);
-                        if (col_no == index_col_no) {
-                                return(true);
-                        }
-                }
-        }
+			if (is_v && dict_col_is_virtual(idx_col)) {
+				const dict_v_col_t*   v_col = reinterpret_cast<
+					const dict_v_col_t*>(idx_col);
+				if (v_col->v_pos == col_no) {
+					return(true);
+				}
+			}
 
-        return(false);
+			if (!is_v && !dict_col_is_virtual(idx_col)
+			    && dict_col_get_no(idx_col) == col_no) {
+				return(true);
+			}
+		}
+	}
+
+	return(false);
 }
 
 /** Commit the changes made during prepare_inplace_alter_table()
@@ -7231,19 +7259,29 @@ commit_try_norebuild(
 		       == ha_alter_info->alter_info->drop_list.elements);
 
 
-        std::set<ulint> drop_list;
-        std::set<ulint>::iterator col_no;
+	std::set<ulint> drop_list;
+	std::set<ulint> v_drop_list;
+	std::set<ulint>::iterator col_no;
 
 	/* Check if the column, part of an index to be dropped is part of any
 	other index which is not being dropped. If it so, then set the ord_part
 	of the column to 0. */
-        get_col_list_to_be_dropped(ctx, drop_list);
+        get_col_list_to_be_dropped(ctx, drop_list, v_drop_list);
 
-        for(col_no = drop_list.begin(); col_no != drop_list.end(); ++col_no) {
-                if (!check_col_exists_in_indexes(ctx->new_table, *col_no)) {
+	for (col_no = drop_list.begin(); col_no != drop_list.end(); ++col_no) {
+		if (!check_col_exists_in_indexes(ctx->new_table,
+						 *col_no, false)) {
 			ctx->new_table->cols[*col_no].ord_part = 0;
+		}
+	}
+
+        for (col_no = v_drop_list.begin();
+	     col_no != v_drop_list.end(); ++col_no) {
+                if (!check_col_exists_in_indexes(ctx->new_table,
+						 *col_no, true)) {
+			ctx->new_table->v_cols[*col_no].m_col.ord_part = 0;
                 }
-        }
+	}
 
 	for (ulint i = 0; i < ctx->num_to_add_index; i++) {
 		dict_index_t*	index = ctx->add_index[i];
