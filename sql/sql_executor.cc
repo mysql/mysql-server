0b55ea24c4c8e7f5078106788d3ac819c280e92d
Lakshmi Narayanan Sreethar <lakshmi.narayanan.sreethar@oracle.com>
2015-10-07 19:01:26 +0530

    cherry picking and applying diffs from previously null-merged-in commits from earlier mysql-5.1.77 - cluster-7.1 merger. Following commits' 5.6 diffs were applied :
    aa2bf3a1f5114a39e2ee58e4454c12f651e6eb2f
    b7043a91aad5ee3753a10a46abb295a8bf159e83
    83cfb8e31274cb4d8e05ebb0ab114a46b8944ef0
    4052aa6a3e5d9783dc91fe593172fbc4ea8c7110

--- sql/sql_executor.cc
+++ sql/sql_executor.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2000, 2014, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2000, 2015, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -1602,6 +1602,10 @@ evaluate_join_record(JOIN *join, JOIN_TAB *join_tab)
       join->thd->get_stmt_da()->inc_current_row_for_warning();
       if (rc != NESTED_LOOP_OK)
         DBUG_RETURN(rc);
+
+      /* check for errors evaluating the condition */
+      if (join->thd->is_error())
+        DBUG_RETURN(NESTED_LOOP_ERROR);
 
       if (join_tab->do_loosescan() && join_tab->match_tab->found_match)
       {
