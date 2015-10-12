eec7055fa63b2df12da36c9491e505fa6eb657ea
Mithun C Y <mithun.c.y@oracle.com>
2015-10-12 16:34:49 +0530

    Bug #19918299: INVALID ACCESS OF FREED TMP TABLE DESCRIPTOR.
    Issue:
    ======
    In JOIN::create_intermediate_table we free tmp table
    descriptor because of some error while creating the tmp
    table. But after freeing we failed to reset JOIN_TAB->table
    entry. Further access of this invalid pointer resulted in
    issue.
    
    Solution:
    =========
    Reset JOIN_TAB->table. Also in JOIN::destroy call
    free_tmp_table only if JOIN_TAB->table is set. This avoid
    double free of temp tables.

--- sql/sql_executor.cc
+++ sql/sql_executor.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2000, 2014, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2000, 2015, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -299,7 +299,10 @@ JOIN::create_intermediate_table(JOIN_TAB *tab, List<Item> *tmp_table_fields,
 
 err:
   if (table != NULL)
+  {
     free_tmp_table(thd, table);
+    tab->table= NULL;
+  }
   DBUG_RETURN(true);
 }
 
