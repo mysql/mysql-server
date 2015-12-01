329a24d42f9806d4555237318b0a5bb593c7b4a6
Benny Wang <benny.wang@oracle.com>
2015-12-01 04:34:43 +0100

    Fixed bug#22179637: INSERT INTO TABLE FROM SELECT ACCEPTS TO INSERT INTO GENERATED COLUMNS
    In current version, miss to constraint generated columns assignment to be only
    DEFAULT value for INSERT...SELECT statement. This patch provides such a check.

--- sql/sql_insert.cc
+++ sql/sql_insert.cc
@@ -1304,7 +1304,7 @@ bool Sql_cmd_insert_base::mysql_prepare_insert(THD *thd, TABLE_LIST *table_list,
                         insert_value_list, SELECT_ACL, NULL, false, false);
     if (!res)
       res= check_valid_table_refs(table_list, insert_value_list, map);
-    if (!res && lex->insert_table_leaf->table->vfield)
+    if (!res && lex->insert_table_leaf->table->has_gcol())
       res= validate_gc_assignment(thd, &insert_field_list, values,
                                   lex->insert_table_leaf->table);
     thd->lex->in_update_value_clause= false;
@@ -1319,7 +1319,7 @@ bool Sql_cmd_insert_base::mysql_prepare_insert(THD *thd, TABLE_LIST *table_list,
                         insert_update_list, UPDATE_ACL, NULL, false, true);
       if (!res)
         res= check_valid_table_refs(table_list, insert_update_list, map);
-      if (!res && lex->insert_table_leaf->table->vfield)
+      if (!res && lex->insert_table_leaf->table->has_gcol())
         res= validate_gc_assignment(thd, &insert_update_list,
                                     &insert_value_list,
                                     lex->insert_table_leaf->table);
@@ -2019,6 +2019,10 @@ int Query_result_insert::prepare(List<Item> &values, SELECT_LEX_UNIT *u)
     res= setup_fields(thd, Ref_ptr_array(), values, SELECT_ACL, NULL,
                       false, false);
 
+  if (!res && lex->insert_table_leaf->table->has_gcol())
+    res= validate_gc_assignment(thd, fields, &values,
+                                lex->insert_table_leaf->table);
+
   if (duplicate_handling == DUP_UPDATE && !res)
   {
     Name_resolution_context *const context= &lex->select_lex->context;
@@ -2037,6 +2041,11 @@ int Query_result_insert::prepare(List<Item> &values, SELECT_LEX_UNIT *u)
     if (!res)
       res= setup_fields(thd, Ref_ptr_array(), *update.get_changed_columns(),
                         UPDATE_ACL, NULL, false, true);
+
+    if (!res && lex->insert_table_leaf->table->has_gcol())
+      res= validate_gc_assignment(thd, update.get_changed_columns(),
+                                  update.update_values,
+                                  lex->insert_table_leaf->table);
     /*
       When we are not using GROUP BY and there are no ungrouped aggregate
       functions 
