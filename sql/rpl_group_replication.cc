8edc2d9788988d64c688eb87ceb8f11062e8967f
Manish Kumar <manish.4.kumar@oracle.com>
2015-12-16 15:04:36 +0530

    BUG#22318608 - ON A NORMAL SERVER 'START GROUP_REPLICATION' HITS ER3092, BUT WITHOUT ERROR LOG
    The problem with this bug was that the error reporting was done without
    actually printing any error in the error log.
    
    Added error reporting in the code to give proper error messages in case
    the server encounters any.

--- sql/rpl_group_replication.cc
+++ sql/rpl_group_replication.cc
@@ -23,6 +23,7 @@
 #include "rpl_slave.h"
 #include "tc_log.h"
 #include "mysqld_thd_manager.h"
+#include "log.h"
 
 
 /*
@@ -177,7 +178,7 @@ bool is_group_replication_plugin_loaded()
 
 int group_replication_start()
 {
-  if (group_replication_handler)
+  if (is_group_replication_plugin_loaded())
   {
     /*
       We need to take global_sid_lock because
@@ -197,26 +198,29 @@ int group_replication_start()
     gtid_mode_lock->unlock();
     return ret;
   }
+  sql_print_error("Group Replication plugin is not installed.");
   return 1;
 }
 
 int group_replication_stop()
 {
-  if (group_replication_handler)
+  if (is_group_replication_plugin_loaded())
    return group_replication_handler->stop();
+
+  sql_print_error("Group Replication plugin is not installed.");
   return 1;
 }
 
 bool is_group_replication_running()
 {
-  if (group_replication_handler)
+  if (is_group_replication_plugin_loaded())
     return group_replication_handler->is_running();
   return false;
 }
 
 int set_group_replication_retrieved_certification_info(View_change_log_event *view_change_event)
 {
-  if (group_replication_handler)
+  if (is_group_replication_plugin_loaded())
     return group_replication_handler->set_retrieved_certification_info(view_change_event);
   return 1;
 }
@@ -224,7 +228,7 @@ int set_group_replication_retrieved_certification_info(View_change_log_event *vi
 bool get_group_replication_connection_status_info(
     const GROUP_REPLICATION_CONNECTION_STATUS_CALLBACKS& callbacks)
 {
-  if (group_replication_handler)
+  if (is_group_replication_plugin_loaded())
     return group_replication_handler->get_connection_status_info(callbacks);
   return true;
 }
@@ -233,7 +237,7 @@ bool get_group_replication_group_members_info(
     unsigned int index,
     const GROUP_REPLICATION_GROUP_MEMBERS_CALLBACKS& callbacks)
 {
-  if (group_replication_handler)
+  if (is_group_replication_plugin_loaded())
     return group_replication_handler->get_group_members_info(index, callbacks);
   return true;
 }
@@ -241,14 +245,14 @@ bool get_group_replication_group_members_info(
 bool get_group_replication_group_member_stats_info(
     const GROUP_REPLICATION_GROUP_MEMBER_STATS_CALLBACKS& callbacks)
 {
-  if (group_replication_handler)
+  if (is_group_replication_plugin_loaded())
     return group_replication_handler->get_group_member_stats_info(callbacks);
   return true;
 }
 
 unsigned int get_group_replication_members_number_info()
 {
-  if (group_replication_handler)
+  if (is_group_replication_plugin_loaded())
     return group_replication_handler->get_members_number_info();
   return 0;
 }
