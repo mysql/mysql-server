67d0fc35ad23cabcf1c6ba0db8b0e4e9113b1647
Mark Leith <mark.leith@oracle.com>
2015-12-22 10:00:07 +0000

    WL#8993: Option to not log in the error log when unsafe statements are executed whilst using STATEMENT based binary logging
    Added a log_statements_unsafe_for_binlog option, which purely disables writing the unsafe statement warnings to the error log (warnings are still sent to the clients).
    
    Reviewed-by: Joao Gramacho <joao.gramacho@racle.com>
    Reviewed-by: Luis Soares <luis.soares@oracle.com>
    
    RB: 11076

--- sql/binlog.cc
+++ sql/binlog.cc
@@ -11143,7 +11143,7 @@ void THD::issue_unsafe_warnings()
                           ER_BINLOG_UNSAFE_STATEMENT,
                           ER(ER_BINLOG_UNSAFE_STATEMENT),
                           ER(LEX::binlog_stmt_unsafe_errcode[unsafe_type]));
-      if (log_error_verbosity > 1)
+      if (log_error_verbosity > 1 && opt_log_unsafe_statements)
       {
         if (unsafe_type == LEX::BINLOG_STMT_UNSAFE_LIMIT)
           do_unsafe_limit_checkout( buf, unsafe_type, query().str);
