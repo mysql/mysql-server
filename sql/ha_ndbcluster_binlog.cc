76d771b8dcd2c789f3d777966dfc3b207329a87c
Ole John Aske <ole.john.aske@oracle.com>
2016-01-15 09:55:05 +0100

    Follow up patch for performance regression introduced by patch for bug#20957068
    Introduce some sched_yield() in the binlog-thread loops where
    the injector_mutex is held >99% if the elapsed time. The yields
    let other threads waiting to be scheduled a chance to run,
    and grab the injector_mutex when not held by the binlog-thread.
    
    (cherry picked from commit 000394fbe3a8d7a2945fb6b483024b77e16ab20a)

--- sql/ha_ndbcluster_binlog.cc
+++ sql/ha_ndbcluster_binlog.cc
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2006, 2015, Oracle and/or its affiliates. All rights reserved.
+  Copyright (c) 2006, 2016, Oracle and/or its affiliates. All rights reserved.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -7183,10 +7183,12 @@ restart_cluster_failure:
       if (ndbcluster_binlog_terminating)
         goto err;
 
+      sched_yield();
       pthread_mutex_lock(&injector_mutex);
       schema_res= s_ndb->pollEvents(100, &schema_gci);
       pthread_mutex_unlock(&injector_mutex);
     } while (schema_gci == 0 || ndb_latest_received_binlog_epoch == schema_gci);
+
     if (ndb_binlog_running)
     {
       Uint64 gci= i_ndb->getLatestGCI();
@@ -7194,6 +7196,8 @@ restart_cluster_failure:
       {
         if (ndbcluster_binlog_terminating)
           goto err;
+
+        sched_yield();
         pthread_mutex_lock(&injector_mutex);
         res= i_ndb->pollEvents(10, &gci);
         pthread_mutex_unlock(&injector_mutex);
@@ -7289,7 +7293,21 @@ restart_cluster_failure:
     */
     thd->proc_info= "Waiting for event from ndbcluster";
     thd->set_time();
-    
+
+    /**
+     * The binlog-thread holds the injector_mutex when waiting for
+     * pollEvents() - which is >99% of the elapsed time. As the
+     * pthread mutex guarantees no 'fairness', there is no guarantee
+     * that another thread waiting for the mutex will immeditately
+     * get the lock when unlocked by this thread. Thus this thread
+     * may lock it again rather soon and starve the waiting thread.
+     * To avoid this, sched_yield() is used to give any waiting
+     * threads a chance to run and grab the injector_mutex when
+     * it is available. The same pattern is used multiple places
+     * in the BI-thread where there are wait-loops holding this mutex.
+     */
+    sched_yield();
+
     /* Can't hold injector_mutex too long, so wait for events in 10ms steps */
     int tot_poll_wait= 10;
 
@@ -7353,6 +7371,8 @@ restart_cluster_failure:
                     (uint)(ndb_latest_received_binlog_epoch >> 32),
                     (uint)(ndb_latest_received_binlog_epoch));
         thd->proc_info= buf;
+
+        sched_yield();
         pthread_mutex_lock(&injector_mutex);
         schema_res= s_ndb->pollEvents(10, &schema_epoch);
         pthread_mutex_unlock(&injector_mutex);
