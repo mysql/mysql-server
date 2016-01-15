873e7fc388e2d56027131ec61bb7ca2e3941d834
Magnus Blåudd <magnus.blaudd@oracle.com>
2016-01-15 12:51:36 +0100

    WL#6815 Adapt MySQL Cluster to 5.7
     - rename calls to pthread_mutex_ -> native_mutex

--- sql/ha_ndbcluster_binlog.cc
+++ sql/ha_ndbcluster_binlog.cc
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2000, 2015, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2000, 2016, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -3841,9 +3841,9 @@ public:
        * Once restarted, a new schema_ndb is recreated and ndb_binlog_setup()
        * is allowed to do its tasks.
        */
-      pthread_mutex_lock(&injector_mutex);
+      native_mutex_lock(&injector_mutex);
       schema_ndb= NULL;
-      pthread_mutex_unlock(&injector_mutex);
+      native_mutex_unlock(&injector_mutex);
 
       /* release the ndb_schema_share */
       native_mutex_lock(&ndb_schema_share_mutex);
