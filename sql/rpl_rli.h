6a6f6bfe5c7dd639bcdd8174601581374807f2dd
Sujatha Sivakumar <sujatha.sivakumar@oracle.com>
2016-01-19 15:40:27 +0530

    Bug#21198611: MULTI-THREADED SLAVE LOG SPAMMING ON FAILURE
    Problem:
    =======
    When a multi-threaded slave stops with an error, the same
    error message is printed three times.
    
    Analysis:
    =========
    "sql_slave_killed" function is called frequently to check
    if SQL thread is alive or got killed. This function doesn't
    preserve SQL thread's kill command acceptance status. This
    causes the whole function to be re-executed each time. i.e
    even though the kill command got accepted during previous
    execution, since it is not preserved the function is
    re-executed. As part of this function's execution when SQL
    thread accepts kill status it prints few error messages to
    let the user know the current status. Because of these
    re-executions these error messages are also printed once
    again in the error log.
    
    Fix:
    ===
    Added a new variable which stores the SQL thread's kill
    acceptance status. If kill is accepted it is set to true
    and the error messages are printed only once at this point.
    During further invocations of this function, since kill
    is already accepted, nothing will be done by this function
    it simply returns the kill acceptance status.

--- sql/rpl_rli.h
+++ sql/rpl_rli.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2005, 2015, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2005, 2016, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -848,6 +848,11 @@ public:
     with several warning messages.
   */
   bool reported_unsafe_warning;
+
+  /*
+    'sql_thread_kill_accepted is set to TRUE when killed status is recognized.
+  */
+  bool sql_thread_kill_accepted;
 
   time_t get_row_stmt_start_timestamp()
   {
