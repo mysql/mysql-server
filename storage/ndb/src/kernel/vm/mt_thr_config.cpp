6d0944028bf3c744eb5ec2f117bde16e5c1d4ec0
Mikael Ronström <mikael.ronstrom@oracle.com>
2016-02-05 12:58:40 +0100

    BUG#22647476: Fix spintime, was converted to bool, thus spintime became 1 even when set higher

--- storage/ndb/src/kernel/vm/mt_thr_config.cpp
+++ storage/ndb/src/kernel/vm/mt_thr_config.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2011, 2015, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2011, 2016, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -1216,7 +1216,7 @@ THRConfigApplier::do_get_spintime(const unsigned short list[],
                                   unsigned cnt) const
 {
   const T_Thread* thr = find_thread(list, cnt);
-  return (bool)thr->m_spintime;
+  return thr->m_spintime;
 }
 
 bool
