fc4052adfeb7bc2bc4a7c7047ed646023faca68f
Knut Anders Hatlen <knut.hatlen@oracle.com>
2016-02-12 10:52:50 +0100

    Bug#22700508: Remove redundant calls to Json_wrapper::empty()
    Json_wrapper::seek_no_ellipsis() checks both Json_wrapper::type() and
    Json_wrapper::empty(). Since type() already checks empty(), it
    suffices for seek_no_ellipsis() to check type().
    
    Json_wrapper's copy assignment operator checks empty() to avoid
    deleting the contained Json_dom if it is NULL. This check is
    redundant, since delete is defined to be a no-op when called on NULL.
    Furthermore, the code achieves exactly the same as Json_wrapper's
    destructor, only more complexly. Call the destructor instead to avoid
    code duplication.

--- sql/json_dom.cc
+++ sql/json_dom.cc
@@ -1883,11 +1883,8 @@ Json_wrapper &Json_wrapper::operator=(const Json_wrapper& from)
     return *this;   // self assignment: no-op
   }
 
-  if (m_is_dom && !m_dom_alias &&!empty())
-  {
-    // we own our own copy, so we are responsible for deallocation
-    delete m_dom_value;
-  }
+  // Deallocate DOM if needed.
+  this->~Json_wrapper();
 
   // Copy the value into this.
   new (this) Json_wrapper(from);
@@ -2491,7 +2488,7 @@ bool Json_wrapper::seek_no_ellipsis(const Json_seekable_path &path,
           size_t key_length= path_leg->get_member_name_length();
           Json_wrapper member= lookup(key, key_length);
 
-          if (!member.empty() & !(member.type() == Json_dom::J_ERROR))
+          if (member.type() != Json_dom::J_ERROR)
           {
             // recursion
             if (member.seek_no_ellipsis(path, hits, leg_number + 1, auto_wrap,
