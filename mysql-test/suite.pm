3889b19f5414f689c8db98b81a19425629c98faa
Sergei Golubchik <serg@mariadb.org>
2016-02-14 22:19:27 +0100

    more strict ipv6_ok check in mtr
    don't use ipv6 is the check fails for any reason. for example, an
    ancient solaris perl didn't have sockaddr_in6() in Socket at all.

--- mysql-test/suite.pm
+++ mysql-test/suite.pm
@@ -35,7 +35,7 @@ sub skip_combinations {
     return 0 unless socket my $sock, PF_INET6, SOCK_STREAM, getprotobyname('tcp');
     # eval{}, if there's no Socket::sockaddr_in6 at all, old Perl installation
     eval { connect $sock, sockaddr_in6(7, Socket::IN6ADDR_LOOPBACK) };
-    return $! != 101;
+    return $@ eq "";
   }
   $skip{'include/check_ipv6.inc'} = 'No IPv6' unless ipv6_ok();
 
