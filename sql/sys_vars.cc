f3d81a295e95100b5c0b87a350f8bedca30118f5
Tor Didriksen <tor.didriksen@oracle.com>
2016-02-18 12:33:17 +0100

    Bug#14744160 LIMIT PARSER MEMORY USAGE
    This is a followup patch.
    
    Problem: check-testcase by mtr may fail. It does
        SELECT * FROM INFORMATION_SCHEMA.VIEWS
    which will invoke the parser recursively, and will need more than 400kbytes
    
    Solution: raise the minimum value of parser_max_mem_size to 10MB

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -2701,13 +2701,17 @@ static unsigned long max_mem_sz = ~0;
 static unsigned long long max_mem_sz = ~0;
 #endif
 
-// Need at least 400Kb to get through bootstrap.
+/*
+  Need at least 400Kb to get through bootstrap.
+  Need at least 8Mb to get through mtr check testcase, which does
+    SELECT * FROM INFORMATION_SCHEMA.VIEWS
+*/
 static Sys_var_ulonglong Sys_parser_max_mem_size(
       "parser_max_mem_size",
       "Maximum amount of memory available to the parser",
       SESSION_VAR(parser_max_mem_size),
       CMD_LINE(REQUIRED_ARG),
-      VALID_RANGE(400 * 1000, max_mem_sz),
+      VALID_RANGE(10 * 1000 * 1000, max_mem_sz),
       DEFAULT(max_mem_sz),
       BLOCK_SIZE(1),
       NO_MUTEX_GUARD, NOT_IN_BINLOG,
