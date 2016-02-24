d98ff6dcb54bf818b9b2153f1d19ff6fe1495120
Erlend Dahl <erlend.dahl@oracle.com>
2016-02-24 15:41:29 +0100

    Revert "Bug#22136709 INFINITE RECURSION BY CALLING MY_MESSAGE FROM MYSQL_AUDIT_GENERAL_CLASS HANDLER"
    Patch broke compilation.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -2215,8 +2215,7 @@ void my_message_sql(uint error, const char *str, myf MyFlags)
   }
 
 #ifndef EMBEDDED_LIBRARY
-  if (error != ER_STACK_OVERRUN_NEED_MORE)
-    mysql_audit_general(thd, MYSQL_AUDIT_GENERAL_ERROR, error, str);
+  mysql_audit_general(thd, MYSQL_AUDIT_GENERAL_ERROR, error, str);
 #endif
 
   if (thd)
