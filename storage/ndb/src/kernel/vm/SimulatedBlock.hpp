f2f6d29d7f85d6fa3713941c9be24305ce2ea799
Mikael Ronström <mikael.ronstrom@oracle.com>
2016-03-15 18:19:41 +0100

    WL#8968 Send thread redesign, introduced CPU measurements in thrman.cpp with similar capabilities to top, introduced 5 new ndbinfo tables: threads, cpustat, cpustat_50ms, cpustat_1sec, cpustat_20sec

--- storage/ndb/src/kernel/vm/SimulatedBlock.hpp
+++ storage/ndb/src/kernel/vm/SimulatedBlock.hpp
@@ -120,6 +120,16 @@ struct PackedWordsContainer
   Uint32 packedWords[30];
 }; // 128 bytes
 
+#define LIGHT_LOAD_CONST 0
+#define MEDIUM_LOAD_CONST 1
+#define OVERLOAD_CONST 2
+enum OverloadStatus
+{
+  LIGHT_LOAD = LIGHT_LOAD_CONST,
+  MEDIUM_LOAD = MEDIUM_LOAD_CONST,
+  OVERLOAD = OVERLOAD_CONST
+};
+
 /**
   Description of NDB Software Architecture
   ----------------------------------------
@@ -584,6 +594,7 @@ public:
 
   void EXECUTE_DIRECT(ExecFunction f,
                       Signal *signal);
+
 protected:
   static Callback TheEmptyCallback;
   void TheNULLCallbackFunction(class Signal*, Uint32, Uint32);
@@ -591,8 +602,40 @@ protected:
   void execute(Signal* signal, Callback & c, Uint32 returnCode);
   
 
+  /**
+   * Various methods to get data from ndbd/ndbmtd such as time
+   * spent in sleep, sending and executing, number of signals
+   * in queue, and send buffer level.
+   *
+   * Also retrieving a thread name (this name must be pointing to a
+   * static pointer since it will be stored and kept for a long
+   * time. So the pointer cannot be changed.
+   *
+   * Finally also the ability to query for send thread information.
+   */
   void getSendBufferLevel(NodeId node, SB_LevelType &level);
   Uint32 getSignalsInJBB();
+  void setOverloadStatus(OverloadStatus new_status);
+  void setWakeupThread(Uint32 wakeup_instance);
+  void setNodeOverloadStatus(OverloadStatus new_status);
+  void setSendNodeOverloadStatus(OverloadStatus new_status);
+  void setNeighbourNode(NodeId node);
+  void getPerformanceTimers(Uint64 &micros_sleep,
+                            Uint64 &spin_time,
+                            Uint64 &buffer_full_micros_sleep,
+                            Uint64 &micros_send);
+  void getSendPerformanceTimers(Uint32 send_instance,
+                                Uint64 & exec_time,
+                                Uint64 & sleep_time,
+                                Uint64 & spin_time,
+                                Uint64 & user_time_os,
+                                Uint64 & kernel_time_os,
+                                Uint64 & elapsed_time_os);
+  Uint32 getSpintime();
+  Uint32 getNumSendThreads();
+  Uint32 getNumThreads();
+  const char * getThreadName();
+  const char * getThreadDescription();
 
   NDB_TICKS getHighResTimer();
 
