025223388e1798d3a886ecc139afee7277d6300a
Dinesh Surya Prakash <dinesh.prakash@oracle.com>
2016-03-22 12:21:26 +0530

    Bug#21441297  INVALIDATTRINFO RETURNED FROM EXECTUX_BOUND_INFO WHILE SCANNING
    When we perform "analyze" command in a table with index, ndbmtd crashes
    and throws "InvalidAttrInfo" error due to corruption in the signal.
    
    The problem was there in the Trix block.Here we use stack variable in
    building up a signal, hence it goes out of scope when the signal is
    refereed in other modules. As the memory is invalid it gives a crash
    when signal is de-refereed in other modules.
    
    Code was modified such that the signal is now build with pooled
    variable.As this is a global variable too it can be refereed in other
    modules.

--- storage/ndb/src/kernel/blocks/trix/Trix.cpp
+++ storage/ndb/src/kernel/blocks/trix/Trix.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2013, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2016, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -1061,7 +1061,7 @@ void Trix::startTableScan(Signal* signal, SubscriptionRecPtr subRecPtr)
   {
     jam();
     StatOp& stat = statOpGetPtr(subRecPtr.p->m_statPtrI);
-    StatOp::Clean clean = stat.m_clean;
+    StatOp::Clean& clean = stat.m_clean;
     orderPtr[1].p = clean.m_bound;
     orderPtr[1].sz = clean.m_boundSize;
     noOfSections = 2;
