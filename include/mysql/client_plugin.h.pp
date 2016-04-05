2da4222506c7c3a5bc19d8b8385e0e0c7574db5c
Terje Rosten <terje.rosten@oracle.com>
2016-04-05 09:00:38 +0200

    Bug#23047194 USE OF <>-STYLE INCLUDES CONFUSES PREPROCESSOR
    Building client applications with MySQL C API using absolute
    path to mysql.h or using <mysql/mysql.h>  and not setting
    include path failed.
    
    Cause is use of <>-style over ""-style includes in internal
    mysql header files.
    
    Updated *.pp files to reflect change.

--- include/mysql/client_plugin.h.pp
+++ include/mysql/client_plugin.h.pp
@@ -3,7 +3,7 @@ struct st_mysql_client_plugin
   int type; unsigned int interface_version; const char *name; const char *author; const char *desc; unsigned int version[3]; const char *license; void *mysql_api; int (*init)(char *, size_t, int, va_list); int (*deinit)(); int (*options)(const char *option, const void *);
 };
 struct st_mysql;
-#include <mysql/plugin_auth_common.h>
+#include "plugin_auth_common.h"
 typedef struct st_plugin_vio_info
 {
   enum { MYSQL_VIO_INVALID, MYSQL_VIO_TCP, MYSQL_VIO_SOCKET,
