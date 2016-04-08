e3326dfbebcc4b367edf42294b248630ce8fd27e
Mauritz Sundell <mauritz.sundell@oracle.com>
2016-04-08 21:32:06 +0200

    Bug#23062205 AUTOTEST UPGRADE TESTING DO NOT WORK SINCE LAB MOVE
    Add prefix to use when ATRT look for atrt-scripts.

--- storage/ndb/test/run-test/atrt.hpp
+++ storage/ndb/test/run-test/atrt.hpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2015, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2016, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -197,6 +197,7 @@ extern const char * g_my_cnf;
 extern const char * g_user;
 extern const char * g_basedir;
 extern const char * g_prefix;
+extern const char * g_prefix0;
 extern const char * g_prefix1;
 extern int          g_baseport;
 extern int          g_fqpn;
