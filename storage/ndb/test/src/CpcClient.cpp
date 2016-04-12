6fcd939b30211aac7d409268b0da54d667958d77
Mauritz Sundell <mauritz.sundell@oracle.com>
2016-04-13 00:49:46 +0200

    Bug#23062205 AUTOTEST UPGRADE TESTING DO NOT WORK SINCE LAB MOVE
    Introduce appendable long strings in parser for MGM and CPC protocol
    keeping the maximum line size to 512 bytes.
    
    Use LongString for CPC protocol 'define process' arguments 'env' and 'args'.
    
    These long strings are sent as:
    argname:"first-part-of-data..."\n
    +argname:"second-part-of-data..."\n
    ...
    +argname:"last-part-of-data..."\n
    
    Sending lines starting with '+' to old clients will make CPCD not defining
    process, in same way as specifing unknown argument.
    
    The use of quotes is to disable triming white spaces since they need to be
    kept while concatenating the parts.  Using quotes is an old feature.

--- storage/ndb/test/src/CpcClient.cpp
+++ storage/ndb/test/src/CpcClient.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2003-2008 MySQL AB, 2009, 2010 Sun Microsystems, Inc.
+   Copyright (c) 2003, 2016, Oracle and/or its affiliates. All rights reserved.
 
    All rights reserved. Use is subject to license terms.
 
@@ -432,6 +432,7 @@ SimpleCpcClient::cpc_send(const char *cmd,
     PropertiesType t;
     Uint32 val_i;
     BaseString val_s;
+    const size_t namelen = strlen(name);
 
     args.getTypeOf(name, &t);
     switch(t) {
@@ -440,18 +441,33 @@ SimpleCpcClient::cpc_send(const char *cmd,
       cpc_out.println("%s: %d", name, val_i);
       break;
     case PropertiesType_char:
+    {
+      /**
+       * Long string, chop up into multiple lines:
+       * argname:"val-prefix..."\n
+       * +argname:"val-part..."\n
+       * ...
+       * +argname:"val-part..."\n
+       */
       args.get(name, val_s);
-      if (strlen(val_s.c_str()) > Parser_t::Context::MaxParseBytes)
+      /**
+       * For first line there are five characters in addition to name and
+       * value part, for the following lines the maximum length of a value
+       * part is one character less since the extra plus sign is needed.
+       */
+      const char * value = val_s.c_str();
+      int valuelen = val_s.length();
+      int partlen = Parser_t::Context::MaxParseBytes - namelen - 5;
+      cpc_out.print("%s:\"%.*s\"\n", name, partlen, value);
+      int valueoff = partlen;
+      partlen--; // Maximum part length decreased to make room for plus sign
+      while (valueoff < valuelen)
       {
-        ndbout << "Argument " << name << " at " 
-               << strlen(val_s.c_str())
-               << " longer than max of "
-               << Parser_t::Context::MaxParseBytes
-               << endl;
-        abort();
+        cpc_out.print("+%s:\"%.*s\"\n", name, partlen, value + valueoff);
+        valueoff += partlen;
       }
-      cpc_out.println("%s: %s", name, val_s.c_str());
       break;
+    }
     default:
       /* Silently ignore */
       break;
