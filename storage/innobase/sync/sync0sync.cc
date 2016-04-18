67ebbbd14473cb6a707e37c29892de9046129004
Elżbieta Babij <elzbieta.babij@oracle.com>
2016-04-18 10:46:17 +0200

    Bug#22844987 - SPIN ROUNDS PER WAIT WILL BE NEGATIVE IN INNODB STATUS IF SPIN WAITS > MAX32INT
    Changed in the ternary operator 1 to uint64_t(1) - thanks to that counter value won't cast to int32, which could cause in negative values.
    Changed this ternary operator changed to std::max -- makes code more readable.
    Also changed the type in counter from signed to unsigned, since counter don't use negative values.
    
    Restoring missed incrementing of rw_s_spin_wait_count and rw_x_spin_wait_count.
    
    Adding test checking whether RW-S lock round counts is increased after some operations.
    
    RB#12420

--- storage/innobase/sync/sync0sync.cc
+++ storage/innobase/sync/sync0sync.cc
@@ -147,28 +147,25 @@ sync_print_wait_info(FILE* file)
 		" OS waits " UINT64PF "\n"
 		"RW-sx spins " UINT64PF ", rounds " UINT64PF ","
 		" OS waits " UINT64PF "\n",
-		(ib_uint64_t) rw_lock_stats.rw_s_spin_wait_count,
-		(ib_uint64_t) rw_lock_stats.rw_s_spin_round_count,
-		(ib_uint64_t) rw_lock_stats.rw_s_os_wait_count,
-		(ib_uint64_t) rw_lock_stats.rw_x_spin_wait_count,
-		(ib_uint64_t) rw_lock_stats.rw_x_spin_round_count,
-		(ib_uint64_t) rw_lock_stats.rw_x_os_wait_count,
-		(ib_uint64_t) rw_lock_stats.rw_sx_spin_wait_count,
-		(ib_uint64_t) rw_lock_stats.rw_sx_spin_round_count,
-		(ib_uint64_t) rw_lock_stats.rw_sx_os_wait_count);
+		(uint64_t) rw_lock_stats.rw_s_spin_wait_count,
+		(uint64_t) rw_lock_stats.rw_s_spin_round_count,
+		(uint64_t) rw_lock_stats.rw_s_os_wait_count,
+		(uint64_t) rw_lock_stats.rw_x_spin_wait_count,
+		(uint64_t) rw_lock_stats.rw_x_spin_round_count,
+		(uint64_t) rw_lock_stats.rw_x_os_wait_count,
+		(uint64_t) rw_lock_stats.rw_sx_spin_wait_count,
+		(uint64_t) rw_lock_stats.rw_sx_spin_round_count,
+		(uint64_t) rw_lock_stats.rw_sx_os_wait_count);
 
 	fprintf(file,
 		"Spin rounds per wait: %.2f RW-shared,"
 		" %.2f RW-excl, %.2f RW-sx\n",
 		(double) rw_lock_stats.rw_s_spin_round_count /
-		(rw_lock_stats.rw_s_spin_wait_count
-		 ? rw_lock_stats.rw_s_spin_wait_count : 1),
+		std::max(uint64_t(1), (uint64_t) rw_lock_stats.rw_s_spin_wait_count),
 		(double) rw_lock_stats.rw_x_spin_round_count /
-		(rw_lock_stats.rw_x_spin_wait_count
-		 ? rw_lock_stats.rw_x_spin_wait_count : 1),
+		std::max(uint64_t(1), (uint64_t) rw_lock_stats.rw_x_spin_wait_count),
 		(double) rw_lock_stats.rw_sx_spin_round_count /
-		(rw_lock_stats.rw_sx_spin_wait_count
-		 ? rw_lock_stats.rw_sx_spin_wait_count : 1));
+		std::max(uint64_t(1), (uint64_t) rw_lock_stats.rw_sx_spin_wait_count));
 }
 
 /**
