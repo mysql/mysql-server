c6493ad6362e4aa4f25274caeaf8192ea99e1959
Jon Olav Hauglid <jon.hauglid@oracle.com>
2016-04-18 15:25:36 +0200

    Bug#23106060: FIX COMPILE WARNINGS REPORTED BY CLANG 3.8
    Fix new compile warnings reported by Clang 3.8.
    Initialize variables that can be used uninitialized otherwise.

--- client/mysqladmin.cc
+++ client/mysqladmin.cc
@@ -1087,12 +1087,14 @@ static int execute_commands(MYSQL *mysql,int argc, char **argv)
 
       /* Warn about password being set in non ssl connection */
 #if defined(HAVE_OPENSSL) && !defined(EMBEDDED_LIBRARY)
-      uint ssl_mode;
-      if (!mysql_get_option(mysql, MYSQL_OPT_SSL_MODE, &ssl_mode) &&
-          ssl_mode <= SSL_MODE_PREFERRED)
       {
-        fprintf(stderr, "Warning: Since password will be sent to server in "
-                "plain text, use ssl connection to ensure password safety.\n");
+        uint ssl_mode= 0;
+        if (!mysql_get_option(mysql, MYSQL_OPT_SSL_MODE, &ssl_mode) &&
+            ssl_mode <= SSL_MODE_PREFERRED)
+        {
+          fprintf(stderr, "Warning: Since password will be sent to server in "
+                  "plain text, use ssl connection to ensure password safety.\n");
+        }
       }
 #endif
       memset(buff, 0, sizeof(buff));
