c798910fe03ec78394ef07b8d6daa9044edc7e07
Magnus Blåudd <magnus.blaudd@oracle.com>
2016-04-19 16:40:37 +0200

    Bug#23135134 UNNECESSARY PORTABILITY CODE IN MGMAPI.CPP
     - the mgmapi.cpp implementation used #ifdef's for
       windows unnecessarily. This causes duplicated code.
     - fix by using the ndb_socket functions and remove
       the duplicated code

--- storage/ndb/test/ndbapi/testMgm.cpp
+++ storage/ndb/test/ndbapi/testMgm.cpp
@@ -43,11 +43,7 @@ int runTestApiSession(NDBT_Context* ctx, NDBT_Step* step)
   h= ndb_mgm_create_handle();
   ndb_mgm_set_connectstring(h, mgmd.getConnectString());
   ndb_mgm_connect(h,0,0,0);
-#ifdef NDB_WIN
-  SOCKET s = ndb_mgm_get_fd(h);
-#else
-  int s= ndb_mgm_get_fd(h);
-#endif
+  ndb_native_socket_t s = ndb_mgm_get_fd(h);
   session_id= ndb_mgm_get_session_id(h);
   ndbout << "MGM Session id: " << session_id << endl;
   send(s,"get",3,0);
