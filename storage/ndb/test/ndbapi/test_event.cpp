18a5983c5896252622cc3733f595c51eacc926c1
Magnus Blåudd <magnus.blaudd@oracle.com>
2016-04-19 16:33:43 +0200

    Bug#23135093 test_event compilation failure on Windows
     - compilation error occured because of non portable code which
       assigned to ndb_socket_t::fd which is called
       ndb_socket_t::s on Windows in order to catch errors like
       this
     - fix by introducing new ndb_socket_create_from_native() function
       which can be used to create a ndb_socket_t from ndb_native_socket_t
       in a portable way without using #ifdefs.
     - also replace usages of SOCKET and int with ndb_native_socket_t

--- storage/ndb/test/ndbapi/test_event.cpp
+++ storage/ndb/test/ndbapi/test_event.cpp
@@ -5467,9 +5467,8 @@ int runGetLogEventPretty(NDBT_Context* ctx, NDBT_Step* step)
     return NDBT_FAILED;
 
   int filter[] = {15, NDB_MGM_EVENT_CATEGORY_INFO, 0};
-  NDB_SOCKET_TYPE my_fd;
-  int fd= ndb_mgm_listen_event(mgmd.handle(), filter);
-  my_fd.fd= fd;
+  ndb_native_socket_t fd= ndb_mgm_listen_event(mgmd.handle(), filter);
+  ndb_socket_t my_fd = ndb_socket_create_from_native(fd);
 
   if(!my_socket_valid(my_fd))
   {
