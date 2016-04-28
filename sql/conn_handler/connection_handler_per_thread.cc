1edc52aa3a83beed3baf2ceaceba7db99a560690
Marek Szymczak <marek.szymczak@oracle.com>
2016-04-28 02:11:07 +0200

    WL#5705 Audit log plugin fine-grained pre-filtering
    Audit log extension that allows pre-filtering of the events before they
    are logged into a log file.
    
    Reviewed-by:
    ============
    Georgi Kodinov <georgi.kodinov@oracle.com>
    Ramil Kalimullin <ramil.kalimullin@oracle.com>

--- sql/conn_handler/connection_handler_per_thread.cc
+++ sql/conn_handler/connection_handler_per_thread.cc
@@ -303,7 +303,7 @@ extern "C" void *handle_connection(void *arg)
       }
       end_connection(thd);
     }
-    close_connection(thd);
+    close_connection(thd, 0, false, false);
 
     thd->get_stmt_da()->reset_diagnostics_area();
     thd->release_resources();
