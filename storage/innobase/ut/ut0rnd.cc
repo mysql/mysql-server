ee9cdcac81fabcdc3f31d2bad40e1b4912647f7f
Aakanksha Verma <aakanksha.verma@oracle.com>
2016-05-06 10:16:35 +0530

    Bug #22733635   CACHE LINE CONTENTION ON UT_RND_ULINT_COUNTER
    PROBLEM
    
    InnoDB implements the random backoff for spinlocks. The function
    ut_rnd_interval() is not really
    scalable on NUMA core systems since it maintains a global counter
    ut_rnd_ulint_counter i.e
    updated on each call leading to cache coherency and performance
    degradation with many threads
    accessing and incrementing the variable.
    
    FIX
    
    As suggested by the bug reporter to make the variable thread local so
    that each thread would
    maintain a local copy of it and access it reducing the contentions and
    delays.
    Used TLS library from the server code to make the counter variable
    specific to each thread.
    
    Reviewed by: Jimmy Yang<jimmy.yang@oracle.com>
    Rb :12444

--- storage/innobase/ut/ut0rnd.cc
+++ storage/innobase/ut/ut0rnd.cc
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
-Copyright (c) 1994, 2014, Oracle and/or its affiliates. All Rights Reserved.
+Copyright (c) 1994, 2016, Oracle and/or its affiliates. All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -24,6 +24,7 @@ Created 5/11/1994 Heikki Tuuri
 ********************************************************************/
 
 #include "ut0rnd.h"
+#include <my_thread_local.h>
 
 #ifdef UNIV_NONINL
 #include "ut0rnd.ic"
@@ -36,9 +37,8 @@ Created 5/11/1994 Heikki Tuuri
 #define UT_RANDOM_3	1.0132677
 /*@}*/
 
-/** Seed value of ut_rnd_gen_ulint(). */
-ulint	ut_rnd_ulint_counter = 65654363;
-
+/** Key for thread local variable ut_rnd_ulint_counter */
+thread_local_key_t   ut_rnd_ulint_counter_key;
 /***********************************************************//**
 Looks for a prime number slightly greater than the given argument.
 The prime is chosen so that it is not near any power of 2.
