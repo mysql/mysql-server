0f27e70a8e26f9f7d49650cb1c5c463a167071f1
Jon Olav Hauglid <jon.hauglid@oracle.com>
2016-05-09 10:23:32 +0200

    Bug#23080289: CORRECT MAKE_PAIR FOR C++11 (CONTRIBUTION)
    This patch fixes a compilation error when compiling
    5.7 in C++11 mode.
    
    Note that this does *not* mean that C++11 is supported
    for 5.7. Several of the compiler versions used by our
    supported platforms for 5.7 are too old to support C++11.
    
    Patch based on contribution from Daniel Black.

--- client/auth_utils.cc
+++ client/auth_utils.cc
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2012, 2015, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2012, 2016, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -61,7 +61,7 @@ int parse_cnf_file(istream &sin, map<string, string > *options,
     getline(sin, option_value);
     trim(&option_value);
     if (option_name.length() > 0)
-      options->insert(make_pair<string, string >(option_name, option_value));
+      options->insert(make_pair(option_name, option_value));
   }
   return ALL_OK;
   } catch(...)
