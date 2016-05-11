05ca74b01519c7fb76f34877b4a5401a5cc705c1
Dinesh Surya Prakash <dinesh.prakash@oracle.com>
2016-05-11 13:59:14 +0530

    Bug#21098142 7.4 MYSQLD HANGING IN SHUTDOWN
    When we do a "shutdown" in mysqld, a deadlock occurs if there is a thread
    executing ndb_index_stat at the same time.
    
    The ndb_index_stat thread first takes a local lock and then tries to
    acquire a global lock. At the same time, the  shutdown thread takes the
    same global lock first and then tries to acquire the same local lock.
    
    Fixed the index stat code to separate the critical sections for the global
    lock and the local lock. Now the local lock is released before taking the
    global lock, so the deadlock is avoided.

--- sql/ha_ndb_index_stat.h
+++ sql/ha_ndb_index_stat.h
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2011, 2016, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -33,8 +33,8 @@ class Ndb_index_stat_thread : public Ndb_component
 {
   // Someone is waiting for stats
   bool client_waiting;
-  pthread_mutex_t LOCK;
-  pthread_cond_t COND;
+  pthread_mutex_t LOCK_client_waiting;
+  pthread_cond_t COND_client_waiting;
 public:
   Ndb_index_stat_thread();
   virtual ~Ndb_index_stat_thread();
