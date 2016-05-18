92abadf25ed5d1916b6a53bfcb0ac40d606c2cd4
Martin Sköld <Martin.Skold@oracle.com>
2016-05-18 18:28:51 +0200

    WL#7162 Move ndb_binlog_index table from MyISAM to transactional storage
    Changing storage engine of ndb_binlog_index to use InnoDB.
    This gives better behavior when purging logs where large parts of
    the table can be deleted. Avoiding MyISAM table locking as well
    as using InnoDB lock-free reads will give better availability.
    
    Installation scripts mysql_system_tables.sql and mysql_system_tables_fix.sql
    are updated to define/alter the ndb_binlog_index table to use InnoDB.
    
    A new testcase ndb_binlog_purge_lock tests concurrent inserts
    during purging of the binary logs (which deletes rows in ndb_binlog_index).
    A new testcase ndb_binlog_purge_upgrade that first uses the mysql_upgrade
    script and then checks ndb_binlog_purge_lock.
    Updated ndb_rpl_circular to make scan order predictable.
    Updated ndb_binlog_index.result since it checks table engine.

--- scripts/mysql_system_tables_fix.sql
+++ scripts/mysql_system_tables_fix.sql
@@ -1,4 +1,4 @@
--- Copyright (c) 2003, 2015, Oracle and/or its affiliates. All rights reserved.
+-- Copyright (c) 2003, 2016, Oracle and/or its affiliates. All rights reserved.
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -832,6 +832,8 @@ ALTER TABLE ndb_binlog_index
   ADD COLUMN next_position BIGINT UNSIGNED NOT NULL;
 ALTER TABLE ndb_binlog_index
   ADD COLUMN next_file VARCHAR(255) NOT NULL;
+ALTER TABLE ndb_binlog_index
+  ENGINE=InnoDB;
 
 --
 -- Check for non-empty host table and issue a warning
