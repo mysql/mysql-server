651de05b820d964280719b85c5753a24e9d83095
Jon Olav Hauglid <jon.hauglid@oracle.com>
2016-05-24 09:16:23 +0200

    Bug#23212938 ADD SUPPORT FOR SOLARIS STUDIO 12.5 AKA 5.14
    Post-push fix: Fix broken build with MERGE_UNITTESTS=0

--- unittest/gunit/decimal-t.cc
+++ unittest/gunit/decimal-t.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011, 2015, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2011, 2016, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -21,10 +21,11 @@
 #include "my_config.h"
 #include <gtest/gtest.h>
 
-#include <my_global.h>
-#include <m_string.h>
+#include "my_global.h"
+#include "m_string.h"
 
-#include <decimal.h>
+#include "decimal.h"
+#include "my_decimal.h"
 
 
 namespace decimal_unittest {
