98cd8218dd5d51c1732cf70a0c918eb87a42fba1
Venkatesh Duggirala <venkatesh.duggirala@oracle.com>
2016-05-26 11:02:18 +0530

    Bug#23344451	INTERNAL CHANNEL PORT NUMBER SHOULD BE ALLOWED TO SET TO ZERO
    Problem1: there is no actual network usage of 'group_replication_applier'
    channel (internal channel) but channel is being setup with port number
    3306.
    Problem2: After group replication recovery is done, 'group_replication_recovery'
    channel (internal channel) configuration parameters are set to NULL to
    avoid confusion. But the port number is still showing non-zero value.
    
    Analysis:  group replication plugin code is already calling change_master
    with port number '0'. But in the server, port number is reset to given
    value only if it is non-zero value.
    
    Fix: Changing the change master logic to allow setting the port value
    to 'zero' if it is explicitly provided by the user (in this example group
    replication channels).

--- sql/sql_lex.h
+++ sql/sql_lex.h
@@ -306,8 +306,8 @@ typedef struct st_lex_master_info
     changed variable or if it should be left at old value
    */
   enum {LEX_MI_UNCHANGED= 0, LEX_MI_DISABLE, LEX_MI_ENABLE}
-    ssl, ssl_verify_server_cert, heartbeat_opt, repl_ignore_server_ids_opt, 
-    retry_count_opt, auto_position;
+    ssl, ssl_verify_server_cert, heartbeat_opt, repl_ignore_server_ids_opt,
+    retry_count_opt, auto_position, port_opt;
   char *ssl_key, *ssl_cert, *ssl_ca, *ssl_capath, *ssl_cipher;
   char *ssl_crl, *ssl_crlpath, *tls_version;
   char *relay_log_name;
