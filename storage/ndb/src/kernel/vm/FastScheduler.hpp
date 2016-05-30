b605b1e95730c7f8ea146b250cf400e733d32ccd
Frazer Clement <frazer.clement@oracle.com>
2016-05-30 23:57:13 +0100

    WL#7656 Per fragment locks information
    Three new Ndbinfo views added :
    
     - locks_per_fragment
    
       Companion table to operations_per_fragment and memory_per_fragment.
       Keyed in a similar way.
       Shows counts of lock claim requests, and their outcomes per
       fragment.  Shows total time spent waiting for locks successfully
       and unsuccessfully.
    
     - cluster_locks
    
       Companion table to cluster_operations.
       Keyed in a similar way.
       Shows current lock requests holding and waiting for locks.
       Allows stalls and deadlocks to be investigated.
    
     - server_locks
    
       Companion table to server_operations.
       Keyed in a similar way.
       Essentially a subset of cluster_operations, for transactions
       running on the local MySQLD, showing a connection id per
       transaction.
    
    The two tables are based on two underlying tables
      - ndb$frag_locks (locks_per_fragment)
      - ndb$acc_operations (cluster_locks, server_locks)
    
    Three new testcases added :
      - ndbinfo_locks_per_fragment
      - ndbinfo_cluster_locks
      - ndbinfo_server_locks

--- storage/ndb/src/kernel/vm/FastScheduler.hpp
+++ storage/ndb/src/kernel/vm/FastScheduler.hpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2015, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2016, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -148,6 +148,7 @@ public:
 
   /* Get/Set high resolution timer in microseconds */
   NDB_TICKS getHighResTimer() { return curr_ticks; }
+  const NDB_TICKS* getHighResTimerPtr() { return &curr_ticks; } 
   void setHighResTimer(NDB_TICKS ticks)
   { curr_ticks = ticks;}
 private:
