99140924e04ecd181263dececb3efb86b1fef1a8
Craig L Russell <craig.russell@oracle.com>
2016-05-30 17:17:05 -0700

    Fix typo in define guard

--- sql/sql_alter_instance.h
+++ sql/sql_alter_instance.h
@@ -14,7 +14,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 
 #ifndef SQL_ALTER_INSTANCE_INCLUDED
-#define SQL_ALTER_INSTNACE_INCLUDED
+#define SQL_ALTER_INSTANCE_INCLUDED
 
 class THD;
 /*
