1b5c104dd537cf23e939efcd12ef19a0b4b54459
Arun Kuruvila <arun.kuruvila@oracle.com>
2016-07-22 10:47:37 +0530

    Bug#22922023: PASSWORD VALIDATION PLUGIN FLAW - FAILED               PASSWORDS LOGGED IN CLEAR TEXT
    Description:- Passwords which fail validation by the
    password validation plugin are logged in mysqld error log
    as warning messages in clear text.
    
    Analysis:- Logging of warning message for completely failed
    "GRANT/REVOKE users" queries is added as part of another bug
    fix. The intention of that bug fix was to handle partial
    execution of queries by adding an incident event in the
    binary log. But along with addition of incident event in the
    binary log for partial execution, a warning message is also
    added for quires which fails completely.
    
    Fix:- Logging of warning message to the mysqld error log
    is removed which will in turn solve this issue.

--- sql/auth/sql_authorization.cc
+++ sql/auth/sql_authorization.cc
@@ -1478,8 +1478,7 @@ int mysql_table_grant(THD *thd, TABLE_LIST *table_list,
     additional privileges on the slave).
     Before ACLs are changed to execute fully or none at all, when
     some error happens, write an incident if one or more users are
-    granted/revoked successfully (it has a partial execution), a
-    warning if no user is granted/revoked successfully.
+    granted/revoked successfully (it has a partial execution).
   */
   if (result)
   {
@@ -1492,10 +1491,6 @@ int mysql_table_grant(THD *thd, TABLE_LIST *table_list,
         mysql_bin_log.write_incident(thd, true /* need_lock_log=true */,
                                      err_msg);
       }
-      else
-        sql_print_warning("Did not write failed '%s' into binary log while "
-                          "storing table level and column level grants in "
-                          "the privilege tables.", thd->query().str);
     }
   }
   else
@@ -1739,8 +1734,7 @@ bool mysql_routine_grant(THD *thd, TABLE_LIST *table_list, bool is_proc,
     /*
       Before ACLs are changed to execute fully or none at all, when
       some error happens, write an incident if one or more users are
-      granted/revoked successfully (it has a partial execution), a
-      warning if no user is granted/revoked successfully.
+      granted/revoked successfully (it has a partial execution).
     */
     if (result)
     {
@@ -1753,10 +1747,6 @@ bool mysql_routine_grant(THD *thd, TABLE_LIST *table_list, bool is_proc,
           mysql_bin_log.write_incident(thd, true /* need_lock_log=true */,
                                        err_msg);
         }
-        else
-          sql_print_warning("Did not write failed '%s' into binary log while "
-                            "storing routine level grants in the privilege "
-                            "tables.", thd->query().str);
       }
     }
     else
@@ -2009,8 +1999,7 @@ bool mysql_grant(THD *thd, const char *db, List <LEX_USER> &list,
   /*
     Before ACLs are changed to execute fully or none at all, when
     some error happens, write an incident if one or more users are
-    granted/revoked successfully (it has a partial execution), a
-    warning if no user is granted/revoked successfully.
+    granted/revoked successfully (it has a partial execution).
   */
   if (result)
   {
@@ -2023,10 +2012,6 @@ bool mysql_grant(THD *thd, const char *db, List <LEX_USER> &list,
         mysql_bin_log.write_incident(thd, true /* need_lock_log=true */,
                                      err_msg);
       }
-      else
-        sql_print_warning("Did not write failed '%s' into binary log while "
-                          "granting/revoking privileges in databases.",
-                          thd->query().str);
     }
   }
   else
@@ -3470,8 +3455,7 @@ user_end:
   /*
     Before ACLs are changed to execute fully or none at all, when
     some error happens, write an incident if one or more users are
-    revoked successfully (it has a partial execution), a warning
-    if no user is granted/revoked successfully.
+    revoked successfully (it has a partial execution).
   */
   if (result)
   {
@@ -3485,10 +3469,6 @@ user_end:
         mysql_bin_log.write_incident(thd, true /* need_lock_log=true */,
                                      err_msg);
       }
-      else
-        sql_print_warning("Did not write failed '%s' into binary log while "
-                          "revoking all_privileges from a list of users.",
-                          thd->query().str);
     }
   }
   else
