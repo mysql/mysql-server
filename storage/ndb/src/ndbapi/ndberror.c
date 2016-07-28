bac696ac33525015d571e9e11c894fe590485573
Priyanka Sangam <priyanka.sangam@oracle.com>
2016-07-28 15:02:27 +0530

    Bug #23748958 NDBMTD CRASH ON CREATE NODEGROUP WITH INVALID NODE ID (PTR TOO LARGE)
    When DBDIH receives a request to create a nodegroup, it checks
    the status of the node IDs passed in the CREATE NODEGROUP command.
    Out-of-range node IDs result in a crash since there is no bounds
    checking before node ID lookup. Added a check for out-of-range
    node IDs for CREATE NODEGROUP in DBDIH. Also added a new error code
    for invalid or undefined node IDs.

--- storage/ndb/src/ndbapi/ndberror.c
+++ storage/ndb/src/ndbapi/ndberror.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2004, 2014, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2004, 2016, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -438,6 +438,7 @@ ErrorBundle ErrorCodes[] = {
   { 322,  DMEC, AE, "Invalid node(s) specified for new nodegroup, node already in nodegroup" },
   { 323,  DMEC, AE, "Invalid nodegroup id, nodegroup already existing" },
   { 324,  DMEC, AE, "Invalid node(s) specified for new nodegroup, no node in nodegroup is started" },
+  { 325,  DMEC, AE, "Invalid node(s) specified for new nodegroup, node ID invalid or undefined" },
   { 417,  DMEC, AE, "Bad operation reference - double unlock" },
 
   /** 
