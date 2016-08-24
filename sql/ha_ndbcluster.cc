2ad5e1acabf3f01245b6208b57142ea4b15800a6
Ole John <ole.john.aske@oracle.com>
2016-08-24 13:27:00 +0200

    Bug#23553267 DEADLOCK WHEN MDL_EXCLUSIVE ACQUIRES A GLOBAL SCHEMA LOCK (GSL)
    Cherry pick of the server part of the patch from mysql-5.7
    In adition this commit adds the Cluster specific part of patch
    
    NdbCluster code for acquiring a Global Schema Lock has been changed
    to not retry the GSL lock if it failed due to a timeout (default 3000ms),
    *and* there is a potential for this lock request participating in a MDL-GSL
    deadlock. In these cases it select itself as a 'victim', and return
    this decission to the MDL requestor.
    
    MDL code will then either handle such a victimized GSL request either as
    a failed lock request - Which is better than staying in a deadlock state
    forever.
    
    Or, where a deadlock_handler exist in the MDL code, it will backof
    and retry the MDL+GSL locking again. (Valid for CREATE TABLE which
    was the soure for the deadlocks in the attached test case)

--- sql/ha_ndbcluster.cc
+++ sql/ha_ndbcluster.cc
@@ -1303,6 +1303,8 @@ Thd_ndb::Thd_ndb(THD* thd) :
 
 Thd_ndb::~Thd_ndb()
 {
+  DBUG_ASSERT(global_schema_lock_count == 0);
+
   if (opt_ndb_extra_logging > 1)
   {
     /*
@@ -10603,7 +10605,7 @@ int ha_ndbcluster::create(const char *name,
 
   Thd_ndb *thd_ndb= get_thd_ndb(thd);
 
-  if (!(thd_ndb->check_option(Thd_ndb::NO_GLOBAL_SCHEMA_LOCK) ||
+  if (!(thd_ndb->check_option(Thd_ndb::IS_SCHEMA_DIST_PARTICIPANT) ||
         thd_ndb->has_required_global_schema_lock("ha_ndbcluster::create")))
   {
     DBUG_RETURN(HA_ERR_NO_CONNECTION);
