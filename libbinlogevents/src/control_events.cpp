603ee8eeb9ff24e0338f4223f02200a029b0785b
Nuno Carvalho <nuno.carvalho@oracle.com>
2016-09-30 16:35:46 +0200

    BUG#24764113: TRANSACTION_CONTEXT_EVENT::WRITE_SET_LEN HAS NOT ENOUGH INTEGER SIZE
    On Group Replication transactions are executed optimistically and
    then, at commit time, are checked for conflicts. If there are
    conflicts, in order to maintain consistency across the group, some
    transactions will be rolled back.
    In order to detect conflicts, at commit time, each transaction data
    and write sets are sent to all members. It was discovered that
    serialization of the write sets to the network format was limited by
    a 16 bits integer, which is not enough to hold the size of big
    transactions write sets. This was causing write sets to be trim and
    conflict detection unable to find conflicts.
    
    In order to fix the above issue, now we do use a 32 bit integer on
    Transaction_context_log_event write set length which can hold big
    transactions write sets.
    Since we are already changing Transaction_context_log_event, we did
    also fix the size of thread_id field from 64 to 32 bits.

--- libbinlogevents/src/control_events.cpp
+++ libbinlogevents/src/control_events.cpp
@@ -707,15 +707,15 @@ Transaction_context_event(const char *buffer, unsigned int event_len,
   uint8_t server_uuid_len= (static_cast<unsigned int>
                            (data_head[ENCODED_SERVER_UUID_LEN_OFFSET]));
 
-  uint16_t write_set_len= 0;
+  uint32_t write_set_len= 0;
   memcpy(&write_set_len, data_head + ENCODED_WRITE_SET_ITEMS_OFFSET,
          sizeof(write_set_len));
-  write_set_len= le16toh(write_set_len);
+  write_set_len= le32toh(write_set_len);
 
-  uint16_t read_set_len= 0;
+  uint32_t read_set_len= 0;
   memcpy(&read_set_len, data_head + ENCODED_READ_SET_ITEMS_OFFSET,
          sizeof(read_set_len));
-  read_set_len= le16toh(read_set_len);
+  read_set_len= le32toh(read_set_len);
 
   encoded_snapshot_version_length= 0;
   memcpy(&encoded_snapshot_version_length,
@@ -724,7 +724,7 @@ Transaction_context_event(const char *buffer, unsigned int event_len,
   encoded_snapshot_version_length= le32toh(encoded_snapshot_version_length);
 
   memcpy(&thread_id, data_head + ENCODED_THREAD_ID_OFFSET, sizeof(thread_id));
-  thread_id= (uint64_t) le64toh(thread_id);
+  thread_id= (uint32_t) le32toh(thread_id);
   gtid_specified= (int8_t) data_head[ENCODED_GTID_SPECIFIED_OFFSET];
 
   const char *pos = data_head + TRANSACTION_CONTEXT_HEADER_LEN;
@@ -766,11 +766,11 @@ err:
             value.
 */
 const char* Transaction_context_event::read_data_set(const char *pos,
-                                                     uint16_t set_len,
+                                                     uint32_t set_len,
                                                      std::list<const char*> *set)
 {
   uint16_t len= 0;
-  for (int i= 0; i < set_len; i++)
+  for (uint32_t i= 0; i < set_len; i++)
   {
     memcpy(&len, pos, 2);
     len= le16toh(len);
