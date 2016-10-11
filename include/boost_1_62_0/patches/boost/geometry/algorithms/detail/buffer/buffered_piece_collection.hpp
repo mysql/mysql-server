b28a1651e75ea1183ef4a32509aa29bf75e74131
Norvald H. Ryeng <norvald.ryeng@oracle.com>
2016-10-11 12:33:23 +0200

    Bug#24579061 UPGRADE TO BOOST 1.62.0
    Upgrade dependency to 1.62.0 and remove those patches for Boost 1.60.0
    that are already included in 1.62.0. Add patches to fix bugs in Boost
    1.62.0.
    
    Also add patch to boost/exception/info.hpp to fix error C4099 (type
    first declared struct, then class) in Visual Studio. Add
    BOOST_PATCHES_DIR to the include path in X plugin.
    
    Remove test case for bug #20357097 since it no longer returns an
    error, but instead takes a long time to complete.

--- include/boost_1_62_0/patches/boost/geometry/algorithms/detail/buffer/buffered_piece_collection.hpp
+++ include/boost_1_62_0/patches/boost/geometry/algorithms/detail/buffer/buffered_piece_collection.hpp
@@ -2,6 +2,10 @@
 
 // Copyright (c) 2012-2014 Barend Gehrels, Amsterdam, the Netherlands.
 
+// This file was modified by Oracle on 2016.
+// Modifications copyright (c) 2016 Oracle and/or its affiliates.
+// Contributed and/or modified by Adam Wulkiewicz, on behalf of Oracle
+
 // Use, modification and distribution is subject to the Boost Software License,
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -31,6 +35,7 @@
 
 #include <boost/geometry/algorithms/detail/buffer/buffered_ring.hpp>
 #include <boost/geometry/algorithms/detail/buffer/buffer_policies.hpp>
+#include <boost/geometry/algorithms/detail/overlay/cluster_info.hpp>
 #include <boost/geometry/algorithms/detail/buffer/get_piece_turns.hpp>
 #include <boost/geometry/algorithms/detail/buffer/turn_in_piece_visitor.hpp>
 #include <boost/geometry/algorithms/detail/buffer/turn_in_original_visitor.hpp>
@@ -256,8 +261,11 @@ struct buffered_piece_collection
         {
             geometry::envelope(m_ring, m_box);
 
-            // create monotonic sections in y-dimension
-            typedef boost::mpl::vector_c<std::size_t, 1> dimensions;
+            // create monotonic sections in x-dimension
+            // The dimension is critical because the direction is later used
+            // in the optimization for within checks using winding strategy
+            // and this strategy is scanning in x direction.
+            typedef boost::mpl::vector_c<std::size_t, 0> dimensions;
             geometry::sectionalize<false, dimensions>(m_ring,
                     detail::no_rescale_policy(), m_sections);
         }
@@ -286,6 +294,15 @@ struct buffered_piece_collection
     // but also for large joins with many points
     typedef geometry::sections<robust_box_type, 2> sections_type;
     sections_type monotonic_sections;
+
+    // Define the clusters, mapping cluster_id -> turns
+    typedef std::map
+        <
+            signed_size_type,
+            detail::overlay::cluster_info
+        > cluster_type;
+
+    cluster_type m_clusters;
 
 
     RobustPolicy const& m_robust_policy;
@@ -480,7 +497,8 @@ struct buffered_piece_collection
                 // Within can have in rare cases a rounding issue. We don't discard this
                 // point, so it can be used to continue started rings in traversal. But
                 // will never start a new ring from this type of points.
-                it->selectable_start = false;
+                it->operations[0].enriched.startable = false;
+                it->operations[1].enriched.startable = false;
             }
 #endif
         }
@@ -932,7 +950,11 @@ struct buffered_piece_collection
         piece pc;
         pc.type = type;
         pc.index = static_cast<signed_size_type>(boost::size(m_pieces));
+
+        current_segment_id.piece_index = pc.index;
+
         pc.first_seg_id = current_segment_id;
+
 
         // Assign left/right (for first/last piece per ring they will be re-assigned later)
         pc.left_index = pc.index - 1;
@@ -1196,9 +1218,8 @@ struct buffered_piece_collection
             typename cs_tag<Ring>::type
         >::type side_strategy_type;
 
-        enrich_intersection_points<false, false, overlay_union>(m_turns,
-                    detail::overlay::operation_union,
-                    offsetted_rings, offsetted_rings,
+        enrich_intersection_points<false, false, overlay_buffer>(m_turns,
+                    m_clusters, offsetted_rings, offsetted_rings,
                     m_robust_policy, side_strategy_type());
     }
 
@@ -1214,7 +1235,7 @@ struct buffered_piece_collection
                 offsetted_rings[it->operations[0].seg_id.multi_index].has_discarded_intersections = true;
                 offsetted_rings[it->operations[1].seg_id.multi_index].has_discarded_intersections = true;
             }
-            else if (! it->both(detail::overlay::operation_union))
+            else
             {
                 offsetted_rings[it->operations[0].seg_id.multi_index].has_accepted_intersections = true;
                 offsetted_rings[it->operations[1].seg_id.multi_index].has_accepted_intersections = true;
@@ -1310,17 +1331,12 @@ struct buffered_piece_collection
         for (typename boost::range_iterator<turn_vector_type>::type it =
             boost::begin(m_turns); it != boost::end(m_turns); ++it)
         {
-            if (it->location != location_ok)
+            buffer_turn_info_type& turn = *it;
+            if (turn.location != location_ok)
             {
-                // Set it to blocked. They should not be discarded, to avoid
-                // generating rings over these turns
-                // Performance goes down a tiny bit from 161 s to 173 because there
-                // are sometimes much more turns.
-                // We might speed it up a bit by keeping only one blocked
-                // intersection per segment, but that is complex to program
-                // because each turn involves two segments
-                it->operations[0].operation = detail::overlay::operation_blocked;
-                it->operations[1].operation = detail::overlay::operation_blocked;
+                // Discard this turn (don't set it to blocked to avoid colocated
+                // clusters being discarded afterwards
+                turn.discarded = true;
             }
         }
     }
@@ -1332,13 +1348,15 @@ struct buffered_piece_collection
                 false, false,
                 buffered_ring_collection<buffered_ring<Ring> >,
                 buffered_ring_collection<buffered_ring<Ring > >,
+                overlay_buffer,
                 backtrack_for_buffer
             > traverser;
 
         traversed_rings.clear();
+        buffer_overlay_visitor visitor;
         traverser::apply(offsetted_rings, offsetted_rings,
-                        detail::overlay::operation_union,
-                        m_robust_policy, m_turns, traversed_rings);
+                        m_robust_policy, m_turns, traversed_rings,
+                        m_clusters, visitor);
 
         overlay::split_rings
             <
--- include/boost_1_62_0/patches/boost/geometry/algorithms/detail/overlay/overlay.hpp
+++ include/boost_1_62_0/patches/boost/geometry/algorithms/detail/overlay/overlay.hpp
@@ -24,6 +24,7 @@
 
 #include <boost/geometry/core/ring_type.hpp>
 
+#include <boost/geometry/algorithms/detail/overlay/cluster_info.hpp>
 #include <boost/geometry/algorithms/detail/overlay/enrich_intersection_points.hpp>
 #include <boost/geometry/algorithms/detail/overlay/enrichment_info.hpp>
 #include <boost/geometry/algorithms/detail/overlay/get_turns.hpp>
@@ -63,28 +64,50 @@ namespace detail { namespace overlay
 {
 
 
-template <typename TurnPoints, typename TurnInfoMap>
-inline void get_ring_turn_info(TurnInfoMap& turn_info_map,
-        TurnPoints const& turn_points)
+//! Default visitor for overlay, doing nothing
+struct overlay_null_visitor
 {
-    typedef typename boost::range_value<TurnPoints>::type turn_point_type;
-    typedef typename turn_point_type::container_type container_type;
+    void print(char const* ) {}
 
-    for (typename boost::range_iterator<TurnPoints const>::type
-            it = boost::begin(turn_points);
-         it != boost::end(turn_points);
+    template <typename Turns>
+    void print(char const* , Turns const& , int) {}
+
+    template <typename Turns>
+    void print(char const* , Turns const& , int , int ) {}
+
+    template <typename Turns>
+    void visit_turns(int , Turns const& ) {}
+
+    template <typename Clusters, typename Turns>
+    void visit_clusters(Clusters const& , Turns const& ) {}
+
+    template <typename Turns, typename Turn, typename Operation>
+    void visit_traverse(Turns const& , Turn const& , Operation const& , char const*)
+    {}
+
+    template <typename Turns, typename Turn, typename Operation>
+    void visit_traverse_reject(Turns const& , Turn const& , Operation const& , traverse_error_type )
+    {}
+};
+
+template <typename Turns, typename TurnInfoMap>
+inline void get_ring_turn_info(TurnInfoMap& turn_info_map, Turns const& turns)
+{
+    typedef typename boost::range_value<Turns>::type turn_type;
+    typedef typename turn_type::container_type container_type;
+
+    for (typename boost::range_iterator<Turns const>::type
+            it = boost::begin(turns);
+         it != boost::end(turns);
          ++it)
     {
-        typename boost::range_value<TurnPoints>::type const& turn_info = *it;
-        bool both_uu = turn_info.both(operation_union);
-        bool skip = (turn_info.discarded || both_uu)
-            && ! turn_info.any_blocked()
-            && ! turn_info.both(operation_intersection)
-            ;
+        typename boost::range_value<Turns>::type const& turn_info = *it;
 
-        if (! both_uu && turn_info.colocated)
+        if (turn_info.discarded
+            && ! turn_info.any_blocked()
+            && ! turn_info.colocated)
         {
-            skip = true;
+            continue;
         }
 
         for (typename boost::range_iterator<container_type const>::type
@@ -92,21 +115,13 @@ inline void get_ring_turn_info(TurnInfoMap& turn_info_map,
             op_it != boost::end(turn_info.operations);
             ++op_it)
         {
-            ring_identifier ring_id
+            ring_identifier const ring_id
                 (
                     op_it->seg_id.source_index,
                     op_it->seg_id.multi_index,
                     op_it->seg_id.ring_index
                 );
-
-            if (! skip)
-            {
-                turn_info_map[ring_id].has_normal_turn = true;
-            }
-            else if (both_uu)
-            {
-                turn_info_map[ring_id].has_uu_turn = true;
-            }
+            turn_info_map[ring_id].has_normal_turn = true;
         }
     }
 }
@@ -169,12 +184,12 @@ template
 class overlay
 {
 private:
-    template <typename RobustPolicy, typename OutputIterator, typename Strategy>
-    static inline OutputIterator do_overlay(
+    template <typename RobustPolicy, typename OutputIterator, typename Strategy, typename Visitor>    static inline OutputIterator do_overlay(
                 Geometry1 const& geometry1, Geometry2 const& geometry2,
                 RobustPolicy const& robust_policy,
                 OutputIterator out,
-                Strategy const& )
+                Strategy const& ,
+                Visitor& visitor)
     {
         bool const is_empty1 = geometry::is_empty(geometry1);
         bool const is_empty2 = geometry::is_empty(geometry2);
@@ -198,14 +213,21 @@ private:
             point_type,
             typename geometry::segment_ratio_type<point_type, RobustPolicy>::type
         > turn_info;
-        typedef std::deque<turn_info> container_type;
+        typedef std::deque<turn_info> turn_container_type;
 
         typedef std::deque
             <
                 typename geometry::ring_type<GeometryOut>::type
             > ring_container_type;
 
-        container_type turn_points;
+        // Define the clusters, mapping cluster_id -> turns
+        typedef std::map
+            <
+                signed_size_type,
+                cluster_info
+            > cluster_type;
+
+        turn_container_type turns;
 
 #ifdef BOOST_GEOMETRY_DEBUG_ASSEMBLE
 std::cout << "get turns" << std::endl;
@@ -215,19 +237,24 @@ std::cout << "get turns" << std::endl;
             <
                 Reverse1, Reverse2,
                 detail::overlay::assign_null_policy
-            >(geometry1, geometry2, robust_policy, turn_points, policy);
+            >(geometry1, geometry2, robust_policy, turns, policy);
+
+        visitor.visit_turns(1, turns);
 
 #ifdef BOOST_GEOMETRY_DEBUG_ASSEMBLE
 std::cout << "enrich" << std::endl;
 #endif
         typename Strategy::side_strategy_type side_strategy;
-        geometry::enrich_intersection_points<Reverse1, Reverse2, OverlayType>(turn_points,
-                OverlayType == overlay_union
-                    ? geometry::detail::overlay::operation_union
-                    : geometry::detail::overlay::operation_intersection,
-                    geometry1, geometry2,
+        cluster_type clusters;
+
+        geometry::enrich_intersection_points<Reverse1, Reverse2, OverlayType>(turns,
+                clusters, geometry1, geometry2,
                     robust_policy,
                     side_strategy);
+
+        visitor.visit_turns(2, turns);
+
+        visitor.visit_clusters(clusters, turns);
 
 #ifdef BOOST_GEOMETRY_DEBUG_ASSEMBLE
 std::cout << "traverse" << std::endl;
@@ -236,18 +263,17 @@ std::cout << "traverse" << std::endl;
         // Note that these rings are always in clockwise order, even in CCW polygons,
         // and are marked as "to be reversed" below
         ring_container_type rings;
-        traverse<Reverse1, Reverse2, Geometry1, Geometry2>::apply
+        traverse<Reverse1, Reverse2, Geometry1, Geometry2, OverlayType>::apply
                 (
                     geometry1, geometry2,
-                    OverlayType == overlay_union
-                        ? geometry::detail::overlay::operation_union
-                        : geometry::detail::overlay::operation_intersection,
                     robust_policy,
-                    turn_points, rings
+                    turns, rings,
+                    clusters,
+                    visitor
                 );
 
         std::map<ring_identifier, ring_turn_info> turn_info_per_ring;
-        get_ring_turn_info(turn_info_per_ring, turn_points);
+        get_ring_turn_info(turn_info_per_ring, turns);
 
         typedef ring_properties
         <
@@ -315,25 +341,27 @@ public:
                                                      modified_geometry2,
                                                      robust_policy);
 
+        overlay_null_visitor visitor;
+
         if (modified1 && modified2)
         {
             return do_overlay(modified_geometry1, modified_geometry2,
-                              robust_policy, out, strategy);
+                              robust_policy, out, strategy, visitor);
         }
         else if (! modified1 && modified2)
         {
             return do_overlay(geometry1, modified_geometry2,
-                              robust_policy, out, strategy);
+                              robust_policy, out, strategy, visitor);
         }
         else if (modified1 && ! modified2)
         {
             return do_overlay(modified_geometry1, geometry2,
-                              robust_policy, out, strategy);
+                              robust_policy, out, strategy, visitor);
         }
         else
         {
             return do_overlay(geometry1, geometry2,
-                              robust_policy, out, strategy);
+                              robust_policy, out, strategy, visitor);
         }
     }
 };
