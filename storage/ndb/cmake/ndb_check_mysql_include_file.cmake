fc450ccb711824f35193cc46d2a4dea074a28d9f
Tor Didriksen <tor.didriksen@oracle.com>
2016-10-24 13:24:07 +0200

    Bug#22561096 UPGRADE TO CMAKE 3.2.3 BROKE NDBCLUSTER WINDOWS BUILD IN MYSQL-TRUNK Bug#22572632 CMAKE WARNING IN NDB_CHECK_MYSQL_INCLUDE_FILE.CMAKE
    Backport to 5.7

--- storage/ndb/cmake/ndb_check_mysql_include_file.cmake
+++ storage/ndb/cmake/ndb_check_mysql_include_file.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2012, 2016, Oracle and/or its affiliates. All rights reserved.
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 MACRO(NDB_CHECK_MYSQL_INCLUDE_FILE INCLUDE VARIABLE)
-  IF("${VARIABLE}" MATCHES "^${VARIABLE}$")
+  IF("${${VARIABLE}}" MATCHES "^${${VARIABLE}}$")
     SET(_msg "Looking for MySQL include file ${INCLUDE}")
     MESSAGE(STATUS "${_msg}")
     IF(EXISTS "${CMAKE_SOURCE_DIR}/include/${INCLUDE}")
