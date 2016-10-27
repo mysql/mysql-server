172ad94132c7c19ea22d2a22eb22da44144c1db6
Catalin Besleaga <catalin.besleaga@oracle.com>
2016-10-27 16:07:10 +0200

    Bug#24930829 BITWISE OPERATORS: CRASH IN ITEM_FUNC_BIT_COUNT::VAL_INT
    Problem:
    -------
    Item_func_json_depth::val_int sets null_value= true even though
    maybe_null is false.
    
    Solution:
    --------
    returned error_int as it will set the null_value accordingly.
    
    Change-Id: I23ca1cb7849ffe1d19e916f0d326a98dd26c6232

--- sql/item_json_func.cc
+++ sql/item_json_func.cc
@@ -1858,19 +1858,13 @@ longlong Item_func_json_length::val_int()
 
 longlong Item_func_json_depth::val_int()
 {
-  DBUG_ASSERT(fixed == 1);
-  longlong result= 0;
-
+  DBUG_ASSERT(fixed);
   Json_wrapper wrapper;
 
   try
   {
-    if (get_json_wrapper(args, 0, &m_doc_value, func_name(), &wrapper) ||
-        args[0]->null_value)
-    {
-      null_value= true;
-      return 0;
-    }
+    if (get_json_wrapper(args, 0, &m_doc_value, func_name(), &wrapper))
+      return error_int();
   }
   catch (...)
   {
@@ -1880,10 +1874,11 @@ longlong Item_func_json_depth::val_int()
     /* purecov: end */
   }
 
-  result= wrapper.depth(current_thd);
+  null_value= args[0]->null_value;
+  if (null_value)
+    return 0;
 
-  null_value= false;
-  return result;
+  return wrapper.depth(current_thd);
 }
 
 
