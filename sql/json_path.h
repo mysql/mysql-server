0d04167ef6ee57daec125012604fb5daac02a67c
Knut Anders Hatlen <knut.hatlen@oracle.com>
2016-11-03 12:28:36 +0100

    Bug#25039800: COMPILE WARNINGS IN JSON CODE WHEN USING CLANG 3.9
    Problem: Clang 3.9 emits the following warning: "destructor called on
    non-final 'Json_path' that has virtual functions but non-virtual
    destructor"
    
    Fix: Add a virtual destructor to Json_path's base class.
    
    This fix is for mysql-5.7 only. The destructor is already virtual in
    mysql-trunk.

--- sql/json_path.h
+++ sql/json_path.h
@@ -1,7 +1,7 @@
 #ifndef SQL_JSON_PATH_INCLUDED
 #define SQL_JSON_PATH_INCLUDED
 
-/* Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2015, 2016, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -87,6 +87,7 @@ public:
 class Json_seekable_path
 {
 public:
+  virtual ~Json_seekable_path() {}
 
   /** Return the number of legs in this searchable path */
   virtual size_t leg_count() const =0;
