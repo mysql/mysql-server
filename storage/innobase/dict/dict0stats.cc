7a2585002037e94dccfb915db9af25b8433d4df3
Knut Anders Hatlen <knut.hatlen@oracle.com>
2016-11-04 13:44:36 +0100

    Bug#25048573: STD::MAP INSTANTIATIONS CAUSE STATIC ASSERT FAILURES ON FREEBSD 11
    Problem: Some instantiations of std::map have discrepancies between
    the value_type of the map and the value_type of the map's allocator.
    On FreeBSD 11 this is detected by Clang, and an error is raised at
    compilation time.
    
    Fix: Specify the correct value_type for the allocators.
    
    Also fix an unused variable warning in storage/innobase/os/os0file.cc.
    
    (cherry picked from commit 84407083481e8d98751252fa87501ec27f87ec48)

--- storage/innobase/dict/dict0stats.cc
+++ storage/innobase/dict/dict0stats.cc
@@ -139,7 +139,7 @@ then we would store 5,7,10,11,12 in the array. */
 typedef std::vector<ib_uint64_t, ut_allocator<ib_uint64_t> >	boundaries_t;
 
 /** Allocator type used for index_map_t. */
-typedef ut_allocator<std::pair<const char*, dict_index_t*> >
+typedef ut_allocator<std::pair<const char* const, dict_index_t*> >
 	index_map_t_allocator;
 
 /** Auxiliary map used for sorting indexes by name in dict_stats_save(). */
