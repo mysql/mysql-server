f1ab27886886c70218e2b29c60375ef4772536d5
Ole John Aske <ole.john.aske@oracle.com>
2016-12-01 10:15:15 +0100

    Bug #25056166 REFACTOR: MOVE M_LOCKED_CLIENTS[] FROM POLLQUEUE TO CLASS TRANSPORTERFACADE
    1) Move the ::m_locked_client[] array and ::m_locked_cnt from
       trp_client::pollQueue into TransporterFacade. These two
       members could only be operated on by the client being the
       poll_owner - Thus it is sufficient to have one instance of
       these member variables, instead of duplication them in each trp_client.
    
    2) The PollQueue methods ::lock_client(), ::check_if_locked()
       and ::start:poll() operating on the members in 1), has
       been made TransporterFacade members instead.
    
    3) The existing methods trp_client::start_poll() and
       TransporterFacade::start_poll() has been renamed to ::prepare_poll().
       This better reflect their real functionality as no polling does
       'start' until it has become poll_owner, else it will just wait in the
       poll queue without doing any 'poll' at all.
       Renaming also seperate it from the ::start_poll() from 2), which
       actually does 'start' the poll.
    
    4) Replace PollQueue::assert_destroy() with PollQueue d'tor.
    
    5) Complete the c'tors for trp_client and trp_client::PollQueue.
    
    6) Remove the TransporterFacade methods ::prepare_poll()
       and ::complete_poll(). These were both only used from the
       trp_client methods ::prepare_poll() and ::complete_poll().
       They didn't really do anything except asserting
       on some trp_client::PollQueue memebers, which is better
       to do from the trp_client methods.
    
    7) Moved TransporterFacade::start_poll() to right before
       its cousine TransporterFacade::finish_poll()

--- storage/ndb/src/ndbapi/trp_client.hpp
+++ storage/ndb/src/ndbapi/trp_client.hpp
@@ -43,11 +43,10 @@ public:
   virtual void trp_wakeup()
     {};
 
-  Uint32 open(class TransporterFacade*, int blockNo = -1,
-              bool receive_thread = false);
+  Uint32 open(class TransporterFacade*, int blockNo = -1);
   void close();
 
-  void start_poll();
+  void prepare_poll();
   void do_poll(Uint32);
   void complete_poll();
   void wakeup();
@@ -127,7 +126,7 @@ private:
   struct PollQueue
   {
     PollQueue();
-    void assert_destroy() const;
+    ~PollQueue();
 
     /**
      * PQ_WAITING - trp_client either waits in the poll-queue, or
@@ -147,19 +146,6 @@ private:
     trp_client *m_prev;
     trp_client *m_next;
     NdbCondition * m_condition;
-
-    /**
-     * This is called by poll owner
-     *   before doing external poll
-     */
-    void start_poll(trp_client* self);
-
-    void lock_client(trp_client*);
-    bool check_if_locked(const trp_client*,
-                         const Uint32 start) const;
-    Uint32 m_locked_cnt;
-    Uint32 m_lock_array_size;
-    trp_client ** m_locked_clients;
   } m_poll;
 
   /**
@@ -194,7 +180,7 @@ inline
 bool
 trp_client::check_if_locked()
 {
-  return m_facade->m_poll_owner->m_poll.check_if_locked(this, (Uint32)0);
+  return m_facade->check_if_locked(this, 0);
 }
 
 inline
@@ -203,7 +189,7 @@ trp_client::lock_client()
 {
   if (!check_if_locked())
   {
-    m_facade->m_poll_owner->m_poll.lock_client(this);
+    m_facade->lock_client(this);
   }
 }
 
@@ -276,45 +262,6 @@ trp_client::raw_sendFragmentedSignal(const NdbApiSignal * signal, Uint32 nodeId,
 {
   assert(m_poll.m_locked);
   return m_facade->sendFragmentedSignal(this, signal, nodeId, ptr, secs);
-}
-
-inline
-trp_client::PollQueue::PollQueue()
-{
-  m_waiting = PQ_IDLE;
-  m_locked = false;
-  m_poll_owner = false;
-  m_poll_queue = false;
-  m_next = 0;
-  m_prev = 0;
-  m_condition = NdbCondition_Create();
-  m_locked_cnt = 0;
-  m_lock_array_size = 0;
-  m_locked_clients = NULL;
-}
-
-inline
-void
-trp_client::PollQueue::assert_destroy() const
-{
-  if (m_waiting != PQ_IDLE ||
-      m_locked == true ||
-      m_poll_owner == true ||
-      m_poll_queue == true ||
-      m_next != 0 ||
-      m_prev != 0 ||
-      m_locked_cnt != 0)
-  {
-    ndbout << "ERR: ~trp_client: Deleting trp_clnt in use: waiting"
-	   << m_waiting
-	   << " locked  " << m_locked
-	   << " poll_owner " << m_poll_owner
-	   << " poll_queue " << m_poll_queue
-	   << " next " << m_next
-	   << " prev " << m_prev
-	   << " condition " << m_condition << endl;
-    require(false);
-  }
 }
 
 #endif
