7df7fe773741677ee945456a009ed58b6eb66c35
John David Duncan <john.duncan@oracle.com>
2016-12-12 13:00:56 -0800

    wl#9819 patch #7: Define new signal ProcessInfoRep  Define GSN  Define signal data structure  Define debugger print function

---
+++ storage/ndb/include/kernel/signaldata/ProcessInfoRep.hpp
@@ -0,0 +1,44 @@
+/*
+   Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+#ifndef NDB_PROCESSINFO_REP_H
+#define NDB_PROCESSINFO_REP_H
+
+#include "SignalData.hpp"
+
+class ProcessInfoRep {
+  friend class ClusterMgr;     // Sender
+  friend class Qmgr;           // Receiver
+  friend class ProcessInfo;    // Stored format
+  friend bool printPROCESSINFO_REP(FILE *, const Uint32 *, Uint32, Uint16);
+
+public:
+  STATIC_CONST( SignalLength = 16);
+  STATIC_CONST( ConnNameSectionNum = 0);
+  STATIC_CONST( HostAddrSectionNum = 1);
+
+private:
+  Uint8 process_name[48];
+  Uint32 node_id;
+  Uint32 process_id;
+  Uint32 angel_process_id;
+  Uint32 application_port;
+};
+
+// connection_name and host_address, if set, are sent as separate sections
+
+#endif
