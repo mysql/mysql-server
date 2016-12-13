5dcb8ea751d6352cad954833b4d063c77d340978
sensssz <hjmsens@gmail.com>
2016-12-12 23:59:56 -0500

    Handle NULL pointer in is_replication_slave.

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -1380,7 +1380,7 @@ thd_is_replication_slave_thread(
 /*============================*/
 	THD*	thd)	/*!< in: thread handle */
 {
-	return((ibool) thd_slave_thread(thd));
+	return(thd && (ibool) thd_slave_thread(thd));
 }
 
 /******************************************************************//**
