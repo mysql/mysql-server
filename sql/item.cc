268bb69beaec027b9f713d13316aa78c5c292817
Sergei Petrunia <psergey@askmonty.org>
2016-12-16 17:08:31 +0300

    MDEV-7691: Assertion `outer_context || !*from_field || *from_field == not_found_field' ...
    The bug occurred when a subquery
    - has a reference to outside, to grand-parent query or further up
    - is converted to a semi-join (i.e. merged into its parent).
    
    Then the reference to outside had form Item_ref(Item_field(...)).
    - Conversion to semi-join would call item->fix_after_pullout() for the
      outside reference.
    - Item_ref::fix_after_pullout would call Item_field->fix_after_pullout
    - The Item_field would construct a new Name_resolution_context object
      This process ignored the fact that the Item_field does not belong to
      any of the subselects being flattened.
    The result was crash in the next call to Item_field::fix_fields(), where
    we would try to use an invalid Name_resolution_context object.
    
    Fixed by not creating Name_resolution_context object if the Item_field's
    context does not belong to the subselect(s) that were flattened.

--- sql/item.cc
+++ sql/item.cc
@@ -2777,6 +2777,44 @@ void Item_field::fix_after_pullout(st_select_lex *new_parent, Item **ref)
     depended_from= NULL;
   if (context)
   {
+    bool need_change= false;
+    /*
+      Suppose there are nested selects:
+
+       select_id=1
+         select_id=2
+           select_id=3  <----+
+             select_id=4    -+
+               select_id=5 --+
+
+      Suppose, pullout operation has moved anything that had select_id=4 or 5
+      in to select_id=3.
+
+      If this Item_field had a name resolution context pointing into select_lex
+      with id=4 or id=5, it needs a new name resolution context.
+
+      However, it could also be that this object is a part of outer reference:
+      Item_ref(Item_field(field in select with select_id=1))).
+      - The Item_ref object has a context with select_id=5, and so needs a new
+        name resolution context.
+      - The Item_field object has a context with select_id=1, and doesn't need
+        a new name resolution context.
+
+      So, the following loop walks from Item_field's current context upwards.
+      If we find that the select we've been pulled out to is up there, we
+      create the new name resolution context. Otherwise, we don't.
+    */
+    for (Name_resolution_context *ct= context; ct; ct= ct->outer_context)
+    {
+      if (new_parent == ct->select_lex)
+      {
+        need_change= true;
+        break;
+      }
+    }
+    if (!need_change)
+      return;
+
     Name_resolution_context *ctx= new Name_resolution_context();
     if (context->select_lex == new_parent)
     {
