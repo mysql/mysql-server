4901a9fc6384ea727594e7058dd205064126ebc1
Magnus Blåudd <magnus.blaudd@oracle.com>
2017-01-18 08:46:09 +0100

    Bug #24710065: SPLIT MY_GLOBAL.H
      - Build failure of the ndbapi example ndbapi_event after 'run include-
        the #includes (and nothing else); then remove "my_dbug.h" from
    my_global.h.'
     - Fix by adding missing #include my_dbug.h in ndbapi_event.cpp

--- storage/ndb/ndbapi-examples/ndbapi_event/ndbapi_event.cpp
+++ storage/ndb/ndbapi-examples/ndbapi_event/ndbapi_event.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2005, 2014, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2005, 2017, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -57,6 +57,7 @@
 #include <stdio.h>
 #include <iostream>
 #ifdef VM_TRACE
+#include <my_dbug.h>
 #include <my_global.h>
 #endif
 #ifndef assert
