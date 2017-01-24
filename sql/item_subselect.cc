46eef1ede2ddfceaa056a71ea52ecacdde2bc44e
Igor Babaev <igor@askmonty.org>
2017-01-23 19:40:22 -0800

    Fixed bug mdev-11859.
    As the function Item_subselect::fix_fields does it the function
    Item_subselect::update_used_tables must ignore UNCACHEABLE_EXPLAIN
    when deciding whether the subquery item should be considered as a
    constant item.

--- sql/item_subselect.cc
+++ sql/item_subselect.cc
@@ -878,7 +878,7 @@ void Item_subselect::update_used_tables()
   if (!forced_const)
   {
     recalc_used_tables(parent_select, FALSE);
-    if (!engine->uncacheable())
+    if (!(engine->uncacheable() & ~UNCACHEABLE_EXPLAIN))
     {
       // did all used tables become static?
       if (!(used_tables_cache & ~engine->upper_select_const_tables()))
