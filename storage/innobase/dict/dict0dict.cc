2d0cf54f31523664a9a8dcc4161138dc8a7ceab1
Bin Su <bin.x.su@oracle.com>
2017-01-24 10:38:10 +0800

    WL#9532: Don't set dirty_status to METADATA_CLEAN if it's the second time to apply dynamic metadata, because the status could be already METADATA_BUFFERED.

--- storage/innobase/dict/dict0dict.cc
+++ storage/innobase/dict/dict0dict.cc
@@ -5828,13 +5828,15 @@ dict_table_load_dynamic_metadata(
 					 table);
 			table->dirty_status = METADATA_BUFFERED;
 			ut_d(table->in_dirty_dict_tables_list = true);
-		} else {
-			/* The row in DDTableBuffer is invalid, which
-			is due to the corrupted index marked has been dropped.
-			We can remove it, but this is in load table,
-			it should be in read only mode, so we just keep it */
-			table->dirty_status = METADATA_CLEAN;
 		}
+		/* If !is_dirty, it could be either:
+		1. It's first time to load this table, and the corrupted
+		index marked has been dropped. Current dirty_status should
+		be METADATA_CLEAN.
+		2. It's the second time to apply dynamic metadata to this
+		table, current in-memory dynamic metadata is up-to-date.
+		Current dirty_status should be METADATA_BUFFERED.
+		In both cases, we don't have to change the dirty_status */
 	}
 
 	mutex_exit(&dict_persist->mutex);
