ee8b04fe168173f93faef8f0f47ba1a846a996d3
Mauritz Sundell <mauritz.sundell@oracle.com>
2017-02-01 15:50:02 +0100

    Bug#25253540 LOOP OPTIMIZATION COMPILE BUG ON SPARC WITH GCC 5.3.0
    Detect GNU C/C++ 5.3.0 Bug 78807 - Loop optimization trigger bus error
    
    See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=78807
    
    If compiler bug detected using test program, some tree loop optimizations
    are turned off.
    
    Two CMake functions CHECK_C_COMPILER_BUG and CHECK_CXX_COMPILER_BUG are
    added that first check if bug is detected, and if so checks if given
    workaround works if not build is aborted otherwise the workaround flags
    are appended to compile flags.

--- cmake/build_configurations/compiler_options.cmake
+++ cmake/build_configurations/compiler_options.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2012, 2016, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2012, 2017, Oracle and/or its affiliates. All rights reserved.
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -35,19 +35,12 @@ IF(UNIX)
     IF(WITH_VALGRIND)
       SET(COMMON_C_FLAGS             "-fno-inline ${COMMON_C_FLAGS}")
     ENDIF()
-    # Disable expensive-optimization if shift-or-optimization bug effective
-    IF(HAVE_C_SHIFT_OR_OPTIMIZATION_BUG)
-      SET(C_NO_EXPENSIVE_OPTIMIZATIONS TRUE)
-    ENDIF()
     # Disable floating point expression contractions to avoid result differences
     IF(HAVE_C_FLOATING_POINT_FUSED_MADD)
       SET(COMMON_C_FLAGS "${COMMON_C_FLAGS} -ffp-contract=off")
     ENDIF()
     IF(NOT DISABLE_SHARED)
       SET(COMMON_C_FLAGS             "-fPIC ${COMMON_C_FLAGS}")
-    ENDIF()
-    IF(C_NO_EXPENSIVE_OPTIMIZATIONS)
-      SET(COMMON_C_FLAGS "${COMMON_C_FLAGS} -fno-expensive-optimizations")
     ENDIF()
     SET(CMAKE_C_FLAGS_DEBUG          "${COMMON_C_FLAGS}")
     SET(CMAKE_C_FLAGS_RELWITHDEBINFO "-O3 ${COMMON_C_FLAGS}")
@@ -58,19 +51,12 @@ IF(UNIX)
     IF(WITH_VALGRIND)
       SET(COMMON_CXX_FLAGS             "-fno-inline ${COMMON_CXX_FLAGS}")
     ENDIF()
-    # Disable expensive-optimization if shift-or-optimization bug effective
-    IF(HAVE_CXX_SHIFT_OR_OPTIMIZATION_BUG)
-      SET(CXX_NO_EXPENSIVE_OPTIMIZATIONS TRUE)
-    ENDIF()
     # Disable floating point expression contractions to avoid result differences
     IF(HAVE_CXX_FLOATING_POINT_FUSED_MADD)
       SET(COMMON_CXX_FLAGS "${COMMON_CXX_FLAGS} -ffp-contract=off")
     ENDIF()
     IF(NOT DISABLE_SHARED)
       SET(COMMON_CXX_FLAGS             "-fPIC ${COMMON_CXX_FLAGS}")
-    ENDIF()
-    IF(CXX_NO_EXPENSIVE_OPTIMIZATIONS)
-      SET(COMMON_CXX_FLAGS "${COMMON_CXX_FLAGS} -fno-expensive-optimizations")
     ENDIF()
     SET(CMAKE_CXX_FLAGS_DEBUG          "${COMMON_CXX_FLAGS}")
     SET(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O3 ${COMMON_CXX_FLAGS}")
@@ -145,3 +131,16 @@ IF(UNIX)
     ENDIF()
   ENDIF()
 ENDIF()
+
+SET(CMAKE_C_FLAGS_DEBUG
+      "${CMAKE_C_FLAGS_DEBUG} ${COMMON_C_WORKAROUND_FLAGS}")
+SET(CMAKE_CXX_FLAGS_DEBUG
+      "${CMAKE_CXX_FLAGS_DEBUG} ${COMMON_CXX_WORKAROUND_FLAGS}")
+SET(CMAKE_C_FLAGS_RELWITHDEBINFO
+      "${CMAKE_C_FLAGS_RELWITHDEBINFO} ${COMMON_C_WORKAROUND_FLAGS}")
+SET(CMAKE_CXX_FLAGS_RELWITHDEBINFO
+      "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} ${COMMON_CXX_WORKAROUND_FLAGS}")
+SET(CMAKE_C_FLAGS_RELEASE
+      "${CMAKE_C_FLAGS_RELEASE} ${COMMON_C_WORKAROUND_FLAGS}")
+SET(CMAKE_CXX_FLAGS_RELEASE
+      "${CMAKE_CXX_FLAGS_RELEASE} ${COMMON_CXX_WORKAROUND_FLAGS}")
