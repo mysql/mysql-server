7efac8141f19ae555b877ccbf731da8fbc9736d2
Anushree Prakash B <anushree.prakash.b@oracle.com>
2017-02-10 11:50:18 +0530

    Bug#25522709 - MAIN.MYSQLHOTCOPY_ARCHIVE AND                MAIN.MYSQLHOTCOPY_MYISAM TESTS FAILING                IN PB2 IN 5.6
    DESCRIPTION:
    ============
    main.mysqlhotcopy_archive and main.mysqlhotcopy_myisam tests
    are failing in PB2 weekly-5.6 on the freebsd platforms.
    
    ANALYSIS:
    ========
    This is caused by an explicit shebang in
    checkDBI_DBD-mysql.pl and in mysqlhotcopy.sh using
    #!/usr/bin/perl which is not correct on a standard install
    of FreeBSD 11. This results in a failure of the script
    execution.
    
    FIX:
    ====
    The fix is to use a portable shebang -> #!/usr/bin/env perl
    instead of #!/usr/bin/perl.

--- scripts/mysqlhotcopy.sh
+++ scripts/mysqlhotcopy.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
-# Copyright (c) 2000, 2010, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2000, 2017, Oracle and/or its affiliates. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Library General Public
