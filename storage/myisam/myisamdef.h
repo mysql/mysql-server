7349af1645e5aec64d0d78ad807e5ef6929c7e59
Steinar H. Gunderson <steinar.gunderson@oracle.com>
2017-02-22 14:37:29 +0100

    Bug #25589691: FIX MISUSES OF MY_BOOL
    Fix a bit mask being stored as my_bool.
    
    Change-Id: Id6eb144290027587e6a1a5f16e2d896360039362

--- storage/myisam/myisamdef.h
+++ storage/myisam/myisamdef.h
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2000, 2016, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2000, 2017, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -306,7 +306,7 @@ struct st_myisam_info {
   my_bool quick_mode;
   my_bool page_changed;		/* If info->buff can't be used for rnext */
   my_bool buff_used;		/* If info->buff has to be reread for rnext */
-  my_bool once_flags;           /* For MYISAMMRG */
+  uint8 once_flags;             /* For MYISAMMRG */
 
   /* Used in mi_rnext_same to fill rnext_same_key for the first time. */
     my_bool set_rnext_same_key;
