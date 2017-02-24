208d29b3b4b3e8b2e50fec3cf066ab62453d6412
Mikael Ronström <mikael.ronstrom@oracle.com>
2017-02-24 12:46:21 +0100

    WL#9864, BUG#25050703: Fix crash logic such that we can always survive single computer failure and can handle multiple sequential failures with 3-4 replicas

--- storage/ndb/src/kernel/blocks/qmgr/Qmgr.hpp
+++ storage/ndb/src/kernel/blocks/qmgr/Qmgr.hpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2014, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2017, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -452,8 +452,10 @@ private:
   void stateArbitRun(Signal* signal);
   void stateArbitChoose(Signal* signal);
   void stateArbitCrash(Signal* signal);
+  Uint32 count_previously_alive_nodes();
   void computeArbitNdbMask(NodeBitmaskPOD& aMask);
   void computeArbitNdbMask(NdbNodeBitmaskPOD& aMask);
+  void computeNonDiedNdbMask(NdbNodeBitmaskPOD& aMask);
   void reportArbitEvent(Signal* signal, Ndb_logevent_type type,
                         const NodeBitmask mask = NodeBitmask());
 
