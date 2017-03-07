9d462f51c6db1baeb01e0410bab8c6c3efb344fa
Ole-Hjalmar Kristensen <Ole-Hjalmar.Kristensen@oracle.com>
2017-03-07 18:42:21 +0100

    Bug#22671846: SENDING LARGE MESSAGES CAN TRIGGER NODE EXIT FROM GROUP
    Problem:
    
    When large messages are sent, the XCOM thread becomes busy for a long
    time sending it. As this happens, mainly as the number of member
    nodes grows, the alive task can trigger a group view change and GCS
    can trigger a node exit due to the 5 second timeout period.
    
    Solution:
    
    For large messages, the select will mark the socket as readable many
    times for a single message, since TCP sends a large message as
    multiple fragments anyway. Each time this happens, the
    acceptor_learner task is activated and will call read_msg/read_bytes.
    Until now we have updated only when a complete messages has been
    received, which may take a long time. By updating the server timestamp
    inside the loop in read_bytes/buffered_read_bytes, the timestamp will
    be updated often enough that nodes are not marked as being
    unavailable.

--- rapid/plugin/group_replication/libmysqlgcs/src/bindings/xcom/xcom/xcom_detector.h
+++ rapid/plugin/group_replication/libmysqlgcs/src/bindings/xcom/xcom/xcom_detector.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015, 2016, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2015, 2017, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -24,7 +24,6 @@ extern "C" {
 typedef double	detector_state[NSERVERS];
 struct site_def;
 
-void note_detected(struct site_def const *site, node_no node);
 int	may_be_dead(detector_state const ds, node_no i, double seconds);
 void init_detector(detector_state ds);
 void invalidate_detector_sites(struct site_def *site);
