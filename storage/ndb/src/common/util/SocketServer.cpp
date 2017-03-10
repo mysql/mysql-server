f245ad2eb0234d6f2229b68bb4f0d96675047e3b
John David Duncan <john.duncan@oracle.com>
2017-03-10 12:00:15 -0800

    wl#9819 Version 2.  Patch #3: ProcessInfo, OwnProcessInfo, and SocketServer

--- storage/ndb/src/common/util/SocketServer.cpp
+++ storage/ndb/src/common/util/SocketServer.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2016, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2017, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -142,7 +142,7 @@ SocketServer::setup(SocketServer::Service * service,
     DBUG_RETURN(false);
   }
   *port = ntohs(serv_addr.sin_port);
-  setOwnProcessInfoServerAddress(&serv_addr);
+  setOwnProcessInfoServerAddress(& serv_addr.sin_addr);
 
   DBUG_PRINT("info",("bound to %u", *port));
 
