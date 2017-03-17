6b690d13f1a06d700a24b40b9ae224d31c7b30b5
Srikanth B R <srikanth.b.r@oracle.com>
2017-03-17 14:17:59 +0530

    Bug#25349924 MTR: FIXES AND ENHANCEMENTS TO THE --XML-REPORT OPTION
    Issues
    ------
    The XML report generated by MTR when the --xml-report option is used has
    the below problems :-
    - It does not report any test which has failed in one of the retries
      but has passed on the final attempt as a failure i.e., it is
      reported like a successful test making usage of the option with
      --retry > 1 ambiguous
    - It does not distinguish between disabled and skipped tests
    - The xml reports are generated in the source directory 'mysql-test'
      even for out-of-source builds
    - Testcase tags lack information about combinations.
    - There can be multiple <testsuite tags> for a single suite scattered
      accross the report such that each tag has a bunch of tests belonging
      to a suite.
    - The <failure> tag does not include any information regarding failed tests
    
    Changes
    -------
    The patch does the following :-
    - Reports tests failing on any retry attempt with the <failure> tag
    - Adds statistics and fields for skipped tests and disabled tests separately
    - Creates xml report in build directory if no absolute path is given for
      out-of-source builds
    - Adds a field named variation in the <testcase> tag in case it is a
      combination run
    - Aggregate results of all tests belonging to a suite within a single
      <testsuite> tag
    - Generates and reports information regarding failures with a brief reason
      in an attribute named 'message' and embeds other details within the tag
    
    Reviewed-by: Pavan Naik <pavan.naik@oracle.com>
    RB: 15016

--- mysql-test/lib/mtr_report.pm
+++ mysql-test/lib/mtr_report.pm
@@ -254,98 +254,212 @@ sub mtr_report_test ($) {
 
 
 sub mtr_generate_xml_report($) {
-  my ($tests) = @_;
-  my $tsuite;
-  my $tname;
+  my ($tests)= @_;
   my $suite_group;
 
-  my $all_t = 0;
-  my $all_f = 0;
-  my $all_d = 0;
-  my $all_e = 0;
-  my $all_time = 0;
+  # ------------------------------------
+  # Calculate suitewise/total statistics
+  # ------------------------------------
 
-  my @suite_t;
-  my @suite_f;
-  my @suite_d;
-  my @suite_e;
-  my @suite_time;
+  my %suite_stats;
+  my %total_stats;
+  my @stat_list= ('tests', 'failures', 'disabled', 'skipped',
+                  'errors', 'time');
 
+  # Initialize overall statistics
+  map { $total_stats{$_}= 0 } @stat_list;
 
-  # calculate totals
-  foreach my $tinfo (@$tests) 
+  # Sort in order to group tests suitewise
+  my @sorted_tests= sort { $a->{'name'} cmp $b->{'name'} } @$tests;
+
+  foreach my $tinfo (@sorted_tests)
   {
-    my @parts = split /\./, $tinfo->{'name'};
-    $tsuite = $parts[0];
-    $tname = $parts[1];
+    my ($tsuite, $tname) = split /\./, $tinfo->{'name'};
 
+    # Initialize suitewise statistics for new suites
     if ($tsuite ne $suite_group)
     {
-      push(@suite_t, 0);
-      push(@suite_f, 0);
-      push(@suite_d, 0);
-      push(@suite_e, 0);
-      push(@suite_time, 0);
-      $suite_group = $tsuite;
+      map{ $suite_stats{$tsuite}{$_}= 0 } @stat_list;
+      $suite_group= $tsuite;
     }
 
-    $all_t++;
-    $suite_t[-1]++;
+    # Increment test counters
+    $suite_stats{$tsuite}{'tests'}++;
+    $total_stats{'tests'}++;
 
-    if ( $tinfo->{'result'} eq 'MTR_RES_FAILED' ) {
-        $suite_f[-1]++;
-        $all_f++;
+    # Increment result counters based on outcome of the test
+    if ($tinfo->{'result'} eq 'MTR_RES_FAILED' or $tinfo->{failures})
+    {
+      $suite_stats{$tsuite}{'failures'}++;
+      $total_stats{'failures'}++;
     }
-    elsif ( $tinfo->{'result'} eq 'MTR_RES_SKIPPED' ) {
-        $suite_d[-1]++;
-        $all_d++;
+    elsif ($tinfo->{'result'} eq 'MTR_RES_SKIPPED')
+    {
+      if ($tinfo->{'disable'})
+      {
+        $suite_stats{$tsuite}{'disabled'}++;
+        $total_stats{'disabled'}++;
+      }
+      else
+      {
+        $suite_stats{$tsuite}{'skipped'}++;
+        $total_stats{'skipped'}++;
+      }
     }
 
-    $all_time = $all_time + $tinfo->{'timer'};
-    $suite_time[-1] = $suite_time[-1] + $tinfo->{'timer'};
+    # Update duration
+    $suite_stats{$tsuite}{'time'}+= $tinfo->{'timer'};
+    $total_stats{'time'}+= $tinfo->{'timer'};
   }
 
-  $suite_group = "";
-  my $s = 0;
-  # output data
- 
-  $all_time = $all_time / 1000.0;
+  # --------------------------------------
+  # Generate xml report from obtained data
+  # --------------------------------------
 
-  print $xml_report_file "<testsuites tests=\"$all_t\" failures=\"$all_f\" disabled=\"$all_d\" errors=\"0\" time=\"$all_time\" name=\"AllTests\">\n";
-  foreach my $tinfo (@$tests) 
+  # Convert overall test duration from ms to s
+  $total_stats{'time'}/= 1000.0;
+
+  # Top level xml tag
+  print $xml_report_file "<testsuites tests=\"$total_stats{'tests'}\" ".
+                         "failures=\"$total_stats{'failures'}\" ".
+                         "disabled=\"$total_stats{'disabled'}\" ".
+                         "skipped=\"$total_stats{'skipped'}\" ".
+                         "errors=\"$total_stats{'errors'}\" ".
+                         "time=\"$total_stats{'time'}\" ".
+                         "name=\"AllTests\">\n";
+
+  $suite_group= "";
+
+  foreach my $tinfo (@sorted_tests)
   {
-    my @parts = split /\./, $tinfo->{'name'};
-    $tsuite = $parts[0];
-    $tname = $parts[1];
+    my ($tsuite, $tname)= split /\./, $tinfo->{'name'};
 
+    # Put reports in a suite-wise manner
     if ($tsuite ne $suite_group)
     {
       if ($suite_group)
       {
-        print $xml_report_file "  </testsuite>\n";
+        print $xml_report_file " " x 2;
+        print $xml_report_file "</testsuite>\n";
       }
-      $suite_group = $tsuite;
-      my $tmp = $suite_time[$s] / 1000.0;
-      print $xml_report_file "  <testsuite name=\"$tsuite\" tests=\"@suite_t[$s]\" failures=\"$suite_f[$s]\" disabled=\"$suite_d[$s]\" errors=\"$suite_e[$s]\" time=\"$tmp\">\n";
-      $s++;
+
+      $suite_group= $tsuite;
+      my $suite_time= $suite_stats{$tsuite}{'time'} / 1000.0;
+
+      print $xml_report_file " " x 2;
+      print $xml_report_file "<testsuite name=\"$tsuite\" ".
+                             "tests=\"$suite_stats{$tsuite}{'tests'}\" ".
+                             "failures=\"$suite_stats{$tsuite}{'failures'}\" ".
+                             "disabled=\"$suite_stats{$tsuite}{'disabled'}\" ".
+                             "skipped=\"$suite_stats{$tsuite}{'skipped'}\" ".
+                             "errors=\"$suite_stats{$tsuite}{'errors'}\" ".
+                             "time=\"$suite_time\">\n";
     }
 
-    my $time = $tinfo->{'timer'} / 1000.0;
-    if ( $tinfo->{'result'} eq 'MTR_RES_FAILED' ) {
-      print $xml_report_file "    <testcase name=\"$tname\" status=\"run\" time=\"$time\" classname=\"$tsuite\" >\n";
-      print $xml_report_file "       <failure message=\"test failed:\" type=\"\"><![CDATA[$tinfo->{'comment'}]]></failure>\n";
-      print $xml_report_file "    </testcase>\n";
+    # Get the test's variant(if any)
+    my $combination= $tinfo->{combination} ?
+                     " combination=\"".$tinfo->{combination}."\"" : "";
+
+    my $test_time= $tinfo->{'timer'} / 1000.0;
+
+    # Print outcome-based tags
+    if ($tinfo->{'result'} eq 'MTR_RES_FAILED' or $tinfo->{'failures'})
+    {
+      print $xml_report_file " " x 4;
+      print $xml_report_file "<testcase name=\"$tname\"$combination ".
+                             "status=\"fail\" ".
+                             "time=\"$test_time\" ".
+                             "suitename=\"$tsuite\" >\n";
+
+      # Get information regarding the failure
+      my $failure_log= $tinfo->{'logfile'};
+      my $failure_comment;
+
+      if ($tinfo->{'warnings'})
+      {
+        $failure_comment= ": Found warnings/errors in server log file!";
+        $failure_log= $failure_log."\n".$tinfo->{'warnings'};
+      }
+
+      $failure_comment= ": ".$1 if ($failure_log =~ /mysqltest: (.*)/);
+
+      # For large comments from mysql-test-run.pl, display a brief message in
+      # the attribute 'message' and prepend details within the <failure> tag
+      my @comment_patterns=
+      ("MTR's internal check of the test case '.+' failed\\.",
+       "Test case timeout after \\d+ seconds",
+       "Could not execute 'check-testcase' [a-z]+ testcase '.+' \\(res: .*\\)",
+       "Could not execute 'check-warnings' for testcase '.+' \\(res: .*\\)",
+       "Server .+ failed during test run",
+       "The server .+ crashed while running 'check testcase [a-z]+ test'",
+       "The server .+ crashed while running 'check warnings'",
+       "mysqltest failed but provided no output");
+
+      my $comment_regex= "(". join('|', @comment_patterns). ")(.*)";
+      if ($tinfo->{'comment'}=~ /$comment_regex/s)
+      {
+        $failure_comment.= ", ".$1;
+        $failure_log= $2."\n".$failure_log;
+      }
+      else
+      {
+        $failure_comment= ", ".$tinfo->{'comment'} if ($tinfo->{'comment'});
+      }
+
+      if (!$failure_log)
+      {
+        $failure_log= "Failure log not found";
+        $failure_comment= ": Unknown result, no comment or log found"
+          if (!$failure_comment);
+      }
+
+      print $xml_report_file " " x 7;
+      print $xml_report_file "<failure message=\"Test failed".
+                             "$failure_comment\">\n";
+
+      # Embed failure log as a literal string so that the XML parser
+      # ignores any markup which might be included
+      my $failure_log_indent= " " x 10;
+      my $failure_log_message= "";
+      map { $failure_log_message= $failure_log_message.$failure_log_indent.$_ }
+          (split /^/, $failure_log);
+
+      print $xml_report_file $failure_log_indent."<![CDATA[\n".
+                             $failure_log_message."\n".
+                             $failure_log_indent."]]>\n";
+
+      print $xml_report_file " " x 7;
+      print $xml_report_file "</failure>\n";
+
+      print $xml_report_file " " x 4;
+      print $xml_report_file "</testcase>\n";
     }
-    elsif ( $tinfo->{'result'} eq 'MTR_RES_SKIPPED' ) {
-      print $xml_report_file "    <testcase name=\"$tname\" status=\"notrun\" time=\"$time\" classname=\"$tsuite\" />\n";
+    elsif ($tinfo->{'result'} eq 'MTR_RES_SKIPPED')
+    {
+      # Differentiate disabled and skipped tests as we return
+      # the same outcome code for both
+      my $not_run_status= $tinfo->{'disable'} ? "disabled" : "skipped";
+
+      print $xml_report_file " " x 4;
+      print $xml_report_file "<testcase name=\"$tname\"$combination ".
+                             "status=\"$not_run_status\" ".
+                             "time=\"$test_time\" ".
+                             "suitename=\"$tsuite\" ".
+                             "comment=\"$tinfo->{'comment'}\" />\n";
     }
-    elsif ( $tinfo->{'result'} eq 'MTR_RES_PASSED' ) {
-      print $xml_report_file "    <testcase name=\"$tname\" status=\"run\" time=\"$time\" classname=\"$tsuite\" />\n";
+    elsif ($tinfo->{'result'} eq 'MTR_RES_PASSED')
+    {
+      print $xml_report_file " " x 4;
+      print $xml_report_file "<testcase name=\"$tname\"$combination ".
+                             "status=\"pass\" ".
+                             "time=\"$test_time\" ".
+                             "suitename=\"$tsuite\" />\n";
     }
   }
   if ($suite_group)
   {
-    print $xml_report_file "  </testsuite>\n";
+    print $xml_report_file " " x 2;
+    print $xml_report_file "</testsuite>\n";
   }
   print $xml_report_file "</testsuites>\n";
 }
@@ -564,7 +678,15 @@ sub mtr_print_header ($) {
 ##############################################################################
 
 sub mtr_xml_init($) {
-  my ($fn) = @_;
+  my ($fn)= @_;
+
+  # Write xml-report in the build directory for out-of-source builds or current
+  # path otherwise, unless an absolute path is already specified
+  $fn= File::Spec->rel2abs($fn,
+                           $ENV{MTR_BINDIR} ?
+                           File::Spec->catfile($ENV{MTR_BINDIR}, "mysql-test"):
+                           "");
+
   unless(open $xml_report_file, '>', $fn) {
     mtr_error("could not create xml_report file $fn");
   }
