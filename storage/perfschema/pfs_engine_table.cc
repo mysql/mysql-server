a5c09ab75a71230c50b7bf567f4d29559ed02441
Maria Couceiro <maria.couceiro@oracle.com>
2017-03-23 15:54:10 +0000

    BUG#25041396 --LOG-BIN MARKS MANY PERFORMANCE SCHEMA TABLES INVALID
    Problem:
    When a performance_schema table was dropped or does not
    have the expected structure, and later the server was
    restarted with binlog enabled, it marked all performance
    schema tables encountered after the first error as invalid.
    
    Analysis:
    The method decide_log_format would return with an error if
    there was any currently active error and the table was not
    to be replicated (as it is the case with P_S tables).
    This error was being generated when checking the P_S tables
    upon restart. However, it is sufficient to print the error
    to the log, because the check is not supposed to return
    any error.
    
    Fix:
    Since checking the performance schema at server start is not
    supposed to generate an error, it is safe to clear the error
    in case there is one.

--- storage/perfschema/pfs_engine_table.cc
+++ storage/perfschema/pfs_engine_table.cc
@@ -773,9 +773,12 @@ PFS_engine_table_share::check_one_table(THD *thd)
     close_thread_tables(thd);
   }
   else
+  {
     sql_print_error(ER_DEFAULT(ER_WRONG_NATIVE_TABLE_STRUCTURE),
                     PERFORMANCE_SCHEMA_str.str,
                     m_name.str);
+    thd->clear_error();
+  }
 
   lex_end(&dummy_lex);
   thd->lex = old_lex;
