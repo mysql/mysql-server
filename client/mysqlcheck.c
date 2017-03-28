007d0d68e7a6e0ea9019e0c7efe4c3ca88cb9eaf
Sreedhar Sreedhargadda <sreedhar.sreedhargadda@oracle.com>
2017-03-28 07:33:07 +0200

    Applying patch supplied by Ramil for Bug#25575605 SETTING --SSL-MODE=REQUIRED SENDS CREDENTIALS BEFORE VERIFYING SSL

--- client/mysqlcheck.c
+++ client/mysqlcheck.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2001, 2016, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2001, 2017, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -937,7 +937,7 @@ static int dbConnect(char *host, char *user, char *passwd)
   if (!(sock = mysql_connect_ssl_check(&mysql_connection, host, user, passwd,
                                        NULL, opt_mysql_port,
                                        opt_mysql_unix_port, 0,
-                                       opt_ssl_required)))
+                                       opt_ssl_mode == SSL_MODE_REQUIRED)))
   {
     DBerror(&mysql_connection, "when trying to connect");
     DBUG_RETURN(1);
