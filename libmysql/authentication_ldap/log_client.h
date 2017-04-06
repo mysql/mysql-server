426efb01b44d2df87941b9f5c659113787082791
Yashwant Sahu <yashwant.sahu@oracle.com>
2017-04-06 15:38:20 +0530

    final

--- libmysql/authentication_ldap/log_client.h
+++ libmysql/authentication_ldap/log_client.h
@@ -4,7 +4,6 @@
 
 #include <stdio.h>
 #include <iostream>
-#include <fstream>
 #include <sstream>
 #include <my_dbug.h>
 
@@ -70,7 +69,6 @@ void Logger::log(std::string msg) {
   };
 
   /** We can write debug messages also in error log file if logging level is set to debug.
-      For MySQL server this will be set using option.
       For MySQL client this will come from environment variable */
   if (m_log_writer){
     header << my_getsystime() << ": ";
@@ -79,7 +77,7 @@ void Logger::log(std::string msg) {
   }
 WRITE_DBG:
   /** Log all the messages as debug messages as well. */
-  DBUG_PRINT("ldap/sasl auth plugin: ", (": %s", msg.c_str()));
+  DBUG_PRINT("ldap/sasl auth client plug-in: ", (": %s", msg.c_str()));
 }
 
 extern Logger *g_logger;
