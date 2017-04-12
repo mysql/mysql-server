3c582a30cfe426ffe9da99e9caf2425a2ecba400
Magnus Blåudd <magnus.blaudd@oracle.com>
2017-04-12 11:55:56 +0200

    WL#8500 Adapt MySQL Cluster to 8.0
     - problem with no rows returned from information_schema tables
       related to foreign keys in NDB
     - fix by setting the flag showing that ndbcluster plugin supports
       foreign keys.
     - add testcasse for showing output from information_schema tables
       related to foreign keys in NDB

--- sql/ha_ndbcluster.cc
+++ sql/ha_ndbcluster.cc
@@ -14239,7 +14239,8 @@ int ndbcluster_init(void* p)
     ndbcluster_binlog_init(h);
     h->flags=            HTON_CAN_RECREATE |
                          HTON_TEMPORARY_NOT_SUPPORTED |
-                         HTON_NO_BINLOG_ROW_OPT;
+                         HTON_NO_BINLOG_ROW_OPT |
+                         HTON_SUPPORTS_FOREIGN_KEYS;
     h->discover=         ndbcluster_discover;
     h->find_files=       ndbcluster_find_files;
     h->table_exists_in_engine= ndbcluster_table_exists_in_engine;
