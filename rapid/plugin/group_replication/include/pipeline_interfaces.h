79bfd72bda4f5d1eee3030122902bae7154c73e3
Anibal Pinto <anibal.pinto@oracle.com>
2017-04-28 09:33:51 +0200

    BUG#25793366: GR NODE IS IN RECOVERING STATE IF BINLOG_CHECKSUM CONFIGURED ON RUNNING SERVER
    When node configured in GR setup with binlog_checksum='NONE' on a running
    server, then start group replication return success but node is in RECOVERING
    mode forever and server shutdown hangs at this moment. Only option is kill the
    server.
    
    The Continuation object used on the plugin applier pipeline to wait for the
    applications of events was the source of the issue.  Multiple invocations of
    the waiting for execution method when there was a previous error lead to a
    stuck pipeline.
    
    As a solution the waiting method no longer waits when there is a known previous
    error.

--- rapid/plugin/group_replication/include/pipeline_interfaces.h
+++ rapid/plugin/group_replication/include/pipeline_interfaces.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2014, 2017, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -464,6 +464,9 @@ public:
   /**
     Wait until release.
 
+    @note The continuation will not wait if an error as occurred in the past
+          until reset_error_code() is invoked.
+
     @return the end status
       @retval 0      OK
       @retval !=0    Error returned on the execution
@@ -471,7 +474,7 @@ public:
   int wait()
   {
     mysql_mutex_lock(&lock);
-    while (!ready)
+    while (!ready && !error_code)
     {
       mysql_cond_wait(&cond, &lock); /* purecov: inspected */
     }
