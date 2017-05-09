fa4ba1075e510f3b282f781ff5ad95f9270c3f13
Maria Couceiro <maria.couceiro@oracle.com>
2017-05-09 11:28:48 +0100

    BUG#24411680 SET BINLOG_FORMAT DOES NOT GENERATE ERROR WHEN THERE ARE TEMPORARY TABLES
    Problem:
    The statement 'SET @@binlog_format=statement' when binlog_format was row
    or mixed succeeds without causing an error when there are open temporary
    tables.
    
    Analysis:
    Even though the default scope of binlog_format is session, the condition
    that checks whether the error should be issued only considered
    OPT_SESSION, which in this particular statement (@@binlog_format) is
    zero while OPT_DEFAULT is 1. So, the binlog format change would always
    succeed.
    
    Fix:
    Changed the condition that checked if the binlog format could be changed
    because of temporary tables so that setting the variable using the
    format '@@binlog_format' or '@@SESSION.binlog_format' will have the same
    behaviour. The check for the scope of the variable can be skipped since
    OPT_GLOBAL and OPT_PERSIST have already been dealt with.

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -954,7 +954,7 @@ static bool binlog_format_check(sys_var *self, THD *thd, set_var *var)
      switching @@SESSION.binlog_format from MIXED to STATEMENT when there are
      open temp tables and we are logging in row format.
   */
-  if (thd->temporary_tables && var->type == OPT_SESSION &&
+  if (thd->temporary_tables &&
       var->save_result.ulonglong_value == BINLOG_FORMAT_STMT &&
       ((thd->variables.binlog_format == BINLOG_FORMAT_MIXED &&
         thd->is_current_stmt_binlog_format_row()) ||
