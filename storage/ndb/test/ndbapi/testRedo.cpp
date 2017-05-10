e093990c7bd59fead3fe7fec7fc764cfb3c00d20
Mauritz Sundell <mauritz.sundell@oracle.com>
2017-05-10 14:50:38 +0200

    Use {UINT,INT}32_{MIN,MAX} from stdint.h
    Bug#25974560 COMPILER WARNINGS ON WINDOWS: WARNING C4005: 'INT32_MAX' : MACRO REDEFINITION
    
    Remove the definitions for the above from ndb_constants.h.
    
    This should get rid of build warnings on Windows.

--- storage/ndb/test/ndbapi/testRedo.cpp
+++ storage/ndb/test/ndbapi/testRedo.cpp
@@ -15,6 +15,8 @@
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
 */
 
+#include "ndb_global.h" /* defines __STDC_LIMIT_MACROS for stdint.h */
+#include <stdint.h>
 #include <NDBT.hpp>
 #include <NDBT_Test.hpp>
 #include <HugoOperations.hpp>
