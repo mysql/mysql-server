39157d1add78ab947f2be6ba0543cab267a6f1fb
Norvald H. Ryeng <norvald.ryeng@oracle.com>
2017-06-02 12:29:19 +0200

    WL#8685: [25/47] ST_Within
    Add a new implementation of function ST_Within that supports both
    Cartesian and geographic geometries.
    
    Change-Id: Ie26b02e56c5bf1af418cd9ed33fd42fafc2d09b4

--- sql/item_geofunc_relchecks.cc
+++ sql/item_geofunc_relchecks.cc
@@ -1456,3 +1456,12 @@ bool Item_func_st_overlaps::eval(const dd::Spatial_reference_system *srs,
 {
   return gis::overlaps(srs, g1, g2, func_name(), result, null);
 }
+
+
+bool Item_func_st_within::eval(const dd::Spatial_reference_system *srs,
+                               const gis::Geometry *g1,
+                               const gis::Geometry *g2,
+                               bool *result, bool *null)
+{
+  return gis::within(srs, g1, g2, func_name(), result, null);
+}
