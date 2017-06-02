4a34c5f2b42baf4c42f5c3203c6ddc1f2b36a853
Norvald H. Ryeng <norvald.ryeng@oracle.com>
2017-06-02 12:28:58 +0200

    WL#8685: [19/47] gis::crosses
    Add functor gis::Crosses and function gis::crosses.
    
    Change-Id: I8bf1595cf0e3eba22f2c4fd7af8365c5aa45f57a

--- sql/gis/relops.h
+++ sql/gis/relops.h
@@ -26,6 +26,27 @@
 
 namespace gis {
 
+/// Computes the crosses relation between two geometries.
+///
+/// Both geometries must be in the same coordinate system (Cartesian or
+/// geographic), and the coordinate system of the geometries must match
+/// the coordinate system of the SRID. It is the caller's responsibility
+/// to guarantee this.
+///
+/// @param[in] srs The spatial reference system, common to both geometries.
+/// @param[in] g1 First geometry.
+/// @param[in] g2 Second geometry.
+/// @param[in] func_name Function name used in error reporting.
+/// @param[out] crosses Whether g1 crosses g2.
+/// @param[out] null True if the return value is NULL.
+///
+/// @retval false Success.
+/// @retval true An error has occurred. The error has been reported with
+/// my_error().
+bool crosses(const dd::Spatial_reference_system *srs, const Geometry *g1,
+             const Geometry *g2, const char *func_name, bool *crosses,
+             bool *null) noexcept;
+
 /// Computes the disjoint relation between two geometries.
 ///
 /// Both geometries must be in the same coordinate system (Cartesian or
