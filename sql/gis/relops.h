d922bef04eba275849b8850e3c4e4f6f1bd00428
Norvald H. Ryeng <norvald.ryeng@oracle.com>
2017-06-02 12:28:25 +0200

    WL#8685: [11/47] gis::disjoint
    Add functor gis::Disjoint and function gis::disjoint.
    
    Change-Id: Iaf6019f7605a986ba3ec04f7e710901e547c4f62

---
+++ sql/gis/relops.h
@@ -0,0 +1,52 @@
+#ifndef GIS__RELOPS_H_INCLUDED
+#define GIS__RELOPS_H_INCLUDED
+
+// Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the Free Software Foundation, 51 Franklin
+// Street, Suite 500, Boston, MA 02110-1335 USA.
+
+/// @file
+///
+/// This file declares the interface of relational GIS operations. These are
+/// boolean operations that compute relations between geometries.
+
+#include "dd/types/spatial_reference_system.h"  // dd::Spatial_reference_system
+#include "geometries.h"
+
+namespace gis {
+
+/// Computes the disjoint relation between two geometries.
+///
+/// Both geometries must be in the same coordinate system (Cartesian or
+/// geographic), and the coordinate system of the geometries must match
+/// the coordinate system of the SRID. It is the caller's responsibility
+/// to guarantee this.
+///
+/// @param[in] srs The spatial reference system, common to both geometries.
+/// @param[in] g1 First geometry.
+/// @param[in] g2 Second geometry.
+/// @param[in] func_name Function name used in error reporting.
+/// @param[out] disjoint Whether g1 is disjoint from g2.
+/// @param[out] null True if the return value is NULL.
+///
+/// @retval false Success.
+/// @retval true An error has occurred. The error has been reported with
+/// my_error().
+bool disjoint(const dd::Spatial_reference_system *srs, const Geometry *g1,
+              const Geometry *g2, const char *func_name, bool *disjoint,
+              bool *null) noexcept;
+
+}  // namespace gis
+
+#endif  // GIS__RELOPS_H_INCLUDED
