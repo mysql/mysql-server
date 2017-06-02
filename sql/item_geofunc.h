e37a5194d7bea95ccc90e3f64e9696f95789b48f
Norvald H. Ryeng <norvald.ryeng@oracle.com>
2017-06-02 12:29:15 +0200

    WL#8685: [24/47] ST_Overlaps
    Add a new implementation of function ST_Overlaps that supports both
    Cartesian and geographic geometries.
    
    Change-Id: Icd996ec35ce5f32925b63bd09b75cc1abe38efa1

--- sql/item_geofunc.h
+++ sql/item_geofunc.h
@@ -1189,11 +1189,11 @@ public:
 };
 
 
-class Item_func_st_overlaps final : public Item_func_spatial_rel
+class Item_func_st_overlaps final : public Item_func_spatial_relation
 {
 public:
   Item_func_st_overlaps(const POS &pos, Item *a, Item *b)
-    : Item_func_spatial_rel(pos, a, b)
+    : Item_func_spatial_relation(pos, a, b)
   {}
   enum Functype functype() const override
   { return SP_OVERLAPS_FUNC; }
@@ -1201,15 +1201,8 @@ public:
   { return SP_OVERLAPS_FUNC; }
   const char *func_name() const override
   { return "st_overlaps"; }
-  bool resolve_type(THD *) override
-  {
-    maybe_null= true;
-    return false;
-  }
-  void print(String *str, enum_query_type query_type) override
-  {
-    Item_func::print(str, query_type);
-  }
+  bool eval(const dd::Spatial_reference_system *srs, const gis::Geometry *g1,
+            const gis::Geometry *g2, bool *result, bool *null) override;
 };
 
 
