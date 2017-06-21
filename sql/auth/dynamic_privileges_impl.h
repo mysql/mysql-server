7a846e8f1bc877e7e23e6d8987d12bc89a00e2bd
Erlend Dahl <erlend.dahl@oracle.com>
2017-06-21 12:31:59 +0200

    Bug#26279805 WRONG COPYRIGHT HEADERS IN COMPONENTS/ FILES
    Fix zip number in FSF address. Also fix some additional cases of wrong
    FSF address.
    
    Approved by Terje Rosten <terje.rosten@oracle.com>

--- sql/auth/dynamic_privileges_impl.h
+++ sql/auth/dynamic_privileges_impl.h
@@ -11,7 +11,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02111-1307  USA */
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
 #ifndef DYNAMIC_PRIVILEGES_IMPL_H
 #define DYNAMIC_PRIVILEGES_IMPL_H
 #include <mysql/components/service.h>
