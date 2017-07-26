ea8278cb900d78400450294416a579ed58622ae3
Jimmy Yang <jimmy.yang@oracle.com>
2017-07-26 11:26:56 +0800

    More fixes for metadata reset patch

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -3541,30 +3541,39 @@ static
 void
 innobase_dict_cache_reset_tables_and_tablespaces()
 {
-	dict_table_t*	table = nullptr;
+	dict_table_t*	table = UT_LIST_GET_FIRST(dict_sys->table_LRU);
 
 	/* There should be no DDL/DML activity at this stage, so access
 	the LRU chain without mutex. We only invalidates the table
 	in LRU list */
-	for (table = UT_LIST_GET_FIRST(dict_sys->table_LRU); table;
-             table = UT_LIST_GET_NEXT(table_LRU, table)) {
+	while (table) {
 		/* Make sure table->is_dd_table is set */
                 char	db_buf[NAME_LEN + 1];
                 char	tbl_buf[NAME_LEN + 1];
+
+		dict_table_t*	next_table = UT_LIST_GET_NEXT(table_LRU, table);
 
 		dd_parse_tbl_name(table->name.m_name, db_buf, tbl_buf,
 				  nullptr, nullptr);
 
 		/* TODO: Remove follow if we have better way to identify
 		DD "system table" */
-		if (strcmp(db_buf, "mysql") == 0 || table->is_dd_table) {
+		if (strcmp(db_buf, "mysql") == 0 || table->is_dd_table
+		    || table->is_corrupted()) {
+			table = next_table;
 			continue;
 		}
+
+		mutex_enter(&dict_sys->mutex);
+		table->acquire();
+		mutex_exit(&dict_sys->mutex);
 		btr_drop_ahi_for_table(table);
+		dd_table_close(table, nullptr, nullptr, false);
 
 		mutex_enter(&dict_sys->mutex);
 		dict_table_remove_from_cache(table);
 		mutex_exit(&dict_sys->mutex);
+		table = next_table;
 	}
 }
 
