9af0598979dcfc2ea42ff8d3348068407e73cee0
Aditya A <aditya.a@oracle.com>
2017-08-10 17:26:56 +0530

    WL#10551: Upgrade zlib libraries to 1.2.11 in trunk
    Post push fix to fix the UBSAN error which was coming
    because zmemcpy was being called with source as NULL
    and length to copy as zero.
    Fix is to avoid calling zmemcpy (which is mapped to memcpy)
    if length to copy is zero

--- extra/zlib/trees.c
+++ extra/zlib/trees.c
@@ -870,7 +870,11 @@ void ZLIB_INTERNAL _tr_stored_block(s, buf, stored_len, last)
     bi_windup(s);        /* align on byte boundary */
     put_short(s, (ush)stored_len);
     put_short(s, (ush)~stored_len);
-    zmemcpy(s->pending_buf + s->pending, (Bytef *)buf, stored_len);
+    if (stored_len != 0) {
+	/* Avoid calling memcpy when stored_len equals to zero to
+	avoid UBSAN errors */
+	zmemcpy(s->pending_buf + s->pending, (Bytef *)buf, stored_len);
+    }
     s->pending += stored_len;
 #ifdef ZLIB_DEBUG
     s->compressed_len = (s->compressed_len + 3 + 7) & (ulg)~7L;
