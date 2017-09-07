a0092ed35eb9f839f57768bff16c2b0619d82e2d
Marc Alff <marc.alff@oracle.com>
2017-09-07 10:50:56 +0200

    WL#10985 DEPRECATE PERFORMANCE_SCHEMA.SETUP_TIMERS IN 5.7

--- storage/perfschema/table_setup_timers.cc
+++ storage/perfschema/table_setup_timers.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008, 2015, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2008, 2017, Oracle and/or its affiliates. All rights reserved.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -24,6 +24,7 @@
 #include "pfs_column_values.h"
 #include "pfs_timer.h"
 #include "field.h"
+#include "derror.h" /* ER_THD */
 
 #define COUNT_SETUP_TIMERS 5
 
@@ -90,6 +91,13 @@ table_setup_timers::m_share=
 
 PFS_engine_table* table_setup_timers::create(void)
 {
+  THD *thd = current_thd;
+  push_warning_printf(thd,
+                      Sql_condition::SL_WARNING,
+                      ER_WARN_DEPRECATED_SYNTAX_NO_REPLACEMENT,
+                      ER_THD(thd, ER_WARN_DEPRECATED_SYNTAX_NO_REPLACEMENT),
+                      "performance_schema.setup_timers");
+
   return new table_setup_timers();
 }
 
