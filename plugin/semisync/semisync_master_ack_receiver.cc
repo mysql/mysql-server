1800b015a1d487330f7b15f2020b887be348a66b
Venkatesh Duggirala <venkatesh.duggirala@oracle.com>
2017-09-08 20:29:22 +0530

    Bug#26027024	SLAVE_COMPRESSED_PROTOCOL DOESN'T WORK WITH SEMI-SYNC REPLICATION IN MYSQL-5.7
    Analysis: In mysql-5.6, dump thread (the thread that is created
    on Master after Slave requested for a binlog dump) is also used
    to receive acknowledgements from the Slave and act on them accordingly.
    For performance reasons, a special thread called Ack Receiver thread
    is added in mysql-5.7 Semi synchronous replication plugin.
    This thread does not have special handling to receive acknowledgements
    if Slave has enabled compression in the protocol. Hence Master is
    unable to handle any slave if Slave_compressed_protocol is enabled
    on it.
    
    Fix: Enable compress flag on the communication channels if the Slave
    has Slave_compressed_protocol ON.

--- plugin/semisync/semisync_master_ack_receiver.cc
+++ plugin/semisync/semisync_master_ack_receiver.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2014, 2017, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -263,6 +263,12 @@ void Ack_receiver::run()
 
         net_clear(&net, 0);
         net.vio= &m_slaves[i].vio;
+        /*
+          Set compress flag. This is needed to support
+          Slave_compress_protocol flag enabled Slaves
+        */
+        net.compress=
+          m_slaves[i].thd->get_protocol_classic()->get_compression();
 
         len= my_net_read(&net);
         if (likely(len != packet_error))
