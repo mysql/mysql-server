e50e654a560aec571b36ff5fcff48dbec4d20982
Evgeny Potemkin <evgeny.potemkin@oracle.com>
2017-09-25 16:29:25 +0400

    WL#8867: Add JSON_TABLE function
    Fixed bad merge.

--- sql/table.cc
+++ sql/table.cc
@@ -7809,8 +7809,8 @@ void TABLE_LIST::propagate_table_maps(table_map map_arg)
 }
 
 
-st_lex_user *
-st_lex_user::alloc(THD *thd, LEX_STRING *user_arg, LEX_STRING *host_arg)
+LEX_USER *
+LEX_USER::alloc(THD *thd, LEX_STRING *user_arg, LEX_STRING *host_arg)
 {
   LEX_USER *ret= static_cast<LEX_USER *>(thd->alloc(sizeof(LEX_USER)));
   if (ret == NULL)
