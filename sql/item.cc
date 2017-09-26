84460dbe5ad89e45d7eae97261632656295c2983
Knut Anders Hatlen <knut.hatlen@oracle.com>
2017-09-26 12:19:51 +0200

    Bug#26867509: JSON_OBJECT CREATES INVALID JSON CODE
    When inserting JSON values from a grouped query into a string column,
    the inserted values could sometimes include the concatenation of all
    the values previously inserted into that column.
    
    The fix is to make Item_copy_json::save_in_field_inner() reset the
    buffer before converting the JSON value to text.
    
    (cherry picked from commit 0178fbc193bfaa0af49136e444ad5b73586d6f83)

--- sql/item.cc
+++ sql/item.cc
@@ -4838,6 +4838,7 @@ Item_copy_json::save_in_field_inner(Field *field, bool no_conversions)
   }
   else
   {
+    str_value.length(0);
     if (m_value->to_string(&str_value, true, item_name.ptr()))
       return set_field_to_null(field);        /* purecov: inspected */
     return save_str_value_in_field(field, &str_value);
