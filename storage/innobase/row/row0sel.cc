ffdf388df94b240757af3a546efb2a035f2d41d3
Tor Didriksen <tor.didriksen@oracle.com>
2017-10-05 13:25:20 +0200

    Bug#26918439 ROW0SEL.CC:5178:32 ERROR: ISO C++ FORBIDS COMPARISON BETWEEN POINTER AND INTEGE
    prebuilt->idx_cond is a pointer, so compare it to NULL rather than false.
    
    Change-Id: Id01bda87fc921d0d595e07f9a1d11151e723618e

--- storage/innobase/row/row0sel.cc
+++ storage/innobase/row/row0sel.cc
@@ -5193,7 +5193,7 @@ rec_loop:
 		reporting due to search views etc. */
 		if (prev_rec != NULL
 		    && prebuilt->m_mysql_handler->end_range != NULL
-		    && prebuilt->idx_cond == false && end_loop >= 100) {
+		    && prebuilt->idx_cond == NULL && end_loop >= 100) {
 
 			dict_index_t*	key_index = prebuilt->index;
 			bool		clust_templ_for_sec = false;
