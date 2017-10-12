c5f9a1d0452cf0b24c8b5488a12dcdcce959444e
Lakshmi Narayanan Sreethar <lakshmi.narayanan.sreethar@oracle.com>
2017-10-13 01:09:08 +0530

    Bug#26758939 : FAIL TO LOCK THREAD TO CPU - Post Push Fix
    Previous patches for Bug#26758939 disabled Lock/UnlockCPU apis in
    environment where the glibc version was less than 2.23 to avoid the
    effects of a bug. But since this seems too strict, most of the
    changes made as a part of Bug#26758939 are reverted in this patch. This
    re enables Lock/UnlockCPU apis in all environments regardless of the
    glibc version used.
    
    The glibc bug causes the sched_setaffinity() call to fail when an all
    ones CPU mask is set. The call fails and the error is set to EINVAL -
    invalid arguments. UnlockCPU uses a all ones CPU mask and will fail
    similarly if the glibc version is lower than 2.23. This patch instead
    of using an all ones CPU mask, uses sysconf to get the actual number of
    processors available to the system and then sets the cpu mask
    accordingly.

--- storage/ndb/src/common/portlib/NdbThread.c
+++ storage/ndb/src/common/portlib/NdbThread.c
@@ -71,18 +71,6 @@ static int g_max_prio = 0;
 static my_bool get_prio_first = TRUE;
 #endif
 
-#if defined(HAVE_LINUX_SCHEDULING)
-static inline my_bool has_required_glibc_version() {
-  /* sched_setaffinity has a bug due to which it fails when an all-ones
-   * CPU mask is set. The bug is fixed in GLIBC v2.23. */
-  if (__GLIBC__ > 2 || ( __GLIBC__ == 2 && __GLIBC_MINOR__ >= 23 ))
-  {
-    return TRUE;
-  }
-  return FALSE;
-}
-#endif
-
 static NdbMutex *ndb_thread_mutex = 0;
 static struct NdbCondition * ndb_thread_condition = 0;
 
@@ -1354,13 +1342,7 @@ NdbThread_UnlockCPU(struct NdbThread* pThread)
      */
     cpu_set_t cpu_set;
     Uint32 i;
-    Uint32 num_cpus = sizeof(cpu_set_t) * 8;
-
-    if (!has_required_glibc_version())
-    {
-      error_no = BIND_CPU_NOT_SUPPORTED_ERROR;
-      return error_no;
-    }
+    Uint32 num_cpus = sysconf(_SC_NPROCESSORS_ONLN);
 
     CPU_ZERO(&cpu_set);
     for (i = 0; i < num_cpus; i++)
@@ -1564,7 +1546,6 @@ NdbThread_LockCPUSet(struct NdbThread* pThread,
 #if defined(HAVE_LINUX_SCHEDULING)
   /* Linux */
   cpu_set_t *cpu_set_ptr;
-  Uint32 i, num_cpus;
 #elif defined HAVE_CPUSET_SETAFFINITY
   /* FreeBSD */
   cpuset_t *cpu_set_ptr;
@@ -1579,23 +1560,6 @@ NdbThread_LockCPUSet(struct NdbThread* pThread,
 #if defined HAVE_LINUX_SCHEDULING
   /* Linux */
   cpu_set_ptr = (cpu_set_t*)ndb_cpu_set;
-
-  /* Check if an all mask cpu set is used */
-  num_cpus = sizeof(cpu_set_t) * 8;
-  for (i = 0; i < num_cpus; i++)
-  {
-    if (CPU_ISSET(i, cpu_set_ptr) == 0)
-    {
-      break;
-    }
-  }
-
-  if (i == num_cpus && !has_required_glibc_version())
-  {
-    /* All CPU masks are set but the environment doesn't
-     * have the required glibc version */
-    return BIND_CPU_NOT_SUPPORTED_ERROR;
-  }
 
   /* Lock against the bitmask defined by CPUSet */
   ret= sched_setaffinity(pThread->tid,
