f1e4a00eb2edc5c411588a1699c0fe8bc1181d28
Gopal Shankar <gopal.shankar@oracle.com>
2017-10-22 07:10:14 +0200

    Bug#26199233 create like ignores innodb_file_per_table
    This patch fixes following three issues,
    
    Consider following table,
      SET @@global.innodb_file_per_table=OFF;
      CREATE TABLE t1 (c1 INT) ENGINE=InnoDB;
    
    Problem 1)
      The statements
      SET @@global.innodb_file_per_table=ON;
      CREATE TABLE t2 LIKE t1;
    
      Ignores innodb_file_per_table setting and picks the
      tablespace name used by table t1, even if user did not
      explicitly specify the tablespace name for t1. MySQL 5.7
      assigns tablespace name based on innodb_file_per_table
      setting.
    
    Problem 2)
      SHOW CREATE on table t1, shows TABLESPACE `innodb_system`
      clause, even when user did not explicitly specify
      tablespace.  MySQL 5.7 do not show TABLESPACE clause.
    
    Problem 3)
      SHOW CREATE on table names that match DD table names
      created in any database, endup showing tablespace name as
      'mysql'.
      E.g.,
        ...
        CREATE DATABASE db1;
        CREATE TABLESPACE ts0 ADD DATAFILE 'df0.ibd';
        CREATE TABLE db1.tables (c1 INT)
          ENGINE=InnoDB tablespace=ts0;
        SHOW CREATE TABLE db1.tables;
        Table Create Table
        tables  CREATE TABLE `tables` (
           `c1` int(11) DEFAULT NULL
        ) /*!50100 TABLESPACE `mysql` */
        ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
        ...
    
    The patch fixes above issues by,
    
    - Maintain a boolean property 'explicit_tablespace=true' in
      mysql.tables.options, if user provides TABLESPACE clause
      when creating tables.
    
    - Make CREATE LIKE copy tablespace name, only
      if 'explicit_tablespace' property is true for the source
      table.  Otherwise InnoDB would decide the tablespace name
      based on 'innodb_file_per_table', this fixes 1)
    
    - SHOW CREATE implementation now reads the table property
      'explicit_tablespace' to decide if we need to print the
      TABLESPACE clause in the output. This fixes 2)
    
    - Use 'mysql' tablespace id to speed-up
      get_tablespace_name() on DD tables. This fixes 3).
    
    - The recorded result files now match 5.7 results.
    
    - Added new test case in innodb.create_tablespace.
    
    Change-Id: I7f890569f41a048e5e264d50434c30b8d4773311

--- sql/sql_show.cc
+++ sql/sql_show.cc
@@ -66,6 +66,7 @@
 #include "sql/auth/sql_security_ctx.h"
 #include "sql/dd/cache/dictionary_client.h" // dd::cache::Dictionary_client
 #include "sql/dd/dd_schema.h"               // dd::Schema_MDL_locker
+#include "sql/dd/types/table.h"             // dd::Table
 #include "sql/dd/string_type.h"
 #include "sql/debug_sync.h"                 // DEBUG_SYNC
 #include "sql/derror.h"                     // ER_THD
@@ -1625,12 +1626,33 @@ int store_create_info(THD *thd, TABLE_LIST *table_list, String *packet,
   {
     show_table_options= TRUE;
 
+    // Show tablespace name only if it is explicitly provided by user.
+    bool show_tablespace= false;
+    if (share->tmp_table)
+    {
+      // Innodb allows temporary tables in be in system temporary tablespace.
+      show_tablespace= share->tablespace;
+    }
+    else if (share->tablespace)
+    {
+      dd::cache::Dictionary_client::Auto_releaser releaser(thd->dd_client());
+      const dd::Table *table_obj= nullptr;
+      if (thd->dd_client()->acquire(dd::String_type(share->db.str),
+                                    dd::String_type(share->table_name.str),
+                                    &table_obj))
+      {
+        DBUG_RETURN(true);
+      }
+      DBUG_ASSERT(table_obj != nullptr);
+      show_tablespace= table_obj->is_explicit_tablespace();
+    }
+
     /* TABLESPACE and STORAGE */
-    if (share->tablespace ||
+    if (show_tablespace ||
         share->default_storage_media != HA_SM_DEFAULT)
     {
       packet->append(STRING_WITH_LEN(" /*!50100"));
-      if (share->tablespace)
+      if (show_tablespace)
       {
         packet->append(STRING_WITH_LEN(" TABLESPACE "));
         append_identifier(thd, packet, share->tablespace,
