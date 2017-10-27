8bcd7074992a56568304453415f2c4bb9a742d70
Terje Rosten <terje.rosten@oracle.com>
2017-10-27 10:17:40 +0200

    Bug#25967079 WRONG MIN_VALUE IN PFS.VARIABLES_INFO ON 32 BITS BIG ENDIAN PLATFORM
    Wrong format specifier caused issue on 32 bits BE platforms.
    
    Change is mysql-8.0 specific, resolved in trunk by
      BUG#26696147: REMOVE MY_SNPRINTF

--- storage/perfschema/pfs_variable.cc
+++ storage/perfschema/pfs_variable.cc
@@ -800,7 +800,7 @@ System_variable::init(THD *target_thd, const SHOW_VAR *show_var)
   }
   my_snprintf(m_min_value_str,
               sizeof(m_min_value_str),
-              "%ld",
+              "%lld",
               system_var->get_min_value());
   m_min_value_length = strlen(m_min_value_str);
   my_snprintf(m_max_value_str,
