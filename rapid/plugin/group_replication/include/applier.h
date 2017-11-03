c6998afc928d46babd2842b08c17379483d8616c
Nuno Carvalho <nuno.carvalho@oracle.com>
2017-11-03 18:05:18 +0100

    BUG#27049034: SOMETIMES MEMBERS ENTER IN ERROR STATE AFTER LONG RECOVERY
    In Group Replication, when a member join a group it goes through the
    recovery process in order to have the same data as of the group.
    The recovery process is composed by 3 stages:
      1) Apply existent transactions on group_replication_applier, if
         existent, this happens when this member already was a member of
         the group and left with transactions to apply.
      2) Fetch and apply the data the new member is missing when
         compared with the group. This happens through a asynchronous
         replication channel, named group_replication_recovery, which
         stop condition is apply all data until the moment on which the
         new member joined, marked by View_change_log_event.
         While this is ongoing, the concurrent group workload is being
         queued and will be deal with on the next stage.
      3) Apply the queued transactions during stage 2 and then switch
         the member status to ONLINE.
         The stop condition is:
           5.7: the new member will wait until the queue is empty and
                group_replication_applier applied all transactions.
           8.0: the new member will wait until the number of
                transactions queued during the 2nd stage are applied.
    
    On this bug, at the end of the 3rd stage, the recovery process
    reached its termination condition, that is, all accounted
    transactions are applied.
    On that *exact* moment, a new transaction is being queued. Please
    remember that transactions are materialized as events on binary and
    relay logs.
    When the applier signals that applied all transactions, it is also
    blindly searching for partial transactions, that will remove to
    avoid future applier errors, which would happen if the applier did
    stop at this point.
    Though, this search and remove does only make sense on applier stop
    cases. On execution completeness that must not be done, otherwise it
    can corrupt or purge the applier relay log, which can led to data
    loss.
    
    To solve the above issue, when the applier is waiting for execution
    completeness, it does not search and remove partial transactions.

--- rapid/plugin/group_replication/include/applier.h
+++ rapid/plugin/group_replication/include/applier.h
@@ -138,7 +138,8 @@ public:
                                                    bool wait_for_execution= true)= 0;
   virtual void awake_applier_module()= 0;
   virtual void interrupt_applier_suspension_wait()= 0;
-  virtual int wait_for_applier_event_execution(double timeout)= 0;
+  virtual int wait_for_applier_event_execution(double timeout,
+                                               bool check_and_purge_partial_transactions)= 0;
   virtual size_t get_message_queue_size()= 0;
   virtual Member_applier_state get_applier_status()= 0;
   virtual void add_suspension_packet()= 0;
@@ -432,13 +433,17 @@ public:
 
     @param timeout  the time (seconds) after which the method returns if the
                     above condition was not satisfied
+    @param check_and_purge_partial_transactions
+                    on successful executions, check and purge partial
+                    transactions in the relay log
 
     @return the operation status
       @retval 0      All transactions were executed
       @retval -1     A timeout occurred
       @retval -2     An error occurred
   */
-  virtual int wait_for_applier_event_execution(double timeout);
+  virtual int wait_for_applier_event_execution(double timeout,
+                                               bool check_and_purge_partial_transactions);
 
   /**
     Returns the handler instance in the applier module responsible for
