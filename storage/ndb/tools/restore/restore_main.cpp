425cd3dfc58642eb83ae486da63b2d897c066d96
Thomas Nielsen <thomas.nielsen@oracle.com>
2017-11-08 11:31:09 +0530

    Bug #25112726 NDB_RESTORE RETURNS -1 INSTEAD OF EXPECTED EXIT CODE ON INDEX REBUILD FAILURE
    Changes made in order to return appropriate error code instead of -1 in ndb_restore.

--- storage/ndb/tools/restore/restore_main.cpp
+++ storage/ndb/tools/restore/restore_main.cpp
@@ -1822,7 +1822,7 @@ main(int argc, char** argv)
       if (!g_consumers[i]->update_apply_status(metaData))
       {
         err << "Restore: Failed to restore epoch" << endl;
-        return -1;
+        exitHandler(NDBT_FAILED);
       }
   }
 
@@ -1853,13 +1853,13 @@ main(int argc, char** argv)
       for(Uint32 j= 0; j < g_consumers.size(); j++)
       {
         if (!g_consumers[j]->rebuild_indexes(* table))
-          return -1;
+          exitHandler(NDBT_FAILED);
       }
     }
     for(Uint32 j= 0; j < g_consumers.size(); j++)
     {
       if (!g_consumers[j]->endOfTablesFK())
-        return -1;
+        exitHandler(NDBT_FAILED);
     }
   }
 
