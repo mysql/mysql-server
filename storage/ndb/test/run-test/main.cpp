de856432acab06a0515b34066adc5abd4d0231f0
Mauritz Sundell <mauritz.sundell@oracle.com>
2017-11-10 15:11:07 +0100

    Bug#26826272 Make load_default MEM_ROOT live the whole process life again.
    Atrt suffered from too early free of MEM_ROOT.
    
    See Bug#27102375 MEM_ROOT FOR LOAD_DEFAULTS FREED TOO EARLY

--- storage/ndb/test/run-test/main.cpp
+++ storage/ndb/test/run-test/main.cpp
@@ -649,8 +649,8 @@ parse_args(int argc, char** argv)
   g_logger.info("Bootstrapping using %s", mycnf.c_str());
   
   const char *groups[] = { "atrt", 0 };
-  MEM_ROOT alloc{PSI_NOT_INSTRUMENTED, 512, 0};
-  int ret = load_defaults(mycnf.c_str(), groups, &argc, &argv, &alloc);
+  MEM_ROOT* alloc = new MEM_ROOT{PSI_NOT_INSTRUMENTED, 512, 0}; // LEAK
+  int ret = load_defaults(mycnf.c_str(), groups, &argc, &argv, alloc);
 
   if (ret)
   {
--- storage/ndb/test/run-test/setup.cpp
+++ storage/ndb/test/run-test/setup.cpp
@@ -93,8 +93,8 @@ bool setup_config(atrt_config& config, const char* atrt_mysqld) {
     argv[argc++] = buf.c_str();
     char** tmp = (char**)argv;
     const char* groups[] = {"cluster_config", 0};
-    MEM_ROOT alloc{PSI_NOT_INSTRUMENTED, 512, 0};
-    int ret = load_defaults(g_my_cnf, groups, &argc, &tmp, &alloc);
+    MEM_ROOT* alloc = new MEM_ROOT{PSI_NOT_INSTRUMENTED, 512, 0}; // LEAK
+    int ret = load_defaults(g_my_cnf, groups, &argc, &tmp, alloc);
     if (ret) {
       g_logger.error("Unable to load defaults for cluster: %s",
                      clusters[i].c_str());
@@ -161,8 +161,8 @@ bool setup_config(atrt_config& config, const char* atrt_mysqld) {
       argv[argc++] = buf.c_str();
       const char* groups[] = {"mysql_cluster", 0};
       char** tmp = (char**)argv;
-      MEM_ROOT alloc{PSI_NOT_INSTRUMENTED, 512, 0};
-      ret = load_defaults(g_my_cnf, groups, &argc, &tmp, &alloc);
+      MEM_ROOT* alloc = new MEM_ROOT{PSI_NOT_INSTRUMENTED, 512, 0}; // LEAK
+      ret = load_defaults(g_my_cnf, groups, &argc, &tmp, alloc);
 
       if (ret) {
         g_logger.error("Unable to load defaults for cluster: %s",
@@ -307,8 +307,8 @@ static bool load_process(atrt_config& config, atrt_cluster& cluster,
       return false;
   }
 
-  MEM_ROOT alloc{PSI_NOT_INSTRUMENTED, 512, 0};
-  int ret = load_defaults(g_my_cnf, groups, &argc, &tmp, &alloc);
+  MEM_ROOT* alloc = new MEM_ROOT{PSI_NOT_INSTRUMENTED, 512, 0}; // LEAK
+  int ret = load_defaults(g_my_cnf, groups, &argc, &tmp, alloc);
   if (ret) {
     g_logger.error("Unable to load defaults for cluster: %s",
                    cluster.m_name.c_str());
