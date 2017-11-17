a37646f6ae90126704eb31958ad6d592fac60548
Norvald H. Ryeng <norvald.ryeng@oracle.com>
2017-11-17 13:08:11 +0100

    WL#11096: [05/17] Reject unsupported SRSs in ST_CONVEXHULL
    Raise ER_NOT_IMPLEMENTED_FOR_GEOGRAPHIC_SRS and ER_SRS_NOT FOUND in
    ST_CONVEXHULL.
    
    Change-Id: I9cda23c14b59de8eadf9f315a41f70d41eba8561

--- sql/item_geofunc.cc
+++ sql/item_geofunc.cc
@@ -4517,22 +4517,8 @@ String *Item_func_convex_hull::val_str(String *str)
     return error_str();
   }
 
-  if (geom->get_srid() != 0)
-  {
-    bool srs_exists= false;
-    if (Srs_fetcher::srs_exists(current_thd, geom->get_srid(), &srs_exists))
-      return error_str(); // Error has already been flagged.
-
-    if (!srs_exists)
-    {
-      push_warning_printf(current_thd,
-                          Sql_condition::SL_WARNING,
-                          ER_WARN_SRS_NOT_FOUND,
-                          ER_THD(current_thd, ER_WARN_SRS_NOT_FOUND),
-                          geom->get_srid(),
-                          func_name());
-    }
-  }
+  if (verify_cartesian_srs(geom, func_name()))
+    return error_str();
 
   if (bg_convex_hull<bgcs::cartesian>(geom, str))
     return error_str();
