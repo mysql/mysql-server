2d9bf57d679d25254751123cd8ae302496598450
Neha Kumari <neha.n.kumari@oracle.com>
2017-11-24 15:23:39 +0530

    Bug#27149075: RENAME FUNCTION SET_UNKNOW_ERROR TO CORRECT SET_UNKNOWN_ERROR
    The name of the function set_unknow_error need to be corrected to
    set_unknown_error.

--- sql/rpl_binlog_sender.cc
+++ sql/rpl_binlog_sender.cc
@@ -139,7 +139,7 @@ void Binlog_sender::init()
                (thd, m_flag, m_start_file, m_start_pos,
                 &m_observe_transmission)))
   {
-    set_unknow_error("Failed to run hook 'transmit_start'");
+    set_unknown_error("Failed to run hook 'transmit_start'");
     DBUG_VOID_RETURN;
   }
   m_transmit_started=true;
@@ -185,7 +185,7 @@ void Binlog_sender::init()
 
 #ifndef DBUG_OFF
   if (opt_sporadic_binlog_dump_fail && (binlog_dump_count++ % 2))
-    set_unknow_error("Master fails in COM_BINLOG_DUMP because of "
+    set_unknown_error("Master fails in COM_BINLOG_DUMP because of "
                      "--sporadic-binlog-dump-fail");
   m_event_count= 0;
 #endif
@@ -978,7 +978,7 @@ inline int Binlog_sender::reset_transmit_packet(ushort flags, size_t event_len)
   if (m_observe_transmission &&
       RUN_HOOK(binlog_transmit, reserve_header, (m_thd, flags, &m_packet)))
   {
-    set_unknow_error("Failed to run hook 'reserve_header'");
+    set_unknown_error("Failed to run hook 'reserve_header'");
     DBUG_RETURN(1);
   }
 
@@ -1219,7 +1219,7 @@ inline int Binlog_sender::flush_net()
   if (DBUG_EVALUATE_IF("simulate_flush_error", 1,
       m_thd->get_protocol()->flush()))
   {
-    set_unknow_error("failed on flush_net()");
+    set_unknown_error("failed on flush_net()");
     return 1;
   }
   return 0;
@@ -1238,7 +1238,7 @@ inline int Binlog_sender::send_packet()
                          m_thd->get_protocol_classic()->get_net(),
                          (uchar*) m_packet.ptr(), m_packet.length())))
   {
-    set_unknow_error("Failed on my_net_write()");
+    set_unknown_error("Failed on my_net_write()");
     DBUG_RETURN(1);
   }
 
@@ -1260,7 +1260,7 @@ inline int Binlog_sender::before_send_hook(const char *log_file,
       RUN_HOOK(binlog_transmit, before_send_event,
                (m_thd, m_flag, &m_packet, log_file, log_pos)))
   {
-    set_unknow_error("run 'before_send_event' hook failed");
+    set_unknown_error("run 'before_send_event' hook failed");
     return 1;
   }
   return 0;
@@ -1273,7 +1273,7 @@ inline int Binlog_sender::after_send_hook(const char *log_file,
       RUN_HOOK(binlog_transmit, after_send_event,
                (m_thd, m_flag, &m_packet, log_file, log_pos)))
   {
-    set_unknow_error("Failed to run hook 'after_send_event'");
+    set_unknown_error("Failed to run hook 'after_send_event'");
     return 1;
   }
 
@@ -1283,7 +1283,7 @@ inline int Binlog_sender::after_send_hook(const char *log_file,
   */
   if (m_thd->get_protocol_classic()->get_net()->last_errno != 0)
   {
-    set_unknow_error("Found net error");
+    set_unknown_error("Found net error");
     return 1;
   }
   return 0;
@@ -1297,7 +1297,7 @@ inline int Binlog_sender::check_event_count()
   if (max_binlog_dump_events != 0 &&
       (++m_event_count > max_binlog_dump_events))
   {
-    set_unknow_error("Debugging binlog dump abort");
+    set_unknown_error("Debugging binlog dump abort");
     return 1;
   }
   return 0;
