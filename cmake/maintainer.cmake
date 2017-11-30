894ceecec2f2142f8eb29ee4c2a3a1c124268b55
Tor Didriksen <tor.didriksen@oracle.com>
2017-11-30 16:33:33 +0100

    Bug#27199465 REMOVE -WDECLARATION-AFTER-STATEMENT FROM WARNING FLAGS
    Remove -Wdeclaration-after-statement from -Werror list.
    All compilers in use support it, and it gives spurious warnings when building
    third-party code.
    
    Change-Id: I078893aa07058dd13eba32b97802bad2fd93efa6
    (cherry picked from commit 547061961a9975b4cba8d05d07d314cdc9a012fb)

--- cmake/maintainer.cmake
+++ cmake/maintainer.cmake
@@ -38,7 +38,7 @@ SET(MY_WARNING_FLAGS
 
 # Common warning flags for GCC and Clang
 SET(MY_C_WARNING_FLAGS
-    "${MY_WARNING_FLAGS} -Wwrite-strings -Wdeclaration-after-statement")
+    "${MY_WARNING_FLAGS} -Wwrite-strings")
 
 # Common warning flags for G++ and Clang++
 SET(MY_CXX_WARNING_FLAGS "${MY_WARNING_FLAGS} -Woverloaded-virtual")
