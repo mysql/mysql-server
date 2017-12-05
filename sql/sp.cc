8afb3c724004e302dd62fa1178ee4c659d7b9b29
Karthik Kamath <karthik.kamath@oracle.com>
2017-12-05 19:49:59 +0530

    BUG#26881798: SERVER EXITS WHEN PRIMARY KEY IN MYSQL.PROC               IS DROPPED
    ANALYSIS:
    =========
    It is advised not to tamper with the system tables.
    When primary key is dropped from a system table, certain
    operations on the table which tries to access the table key
    information may lead to server exit.
    
    FIX:
    ====
    An appropriate error is now reported in such a case.

--- sql/sp.cc
+++ sql/sp.cc
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2002, 2016, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2002, 2017, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -351,7 +351,7 @@ private:
   bool m_print_once;
 
 public:
-  Proc_table_intact() : m_print_once(TRUE) {}
+  Proc_table_intact() : m_print_once(TRUE) { has_keys= TRUE; }
 
 protected:
   void report_error(uint code, const char *fmt, ...);
