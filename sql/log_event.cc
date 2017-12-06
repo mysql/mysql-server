0ba9cd7cfd753bffec55cd9be69b0d5327e35d78
Thayumanavar S <thayumanavar.x.sachithanantha@oracle.com>
2017-12-06 08:18:41 +0100

    BUG#26996065 - CONTRIBUTION: FIX MINOR TYPO IN COMMENT.
    The patch fixes some minor typo in comments. This patch is based on
    a contribution made by BOOKING.COM BV.

--- sql/log_event.cc
+++ sql/log_event.cc
@@ -3184,7 +3184,7 @@ bool Log_event::contains_partition_info(bool end_group_sets_max_dbs)
     being executed.
 
   @param        ev log event that has to be scheduled next.
-  @param       rli Pointer to coordinato's relay log info.
+  @param       rli Pointer to coordinator's relay log info.
   @return      true if error
                false otherwise
  */
@@ -6706,7 +6706,7 @@ bool Xid_log_event::do_commit(THD *thd_arg)
 
 /**
    Worker commits Xid transaction and in case of its transactional
-   info table marks the current group as done in the Coordnator's
+   info table marks the current group as done in the Coordinator's
    Group Assigned Queue.
 
    @return zero as success or non-zero as an error
