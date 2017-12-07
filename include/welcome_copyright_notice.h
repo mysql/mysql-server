db3fd68e09e90badf1b4889be6b9ed3de0eef21e
Gleb Shchepa <gleb.shchepa@oracle.com>
2017-12-07 16:21:28 +0400

    WL#8989: Create an internal API for the MySQL parser to enumerate reserved and non-reserved words
    Part I. Grammar preparation.
    
    * Replace the existent type tag <symbol> with <keyword> for a better
      readability.
    
    * Add the new type tag <keyword> to currently untyped terminal and
      non-terminal symbols.
    
    Part II. Create a simple compile-time program for transforming
             sql_yacc.yy into a .h file that contains an array of
    	 reserved and non-reserved words.
    
    * Find %token declarations with the <keyword> type tag by a regexp.
    
    * Separate %type declarations are ignored for the simplicity.
    
    Part III. Introduce a new system view: INFORMATION_SCHEMA.keywords.
    
    * Hardcode the statical list of keywords and reserved words into a view
      on top of a long JSON_TABLE(...) expression.
    
    * Use a trick in the .test file to trace changes in keywords: notify the
      developer to update the target I_S version in the mysqld binary where
      necessary -- this will force DD to refresh I_S view declarations on
      the next run.
    
    Change-Id: Ib40e7f743f4deb069df0a0bbc00c39e4460f7c33

--- include/welcome_copyright_notice.h
+++ include/welcome_copyright_notice.h
@@ -39,4 +39,40 @@
    "Oracle Corporation and/or its\naffiliates. Other names may be " \
    "trademarks of their respective\nowners.\n")
 
+
+#define ORACLE_GPL_COPYRIGHT_NOTICE(first_year) \
+  (strcmp(first_year, COPYRIGHT_NOTICE_CURRENT_YEAR) ? \
+   "/* Copyright (c) " first_year ", " COPYRIGHT_NOTICE_CURRENT_YEAR                       \
+   ", Oracle and/or its affiliates. All rights reserved.\n"                                \
+   "\n"                                                                                    \
+   "   This program is free software; you can redistribute it and/or modify\n"             \
+   "   it under the terms of the GNU General Public License as published by\n"             \
+   "   the Free Software Foundation; version 2 of the License.\n"                          \
+   "\n"                                                                                    \
+   "   This program is distributed in the hope that it will be useful,\n"                  \
+   "   but WITHOUT ANY WARRANTY; without even the implied warranty of\n"                   \
+   "   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"                    \
+   "   GNU General Public License for more details.\n"                                     \
+   "\n"                                                                                    \
+   "   You should have received a copy of the GNU General Public License\n"                \
+   "   along with this program; if not, write to the Free Software\n"                      \
+   "   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */\n"     \
+   :                                                                                       \
+   "/* Copyright (c) " first_year ", Oracle and/or its affiliates. All rights reserved.\n" \
+   "\n"                                                                                    \
+   "   This program is free software; you can redistribute it and/or modify\n"             \
+   "   it under the terms of the GNU General Public License as published by\n"             \
+   "   the Free Software Foundation; version 2 of the License.\n"                          \
+   "\n"                                                                                    \
+   "   This program is distributed in the hope that it will be useful,\n"                  \
+   "   but WITHOUT ANY WARRANTY; without even the implied warranty of\n"                   \
+   "   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"                    \
+   "   GNU General Public License for more details.\n"                                     \
+   "\n"                                                                                    \
+   "   You should have received a copy of the GNU General Public License\n"                \
+   "   along with this program; if not, write to the Free Software\n"                      \
+   "   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */\n"     \
+  )
+
+
 #endif /* _welcome_copyright_notice_h_ */
