a02c0ab92e424d90cb26ff34c0124428973d54e1
Jaideep Karande <jaideep.karande@oracle.com>
2017-12-12 18:48:34 +0100

    Bug#27191586: GROUP REPLICATION CHANNEL OBSERVERS MAY USE NULL POINTERS ON UNINSTALL
    Issue: Order of Unregister and delete observation manager should be different
    
    Description: Test cases are not failing, but cleanup sequence should be as
    suggested i.e. delete happens post un-register.
    
    Resolution:
    Corrected code cleanup part, unregister followed by delete observation manager.
    
    NOTE: This bug was found through code anaylsis under development.
    No issue seen in production.

--- rapid/plugin/group_replication/src/plugin.cc
+++ rapid/plugin/group_replication/src/plugin.cc
@@ -1073,12 +1073,6 @@ int plugin_group_replication_deinit(void *p)
     compatibility_mgr= NULL;
   }
 
-  if (channel_observation_manager != NULL)
-  {
-    delete channel_observation_manager;
-    channel_observation_manager= NULL;
-  }
-
   if (unregister_server_state_observer(&server_state_observer, p))
   {
     log_message(MY_ERROR_LEVEL,
@@ -1104,6 +1098,12 @@ int plugin_group_replication_deinit(void *p)
     log_message(MY_INFORMATION_LEVEL,
                 "All Group Replication server observers"
                 " have been successfully unregistered");
+
+  if (channel_observation_manager != NULL)
+  {
+    delete channel_observation_manager;
+    channel_observation_manager= NULL;
+  }
 
   delete gcs_module;
   gcs_module= NULL;
