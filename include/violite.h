0fe49b684fcb9d8725068a6a2eb4f3363f426db0
Thayumanavar S <thayumanavar.x.sachithanantha@oracle.com>
2017-12-14 07:35:26 +0100

    Bug#27240031 -  MEMORY LEAKS IN THREAD_POOL / VALGRIND Post push fix.

--- include/violite.h
+++ include/violite.h
@@ -204,6 +204,11 @@ int vio_getnameinfo(const struct sockaddr *sa,
 #define DES_set_key_unchecked(k,ks) des_set_key_unchecked((k),*(ks))
 #define DES_ede3_cbc_encrypt(i,o,l,k1,k2,k3,iv,e) des_ede3_cbc_encrypt((i),(o),(l),*(k1),*(k2),*(k3),(iv),(e))
 #endif
+
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+#define HAVE_OPENSSL11 1
+#endif // OPENSSL_VERSION_NUMBER
+
 /* apple deprecated openssl in MacOSX Lion */
 #ifdef __APPLE__
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
