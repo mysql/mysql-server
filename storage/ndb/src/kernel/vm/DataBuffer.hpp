eb7823103056df3e3dcee7b0926645708d9d1d69
Mauritz Sundell <mauritz.sundell@oracle.com>
2018-01-15 01:19:15 +0100

    Bug#27390816 FIX AND DISALLOW UNINITIALIZED WARNINGS IN NDB
    Argument to DataBuffer<>::first(DataBufferIterator&) is not initialised, no meaning copy it.

--- storage/ndb/src/kernel/vm/DataBuffer.hpp
+++ storage/ndb/src/kernel/vm/DataBuffer.hpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2010, 2016, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2010, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -488,7 +488,6 @@ inline
 bool
 DataBuffer<sz, Pool>::first(DataBufferIterator & it){
   ConstDataBufferIterator tmp;
-  tmp.assign(it);
   bool ret = first(tmp);
   it.assign(tmp);
   return ret;
