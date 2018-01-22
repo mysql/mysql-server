a28473eae609c548cc7ed321aec06712a7e8fe53
Mikael Ronström <mikael.ronstrom@oracle.com>
2018-01-23 00:01:01 +0100

    WL#7512: Windows compiler fix

--- storage/ndb/src/common/transporter/TransporterRegistry.cpp
+++ storage/ndb/src/common/transporter/TransporterRegistry.cpp
@@ -1024,7 +1024,8 @@ Uint32
 TransporterRegistry::spin_check_transporters(
                           TransporterReceiveHandle& recvdata)
 {
-  Uint32 res;
+  Uint32 res = 0;
+#ifndef WIN32
   Uint64 micros_passed = 0;
   bool any_connected = false;
 
@@ -1048,6 +1049,7 @@ TransporterRegistry::spin_check_transporters(
       NdbTick_Elapsed(start, now).microSec();
   } while (micros_passed < Uint64(recvdata.m_spintime));
   recvdata.m_total_spintime += micros_passed;
+#endif
   return res;
 }
 
