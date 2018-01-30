a9d39aedf48f2a4afb2115ea52dcf174dcaf7448
Bharathy Satish <bharathy.x.satish@oracle.com>
2018-01-30 05:52:32 +0100

    Bug #27322254: WITH PERSIST_ONLY, VARIABLE VALUE EQUALS 18446744073709547520                IS STORED AS -4096
    Problem: @@max_binlog_cache_size when persisted using PERSIST_ONLY reflects
    wrong value when performance_schema.persisted_variables is queried. This is
    because in Item_func_get_system_var::val_str() unsigned flag for this value
    is ignored, thus ends up converting this value to a longlong.
    
    Fix: Fix is to check for unsigned_flag flag when storing the value in a
    string buffer.

--- sql/item_func.cc
+++ sql/item_func.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2000, 2017, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2000, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -7481,7 +7481,10 @@ String* Item_func_get_system_var::val_str(String* str)
     case SHOW_HA_ROWS:
     case SHOW_BOOL:
     case SHOW_MY_BOOL:
-      str->set (val_int(), collation.collation);
+      if (unsigned_flag)
+        str->set ((ulonglong)val_int(), collation.collation);
+      else
+        str->set (val_int(), collation.collation);
       break;
     case SHOW_DOUBLE:
       str->set_real (val_real(), decimals, collation.collation);
