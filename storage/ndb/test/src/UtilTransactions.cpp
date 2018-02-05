d980e094feee6bb991cd71d31153dc45a105186a
Mikael Ronström <mikael.ronstrom@oracle.com>
2018-02-05 18:35:27 +0100

    BUG#27470261: Extended sleep after out of REDO/UNDO log in a few places

--- storage/ndb/test/src/UtilTransactions.cpp
+++ storage/ndb/test/src/UtilTransactions.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2017, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -130,7 +130,10 @@ UtilTransactions::clearTable(Ndb* pNdb,
 	if(err.status == NdbError::TemporaryError){
 	  NDB_ERR(err);
 	  closeTransaction(pNdb);
-	  NdbSleep_MilliSleep(50);
+          if (err.code == 410 || err.code == 1501)
+	    NdbSleep_MilliSleep(2000);
+          else
+	    NdbSleep_MilliSleep(50);
 	  par = 1;
 	  goto restart;
 	}
