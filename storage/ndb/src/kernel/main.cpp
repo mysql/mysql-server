8ddf09011030713c1280ff44c7f220b37121fdff
John David Duncan <john.duncan@oracle.com>
2018-02-09 06:40:19 -0800

    bug#26930148 use Ndb_opts in ndbd.

--- storage/ndb/src/kernel/main.cpp
+++ storage/ndb/src/kernel/main.cpp
@@ -53,14 +53,6 @@ static int opt_delay;
 
 extern NdbNodeBitmask g_nowait_nodes;
 
-/* FIXME  Temporary  */
-extern "C" void
-  ndb_usage(void (*)(void), const char *[], struct my_option *);
-
-extern "C" void
-  ndb_load_defaults(const char*, const char**, int *, char***);
-
-
 static struct my_option my_long_options[] =
 {
   NDB_STD_OPTS("ndbd"),
@@ -134,11 +126,6 @@ static void short_usage_sub(void)
   ndb_service_print_options("ndbd");
 }
 
-static void usage()
-{
-  ndb_usage(short_usage_sub, load_default_groups, my_long_options);
-}
-
 /**
  * C++ Standard 3.6.1/3:
  *  The function main shall not be used (3.2) within a program.
@@ -149,6 +136,8 @@ int
 real_main(int argc, char** argv)
 {
   NDB_INIT(argv[0]);
+  Ndb_opts::release();  // because ndbd can fork and call real_main() again
+  Ndb_opts opts(argc, argv, my_long_options, load_default_groups);
 
   // Print to stdout/console
   g_eventLogger->createConsoleHandler();
@@ -163,8 +152,7 @@ real_main(int argc, char** argv)
   // Turn on max loglevel for startup messages
   g_eventLogger->m_logLevel.setLogLevel(LogLevel::llStartUp, 15);
 
-  ndb_opt_set_usage_funcs(short_usage_sub, usage);
-  ndb_load_defaults(NULL,load_default_groups,&argc,&argv);
+  opts.set_usage_funcs(short_usage_sub);
 
 #ifndef DBUG_OFF
   opt_debug= "d:t:O,/tmp/ndbd.trace";
@@ -181,8 +169,7 @@ real_main(int argc, char** argv)
   }
 
   int ho_error;
-  if ((ho_error=handle_options(&argc, &argv, my_long_options,
-                               ndb_std_get_one_option)))
+  if ((ho_error=opts.handle_options()))
     exit(ho_error);
 
   if (opt_no_daemon || opt_foreground) {
