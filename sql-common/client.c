cf87d9952323eb07251702930d0ed5547f4309ed
Georgi Kodinov <georgi.kodinov@oracle.com>
2018-02-20 14:04:33 +0100

    Bug #27366143: HEAP-USE-AFTER-FREE IN CACHING SHA2
    client.cc: the client session can disconnect while trying to read
    the COM_CHANGE_USER reply and this will cause the net buffer to
    be freed.
    Added proper check for packet error.

--- sql-common/client.c
+++ sql-common/client.c
@@ -3790,6 +3790,11 @@ static int client_mpvio_read_packet(struct st_plugin_vio *mpv, uchar **buf)
 
   /* otherwise read the data */
   pkt_len= (*mysql->methods->read_change_user_result)(mysql);
+
+  /* error while reading the change user request */
+  if (pkt_len == packet_error)
+    return (int)packet_error;
+
   mpvio->last_read_packet_len= pkt_len;
   *buf= mysql->net.read_pos;
 
