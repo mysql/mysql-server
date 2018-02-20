b2477ba65202a780b02e2e6d83e2cd68ae98fee7
Sanjana DS <sanjana.ds@oracle.com>
2018-02-20 14:46:37 +0530

    Bug #27550943 MAKE THE DATA NODE LOG BUFFER SIZE CONFIGURABLE
    In Bug #24748843, asynchronous IO was introduced in ndbd/ndbmtd.
    This includes maintaining a log buffer in memory which stores logs
    before it can be written to the disk.
    There could be situations when the log buffer runs out of space if it
    gets clogged with too many log messages; some log messages get lost due
    to this.
    Though this should not occur during normal operation, it is likely to
    occur while debugging with huge amount of extra logging.
    
    So, this patch adds capability to configure the log buffer size through
    an option --logbuffer-size that can be given to ndbd/ndbmtd as a
    command-line parameter.

--- storage/ndb/src/kernel/ndbd.hpp
+++ storage/ndb/src/kernel/ndbd.hpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009, 2013, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2009, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -23,7 +23,8 @@ void
 ndbd_run(bool foreground, int report_fd,
          const char* connect_str, int force_nodeid, const char* bind_address,
          bool no_start, bool initial, bool initialstart,
-         unsigned allocated_nodeid, int connect_retries, int connect_delay);
+         unsigned allocated_nodeid, int connect_retries, int connect_delay,
+         size_t logbuffer_size);
 
 enum NdbShutdownType {
   NST_Normal,
