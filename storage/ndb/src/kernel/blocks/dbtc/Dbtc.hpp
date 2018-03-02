003444202c4c4e382c8a8b1161d15b3e007f72a6
Mauritz Sundell <mauritz.sundell@oracle.com>
2018-03-02 19:57:22 +0100

    Bug#27629680 ERROR OBJECT: DBTC (LINE: 4821) 0X00000002 CHECK TRIGPTR.P->REFCOUNT FAILED
    Increment reference count for trigger record when it is linked to
    operation record in Dbtc.
    
    Also, pass only i-value instead of pointer to Ptr<> to Dbtc::fk_scanFromChildTable.

--- storage/ndb/src/kernel/blocks/dbtc/Dbtc.hpp
+++ storage/ndb/src/kernel/blocks/dbtc/Dbtc.hpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2017, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -1882,7 +1882,7 @@ private:
   void fk_scanFromChildTable(Signal* signal,
                              TcFiredTriggerData* firedTriggerData,
                              ApiConnectRecordPtr* transPtr,
-                             TcConnectRecordPtr* opPtr,
+                             Uint32 opPtrI,
                              TcFKData* fkData,
                              Uint32 op, Uint32 attrValuesPtrI);
   void fk_scanFromChildTable_done(Signal* signal, TcConnectRecordPtr);
