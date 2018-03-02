b22b505af256851a613d28835254225a804efc5d
Knut Anders Hatlen <knut.hatlen@oracle.com>
2018-03-02 09:40:35 +0100

    Bug#27403367: GENERATED COLUMN EXPRESSIONS IGNORED WITH PREFIX INDEX
    Post-push fix for failure in gcol_ndb.gcol_keys_ndb.
    
    NDB doesn't allow prefix indexes on BLOB-based columns, so change the
    type of the column in the test case from TEXT to VARCHAR.
    
    Also harmonize the checks for whether a column is indexed in
    substitute_gc() and substitute_gc_expression() to avoid some extra
    optimizer trace output in the NDB test because the former thinks a
    column is eligible for substitution and the latter determines it's
    not.

--- sql/sql_optimizer.cc
+++ sql/sql_optimizer.cc
@@ -839,7 +839,9 @@ bool substitute_gc(THD *thd, SELECT_LEX *select_lex, Item *where_cond,
     if (tl->table->s->keys == 0) continue;
     for (uint i = 0; i < tl->table->s->fields; i++) {
       Field *fld = tl->table->field[i];
-      if (fld->is_gcol() && fld->m_indexed &&
+      if (fld->is_gcol() &&
+          !(fld->part_of_key.is_clear_all() &&
+            fld->part_of_prefixkey.is_clear_all()) &&
           fld->gcol_info->expr_item->can_be_substituted_for_gc()) {
         // Don't check allowed keys here as conditions/group/order use
         // different keymaps for that.
