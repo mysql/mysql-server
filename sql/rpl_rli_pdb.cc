21712df634870162277ed5438058f45719b1a448
Venkatesh Duggirala <venkatesh.duggirala@oracle.com>
2018-03-28 16:19:01 +0530

    Bug#27368735 BINLOG NAME AND POS ARE WRONG IN GROUP_REPLICATION_APPLIER CHANNEL'S ERROR MSGS
    Problem: When group_replication_applier channel's applier thread is facing
             an error, master_log_name and end_log_pos in the error message
             are incorrect.
    
    Analysis: In Group replication, events of a transaction are replicated
              before they were written in the binlog of the originating node. So
              the final master_log_name and the end_log_pos of those events are unknown
              at the time they are getting applied on the replica by group_replication_applier's
              applier thread.
    
    Fix: Now the error messages caused on group_replication_applier channel
         will not contain binary log name and the binary log position.

--- sql/rpl_rli_pdb.cc
+++ sql/rpl_rli_pdb.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011, 2017, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2011, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -18,6 +18,7 @@
 
 #include "log.h"                            // sql_print_error
 #include "rpl_slave_commit_order_manager.h" // Commit_order_manager
+#include "rpl_msr.h"                        // for channel_map
 
 #include "pfs_file_provider.h"
 #include "mysql/psi/mysql_file.h"
@@ -1629,6 +1630,8 @@ void Slave_worker::do_report(loglevel level, int err_code, const char *msg,
   char buff_gtid[Gtid::MAX_TEXT_LENGTH + 1];
   const char* log_name= const_cast<Slave_worker*>(this)->get_master_log_name();
   ulonglong log_pos= const_cast<Slave_worker*>(this)->get_master_log_pos();
+  bool is_group_replication_applier_channel=
+    channel_map.is_group_replication_channel_name(c_rli->get_channel(), true);
   const Gtid_specification *gtid_next= &info_thd->variables.gtid_next;
   THD *thd= info_thd;
 
@@ -1638,17 +1641,31 @@ void Slave_worker::do_report(loglevel level, int err_code, const char *msg,
       thd->get_transaction()->cannot_safely_rollback(Transaction_ctx::SESSION)))
   {
     char coordinator_errmsg[MAX_SLAVE_ERRMSG];
-
-    my_snprintf(coordinator_errmsg, MAX_SLAVE_ERRMSG,
-                "Coordinator stopped because there were error(s) in the "
-                "worker(s). "
-                "The most recent failure being: Worker %u failed executing "
-                "transaction '%s' at master log %s, end_log_pos %llu. "
-                "See error log and/or "
-                "performance_schema.replication_applier_status_by_worker "
-                "table for "
-                "more details about this failure or others, if any.",
-                internal_id, buff_gtid, log_name, log_pos);
+    if (is_group_replication_applier_channel)
+    {
+      my_snprintf(coordinator_errmsg, MAX_SLAVE_ERRMSG,
+                  "Coordinator stopped because there were error(s) in the "
+                  "worker(s). "
+                  "The most recent failure being: Worker %u failed executing "
+                  "transaction '%s'. See error log and/or "
+                  "performance_schema.replication_applier_status_by_worker "
+                  "table for "
+                  "more details about this failure or others, if any.",
+                  internal_id, buff_gtid);
+    }
+    else
+    {
+      my_snprintf(coordinator_errmsg, MAX_SLAVE_ERRMSG,
+                  "Coordinator stopped because there were error(s) in the "
+                  "worker(s). "
+                  "The most recent failure being: Worker %u failed executing "
+                  "transaction '%s' at master log %s, end_log_pos %llu. "
+                  "See error log and/or "
+                  "performance_schema.replication_applier_status_by_worker "
+                  "table for "
+                  "more details about this failure or others, if any.",
+                  internal_id, buff_gtid, log_name, log_pos);
+    }
 
     /*
       We want to update the errors in coordinator as well as worker.
@@ -1661,10 +1678,19 @@ void Slave_worker::do_report(loglevel level, int err_code, const char *msg,
     c_rli->fill_coord_err_buf(level, err_code, coordinator_errmsg);
   }
 
-  my_snprintf(buff_coord, sizeof(buff_coord),
-              "Worker %u failed executing transaction '%s' at "
-              "master log %s, end_log_pos %llu",
-              internal_id, buff_gtid, log_name, log_pos);
+  if (is_group_replication_applier_channel)
+  {
+    my_snprintf(buff_coord, sizeof(buff_coord),
+                "Worker %u failed executing transaction '%s'",
+                internal_id, buff_gtid);
+  }
+  else
+  {
+    my_snprintf(buff_coord, sizeof(buff_coord),
+                "Worker %u failed executing transaction '%s' at "
+                "master log %s, end_log_pos %llu",
+                internal_id, buff_gtid, log_name, log_pos);
+  }
 
   /*
     Error reporting by the worker. The worker updates its error fields as well
