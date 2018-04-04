b03d2bfcbc6af7c3738ac2450ddc278934f67149
John David Duncan <john.duncan@oracle.com>
2018-04-04 12:20:06 -0700

    bug#27773153 [noclose] NDB Node.js compilation failures
    Fix ndb nodejs code to remove warnings from -Wheader-hygiene
    Remove "using namespace v8" from global scope in header files.
    
    Imported from mysql-js commit d82b76c

--- storage/ndb/nodejs/jones-ndb/impl/include/common/JsConverter.h
+++ storage/ndb/nodejs/jones-ndb/impl/include/common/JsConverter.h
@@ -1,21 +1,25 @@
 /*
- Copyright (c) 2012, 2016, Oracle and/or its affiliates. All rights
- reserved.
+ Copyright (c) 2012, 2018, Oracle and/or its affiliates. All rights reserved.
  
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; version 2 of
- the License.
- 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License, version 2.0,
+ as published by the Free Software Foundation.
+
+ This program is also distributed with certain software (including
+ but not limited to OpenSSL) that is licensed under separate terms,
+ as designated in a particular file or component or in included license
+ documentation.  The authors of MySQL hereby grant you an additional
+ permission to link the program and your derivative works with the
+ separately licensed software that they have included with MySQL.
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
- 
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License, version 2.0, for more details.
+
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- 02110-1301  USA
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
  */
 
 #ifndef NODEJS_ADAPTER_INCLUDE_JSCONVERTER_H
@@ -24,9 +28,8 @@
 #include "JsWrapper.h"
 #include "node_buffer.h"
 
-using namespace v8;
-typedef Local<Value> jsvalue;
-typedef Handle<Object> jsobject;
+typedef Local<v8::Value> jsvalue;
+typedef Handle<v8::Object> jsobject;
  
 
 /*****************************************************************
@@ -153,13 +156,13 @@ public:
 
 /* Pass through of JavaScript value */
 template <>
-class JsValueConverter <Handle<Function> > {
+class JsValueConverter <Handle<v8::Function> > {
 public:
-  Local<Function> jsval;
+  Local<v8::Function> jsval;
   JsValueConverter(Local<Value> v) {
-    jsval = Local<Function>::New(v8::Isolate::GetCurrent(), Local<Function>::Cast(v));
+    jsval = Local<v8::Function>::New(v8::Isolate::GetCurrent(), Local<v8::Function>::Cast(v));
   };
-  Handle<Function> toC()  { return jsval;  };
+  Handle<v8::Function> toC()  { return jsval;  };
 };
 
 /*****************************************************************
--- storage/ndb/nodejs/jones-ndb/impl/include/ndb/NdbJsConverters.h
+++ storage/ndb/nodejs/jones-ndb/impl/include/ndb/NdbJsConverters.h
@@ -1,21 +1,25 @@
 /*
- Copyright (c) 2012, 2016, Oracle and/or its affiliates. All rights
- reserved.
+ Copyright (c) 2012, 2018, Oracle and/or its affiliates. All rights reserved.
  
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; version 2 of
- the License.
- 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License, version 2.0,
+ as published by the Free Software Foundation.
+
+ This program is also distributed with certain software (including
+ but not limited to OpenSSL) that is licensed under separate terms,
+ as designated in a particular file or component or in included license
+ documentation.  The authors of MySQL hereby grant you an additional
+ permission to link the program and your derivative works with the
+ separately licensed software that they have included with MySQL.
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
- 
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License, version 2.0, for more details.
+
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- 02110-1301  USA
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
  */
 
 #include <NdbApi.hpp>
@@ -23,10 +27,6 @@
 #include "JsConverter.h"
 
 /* Template Specializations that are specific to NDBAPI */
-
-
-using namespace v8;
-typedef Local<Value> jsvalue;
 
 
 /*****************************************************************
@@ -88,6 +88,6 @@ template <>
 inline Local<Value> toJS<NdbTransaction::CommitStatusType>
                         (Isolate * isolate,
                          NdbTransaction::CommitStatusType cval) {
-  return Number::New(isolate, static_cast<int>(cval));
+  return v8::Number::New(isolate, static_cast<int>(cval));
 };
 
