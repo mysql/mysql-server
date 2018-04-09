97782404ee65e423c41d8f84186fdfbc4762f819
Jens Even Berg Blomsoy <jens.even.blomsoy@oracle.com>
2018-04-09 13:08:28 +0200

    Bug #27780519 MISSING SPACE IN ERROR MESSAGE
      Space before parenthesis in the error message was missing for
      the ER_SQL_USER_TABLE_CREATE_WARNING and ER_SQL_USER_TABLE_ALTER_WARNING.
    
      Patch fixes missing space in these error messages and spelling mistakes
      in some of the other error messages.

--- mysql-test/include/mtr_warnings.sql
+++ mysql-test/include/mtr_warnings.sql
@@ -150,7 +150,7 @@ INSERT INTO global_suppressions VALUES
  ("Slave: Query caused different errors on master and slave"),
  ("Slave: Table .* doesn't exist"),
  ("Slave: Table width mismatch"),
- ("Slave: The incident LOST_EVENTS occured on the master"),
+ ("Slave: The incident LOST_EVENTS occurred on the master"),
  ("Slave: Unknown error.* MY-001105"),
  ("Slave: Can't drop database.* database doesn't exist"),
  ("Sort aborted"),
