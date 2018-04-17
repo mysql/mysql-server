744b89e5416498620e0f56a30bde19ab120a275e
Knut Anders Hatlen <knut.hatlen@oracle.com>
2018-04-17 13:16:09 +0200

    Bug#27872281: ADD INTERFACE FOR RETRIEVING NUMBER OF DISTINCT VALUES               FROM HISTOGRAM STATISTICS
    Added a new function histograms::Histogram::get_num_distinct_values()
    which returns the estimated number of distinct non-NULL values.
    
    Change-Id: I0c6e273ede99ec56022c249ad3b4c8c932fbda5c

--- sql/histograms/equi_height.h
+++ sql/histograms/equi_height.h
@@ -1,7 +1,7 @@
 #ifndef HISTOGRAMS_EQUI_HEIGHT_INCLUDED
 #define HISTOGRAMS_EQUI_HEIGHT_INCLUDED
 
-/* Copyright (c) 2016, 2017, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2016, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -226,6 +226,12 @@ class Equi_height : public Histogram {
     @return number of buckets in this histogram
   */
   size_t get_num_buckets() const override { return m_buckets.size(); }
+
+  /**
+    Get the estimated number of distinct non-NULL values.
+    @return number of distinct non-NULL values
+  */
+  size_t get_num_distinct_values() const override;
 
   /**
     Convert this histogram to a JSON object.
