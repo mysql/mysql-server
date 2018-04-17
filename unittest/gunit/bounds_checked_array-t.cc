ac76be3fa39fd0b12e2e83d079698e8b45a28819
Tor Didriksen <tor.didriksen@oracle.com>
2018-04-17 13:59:51 +0200

    Bug#27874498 REMOVE OBSOLETE UNIT TESTS FOR MISC QUEUE IMPLEMENTATIONS
    The bounded_queue unit tests were written for benchmarking misc. possible
    priority queue implementations. The tests are currently obsolete, and a
    maintenance burden, so removing them.
    
    Also re-enable bounds_checked_array unit test.
    
    Change-Id: I92d789e3f0eee5db3ce165d0901c00167777179a

--- unittest/gunit/bounds_checked_array-t.cc
+++ unittest/gunit/bounds_checked_array-t.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011, 2017, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2011, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -20,11 +20,10 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 
-// First include (the generated) my_config.h, to get correct platform defines.
 #include <gtest/gtest.h>
 #include <algorithm>
 
-#include "sql_array.h"
+#include "sql/sql_array.h"
 
 namespace bounds_check_array_unittest {
 
