98356f1afac1a9a27cf23eee0ba51eb52c41ab3c
Mauritz Sundell <mauritz.sundell@oracle.com>
2018-04-20 13:40:07 +0200

    Fixup: Tagged intrusive data list. Remove obselete LinkMethods

--- storage/ndb/src/kernel/blocks/dbacc/Dbacc.hpp
+++ storage/ndb/src/kernel/blocks/dbacc/Dbacc.hpp
@@ -306,21 +306,6 @@ struct Page8 {
 }; /* p2c: size = 8192 bytes */
   typedef Ptr<Page8> Page8Ptr;
 
-struct Page8SLinkMethods
-{
-  static Uint32 getNext(Page8 const& item) { return item.word32[Page8::NEXT_PAGE]; }
-  static void setNext(Page8& item, Uint32 next) { item.word32[Page8::NEXT_PAGE] = next; }
-  static void setPrev(Page8& /* item */, Uint32 /* prev */) { /* no op for single linked list */ }
-};
-
-struct ContainerPageLinkMethods
-{
-  static Uint32 getNext(Page8 const& item) { return item.word32[Page8::NEXT_PAGE]; }
-  static void setNext(Page8& item, Uint32 next) { item.word32[Page8::NEXT_PAGE] = next; }
-  static Uint32 getPrev(Page8 const& item) { return item.word32[Page8::PREV_PAGE]; }
-  static void setPrev(Page8& item, Uint32 prev) { item.word32[Page8::PREV_PAGE] = prev; }
-};
-
 struct Page32
 {
   enum { MAGIC = 0x17283482 };
