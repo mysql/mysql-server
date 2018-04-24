637026a4b79e6cb1cb744f83764c4b31668b0739
Arun Kuruvila <arun.kuruvila@oracle.com>
2018-04-24 10:02:04 +0530

    Bug#27407480: AUTOMATIC_SP_PRIVILEGES REQUIRES NEED THE               INSERT PRIVILEGES FOR MYSQL.USER TABLE
    Description:- Incorrect granting of EXECUTE and ALTER
    ROUTINE privileges when the 'automatic_sp_privileges'
    variable is set.
    
    Fix:- EXECUTE and ALTER ROUTINE privileges are correctly
    granted to the creator of the procedure when the
    'automatic_sp_privileges' is SET.

--- sql/sql_acl.cc
+++ sql/sql_acl.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2000, 2017, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2000, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -7670,19 +7670,12 @@ bool sp_grant_privileges(THD *thd, const char *sp_db, const char *sp_name,
   if (!(combo=(LEX_USER*) thd->alloc(sizeof(st_lex_user))))
     DBUG_RETURN(TRUE);
 
-  combo->user.str= sctx->user;
+  combo->user.str= (char *) sctx->priv_user;
 
   mysql_mutex_lock(&acl_cache->lock);
 
-  if ((au= find_acl_user(combo->host.str=(char*)sctx->host_or_ip,combo->user.str,FALSE)))
-    goto found_acl;
-  if ((au= find_acl_user(combo->host.str=(char*)sctx->get_host()->ptr(),
-                         combo->user.str,FALSE)))
-    goto found_acl;
-  if ((au= find_acl_user(combo->host.str=(char*)sctx->get_ip()->ptr(),
-                         combo->user.str,FALSE)))
-    goto found_acl;
-  if((au= find_acl_user(combo->host.str=(char*)"%", combo->user.str, FALSE)))
+ if ((au= find_acl_user(combo->host.str= (char *) sctx->priv_host,
+                        combo->user.str, FALSE)))
     goto found_acl;
 
   mysql_mutex_unlock(&acl_cache->lock);
