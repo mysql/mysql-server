445cd6bec421837a61e4dd640a027ff2ba9fd50f
Arnab Ray <arnab.r.ray@oracle.com>
2018-05-07 15:03:56 +0530

    Bug#23523926 DEPRECATE "PERROR --NDB" IN 7.5
    - Post push fix. Mark the deprecation warning for "perror --ndb"
      with a #ifndef MCP_BUG23523926
    
    Change-Id: I1ef25efcf706288c8d2480add971f82b701cee78

--- extra/perror.c
+++ extra/perror.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2000, 2017, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2000, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -295,10 +295,12 @@ int main(int argc,char *argv[])
         else
           msg= ndb_string;
 
+#ifndef MCP_BUG23523926
         fprintf(stderr,
                 "Warning: using '--ndb' with 'perror' is deprecated and this "
                 "functionality may not be available in the future versions, "
                 "please use 'ndb_perror' instead\n");
+#endif
 
         if (msg)
         {
