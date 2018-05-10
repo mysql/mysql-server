35ef3fe0f3bbb6026ff96c12fd97e91104177b7c
Mikael Ronström <mikael.ronstrom@oracle.com>
2018-05-10 14:07:25 +0200

    BUG#27926532 ------------ We could during a drop table create CTL files with table version 0 that aren't removed as part of drop table. We need to be ready to handle those special cases properly both during a normal LCP and during restore activities.

--- storage/ndb/src/kernel/blocks/backup/Backup.cpp
+++ storage/ndb/src/kernel/blocks/backup/Backup.cpp
@@ -11010,15 +11010,24 @@ Backup::lcp_read_ctl_file_done(Signal* signal, BackupRecordPtr ptr)
   Uint32 createTableVersion;
   Uint32 lqhCreateTableVersion;
 
-  if (lcpCtlFilePtr0->ValidFlag == 0)
+  /**
+   * Ignore LCP files that are not valid, a file that have
+   * CreateTableVersion equal to 0 is also not valid. This kind of
+   * file can be created during Drop Table processing.
+   */
+  if (lcpCtlFilePtr0->ValidFlag == 0 ||
+      lcpCtlFilePtr0->CreateTableVersion == 0)
   {
     jam();
+    lcpCtlFilePtr0->ValidFlag = 0;
     lcpCtlFilePtr0->LcpId = 0;
     lcpCtlFilePtr0->LocalLcpId = 0;
   }
-  if (lcpCtlFilePtr1->ValidFlag == 0)
+  if (lcpCtlFilePtr1->ValidFlag == 0 ||
+      lcpCtlFilePtr1->CreateTableVersion == 0)
   {
     jam();
+    lcpCtlFilePtr1->ValidFlag = 0;
     lcpCtlFilePtr1->LcpId = 0;
     lcpCtlFilePtr1->LocalLcpId = 0;
   }
