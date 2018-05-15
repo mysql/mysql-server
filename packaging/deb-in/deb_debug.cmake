b0ae3b7383ed86a16d98db7b4b243f2aa4aab79e
Lars Tangvald <lars.tangvald@oracle.com>
2018-05-15 11:39:15 +0200

    Bug#28025599	DEB PACKAGING: USE DEFAULT SSL OPTION
    Removed WITH_SSL=bundled from build script

--- packaging/deb-in/deb_debug.cmake
+++ packaging/deb-in/deb_debug.cmake
@@ -37,7 +37,6 @@ SET (DEB_RULES_DEBUG_CMAKE
 		-DINSTALL_SBINDIR=sbin \\
 		-DSYSCONFDIR=/etc/mysql \\
 		-DMYSQL_UNIX_ADDR=/var/run/mysqld/mysqld.sock \\
-		-DWITH_SSL=bundled \\
 		-DWITH_INNODB_MEMCACHED=1 \\
 		-DWITH_MECAB=system \\
 		-DWITH_NUMA=ON \\
