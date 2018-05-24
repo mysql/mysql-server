2907205d5f88bb4820c0e732bcf85ad84ebe3c67
Venkatesh Duggirala <venkatesh.duggirala@oracle.com>
2018-05-24 11:58:08 +0530

    Bug#27610678 SEMI-SYNC REPLICATION DOESN'T WORK FOR MINUTES AFTER RESTART REPLICATION
    Problem:
    ========
    
    When semisync master is busy in reading acknowledgements from one semisync
    slave, adding another semisync slave to the master is getting delayed.
    
    Analysis:
    =========
    
    In Ack_receiver::run(), server is holding m_mutex while it is reading
    acknowledgements from the ack receiver socket. The same mutex is required to
    add/remove a semisync slave to the Master. So if server is busy
    in reading the acknowledgements from the ack receiver socket, then add/remove
    a semisync will be delayed until m_mutex is released by Ack_receiver::run().
    
    
    Fix:
    ====
    Adjust acknowledgment reading logic in Ack_receiver::run() function in such
    a way that m_mutex is not holded while reading for the acknowledgments from
    the ack receiver socket.

--- plugin/semisync/semisync_master_ack_receiver.h
+++ plugin/semisync/semisync_master_ack_receiver.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2014, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -23,11 +23,12 @@
 
 struct Slave
 {
-  THD *thd;
+  uint32_t thread_id;
   Vio vio;
+  uint server_id;
+  bool net_compress;
 
   my_socket sock_fd() const { return vio.mysql_socket.fd; }
-  uint server_id() const { return thd->server_id; }
 };
 
 typedef std::vector<Slave> Slave_vector;
