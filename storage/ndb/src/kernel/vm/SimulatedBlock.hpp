2a72a5601acda2352168f1bf59826addc6f99455
Mauritz Sundell <mauritz.sundell@oracle.com>
2018-06-21 22:41:36 +0200

    Bug#28263631 SIMULATEDBLOCK LEAK FILEOUTPUTSTREAM: COUNTINGPOOL-T DYNARR256-T INTRUSIVELIST-T
    Problem
    =======
    In debug build the constructor of SimulatedBlock leaks an FileOutputStream
    and NdbOut object used to connect its debugOut object to current output FILE
    object.
    
      SimulatedBlock::SimulatedBlock(BlockNumber blockNumber,
                                     struct Block_context & ctx,
                                     Uint32 instanceNumber)
        : theNodeId(globalData.ownId),
        ...
          c_counterMgr(* this)
      #ifdef VM_TRACE
          ,debugOut(*new NdbOut(*new
    FileOutputStream(globalSignalLoggers.getOutputStream())))
      #endif
      #ifdef VM_TRACE_TIME
          ,m_currentGsn(0)
      #endif
      {
    
    In ASAN builds, at least three unit tests fail due to this leak is detected,
    those tests are CountingPool-t, DynArr256-t, and, IntrusiveList-t.
    
    The reason the leak is not detected in the data nodes themself is that they
    never end, and if they do end it is not by returning via main function and so
    leak detection is never done.
    
    Fix
    ===
    The extra NdbOut is not needed and can be removed.
    Make the FileOutputStream member of SimulatedBlock instead of do separate
    allocation of it.

--- storage/ndb/src/kernel/vm/SimulatedBlock.hpp
+++ storage/ndb/src/kernel/vm/SimulatedBlock.hpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2017, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -32,6 +32,7 @@
 #include "Pool.hpp"
 #include <NodeInfo.hpp>
 #include <NodeState.hpp>
+#include "OutputStream.hpp"
 #include "GlobalData.hpp"
 #include "LongSignal.hpp"
 #include <SignalLoggerManager.hpp>
@@ -1431,6 +1432,7 @@ public:
 
 #ifdef VM_TRACE
 public:
+  FileOutputStream debugOutFile;
   NdbOut debugOut;
   NdbOut& debugOutStream() { return debugOut; };
   bool debugOutOn();
