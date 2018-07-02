ad6444c746770cf6e43470ea4bf0b5063d40ce46
Sanjana DS <sanjana.ds@oracle.com>
2018-07-02 19:01:12 +0530

    Bug #17732772 NDB_MGMD : CLUSTER LOG SYNCHRONOUSLY WRITTEN BY SIGNAL HANDLING THREAD
    Code refactoring:
    Pass timestamp to LogHandler::append(), don't store it as a member.
    Pass it down to getDefaultHandler() and make getDefaultHeader() use this for
    header generation.
    
    This change is made so that all the LogHandler::append() parameters can be
    stored in a buffer and retrieved later and reconstitued to generate the log message.

--- storage/ndb/include/logger/SysLogHandler.hpp
+++ storage/ndb/include/logger/SysLogHandler.hpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -78,7 +78,8 @@ public:
   bool setFacility(const BaseString &facility);
 
 protected:	
-  virtual void writeHeader(const char* pCategory, Logger::LoggerLevel level);
+  virtual void writeHeader(const char* pCategory, Logger::LoggerLevel level,
+                           time_t now);
   virtual void writeMessage(const char* pMsg);
   virtual void writeFooter();
 
--- storage/ndb/src/common/logger/EventLogHandler.hpp
+++ storage/ndb/src/common/logger/EventLogHandler.hpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2010, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -48,7 +48,8 @@ public:
   static int printf(Logger::LoggerLevel m_level, const char* source_name,
                     const char* msg, ...) ATTRIBUTE_FORMAT(printf, 3, 4);
 private:
-  virtual void writeHeader(const char* pCategory, Logger::LoggerLevel level);
+  virtual void writeHeader(const char* pCategory, Logger::LoggerLevel level,
+                           time_t now);
   virtual void writeMessage(const char* pMsg);
   virtual void writeFooter();
 
