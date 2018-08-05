b9833f880bb54ed170e69412bf49e3dade9b7d5f
Lukasz Kotula <lukasz.kotula@oracle.com>
2018-08-05 13:43:32 +0200

    BUG#28125030 - C/JAVA ASYNCHRONOUS XPLUGIN TESTS CAUSES CRASH IN MYSQL SERVER 8.0.12
    Follow-up fix for build with -DWITH_ICU=system.

--- plugin/x/src/xpl_regex.cc
+++ plugin/x/src/xpl_regex.cc
@@ -53,7 +53,7 @@ bool xpl::Regex::match(const char *value) const {
    * and parse the text patter each time that xpl::Regex::match
    * is called.
    */
-  UnicodeString value_as_utf8{icu::UnicodeString::fromUTF8(value)};
+  icu::UnicodeString value_as_utf8{icu::UnicodeString::fromUTF8(value)};
   std::unique_ptr<icu::RegexMatcher> regexp{
       m_pattern->matcher(value_as_utf8, match_status)};
 
