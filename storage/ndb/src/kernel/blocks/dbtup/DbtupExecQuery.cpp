5c8d08276744abda873844e5b7bf7f38e593c7b5
Mikael Ronström <mikael.ronstrom@oracle.com>
2018-08-14 17:00:09 +0200

    WL#11722: Step 61: Activate some Partial LCP debugging

--- storage/ndb/src/kernel/blocks/dbtup/DbtupExecQuery.cpp
+++ storage/ndb/src/kernel/blocks/dbtup/DbtupExecQuery.cpp
@@ -41,10 +41,10 @@
 //#define DEBUG_LCP 1
 //#define DEBUG_DELETE 1
 //#define DEBUG_DELETE_NR 1
-//#define DEBUG_LCP_SKIP_DELETE 1
 //#define DEBUG_LCP_LGMAN 1
 //#define DEBUG_LCP_SKIP_DELETE 1
 #endif
+#define DEBUG_LCP_SKIP_DELETE 1
 
 #ifdef DEBUG_LCP
 #define DEB_LCP(arglist) do { g_eventLogger->info arglist ; } while (0)
