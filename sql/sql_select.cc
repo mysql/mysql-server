192ed2bab67a83119ae4fe93fa7ff99979683c65
Sreeharsha Ramanavarapu <sreeharsha.ramanavarapu@oracle.com>
2018-08-18 19:01:24 +0530

    Bug #28145710: SQL_SAFE_UPDATES ERROR IS INSUFFICIENTLY                INFORMATIVE
    Issue:
    ------
    When memory allocation by range optimizer exceeds
    "range_optimizer_max_mem_size", optimizer chooses a table
    scan.
    
    This can also happen with data truncation when column with
    index is a varchar and value is an integer.
    
    In the DELETE/UPDATE code, the check for SQL_SAFE_UPDATES
    interprets a table scan as the absence of a WHERE clause.
    So it rejects the DML statement with the error indicating
    a violation of the safe update mode.
    
    Solution:
    ---------
    Append the warning (data truncation /
    'range_optimizer_max_mem_size' exceeded) to the
    safe update error. This will allow the user to understand
    why index couldn't be used for the DELETE / UPDATE.
    
    A few other changes:
    1) No error will be thrown for EXPLAIN. This allows the
       user to understand why an index isn't used.
    2) For Multi-Delete / Multi-Update throw the error only if
       the target table chooses a table scan.

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2000, 2017, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2000, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -2341,11 +2341,22 @@ bool error_if_full_join(JOIN *join)
   for (uint i= 0; i < join->primary_tables; i++)
   {
     JOIN_TAB *const tab= join->best_ref[i];
+    THD *thd = join->thd;
 
-    if (tab->type() == JT_ALL && (!tab->quick()))
+    /*
+      Safe update error isn't returned if:
+      1) It is  an EXPLAIN statement OR
+      2) Table is not the target.
+
+      Append the first warning (if any) to the error message. Allows the user
+      to understand why index access couldn't be chosen.
+    */
+
+    if (!thd->lex->is_explain() && tab->table()->pos_in_table_list->updating &&
+        tab->type() == JT_ALL)
     {
-      my_message(ER_UPDATE_WITHOUT_KEY_IN_SAFE_MODE,
-                 ER(ER_UPDATE_WITHOUT_KEY_IN_SAFE_MODE), MYF(0));
+      my_error(ER_UPDATE_WITHOUT_KEY_IN_SAFE_MODE, MYF(0),
+               thd->get_stmt_da()->get_first_condition_message());
       return true;
     }
   }
