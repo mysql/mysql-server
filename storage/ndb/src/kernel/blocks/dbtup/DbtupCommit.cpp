49ae3ac0117514e735d5699a90b7dda55c2e8c3e
Sanjana DS <sanjana.ds@oracle.com>
2018-09-21 14:24:25 +0530

    Revert "Bug #27584165 TUPLE CORRUPTION DETECTED AND DATA NODE CRASH IN DBTUPEXECQUERY.CPP"
    This reverts commit 7d01d8d94784da07d8bec2ab072f8f39aba21f6d.
    
    Reverting due to the following autotest failures:
    
    > testNodeRestart -l 1 -n MixedPkReadPkUpdate
    > testNodeRestart -l 1 -n MixedReadUpdateScan
    
    (cherry picked from commit fd05b58532934c6be53d8123bfd506793c9cc6e8)

--- storage/ndb/src/kernel/blocks/dbtup/DbtupCommit.cpp
+++ storage/ndb/src/kernel/blocks/dbtup/DbtupCommit.cpp
@@ -895,8 +895,6 @@ void Dbtup::execTUP_COMMITREQ(Signal* signal)
                            regOperPtr,
                            diskPagePtr) == 0)
     {
-      // set bit
-      tuple_ptr->m_header_bits = tuple_ptr->m_header_bits | Tuple_header::DELETE_WAIT;
       return; // Data page has not been retrieved yet.
     }
     get_page = true;
@@ -966,9 +964,6 @@ skip_disk:
                     regFragPtr.p,
                     regTabPtr.p,
                     diskPagePtr);
-
-      //reset bit
-      tuple_ptr->m_header_bits = tuple_ptr->m_header_bits & (~Tuple_header::DELETE_WAIT);
     }
     else if(regOperPtr.p->op_type != ZREFRESH)
     {
--- storage/ndb/src/kernel/blocks/dbtup/DbtupScan.cpp
+++ storage/ndb/src/kernel/blocks/dbtup/DbtupScan.cpp
@@ -1118,8 +1118,7 @@ Dbtup::scanNext(Signal* signal, ScanOpPtr scanPtr)
 	  {
 	    jam();
             thbits = th->m_header_bits;
-      if (! ((thbits & Tuple_header::FREE) ||
-             (thbits & Tuple_header::DELETE_WAIT)))
+	    if (! (thbits & Tuple_header::FREE))
 	    {
               goto found_tuple;
 	    } 
@@ -1136,8 +1135,7 @@ Dbtup::scanNext(Signal* signal, ScanOpPtr scanPtr)
 	    if ((foundGCI = *th->get_mm_gci(tablePtr.p)) > scanGCI ||
                 foundGCI == 0)
 	    {
-	      if (! ((thbits & Tuple_header::FREE) ||
-	             (thbits & Tuple_header::DELETE_WAIT)))
+	      if (! (thbits & Tuple_header::FREE))
 	      {
 		jam();
 		goto found_tuple;
@@ -1231,8 +1229,7 @@ Dbtup::scanNext(Signal* signal, ScanOpPtr scanPtr)
 	  if ((foundGCI = *th->get_mm_gci(tablePtr.p)) > scanGCI ||
               foundGCI == 0)
 	  {
-      if (! ((thbits & Tuple_header::FREE) ||
-             (thbits & Tuple_header::DELETE_WAIT)))
+	    if (! (thbits & Tuple_header::FREE))
 	      break;
 	  }
 	}
