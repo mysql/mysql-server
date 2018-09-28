632545c73b6c0da0c3523281160d3957298768f2
Erlend Dahl <erlend.dahl@oracle.com>
2018-09-28 12:52:04 +0200

    Revert "WL#11585 Integrate InnoDB parallel read with Secondary engine."
    This reverts commit 76f3155ad4d48fdbabcf2d1b03ab25782b930070.

--- storage/innobase/handler/ha_innodb.h
+++ storage/innobase/handler/ha_innodb.h
@@ -32,7 +32,7 @@ this program; if not, write to the Free Software Foundation, Inc.,
 #include <sys/types.h>
 #include "handler.h"
 #include "my_dbug.h"
-#include "row0pread-adapter.h"
+#include "row0sec_engine.h"
 #include "trx0trx.h"
 
 /** "GEN_CLUST_INDEX" is the name reserved for InnoDB default
@@ -422,7 +422,7 @@ class ha_innobase : public handler {
   @param[in, out]   num_threads     number of threads to be spawned
   @return error code
   @retval 0 on success */
-  int pread_adapter_scan_get_num_threads(size_t &num_threads) override;
+  int secondary_engine_scan_get_num_threads(size_t &num_threads) override;
 
   /** Start parallel read of InnoDB records.
   @param[in]      thread_contexts context for each of the spawned threads
@@ -434,10 +434,10 @@ class ha_innobase : public handler {
   thread when processing of rows has ended.
   @return error code
   @retval 0 on success */
-  int pread_adapter_scan_parallel_load(
-      void **thread_contexts, pread_adapter_pload_init_cbk load_init_fn,
-      pread_adapter_pload_row_cbk load_rows_fn,
-      pread_adapter_pload_end_cbk load_end_fn) override;
+  int secondary_engine_scan_parallel_load(
+      void **thread_contexts, secondary_engine_pload_init_cbk load_init_fn,
+      secondary_engine_pload_row_cbk load_rows_fn,
+      secondary_engine_pload_end_cbk load_end_fn) override;
 
   bool check_if_incompatible_data(HA_CREATE_INFO *info,
                                   uint table_changes) override;
@@ -661,8 +661,10 @@ class ha_innobase : public handler {
   /** If mysql has locked with external_lock() */
   bool m_mysql_has_locked;
 
-  /** Do a parallel scan of an index. */
-  Parallel_reader_adapter *m_parallel_reader{nullptr};
+  /** Secondary engine context to traverse a table index and send all the rows
+  from InnoDB to secondary engine.
+  @note Used only by the secondary engine. */
+  Secondary_engine_reader *m_secondary_engine_reader{nullptr};
 };
 
 struct trx_t;
