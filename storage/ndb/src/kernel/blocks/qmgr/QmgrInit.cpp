7cc7d30af4192d0bae99d6715b31d29cacf50e53
Frazer Clement <frazer.clement@oracle.com>
2018-10-04 11:50:31 +0100

    Bug #28380808 NDB : GRACEFUL STOP API STATE NOTIFICATION TOO SLOW
    APIs should observe that a node is moving through SL_STOPPING
    phases and stop using the node for new transactions.  This
    minimises the disruption in the later phases of the node stopping.
    
    However, APIs are only informed of node state changes via periodic
    heartbeat signals, so may not have the chance to take avoiding
    action.
    
    Where the heartbeat interval is high, APIs continue to attempt
    to use nodes which are shutting down, generating unnecessary
    failures.
    
    Solution is to directly notify all APIs when a data node is
    being gracefully stopped.  This allows them to experience
    minimal disruption.

--- storage/ndb/src/kernel/blocks/qmgr/QmgrInit.cpp
+++ storage/ndb/src/kernel/blocks/qmgr/QmgrInit.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2017, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -247,6 +247,9 @@ Qmgr::Qmgr(Block_context& ctx)
 
   addRecSignal(GSN_READ_LOCAL_SYSFILE_CONF,
                &Qmgr::execREAD_LOCAL_SYSFILE_CONF);
+
+  addRecSignal(GSN_NODE_STATE_REP,
+               &Qmgr::execNODE_STATE_REP, true); // Override
 
   initData();
 }//Qmgr::Qmgr()
