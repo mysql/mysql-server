b08a8908ccb4ffc597283f9ec574e6db3532280c
Erlend Dahl <erlend.dahl@oracle.com>
2018-10-16 13:45:18 +0200

    Bug#28799660 EXTENDED OR BADLY FORMATTED COPYRIGHT HOLDERS

--- sql/regexp/regexp_facade.h
+++ sql/regexp/regexp_facade.h
@@ -1,7 +1,7 @@
 #ifndef SQL_REGEXP_REGEXP_FACADE_H_
 #define SQL_REGEXP_REGEXP_FACADE_H_
 
-/* Copyright (c) 2017, 2018 Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2017, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
