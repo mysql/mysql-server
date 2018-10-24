07eaeb2bbab9ed3b4855aae091106e6b583ac737
Martin Sköld <Martin.Skold@oracle.com>
2018-10-24 09:19:48 +0200

    Bug #25817834 MYSQLD CRASH DURING STARTUP DUE TO BINLOG PURGE
    Crash occurred because server was not ready to execucte delete of
    rows in ndb_binlog_index table before server is properly started.
    Changed behavior when binlog purge is requested before the server
    is properly started to delay the actual purge of ndb_binlog_index.
    By saving purge requests in a queue they can be done by the binlog_thread
    at startup and when the server is properly started.
    Added new ndb_binlog_purge_startup test that forces purging of all
    binlog files at startup.

--- sql/ndb_component.h
+++ sql/ndb_component.h
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2011, 2016, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2011, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -40,6 +40,11 @@ public:
   virtual int stop();
   virtual int deinit();
 
+  /*
+    Check if the server has started. This checks if the Ndb_component
+    has been informed that the server has started.
+   */
+  bool is_server_started();
 protected:
   /**
    * Con/de-structor is protected...so that sub-class needs to provide own
