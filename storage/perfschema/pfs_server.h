c977c4c7f84860bcf876e47acfbb9d24a754cbd8
Marc Alff <marc.alff@oracle.com>
2018-11-27 10:38:13 +0100

    Post merge fix.
    WL#12445 Improve Windows named pipe access control
    introduced new instrumented read write locks,
    hitting the current limit in the performance schema:
      PFS_MAX_RWLOCK_CLASS 40
    
    This fix increases PFS_MAX_RWLOCK_CLASS to 50,
    and adjusts all tests results accordingly.
    
    (cherry picked from commit 4afa5c6941261d0deef272d64ba5ba2e2c8402d0)

--- storage/perfschema/pfs_server.h
+++ storage/perfschema/pfs_server.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008, 2017, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2008, 2018, Oracle and/or its affiliates. All rights reserved.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -28,7 +28,7 @@
   #define PFS_MAX_MUTEX_CLASS 210
 #endif
 #ifndef PFS_MAX_RWLOCK_CLASS
-  #define PFS_MAX_RWLOCK_CLASS 40
+  #define PFS_MAX_RWLOCK_CLASS 50
 #endif
 #ifndef PFS_MAX_COND_CLASS
   #define PFS_MAX_COND_CLASS 80
