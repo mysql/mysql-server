d5f5366c8225cf54a302784056d83d404fa221b9
Frazer Clement <frazer.clement@oracle.com>
2018-12-17 13:12:58 +0000

    Bug #28435082 SQL NODE CRASHES TRYING TO GET TABLE NAME WHILE HANDLING COMMIT ERROR
    The NdbTransaction::getNdbErrorOperation() method returns a pointer to the
    NdbOperation object which has most recently had an error condition, if
    any.
    
    MySQLD uses the internal NdbTransaction::releaseCompletedOperations() method
    to release NdbApi side operation objects after a batch is executed.
    
    In cases where NdbTransaction::getNdbErrorOperation() is called after
    the operation with an error has been released by
    NdbTransaction::releaseCompletedOperations(), it is possible for
    invalid data to be read.
    
    This problem is corrected by invalidating the NdbTransaction's
    error operation pointer when releasing completed operations.
    
    Tested by :
     - Adding debug-only extra invalidation of operation members
       when operations are released
     - Using Insert Ignore and TransacionInactiveTimeout to :
       - Cause an operation error without aborting the transaction
       - Hit a non-operation related error on transaction COMMIT

--- storage/ndb/src/ndbapi/NdbOperation.cpp
+++ storage/ndb/src/ndbapi/NdbOperation.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2016, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -230,6 +230,13 @@ NdbOperation::release()
 
   theLockHandle = NULL;
   m_blob_lock_upgraded = false;
+
+#ifndef NDEBUG
+  // Poison members to detect late usage
+  m_accessTable = m_currentTable = (NdbTableImpl*) 0x1;
+  theNdbCon = (NdbTransaction*) 0x1;
+  m_key_record = m_attribute_record = (NdbRecord*) 0x1;
+#endif
 }
 
 void
--- storage/ndb/src/ndbapi/NdbTransaction.cpp
+++ storage/ndb/src/ndbapi/NdbTransaction.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2017, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -1315,6 +1315,8 @@ NdbTransaction::releaseCompletedOperations()
   releaseOps(theCompletedFirstOp);
   theCompletedFirstOp = NULL;
   theCompletedLastOp = NULL;
+  theErrorLine = 0;
+  theErrorOperation = NULL;
 }//NdbTransaction::releaseCompletedOperations()
 
 
