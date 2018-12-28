bb3df6f708c47fb14bff493fb562b027cf81183c
root <root@debian-c-32-64gib-nyc1-01.localdomain>
2018-12-28 05:19:39 +0000

    remove cout debugging calls

--- sql/sql_client.cc
+++ sql/sql_client.cc
@@ -20,6 +20,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 
+
 /*
   This files defines some MySQL C API functions that are server specific
 */
