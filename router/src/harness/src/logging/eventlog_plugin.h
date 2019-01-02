9c5d2f50a10d093b6f504d4db68ff139c9c9502c
Jon Olav Hauglid <jon.hauglid@oracle.com>
2019-01-02 12:52:22 +0100

    Bug#29160214: NINJA LLVM BUILD BROKEN
    Fix inconsistent language linkage for harness_plugin_eventlog.
    
    Change-Id: Ibf76dcd6ccc966fe2adcf703af62b4572cfa978e

--- router/src/harness/src/logging/eventlog_plugin.h
+++ router/src/harness/src/logging/eventlog_plugin.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+  Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License, version 2.0,
@@ -30,7 +30,7 @@
 #include "mysql/harness/plugin.h"
 
 constexpr const char *kEventlogPluginName = "eventlog";
-extern mysql_harness::Plugin harness_plugin_eventlog;
+extern "C" mysql_harness::Plugin harness_plugin_eventlog;
 
 /**
  * Windows specific logging handler(sink) that writes the logs to the Windows
