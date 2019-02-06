0e261ee600bf194445fc99b0d9ea757735d22859
Dinesh Surya Prakash <dinesh.prakash@oracle.com>
2019-02-06 12:40:18 +0530

    Bug#29284643 ASAN BUILD USING CLANG-7 DETECTS MEMORY LEAK WHEN STARTING NDB_MGMD
    ConfigManager::find_nodeid_from_config() uses load_config() to load the
    user configuration from --config-file. But, did not release the memory when
    it returns. So, released the memory before returning the function to avoid
    the memory leak.
    
    Change-Id: I09366cc7e50cd98262da03edd7ad09cf3bc4fad2

--- storage/ndb/src/mgmsrv/ConfigManager.cpp
+++ storage/ndb/src/mgmsrv/ConfigManager.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008, 2013, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2008, 2019, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -242,6 +242,7 @@ ConfigManager::find_nodeid_from_config(void)
     return 0;
   }
 
+  delete conf;
   return found_nodeid;
 }
 
