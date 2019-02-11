9199e52cee10a120549fbfb8001cf542ca62bc06
Mauritz Sundell <mauritz.sundell@oracle.com>
2019-02-11 17:20:43 +0100

    Bug#29338712 PB2: NDBMUTEX.C:149:5:ERROR: ISO C90 FORBIDS MIXED DECLARATIONS AND CODE [-WERRO
    On Jan 25 -Werror was turned on also for release builds of build of el7-x86-64bit-werror for daily-5.7-cluster-7.6 in PB2.
    
    This turned a previous warning into build failure.
    
    NdbMutex.c:149:5:error: ISO C90 forbids mixed declarations and code [-Werror=declaration-after-state[...]
    
    Fixed by adding scope around declaration and associated code.
    
    Reviewed-by: Frazer Clement <frazer.clement@oracle.com>

--- storage/ndb/src/common/portlib/NdbMutex.c
+++ storage/ndb/src/common/portlib/NdbMutex.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2019, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -146,6 +146,7 @@ int NdbMutex_InitWithName_local(NdbMutex* pNdbMutex,
 #else
 #if defined(HAVE_PTHREAD_MUTEXATTR_INIT) && \
     defined(HAVE_PTHREAD_MUTEXATTR_SETPSHARED)
+  {
     pthread_mutexattr_t t;
     pthread_mutexattr_init(&t);
     pthread_mutexattr_setpshared(&t, PTHREAD_PROCESS_SHARED);
@@ -156,6 +157,7 @@ int NdbMutex_InitWithName_local(NdbMutex* pNdbMutex,
 
     require(result == 0);
     pthread_mutexattr_destroy(&t);
+  }
 #else
     if (shared)
       result = 1;
