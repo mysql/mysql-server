01c2663594be72c9abecbf1ac184058158424342
Jon Olav Hauglid <jon.hauglid@oracle.com>
2019-02-13 09:08:12 +0100

    Bug#29341281: REMOVE UNNECESSARY CMAKE CHECKS [noclose]
    Remove some InnoDB CMake checks where the results are unused.
    
    Change-Id: I8355500edadef357315206fe00f9dd7d51e41f23

--- cmake/os/WindowsCache.cmake
+++ cmake/os/WindowsCache.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2010, 2018, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2010, 2019, Oracle and/or its affiliates. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2.0,
@@ -227,7 +227,6 @@ SET(HAVE_STRUCT_IN6_ADDR TRUE CACHE INTERNAL "")
 #
 SET(HAVE_SCHED_GETCPU CACHE  INTERNAL "")
 SET(HAVE_NANOSLEEP CACHE  INTERNAL "")
-SET(HAVE_ASPRINTF CACHE INTERNAL "")
 SET(HAVE_NUMA_H CACHE INTERNAL "")
 SET(HAVE_NUMAIF_H CACHE INTERNAL "")
 
