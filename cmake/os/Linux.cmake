304dfe8ca459973634b8117686dbe8b02e195631
Tor Didriksen <tor.didriksen@oracle.com>
2019-03-12 08:59:22 +0100

    Bug#29471723 BROKEN BUILD FEDORA 28, UNDEFINED REFERENCE TO SYMBOL '__BSS_START'
    The patch for
    Bug#29389789 HANDLE SIGNALS, AND ENABLE STACK TRACE IN UNIT TESTS
    added dependencies on stack tracing routines from gunit_test_main.cc.
    
    On Fedora 28, we get undefined reference to symbol '__bss_start' for
    several unit tests and plugins when linking with the
    standard linker (not using -fuse-ld=gold)
    
    The workaround is to add "--copy-dt-needed-entries" to linker flags.
    
    Change-Id: Ib07f03874e795039719eb82a02ea695fb8e8c82c

--- cmake/os/Linux.cmake
+++ cmake/os/Linux.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2010, 2018, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2010, 2019, Oracle and/or its affiliates. All rights reserved.
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2.0,
@@ -33,6 +33,15 @@ ENDIF()
 
 IF(EXISTS "/etc/alpine-release")
   SET(LINUX_ALPINE 1)
+ENDIF()
+
+IF(EXISTS "/etc/fedora-release")
+  SET(LINUX_FEDORA 1)
+  FILE(READ "/etc/fedora-release" FEDORA_RELEASE)
+  IF(FEDORA_RELEASE MATCHES "Fedora" AND
+      FEDORA_RELEASE MATCHES "28")
+    SET(LINUX_FEDORA_28 1)
+  ENDIF()
 ENDIF()
 
 # We require at least GCC 5.3 or Clang 3.4.
