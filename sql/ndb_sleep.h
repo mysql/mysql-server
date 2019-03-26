2a543dadf2a4ea9fefaff1ab10fcfb838c788764
Lakshmi Narayanan Sreethar <lakshmi.narayanan.sreethar@oracle.com>
2019-03-26 21:26:50 +0530

    WL#12731: Improve database synchronization during mysqld startup [Part 5/6]
    Add wrapper function ndb_trans_retry() that executes the given
    function and if required, retries the function on temporary error.
    The function internally uses the newly added ndb_execute_and_retry()
    function to perform the execution.
    
    Also added a missing include file required to use the rand() function
    in ndb_sleep.h header file.
    
    Change-Id: Ibe971752d2d4f4da9de756f11037113b7569ad2e

--- sql/ndb_sleep.h
+++ sql/ndb_sleep.h
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2016, 2017, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2016, 2019, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -31,6 +31,7 @@
 #include <sys/select.h>
 #include <time.h>
 #endif
+#include <stdlib.h>
 
 /* Wait a given number of milliseconds */
 static inline
