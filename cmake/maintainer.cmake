1cff3f7eac7f4878c8f913248ee298747e931cf2
Jan Kneschke <jan.kneschke@oracle.com>
2019-04-12 08:57:20 +0200

    Bug#30042753 disable verbose warnings on clang-cl
    Problem
    =======
    
    By default '-Wall -Wextra' is passed to clang based builds to enable
    _most_ warnings.
    
    In case of clang-cl '-Wall' is treated as the cl.exe opton '-Wall' which
    means "enable _all_ warnings" (ala -Weverything in clang/gcc-speek).
    
    It leads to lots of unwanted warnings like:
    
      -Wc++98-compat
    
    which part of clang's -Weverything, but not part of '-Wall'.
    
    Related: https://reviews.llvm.org/rL319116
    
    Change
    ======
    
    - for clang-cl replace '-Wall -Wextra' by '/W4'
    
    RB: 22573

--- cmake/maintainer.cmake
+++ cmake/maintainer.cmake
@@ -47,6 +47,11 @@ IF(SIZEOF_VOIDP EQUAL 8)
   STRING_APPEND(MY_WARNING_FLAGS " -Wmissing-format-attribute")
 ENDIF()
 
+# Clang 6.0 and newer on Windows treat -Wall as -Weverything; use /W4 instead
+IF(WIN32_CLANG)
+  STRING(REPLACE "-Wall -Wextra" "/W4" MY_WARNING_FLAGS "${MY_WARNING_FLAGS}")
+ENDIF()
+
 # Common warning flags for GCC and Clang
 SET(MY_C_WARNING_FLAGS "${MY_WARNING_FLAGS} -Wwrite-strings")
 
@@ -56,60 +61,6 @@ SET(MY_CXX_WARNING_FLAGS "${MY_WARNING_FLAGS} -Woverloaded-virtual -Wcast-qual")
 # The default =3 given by -Wextra is a bit too strict for our code.
 IF(CMAKE_COMPILER_IS_GNUCXX)
   MY_ADD_CXX_WARNING_FLAG("Wimplicit-fallthrough=2")
-ENDIF()
-
-# Clang 6.0 and newer on Windows seems to enable -Weverything; turn off some
-# that are way too verbose for us.
-IF(WIN32 AND CMAKE_C_COMPILER_ID MATCHES "Clang")
-  # We don't need C++98 compatibility.
-  MY_ADD_CXX_WARNING_FLAG("Wno-c++98-compat")
-  MY_ADD_CXX_WARNING_FLAG("Wno-c++98-compat-pedantic")
-
-  # Also warns on using NULL.
-  MY_ADD_CXX_WARNING_FLAG("Wno-zero-as-null-pointer-constant")
-
-  # Should be enabled globally, but too noisy right now.
-  MY_ADD_CXX_WARNING_FLAG("Wno-old-style-cast")
-  MY_ADD_CXX_WARNING_FLAG("Wno-reserved-id-macro")
-  MY_ADD_CXX_WARNING_FLAG("Wno-documentation")
-  MY_ADD_CXX_WARNING_FLAG("Wno-documentation-pedantic")
-  MY_ADD_CXX_WARNING_FLAG("Wno-documentation-unknown-command")
-  MY_ADD_CXX_WARNING_FLAG("Wno-missing-variable-declarations")
-  MY_ADD_CXX_WARNING_FLAG("Wno-language-extension-token")
-  MY_ADD_CXX_WARNING_FLAG("Wno-shorten-64-to-32")
-  MY_ADD_CXX_WARNING_FLAG("Wno-shadow")
-  MY_ADD_CXX_WARNING_FLAG("Wno-c++2a-compat")
-  MY_ADD_CXX_WARNING_FLAG("Wno-macro-redefined")
-  MY_ADD_CXX_WARNING_FLAG("Wno-unused-const-variable")
-  MY_ADD_CXX_WARNING_FLAG("Wno-gnu-anonymous-struct")
-
-  # Various things we don't want to warn about.
-  MY_ADD_CXX_WARNING_FLAG("Wno-global-constructors")
-  MY_ADD_CXX_WARNING_FLAG("Wno-exit-time-destructors")
-  MY_ADD_CXX_WARNING_FLAG("Wno-undefined-func-template")
-  MY_ADD_CXX_WARNING_FLAG("Wno-nonportable-system-include-path")
-  MY_ADD_CXX_WARNING_FLAG("Wno-sign-conversion")
-  MY_ADD_CXX_WARNING_FLAG("Wno-unused-exception-parameter")
-  MY_ADD_CXX_WARNING_FLAG("Wno-missing-prototypes")
-  MY_ADD_CXX_WARNING_FLAG("Wno-shadow-field-in-constructor")
-  MY_ADD_CXX_WARNING_FLAG("Wno-float-equal")
-  MY_ADD_CXX_WARNING_FLAG("Wno-float-conversion")
-  MY_ADD_CXX_WARNING_FLAG("Wno-double-promotion")
-  MY_ADD_CXX_WARNING_FLAG("Wno-covered-switch-default")
-  MY_ADD_CXX_WARNING_FLAG("Wno-used-but-marked-unused")
-  MY_ADD_CXX_WARNING_FLAG("Wno-conversion")
-  MY_ADD_CXX_WARNING_FLAG("Wno-sign-conversion")
-  MY_ADD_CXX_WARNING_FLAG("Wno-microsoft-pure-definition")  # False positives.
-  MY_ADD_CXX_WARNING_FLAG("Wno-format-nonliteral")
-  MY_ADD_CXX_WARNING_FLAG("Wno-format-pedantic")
-  MY_ADD_CXX_WARNING_FLAG("Wno-cast-align")
-  MY_ADD_CXX_WARNING_FLAG("Wno-gnu-zero-variadic-macro-arguments")
-  MY_ADD_CXX_WARNING_FLAG("Wno-comma")
-  MY_ADD_CXX_WARNING_FLAG("Wno-sign-compare")
-  MY_ADD_CXX_WARNING_FLAG("Wno-switch-enum")
-  MY_ADD_CXX_WARNING_FLAG("Wno-implicit-fallthrough")  # Does not take the same signals as GCC.
-  MY_ADD_CXX_WARNING_FLAG("Wno-unused-macros")
-  MY_ADD_CXX_WARNING_FLAG("Wno-disabled-macro-expansion")
 ENDIF()
 
 IF(CMAKE_COMPILER_IS_GNUCC)
