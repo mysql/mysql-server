ec39bef9796c351c141612d635333f0b71c52434
Anushree Prakash B <anushree.prakash.b@oracle.com>
2019-04-22 13:17:14 +0530

    Bug#25933891 - STATUS_BY_THREAD DOES NOT HAVE ANY DATA WHEN                THE THREAD POOL PLUGIN IS ENABLED
    DESCRIPTION:
    ============
    When the thread pool plugin is enabled, the
    performance_schema.status_by_thread does not contain any
    data.
    
    ANALYSIS:
    =========
    Without thread pool, the THD is associated with the
    instrumented thread while handling new incoming connections.
    However, without the thread pool, the association of
    THD to the instrumented thread was missing because of which
    the data from some pfs tables could not be retrieved.
    
    FIX:
    ====
    Make sure that the thread is associated with the
    instrumented thread so that the output of the table
    status_by_thread is fetched correctly. The patch also fixes
    a similar problem for the pfs table variables_by_thread.
    
    RB: 21798

--- sql/sql_thd_internal_api.cc
+++ sql/sql_thd_internal_api.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2015, 2019, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -42,6 +42,7 @@ int thd_init(THD *thd, char *stack_start, bool bound, PSI_thread_key psi_key)
   {
     PSI_THREAD_CALL(set_thread_os_id)(psi);
   }
+  PSI_THREAD_CALL(set_thread_THD)(psi, thd);
   thd->set_psi(psi);
 #endif
 
