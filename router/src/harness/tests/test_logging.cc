358711c915124907ce7ef1b5111b72f2017537da
Thomas Nielsen <thomas.nielsen@oracle.com>
2019-05-09 08:30:39 +0200

    Bug#29540910 LOG LINES OVER 512 CHARACTERS ARE TRUNCATED
    The return value from snprintf() was unchecked when formatting into a fixed size
    buffer, resulting in trucation of log messages longer than 512 characters. Fix
    checks return value, and falls back to using a dynamically allocated buffer if
    needed. Fix includes minor extension to existing testcase.
    
    RB: 22063

--- router/src/harness/tests/test_logging.cc
+++ router/src/harness/tests/test_logging.cc
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2016, 2018, Oracle and/or its affiliates. All rights reserved.
+  Copyright (c) 2016, 2019, Oracle and/or its affiliates. All rights reserved.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License, version 2.0,
@@ -549,6 +549,9 @@ TEST_F(LoggingTest, Messages) {
   check_message("Sloth tantrum", LogLevel::kWarning, " WARNING ");
   check_message("Russel's teapot", LogLevel::kInfo, " INFO ");
   check_message("Bugs galore", LogLevel::kDebug, " DEBUG ");
+
+  // Ensure no truncation of long messages
+  check_message(std::string(512, 'a'), LogLevel::kInfo, " INFO ");
 
   // clean up
   g_registry->remove_handler("TestStreamHandler");
