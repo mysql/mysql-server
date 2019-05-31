403b14c9ae8301d7cc068e77355aca8aab731875
Jan Kneschke <jan.kneschke@oracle.com>
2019-05-31 16:04:50 +0200

    WL#10703 scale routing core to 50k connections [5/9]
    Change
    ======
    
    added high-level tests and example applications.
    
    RB: 24681

---
+++ router/src/harness/tests/net_ts_accept.cc
@@ -0,0 +1,186 @@
+/*
+  Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2.0,
+  as published by the Free Software Foundation.
+
+  This program is also distributed with certain software (including
+  but not limited to OpenSSL) that is licensed under separate terms,
+  as designated in a particular file or component or in included license
+  documentation.  The authors of MySQL hereby grant you an additional
+  permission to link the program and your derivative works with the
+  separately licensed software that they have included with MySQL.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <iostream>
+
+#include "mysql/harness/net_ts.h"
+
+class Connection {
+ public:
+  Connection(net::ip::tcp::socket &&conn)
+      : conn_{std::move(conn)},
+        read_buffer_{net::buffer(read_buffer_storage_)} {}
+
+  void async_read_all() {
+    conn_.async_receive(read_buffer_, [this](std::error_code ec,
+                                             size_t /* bytes_transferred */) {
+      if (ec) {
+        if (ec != net::stream_errc::eof) {
+          std::cerr << __LINE__ << ": "
+                    << "failed: " << ec << std::endl;
+        }
+
+        // the socket is closed, we can close too without waiting
+        conn_.close();
+        return;  // something failed
+      }
+
+      //      std::cerr << __LINE__ << ": recv()ed: " <<
+      //      bytes_transferred << std::endl;
+
+      // if there is more, read that too until we reach EOF
+      async_read_all();
+
+      return;
+    });
+  }
+
+ private:
+  net::ip::tcp::socket conn_;
+
+  std::array<char, 1024 * 1024> read_buffer_storage_;
+  net::mutable_buffer read_buffer_;
+};
+
+class Drainer {
+ public:
+  Drainer(net::io_context &io_ctx)
+      : io_ctx_{io_ctx}, acceptor_{io_ctx}, ac_{acceptor_, connections_} {}
+
+  stdx::expected<net::ip::tcp::resolver::results_type, std::error_code> resolve(
+      const std::string &hostname, const std::string &service) {
+    net::ip::tcp::resolver resolver(io_ctx_);
+    return resolver.resolve(hostname, service, net::ip::tcp::resolver::passive);
+  }
+
+  stdx::expected<void, std::error_code> open(const net::ip::tcp::endpoint &ep) {
+#ifdef SOCK_NONBLOCK
+    int open_flags = SOCK_NONBLOCK;
+#else
+    int open_flags = 0;
+#endif
+
+    // try to set the non-blocking mode on the socket right from the start,
+    // saving a syscall later for ioctl() later
+    auto open_res = acceptor_.open(ep.protocol(), open_flags);
+    if (!open_res) return open_res;
+
+    acceptor_.set_option(net::socket_base::reuse_address(true));
+#ifdef TCP_FASTOPEN
+    acceptor_.set_option(net::ip::tcp::fast_open(0));
+#endif
+    if (!acceptor_.native_non_blocking()) acceptor_.native_non_blocking(true);
+
+    return {};
+  }
+
+  stdx::expected<void, std::error_code> bind(const net::ip::tcp::endpoint &ep) {
+    return acceptor_.bind(ep);
+  }
+
+  stdx::expected<void, std::error_code> listen() {
+    return acceptor_.listen(128);
+  }
+
+  class Acceptor {
+   public:
+    Acceptor(net::ip::tcp::acceptor &acceptor,
+             std::list<Connection> &connections)
+        : acceptor_(acceptor), connections_(connections) {}
+
+    void operator()(std::error_code ec) {
+      if (ec) return;
+#ifdef SOCK_NONBLOCK
+      int open_flags = SOCK_NONBLOCK;
+#else
+      int open_flags = 0;
+#endif
+
+      for (;;) {
+        auto res = acceptor_.accept(open_flags);
+        if (res) {
+          connections_.emplace_back(std::move(*res));
+          connections_.back().async_read_all();
+        } else if (res.error() == std::errc::operation_would_block) {
+          acceptor_.async_wait(net::socket_base::wait_read, std::move(*this));
+          return;
+        } else {
+          // error ... wanna log it?
+          return;
+        }
+      }
+    }
+
+   private:
+    net::ip::tcp::acceptor &acceptor_;
+    std::list<Connection> &connections_;
+  };
+
+  void start_accept() { ac_({}); }
+
+ private:
+  net::io_context &io_ctx_;
+
+  std::list<Connection> connections_;
+
+  net::ip::tcp::acceptor acceptor_;
+
+  Acceptor ac_;
+};
+
+int main() {
+  net::impl::socket::init();
+
+  net::io_context io_ctx;
+
+  Drainer drainer(io_ctx);
+
+  auto resolve_res = drainer.resolve("", "3308");
+  if (!resolve_res) {
+    std::cerr << __LINE__
+              << ": resolve() failed: " << resolve_res.error().message()
+              << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  auto open_res = drainer.open(resolve_res->begin()->endpoint());
+  if (!open_res) {
+    std::cerr << __LINE__ << ": open() failed: " << open_res.error().message()
+              << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  auto bind_res = drainer.bind(resolve_res->begin()->endpoint());
+  if (!bind_res) {
+    std::cerr << __LINE__ << ": bind() failed: (" << bind_res.error() << ") "
+              << bind_res.error().message() << std::endl;
+    return EXIT_FAILURE;
+  }
+  drainer.listen();
+  drainer.start_accept();
+
+  io_ctx.run();
+
+  return 0;
+}
---
+++ router/src/harness/tests/net_ts_forward.cc
@@ -0,0 +1,271 @@
+/*
+  Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2.0,
+  as published by the Free Software Foundation.
+
+  This program is also distributed with certain software (including
+  but not limited to OpenSSL) that is licensed under separate terms,
+  as designated in a particular file or component or in included license
+  documentation.  The authors of MySQL hereby grant you an additional
+  permission to link the program and your derivative works with the
+  separately licensed software that they have included with MySQL.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <csignal>  // SIGPIPE
+#include <iostream>
+
+#include "mysql/harness/net_ts.h"
+#include "mysql/harness/net_ts/impl/file.h"
+
+#ifndef SPLICE_F_MOVE
+#define SPLICE_F_MOVE 0
+#endif
+
+class Splicer {
+ public:
+  Splicer(net::ip::tcp::socket &sock_in, net::ip::tcp::socket &sock_out)
+      : sock_in_{sock_in}, sock_out_{sock_out} {}
+
+  stdx::expected<void, std::error_code> open() {
+    close();
+
+    auto res = net::impl::file::pipe();
+    if (!res) return stdx::make_unexpected(res.error());
+
+#if 0
+    // get the size of the queue
+    std::cerr << __LINE__ << ": "
+              << net::impl::file::fcntl(res->at(0),
+                                        net::impl::file::get_pipe_size())
+                     .value()
+              << std::endl;
+#endif
+
+    fds_ = *res;
+
+    return {};
+  }
+
+  stdx::expected<size_t, std::error_code> read_some(size_t len) {
+    auto res = net::impl::socket::splice_to_pipe(
+        sock_in_.native_handle(), fds_.second, len, SPLICE_F_MOVE);
+    if (res) {
+      in_queue_ += *res;
+    }
+
+    return res;
+  }
+
+  stdx::expected<size_t, std::error_code> write_some(size_t len) {
+    auto res = net::impl::socket::splice_from_pipe(
+        fds_.first, sock_out_.native_handle(), len, SPLICE_F_MOVE);
+    if (res) {
+      in_queue_ -= *res;
+    }
+    return res;
+  }
+
+  size_t queued() const noexcept { return in_queue_; }
+
+  bool empty() const noexcept { return in_queue_ == 0; }
+
+  void close() {
+    if (fds_.first != net::impl::file::kInvalidHandle)
+      net::impl::file::close(fds_.first);
+    fds_.first = net::impl::file::kInvalidHandle;
+    if (fds_.second != net::impl::file::kInvalidHandle)
+      net::impl::file::close(fds_.second);
+    fds_.second = net::impl::file::kInvalidHandle;
+  }
+
+  ~Splicer() { close(); }
+
+ private:
+  net::ip::tcp::socket &sock_in_;
+  net::ip::tcp::socket &sock_out_;
+  std::pair<net::impl::file::file_handle_type,
+            net::impl::file::file_handle_type>
+      fds_{net::impl::file::kInvalidHandle, net::impl::file::kInvalidHandle};
+
+  size_t in_queue_{0};
+};
+
+int main() {
+  net::impl::socket::init();
+
+#ifndef _WIN32
+  signal(SIGPIPE, SIG_IGN);
+#endif
+
+  net::io_context io_ctx;
+  net::ip::tcp::acceptor acceptor(io_ctx);
+
+  net::ip::tcp::resolver resolver(io_ctx);
+  auto resolve_res =
+      resolver.resolve("", "3307", net::ip::tcp::resolver::passive);
+  if (!resolve_res) {
+    std::cerr << __LINE__
+              << ": resolve() failed: " << resolve_res.error().message()
+              << std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cerr << __LINE__
+            << ": resolve()d as: " << resolve_res->begin()->endpoint()
+            << std::endl;
+
+  auto open_res = acceptor.open(resolve_res->begin()->endpoint().protocol());
+  if (!open_res) {
+    std::cerr << __LINE__ << ": open() failed: " << open_res.error().message()
+              << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  auto bind_res = acceptor.bind(resolve_res->begin()->endpoint());
+  if (!bind_res) {
+    std::cerr << __LINE__ << ": bind() failed: (" << bind_res.error() << ") "
+              << bind_res.error().message() << std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cerr << __LINE__ << ": bind() to " << acceptor.local_endpoint().value()
+            << std::endl;
+
+  acceptor.set_option(net::socket_base::reuse_address(true));
+#ifdef TCP_FASTOPEN
+  // acceptor.set_option(net::ip::tcp::fast_open(1));
+#endif
+  // acceptor.native_non_blocking(true);
+
+  auto listen_res = acceptor.listen(128);
+  if (!listen_res) {
+    std::cerr << __LINE__ << ": listen() failed: (" << listen_res.error()
+              << ") " << listen_res.error().message() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  while (true) {
+    auto accept_res = acceptor.accept();
+    if (!accept_res) {
+      std::cerr << __LINE__ << ": " << accept_res.error().message()
+                << std::endl;
+      break;
+    } else {
+      // unwrap
+      auto client_conn = std::move(*accept_res);
+      client_conn.native_non_blocking(true);
+
+      std::cerr << __LINE__ << ": accepted(). "
+                << "fd=" << client_conn.native_handle() << " connected "
+                << client_conn.local_endpoint().value() << " to "
+                << client_conn.remote_endpoint().value() << std::endl;
+
+      auto backend_resolve_res = resolver.resolve("localhost", "3308");
+      for (const auto &addr : backend_resolve_res.value()) {
+        net::ip::tcp::socket server_conn(io_ctx);
+        server_conn.open(addr.endpoint().protocol());
+        auto connect_res = server_conn.connect(addr.endpoint());
+        if (!connect_res) {
+          std::cerr << __LINE__ << ": (" << __func__ << "): connect("
+                    << addr.endpoint() << ") failed: (" << connect_res.error()
+                    << ") " << connect_res.error().message() << std::endl;
+
+          continue;
+        }
+        std::cerr << __LINE__ << " (" << __func__ << "): "
+                  << "fd=" << server_conn.native_handle() << " connected "
+                  << server_conn.local_endpoint().value() << " to "
+                  << server_conn.remote_endpoint().value() << std::endl;
+
+        // copy the data from client to server
+#if 0
+        std::array<char, 1024> buffers;
+
+        auto recv_res = net::read(client_conn, net::buffer(buffers));
+        if (!recv_res) {
+          std::cerr << __LINE__ << ": recv() failed: (" << recv_res.error()
+                    << ") " << recv_res.error().message() << std::endl;
+        } else {
+          std::cerr << __LINE__ << ": recv()ed: " << recv_res.value()
+                    << std::endl;
+        }
+
+        auto write_res =
+            net::write(server_conn, net::buffer(buffers, recv_res.value()));
+        if (!write_res) {
+          std::cerr << __LINE__ << ": write() failed: (" << write_res.error()
+                    << ") " << write_res.error().message() << std::endl;
+        } else {
+          std::cerr << __LINE__ << ": write()ed: " << write_res.value()
+                    << std::endl;
+        }
+#else
+        Splicer splicer(client_conn, server_conn);
+        splicer.open();
+
+        while (client_conn.is_open() ||
+               (!splicer.empty() && server_conn.is_open())) {
+          if (client_conn.is_open()) {
+            auto wait_res = client_conn.wait(net::socket_base::wait_read);
+            if (!wait_res) {
+              std::cerr << __LINE__ << ": read.wait() failed: ("
+                        << wait_res.error() << ") "
+                        << wait_res.error().message() << std::endl;
+              client_conn.close();
+            } else {
+              // TODO: what if the ioctl() fails?
+              auto bytes_available = client_conn.available().value();
+              if (bytes_available == 0) {
+                // client is done
+                client_conn.close();
+              } else {
+                auto splice_read_res = splicer.read_some(bytes_available);
+                if (!splice_read_res) {
+                  std::cerr << __LINE__ << ": read.splice() failed: ("
+                            << splice_read_res.error() << ") "
+                            << splice_read_res.error().message() << std::endl;
+
+                  client_conn.close();
+                } else {
+#if 0
+                  std::cerr << __LINE__
+                            << ": read.spliced()ed: " << splice_read_res.value()
+                            << std::endl;
+#endif
+                }
+              }
+            }
+          }
+
+          if (!splicer.empty()) {
+            auto splice_write_res = splicer.write_some(splicer.queued());
+            if (!splice_write_res) {
+              std::cerr << __LINE__ << ": write.splice() failed: ("
+                        << splice_write_res.error() << ") "
+                        << splice_write_res.error().message() << std::endl;
+              server_conn.close();
+            } else {
+#if 0
+              std::cerr << __LINE__
+                        << ": write.spliced()ed: " << splice_write_res.value()
+                        << std::endl;
+#endif
+            }
+          }
+        }
+#endif
+      }
+    }
+  }
+
+  return 0;
+}
