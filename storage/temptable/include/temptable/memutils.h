e55455e1e34396bc90928f7aac633b2e8999691b
Jusufadis Bakamovic <jusufadis.bakamovic@oracle.com>
2019-06-11 10:23:31 +0200

    Bug#29300927: MEMORY LEAK IN TEMPTABLE::HANDLER::CREATE*
    Post-fix for Doxygen error.
    
    Reviewed-by: Jakub Lopuszanski <jakub.lopuszanski@oracle.com>

--- storage/temptable/include/temptable/memutils.h
+++ storage/temptable/include/temptable/memutils.h
@@ -20,7 +20,7 @@ You should have received a copy of the GNU General Public License along with
 this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 
-/** @file storage/temptable/include/temptable/mem.h
+/** @file storage/temptable/include/temptable/memutils.h
 Memory utilities for temptable-allocator. */
 
 #ifndef TEMPTABLE_MEM_H
