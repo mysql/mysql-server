dc3aa19a7d1f31c474f79010820813fe87b2ac3e
Tor Didriksen <tor.didriksen@oracle.com>
2019-06-24 13:52:49 +0200

    Bug#29948728 MYSQL-CLIENT BROKEN ON MAC OS
    cmake -DWITHOUT_SERVER=1 was broken
    (implicit function declarations, and/or missing symbols)
    
    The fix is to add a missing #include.
    
    Change-Id: I1d71da7d088569ff8a581ecf454198db1280378b

--- mysys/mf_iocache2.c
+++ mysys/mf_iocache2.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2000, 2018, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2000, 2019, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -22,6 +22,8 @@
 #include <m_string.h>
 #include <stdarg.h>
 #include <m_ctype.h>
+
+#include "mysql/psi/mysql_file.h"
 
 /*
   Copy contents of an IO_CACHE to a file.
