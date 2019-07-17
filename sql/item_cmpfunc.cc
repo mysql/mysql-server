5e88e3f7c8dba50e8836d3dd7df01c3037833431
Alexander Peresypkin <o.peresypkin@fashionette.de>
2019-07-17 12:07:27 +0200

    Fix bug https://bugs.mysql.com/bug.php?id=95927

--- sql/item_cmpfunc.cc
+++ sql/item_cmpfunc.cc
@@ -5687,10 +5687,10 @@ float Item_func_xor::get_filtering_effect(THD *thd, table_map filter_for_table,
 
 longlong Item_func_xor::val_int() {
   DBUG_ASSERT(fixed == 1);
-  int result = 0;
+  uint result = 0;
   null_value = false;
   for (uint i = 0; i < arg_count; i++) {
-    result ^= (args[i]->val_int() != 0);
+    result ^= (args[i]->val_bool() != 0);
     if (args[i]->null_value) {
       null_value = true;
       return 0;
