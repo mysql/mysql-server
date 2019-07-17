25cca76e15c48f60bff0d4ae22781c4234c05f85
Alexander Peresypkin <o.peresypkin@fashionette.de>
2019-07-17 10:03:05 +0200

    Fix bug https://bugs.mysql.com/bug.php?id=95927

--- sql/item_cmpfunc.cc
+++ sql/item_cmpfunc.cc
@@ -5834,11 +5834,11 @@ bool Item_func_like::turboBM_matches(const char* text, int text_len) const
 longlong Item_func_xor::val_int()
 {
   DBUG_ASSERT(fixed == 1);
-  int result= 0;
+  uint result= 0;
   null_value= false;
   for (uint i= 0; i < arg_count; i++)
   {
-    result^= (args[i]->val_int() != 0);
+    result^= (args[i]->val_bool() != 0);
     if (args[i]->null_value)
     {
       null_value= true;
