035c3c7b630227cfd33e36f376f7d10e07071d66
Knut Anders Hatlen <knut.hatlen@oracle.com>
2019-07-18 09:19:36 +0200

    Bug#29508068: UNNECESSARY NEXT-KEY LOCK TAKEN
    Post-push fix for compiler warning:
    
    error: parameter 'eq_range' shadows member inherited from type
    'handler' [-Werror,-Wshadow-field]
    
    Reviewed-by: Jon Olav Hauglid <jon.hauglid@oracle.com>

--- storage/innobase/handler/ha_innodb.h
+++ storage/innobase/handler/ha_innodb.h
@@ -170,7 +170,7 @@ class ha_innobase : public handler {
   int index_last(uchar *buf) override;
 
   int read_range_first(const key_range *start_key, const key_range *end_key,
-                       bool eq_range, bool sorted) override;
+                       bool eq_range_arg, bool sorted) override;
 
   int read_range_next() override;
 
