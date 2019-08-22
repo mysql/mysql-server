74b4fcb9beb8fa871233f5cad469a150d6cdf420
Mauritz Sundell <mauritz.sundell@oracle.com>
2019-08-22 15:51:50 +0200

    Bug#30218404 IMPROVE STACKTRACE WITH PIE BUILDS FOR ASLR.
    Print slide in stack trace if relevant.
    
    Reviewed-by: Tor Didriksen <tor.didriksen@oracle.com>
    Reviewed-by: Frazer Clement <frazer.clement@oracle.com>

--- storage/ndb/src/common/portlib/ndb_stacktrace.cpp
+++ storage/ndb/src/common/portlib/ndb_stacktrace.cpp
@@ -25,6 +25,19 @@
 
 #include "my_stacktrace.h"
 
+#include <climits>
+
+#if defined(__APPLE__)
+#include <mach-o/dyld.h>
+#elif !defined(_WIN32)
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#include <link.h>
+#endif
+
+static unsigned long long ndb_get_program_base_address();
+
 void ndb_init_stacktrace()
 {
 #ifdef HAVE_STACKTRACE
@@ -39,8 +52,55 @@ void ndb_print_stacktrace()
       "For help with below stacktrace consult:\n"
       "https://dev.mysql.com/doc/refman/en/using-stack-trace.html\n"
       "Also note that stack_bottom and thread_stack will always show up as zero.\n");
+
+  unsigned long long base_address = ndb_get_program_base_address();
+  if (base_address != 0 && base_address != ULLONG_MAX)
+  {
+    my_safe_printf_stderr(
+        "Base address/slide: 0x%llx\n"
+        "With use of addr2line, llvm-symbolizer, or, atos, subtract the "
+        "addresses in\n"
+        "stacktrace with the base address before passing them to tool.\n"
+        "For tools that have options for slide use that, e.g.:\n"
+        "llvm-symbolizer --adjust-vma=0x%llx ...\n"
+        "atos -s 0x%llx ...\n",
+        base_address,
+        base_address,
+        base_address);
+  }
+
   my_print_stacktrace(nullptr, 0);
 #endif
 }
+
+#if defined(__APPLE__)
+unsigned long long ndb_get_program_base_address()
+{
+  return static_cast<unsigned long long>(_dyld_get_image_vmaddr_slide(0));
+}
+#elif defined(_WIN32)
+unsigned long long ndb_get_program_base_address()
+{
+  return ULLONG_MAX;
+}
+#else
+static int
+ndb_get_program_base_address_callback(struct dl_phdr_info *info,
+                                      size_t /* size */,
+                                      void *data)
+{
+  unsigned long long *base_address = (unsigned long long *)data;
+  *base_address = info->dlpi_addr;
+  return 1; // End iteration after first module which is program.
+}
+
+unsigned long long ndb_get_program_base_address()
+{
+  unsigned long long base_address = ULLONG_MAX;
+  void *callback_data = (void *)&base_address;
+  dl_iterate_phdr(&ndb_get_program_base_address_callback, callback_data);
+  return base_address;
+}
+#endif
 
 #endif
