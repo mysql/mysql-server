c87e1abeb8968eaaff0dffabc41f3c90fd4ba4be
Mikael Ronström <mikael.ronstrom@oracle.com>
2019-08-23 13:57:06 +0200

    BUG#30216204 REINTRODUCE MAXCONCURRENTOPERATIONS AS GLOBAL LIMIT IN DBTC
    Reviewed-by: Mauritz Sundell <mauritz.sundell@oracle.com>
    
    Fixed test case testNdbApi -n TestMaxOperations
    Changed error message slightly to also point to MaxNoOfConcurrentOperations as the
    config variable to change.

--- storage/ndb/test/ndbapi/testNdbApi.cpp
+++ storage/ndb/test/ndbapi/testNdbApi.cpp
@@ -288,6 +288,7 @@ int runTestMaxOperations(NDBT_Context* ctx, NDBT_Step* step){
         // Fall through - to '233' which also terminate test, but not 'FAILED'
       case 233:  // Out of operation records in transaction coordinator  
       case 1217:  // Out of operation records in local data manager (increase MaxNoOfLocalOperations)
+      case 261: //Increased beyond MaxDMLOperationsPerTransaction or MaxNoOfConcurrentOperations
         // OK - end test
         endTest = true;
         break;
