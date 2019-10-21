cf62c7a6fba84ad5f0409dbe4fac660c467ced2e
Tatiana Azundris Nuernberg <tatjana.nuernberg@oracle.com>
2019-10-21 06:18:24 +0200

    Bug#16636373: SERVER DOES NOT EXPOSE --LOG-RAW IN SHOW VARIABLES
    --log-raw is now a system variable. It can be queried at run-time
    to facilitate auditing. As a global system variable, its value can
    be changed by users who have the SUPER permission. (Also fixes its
    duplicate, Bug#19622629).
    
    Original patch creator was has left, so patch is pushed by
    a reviewer (Tatiana).
    
    Approved by: Harin Vadodaria <harin.vadodaria@oracle.com>

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -7822,12 +7822,6 @@ struct my_option my_long_options[] = {
     {"log-isam", OPT_ISAM_LOG, "Log all MyISAM changes to file.",
      &myisam_log_filename, &myisam_log_filename, 0, GET_STR, OPT_ARG, 0, 0, 0,
      0, 0, 0},
-    {"log-raw", 0,
-     "Log to general log before any rewriting of the query. For use in "
-     "debugging, not production as "
-     "sensitive information may be logged.",
-     &opt_general_log_raw, &opt_general_log_raw, 0, GET_BOOL, NO_ARG, 0, 0, 1,
-     0, 1, 0},
     {"log-short-format", 0,
      "Don't log extra information to update and slow-query logs.",
      &opt_short_log_format, &opt_short_log_format, 0, GET_BOOL, NO_ARG, 0, 0, 0,
