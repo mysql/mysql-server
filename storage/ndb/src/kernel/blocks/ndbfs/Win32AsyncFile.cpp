23ae594744662a8431dc07cd0d3082529ea972cc
Mauritz Sundell <mauritz.sundell@oracle.com>
2019-10-30 14:08:09 +0100

    Bug#30484272 USING DISK DATA ON WINDOWS FAILS NODE RESTART: TSMAN: FILE SYSTEM CLOSE FAILED.
    PROBLEM
    =======
    
    On Windows when restarting a non master data node with disk data it fails
    with below log messages:
    
        2019-10-30 12:43:11 [ndbd] INFO     -- TSMAN: File system close failed.
    OS errno: 6
        2019-10-30 12:43:11 [ndbd] ALERT    -- Node 1: Forced node shutdown
    completed. Occurred during startphase 0.
    
    This is caused by a bug in Win32AsyncFile::openReq() there the error
    checking for a call to GetFileSizeEx() was reversed.
    
        void Win32AsyncFile::openReq(Request* request)
        ...,
          if (flags & FsOpenReq::OM_READ_SIZE)
          {
            LARGE_INTEGER size;
            BOOL ret_code = GetFileSizeEx(hFile, &size);
            if (ret_code) <<<<--- ERROR should be: if (!ret_code)
            {
              request->error = GetLastError();
              (void)CloseHandle(hFile);
    
    SOLUTION
    ========
    
    Treat false from GetFileSizeEx() as error.
    
    Reviewed-by: Frazer Clement <frazer.clement@oracle.com>

--- storage/ndb/src/kernel/blocks/ndbfs/Win32AsyncFile.cpp
+++ storage/ndb/src/kernel/blocks/ndbfs/Win32AsyncFile.cpp
@@ -1,5 +1,5 @@
 /* 
-   Copyright (c) 2007, 2017, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2007, 2019, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -269,7 +269,7 @@ void Win32AsyncFile::openReq(Request* request)
   {
     LARGE_INTEGER size;
     BOOL ret_code = GetFileSizeEx(hFile, &size);
-    if (ret_code)
+    if (!ret_code)
     {
       request->error = GetLastError();
       (void)CloseHandle(hFile);
