a5f60bf8f4275dd1e7f659e6dfe5513497018690
Steinar H. Gunderson <steinar.gunderson@oracle.com>
2019-12-18 16:30:44 +0100

    Bug #30473261: CONVERT THE INDEX SUBQUERY ENGINES INTO USING THE ITERATOR EXECUTOR [patch 2/10, indent fix]
    Preparatory indent fix in sql_opt_exec_shared.h.
    
    Change-Id: I06d45a99429c4bf213a0922c74bc937156394bbf

--- sql/sql_opt_exec_shared.h
+++ sql/sql_opt_exec_shared.h
@@ -166,55 +166,53 @@ class Semijoin_mat_exec;
 /*
   The structs which holds the join connections and join states
 */
-enum join_type { /*
-                         Initial state. Access type has not yet been decided
-                         for the table
-                       */
-                 JT_UNKNOWN,
-                 /* Table has exactly one row */
-                 JT_SYSTEM,
-                 /*
-                   Table has at most one matching row. Values read
-                   from this row can be treated as constants. Example:
-                   "WHERE table.pk = 3"
-                  */
-                 JT_CONST,
-                 /*
-                   '=' operator is used on unique index. At most one
-                   row is read for each combination of rows from
-                   preceding tables
-                 */
-                 JT_EQ_REF,
-                 /*
-                   '=' operator is used on non-unique index
-                 */
-                 JT_REF,
-                 /*
-                   Full table scan.
-                 */
-                 JT_ALL,
-                 /*
-                   Range scan.
-                 */
-                 JT_RANGE,
-                 /*
-                   Like table scan, but scans index leaves instead of
-                   the table
-                 */
-                 JT_INDEX_SCAN,
-                 /* Fulltext index is used */
-                 JT_FT,
-                 /*
-                   Like ref, but with extra search for NULL values.
-                   E.g. used for "WHERE col = ... OR col IS NULL"
-                  */
-                 JT_REF_OR_NULL,
-                 /*
-                   Do multiple range scans over one table and combine
-                   the results into one. The merge can be used to
-                   produce unions and intersections
-                 */
-                 JT_INDEX_MERGE
+enum join_type {
+  /* Initial state. Access type has not yet been decided for the table */
+  JT_UNKNOWN,
+  /* Table has exactly one row */
+  JT_SYSTEM,
+  /*
+    Table has at most one matching row. Values read
+    from this row can be treated as constants. Example:
+    "WHERE table.pk = 3"
+   */
+  JT_CONST,
+  /*
+    '=' operator is used on unique index. At most one
+    row is read for each combination of rows from
+    preceding tables
+  */
+  JT_EQ_REF,
+  /*
+    '=' operator is used on non-unique index
+  */
+  JT_REF,
+  /*
+    Full table scan.
+  */
+  JT_ALL,
+  /*
+    Range scan.
+  */
+  JT_RANGE,
+  /*
+    Like table scan, but scans index leaves instead of
+    the table
+  */
+  JT_INDEX_SCAN,
+  /* Fulltext index is used */
+  JT_FT,
+  /*
+    Like ref, but with extra search for NULL values.
+    E.g. used for "WHERE col = ... OR col IS NULL"
+   */
+  JT_REF_OR_NULL,
+  /*
+    Do multiple range scans over one table and combine
+    the results into one. The merge can be used to
+    produce unions and intersections
+  */
+  JT_INDEX_MERGE
 };
 
 /// Holds members common to JOIN_TAB and QEP_TAB.
