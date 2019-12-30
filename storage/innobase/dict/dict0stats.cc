239d2884860a9ab78edc79f49e1ca4bd2b8e72aa
Kamil Holubicki <kamil.holubicki@percona.com>
2019-12-30 12:29:32 +0100

    PS-6148: If ANALYZE TABLE with transient statistics runs more than 600 seconds diagnostic query may crash server
    https://jira.percona.com/browse/PS-6148
    
    Locking of table statistics limited to the time when statistics related table members are changed. While calculation is done, statistics are not locked, so can be queried.
    
    Index analysis protected by analyze_index_mutex (the same which protects index when analysis is done for persistent statistics)

--- storage/innobase/dict/dict0stats.cc
+++ storage/innobase/dict/dict0stats.cc
@@ -819,6 +819,10 @@ dict_stats_update_transient_for_index(
 /*==================================*/
 	dict_index_t*	index)	/*!< in/out: index */
 {
+	/* stats_latch is created on 1st lock. */
+	ut_ad(!(index->table->stats_latch_created) ||
+		!rw_lock_own(index->table->stats_latch, RW_X_LATCH));
+
 	if (srv_force_recovery >= SRV_FORCE_NO_TRX_UNDO
 	    && (srv_force_recovery >= SRV_FORCE_NO_LOG_REDO
 		|| !dict_index_is_clust(index))) {
@@ -885,6 +889,10 @@ dict_stats_update_transient(
 	dict_index_t*	index;
 	ulint		sum_of_index_sizes	= 0;
 
+	dict_table_analyze_index_lock(table);
+
+	DEBUG_SYNC_C("innodb_dict_stats_update_transient");
+
 	/* Find out the sizes of the indexes and how many different values
 	for the key they approximately have */
 
@@ -893,6 +901,7 @@ dict_stats_update_transient(
 	if (dict_table_is_discarded(table)) {
 		/* Nothing to do. */
 		dict_stats_empty_table(table);
+		dict_table_analyze_index_unlock(table);
 		return;
 	} else if (index == NULL) {
 		/* Table definition is corrupt */
@@ -900,6 +909,7 @@ dict_stats_update_transient(
 		ib::warn() << "Table " << table->name
 			<< " has no indexes. Cannot calculate statistics.";
 		dict_stats_empty_table(table);
+		dict_table_analyze_index_unlock(table);
 		return;
 	}
 
@@ -924,6 +934,8 @@ dict_stats_update_transient(
 
 	index = dict_table_get_first_index(table);
 
+	dict_table_stats_lock(table, RW_X_LATCH);
+
 	table->stat_n_rows = index->stat_n_diff_key_vals[
 		dict_index_get_n_unique(index) - 1];
 
@@ -937,6 +949,9 @@ dict_stats_update_transient(
 	table->stat_modified_counter = 0;
 
 	table->stat_initialized = TRUE;
+
+	dict_table_stats_unlock(index->table, RW_X_LATCH);
+	dict_table_analyze_index_unlock(table);
 }
 
 /* @{ Pseudo code about the relation between the following functions
@@ -3066,9 +3081,9 @@ dict_stats_update_for_index(
 			" corrupted. Using transient stats instead.";
 	}
 
-	dict_table_stats_lock(index->table, RW_X_LATCH);
+	dict_table_analyze_index_lock(index->table);
 	dict_stats_update_transient_for_index(index);
-	dict_table_stats_unlock(index->table, RW_X_LATCH);
+	dict_table_analyze_index_unlock(index->table);
 
 	DBUG_VOID_RETURN;
 }
@@ -3278,11 +3293,7 @@ dict_stats_update(
 
 transient:
 
-	dict_table_stats_lock(table, RW_X_LATCH);
-
 	dict_stats_update_transient(table);
-
-	dict_table_stats_unlock(table, RW_X_LATCH);
 
 	return(DB_SUCCESS);
 }
