5be5f2bce9a13f2990c1301afc270f068e5e465b
Pedro Figueiredo <pedro.figueiredo@oracle.com>
2020-01-21 15:53:42 +0000

    BUG#30782737 STATIC INITIALIZATION STARTUP CRASH REFERENCING BINLOG COMPRESSION MONITORING
    Description
    -----------
    With some specific compiler versions and options, the server crashes at startup
    with a segmentation fault during the initialization of the
    `Compression_stats::ZERO_TRX_ROW` static variable.
    
    Analysis
    --------
    It looks like the "static initialization fiasco", known problem with the
    non-deterministic ordering of static variable initialization.
    
    Also, the compiler version being used --10.0.0 (experimental)-- may contribute
    to the issue since the all fiasco is tie to the compilation unit initialization
    order, which in turn may be tie to compilation time re-ordering.
    
    Fix
    ---
    Use the "Construct On First Use" idiom and wrap the static in a method to avoid
    the compilation unit ordering dependency.
    
    RB: 23692
    Reviewed-by: Luís Soares <luis.soares@oracle.com>
    Reviewed-by: Pedro Gomes <pedro.gomes@oracle.com>

--- sql/binlog/monitoring/context.h
+++ sql/binlog/monitoring/context.h
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2019, 2020, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -55,7 +55,7 @@ class Compression_stats {
   /**
     A constant and static instance of the transaction compression stats.
    */
-  const static Compression_stats_trx_row ZERO_TRX_ROW;
+  static const Compression_stats_trx_row &ZERO_TRX_ROW();
 
  protected:
   /**
