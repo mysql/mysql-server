9d7eebb9baa7bb2f8ce3de0a6e13ed57fdb103c3
John David Duncan <john.duncan@oracle.com>
2020-01-22 12:47:26 -0800

    Bug#30795085 WARNINGS FOR -WC++98-COMPAT-EXTRA-SEMI IN NDB CODE
    Remove extraneous semicolons
    
    Change-Id: I8cacbdf2eb23a8aa620b23567c79d7ae842f92e1

--- storage/ndb/test/ndbapi/testBasic.cpp
+++ storage/ndb/test/ndbapi/testBasic.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2019, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2020 Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -4553,7 +4553,7 @@ TESTCASE("CheckCompletedLCPStats",
 {
   STEP(runCheckLCPStats);
 }
-NDBT_TESTSUITE_END(testBasic);
+NDBT_TESTSUITE_END(testBasic)
 
 #if 0
 TESTCASE("ReadConsistency",
