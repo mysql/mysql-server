9ad93eb651fbd11af7549f5661e8cc81c69a5f27
Steinar H. Gunderson <steinar.gunderson@oracle.com>
2020-01-23 11:17:21 +0100

    Bug #30791074: WEIGHT_STRING: ASSERTION `0' FAILED. IN TIME_ZONE_UTC::TIME_TO_GMT_SEC
    In bug #30776132 (MAKE FILESORT KEYS CONSISTENT BETWEEN FIELDS AND ITEMS),
    we made filesort unconditionally switch to UTC when evaluating timestamps.
    However, this was made by setting the time zone to a global of type
    Time_zone_utc. This class is not the same as the regular UTC time zone
    (typically of type Time_zone_system or similar), and was missing one important
    conversion, namely that of MYSQL_TIME to my_time_t, causing wrong results
    and also assertion failures in debug mode. The conversion is trivial,
    so we simply add it.
    
    Change-Id: I807f189973a3a22fdf6040b95c0862b2c68e97f9

--- sql/tztime.cc
+++ sql/tztime.cc
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2004, 2019, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2004, 2020, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -1134,20 +1134,12 @@ class Time_zone_utc : public Time_zone {
                         value passed doesn't really exist (i.e. falls into
                         spring time-gap) and is not touched otherwise.
 
-  DESCRIPTION
-    Since Time_zone_utc is used only internally for my_time_t -> TIME
-    conversions, this function of Time_zone interface is not implemented for
-    this class and should not be called.
-
   RETURN VALUE
-    0
+    Corresponding my_time_t value, or 0 in case of error.
 */
 my_time_t Time_zone_utc::TIME_to_gmt_sec(
-    const MYSQL_TIME *t MY_ATTRIBUTE((unused)),
-    bool *in_dst_time_gap MY_ATTRIBUTE((unused))) const {
-  /* Should be never called */
-  DBUG_ASSERT(0);
-  return 0;
+    const MYSQL_TIME *mt, bool *in_dst_time_gap MY_ATTRIBUTE((unused))) const {
+  return sec_since_epoch(*mt);
 }
 
 /*
