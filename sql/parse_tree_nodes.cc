f7ef1eb14114d03d7e2cdff5d2095cd2a940f12b
Guilhem Bichot <guilhem.bichot@oracle.com>
2020-01-23 19:00:55 +0100

    Bug#30796015 INCONSISTENT BEHAVIOUR FOR DELETE ON CTE
    When trying to open the table to delete from, single-table DELETE
    ignores CTEs, which thus violates SQL's name visibility rules.
    That's because it calls add_table_to_list() with parse_context==nullptr,
    which has the effect that adding the table-to-delete doesn't
    search in the WITH clause to see if it is a CTE, as the WITH clause
    is accessible from parse_context.
    Fix: set the parse_context argument. I have gone through all calls
    to add_table_to_list(pc=nullptr) in all types of SQL statements,
    and I think only this call needs fixing.
    
    Reviewed-by: Gleb Shchepa <gleb.shchepa@oracle.com>
    Change-Id: Iea3167adf861618bd6b351fa164057cf102c78e9

--- sql/parse_tree_nodes.cc
+++ sql/parse_tree_nodes.cc
@@ -689,9 +689,9 @@ bool PT_delete::add_table(Parse_context *pc, Table_ident *table) {
   const enum_mdl_type mdl_type = (opt_delete_options & DELETE_LOW_PRIORITY)
                                      ? MDL_SHARED_WRITE_LOW_PRIO
                                      : MDL_SHARED_WRITE;
-  return !pc->select->add_table_to_list(pc->thd, table, opt_table_alias,
-                                        table_opts, lock_type, mdl_type,
-                                        nullptr, opt_use_partition);
+  return !pc->select->add_table_to_list(
+      pc->thd, table, opt_table_alias, table_opts, lock_type, mdl_type, nullptr,
+      opt_use_partition, nullptr, pc);
 }
 
 Sql_cmd *PT_delete::make_cmd(THD *thd) {
