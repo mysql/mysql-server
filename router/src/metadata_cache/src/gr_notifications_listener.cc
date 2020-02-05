6d6e4d915d506909fb28af8fd0225fc71d1766f1
Andrzej Religa <andrzej.religa@oracle.com>
2020-02-05 06:37:48 -0500

    Bug #30850734 ROUTER LOGS WARNING 'UNKNOWN NAMESPACE XPLUGIN' WHEN GR NOTIFICATIONS ARE ON
    If you run the MySQLRouter with the use_gr_notifications=1
    every few hours it will log the following error:
    020-02-03 14:38:46 metadata_cache WARNING [7f10e7edd700] Failed sending ping
    on connection to 127.0.0.1:50010; (err_code=5162; err_msg='Unknown namespace
    xplugin')
    
    This is because the Router uses obsolete namespace name "xprotocol" when sending
    the PING message over the xprotocol connection to avoid the idle connection
    being closed. This namespace was removed in 8.0.19
    
    This patch fixes the code to use the correct namespace.
    
    RB: 23781

--- router/src/metadata_cache/src/gr_notifications_listener.cc
+++ router/src/metadata_cache/src/gr_notifications_listener.cc
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+  Copyright (c) 2019, 2020, Oracle and/or its affiliates. All rights reserved.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License, version 2.0,
@@ -457,7 +457,7 @@ void GRNotificationListener::Impl::set_mysqlx_wait_timeout(
 xcl::XError GRNotificationListener::Impl::ping(
     xcl::XSession &session) noexcept {
   xcl::XError out_error;
-  session.execute_stmt("xplugin", "ping", {}, &out_error);
+  session.execute_stmt("mysqlx", "ping", {}, &out_error);
 
   return out_error;
 }
