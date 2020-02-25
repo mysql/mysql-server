c1bc61dc7a90fec04e7bb372834d4a9cda265e9d
Yasufumi Kinoshita <yasufumi.kinoshita@oracle.com>
2020-02-25 12:51:09 +0900

    Bug#30941418 : BUF_FLUSH_END() DOESN'T FLUSH DATAFILE WHEN INNODB_DOUBLEWRITE=OFF
    After WL#5655 applied, buf_flush_end() doesn't flush datafile when innodb_doublewrite=OFF.
    fsync() for datafile is not called until checkpoint, when innodb_doublewrite=OFF.
    It should call buf_flush_sync_datafiles() at buf_flush_end() for the case.
    
    Approved by: Sunny Bains <sunny.bains@oracle.com>

--- storage/innobase/buf/buf0flu.cc
+++ storage/innobase/buf/buf0flu.cc
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
-Copyright (c) 1995, 2019, Oracle and/or its affiliates. All Rights Reserved.
+Copyright (c) 1995, 2020, Oracle and/or its affiliates. All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License, version 2.0, as published by the
@@ -1990,8 +1990,12 @@ static void buf_flush_end(buf_pool_t *buf_pool, buf_flush_t flush_type) {
 
   mutex_exit(&buf_pool->flush_state_mutex);
 
-  if (!srv_read_only_mode && dblwr::enabled) {
-    dblwr::force_flush(flush_type, buf_pool_index(buf_pool));
+  if (!srv_read_only_mode) {
+    if (dblwr::enabled) {
+      dblwr::force_flush(flush_type, buf_pool_index(buf_pool));
+    } else {
+      buf_flush_sync_datafiles();
+    }
   } else {
     os_aio_simulated_wake_handler_threads();
   }
