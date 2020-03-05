c4a42d0a2a858a8ef001946245f836faa41a6627
Lukasz Kotula <lukasz.kotula@oracle.com>
2020-03-05 13:18:17 +0100

    BUG#29127302 - X PLUGINS DELAYED INITIALIZATION IS NOT NEED ANYMORE
    [post-push fix]
    
    Description
    ===========
    
    X Plugin creates the X UNIX-sockets when server is creating database
    (running with --initialize or --initialize-insecure). X Plugin shouldn't
    create accepting endpoints in this case.
    
    RB: 24028
    Reviewed by: Grzegorz Szwarc <grzegorz.szwarc@oracle.com>

--- plugin/x/src/mysql_variables.cc
+++ plugin/x/src/mysql_variables.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2.0,
@@ -32,6 +32,7 @@ namespace mysqld {
 
 bool is_terminating() { return connection_events_loop_aborted(); }
 
+bool get_initialize() { return opt_initialize; }
 const char *get_my_localhost() { return my_localhost; }
 
 const CHARSET_INFO *get_default_charset() {
