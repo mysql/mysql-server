038fd407458d4f5deb6b8ba8ba097ccb1aa32b33
Kari Juul Wedde <kari.wedde@oracle.com>
2020-03-26 13:52:04 +0100

    Bug#31068648 ADAPT MCC TO THE NEW DOJO PROMISE API.
    MCC needs to be adapted to the new dojo promise api before we upgrade to dojo 1.16.2.
    The new api also works for 1.8.14 - the current dojo version.
    
    Remove the use of addCallback in the expandAll fuction in
    
    hostTree.js
    processTree.js
    deploymentTree.js
    
    Approved by: John Duncan <john.duncan@oracle.com>

--- storage/ndb/mcc/frontend/js/mcc/gui/deploymenttree.js
+++ storage/ndb/mcc/frontend/js/mcc/gui/deploymenttree.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2012, 2019, Oracle and/or its affiliates. All rights reserved.
+Copyright (c) 2012, 2020, Oracle and/or its affiliates. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2.0,
@@ -461,7 +461,7 @@ function deploymentTreeViewSetup (clusterName) {
             function expand (node) {
                 var def = new dojo.Deferred();
                 // Expand the node
-                _this._expandNode(node).addCallback(function () {
+                _this._expandNode(node).then(function () {
                     // When expanded, expand() non-leaf children recursively
                     var childBranches = dojo.filter(
                         node.getChildren() || [], function (node) {
@@ -469,8 +469,8 @@ function deploymentTreeViewSetup (clusterName) {
                         });
                     var defs = dojo.map(childBranches, expand);
                     // When recursive calls finish, signal that I'm finished
-                    new dojo.DeferredList(defs).addCallback(function () {
-                        def.callback();
+                    new dojo.DeferredList(defs).then(function () {
+                        def.resolve();
                     });
                 });
                 return def;
