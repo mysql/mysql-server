39b874bb10cd9c77fdc9cded83a6ae0b9d4570f6
Erlend Dahl <erlend.dahl@oracle.com>
2020-04-08 15:08:51 +0200

    Bug#28799660 EXTENDED OR BADLY FORMATTED COPYRIGHT HOLDERS [noclose]
    Yet more fixes to copyright headers.
    
    Approvced by Gipson Pulla <gipson.pulla@oracle.com>
    
    Change-Id: Ib4b5ad7c5b02b552fb2129dcc9fa07a4f11979af

--- storage/innobase/include/log0meb.h
+++ storage/innobase/include/log0meb.h
@@ -18,9 +18,9 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License, version 2.0, for more details.
 
-You should have received a copy of the GNU General Public License along with
-this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Suite 500, Boston, MA 02110-1335 USA
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
 
 *****************************************************************************/
 #ifndef log0meb_h
