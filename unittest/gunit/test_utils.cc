ca41c1fb5820ba6ba9ab462e39cc75c907c29729
Tor Didriksen <tor.didriksen@oracle.com>
2020-04-16 09:55:19 +0200

    WL#13821 REFACTORING: DEADLOCK WITH PLUGIN INSTALL
    Post-push fix for broken unit test get_diagnostics-t
    
    THD::raise_condition() now does
        mysql_mutex_assert_not_owner(&LOCK_plugin);
    So LOCK_plugin must be initialized for unit tests.
    
    Change-Id: I4643bf2d1314d58ee559c5dbfaadf8bedeaba458

--- unittest/gunit/test_utils.cc
+++ unittest/gunit/test_utils.cc
@@ -57,6 +57,7 @@
 #include "sql/sql_class.h"
 #include "sql/sql_lex.h"
 #include "sql/sql_locale.h"
+#include "sql/sql_plugin.h"
 #include "sql/xa.h"
 #include "unicode/uclean.h"
 
@@ -87,6 +88,7 @@ void setup_server_for_unit_tests() {
   set_remaining_args(6, argv);
   system_charset_info = &my_charset_utf8_general_ci;
 
+  mysql_mutex_init(PSI_NOT_INSTRUMENTED, &LOCK_plugin, MY_MUTEX_INIT_FAST);
   mysql_mutex_init(PSI_NOT_INSTRUMENTED, &LOCK_system_variables_hash,
                    MY_MUTEX_INIT_FAST);
   sys_var_init();
