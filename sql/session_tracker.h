abd3b2e6e41f21fb8e20ef3c53cc076688490d09
Erlend Dahl <erlend.dahl@oracle.com>
2020-04-22 11:09:51 +0200

    Bug#31200999 INCORRECT COPYRIGHT YEAR IN SEVERAL FILES
    Adjust the initial copyright year in the files where it was wrong.
    
    RB: 24291
    Change-Id: Ib45c7e6b2ca1b84afdd3f912502cfd092a586c59

--- sql/session_tracker.h
+++ sql/session_tracker.h
@@ -1,7 +1,7 @@
 #ifndef SESSION_TRACKER_INCLUDED
 #define SESSION_TRACKER_INCLUDED
 
-/* Copyright (c) 2015, 2019, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2014, 2019, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
