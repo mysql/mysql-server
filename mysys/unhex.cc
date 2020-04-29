e0a77ad844ecaa0b5998dacac99e31ccff1265ab
Tor Didriksen <tor.didriksen@oracle.com>
2020-04-29 11:54:11 +0200

    BUG#31173103 UNHEX() string function performance improvement
    Post-push fix for broken build on windows:
    \mysys\unhex.cc(87,14): error C2039:  'ptrdiff_t': is not a member of 'std'
    
    Change-Id: Ib6863556e83678b0bbc437097160fd54d2e6235e

--- mysys/unhex.cc
+++ mysys/unhex.cc
@@ -25,6 +25,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 
+#include <cstddef>
 #include <cstdint>
 
 namespace {
