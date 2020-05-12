0714c574b400eb1ab04114ea28bd8602069b4fd9
Tor Didriksen <tor.didriksen@oracle.com>
2020-05-12 13:58:42 +0200

    Bug#31336340 DON'T INSTALL DEBUG VERSION OF CLIENT PLUGINS (AS THERE IS NO DEBUG CLIENT)
    Bug#31123564 - DEB & RPM: MOVE CLIENT PLUGINS TO CLIENT SUB PACKAGE
    
    Don't install debug version of *client* plugins.
    
    Move authentication_ldap_sasl_client.so plugin from server to client sub package.
    
    Change-Id: I52d053c7ea74d01d8dd8fa404a38abb3872ba18d

--- cmake/plugin.cmake
+++ cmake/plugin.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2009, 2020, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2009, 2020, Oracle and/or its affiliates.
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2.0,
@@ -223,9 +223,11 @@ MACRO(MYSQL_ADD_PLUGIN plugin_arg)
       MYSQL_INSTALL_TARGET(${target}
         DESTINATION ${INSTALL_PLUGINDIR}
         COMPONENT ${INSTALL_COMPONENT})
-      INSTALL_DEBUG_TARGET(${target}
-        DESTINATION ${INSTALL_PLUGINDIR}/debug
-        COMPONENT ${INSTALL_COMPONENT})
+      IF(NOT ARG_CLIENT_ONLY)
+        INSTALL_DEBUG_TARGET(${target}
+          DESTINATION ${INSTALL_PLUGINDIR}/debug
+          COMPONENT ${INSTALL_COMPONENT})
+      ENDIF()
     ENDIF()
   ELSE()
     IF(WITHOUT_${plugin})
