69e2de8e0e8b41bd825472549c1823e05c250a7b
Anushree Prakash B <anushree.prakash.b@oracle.com>
2020-05-14 18:49:18 +0530

    Bug#30819012 - MYSQLPUMP VALIDATES ALL DATABASE OBJECTS BY                IGNORING --EXCLUDE_DATABASES AND                --SINGLE-TRANSACTION OPTION
    DESCRIPTION
    ===========
    While trying to dump a single schema using mysqlpump, it
    validates all database objects irrespective of the specific
    database instructed to export using --exclude-databases
    and --single-transaction.
    
    ANALYSIS
    ========
    According to the current mysqlpump design, it first
    enumerates all the DB objects as part of the
    Crawler module and later performs the filtering of the
    objects before dumping them. This causes all the database
    objects to be validated even if a single database was
    instructed to be dumped.
    
    FIX
    ===
    The fix performs object filtering before enumeration
    so that only the databases and their corresponding
    objects that should be included in the dump are validated.
    
    RB# 24340

--- client/dump/program.cc
+++ client/dump/program.cc
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
+  Copyright (c) 2015, 2020, Oracle and/or its affiliates.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License, version 2.0,
@@ -192,7 +192,7 @@ int Program::execute(std::vector<std::string> positional_options)
   }
   I_crawler* crawler= new Mysql_crawler(
     connection_provider, message_handler, id_generator,
-    m_mysql_chain_element_options, this);
+    m_mysql_chain_element_options, m_mysqldump_tool_chain_maker_options, this);
   m_mysqldump_tool_chain_maker_options->process_positional_options(
     positional_options);
   check_mutually_exclusive_options();
