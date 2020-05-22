054d237704a56e1dc51b5ab3232caf60cad460ab
Tor Didriksen <tor.didriksen@oracle.com>
2020-05-22 12:39:58 +0200

    Bug #31364750 UPGRADE TO GOOGLETEST 1.10.X
    Post-push fix: TYPED_TEST_CASE -> TYPED_TEST_SUITE for new unit tests.
    
    Change-Id: I4d22ca25aa2049744e56e7dffd8d8de13e5065a6

--- router/src/harness/tests/test_net_ts_local.cc
+++ router/src/harness/tests/test_net_ts_local.cc
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+  Copyright (c) 2020, Oracle and/or its affiliates.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License, version 2.0,
@@ -42,7 +42,7 @@ using LocalProtocolTypes =
     ::testing::Types<local::stream_protocol, local::datagram_protocol,
                      local::seqpacket_protocol>;
 
-TYPED_TEST_CASE(LocalProtocolTest, LocalProtocolTypes);
+TYPED_TEST_SUITE(LocalProtocolTest, LocalProtocolTypes);
 
 template <class T>
 class LocalTwoWayProtocolTest : public ::testing::Test {
@@ -52,7 +52,7 @@ class LocalTwoWayProtocolTest : public ::testing::Test {
 using LocalTwoWayProtocolTypes =
     ::testing::Types<local::stream_protocol, local::seqpacket_protocol>;
 
-TYPED_TEST_CASE(LocalTwoWayProtocolTest, LocalTwoWayProtocolTypes);
+TYPED_TEST_SUITE(LocalTwoWayProtocolTest, LocalTwoWayProtocolTypes);
 
 TYPED_TEST(LocalProtocolTest, endpoint_construct_default) {
   using protocol_type = TypeParam;
