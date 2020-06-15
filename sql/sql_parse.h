74dfe88447451f198c8d9a4156c9c3ccfa5f22f9
Dmitry Lenev <dmitry.lenev@oracle.com>
2020-06-15 23:37:16 +0300

    Bug#31304432 "INSUFFICIENT PRIVILEGE CHECK BY LOCK TABLES".
    The problem was that LOCK TABLES statement on a view did insufficient
    privilege checking for view's underlying tables.
    
    This patch solves the problem by ensuring that when we lock tables
    through a view we require the same SELECT and LOCK TABLES privileges
    on its underlying tables as would have been required if these tables
    were locked directly.
    
    To avoid breaking mysqldump/pump tools which need to be able to dump
    views with broken definer, which in default mode involves locking of
    such views, we accept both cases when:
    1) User whose context will be used for view execution has necessary
       privileges on underlying tables.
    2) User who originally inititated LOCK TABLES operation has them
       (which is typically for admin/backup users used for dumping).
    
    This is version of the patch for 5.6 branch.
    
    Reviewed-by: Sivert Sorumgaard <sivert.sorumgaard@oracle.com>
    Reviewed-by: Gopal Shankar <gopal.shankar@oracle.com>

--- sql/sql_parse.h
+++ sql/sql_parse.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006, 2017, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2006, 2020, Oracle and/or its affiliates.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -60,6 +60,8 @@ bool check_fk_parent_table_access(THD *thd,
                                   const char *child_table_db,
                                   HA_CREATE_INFO *create_info,
                                   Alter_info *alter_info);
+bool check_lock_view_underlying_table_access(THD *thd, TABLE_LIST *tbl,
+                                             bool *fake_lock_tables_acl);
 
 bool parse_sql(THD *thd,
                Parser_state *parser_state,
