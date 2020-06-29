bd72b258c69698f14d93bdc37d65e430e8318a9f
Nuno Carvalho <nuno.carvalho@oracle.com>
2020-06-29 18:36:30 +0200

    BUG#31554231: BACKPORT BUG#31535262 TO 5.7
    Group_member_info_manager::get_group_member_info_by_member_id() returns a
    internal pointer to a Group_member_info entry, that entry can be removed
    without the pointer holder know.
    
    To solve the above issue, now the method does return a copy of the
    internal instance.
    
    RB: 24701

--- rapid/plugin/group_replication/include/member_info.h
+++ rapid/plugin/group_replication/include/member_info.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, 2018, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2014, 2020, Oracle and/or its affiliates.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -501,6 +501,20 @@ public:
                        Group_member_info::Group_member_status new_status)= 0;
 
   /**
+    Sets the identified member as unreachable.
+
+    @param[in] uuid        member uuid
+   */
+  virtual void set_member_unreachable(const std::string &uuid) = 0;
+
+  /**
+    Sets the identified member as reachable.
+
+    @param[in] uuid        member uuid
+   */
+  virtual void set_member_reachable(const std::string &uuid) = 0;
+
+  /**
     Updates the GTID sets on a single member
 
 
@@ -597,6 +611,10 @@ public:
   void
   update_member_status(const std::string& uuid,
                        Group_member_info::Group_member_status new_status);
+
+  void set_member_unreachable(const std::string &uuid);
+
+  void set_member_reachable(const std::string &uuid);
 
   void update_gtid_sets(const std::string& uuid,
                         std::string& gtid_executed,
