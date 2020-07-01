de48872d46ccd4fa31689500d8da21ff33cb9fa6
Frazer Clement <frazer.clement@oracle.com>
2020-07-01 18:42:10 +0100

    Bug#27772916 NDBAPI : BLOB IMPLICIT EXECUTION SAFETY
    NdbApi robustness is improved by making certain combinations
    of calls illegal.
    
    Specifically, calls which can implicitly execute operations,
    without the usual Blob related handling in execute() are
    not allowed when there are user-level Blob operations
    requiring this handling defined on the transaction.
    
    A new error code is defined to indicate this problem :
      4558 Pending Blob operations must be executed before this call
    
    New testcase testBlobs -bug27772916 is added to daily-devel--07
    
    Approved by : Priyanka Sangam <priyanka.sangam@oracle.com>

--- storage/ndb/src/ndbapi/ndberror.c
+++ storage/ndb/src/ndbapi/ndberror.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2004, 2019, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2004, 2020, Oracle and/or its affiliates.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -800,6 +800,8 @@ ErrorBundle ErrorCodes[] = {
   { 4554, DMEC, AE, "NdbBlob can only be closed from Active state" },
   { 4555, DMEC, AE, "NdbBlob cannot be closed with pending operations" },
   { 4556, DMEC, AE, "RecordSpecification has illegal value in column_flags" },
+  /* 4557 */
+  { 4558, DMEC, AE, "Pending Blob operations must be executed before this call" },
 
   { 4200, DMEC, AE, "Status Error when defining an operation" },
   { 4201, DMEC, AE, "Variable Arrays not yet supported" },
