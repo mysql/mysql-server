718efa54a943977964add44d29d511077486f2f3
John David Duncan <john.duncan@oracle.com>
2020-07-08 15:25:19 -0700

    Bug#31357905 warnings from -wshadow-field in storage/ndb/test
    Fix several places where local parameter names shadow
    names of fields, and lead to a compiler warning.
    
    Reviewed-by: Arnab Ray <arnab.r.ray@oracle.com>

--- storage/ndb/test/include/HugoOperations.hpp
+++ storage/ndb/test/include/HugoOperations.hpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2019, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2020, Oracle and/or its affiliates.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -148,7 +148,7 @@ public:
 
   NdbIndexScanOperation* pIndexScanOp;
 
-  NDBT_ResultRow& get_row(Uint32 idx) { return *rows[idx];}
+  NDBT_ResultRow& get_row(Uint32 index) { return *rows[index];}
 
   int execute_async(Ndb*, NdbTransaction::ExecType, NdbOperation::AbortOption = NdbOperation::AbortOnError);
   int execute_async_prepare(Ndb*, NdbTransaction::ExecType, NdbOperation::AbortOption = NdbOperation::AbortOnError);
