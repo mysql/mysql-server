92a92d8c920c94e320f8acb9a72922b9413f2bb9
Tiago Alves <tiago.alves@oracle.com>
2020-07-22 12:46:41 +0100

    WL#14236 REMOVES UNNECESSARY NDBBACKUP IMPORTS
    Removes unnecessary NdbBackup.hpp import as code doesn't call any
    functionatly exposed from it.
    
    Change-Id: I1efe8891852752e840b88efb42d34177fdbe66f2
    Approved by: Mauritz Sundell <mauritz.sundell@oracle.com>
    
    Change-Id: I09f54d197ab3b94199eea5c889c947e648ac0d6a

--- storage/ndb/test/ndbapi/bank/testBank.cpp
+++ storage/ndb/test/ndbapi/bank/testBank.cpp
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2003, 2019, Oracle and/or its affiliates.  All rights reserved
+ Copyright (c) 2003, 2020, Oracle and/or its affiliates.
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License, version 2.0,
@@ -27,7 +27,6 @@
 #include <HugoTransactions.hpp>
 #include <UtilTransactions.hpp>
 #include <NdbRestarter.hpp>
-#include <NdbBackup.hpp>
 
 
 #define CHECK(b) if (!(b)) { \
