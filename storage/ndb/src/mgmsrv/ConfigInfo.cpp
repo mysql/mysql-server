3377179655a25cc2df78ec2b6deb0749dc2c3df2
Mauritz Sundell <mauritz.sundell@oracle.com>
2020-08-29 22:00:33 +0200

    Bug#31825181 NODEGROUP WAS NOT ASSIGNED AFTER FIRST USE OF NODEGROUP=65536. DBDIH LINE: 2490
    Also caused data node to fail start if there was nodes defined after
    node without nodegroup.
    
    Reviewed-by: Mikael Ronström <mikael.ronstrom@oracle.com>

--- storage/ndb/src/mgmsrv/ConfigInfo.cpp
+++ storage/ndb/src/mgmsrv/ConfigInfo.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2019, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2020, Oracle and/or its affiliates.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -5832,7 +5832,7 @@ check_node_vs_replicas(Vector<ConfigInfo::ConfigRuleSection>&sections,
       {
         if (ng == NDB_NO_NODEGROUP)
         {
-          break;
+          continue;
         }
         else if (ng >= MAX_NDB_NODES)
         {
