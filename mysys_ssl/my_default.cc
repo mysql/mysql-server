33f59b0affbd7124999e68949de2458c9fc589ea
Arun Kuruvila <arun.kuruvila@oracle.com>
2020-10-07 17:22:54 +0530

    Bug#29861961: MYSQL_CONFIG_EDITOR CAN NOT DEAL PASSWORD               WITH "#"
    Description: Options stored in a login path file using
    'mysql_config_editor' utility which contains the special
    charater, "#", is treated incorrectly while reading the
    options.
    
    Analysis: The special character, '#', is interpreted as the
    beginning of a comment when used inside a login path file.
    The obfuscated login path file, .mylogin.cnf, does not
    intend to suppport any comment inside it. So, no special
    treatment should be given to the charater, "#", when used
    inside a login path file.
    
    Fix: Chararter, "#", is treated as a normal charater when
    used inside a login path file.
    
    RB#25166

--- mysys_ssl/my_default.cc
+++ mysys_ssl/my_default.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2000, 2017, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2000, 2020, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -1043,8 +1043,12 @@ static int search_default_file_with_ext(Process_option_func opt_handler,
       goto err;
     }
 
+    /* comments are not supported in login file */
+    if (!is_login_file)
+      end = remove_end_comment(ptr);
+    else
+      end = ptr + strlen(ptr);
 
-    end= remove_end_comment(ptr);
     if ((value= strchr(ptr, '=')))
       end= value;				/* Option without argument */
     for ( ; my_isspace(&my_charset_latin1, end[-1]) ; end--)
