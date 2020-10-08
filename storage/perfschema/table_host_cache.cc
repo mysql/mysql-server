f58f27a026e5c631daaa88ec25939410d0ceed3b
Marc Alff <marc.alff@oracle.com>
2020-10-08 17:23:34 +0200

    Bug#31978763 PERFORMANCE_SCHEMA.HOST_CACHE SHOULD NOT DEPEND ON WHETHER P_S IS ENABLED
    Before this fix,
    
    when starting the server without the performance schema,
    table performance_schema.host_cache was always empty.
    
    This is a bug: the host_cache table is populated from
    the actual host cache maintained by the SQL layer,
    and this table does not need any instrumentation active,
    so requiring the performance schema to be enabled is abusive.
    
    The root cause is in the table host_cache definition:
    the PFS_engine_table_share::m_perpetual flag was not set.
    
    With this fix,
    
    the m_perpetual flag is set for table performance_schema.host_cache,
    which means the table can provide data even when the performance schema is
    disabled.
    
    Approved by: Chris Powers <chris.powers@oracle.com>

--- storage/perfschema/table_host_cache.cc
+++ storage/perfschema/table_host_cache.cc
@@ -89,7 +89,7 @@ PFS_engine_table_share table_host_cache::m_share = {
     sizeof(PFS_simple_index), /* ref length */
     &m_table_lock,
     &m_table_def,
-    false, /* perpetual */
+    true, /* perpetual */
     PFS_engine_table_proxy(),
     {0},
     false /* m_in_purgatory */
