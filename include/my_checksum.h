9ec39a2c73c1851bf81aa7dbe35c3962d73733d7
Dyre Tjeldvoll <Dyre.Tjeldvoll@oracle.com>
2020-10-13 15:43:29 +0200

    Bug#31101633: ARM CRC32 INTRINSIC CALL TO ACCELERATE TABLE-CHECKSUM (NOT CRC32C BUT CRC32)
    This patch is based on contribution by Krunal Suresh Bauskar from Huawei.
    
    Add overloaded wrappers for the ARM CRC32 intrinsics with dummy
    implementations for testing on other platforms.
    
    Add a function which computes the CRC32 of a buffer by repeatedly
    applying the intrinsic wrappers to chunks of the buffer punned to an
    integer value.
    
    Change my_checkum() to call this function on ARM.
    
    Add unit tests for correctness and micro-benchmarks.
    
    Change-Id: Ic4e471e923c8b5cfe4f33209bd5350b5dd29a71b

--- include/my_checksum.h
+++ include/my_checksum.h
@@ -29,13 +29,78 @@
   Abstraction functions over zlib/intrinsics.
 */
 
-#include <zlib.h>  // crc32
 #include <cassert>
 #include <cstdint>      // std::uint32_t
 #include <limits>       // std::numeric_limits
 #include <type_traits>  // std::is_convertible
 
+#include <zlib.h>  // crc32_z
+
+#include "my_compiler.h"  // My_ATTRIBUTE
 #include "my_config.h"
+
+#ifdef HAVE_ARMV8_CRC32_INTRINSIC
+#include <arm_acle.h>   // __crc32x
+#include <asm/hwcap.h>  // HWCAP_CRC32
+#include <sys/auxv.h>   // getauxval
+#endif                  /* HAVE_ARMV8_CRC32_INTRINSIC */
+
+namespace mycrc32 {
+#ifdef HAVE_ARMV8_CRC32_INTRINSIC
+const bool auxv_at_hwcap = (getauxval(AT_HWCAP) & HWCAP_CRC32);
+
+// Some overloads for these since to allow us to work genericly
+MY_ATTRIBUTE((target("+crc")))
+inline std::uint32_t IntegerCrc32(std::uint32_t crc, std::uint8_t b) {
+  return __crc32b(crc, b);
+}
+
+MY_ATTRIBUTE((target("+crc")))
+inline std::uint32_t IntegerCrc32(std::uint32_t crc, std::uint16_t h) {
+  return __crc32h(crc, h);
+}
+
+MY_ATTRIBUTE((target("+crc")))
+inline std::uint32_t IntegerCrc32(std::uint32_t crc, std::uint32_t w) {
+  return __crc32w(crc, w);
+}
+
+MY_ATTRIBUTE((target("+crc")))
+inline std::uint32_t IntegerCrc32(std::uint32_t crc, std::uint64_t d) {
+  return __crc32d(crc, d);
+}
+#else /* HAVE_ARMV8_CRC32_INTRINSIC */
+
+template <class I>
+inline std::uint32_t IntegerCrc32(std::uint32_t crc, I i) {
+  unsigned char buf[sizeof(I)];
+  memcpy(buf, &i, sizeof(I));
+  crc = ~crc;
+  crc = crc32_z(crc, buf, sizeof(I));
+  return ~crc;
+}
+
+#endif /* HAVE_ARMV8_CRC32_INTRINSIC */
+
+template <class PT>
+inline std::uint32_t PunnedCrc32(std::uint32_t crc, const unsigned char *buf,
+                                 size_t len) {
+  crc = ~crc;
+  const unsigned char *plast = buf + (len / sizeof(PT)) * sizeof(PT);
+  const unsigned char *last = buf + len;
+  for (; buf < plast; buf += sizeof(PT)) {
+    PT pv;
+    memcpy(&pv, buf, sizeof(PT));
+    crc = IntegerCrc32(crc, pv);
+  }
+
+  for (; buf < last; ++buf) {
+    crc = IntegerCrc32(crc, *buf);
+  }
+
+  return (~crc);
+}
+}  // namespace mycrc32
 
 using ha_checksum = std::uint32_t;
 
@@ -48,13 +113,16 @@ using ha_checksum = std::uint32_t;
 
    @returns Updated checksum.
 */
-
 inline ha_checksum my_checksum(ha_checksum crc, const unsigned char *pos,
                                size_t length) {
+#ifdef HAVE_ARMV8_CRC32_INTRINSIC
+  assert(mycrc32::auxv_at_hwcap);
+  if (mycrc32::auxv_at_hwcap)
+    return mycrc32::PunnedCrc32<std::uint64_t>(crc, pos, length);
+#endif  // HAVE_ARMV8_CRC32_INTRINSIC
   static_assert(std::is_convertible<uLong, ha_checksum>::value,
                 "uLong cannot be converted to ha_checksum");
-  assert(crc32_z(static_cast<ha_checksum>(crc), pos, length) <
-         std::numeric_limits<ha_checksum>::max());
-  return crc32_z(static_cast<ha_checksum>(crc), pos, length);
+  assert(crc32_z(crc, pos, length) < std::numeric_limits<ha_checksum>::max());
+  return crc32_z(crc, pos, length);
 }
-#endif  // MY_CHECKSUM_INCLUDED
+#endif /* not defined(MY_CEHCKSUM_INCLUDED) */
