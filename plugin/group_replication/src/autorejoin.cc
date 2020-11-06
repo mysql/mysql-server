8e4422ff8f2d527fbda762799bbac3c233ecd4cf
Nuno Carvalho <nuno.carvalho@oracle.com>
2020-11-06 17:00:08 +0100

    BUG#32068730: FAILURES ON GR_AUTOREJOIN_WHILE_RECOVERY_LOCKED AND GR_AUTOREJOIN_WITH_CLONE_WHI
    Tests
      group_replication.gr_autorejoin_while_recovery_locked and
      group_replication.gr_autorejoin_with_clone_while_recovery_locked
    are failing on el6-x86-32bit platform on PB2.
    More precisely, the tests are timing out.
    
    After analysis it was discovered that the root cause was the GR
    rejoin thread not being able to stop the recovery channel.
    This was happening because the thd of the rejoin thread was being
    instantiated on the object stack instead of the local stack.
    
    To fix the above issue, now the rejoin thread instantiates its thd
    on the local stack.
    
    RB: 25424

--- plugin/group_replication/src/autorejoin.cc
+++ plugin/group_replication/src/autorejoin.cc
@@ -247,12 +247,13 @@ void Autorejoin_thread::execute_rejoin_process() {
 
 [[noreturn]] void Autorejoin_thread::autorejoin_thread_handle() {
   // Initialize the MySQL thread infrastructure.
-  m_thd = new THD;
+  THD *thd = new THD;
   my_thread_init();
-  m_thd->set_new_thread_id();
-  m_thd->thread_stack = reinterpret_cast<const char *>(&m_thd);
-  m_thd->store_globals();
-  global_thd_manager_add_thd(m_thd);
+  thd->set_new_thread_id();
+  thd->thread_stack = reinterpret_cast<const char *>(&thd);
+  thd->store_globals();
+  global_thd_manager_add_thd(thd);
+  m_thd = thd;
 
   // Update the thread state and toggle the auto-rejoin ongoing flag.
   mysql_mutex_lock(&m_run_lock);
