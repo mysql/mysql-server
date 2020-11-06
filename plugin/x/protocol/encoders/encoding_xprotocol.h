ed02a5854b7dd199795aede762fac42a9f517b51
Daniel Blanchard <daniel.blanchard@oracle.com>
2020-11-06 11:50:48 +0000

    Bug#32122517 FIX WINDOWS C4251 COMPILER WARNINGS
    Suppress C4251 warning when including
    google/protobuf/wire_format_lite.h
    
    Change-Id: Ibe34a96b02b93f25d62122f85cce4becf95320aa

--- plugin/x/protocol/encoders/encoding_xprotocol.h
+++ plugin/x/protocol/encoders/encoding_xprotocol.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2020, Oracle and/or its affiliates.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2.0,
@@ -25,7 +25,13 @@
 #ifndef PLUGIN_X_PROTOCOL_ENCODERS_ENCODING_XPROTOCOL_H_
 #define PLUGIN_X_PROTOCOL_ENCODERS_ENCODING_XPROTOCOL_H_
 
+#include "my_compiler.h"
+MY_COMPILER_DIAGNOSTIC_PUSH()
+// Suppress warning C4251 'type' : class 'type1' needs to have dll-interface
+// to be used by clients of class 'type2'
+MY_COMPILER_MSVC_DIAGNOSTIC_IGNORE(4251)
 #include <google/protobuf/wire_format_lite.h>
+MY_COMPILER_DIAGNOSTIC_POP()
 #include <cassert>
 #include <cstdint>
 #include <string>
