66546ec117ba588754ee79a221a73220dd6c5d26
Varun Nagaraju <varun.nagaraju@oracle.com>
2020-11-18 02:24:07 +0530

    Bug#32007149 AUTOTEST FAILS TO PRODUCE BACKTRACE WITH LONG PATHS
    Modifies atrt-backtrace.sh to use file(1) utility to fetch the path of
    the binary from the core file instead of gdb since it fails to fetch the
    complete path of the binary if the path exceeds 80 characters and
    fetches only a part of it leading to the failure of generation of
    symbolic backtrace.
    
    Change-Id: Ib479f7dc10ec1aa72b2be447aea408e1019c45ea

--- storage/ndb/test/run-test/atrt-backtrace.sh
+++ storage/ndb/test/run-test/atrt-backtrace.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2008, 2020, Oracle and/or its affiliates.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2.0,
@@ -64,10 +64,7 @@ fi
 #
 # get binary
 #
-tmp=`echo ${core}.tmp`
-gdb -c $core -x $bt -q 2>&1 | grep "Core was generated" | awk '{ print $5;}' | sed 's!`!!' | sed `echo "s/'\.//"` > $tmp
-exe=`cat $tmp`
-rm -f $tmp
+exe=`file $core | sed 's/.*execfn//' | cut -d"'" -f 2`
 
 if [ -x "$exe" ]
 then
