79c51cf989f6b9c58f5db775db87f05bbf059e51
Tor Didriksen <tor.didriksen@oracle.com>
2020-11-24 18:28:31 +0100

    Bug #32199018 SILENCE WARNINGS WHEN BUILDING 3RD PARTY LIBRARIES
    Silence misc compiler warnings when building bundled ICU and libedit.
    Remove obsolete #include of boost/bind.hpp which gives warnings.
    
    Change-Id: I57eb139596865b75e79ed6793766f35205bb5499
    (cherry picked from commit 8e51aefdeecbb8ca3fb050ead3612109724b45e0)

--- client/dump/abstract_mysql_chain_element_extension.cc
+++ client/dump/abstract_mysql_chain_element_extension.cc
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2015, 2019, Oracle and/or its affiliates. All rights reserved.
+  Copyright (c) 2015, 2020, Oracle and/or its affiliates.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License, version 2.0,
@@ -26,7 +26,6 @@
 
 #include <stddef.h>
 #include <boost/algorithm/string.hpp>
-#include <boost/bind.hpp>
 #include <functional>
 #include <sstream>
 
