ec13bf07e68d25cc013451a20e141a0a36ef4309
Pedro Figueiredo <pedro.figueiredo@oracle.com>
2020-11-27 17:25:30 +0000

    BUG#30519928 INTERMEDIATE MASTER WITH SEMISYNC SHOWS HIGH CPU USAGE DUE TO MUTEX SPINNING
    Description
    -----------
    When there is a large amount of slaves to a master using the semi-sync plugin, a
    degradation in performance is observed due to high contention on mutexes.
    
    Analysis
    --------
    For each plugin event that is triggered, semi-sync callbacks will iterate over
    the observers list, acquiring both `LOCK_plugin` and `Delegate::lock` in the
    process.
    
    As we add semi-sync slaves, the number of threads invoking the above callbacks
    --the dump threads-- increases, generating higher contention over the said
    locks.
    
    Fix
    ---
    We can relax the contention over the said locks by:
    
    1) Since the plugin lock acquisition occurring within the semi-sync observer
       callbacks is already protected by a local rw-lock that protects changes in
       the semi-sync plugin itself, we can use the internal semi-sync plugin
       reference instead of looking for it in the plugin list. To prevent eventual
       and unforeseen behavior changes, a global variable
       `replication_optimize_for_static_plugin_config` is introduced. When enabled,
       will defer uninstall of transaction state change consumers by acquiring a
       hard-reference to the plugin and make the semi-sync observer callback
       processor to use the internal plugin reference instead of looking it up in
       the plugin list and, as a result, avoiding the plugin lock acquisition. When
       the option is disabled, the behavior observed prior to the patch is kept.
    
    2) Since the addition and removal of observer plugins aren't, in most cases,
       frequently executed operations, we can replace the current `Delegate` class
       rw-locking with a more read-friendly lock. Therefore, we can replace the
       currently used read-write lock, with a shared-exclusive spin-lock that will
       speed up read access by requiring that acquiring threads check only an atomic
       flag, in the best case scenario. In the worst case, when an exclusive lock
       has been acquired, threads will be made to spin until such lock is
       released. This change in the type of lock is controlled by enabling the
       `replication_optimize_for_static_plugin_config` global variable.
    
    3) For each transaction and binlog changes observer that is registered, a large
       list of callbacks is executed. In the semi-sync case, we only need to be
       notified upon commit. Therefore, an operation mode is added for which we only
       enable event callback on commit, relaxing the stress on the locks. This
       behavior is controlled by enabling the
       `replication_sender_observe_commit_only` global variable.
    
    How to use it
    -------------
    Both `replication_optimize_for_static_plugin_config` and
    `replication_sender_observe_commit_only` are dynamic global variables that can
    be set after server startup.
    
    Both can be enabled --set to `1`-- either before or after installing the plugins
    and no need to stop replication for changing either of them.
    
    In the case of `replication_optimize_for_static_plugin_config`, semi-sync plugin
    will only be uninstalled when the option is disabled. If `UNINSTALL` of the
    semi-sync plugin is executed while the
    `replication_optimize_for_static_plugin_config` is enabled, the plugin will be
    unloaded when the option is set to `0`.
    
    For enabling the new behavior:
    
    ```
         mysql> SET GLOBAL replication_optimize_for_static_plugin_config = 1;
         mysql> SET GLOBAL replication_sender_observe_commit_only = 1;
    ```
    
    For disabling the new behavior:
    
    ```
         mysql> SET GLOBAL replication_optimize_for_static_plugin_config = 0;
         mysql> SET GLOBAL replication_sender_observe_commit_only = 0;
    ```
    
    The new behavior may be enabled both in the master and in the slaves, because
    both `semi-master` and `semi-slave` plugins use the same infra-structure and
    both benefit from the relaxed mutex access behavior.
    
    Reviewed-by: Joro <georgi.kodinov@oracle.com>
    Reviewed-by: Luís Soares <luis.soares@oracle.com>
    Reviewed-by: Vitor Oliveira <vitor.s.p.oliveira@oracle.com>
    RB: 23598

---
+++ sql/locks/shared_spin_lock.cc
@@ -0,0 +1,321 @@
+/* Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License, version 2.0,
+   as published by the Free Software Foundation.
+
+   This program is also distributed with certain software (including
+   but not limited to OpenSSL) that is licensed under separate terms,
+   as designated in a particular file or component or in included license
+   documentation.  The authors of MySQL hereby grant you an additional
+   permission to link the program and your derivative works with the
+   separately licensed software that they have included with MySQL.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License, version 2.0, for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
+
+#include <iostream>
+#include <map>
+#include <memory>
+
+#include "locks/shared_spin_lock.h"
+
+lock::Shared_spin_lock::Guard::Guard(
+    lock::Shared_spin_lock &target,
+    lock::Shared_spin_lock::enum_lock_acquisition acquisition,
+    bool try_and_fail)
+    : m_target(target), m_acquisition(Shared_spin_lock::SL_NO_ACQUISITION)
+{
+  if (acquisition != Shared_spin_lock::SL_NO_ACQUISITION)
+  {
+    this->acquire(acquisition, try_and_fail);
+  }
+}
+
+lock::Shared_spin_lock::Guard::~Guard() { this->release(); }
+
+lock::Shared_spin_lock *lock::Shared_spin_lock::Guard::operator->()
+{
+  return &this->m_target;
+}
+
+lock::Shared_spin_lock &lock::Shared_spin_lock::Guard::operator*()
+{
+  return this->m_target;
+}
+
+lock::Shared_spin_lock::Guard &lock::Shared_spin_lock::Guard::acquire(
+    enum_lock_acquisition acquisition, bool try_and_fail)
+{
+  DBUG_ASSERT(this->m_acquisition == Shared_spin_lock::SL_NO_ACQUISITION);
+  DBUG_ASSERT(acquisition == Shared_spin_lock::SL_SHARED ||
+              acquisition == Shared_spin_lock::SL_EXCLUSIVE);
+
+  this->m_acquisition= acquisition;
+
+  switch (this->m_acquisition)
+  {
+    case Shared_spin_lock::SL_SHARED:
+    {
+      if (try_and_fail)
+      {
+        this->m_target.try_shared();
+        if (!this->m_target.is_shared_acquisition())
+        {
+          this->m_acquisition= Shared_spin_lock::SL_NO_ACQUISITION;
+        }
+      }
+      else
+      {
+        this->m_target.acquire_shared();
+      }
+      break;
+    }
+    case Shared_spin_lock::SL_EXCLUSIVE:
+    {
+      if (try_and_fail)
+      {
+        this->m_target.try_exclusive();
+        if (!this->m_target.is_exclusive_acquisition())
+        {
+          this->m_acquisition= Shared_spin_lock::SL_NO_ACQUISITION;
+        }
+      }
+      else
+      {
+        this->m_target.acquire_exclusive();
+      }
+      break;
+    }
+    default:
+      break;
+  }
+  return (*this);
+}
+
+lock::Shared_spin_lock::Guard &lock::Shared_spin_lock::Guard::release()
+{
+  if (this->m_acquisition == Shared_spin_lock::SL_NO_ACQUISITION)
+  {
+    return (*this);
+  }
+  switch (this->m_acquisition)
+  {
+    case Shared_spin_lock::SL_SHARED:
+    {
+      this->m_target.release_shared();
+      this->m_acquisition= Shared_spin_lock::SL_NO_ACQUISITION;
+      break;
+    }
+    case Shared_spin_lock::SL_EXCLUSIVE:
+    {
+      this->m_target.release_exclusive();
+      this->m_acquisition= Shared_spin_lock::SL_NO_ACQUISITION;
+      break;
+    }
+    default:
+      break;
+  }
+  return (*this);
+}
+
+lock::Shared_spin_lock::Guard::Guard(const Shared_spin_lock::Guard &rhs)
+    : m_target(rhs.m_target)
+{
+}
+
+lock::Shared_spin_lock::Guard &lock::Shared_spin_lock::Guard::operator=(
+    Shared_spin_lock::Guard const &)
+{
+  return (*this);
+}
+
+lock::Shared_spin_lock::Shared_spin_lock()
+    : m_shared_access(0), m_exclusive_access(0), m_exclusive_owner(0)
+{
+}
+
+lock::Shared_spin_lock::~Shared_spin_lock() {}
+
+lock::Shared_spin_lock &lock::Shared_spin_lock::acquire_shared()
+{
+  return this->try_or_spin_shared_lock(false);
+}
+
+lock::Shared_spin_lock &lock::Shared_spin_lock::acquire_exclusive()
+{
+  return this->try_or_spin_exclusive_lock(false);
+}
+
+lock::Shared_spin_lock &lock::Shared_spin_lock::try_shared()
+{
+  return this->try_or_spin_shared_lock(true);
+}
+
+lock::Shared_spin_lock &lock::Shared_spin_lock::try_exclusive()
+{
+  return this->try_or_spin_exclusive_lock(true);
+}
+
+lock::Shared_spin_lock &lock::Shared_spin_lock::release_shared()
+{
+  DBUG_ASSERT(my_atomic_load32(&this->m_shared_access) > 0);
+  my_atomic_add32(&this->m_shared_access, -1);
+  return (*this);
+}
+
+lock::Shared_spin_lock &lock::Shared_spin_lock::release_exclusive()
+{
+  my_thread_t self= my_thread_self();
+  my_thread_t owner= (my_thread_t)(my_atomic_load64(&this->m_exclusive_owner));
+  DBUG_ASSERT(self != 0);
+  DBUG_ASSERT(my_thread_equal(owner, self));
+  if (!my_thread_equal(owner, self)) return (*this);
+
+  if (my_atomic_load32(&this->m_exclusive_access) == 1)
+    my_atomic_store64(&this->m_exclusive_owner, 0);
+
+  DBUG_ASSERT(my_atomic_load32(&this->m_exclusive_access) > 0);
+  my_atomic_add32(&this->m_exclusive_access, -1);
+  return (*this);
+}
+
+bool lock::Shared_spin_lock::is_shared_acquisition()
+{
+  return my_atomic_load32(&this->m_shared_access) != 0;
+}
+
+bool lock::Shared_spin_lock::is_exclusive_acquisition()
+{
+  if (my_atomic_load32(&this->m_exclusive_access) != 0)
+  {
+    my_thread_t self= my_thread_self();
+    my_thread_t owner=
+        (my_thread_t)(my_atomic_load64(&this->m_exclusive_owner));
+    return my_thread_equal(owner, self);
+  }
+  return false;
+}
+
+lock::Shared_spin_lock &lock::Shared_spin_lock::try_or_spin_shared_lock(
+    bool try_and_fail)
+{
+  if (try_and_fail)
+  {
+    this->try_shared_lock();
+  }
+  else
+  {
+    this->spin_shared_lock();
+  }
+  return (*this);
+}
+
+lock::Shared_spin_lock &lock::Shared_spin_lock::try_or_spin_exclusive_lock(
+    bool try_and_fail)
+{
+  my_thread_t self= my_thread_self();
+  my_thread_t owner= (my_thread_t)(my_atomic_load64(&this->m_exclusive_owner));
+  if (owner != 0 && my_thread_equal(owner, self))
+  {
+    my_atomic_add32(&this->m_exclusive_access, 1);
+    return (*this);
+  }
+
+  if (try_and_fail)
+  {
+    if (!this->try_exclusive_lock())
+    {
+      return (*this);
+    }
+  }
+  else
+  {
+    this->spin_exclusive_lock();
+  }
+  my_atomic_store64(&this->m_exclusive_owner, self);
+  return (*this);
+}
+
+bool lock::Shared_spin_lock::try_shared_lock()
+{
+  if (my_atomic_load32(&this->m_exclusive_access) != 0)
+  {
+    return false;
+  }
+
+  my_atomic_add32(&this->m_shared_access, 1);
+
+  if (my_atomic_load32(&this->m_exclusive_access) != 0)
+  {
+    my_atomic_add32(&this->m_shared_access, -1);
+    return false;
+  }
+  return true;
+}
+
+bool lock::Shared_spin_lock::try_exclusive_lock()
+{
+  int32 expected= 0;
+  if (!my_atomic_cas32(&this->m_exclusive_access, &expected, 1))
+  {
+    return false;
+  }
+  if (my_atomic_load32(&this->m_shared_access) != 0)
+  {
+    my_atomic_store32(&this->m_exclusive_access, 0);
+    return false;
+  }
+  return true;
+}
+
+void lock::Shared_spin_lock::spin_shared_lock()
+{
+  do
+  {
+    if (my_atomic_load32(&this->m_exclusive_access) != 0)
+    {
+      this->yield();
+      continue;
+    }
+
+    my_atomic_add32(&this->m_shared_access, 1);
+
+    if (my_atomic_load32(&this->m_exclusive_access) != 0)
+    {
+      my_atomic_add32(&this->m_shared_access, -1);
+      this->yield();
+      continue;
+    }
+
+    break;
+  } while (true);
+}
+
+void lock::Shared_spin_lock::spin_exclusive_lock()
+{
+  bool success= false;
+  do
+  {
+    int32 expected= 0;
+    success= my_atomic_cas32(&this->m_exclusive_access, &expected, 1);
+    if (!success) this->yield();
+  } while (!success);
+
+  while (my_atomic_load32(&this->m_shared_access) != 0)
+  {
+    this->yield();
+  }
+}
+
+lock::Shared_spin_lock &lock::Shared_spin_lock::yield()
+{
+  my_thread_yield();
+  return (*this);
+}
