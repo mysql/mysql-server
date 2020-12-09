42bed315356838d303df41d0b9d41e1c9a49c396
Pedro Gomes <pedro.gomes@oracle.com>
2020-12-09 13:08:21 +0100

    BUG#32019842: WRITE_SET VECTOR HAS NO UPPER MEMORY LIMIT
    When the option is active, each server transaction will collect write
    sets for each row, allowing to identify what changes the transaction
    made, important for replication parallelization in general and Group
    Replication conflict detection.
    
    This patch improves the handling of write set collection for each
    transaction in 2 ways.
    
    First is that write sets are discarded if their size grow above
    binlog_transaction_dependency_history_size. As a consequence the
    transaction will no longer be marked as concurrent with previous ones
    even if they touch different data.
    
    The second one is that components like Group Replication can set
    memory limits for the collection of write sets. Components like GR are
    dependent on write sets being present, so they will never be
    discarded, but breaking this memory limit will cause transactions to
    abort.
    
    ReviewBoard: 25520
    ReviewBoard: 25592
    (cherry picked from commit 933cc1c3b1dcf175de5aef0b6e2a826435481af6)

--- sql/rpl_slave.cc
+++ sql/rpl_slave.cc
@@ -252,6 +252,25 @@ static void set_thd_tx_priority(THD* thd, int priority)
   DBUG_VOID_RETURN;
 }
 
+/**
+  Set for the thread options about the memory and size limits when
+  transactions collect write sets.
+
+  @param thd          Thread handler
+  @param ignore_limit  if the memory limits should be ignored
+  @param allow_drop_writeset if this thread does not require WS to always be
+  logged
+*/
+static void set_thd_write_set_options(THD *thd, bool ignore_limit,
+                                      bool allow_drop_write_set) {
+  thd->get_transaction()
+      ->get_transaction_write_set_ctx()
+      ->set_local_ignore_write_set_memory_limit(ignore_limit);
+  thd->get_transaction()
+      ->get_transaction_write_set_ctx()
+      ->set_local_allow_drop_write_set(allow_drop_write_set);
+}
+
 /*
   Function to set the slave's max_allowed_packet based on the value
   of slave_max_allowed_packet.
@@ -6215,6 +6234,9 @@ extern "C" void *handle_slave_worker(void *arg)
   thd->init_for_queries(w);
   /* Set applier thread InnoDB priority */
   set_thd_tx_priority(thd, rli->get_thd_tx_priority());
+    /* Set write set related options */
+  set_thd_write_set_options(thd, rli->get_ignore_write_set_memory_limit(),
+                            rli->get_allow_drop_write_set());
 
   thd_manager->add_thd(thd);
   thd_added= true;
@@ -7294,6 +7316,9 @@ extern "C" void *handle_slave_sql(void *arg)
   set_thd_in_use_temporary_tables(rli);   // (re)set sql_thd in use for saved temp tables
   /* Set applier thread InnoDB priority */
   set_thd_tx_priority(thd, rli->get_thd_tx_priority());
+  /* Set write set related options */
+  set_thd_write_set_options(thd, rli->get_ignore_write_set_memory_limit(),
+                            rli->get_allow_drop_write_set());
 
   thd_manager->add_thd(thd);
   thd_added= true;
