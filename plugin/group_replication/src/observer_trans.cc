20659f773db4dcf6254658d4e9a15cbb55bb879c
Jaideep Karande <jaideep.karande@oracle.com>
2021-04-09 10:27:00 +0200

    Bug#32613896: GR IGNORES IMMEDIATE_COMMIT_TIMESTAMP ON OBSERVER_TRANS.CC
    Description:
    Gtid_log_event has two timestamps:
    1. immediate_commit_timestamp - generated whenever a transaction is committed.
       For a replicated transaction, the immediate_commit_timestamp may differ
       between the SOURCE binary log and the REPLICA binary log.
    
    2. original_commit_timestamp - generated when a transaction is committed on
       the first source.
       It is propagated to binary logs of replica using the
       @@session.original_commit_timestamp variable.
       For a replicated transaction, the original_commit_timestamp must be
       identical in the SOURCE binary log and in all REPLICA binary logs.
    
    GR is not generating the 'immediate_commit_timestamp'.
    
    This has no impact on data correctness, though it damages observability.
    
    Resolution:
    GR will now generate a 'immediate_commit_timestamp' in similar way GR does
    for 'original_commit_timestamp'.
    If the transaction originated on the group, the original_commit_timestamp
    and immediate_commit_timestamp are equal, generated by GR.
    If the transaction originated outside of the group, the
    original_commit_timestamp is preserved and immediate_commit_timestamp is
    generated by GR.
    
    ReviewBoard: 26111

--- plugin/group_replication/src/observer_trans.cc
+++ plugin/group_replication/src/observer_trans.cc
@@ -301,6 +301,7 @@ int group_replication_trans_before_commit(Trans_param *param) {
   my_off_t cache_log_position = 0;
   const my_off_t trx_cache_log_position = param->trx_cache_log->length();
   const my_off_t stmt_cache_log_position = param->stmt_cache_log->length();
+  unsigned long long immediate_commit_timestamp = 0;
 
   if (trx_cache_log_position > 0 && stmt_cache_log_position == 0) {
     cache_log = param->trx_cache_log;
@@ -427,8 +428,11 @@ int group_replication_trans_before_commit(Trans_param *param) {
      variable so that it won't be re-defined when this GTID is written to the
      binlog
     */
-    *(param->original_commit_timestamp) = my_micro_time();
-  }  // otherwise the transaction did not originate in this server
+    *(param->original_commit_timestamp) = immediate_commit_timestamp =
+        my_micro_time();
+  } else {  // the transaction did not originate in this server
+    immediate_commit_timestamp = my_micro_time();
+  }
 
   *(param->immediate_server_version) = do_server_version_int(::server_version);
   if (*(param->original_server_version) == UNDEFINED_SERVER_VERSION) {
@@ -445,9 +449,9 @@ int group_replication_trans_before_commit(Trans_param *param) {
   // Notice the GTID of atomic DDL is written to the trans cache as well.
   gle = new Gtid_log_event(
       param->server_id, is_dml || param->is_atomic_ddl, 0, sequence_number,
-      may_have_sbr_stmts, *(param->original_commit_timestamp), 0,
-      gtid_specification, *(param->original_server_version),
-      *(param->immediate_server_version));
+      may_have_sbr_stmts, *(param->original_commit_timestamp),
+      immediate_commit_timestamp, gtid_specification,
+      *(param->original_server_version), *(param->immediate_server_version));
   /*
     GR does not support event checksumming. If GR start to support event
     checksumming, the calculation below should take the checksum payload into
