54b5d39f2f0baf21e4335c9820071c3563552318
Tiago Alves <tiago.alves@oracle.com>
2021-04-21 16:34:53 +0100

    BUG#32799519 REMOVES NDB-COMMON FILES AND REFS
    Removes ndb-common-options files that are no longer used after docs team
    changes as part of the on-going work to improve documentation files.
    
    Change-Id: Ib65c4ccb2cd5abe83c59794aa57365a6e6008456

--- packaging/deb-in/deb_ndb.cmake
+++ packaging/deb-in/deb_ndb.cmake
@@ -144,7 +144,6 @@ Description: nodejs
 /usr/bin/ndbinfo_select_all
 /usr/bin/ndbxfrm
 
-/usr/share/man/man1/ndb-common-options.1*
 /usr/share/man/man1/ndb_blob_tool.1*
 /usr/share/man/man1/ndb_config.1*
 /usr/share/man/man1/ndb_cpcd.1*
