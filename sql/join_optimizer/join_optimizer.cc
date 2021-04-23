f218cf224b0176c492196bc0103036bbfa8f1a41
Steinar H. Gunderson <steinar.gunderson@oracle.com>
2021-04-23 13:40:37 +0200

    Bug #32808908: MAKE PREPARED STATEMENT PLACEHOLDERS SARGABLE IN THE HYPERGRAPH OPTIMIZER
    Make prepared statement placeholders sargable in the hypergraph optimizer
    (they are const_for_execution(), not const_item()).
    
    Change-Id: I1e8d3bbd2e6fa0c3a7b3283d1d9cf871183605a0

--- sql/join_optimizer/join_optimizer.cc
+++ sql/join_optimizer/join_optimizer.cc
@@ -574,10 +574,11 @@ bool CostingReceiver::ProposeRefAccess(TABLE *table, int node_idx,
           !(sp.field->cmp_type() == STRING_RESULT &&
             sp.field->match_collation_to_optimize_range() &&
             sp.field->charset() != item->compare_collation())) {
-        // x = const. (And true const or an outer reference,
-        // just not const_for_execution(); so no execution
-        // of queries during optimization.)
-        if (sp.other_side->const_item() ||
+        // x = const. (And true const; no execution of queries
+        // or stored procedures during optimization.)
+        if ((sp.other_side->const_for_execution() &&
+             !sp.other_side->has_subquery() &&
+             !sp.other_side->is_expensive()) ||
             sp.other_side->used_tables() == OUTER_REF_TABLE_BIT) {
           matched_this_keypart = true;
           keyparts[keypart_idx].field = sp.field;
