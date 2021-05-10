2445cd0100725742c97fb6a7a43fb81f4f5d979f
Maitrayi Sabaratnam <maitrayi.sabaratnam@oracle.com>
2021-05-10 11:00:48 +0200

    Bug#32673736 - AUTOTEST: TESTINDEX -N DEFERREDERROR FAILS WITH ARBITRATION ERROR
    Follow up test fix: Add 'return NDBT_OK;' for the new method.
    
    Reviewed by: Frazer Clement <frazer.clement@oracle.com>

--- storage/ndb/test/ndbapi/testNodeRestart.cpp
+++ storage/ndb/test/ndbapi/testNodeRestart.cpp
@@ -6864,6 +6864,7 @@ int runRestartAllNodes(NDBT_Context* ctx, NDBT_Step* step)
   CHECK(restarter.startAll() == 0, "-");
   CHECK(restarter.waitClusterStarted() == 0, "-");
   CHK_NDB_READY(GETNDB(step));
+  return NDBT_OK;
 }
 
 
