e1cdd79ae7889f043e6f825f6149d31ce7e0c203
Mauritz Sundell <mauritz.sundell@oracle.com>
2021-06-02 15:24:12 +0200

    Bug#32998099 NDBCLIENT_EXPORTS.CPP COMPILE ERROR [-WERROR=LTO-TYPE-MISMATCH]
    Fix build warnings turning up on Ubuntu 21.04 building with GCC 10.3.0
    and using lld linker.
    
    storage/ndb/src/ndbclient_exports.cpp:39:17: error: type of 'ndb_usage' does not match original declaration [-Werror=lto-type-mismatch]
    storage/ndb/src/common/util/ndb_opts.cpp:89:6: note: type mismatch in parameter 1
    storage/ndb/src/common/util/ndb_opts.cpp:89:6: note: 'ndb_usage' was previously declared here
    storage/ndb/src/ndbclient_exports.cpp:40:17: error: type of 'myRandom48Init' does not match original declaration [-Werror=lto-type-mismatch]
    storage/ndb/src/common/util/random.cpp:280:6: note: type mismatch in parameter 1
    storage/ndb/src/common/util/random.cpp:280:6: note: type 'long int' should match type 'void'
    storage/ndb/src/common/util/random.cpp:280:6: note: 'myRandom48Init' was previously declared here
    
    Change-Id: Iea8e62518803c5c01eeaca422a8b634232069043

--- storage/ndb/src/ndbclient_exports.cpp
+++ storage/ndb/src/ndbclient_exports.cpp
@@ -29,16 +29,16 @@
   NOTE! _ndbclient_exports() is not intended to be called
 */
 
-#include "../include/ndbapi/NdbApi.hpp"
+#include "ndbapi/NdbApi.hpp"
 #include "ndbapi/NdbInfo.hpp"
-#include "../include/portlib/NdbDir.hpp"
+#include "portlib/NdbDir.hpp"
+#include "util/ndb_opts.h"
+#include "util/ndb_rand.h"
+#include "util/random.h"
 
 #ifdef NDB_WITH_NDBJTIE
 extern "C" void _ndbjtie_exports(void);
 #endif
-extern "C" void ndb_usage(void);
-extern "C" void myRandom48Init(void);
-extern "C" void ndb_rand(void);
 
 extern "C"
 #ifdef _MSC_VER
@@ -60,8 +60,8 @@ _ndbclient_exports(void)
 #ifdef NDB_WITH_NDBJTIE
   _ndbjtie_exports();
 #endif
-  ndb_usage();
-  myRandom48Init();
+  ndb_std_print_version();
+  myRandom48Init(0);
   ndb_rand();
   (void)NdbDir::chdir("");
   (void)BitmaskImpl::setField(0, 0, 0, 37, (Uint32*)0);
