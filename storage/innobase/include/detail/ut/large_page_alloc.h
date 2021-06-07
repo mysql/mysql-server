bb2a592816399dbefb83ac7f17ded94721b94e77
Jusufadis Bakamovic <jusufadis.bakamovic@oracle.com>
2021-06-07 13:55:11 +0200

    Bug #33118309 IMPLEMENT INNODB LIBRARY FOR LARGE-PAGE SUPPORT
    This patch introduces library functions that are designed to handle
    huge-/large-/super-page aligned allocations. Different operating systems
    have different names for such pages. Normally, most CPUs support and default
    to 4K pages as the smallest memory-addressable unit but they also provide
    support for addressing the memory in much larger units, e.g. 2MB, 4MB,
    1GiB etc. How much and how many of these units are supported varies from
    one CPU to another.
    
    Huge-pages allow to reduce the costs which are imposed by the virtual
    memory management routines in the OS kernel and MMU in the CPU. In particular,
    huge-pages contribute to reducing the TLB misses and therefore also reducing
    the number of soft page-faults. Given that huge-pages are not subject to
    the paging algorithms, hard page-faults are impossible to run into. Soft
    page-faults are carrying the cost of having to access several memory data
    points until the page-walker does not find the right match in the
    page-tables. Page-table is generally implemented as a radix-tree. This
    practically means that for one virtual-to-physical memory address
    translation, CPU will need to traverse through several other ones. If
    those ones aren't in the L1/L2 cache, that could very well mean the price
    to pay is going to be the latency of at least one round-trip to RAM.
    
    In contrast to the functions implementing huge-page aligned allocations
    provided by Unix and/or Windows, these ones have built-in support for
    memory instrumentation that is implemented on top of PFS, a performance
    schema storage engine. Memory instrumentation is an opt-in feature.
    
    Set of new library functions are:
     * ut::malloc_large_page_withkey
     * ut::malloc_large_page 
     * ut::free_large_page
     * ut::large_page_allocation_size
    
    Several platforms are supported by this library:
     * Linux
     * OSX
     * Solaris 
     * Windows
    
    This set of library functions will allow for more elegant implementation
    of the follow-up library whose goal is to allocate memory through
    huge-page support but which is able to gracefully fallback to 4K page-aligned
    allocations in the event of being unable to back the allocation up from
    huge-pages (e.g. huge-page exhaustion).
    
    Reviewed-by: Marcin Babij <marcin.babij@oracle.com>
    RB: 26536

---
+++ storage/innobase/include/detail/ut/large_page_alloc.h
@@ -0,0 +1,344 @@
+/*****************************************************************************
+
+Copyright (c) 2021, Oracle and/or its affiliates.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License, version 2.0, as published by the
+Free Software Foundation.
+
+This program is also distributed with certain software (including but not
+limited to OpenSSL) that is licensed under separate terms, as designated in a
+particular file or component or in included license documentation. The authors
+of MySQL hereby grant you an additional permission to link the program and
+your derivative works with the separately licensed software that they have
+included with MySQL.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License, version 2.0,
+for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+
+*****************************************************************************/
+
+/** @file include/detail/ut/large_page_alloc.h
+ Implementation bits and pieces for large (huge) page allocations. */
+
+#ifndef detail_ut_large_page_alloc_h
+#define detail_ut_large_page_alloc_h
+
+#include <cstddef>
+#include <fstream>
+
+#include "my_compiler.h"
+#ifdef _WIN32
+#include "storage/innobase/include/detail/ut/large_page_alloc-win.h"
+#elif defined(__APPLE__)
+#include "storage/innobase/include/detail/ut/large_page_alloc-osx.h"
+#elif defined(__sun)
+#include "storage/innobase/include/detail/ut/large_page_alloc-solaris.h"
+#else
+#include "storage/innobase/include/detail/ut/large_page_alloc-linux.h"
+#endif
+#include "storage/innobase/include/detail/ut/allocator_traits.h"
+#include "storage/innobase/include/detail/ut/pfs.h"
+
+extern const size_t large_page_default_size;
+
+namespace ut {
+namespace detail {
+
+/** Allocation routines which are purposed for allocating memory through the
+    means of what is known as large (huge) pages.
+
+    large_page_aligned_alloc() and large_page_aligned_free() are taking care of
+    OS specific details and Large_page_alloc is a convenience wrapper which only
+    makes the use of large pages more ergonomic so that it serializes the actual
+    size being allocated into the raw memory. This size is then automagically
+    deduced when large page memory is being freed. Otherwise, client code would
+    have been responsible to store and keep that value somewhere until it frees
+    the large page memory segment.
+
+    Memory layout representation looks like the following:
+
+     ---------------------------------------
+     | LARGE-PAGE-META |    ... DATA ...   |
+     ---------------------------------------
+       ^                ^
+       |                |
+       |                |
+       |               ptr (large-page) to be returned to call-site
+       |
+      -----------------
+      |    DATALEN    |
+      -----------------
+       \               \
+        0               \
+                  CPU_PAGE_SIZE - 1
+
+    DATA is an actual segment that will be returned to the call-site and which
+    the client code will be able to use for the application data.
+ */
+struct Large_page_alloc : public allocator_traits<false> {
+  /** This is how much the metadata (LARGE-PAGE-META) segment will be big. */
+  static constexpr auto metadata_len = CPU_PAGE_SIZE;
+
+  /** This is the type we will be using to store the size of successful
+      large-page allocation size.
+    */
+  using datalen_t = size_t;
+
+  /** Sanity check so that we can be sure that the size of our metadata segment
+      is such so that the pointer to DATA segment is always suitably aligned
+      (multiple of alignof(max_align_t).
+    */
+  static_assert(metadata_len % alignof(max_align_t) == 0,
+                "metadata_len must be divisible by alignof(max_align_t)");
+
+  /** Sanity check so that we can be sure that our metadata segment can fit
+      the datalen_t.
+    */
+  static_assert(sizeof(datalen_t) <= metadata_len, "Metadata does not fit!");
+
+  /** Allocates memory through large-page support.
+
+      @param[in] size Size of storage (in bytes) requested to be allocated.
+      @return Pointer to the allocated storage. nullptr if allocation failed.
+   */
+  static inline void *alloc(std::size_t size) {
+    auto total_len = size + Large_page_alloc::metadata_len;
+    auto mem = large_page_aligned_alloc(total_len);
+    if (unlikely(!mem)) return nullptr;
+    *reinterpret_cast<datalen_t *>(mem) = total_len;
+    return static_cast<uint8_t *>(mem) + Large_page_alloc::metadata_len;
+  }
+
+  /** Releases storage allocated through
+      Large_page_alloc::alloc().
+
+      @param[in] data Pointer to storage allocated through
+      Large_page_alloc::alloc()
+   */
+  static inline void free(void *data) noexcept {
+    if (unlikely(!data)) return;
+    large_page_aligned_free(deduce(data),
+                            datalen(data) + Large_page_alloc::metadata_len);
+  }
+
+  /** Returns the number of bytes that have been allocated.
+
+      @param[in] data Pointer to storage allocated through
+      Large_page_alloc::alloc()
+      @return Number of bytes.
+   */
+  static inline datalen_t datalen(void *data) {
+    return *reinterpret_cast<datalen_t *>(static_cast<uint8_t *>(data) -
+                                          Large_page_alloc::metadata_len);
+  }
+
+ private:
+  /** Helper function which deduces the original pointer returned by
+      Large_page_alloc from a pointer which is passed to us by the call-site.
+   */
+  static inline void *deduce(void *data) noexcept {
+    return reinterpret_cast<void *>(static_cast<uint8_t *>(data) -
+                                    Large_page_alloc::metadata_len);
+  }
+};
+
+/** Allocation routines which are purposed for allocating memory through the
+    means of what is known as large (huge) pages. This is a PFS
+    (performance-schema) variant of Large_page_alloc and thus is implemented
+    in terms of PFS_metadata.
+
+    large_page_aligned_alloc() and large_page_aligned_free() are taking care of
+    OS specific details and Large_page_alloc is a convenience wrapper which only
+    makes the use of large pages more ergonomic so that it serializes the actual
+    size being allocated into the raw memory. This size is then automagically
+    deduced when large page memory is being freed. Otherwise, client code would
+    have been responsible to store and keep that value somewhere until it frees
+    the large page memory segment.
+
+    Memory layout representation looks like the following:
+
+     --------------------------------------------------
+     | PFS-META | VARLEN | PFS-META-OFFSET |   DATA   |
+     --------------------------------------------------
+      ^    ^                                ^
+      |    |                                |
+      |   ---------------------------       |
+      |   | OWNER |  DATALEN  | KEY |       |
+      |   ---------------------------       |
+      |                                     |
+     ptr returned by                        |
+     large_page_aligned_alloc                     |
+                                            |
+                        ptr (large-page) to be returned to call-site
+                                   will be pointing here
+
+    OWNER field encodes the owning thread.
+    DATALEN field encodes total size of memory consumed and not only the size of
+    the DATA segment.
+    KEY field encodes the PFS/PSI key.
+
+    VARLEN is the leftover variable-length segment that specialized
+    implementations can further make use of by deducing its size from the
+    following formulae: abs(CPU_PAGE_SIZE - sizeof(PFS-META-OFFSET) -
+    sizeof(PFS-META)). In code that would be std::abs(CPU_PAGE_SIZE -
+    PFS_metadata::pfs_metadata_size). Not used by this implementation.
+
+    PFS-META-OFFSET, strictly speaking, isn't neccesary in this case of
+    large-pages, where alignment is always known in compile-time and thus the
+    offset we will be storing into the PFS-META-OFFSET field is always going
+    to be the same for the given platform. So, rather than serializing this
+    piece of information into the memory as we do right now, we could very
+    well be storing it into the compile-time evaluated constexpr constant. The
+    reason why we don't do it is that there is no advantage (*) of doing so
+    while we would be introducing a disadvantage of having to maintain separate
+    specialization of PFS_metadata and code would be somewhat more fragmented.
+
+      (*) Extra space that we need to allocate in order to be able to fit the
+          PFS_metadata is going to be the same regardless if there is
+          PFS-META-OFFSET field or not. This is due to the fact that PFS-META
+          segment alone is larger than alignof(max_align_t) so in order to
+          keep the DATA segment suitably aligned (% alignof(max_align_t) == 0)
+          we must choose the size for the whole PFS segment that is a multiple
+          of alignof(max_align_t).
+
+    PFS-META-OFFSET is a field which allows us to recover the pointer to
+    PFS-META segment from a pointer to DATA segment.
+
+    DATA is an actual segment that will be returned to the call-site and which
+    the client code will be able to use for the application data.
+ */
+struct Large_page_alloc_pfs : public allocator_traits<true> {
+  using pfs_metadata = PFS_metadata;
+
+  /** This is how much the metadata (PFS-META | VARLEN | PFS-META-OFFSET)
+      segment will be big.
+    */
+  static constexpr auto metadata_len = CPU_PAGE_SIZE;
+
+  /** Sanity check so that we can be sure that the size of our metadata segment
+      is such so that the pointer to DATA segment is always suitably aligned
+      (multiple of alignof(max_align_t).
+    */
+  static_assert(metadata_len % alignof(max_align_t) == 0,
+                "metadata_len must be divisible by alignof(max_align_t)");
+
+  /** Allocates memory through large-page support.
+
+      @param[in] size Size of storage (in bytes) requested to be allocated.
+      @param[in] key PSI memory key to be used for PFS memory instrumentation.
+      @return Pointer to the allocated storage. nullptr if allocation failed.
+    */
+  static inline void *alloc(std::size_t size,
+                            pfs_metadata::pfs_memory_key_t key) {
+    auto total_len = size + Large_page_alloc_pfs::metadata_len;
+    auto mem = large_page_aligned_alloc(total_len);
+    if (unlikely(!mem)) return nullptr;
+
+#ifdef HAVE_PSI_MEMORY_INTERFACE
+    // The point of this allocator variant is to trace the memory allocations
+    // through PFS (PSI) so do it.
+    pfs_metadata::pfs_owning_thread_t owner;
+    key = PSI_MEMORY_CALL(memory_alloc)(key, total_len, &owner);
+    // To be able to do the opposite action of tracing when we are releasing the
+    // memory, we need right about the same data we passed to the tracing
+    // memory_alloc function. Let's encode this it into our allocator so we
+    // don't have to carry and keep this data around.
+    pfs_metadata::pfs_owning_thread(mem, owner);
+    pfs_metadata::pfs_datalen(mem, total_len);
+    pfs_metadata::pfs_key(mem, key);
+    pfs_metadata::pfs_metaoffset(mem, Large_page_alloc_pfs::metadata_len);
+#endif
+
+    return static_cast<uint8_t *>(mem) + Large_page_alloc_pfs::metadata_len;
+  }
+
+  /** Releases storage allocated through Large_page_alloc_pfs::alloc().
+
+      @param[in] data Pointer to storage allocated through
+      Large_page_alloc_pfs::alloc()
+   */
+  static inline void free(PFS_metadata::data_segment_ptr data) noexcept {
+    if (unlikely(!data)) return;
+
+#ifdef HAVE_PSI_MEMORY_INTERFACE
+    // Deduce the PFS data we encoded in Large_page_alloc_pfs::alloc()
+    auto key = pfs_metadata::pfs_key(data);
+    auto owner = pfs_metadata::pfs_owning_thread(data);
+    auto total_len = pfs_metadata::pfs_datalen(data);
+    // With the deduced PFS data, now trace the memory release action.
+    PSI_MEMORY_CALL(memory_free)
+    (key, total_len, owner);
+
+    large_page_aligned_free(deduce(data), total_len);
+#endif
+  }
+
+  /** Returns the number of bytes that have been allocated.
+
+      @param[in] data Pointer to storage allocated through
+      Large_page_alloc_pfs::alloc()
+      @return Number of bytes.
+   */
+  static inline size_t datalen(PFS_metadata::data_segment_ptr data) {
+    return pfs_metadata::pfs_datalen(data) - Large_page_alloc_pfs::metadata_len;
+  }
+
+ private:
+  /** Helper function which deduces the original pointer returned by
+      Large_page_alloc_pfs from a pointer which is passed to us by the
+      call-site.
+   */
+  static inline void *deduce(PFS_metadata::data_segment_ptr data) noexcept {
+    return pfs_metadata::deduce_pfs_meta(data);
+  }
+};
+
+/** Simple utility metafunction which selects appropriate allocator variant
+    (implementation) depending on the input parameter(s).
+  */
+template <bool Pfs_memory_instrumentation_on>
+struct select_large_page_alloc_impl {
+  using type =
+      Large_page_alloc;  // When PFS is OFF, pick ordinary, non-PFS, variant
+};
+
+template <>
+struct select_large_page_alloc_impl<true> {
+  using type = Large_page_alloc_pfs;  // Otherwise, pick PFS variant
+};
+
+/** Just a small helper type which saves us some keystrokes. */
+template <bool Pfs_memory_instrumentation_on>
+using select_large_page_alloc_impl_t =
+    typename select_large_page_alloc_impl<Pfs_memory_instrumentation_on>::type;
+
+/** Small wrapper which utilizes SFINAE to dispatch the call to appropriate
+    aligned allocator implementation.
+  */
+template <typename Impl>
+struct Large_alloc_ {
+  template <typename T = Impl>
+  static inline typename std::enable_if<T::is_pfs_instrumented_v, void *>::type
+  alloc(size_t size, PSI_memory_key key) {
+    return Impl::alloc(size, key);
+  }
+  template <typename T = Impl>
+  static inline typename std::enable_if<!T::is_pfs_instrumented_v, void *>::type
+  alloc(size_t size, PSI_memory_key /*key*/) {
+    return Impl::alloc(size);
+  }
+  static inline void free(void *ptr) { Impl::free(ptr); }
+  static inline size_t datalen(void *ptr) { return Impl::datalen(ptr); }
+};
+
+}  // namespace detail
+}  // namespace ut
+
+#endif
