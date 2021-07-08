2b10e0bf2983a8a204bf6eade373800c4edd2532
Tor Didriksen <tor.didriksen@oracle.com>
2021-07-08 15:39:44 +0200

    Bug #33084073 BUILD AUTHENTICATION_FIDO_CLIENT ALSO FOR COMMUNITY BUILDS [remove]
    Remove things we do not need:
    
    In extra/libfido2
    git rm -r .actions/ .gitattributes .github/ .gitignore  windows/ examples/
    
    In extra/libcbor
    git rm -r .clang-format clang-format.sh .github/ .gitignore .gitmodules demo/ examples misc release.sh refresh_templates.sh codecov.yml appveyor.yml .travis.yml
    
    Change-Id: I72faae3c39bc7594e050d222a4b48e093a52ccb1
    (cherry picked from commit 2cf3cf0528339322225984c937bb7576296791c2)

--- extra/libcbor/misc/seq.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-
-Integer($*[0]).times {|i| print "0x%02X, " % ((Integer($*[1]) + i) % 0xFF) }
--- extra/libcbor/refresh_templates.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-erb $(dirname $0)/test/stream_expectations.c.erb > \
-    $(dirname $0)/test/stream_expectations.c
-clang-format -style=file -i $(dirname $0)/test/stream_expectations.c
