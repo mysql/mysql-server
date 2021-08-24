d85e99f0779c928906ef52541719368d03729fc4
Mauritz Sundell <mauritz.sundell@oracle.com>
2021-08-24 11:53:57 +0200

    Bug#33284372 MySQL 5.7 (and Cluster 7.4, 7.5, 7.6) build fails on macos with XCode
    Hide bzero macro defined in system headers.
    
    Change-Id: I5132bbee491817d801de96dff6f8fdb0dcc19338

--- include/m_string.h
+++ include/m_string.h
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2000, 2012, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2000, 2021, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -50,6 +50,9 @@
 #endif
 
 #define bfill please_use_memset_rather_than_bfill()
+#ifdef bzero
+#undef bzero
+#endif
 #define bzero please_use_memset_rather_than_bzero()
 
 #if !defined(HAVE_MEMCPY) && !defined(HAVE_MEMMOVE)
