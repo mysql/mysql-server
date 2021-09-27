6b4ebcb34b3c62d4dfc9301a24b81553f3d6fd83
Marc Alff <marc.alff@oracle.com>
2021-09-27 15:15:09 +0200

    WL#14779 PERFORMANCE_SCHEMA, ADD CPU TIME TO STATEMENT METRICS
    Post merge fix:
    Fixed build warnings on Windows.
    
    Approved by: Chris Powers <chris.powers@oracle.com>

--- mysys/my_rdtsc.cc
+++ mysys/my_rdtsc.cc
@@ -327,8 +327,8 @@ ulonglong my_timer_thread_cpu(void) {
       /*
         GetThreadTimes() return a number expressed in 100 nanosecs units.
       */
-      result = ((user.dwHighDateTime + system.dwHighDateTime)
-                << 32 + (user.dwLowDateTime + system.dwLowDateTime)) *
+      result = (((user.dwHighDateTime + system.dwHighDateTime) << 32) +
+                (user.dwLowDateTime + system.dwLowDateTime)) *
                100;
     } else {
       result = 0;
