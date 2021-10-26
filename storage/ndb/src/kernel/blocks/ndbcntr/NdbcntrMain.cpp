23d894a218bf1e861378389696ff7c81530506af
Frazer Clement <frazer.clement@oracle.com>
2021-10-26 17:27:19 +0100

    Bug #33162065 NdbCntr startphase sync should verify nodeid
    Cross-node startphase sync nodeid checks.
    
    Approved by : Varun Nagaraju <varun.nagaraju@oracle.com>

--- storage/ndb/src/kernel/blocks/ndbcntr/NdbcntrMain.cpp
+++ storage/ndb/src/kernel/blocks/ndbcntr/NdbcntrMain.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2020, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2021, Oracle and/or its affiliates.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -2896,6 +2896,7 @@ Ndbcntr::wait_sp_rep(Signal* signal)
     return;
   }
 
+  ndbrequire(rep.nodeId < NDB_ARRAY_SIZE(c_start.m_wait_sp));
   c_start.m_wait_sp[rep.nodeId] = rep.sp;
 
   /**
