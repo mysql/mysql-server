7f21a956fb175c63aced7d0b7243772202152ac1
Frazer Clement <frazer.clement@oracle.com>
2021-10-26 17:24:04 +0100

    Bug #33161519 LQH should validate bucket id when dumping operations
    Operation hash bucket id checked.
    
    Approved by : Varun Nagaraju <varun.nagaraju@oracle.com>

--- storage/ndb/src/kernel/blocks/dblqh/DblqhMain.cpp
+++ storage/ndb/src/kernel/blocks/dblqh/DblqhMain.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2019, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2003, 2021, Oracle and/or its affiliates.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -25542,6 +25542,8 @@ Dblqh::execDUMP_STATE_ORD(Signal* signal)
     Uint32 record = signal->theData[2];
     Uint32 len = signal->getLength();
     TcConnectionrecPtr tcRec;
+    ndbrequire(bucket < NDB_ARRAY_SIZE(ctransidHash));
+
     if (record != RNIL)
     {
       jam();
