725b13641b03a38bbb0128cff8b20a09f191580e
Roy Lyseng <roy.lyseng@oracle.com>
2021-11-10 14:15:19 +0100

    Bug#33511690: MySQL fails by mysql_list_processes
    The problem is due to bad check of current query block / query result
    structure when calling mysqld_list_processes() using a direct call
    from a client program.
    
    It is rectified by adding an argument to mysqld_list_processes() that
    tells whether it is called from a command object that manages
    termination of sending to client, or whether the function terminates
    sending by itself.
    
    A comment on why it is required to have two different eof calls is
    also added.
    
    Change-Id: I71340d57d492c6c09d53a1e494d546b48731738b

--- sql/sql_show.h
+++ sql/sql_show.h
@@ -67,7 +67,8 @@ void mysqld_list_fields(THD *thd, TABLE_LIST *table, const char *wild);
 bool mysqld_show_create(THD *thd, TABLE_LIST *table_list);
 bool mysqld_show_create_db(THD *thd, char *dbname, HA_CREATE_INFO *create);
 
-void mysqld_list_processes(THD *thd, const char *user, bool verbose);
+void mysqld_list_processes(THD *thd, const char *user, bool verbose,
+                           bool has_cursor);
 bool mysqld_show_privileges(THD *thd);
 void calc_sum_of_all_status(System_status_var *to);
 void append_definer(const THD *thd, String *buffer,
