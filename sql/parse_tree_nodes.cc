9f64ca81dd693c20bebacabe373437b2821b296a
Maheedhar PV <maheedhar.panchalamarri.venka@oracle.com>
2022-01-14 03:10:30 +0100

    Bug#33714885 - LOAD DATA INFILE fails with ERROR 1054 : Unknown column XXXX in 'where clause'
    The field of the table being loaded into if used in the where clause
    of a SET column, is reported as a unknown.
    
    The outer context of the current query block is not set and trying to
    resolve the name of a column of the outer select fails.
    
    The outer context is set only if the parsing_context of a query bock
    is non-NONE. In case of the LOAD DATA statement the parsing context is
    not being set, hence the error.
    
    Fix:
    Set the parsing_context for the LOAD statement to be similar to a
    statement with UPDATE_VALUE.
    
    Change-Id: I898c6a84c443cf0a2a7efc3c46191e7bd709c5b7

--- sql/parse_tree_nodes.cc
+++ sql/parse_tree_nodes.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013, 2021, Oracle and/or its affiliates.
+/* Copyright (c) 2013, 2022, Oracle and/or its affiliates.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -3184,10 +3184,17 @@ Sql_cmd *PT_load_table::make_cmd(THD *thd) {
     lex->set_ignore(true);
 
   Parse_context pc(thd, select);
-  if (contextualize_safe(&pc, &m_cmd.m_opt_fields_or_vars) ||
-      contextualize_safe(&pc, &m_cmd.m_opt_set_fields) ||
-      contextualize_safe(&pc, &m_cmd.m_opt_set_exprs))
+  if (contextualize_safe(&pc, &m_cmd.m_opt_fields_or_vars)) {
     return nullptr;
+  }
+  assert(select->parsing_place == CTX_NONE);
+  select->parsing_place = CTX_UPDATE_VALUE;
+  if (contextualize_safe(&pc, &m_cmd.m_opt_set_fields) ||
+      contextualize_safe(&pc, &m_cmd.m_opt_set_exprs)) {
+    return nullptr;
+  }
+  assert(select->parsing_place == CTX_UPDATE_VALUE);
+  select->parsing_place = CTX_NONE;
 
   return &m_cmd;
 }
