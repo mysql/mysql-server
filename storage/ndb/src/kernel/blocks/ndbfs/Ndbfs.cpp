d78b945da8b46d342b80bae2a62572fe6924c28c
Mauritz Sundell <mauritz.sundell@oracle.com>
2022-01-20 14:51:19 +0100

    Bug#33772598 Pass Om_ENCRYPT_XTS for TS data file
    Instead of hardcoded logic in Ndbfs to determine when to encrypt data
    node files let the calling block do that.
    
    Change-Id: Ib6cd9e37e966c10a2b9be8e44e8d914bd8c422ca

--- storage/ndb/src/kernel/blocks/ndbfs/Ndbfs.cpp
+++ storage/ndb/src/kernel/blocks/ndbfs/Ndbfs.cpp
@@ -668,11 +668,10 @@ Ndbfs::execFSOPENREQ(Signal* signal)
     if (FsOpenReq::getVersion(fsOpenReq->fileNumber) == 4 &&
        FsOpenReq::v4_getBasePath(fsOpenReq->fileNumber) == FsOpenReq::BP_DD_DF)
     { /* TS data files */
-      require(!(fsOpenReq->fileFlags & FsOpenReq::OM_ENCRYPT_XTS));
+      require((fsOpenReq->fileFlags & FsOpenReq::OM_ENCRYPT_XTS));
       require(!(fsOpenReq->fileFlags & FsOpenReq::OM_ENCRYPT_KEY));
       if (page_size == 0) fprintf(stderr,"YYY: %s: %u: %s: page_size %zu\n",__func__,__LINE__,file->theFileName.c_str(),(size_t)page_size);
       require(page_size > 0);
-      fsOpenReq->fileFlags |= FsOpenReq::OM_ENCRYPT_XTS;
     }
     /* UNDO */
     if (FsOpenReq::getVersion(fsOpenReq->fileNumber) == 4 &&
