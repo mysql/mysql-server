799772eb94906dc6f757393f3234f610265e6b38
Mauritz Sundell <mauritz.sundell@oracle.com>
2022-03-03 17:06:59 +0100

    Bug#33881953 cpcd: -Wunused-parameter [no-close]
    Change-Id: Ibd25f0c338d9696f8354d9fe620238aef8396208

--- storage/ndb/src/cw/cpcd/main.cpp
+++ storage/ndb/src/cw/cpcd/main.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2021, Oracle and/or its affiliates.
+   Copyright (c) 2003, 2022, Oracle and/or its affiliates.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -76,9 +76,8 @@ static struct my_option my_long_options[] =
   { 0, 0, 0, 0, 0, 0, GET_NO_ARG, NO_ARG, 0, 0, 0, 0, 0, 0}
 };
 
-static bool
-get_one_option(int optid, const struct my_option *opt [[maybe_unused]],
-	       char *argument)
+static bool get_one_option(int /*optid*/, const struct my_option * /*opt*/,
+                           char * /*argument*/)
 {
   return 0;
 }
