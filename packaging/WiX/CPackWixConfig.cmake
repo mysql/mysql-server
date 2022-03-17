b0e150d68c568c926a7a2f556e2e0821b3e3ca99
Sreedhar Sreedhargadda <sreedhar.sreedhargadda@oracle.com>
2022-03-17 14:21:41 +0100

    BUG#33972317 Server 8.0 MSI is missing INFO_BIN and INFO_SRC
        Adding Info component to msi packages to install INFO* files
    
    Change-Id: Ia49e469474856318d1609a7f38e1cabd5575c913

--- packaging/WiX/CPackWixConfig.cmake
+++ packaging/WiX/CPackWixConfig.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2010, 2021, Oracle and/or its affiliates.
+# Copyright (c) 2010, 2022, Oracle and/or its affiliates.
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2.0,
@@ -21,7 +21,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
 
 SET(CPACK_COMPONENTS_USED 
-    "Server;Client;DataFiles;Development;SharedLibraries;Documentation;IniFiles;Readme;Server_Scripts;Meb;MebReadme;Router")
+    "Server;Client;DataFiles;Development;SharedLibraries;Documentation;IniFiles;Info;Readme;Server_Scripts;Meb;MebReadme;Router")
 
 IF(WITH_NDBCLUSTER)
   MESSAGE(STATUS "This is Cluster build, append additional components")
