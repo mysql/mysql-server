ea04ae05c80381608a65f6f41c0061a49d4738eb
Erik K. Haugen <erik.kaasboll.haugen@gmail.com>
2022-03-23 12:36:05 +0100

    Dijkstras_functor fewer hash lookups

--- sql/Dijkstras_functor.cc
+++ sql/Dijkstras_functor.cc
@@ -1,61 +1,45 @@
 #include "sql/Dijkstras_functor.h"
 
 std::vector<const Edge*> Dijkstra::operator()(const int& start_point_id, const int& end_point_id, double& total_cost) {
-    empty_path_maps();
+    m_point_map.clear();
     int point = start_point_id;
-    set_point(point, 0, m_heu(point), nullptr);
-    m_popped_map[point] = true;
+    Point& node = m_point_map[point] = Point{ 0, m_heu(point), nullptr };
     // A*
     while (point != end_point_id) {
-      const double point_cost = m_cost_map[point];
-      const std::pair<edge_iterator, edge_iterator> edge_range_it = m_edges_lookup_from->equal_range(point);
+      const std::pair<edge_iterator, edge_iterator> edge_range_it = m_edges->equal_range(point);
       // checks all edges from point (i.e. current point)
       for (edge_iterator edge_it = edge_range_it.first; edge_it != edge_range_it.second; edge_it++) {
         const Edge *edge = edge_it->second;
-        // ignore edges to popped(visited) nodes
-        if (contains(m_popped_map, edge->to)) continue;
-        // ignore edge if new path cost > prev path cost to same node
-        double old_cost, new_cost = point_cost + edge->cost;
-        if (extract(m_cost_map, edge->to, old_cost) && new_cost > old_cost) continue;
+        // grabs existing node or creates new with cost of INFINITY
+        Point& node_to = m_point_map[edge->to];
         
-        set_point(edge->to, new_cost, new_cost + m_heu(edge->to), edge);
+        // ignore longer paths
+        double new_cost = node.cost + edge->cost;
+        if (new_cost >= node_to.cost)
+            continue;
+        
+        node_to.cost = new_cost;
+        node_to.cost_heu = new_cost + m_heu(edge->to);
+        node_to.path = edge;
+
         point_heap.push_back(edge->to);
         std::push_heap(point_heap.begin(), point_heap.end(), heap_cmp);
       }
       if (point_heap.empty()) return {};
       point = point_heap.front();
       point_heap.pop_front();
-      m_popped_map[point] = true;
+      node = m_point_map[point];
     }
-    total_cost = m_cost_map[point];
+    total_cost = node.cost;
     return retrace(point, start_point_id);
-}
-
-template<typename Key, typename T>
-inline bool Dijkstra::contains(const std::unordered_map<Key, T>& map, const Key& key) {
-	return map.find(key) != map.end();
-}
-template<typename Key, typename T>
-inline bool Dijkstra::extract(const std::unordered_map<Key, T>& map, const Key& key, T& val) {
-	auto pair = map.find(key);
-	bool found = pair != map.end();
-  if (found) val = pair->second;
-  return found;
-}
-void Dijkstra::set_point(const int& id, const double& cost, const double& heu_cost, const Edge *const path) {
-  	m_cost_map[id] = cost; m_heu_cost_map[id] = heu_cost; m_path_map[id] = path;
 }
 std::vector<const Edge*> Dijkstra::retrace(int from_point, const int& to_point) {
   std::vector<const Edge*> path;
   while (from_point != to_point) {
-    const Edge* path_ = m_path_map[from_point];
+    const Edge* path_ = m_point_map[from_point].path;
     path.push_back(path_);
     from_point = path_->from;
   }
   std::reverse(path.begin(), path.end());
   return path;
-}
-void Dijkstra::empty_path_maps(){
-  m_popped_map.clear(); m_cost_map.clear(); m_heu_cost_map.clear(); m_path_map.clear();
-  point_heap.clear();
 }
--- sql/Dijkstras_functor.h
+++ sql/Dijkstras_functor.h
@@ -1,10 +1,11 @@
 #ifndef DIJKSTRAS_FUNCTOR_INCLUDED
 #define DIJKSTRAS_FUNCTOR_INCLUDED
 
-#include <queue>
-#include <unordered_map>
-#include <functional>
-#include <algorithm>
+#include <queue>            // std::deque
+#include <unordered_map>    // std::unordered_map & std::unordered_multimap
+#include <functional>       // std::function
+#include <algorithm>        // std::push_heap
+#include <cmath>            // INFINITY
 
 /**
  * @brief Edge data for Dijkstra functor
@@ -24,18 +25,20 @@ class Dijkstra {
   typedef std::unordered_multimap<int, const Edge*>::const_iterator edge_iterator;
 
   // key = Edge.from
-  const std::unordered_multimap<int, const Edge*>* m_edges_lookup_from;
+  const std::unordered_multimap<int, const Edge*>* m_edges;
   const std::function<double(const int& point_id)> m_heu = [](const int&) -> double { return 0.0; };
 
-  std::unordered_map<int, bool> m_popped_map;
-  std::unordered_map<int, double> m_cost_map, m_heu_cost_map; // heu_cost = real_cost + heuristic
-  std::unordered_map<int, const Edge*> m_path_map;
+  struct Point {
+      double cost = INFINITY, cost_heu = INFINITY; // heu_cost = real_cost + heuristic
+      const Edge* path = nullptr;
+  };
+  std::unordered_map<int, Point> m_point_map;
 
   // comparator used for point_heap sorting to make min heap based on m_heu_cost_map
   struct greater_point_heuristic_comparator {
-    bool operator()(const int& a, const int& b) { return val.at(a) > val.at(b); }
-    const std::unordered_map<int, double>& val;
-  } heap_cmp{ m_heu_cost_map };
+    bool operator()(const int& a, const int& b) { return point_map.at(a).cost_heu > point_map.at(b).cost_heu; }
+    const std::unordered_map<int, Point>& point_map;
+  } heap_cmp{ m_point_map };
   std::deque<int> point_heap;
 
  public:
@@ -45,9 +48,9 @@ class Dijkstra {
    * @param edges_lookup_from key must equal edge start node id (i.e. Edge.from)
    * @param heu_func A* heuristic. If not supplied normal dijkstra will be used
    */
-  Dijkstra(const std::unordered_multimap<int, const Edge*>* edges_lookup_from,
+  Dijkstra(const std::unordered_multimap<int, const Edge*>* edges,
            const std::function<double(const int& point_id)>& heu_func = [](const int&) -> double { return 0.0; })
-    : m_edges_lookup_from(edges_lookup_from), m_heu(heu_func) {}
+    : m_edges(edges), m_heu(heu_func) {}
   /**
    * @brief runs A* to find shortest path through m_edges_lookup_from
    * 
@@ -60,53 +63,13 @@ class Dijkstra {
   std::vector<const Edge*> operator()(const int& start_point_id, const int& end_point_id, double& total_cost);
  private:
   /**
-   * @brief checks if map contains key
-   * 
-   * @tparam Key type
-   * @tparam T value type
-   * @param map 
-   * @param key 
-   * @return true if map contains key
-   * @return false if map doesn't contain key
-   */
-  template<typename Key, typename T>
-  static inline bool contains(const std::unordered_map<Key, T>& map, const Key& key);
-  /**
-   * @brief extracts value from map with given key
-   * 
-   * @tparam Key type
-   * @tparam T value type
-   * @param map 
-   * @param key 
-   * @param val 
-   * @return true if map contains key
-   * @return false if map doesn't contain key
-   */
-  template<typename Key, typename T>
-  static inline bool extract(const std::unordered_map<Key, T>& map, const Key& key, T& val);
-  /**
-   * @brief stores point info in node maps (i.e. m_xxx_map)
-   * 
-   * @param id node id
-   * @param cost dijkstra path cost
-   * @param heu_cost cost + heuristic_cost (e.g. euclidean dist)
-   * @param path ptr to edge leading to node
-   */
-  inline void set_point(const int& id, const double& cost, const double& heu_cost, const Edge *const path);
-  /**
-   * @brief finds path by accumulating edge_ptrs from m_path_map and reverting their order
+   * @brief finds path by accumulating Point.path from m_point_map and reverting their order
    *   NB: will deref invalid ptr if path doesn't exist
    * @param from_point node id of path start
    * @param to_point node id of path end
    * @return std::vector<const Edge*> path found in m_path_map
    */
   std::vector<const Edge*> retrace(int from_point, const int& to_point);
-
-  /**
-   * @brief empties all node maps (i.e. m_xxx_map) to remove prev path data
-   * 
-   */
-  inline void empty_path_maps();
 };
 
 #endif /* DIJKSTRAS_FUNCTOR_INCLUDED */
