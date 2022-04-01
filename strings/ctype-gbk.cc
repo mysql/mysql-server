94ccbb1c6d98075dcd5b17b267b6351328d2991f
Tor Didriksen <tor.didriksen@oracle.com>
2022-04-01 11:03:35 +0200

    Bug #34025412 Rename utf8_xxx functions and data structures to utf8mb3_xxx [noclose]
    For better code readability/maintanability, rename functions and data
    structures used for handling utf8mb3 charsets and collations.
    
    This patch renames internal functions/arrays in strings/ctype-*.cc
    
    Also remove some unused code.
    
    Change-Id: I7d5ff51b28939500514a6b120a22748c4ee312f3
    (cherry picked from commit fb14369059a2572220d7ca7f8685a6993315d7f9)

--- strings/ctype-gbk.cc
+++ strings/ctype-gbk.cc
@@ -10014,7 +10014,7 @@ static MY_CHARSET_HANDLER my_charset_handler = {nullptr, /* init */
                                                 ismbchar_gbk,
                                                 mbcharlen_gbk,
                                                 my_numchars_mb,
-                                                my_charpos_mb,
+                                                my_charpos_mb3,
                                                 my_well_formed_len_gbk,
                                                 my_lengthsp_8bit,
                                                 my_numcells_8bit,
