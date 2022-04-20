933dbee62df2f895fdbb7dcd4da4414266381295
Andrzej Religa <andrzej.religa@oracle.com>
2022-04-20 11:18:04 +0200

    Bug#34089826 UPGRADE DUKTAPE TO 2.7.0 [3/3]
    Upgrade bundled duktape from 2.6.0 to 2.7.0
    
    This patch is step 3/3:
    Remove the 2.6.0 version from the source tree
    git rm -r extra/duktape-2.6.0/
    
    Change-Id: I5e75e4bc8dad1b13ed3fdd290137f5eae57daf22

--- extra/duktape/duktape-2.6.0/extras/alloc-pool/ptrcomp_fixup.h
+++
@@ -1,2 +0,0 @@
-/* To provide declarations for inline pointer compression functions. */
-#include "duk_alloc_pool.h"
--- extra/duktape/duktape-2.6.0/extras/duk-v1-compat/test_eval2.js
+++
@@ -1,4 +0,0 @@
-// File to eval, throws error
-print('Hello from test_eval2.js');
-print(new Error('test error for traceback (shows filename)').stack);
-throw new Error('aiee');
