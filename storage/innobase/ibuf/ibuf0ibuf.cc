716001c5011e99dd00838398dd20f06a500ef26c
Marcin Babij <marcin.babij@oracle.com>
2022-08-03 14:52:42 +0200

    Bug #33192496 	Assert: buf0flu.cc:3555:UT_LIST_GET_LEN(buf_pool->flush_list) == 0 thread 140069
    Page IO reads are issued only before SRV_SHUTDOWN_FLUSH_PHASE. These are issued either from user activity before shutdown started, or after shutdown started, from srv_master thread shutdown, when it processes the ibuf merges, which finishes before the shutdown reaches SRV_SHUTDOWN_FLUSH_PHASE. It could happen that IO reads for these pages are still not finished even though the loop has finished. In case these pages incorporate ibuf merges after the IO read is complete, these pages will become dirty and a member of flush list later, thus the flush lists after full flush may be not empty.
    The fix is continue flushing while there are any pending IO reads.
    
    Change-Id: Id0b5f2c48ca72b5db810ef4a493187f12dcab95b

--- storage/innobase/ibuf/ibuf0ibuf.cc
+++ storage/innobase/ibuf/ibuf0ibuf.cc
@@ -2411,7 +2411,7 @@ ulint ibuf_merge_in_background(bool full) {
     mutex_enter(&ibuf_mutex);
 
     /* If the ibuf->size is more than half the max_size
-    then we make more agreesive contraction.
+    then we make more aggressive contraction.
     +1 is to avoid division by zero. */
     if (ibuf->size > ibuf->max_size / 2) {
       ulint diff = ibuf->size - ibuf->max_size / 2;
