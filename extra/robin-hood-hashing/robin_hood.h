c104b96a9e2c414bbba52c7171d74b561be3a7fc
Tor Didriksen <tor.didriksen@oracle.com>
2022-09-27 12:54:10 +0200

    Bug #34638573 Compile MySQL with clang 15 [3rd party]
    Additional patch: silence or fix warnings in 3rd party code.
    
    Change-Id: I0371ec83b3f0da3741f27b251ad3c422f4f29a77

--- extra/robin-hood-hashing/robin_hood.h
+++ extra/robin-hood-hashing/robin_hood.h
@@ -203,13 +203,7 @@ static Counts& counts() {
 #    define ROBIN_HOOD_PRIVATE_DEFINITION_HAS_NATIVE_WCHART() 1
 #endif
 
-// workaround missing "is_trivially_copyable" in g++ < 5.0
-// See https://stackoverflow.com/a/31798726/48181
-#if defined(__GNUC__) && __GNUC__ < 5
-#    define ROBIN_HOOD_IS_TRIVIALLY_COPYABLE(...) __has_trivial_copy(__VA_ARGS__)
-#else
 #    define ROBIN_HOOD_IS_TRIVIALLY_COPYABLE(...) std::is_trivially_copyable<__VA_ARGS__>::value
-#endif
 
 // helpers for C++ versions, see https://gcc.gnu.org/onlinedocs/cpp/Standard-Predefined-Macros.html
 #define ROBIN_HOOD_PRIVATE_DEFINITION_CXX() __cplusplus
