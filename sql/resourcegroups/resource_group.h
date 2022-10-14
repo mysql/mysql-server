bc5e71e4d982f383c0ca0851969760bfecb071b9
Praveenkumar Hulakund <praveenkumar.hulakund@oracle.com>
2022-10-14 15:40:26 +0530

    Bug#34702833 - Allow system thread to bind to user(THD) resource group.
    The resource group(RG) feature supports USER and SYSTEM type
    resource groups. RG of type USER can be assigned to user threads
    (user connections) and RG of type SYSTEM to system (background)
    threads. Assigning RG of type USER to system threads and SYSTEM
    to user threads explicitly by a user is not allowed.
    
    Server even does not allow system threads to internally bind user
    connection's RG to perform user connection's task.
    
    Patch for this bug adds support for binding user connection's
    RG to system thread internally.
    
    Following changes are done as part of this patch:
    
      a) New member is introduced to THD::Resource_group_ctx to
         store OS thread ID of a system thread bound to user
         connection's RG.
    
      b) Resource group code is refactored to handle "SET RESOURCE
         GROUP", "DROP RESOURCE GROUP" and "ALTER RESOURCE GROUP"
         operations when system thread is bound to the user connection's
         RG.
    
         Reference counter is maintained with each RG to count number
         of threads directly associated with RG and number of system
         threads referencing this RG.
    
      c) New RG "SYS_internal" of type SYSTEM with "0" CPU priority
         and without CPU affinity (similar to SYS_default) is
         introduced. This RG can be assigned to the system thread
         internally by server only.
         Following restrictions applies to this RG:
           i) Like other default resource group, user is *not* allowed
              to DROP/ALTER this RG.
          ii) User is *not* allowed to bind this RG for threads.
         iii) User is *not* allowed to assign other RG to threads
              bound to this RG.
    
         System threads whose responsibility is to just run user
         connection's task using user connection's RG and otherwise
         they do not consume much resource, allowing user to assign
         other RG does not provide much advantage. Such system threads
         can be internally bind to "SYS_internal" resource group.
    
      d) New status variable "Resource_group_supported" is added to
         indicate if resource group is supported or not.
    
    Change-Id: I38cbd7c96d2203953f1637592d76a3272de7f603

--- sql/resourcegroups/resource_group.h
+++ sql/resourcegroups/resource_group.h
@@ -24,6 +24,7 @@
 
 #include "resource_group_sql_cmd.h"
 
+#include <atomic>
 #include <functional>
 #include <memory>
 #include <mutex>
@@ -62,6 +63,20 @@ class Resource_group {
   void set_type(Type type) { m_type = type; }
 
   void set_enabled(bool enabled) { m_enabled = enabled; }
+
+  /**
+    Method to check if resource group is defunct.
+
+    @returns true if resource group is defunct else false.
+  */
+
+  bool is_defunct() const { return m_defunct; }
+
+  /**
+    Method to mark resource group defunct.
+  */
+
+  void set_defunct() { m_defunct = true; }
 
   Thread_resource_control *controller() { return &m_thread_resource_control; }
 
@@ -103,6 +118,7 @@ class Resource_group {
   void add_pfs_thread_id(const ulonglong pfs_thread_id) {
     std::unique_lock<std::mutex> lock(m_set_mutex);
     (void)m_pfs_thread_id_set.insert(pfs_thread_id);
+    m_reference_count++;
   }
 
   /**
@@ -114,6 +130,7 @@ class Resource_group {
   void remove_pfs_thread_id(const ulonglong pfs_thread_id) {
     std::unique_lock<std::mutex> lock(m_set_mutex);
     (void)m_pfs_thread_id_set.erase(pfs_thread_id);
+    m_reference_count--;
   }
 
   /**
@@ -122,6 +139,7 @@ class Resource_group {
 
   void clear() {
     std::unique_lock<std::mutex> lock(m_set_mutex);
+    m_reference_count -= m_pfs_thread_id_set.size();
     (void)m_pfs_thread_id_set.clear();
   }
 
@@ -135,6 +153,8 @@ class Resource_group {
     std::unique_lock<std::mutex> lock(m_set_mutex);
     for (auto pfs_thread_id : m_pfs_thread_id_set) control_func(pfs_thread_id);
   }
+
+  std::atomic<ulonglong> &reference_count() { return m_reference_count; }
 
   ~Resource_group() = default;
 
@@ -155,6 +175,11 @@ class Resource_group {
   bool m_enabled;
 
   /**
+    Whether resource group is defunct or operative.
+  */
+  bool m_defunct{false};
+
+  /**
     Thread resource controller object.
   */
   Thread_resource_control m_thread_resource_control;
@@ -168,6 +193,16 @@ class Resource_group {
     Mutex protecting the resource group set.
   */
   std::mutex m_set_mutex;
+
+  /**
+    Count of threads referencing resource group. Count includes threads
+    associated with this resource group (i.e. threads in m_pfs_thread_id_set)
+    and other threads referencing this resource group (Only system threads
+    internally switched to refer user resource group to execute user queries
+    in some cases. User resource group maintains counter of even such
+    references.)
+  */
+  std::atomic<ulonglong> m_reference_count{0};
 
   /**
     Disable copy construction and assignment.
