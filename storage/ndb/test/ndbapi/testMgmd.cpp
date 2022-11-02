7d99939e7ce5b17ed54dc97c88ca82a7727cdf8b
Magnus Blåudd <magnus.blaudd@oracle.com>
2022-11-02 20:29:14 +0100

    Bug#34737568 Stray NDBD processes by testMgmd
    Start ndbd background process in foreground (without angel process) and
    thus killed the subprocess properly as long as testMgmd itself
    terminates gracefully.
    
    This is safe enough since in automated testing the testMgmd program will
    be run using myslqtest_safe_process guarding started processes and no
    stray ndbd processes was left behind even without fix.
    
    Change-Id: I201f59b471ae5156bafdf2fe39f13605ae9ee570

--- storage/ndb/test/ndbapi/testMgmd.cpp
+++ storage/ndb/test/ndbapi/testMgmd.cpp
@@ -395,7 +395,7 @@ public:
     args.add("-c");
     args.add(connect_string.c_str());
     args.add("--ndb-nodeid=", m_nodeid);
-    args.add("--nodaemon");
+    args.add("--foreground");
     return Mgmd::start(working_dir, args);
   }
 
