cade55bdb832026b4ee7c2a0a78d4aaa67b9ab66
Mayank Prasad <mayank.prasad@oracle.com>
2022-11-16 17:27:57 +0100

    Bug#34798706 : server not comming up after kill and restart
    Background:
      When we log DML operations on REDO log, to apply those changes during
      recovery, we need the index information. This index metadata is logged
      in REDO and read and index datastructure is regenerated at recovery.
    
    Issue :
      While parsing index infromation from REDO log, number of dropped
      columns was determined later in the flow. And at that time, n_fields
      and nullable_fields are to be adjusted for index. Which was missing.
    
    Fix: Adjusted n_fields and nullable_fields for index after number of
      dropped columns are determined.
    Change-Id: I0fc18b74fe7f94acc489d8526db1d27be1dc206b

--- storage/innobase/mtr/mtr0log.cc
+++ storage/innobase/mtr/mtr0log.cc
@@ -1101,6 +1101,10 @@ static void update_instant_info(instant_fields_list_t f, dict_index_t *index) {
     if (is_dropped) {
       col->set_version_dropped(field.v_dropped);
       n_dropped++;
+      if (col->is_nullable()) {
+        ut_a(index->n_nullable > 0);
+        --index->n_nullable;
+      }
     }
 
     if (is_added) {
@@ -1274,6 +1278,9 @@ byte *mlog_parse_index(byte *ptr, const byte *end_ptr, dict_index_t **index) {
     ind->row_versions = true;
   }
 
+  ind->n_fields = n - n_dropped;
+  ind->n_total_fields = n;
+
   /* For upgraded table from v1, set following */
   if (inst_cols > 0) {
     ind->instant_cols = true;
@@ -1282,8 +1289,6 @@ byte *mlog_parse_index(byte *ptr, const byte *end_ptr, dict_index_t **index) {
     ind->set_instant_nullable(new_n_nullable);
   }
 
-  ind->n_fields = n - n_dropped;
-  ind->n_total_fields = n;
   table->is_system_table = false;
 
   if (is_instant || is_versioned) {
