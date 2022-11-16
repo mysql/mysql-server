11bedc0f1cb4ebfb83f0fafe2b277bbcaf89558b
Tor Didriksen <tor.didriksen@oracle.com>
2022-11-16 13:45:11 +0100

    Bug #34805489 Add RTLD_NODELETE when loading components for ASAN builds
    For ASAN and LSAN builds:
    
    Do not unload the shared object during dlclose(). LeakSanitizer needs
    this in order to provide call stacks, and to match entries in
    lsan.supp.
    
    Change-Id: Iacc74423e5545afd46ad7e2650a308753fea0f9d
    (cherry picked from commit bfb6a6b66e3dc24b53539bcf7270c453e0bc674f)

--- components/libminchassis/dynamic_loader_scheme_file.cc
+++ components/libminchassis/dynamic_loader_scheme_file.cc
@@ -20,6 +20,8 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 
+#include "my_config.h"
+
 #include <mysql/components/component_implementation.h>
 #include <mysql/components/service_implementation.h>
 #include <mysql/components/services/dynamic_loader.h>
@@ -125,7 +127,14 @@ DEFINE_BOOL_METHOD(mysql_dynamic_loader_scheme_file_imp::load,
 #endif
 
     /* Open library. */
+#if defined(HAVE_ASAN) || defined(HAVE_LSAN)
+    // Do not unload the shared object during dlclose().
+    // LeakSanitizer needs this in order to provide call stacks,
+    // and to match entries in lsan.supp.
+    void *handle = dlopen(file_name.c_str(), RTLD_NOW | RTLD_NODELETE);
+#else
     void *handle = dlopen(file_name.c_str(), RTLD_NOW);
+#endif
     if (handle == nullptr) {
       const char *errmsg;
       int error_number = dlopen_errno;
