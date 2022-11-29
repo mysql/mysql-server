fd3a2598530242759b72973aabb0840c1238eeb9
Magnus Blåudd <magnus.blaudd@oracle.com>
2022-11-29 09:30:42 +0100

    Bug#34843412 Stale list of subscribers when ndb_schema dropped
    Schema distribution timeout is detected by the schema distribution
    coordinator after drop+create of "mysql.ndb_schema" table if some nodes
    that were subscribed before drop+create have not yet (re)subscribed when
    next schema operation is started. This is due to a stale list of
    subscribers being left behind in the schema distribution data.
    
    Fix by clearing the list of known subscribers when the
    "mysql.ndb_schema" table is dropped. Otherwise those subscribers are
    assumed by the coordinator to be participants in a subsequent schema
    operation.
    
    Change-Id: I358b57d5263b3f72ba71c38d78f5ced83b2ffc16

--- storage/ndb/plugin/ha_ndbcluster_binlog.cc
+++ storage/ndb/plugin/ha_ndbcluster_binlog.cc
@@ -1431,6 +1431,14 @@ class Ndb_schema_dist_data {
     }
   }
 
+  void report_unsubscribe_all() {
+    ndb_log_verbose(1, "Unsubscribe all subscribers");
+    for (const auto &it : m_subscriber_bitmaps) {
+      Node_subscribers *subscribers = it.second;
+      subscribers->clear_all();
+    }
+  }
+
   /**
      @brief Get list of current subscribers
      @note A node counts as subscribed as soon as any data node report it as
@@ -4130,6 +4138,7 @@ class Ndb_schema_event_handler {
 
       case NDBEVENT::TE_DROP:
         ndb_log_info("The 'mysql.ndb_schema' table has been dropped");
+        m_schema_dist_data.report_unsubscribe_all();
         handle_schema_table_drop(s_ndb, pOp);
         break;
 
