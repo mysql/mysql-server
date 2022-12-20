cabdb19d80e03ad1ad49a862f62ec6796bdf961e
Jan Kneschke <jan.kneschke@oracle.com>
2022-12-20 13:03:44 +0100

    Bug#34909071 Cluster Endpoint not cleared by the quarantine on Mac OS
    When the Router can't connect to one of the endpoints it puts it into
    quarantine. It should remove it from there once it is back (checked
    every interval which defaults to 1s).
    
    It works as intended on all platforms except MacOS.
    
    Background
    ==========
    
    Quarantine uses non-blocking connect() to check if an endpoint is
    reachable.
    
    On MacOS that non-blocking connect() is backed by:
    
      poll({fd=..., event=POLLOUT})
    
    but poll doesn't return an POLLOUT in case of error, but a POLLHUP which
    isn't handled.
    
    Change
    ======
    
    - If POLLOUT is requested, but POLLHUP is delivered: return
      POLLOUT|POLLHUP by the poll-io-service.
    
    Change-Id: I84dccf2cb80c1cc6b79de0087776464b44b945ac

--- router/tests/component/test_metadata_ttl.cc
+++ router/tests/component/test_metadata_ttl.cc
@@ -220,6 +220,60 @@ std::ostream &operator<<(std::ostream &os,
 
 }  // namespace std
 
+/**
+ * @test Checks that the quarantine works as expected with metadata-cache
+ * updates
+ */
+TEST_F(MetadataChacheTTLTest, Quarantine) {
+  auto cluster_node_classic_port = port_pool_.get_next_available();
+  auto cluster_node_http_port = port_pool_.get_next_available();
+  const std::string json_metadata =
+      get_data_dir().join("metadata_dynamic_nodes_v2_gr.js").str();
+  auto &cluster_node =
+      launch_mysql_server_mock(json_metadata, cluster_node_classic_port,
+                               EXIT_SUCCESS, false, cluster_node_http_port);
+  set_mock_metadata(cluster_node_http_port, "uuid", {cluster_node_classic_port},
+                    0, {cluster_node_classic_port});
+
+  const auto router_port = port_pool_.get_next_available();
+  const std::string metadata_cache_section =
+      get_metadata_cache_section(ClusterType::GR_V2, "0.2");
+  const std::string routing_section = get_metadata_cache_routing_section(
+      router_port, "PRIMARY", "first-available");
+
+  auto &router = launch_router(metadata_cache_section, routing_section,
+                               {cluster_node_classic_port}, EXIT_SUCCESS,
+                               /*wait_for_notify_ready=*/30s);
+  EXPECT_TRUE(wait_for_transaction_count_increase(cluster_node_http_port, 2));
+  make_new_connection_ok(router_port, cluster_node_classic_port);
+
+  SCOPED_TRACE(
+      "// kill the cluster node and wait for it to be added to quarantine");
+  EXPECT_NO_THROW(cluster_node.kill());
+  check_exit_code(cluster_node, EXIT_SUCCESS, 5s);
+
+  SCOPED_TRACE("// connect and trigger a quarantine");
+  verify_new_connection_fails(router_port);
+  EXPECT_TRUE(wait_log_contains(router,
+                                "add destination '127.0.0.1:" +
+                                    std::to_string(cluster_node_classic_port) +
+                                    "' to quarantine",
+                                1s));
+
+  SCOPED_TRACE("// bring back the cluster node");
+  launch_mysql_server_mock(json_metadata, cluster_node_classic_port,
+                           EXIT_SUCCESS, false, cluster_node_http_port);
+  set_mock_metadata(cluster_node_http_port, "uuid", {cluster_node_classic_port},
+                    0, {cluster_node_classic_port});
+
+  SCOPED_TRACE("// .. and wait for it to be cleared by the quarantine");
+  EXPECT_TRUE(wait_log_contains(router,
+                                "Destination candidate '127.0.0.1:" +
+                                    std::to_string(cluster_node_classic_port) +
+                                    "' is available, remove it from quarantine",
+                                10s));
+}
+
 class MetadataChacheTTLTestParam
     : public MetadataChacheTTLTest,
       public ::testing::WithParamInterface<MetadataTTLTestParams> {};
