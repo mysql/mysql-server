23d91ef544eecc059f722db323c83c8601dc7685
Knut Anders Hatlen <knut.hatlen@oracle.com>
2023-01-25 13:14:30 +0100

    Bug#34861841: Assertion : `false' in (anonymous namespace)::CostingReceiver::ProposeAccessPath
    When executing a CREATE TABLE AS SELECT statement with the hypergraph
    optimizer enabled, LEX::using_hypergraph_optimizer was not set until
    the SELECT statement was executed. However, the flag needs to be set
    before resolving the statement, as the resolver uses the flag to
    decide whether a particular semijoin transformation should be
    performed. The hypergraph optimizer does not support this
    transformation, and assert failures were seen during optimization if
    the resolver had performed the transformation.
    
    Fixed by setting the using_hypergraph_optimizer flag before preparing
    the CREATE TABLE AS SELECT statement.
    
    Change-Id: I643fb8b797742651c8cdfa9b279e51e200c5a868

--- sql/sql_cmd_ddl_table.cc
+++ sql/sql_cmd_ddl_table.cc
@@ -101,10 +101,6 @@ static bool populate_table(THD *thd, LEX *lex) {
   if (lex->set_var_list.elements && resolve_var_assignments(thd, lex))
     return true;
 
-  // Use the hypergraph optimizer for the SELECT statement, if enabled.
-  lex->using_hypergraph_optimizer =
-      thd->optimizer_switch_flag(OPTIMIZER_SWITCH_HYPERGRAPH_OPTIMIZER);
-
   lex->set_exec_started();
 
   /*
@@ -368,6 +364,11 @@ bool Sql_cmd_create_table::execute(THD *thd) {
         lex->link_first_table_back(create_table, link_to_local);
         return true;
       }
+
+      // Use the hypergraph optimizer for the SELECT statement, if enabled.
+      lex->using_hypergraph_optimizer =
+          thd->optimizer_switch_flag(OPTIMIZER_SWITCH_HYPERGRAPH_OPTIMIZER);
+
       if (query_expression->prepare(thd, result, nullptr, SELECT_NO_UNLOCK,
                                     0)) {
         lex->link_first_table_back(create_table, link_to_local);
