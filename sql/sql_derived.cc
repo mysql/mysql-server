463608249c245c09b71aa55da3022eac95828a0f
Chaithra Gopalareddy <chaithra.gopalareddy@oracle.com>
2023-01-31 11:57:41 +0530

    Bug#34996488:utf8mb4 identifiers stopped working              since 8.0.32 release
    When cloning a condition to push down to a derived
    table, the method used to print the string in
    Item_string::print() for reparsing the condition,
    assumes that the target character set is already
    set in the character string buffer. In this case,
    it is set to the binary character set(default),
    and the consequence is that the characters are
    correctly converted only for values less than 128,
    i.e. the ASCII subset. Thus, any code point that is
    outside the ASCII subset will be converted to an
    invalid UTF8 character and will cause the resulting
    character string to become invalid.
    For derived tables without UNIONS, it created
    problems when a column name from the derived table
    had characters outside of the ASCII subset and was
    used in the WHERE condition (since we clone only the
    columns that are part of the WHERE condition in this
    case). For derived tables, with UNIONS, it created
    problems when a non-ASCII character was present in
    WHERE condition (since we clone the entire condition
    in this case).
    We fix it by initializing the string used for
    printing the condition to the connection charset.
    
    Change-Id: If741e7405281735e74d8c50029d63059aaca7493

--- sql/sql_derived.cc
+++ sql/sql_derived.cc
@@ -583,7 +583,7 @@ static Item *parse_expression(THD *thd, Item *item, Query_block *query_block) {
   // Also do not write a cloned stored procedure variable to query logs.
   thd->lex->reparse_derived_table_condition = true;
   // Get the printout of the expression
-  StringBuffer<1024> str;
+  StringBuffer<1024> str(thd->charset());
   // For printing parameters we need to specify the flag QT_NO_DATA_EXPANSION
   // because for a case when statement gets reprepared during execution, we
   // still need Item_param::print() to print the '?' rather than the actual data
