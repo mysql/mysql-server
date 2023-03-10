9a085353f14c4f8383c7684a1b4edfbdc3eab8dc
V S Murthy Sidagam <venkata.sidagam@oracle.com>
2023-03-10 10:04:13 +0100

    Bug#35054579 Issue in Oracle MySQL Client using utf16 charset
    Back porting the bug to 5.7 branch.
    
    Change-Id: I06810ab5d85ac6f65b94ff1d12bc116cf40d7fa1

--- include/errmsg.h
+++ include/errmsg.h
@@ -115,7 +115,8 @@ extern const char *client_errors[];	/* Error messages */
 #define CR_DUPLICATE_CONNECTION_ATTR            2060
 #define CR_AUTH_PLUGIN_ERR                      2061
 #define CR_INSECURE_API_ERR                     2062
-#define CR_ERROR_LAST  /*Copy last error nr:*/  2062
+#define CR_INVALID_CLIENT_CHARSET               2063
+#define CR_ERROR_LAST  /*Copy last error nr:*/  2063
 /* Add error numbers before CR_ERROR_LAST and change it accordingly. */
 
 #endif /* ERRMSG_INCLUDED */
