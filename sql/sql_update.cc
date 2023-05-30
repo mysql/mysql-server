3d7b649a55bc9846eb612c6cf2f297ae75d8c679
Dag Wanvik <dag.wanvik@oracle.com>
2023-05-30 18:57:56 +0200

    Bug#35442407 Server exit when executing query
    The server now exits where we used to see an error message.  The
    refactoring performed in commit:
    
      WL#14672: Enable the hypergraph optimizer for UPDATE [7/8, single-table]
    
    has (presumably inadvertently) inverted the logic when we check for
    the presence of ORDER BY and LIMIT with multi-table updates. It seems
    clear that we should give an error message if ORDER BY or LIMIT is
    combined with a de facto multi table update, whether it syntactically
    looks like one, or it is hidden inside a view definition.
    
    The solution is to invert the test back, so that these checks are
    performed.
    
    The fix solves Bug#35438510.
    
    Change-Id: I662775324cc9738e283d298989572077ac8a696d

--- sql/sql_update.cc
+++ sql/sql_update.cc
@@ -1748,10 +1748,11 @@ bool Sql_cmd_update::prepare_inner(THD *thd) {
   /* @todo: downgrade the metadata locks here. */
 
   /*
-    Syntax rule for multi-table update prevents these constructs.
-    But they are possible for single-table UPDATE against multi-table view.
+    ORDER BY and LIMIT is only allowed for single table update, so check. Even
+    through it looks syntactically like a single table update, it may still be
+    a multi-table update hidden if we update a multi-table view.
   */
-  if (is_single_table_syntax && table_list->is_multiple_tables()) {
+  if (!is_single_table_syntax || table_list->is_multiple_tables()) {
     if (select->order_list.elements) {
       my_error(ER_WRONG_USAGE, MYF(0), "UPDATE", "ORDER BY");
       return true;
