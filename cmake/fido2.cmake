ecd8357a6094d1beedcd9e747b234220639200cc
Harin Vadodaria <harin.vadodaria@oracle.com>
2023-08-09 11:31:03 +0200

    Bug#35685515: Upgrade libfido2 to v1.13.0
    Description:
    cmake/fido2.cmake
      - Changed FIDO_VERSION
      - Added OpenSSL version check
      - Added support for value "none" for WITH_FIDO
      - Added fido2 and cbor bundled source version information
      - Changed FIDO_BUNDLE_SRC_PATH, CBOR_BUNDLE_SRC_PATH
      - Added check for OpenSSL version
    
    extra/libfido2/libfido2-1.13.0/CMakeLists.txt
      - disable cmake_minimum_required and policy settings
      - disable PIE and PIC settings
      - disable Makefile preferences
      - set all BUILD options to OFF
      - disable quick exit path for BUILD_*_LIBS=OFF
      - disable some compiler warnings for APPLE
      - disable some checks for misc INCLUDE_DIRS
      - change main include_directories from ${CMAKE_SOURCE_DIR}/src
        to ${CMAKE_CURRENT_SOURCE_DIR}/src
      - add some CBOR includes
      - disable several message(STATUS ....)
      - disable build in regress/ subdirectory.
    
    extra/libfido2/libfido2-1.13.0/src/CMakeLists.txt
      - add our own fido2 shared library target
        with misc additional PATH properties.
      - disable INSTALL of header files, and libfido2.pc
      - Set zlib if missing
      - Silenced OpenSSL 3 deprecated API usage errors
    
    Removed extra/libfido2/libfido2-1.13.0/fuzz/Makefile
    
    extra/libcbor/libcbor-0.10.2/CMakeLists.txt
      - Disable cmake_minimum_required
      - Added policy CMP0048
      - Added -Wdocumentation, -Wstrict-prototypes and
        -Wreturn-type suppressions
      - Removed CBOR_CUSTOM_ALLOC
      - Removed SANITIZE
      - Set COVERAGE to OFF
      - Set CBOR_PRETTY_PRINTER to OFF
      - Removed build instructions for tests and examples
    
    extra/libcbor/libcbor-0.10.2/src/CMakeLists.txt
      - Updated to build static library
    
    Removed extra/libcbor/libcbor-0.10.2/doc
    
    Update RPM spec files
      - Conditional inclusion of libfido2 and fido2 auth plugins
    
    Change-Id: I14ad7a17f0c2cabaa9123a567199388d43b41a8d

--- cmake/fido2.cmake
+++ cmake/fido2.cmake
@@ -25,10 +25,12 @@
 # Version 1.3.1 on Ubuntu 20.04 does *not* work.
 # Version 1.4.0 on Fedora 33 passes all tests.
 SET(MIN_FIDO_VERSION_REQUIRED "1.4.0")
+SET(FIDO_BUNDLED_VERSION "1.13.0")
+SET(CBOR_BUNDLED_VERSION "0.10.2")
 
 MACRO(FIND_FIDO_VERSION)
   IF(WITH_FIDO STREQUAL "bundled")
-    SET(FIDO_VERSION "1.8.0")
+    SET(FIDO_VERSION "${FIDO_BUNDLED_VERSION}")
   ELSE()
     # This does not set any version information:
     # PKG_CHECK_MODULES(SYSTEM_FIDO fido2)
@@ -143,8 +145,10 @@ FUNCTION(FIND_SYSTEM_FIDO)
   ENDIF()
 ENDFUNCTION(FIND_SYSTEM_FIDO)
 
-SET(FIDO_BUNDLE_SRC_PATH "extra/libfido2/libfido2-1.8.0")
-SET(CBOR_BUNDLE_SRC_PATH "extra/libcbor")
+SET(FIDO_BUNDLE_SRC_PATH "extra/libfido2/libfido2-")
+STRING_APPEND(FIDO_BUNDLE_SRC_PATH "${FIDO_BUNDLED_VERSION}")
+SET(CBOR_BUNDLE_SRC_PATH "extra/libcbor/libcbor-")
+STRING_APPEND(CBOR_BUNDLE_SRC_PATH "${CBOR_BUNDLED_VERSION}")
 
 FUNCTION(MYSQL_USE_BUNDLED_FIDO)
 
@@ -179,29 +183,52 @@ FUNCTION(MYSQL_USE_BUNDLED_FIDO)
 ENDFUNCTION(MYSQL_USE_BUNDLED_FIDO)
 
 MACRO(MYSQL_CHECK_FIDO)
-  IF (NOT WITH_FIDO)
-    SET(WITH_FIDO "bundled"
-      CACHE STRING "By default use bundled libfido2.")
-  ENDIF()
-
-  IF(WITH_FIDO STREQUAL "bundled")
-    MYSQL_USE_BUNDLED_FIDO()
-  ELSEIF(WITH_FIDO STREQUAL "system")
-    FIND_SYSTEM_FIDO()
-    IF(NOT FIDO_FOUND)
-      MESSAGE(WARNING "Cannot find system fido2 libraries/headers.")
-    ENDIF()
+  IF("${OPENSSL_MAJOR_MINOR_FIX_VERSION}" VERSION_GREATER "1.1.0")
+    SET(OPENSSL_IS_COMPATIBLE_WITH_BUNDLED_FIDO ON)
   ELSE()
-    MESSAGE(WARNING "WITH_FIDO must be bundled or system")
+    SET(OPENSSL_IS_COMPATIBLE_WITH_BUNDLED_FIDO OFF)
+    SET(WITH_AUTHENTICATION_FIDO OFF)
   ENDIF()
 
-  FIND_FIDO_VERSION()
-  IF(FIDO_VERSION VERSION_LESS MIN_FIDO_VERSION_REQUIRED)
-    MESSAGE(FATAL_ERROR
-      "FIDO version must be at least ${MIN_FIDO_VERSION_REQUIRED}, "
-      "found ${FIDO_VERSION}.\nPlease use -DWITH_FIDO=bundled")
+  IF (NOT WITH_FIDO)
+    IF(OPENSSL_IS_COMPATIBLE_WITH_BUNDLED_FIDO)
+      SET(WITH_FIDO "bundled"
+        CACHE STRING "By default use bundled libfido2.")
+    ELSE()
+      SET(WITH_FIDO "none"
+        CACHE STRING "Do not compile libfido2 for OpenSSL < 1.1.1")
+    ENDIF()
   ENDIF()
-  IF(FIDO_FOUND)
-    ADD_LIBRARY(ext::fido ALIAS fido_interface)
+
+  IF(WITH_FIDO STREQUAL "none")
+    MESSAGE(WARNING
+      "WITH_FIDO is set to \"none\". \
+       FIDO based authentication plugins will be skipped.")
+  ELSE()
+    IF(WITH_FIDO STREQUAL "bundled")
+      IF(OPENSSL_IS_COMPATIBLE_WITH_BUNDLED_FIDO)
+        MYSQL_USE_BUNDLED_FIDO()
+      ELSE()
+        MESSAGE(FATAL_ERROR
+          "Bundled libfido2 requires OpenSSL version >= 1.1.1")
+      ENDIF()
+    ELSEIF(WITH_FIDO STREQUAL "system")
+      FIND_SYSTEM_FIDO()
+      IF(NOT FIDO_FOUND)
+        MESSAGE(WARNING "Cannot find system fido2 libraries/headers.")
+      ENDIF()
+    ELSE()
+      MESSAGE(WARNING "WITH_FIDO must be none, bundled or system")
+    ENDIF()
+
+    FIND_FIDO_VERSION()
+    IF(FIDO_VERSION VERSION_LESS MIN_FIDO_VERSION_REQUIRED)
+      MESSAGE(FATAL_ERROR
+        "FIDO version must be at least ${MIN_FIDO_VERSION_REQUIRED}, "
+        "found ${FIDO_VERSION}.\nPlease use -DWITH_FIDO=bundled")
+    ENDIF()
+    IF(FIDO_FOUND)
+      ADD_LIBRARY(ext::fido ALIAS fido_interface)
+    ENDIF()
   ENDIF()
 ENDMACRO()
