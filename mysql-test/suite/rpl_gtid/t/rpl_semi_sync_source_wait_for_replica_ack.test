mysql/mysql-server

commit 650d2f7bf275e40fd605df39ab09949aa54f5e3f

Bug#33534218 : replication_sender_observe_commit_only=ON leads to break semisync replication
Problem
-------

When setting replication_sender_observe_commit_only into semisync-Source node
in GTID mode, its replica cannot not start semisync-replication after restart
of the mysqld.

Analysis / Root-cause analysis
------------------------------

Additional optimizations implemented in the Observe_transmission_guard
class, which turns off the m_observe_transmission flag in specific cases, turn
off execution of the following hooks: before_send_hook, after_send_hook and
reserve_header_hook. When GTIDs are used and the auto position protocol is
executed, additional heartbeat events are sent to skip transactions and update
log positions. However, reserve_header_hook is not executed and magic number
is not properly encoded in the outgoing message. Replica receives message with
unknown magic number and reports the error.

Solution
--------

Observe_transmission_guard, including before_send_hook and after_send_hook,
apply to outgoing messages with events, therefore:
1. Observe_transmission_guard is created before a call to before_send_hook.
2. The before_send_hook is called before a call to send_packet function.
3. The after_send_hook is called just after execution of the send_packet
function.
This way, reserve_header_hook is always called when sending a heartbeat event
and magic number is properly encoded. Moreover, before_send_hook and
after_send_hook are never called in case the event is in the exclude
group and packet is not sent.

Signed-off-by: Karolina Szczepankiewicz <karolina.szczepankiewicz@oracle.com>
Change-Id: I86309b23447b9745b2f3862626dc8a129b923d41

sql/rpl_binlog_sender.cc
@@ -608,14 +608,9 @@ int Binlog_sender::send_events(File_reader &reader, my_off_t end_pos) {
     });
 
     Sender_context_guard ctx_guard(*this, event_type);
-    Observe_transmission_guard obs_guard(
-        m_observe_transmission, event_type,
-        const_cast<const char *>(reinterpret_cast<char *>(event_ptr)),
-        m_event_checksum_alg, m_prev_event_type);
 
     log_pos = reader.position();
 
-    if (before_send_hook(log_file, log_pos)) return 1;
     /*
       TODO: Set m_exclude_gtid to NULL if all gtids in m_exclude_gtid has
       be skipped. and maybe removing the gtid from m_exclude_gtid will make
@@ -661,11 +656,16 @@ int Binlog_sender::send_events(File_reader &reader, my_off_t end_pos) {
         m_packet.length(tmp.length());
       }
 
-      if (unlikely(send_packet())) return 1;
-    }
+      Observe_transmission_guard obs_guard(
+          m_observe_transmission, event_type,
+          const_cast<const char *>(reinterpret_cast<char *>(event_ptr)),
+          m_event_checksum_alg, m_prev_event_type);
 
-    if (unlikely(after_send_hook(log_file, in_exclude_group ? log_pos : 0)))
-      return 1;
+      if (before_send_hook(log_file, log_pos)) return 1;
+      if (unlikely(send_packet())) return 1;
+      if (unlikely(after_send_hook(log_file, in_exclude_group ? log_pos : 0)))
+        return 1;
+    }
   }
 
   /*

mysql-test/include/spawn_monitoring_process.inc
@@ -37,6 +37,16 @@ if (!$have_windows) {
    OR VARIABLE_NAME LIKE 'enforce_gtid_consistency'
    OR VARIABLE_NAME LIKE 'gtid_mode'
    ORDER BY VARIABLE_NAME;
+
+  if(!$rpl_group_replication) {
+    INSERT INTO test.r_vars (var_name, var_value)
+     SELECT * FROM performance_schema.global_variables
+     WHERE VARIABLE_NAME LIKE 'relay_log'
+     OR VARIABLE_NAME LIKE 'rpl_semi_sync_replica_enabled'
+     OR VARIABLE_NAME LIKE 'rpl_semi_sync_source_enabled'
+     ORDER BY VARIABLE_NAME;
+  }
+
   if($rpl_group_replication)
   {
     --let $gr_ssl_disabled= `SELECT @@group_replication_ssl_mode LIKE 'DISABLED'`

mysql-test/suite/rpl/r/rpl_semi_sync_turn_on_off_optimize_for_static_plugin_config.result
@@ -31,6 +31,7 @@ SET GLOBAL replication_sender_observe_commit_only = 1;
 SHOW VARIABLES LIKE 'rpl_semi_sync_source_enabled';
 Variable_name	Value
 CALL mtr.add_suppression("Semi-sync master failed on net_flush.*");
+CALL mtr.add_suppression(".* Timeout waiting for reply of binlog .*");
 include/install_semisync_source.inc
 [connection server_3]
 SET GLOBAL replication_optimize_for_static_plugin_config = 1;

mysql-test/suite/rpl/t/rpl_semi_sync_turn_on_off_optimize_for_static_plugin_config.test
@@ -67,6 +67,7 @@ while ($i <= $servers)
   {
     SHOW VARIABLES LIKE 'rpl_semi_sync_source_enabled';
     CALL mtr.add_suppression("Semi-sync master failed on net_flush.*");
+    CALL mtr.add_suppression(".* Timeout waiting for reply of binlog .*");
     --source include/install_semisync_source.inc
   }
   if ($i != 2)

mysql-test/suite/rpl_gtid/r/rpl_semi_sync_observe_commit_only.result
@@ -1,0 +1,46 @@
+# 1. Set-up semi sync replication environment
+# a) Setup a 2 layered topology with the given topology 1->2
+include/rpl_init.inc [topology=1->2]
+Warnings:
+Note	####	Sending passwords in plain text without SSL/TLS is extremely insecure.
+Note	####	Storing MySQL user name or password information in the master info repository is not secure and is therefore not recommended. Please consider using the USER and PASSWORD connection options for START SLAVE; see the 'START SLAVE Syntax' in the MySQL Manual for more information.
+[connection server_1]
+SET GLOBAL replication_sender_observe_commit_only = 1;
+SET GLOBAL replication_optimize_for_static_plugin_config = 1;
+CALL mtr.add_suppression("Semi-sync master failed on net_flush.*");
+CALL mtr.add_suppression("Read semi-sync reply magic number error.*");
+CALL mtr.add_suppression("A message intended for a client cannot be sent there as no client-session is attached.*");
+CALL mtr.add_suppression("Timeout waiting for reply of binlog.*");
+include/install_semisync_source.inc
+[connection server_2]
+include/install_semisync_replica.inc
+CALL mtr.add_suppression("The slave coordinator and worker threads are stopped.*");
+# b) Ensure that replication_sender_observe_commit_only is turned on
+[connection server_1]
+# replication_sender_observe_commit_only: 1
+include/assert.inc [replication_sender_observe_commit_only should be ON (source)]
+# 2. Prepare a testing environment and execute statements in the background
+CREATE TABLE t (a int);
+# 3. Issue the replica restart while the source is running statements
+[connection server_2]
+include/rpl_restart_server.inc [server_number=2 gtids=on parameters: --skip_slave_start=0 --rpl_semi_sync_replica_enabled=1]
+# 4. Sanity check - check whether semi-sync replication was correctly restarted
+include/check_slave_is_running.inc
+include/check_slave_no_error.inc
+include/assert.inc [Semi sync should be enabled on the replica]
+# 5. Wait for source to finish the execution of statements
+[connection server_1]
+# 6. Wait for replica to catch-up with latest changes
+[connection server_2]
+include/rpl_sync.inc
+# 7. Check whether the replica replicated table correctly
+include/diff_tables.inc [server_1:t, server_2:t]
+# 8. Clean up
+[connection server_2]
+include/uninstall_semisync_replica.inc
+[connection server_1]
+SET GLOBAL replication_sender_observe_commit_only = OFF;
+SET GLOBAL replication_optimize_for_static_plugin_config = OFF;
+include/uninstall_semisync_source.inc
+DROP TABLE t;
+include/rpl_end.inc

mysql-test/suite/rpl_gtid/t/rpl_semi_sync_observe_commit_only-master.opt
@@ -1,0 +1,1 @@
+$SEMISYNC_PLUGIN_OPT

mysql-test/suite/rpl_gtid/t/rpl_semi_sync_observe_commit_only-slave.opt
@@ -1,0 +1,1 @@
+$SEMISYNC_PLUGIN_OPT

mysql-test/suite/rpl_gtid/t/rpl_semi_sync_observe_commit_only.cnf
@@ -1,0 +1,9 @@
+!include ../my.cnf
+
+[mysqld.1]
+log-replica-updates
+server_id=1
+
+[mysqld.2]
+log-replica-updates
+server_id=2

commit e8a7326b4f4e6c1f27617b4f7a7a2edc279bf73d

Bug#110127: semi-sync master wait for ack when semi-sync replica is down             & async replaces it
https://bugs.mysql.com/bug.php?id=110127

Description
-----------
This is the regression introduced in the commit 650d2f7
(Bug#33534218 : replication_sender_observe_commit_only=ON leads to break semisync replication)
which made the before_send_hook and after_send_hook to be never called if the
event is in the excluded group.

Prior to 8.0.31, dump thread performed below operations
1. Read events from binlog
2. For every event
   2.1. Call before send hook
   2.2. If the event is skipped
        2.2.1. Send hearbeat event if necessary with the latest position
        2.2.2. Update the exclude_end_pos to mark the position in binlog till
               where the replica receiver is synchronized.
   2.3. If the event is not skipped
        2.3.1. Send heartbeat event if necessary with the last
               exclude_group_end_pos.
        2.3.2. Call send_packet() to send events to replica.
   2.4. Call after send hook

This way it ensured that before_send and after_send hooks are executed
irrespective of whether the event is skipped or not.

In 8.0.31 upto 8.0.34,
1. Read events from binlog
2. For every event
   2.1. If the event is skipped
        2.2.1. Send hearbeat event if necessary with the latest position
        2.2.2. Update the exclude_end_pos to mark the position in binlog till
               where the replica receiver is synchronized.
   2.2. If the event is not skipped
        2.3.1. Send heartbeat event if necessary with the last
               exclude_group_end_pos.
        2.3.2. Call before send hook
        2.3.3. Call send_packet() to send events to replica.
        2.3.4. Call after send hook

Here, the above upstream commit made the before_send_hook and after_send_hook
to be called only when an event is not skipped.

However this change caused the client connection thread to wait for the ACK

sql/rpl_binlog_sender.cc
@@ -625,6 +625,8 @@ int Binlog_sender::send_events(File_reader &reader, my_off_t end_pos) {
       auto now = now_in_nanosecs();
       assert(now >= m_last_event_sent_ts);
 
+      if (before_send_hook(log_file, log_pos)) return 1;
+
       // if enough time has elapsed so that we should send another heartbeat
       if (m_heartbeat_period > std::chrono::nanoseconds(0) &&
           (now - m_last_event_sent_ts) >= m_heartbeat_period) {
@@ -633,6 +635,10 @@ int Binlog_sender::send_events(File_reader &reader, my_off_t end_pos) {
       } else {
         exclude_group_end_pos = log_pos;
       }
+
+      if (unlikely(after_send_hook(log_file, in_exclude_group ? log_pos : 0)))
+        return 1;
+
       DBUG_PRINT("info", ("Event of type %s is skipped",
                           Log_event::get_type_str(event_type)));
     } else {

mysql-test/suite/rpl_gtid/r/rpl_semi_sync_source_wait_for_replica_ack.result
@@ -1,0 +1,48 @@
+include/master-slave.inc
+Warnings:
+Note	####	Sending passwords in plain text without SSL/TLS is extremely insecure.
+Note	####	Storing MySQL user name or password information in the connection metadata repository is not secure and is therefore not recommended. Please consider using the USER and PASSWORD connection options for START REPLICA; see the 'START REPLICA Syntax' in the MySQL Manual for more information.
+[connection master]
+#
+# 1) Setup semisync replication
+include/install_semisync.inc
+#
+# 2) Set the rpl_semi_sync_source_timeout on source to a large value.
+[connection master]
+SET @saved_rpl_semi_sync_source_timeout = @@GLOBAL.rpl_semi_sync_source_timeout;
+SET GLOBAL rpl_semi_sync_source_timeout = 36000000;
+#
+# 3) Switch the semi-sync replication on replica to async by restarting
+#    replica threads with rpl_semi_sync_replica_enabled=OFF
+[connection slave]
+SET GLOBAL rpl_semi_sync_replica_enabled=OFF;
+STOP REPLICA;
+START REPLICA;
+#
+# 4) Execute a transaction on source. This should wait for some time for the
+#    replica's acknowledgement.
+[connection master]
+CREATE DATABASE test1;
+[connection master1]
+#
+# 5) Switch the async replication on replica to semi-sync by restarting
+#    replica threads with rpl_semi_sync_replica_enabled=ON. When replica
+#    threads are started the waiting transaction on source must proceed.
+[connection slave]
+SET GLOBAL rpl_semi_sync_replica_enabled = ON;
+STOP REPLICA;
+START REPLICA;
+[connection master1]
+[connection master]
+#
+# 6) Verify that the transaction on source server has finished and the
+#    statement has been replicated.
+include/rpl_sync.inc
+include/rpl_diff.inc
+#
+# 7) Cleanup
+[connection master]
+DROP DATABASE test1;
+SET GLOBAL rpl_semi_sync_source_timeout = @saved_rpl_semi_sync_source_timeout;
+include/uninstall_semisync.inc
+include/rpl_end.inc

mysql-test/suite/rpl_gtid/t/rpl_semi_sync_source_wait_for_replica_ack-master.opt
@@ -1,0 +1,1 @@
+$SEMISYNC_PLUGIN_OPT

mysql-test/suite/rpl_gtid/t/rpl_semi_sync_source_wait_for_replica_ack-slave.opt
@@ -1,0 +1,1 @@
+$SEMISYNC_PLUGIN_OPT

mysql-test/suite/rpl_gtid/t/rpl_semi_sync_source_wait_for_replica_ack.test
@@ -1,0 +1,84 @@
+################################################################################
+# PS-8869: semi-sync master waits for ack when semi-sync replica is down & when
+# we replace it with async
+#
+# Steps to reproduce:
+#  1) Setup semisync replication
+#  2) Set the rpl_semi_sync_source_timeout on source to a large value.
+#  3) Switch the semi-sync replication on replica to async by restarting
+#     replica threads with rpl_semi_sync_replica_enabled=OFF
+#  4) Execute a transaction on source. This should wait for some time for the
+#     replica's acknowledgement.
+#  5) Switch the async replication on replica to semi-sync by restarting
+#     replica threads with rpl_semi_sync_replica_enabled=ON. When replica
+#     threads are started the waiting transaction on source must proceed.
+#  6) Verify that the transaction on source server has finished and the
+#     statement has been replicated.
+#  7) Cleanup
+################################################################################
+
+# Test is independent of Binlog format. One of the three formats is enough
+# for testing. Choosing 'Row' format.
+--source include/have_binlog_format_row.inc
+--source include/not_group_replication_plugin.inc
+--source include/master-slave.inc
+
+--echo #
+--echo # 1) Setup semisync replication
+--source include/install_semisync.inc
+
+--echo #
+--echo # 2) Set the rpl_semi_sync_source_timeout on source to a large value.
+--source include/rpl_connection_master.inc
+SET @saved_rpl_semi_sync_source_timeout = @@GLOBAL.rpl_semi_sync_source_timeout;
+SET GLOBAL rpl_semi_sync_source_timeout = 36000000;
+
+--echo #
+--echo # 3) Switch the semi-sync replication on replica to async by restarting
+--echo #    replica threads with rpl_semi_sync_replica_enabled=OFF
+--source include/rpl_connection_slave.inc
+SET GLOBAL rpl_semi_sync_replica_enabled=OFF; STOP REPLICA; START REPLICA;
+
+--echo #
+--echo # 4) Execute a transaction on source. This should wait for some time for the
+--echo #    replica's acknowledgement.
+--source include/rpl_connection_master.inc
+--send CREATE DATABASE test1
+
+# Sleep for some time to ensure that that the query still keeps waiting for the semi-sync ACK from replica
+--sleep 5
+--source include/rpl_connection_master1.inc
+--let $wait_condition= SELECT State ="Waiting for semi-sync ACK from replica" FROM INFORMATION_SCHEMA.PROCESSLIST WHERE INFO LIKE "CREATE DATA%"
+--source include/wait_condition_or_abort.inc
+
+--echo #
+--echo # 5) Switch the async replication on replica to semi-sync by restarting
+--echo #    replica threads with rpl_semi_sync_replica_enabled=ON. When replica
+--echo #    threads are started the waiting transaction on source must proceed.
+--source include/rpl_connection_slave.inc
+SET GLOBAL rpl_semi_sync_replica_enabled = ON; STOP REPLICA; START REPLICA;
+
+--source include/rpl_connection_master1.inc
+--let $wait_condition= SELECT COUNT(*)=1 FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = "test1"
+--source include/wait_condition_or_abort.inc
+
+--source include/rpl_connection_master.inc
+--reap
+
+--echo #
+--echo # 6) Verify that the transaction on source server has finished and the
+--echo #    statement has been replicated.
+--source include/rpl_sync.inc
+--let $wait_condition= SELECT COUNT(*)=1 FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = "test1"
+--source include/wait_condition_or_abort.inc
+
+--let $rpl_diff_statement= SELECT * FROM INFORMATION_SCHEMA.SCHEMATA
+--source include/rpl_diff.inc
+
+--echo #
+--echo # 7) Cleanup
+--source include/rpl_connection_master.inc
+DROP DATABASE test1;
+SET GLOBAL rpl_semi_sync_source_timeout = @saved_rpl_semi_sync_source_timeout;
+--source include/uninstall_semisync.inc
+--source include/rpl_end.inc