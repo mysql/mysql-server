75f3d8e3e7955bf282fb14ca50154f6f6c0adb28
Kent Boortz <kent.boortz@oracle.com>
2023-08-23 22:11:19 +0200

    Bug#35613791 - Upgrade from Wix 3.11 to 4.0.1 for Server/Cluster MSI builds
    WiX Toolset v3 is no longer actively maintained. Other new and old
    bugs corrected:
    
    Bug#35729602 - MEB msi does not contains the MySQL image like others msi.
    Bug#35734657 - Server MSI installing in "C:\Program Files (x86)" instead of "C:\Program Files"
    
    Change-Id: I08922db98fd7234ffab57e5d05f1c3cb6e297ae8

--- packaging/WiX/configure_file.cmake
+++ packaging/WiX/configure_file.cmake
@@ -20,4 +20,4 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
 
-CONFIGURE_FILE(${INPUT_FILE} ${OUTPUT_FILE})
+CONFIGURE_FILE(${INPUT_FILE} ${OUTPUT_FILE} @ONLY)
