f9699d37b4d7a22cbcdccdbf513690958e5cf1ea
Nemanja Stojoski <nemanja.stojoski@oracle.com>
2023-08-30 09:49:02 +0000

    WL#15811: [InnoDB Changes] HeatWave Statically Guided Load
    This patch introduces a new parameter 'max_desired_threads' in the
    parallel_scan_init() function signature, which is needed as a
    prerequisite for the Statically Guided Load worklog.
    
    'max_desired_threads' allows callers of parallel_scan_init() to directly
    limit the number of read threads they need, instead of relying on
    something external to specify that limit. Passing 0 for this argument
    has no effect.
    
    Specifically in the InnoDB plugin, the read thread limit is controlled
    by the 'innodb_parallel_read_threads' variable. With this patch, the
    number of read threads can be set lower than the current value of the
    session variable by providing a value to 'max_desired_threads' argument
    of parallel_scan_init().
    
    An MTR test in the rapid suite is added to test this behavior.
    
    Change-Id: Ida397c0ef9cd505186cb7006b1f180688bfb587d

--- storage/innobase/handler/ha_innodb.h
+++ storage/innobase/handler/ha_innodb.h
@@ -439,17 +439,21 @@ class ha_innobase : public handler {
   be used across all parallel_scan methods. Also, gets the number of threads
   that would be spawned for parallel scan.
   @param[out]   scan_ctx              A scan context created by this method
-                                      that has to be used in
-                                      parallel_scan
+                                      that has to be used in parallel_scan
   @param[out]   num_threads           Number of threads to be spawned
   @param[in]    use_reserved_threads  true if reserved threads are to be used
                                       if we exhaust the max cap of number of
                                       parallel read threads that can be
                                       spawned at a time
+  @param[in]    max_desired_threads   Maximum number of desired read threads;
+                                      passing 0 has no effect, it is ignored;
+                                      upper-limited by the current value of
+                                      innodb_parallel_read_threads.
   @return error code
   @retval 0 on success */
   int parallel_scan_init(void *&scan_ctx, size_t *num_threads,
-                         bool use_reserved_threads) override;
+                         bool use_reserved_threads,
+                         size_t max_desired_threads) override;
 
   /** Start parallel read of InnoDB records.
   @param[in]  scan_ctx          A scan context created by parallel_scan_init
