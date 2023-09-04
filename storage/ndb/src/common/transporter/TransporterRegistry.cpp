dc95a9058742c82f3ae4a1ef8fae53060d3b0a43
Ole John Aske <ole.john.aske@oracle.com>
2023-09-04 14:52:54 +0200

    Bug#35774109 Reset connection m_error_states before (re-) connecting
    Ensure that a previous error is cleared before we try to reconnect
    a (failed?) connection.
    
    TransporterRegistry::do_connect() is where we initiate a connection
    by setting the CONNECTING state. Note that the state is required
    to be DISCONNECTED at that point.
    
    Patch sets m_error_states[] for the CONNECTING node to 'NO_ERROR'
    as part of validating and initiating CONNECTING.
    
    Change-Id: I143e71fd99da177e8f9afa7f42c40d4b3c400210

--- storage/ndb/src/common/transporter/TransporterRegistry.cpp
+++ storage/ndb/src/common/transporter/TransporterRegistry.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2021, Oracle and/or its affiliates.
+   Copyright (c) 2003, 2023, Oracle and/or its affiliates.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -1983,6 +1983,9 @@ TransporterRegistry::do_connect(NodeId node_id)
                            localNodeId, node_id));
     t->resetBuffers();
   }
+
+  m_error_states[node_id].m_code = TE_NO_ERROR;
+  m_error_states[node_id].m_info = (const char *)~(UintPtr)0;
 
   curr_state= CONNECTING;
   DBUG_VOID_RETURN;
