af0bb78882bacc31336d3c201a06dbf0547aef5d
Dyre Tjeldvoll <Dyre.Tjeldvoll@oracle.com>
2023-10-11 14:28:58 +0200

    Bug#35897778: Upgrade to CURL 8.4.0 [add sources]
    Unpack curl-8.4.0.tar.gz
    
    rm buildconf.bat configure ltmain.sh config.guess config.sub Makefile
    rm -rf docs m4 packages plan9 projects src tests winbuild
    
    git add curl-8.4.0
    
    Change-Id: I9afd097953a3b01d70081b77ffa5661bf0611460

---
+++ extra/curl/curl-8.4.0/lib/content_encoding.h
@@ -0,0 +1,39 @@
+#ifndef HEADER_CURL_CONTENT_ENCODING_H
+#define HEADER_CURL_CONTENT_ENCODING_H
+/***************************************************************************
+ *                                  _   _ ____  _
+ *  Project                     ___| | | |  _ \| |
+ *                             / __| | | | |_) | |
+ *                            | (__| |_| |  _ <| |___
+ *                             \___|\___/|_| \_\_____|
+ *
+ * Copyright (C) Daniel Stenberg, <daniel@haxx.se>, et al.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution. The terms
+ * are also available at https://curl.se/docs/copyright.html.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the COPYING file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ * SPDX-License-Identifier: curl
+ *
+ ***************************************************************************/
+#include "curl_setup.h"
+
+struct contenc_writer;
+
+char *Curl_all_content_encodings(void);
+
+CURLcode Curl_build_unencoding_stack(struct Curl_easy *data,
+                                     const char *enclist, int is_transfer);
+CURLcode Curl_unencode_write(struct Curl_easy *data,
+                             struct contenc_writer *writer,
+                             const char *buf, size_t nbytes);
+void Curl_unencode_cleanup(struct Curl_easy *data);
+
+#endif /* HEADER_CURL_CONTENT_ENCODING_H */
