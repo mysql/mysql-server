4c571bea5e197f8893e0ca5e0f6a29502030b8a1
Georgios Psaropoulos <georgios.psaropoulos@oracle.com>
2023-11-07 14:46:10 +0000

    Bug#34904177 Add lines and numbers to callstack produced by my_print_stacktrace
    - Add library backtrace in extra/libbacktrace/sha9ae4f4a. Approved as
      LIC#99705/BID#154396.
    - Implement stacktrace::{full, simple, pcinfo, syminfo} to encapsulate
      the backtrace state needed for calling the corresponding backtrace_*
      functions - this state needs to be created once in the lifetime of the
      process. The stacktrace namespace is in <backtrace/stacktrace.hpp>.
    - Add a convenience library backtrace and the alias ext::backtrace.
    - Add the CMake option WITH_EXT_BACKTRACE to control if the library will
      be used, in which case HAVE_EXT_BACKTRACE is defined.
    - If HAVE_EXT_BACKTRACE is defined, use <backtrace/stacktrace.hpp> in
      my_print_stacktrace.
    
    Change-Id: I8e0c5fa30b2dd986d42e008b26d9fd1195871177

---
+++ cmake/libbacktrace.cmake
@@ -0,0 +1,64 @@
+# Copyright (c) 2023, Oracle and/or its affiliates.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2.0,
+# as published by the Free Software Foundation.
+#
+# This program is also distributed with certain software (including
+# but not limited to OpenSSL) that is licensed under separate terms,
+# as designated in a particular file or component or in included license
+# documentation.  The authors of MySQL hereby grant you an additional
+# permission to link the program and your derivative works with the
+# separately licensed software that they have included with MySQL.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License, version 2.0, for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+
+SET (VERIFIED_PLATFORMS
+  LINUX_FEDORA
+  LINUX_RHEL7
+  LINUX_RHEL8
+  LINUX_RHEL9
+)
+UNSET(VERIFIED_PLATFORM)
+FOREACH(platform ${VERIFIED_PLATFORMS})
+  IF(${platform})
+    MESSAGE(STATUS "Found verified platform ${platform} for libbacktrace")
+    SET(VERIFIED_PLATFORM ${platform})
+    BREAK()
+  ENDIF()
+ENDFOREACH()
+
+IF(VERIFIED_PLATFORM AND NOT WITH_VALGRIND)
+  SET(DEFAULT_WITH_EXT_BACKTRACE ON)
+ELSE()
+  SET(DEFAULT_WITH_EXT_BACKTRACE OFF)
+ENDIF()
+OPTION(WITH_EXT_BACKTRACE "Use libbacktrace to print stacktraces"
+  ${DEFAULT_WITH_EXT_BACKTRACE}
+)
+
+IF(NOT WITH_EXT_BACKTRACE)
+  RETURN()
+ENDIF()
+
+IF(NOT LINUX)
+  MESSAGE(FATAL_ERROR "libbacktrace can only be used on linux builds")
+ENDIF()
+
+IF(VERIFIED_PLATFORM)
+  MESSAGE(STATUS "Using libbacktrace on ${VERIFIED_PLATFORM}")
+ELSE()
+  MESSAGE(WARNING "Using libbacktrace on unverified platform")
+ENDIF()
+
+
+ADD_SUBDIRECTORY(extra/libbacktrace)
+
+ADD_LIBRARY(ext::backtrace ALIAS backtrace)
