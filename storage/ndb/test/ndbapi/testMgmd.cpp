452e65d0f4c2f4fa6dced2d74588fffd39a8ea73
John David Duncan <john.duncan@oracle.com>
2023-11-08 16:32:05 -0800

    Bug#35934223 [noclose] spurious failures in ndb.test_mgmd
    The "Bug45495" test in testMgmd fails under gcov testing. The
    test results show hundreds of lines of DEBUG log messages
    being written while a fiile_exists() call times out.
    This patch starts the mgmd servers without the --verbose flag,
    so that the DEBUG level log messages are suppressed.
    
    This patch is intended for both mysql-8.0 and mysql-trunk.
    A separate patch for this bug addresses failures in other tests
    and is intended for mysql-trunk only.
    
    Change-Id: I904de0a1926f3a151ee21c194659b61b2d19aed2

--- storage/ndb/test/ndbapi/testMgmd.cpp
+++ storage/ndb/test/ndbapi/testMgmd.cpp
@@ -84,6 +84,7 @@ class Mgmd {
   BaseString m_name;
   BaseString m_exe;
   NdbMgmd m_mgmd_client;
+  bool m_verbose{true};
 
   Mgmd(const Mgmd &other) = delete;
 
@@ -110,6 +111,8 @@ class Mgmd {
   const char *name(void) const { return m_name.c_str(); }
 
   const char *exe(void) const { return m_exe.c_str(); }
+
+  void verbose(bool f) { m_verbose = f; }
 
   bool start(const char *working_dir, NdbProcess::Args &args) {
     g_info << "Starting " << name() << " ";
@@ -143,7 +146,7 @@ class Mgmd {
     }
     args.add("--nodaemon");
     args.add("--log-name=", name());
-    args.add("--verbose");
+    if (m_verbose) args.add("--verbose");
 
     if (first_extra_arg) {
       // Append any extra args
@@ -166,7 +169,7 @@ class Mgmd {
     args.add("--ndb-nodeid=", m_nodeid);
     args.add("--nodaemon");
     args.add("--log-name=", name());
-    args.add("--verbose");
+    if (m_verbose) args.add("--verbose");
 
     if (first_extra_arg) {
       // Append any extra args
@@ -301,7 +304,6 @@ class MgmdProcessList : public Vector<Mgmd *> {
       Mgmd *mgmd = this->operator[](i);
       delete mgmd;
     }
-    //  delete this->[i];
     clear();
   }
 };
@@ -450,6 +452,7 @@ int runTestBug45495(NDBT_Context *ctx, NDBT_Step *step) {
   MgmdProcessList mgmds;
   for (int i = 1; i <= 2; i++) {
     Mgmd *mgmd = new Mgmd(i);
+    mgmd->verbose(false);
     mgmds.push_back(mgmd);
     CHECK(mgmd->start_from_config_ini(wd.path()));
   }
