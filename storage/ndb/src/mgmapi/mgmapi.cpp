d8134150d44578578363916db9e16357388f75c1
Magnus Blåudd <magnus.blaudd@oracle.com>
2023-11-13 20:24:52 +0100

    Bug#36005903 mgmapi failure to parse bindadress
    Problem:
    Trying to start ndb_mgmd with --bind-address=localhost fails with error
    "Illegal bind address". This error is returned from the MgmApi when
    parsing the bind address to split it into a host and port part.
    
      $> ndb_mgmd --bind-address=localhost
      MySQL Cluster Management Server mysql-8.3.0 ndb-8.3.0
      2023-11-13 15:25:05 [MgmtSrvr] ERROR    -- Illegal bind address
    
    Analysis:
    The portnumber is parsed using strtol() and the errno value is examined.
    The empty string cause EINVAL and thus parsing is handled as failure.
    
    Solution:
    Using errno is known to be fragile and is not really neccessary to
    sufficently parse the number. Remove usage of errno in order to accept
    the valid bind address "localhost" as well as more or less any string on
    the form "<hostname>:<number>". Max and min value checks are left in
    place.
    
    Also unrelated fix of missing argument names in doxygen comment.
    
    Change-Id: I177164fdc889ec4a1f5568e7064190babee6dd13

--- storage/ndb/src/mgmapi/mgmapi.cpp
+++ storage/ndb/src/mgmapi/mgmapi.cpp
@@ -279,6 +279,8 @@ extern "C" int ndb_mgm_set_connectstring(NdbMgmHandle handle,
 extern "C" int ndb_mgm_set_bindaddress(NdbMgmHandle handle, const char *arg) {
   DBUG_ENTER("ndb_mgm_set_bindaddress");
   free(handle->m_bindaddress);
+  handle->m_bindaddress = nullptr;
+  handle->m_bindaddress_port = 0;
 
   if (arg) {
     char hostbuf[NI_MAXHOST];
@@ -286,11 +288,8 @@ extern "C" int ndb_mgm_set_bindaddress(NdbMgmHandle handle, const char *arg) {
     if (Ndb_split_string_address_port(arg, hostbuf, sizeof(hostbuf), servbuf,
                                       sizeof(servbuf)) == 0) {
       char *endp = nullptr;
-      errno = 0;
       long val = strtol(servbuf, &endp, 10);
-
-      if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN)) ||
-          (errno != 0) || (*endp != '\0') || (val > UINT16_MAX) || (val < 0)) {
+      if (*endp != '\0' || val > UINT16_MAX || val < 0) {
         // invalid address
         SET_ERROR(handle, NDB_MGM_ILLEGAL_BIND_ADDRESS, "Illegal bind address");
         DBUG_RETURN(-1);
@@ -303,9 +302,6 @@ extern "C" int ndb_mgm_set_bindaddress(NdbMgmHandle handle, const char *arg) {
       SET_ERROR(handle, NDB_MGM_ILLEGAL_BIND_ADDRESS, "Illegal bind address");
       DBUG_RETURN(-1);
     }
-  } else {
-    handle->m_bindaddress = nullptr;
-    handle->m_bindaddress_port = 0;
   }
   if (handle->cfg.ids.size() != 0) {
     handle->cfg.bind_address_port = handle->m_bindaddress_port;
@@ -400,11 +396,11 @@ extern "C" const char *ndb_mgm_get_latest_error_msg(const NdbMgmHandle h) {
   ndb_mgmd.
   Read and return result
 
-  @param The mgmapi handle
-  @param List describing the expected reply
-  @param Name of the command to call
-  @param Arguments for the command
-  @param Any bulk data to send after the command
+  @param handle The mgmapi handle
+  @param command_reply List describing the expected reply
+  @param cmd Name of the command to call
+  @param cmd_args Arguments for the command
+  @param cmd_bulk Any bulk data to send after the command
 
  */
 static const Properties *ndb_mgm_call(
