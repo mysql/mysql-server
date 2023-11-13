adc037dba35d1e9bd4cc3c8fb9fc01fce61136fe
Jan Kneschke <jan.kneschke@oracle.com>
2023-11-13 13:07:14 +0100

    Revert "Bug#35998612 routertest_integration_routing_sharing gtest_repeat=1000 fails"
    This reverts commit 6b4f838a45e3592a77f1ac805e1cdc069261d3c0.
    
    Change-Id: I09f1567d529ce46cf3353e876210e504d8ee62e3

--- router/tests/integration/test_routing_router_require.cc
+++ router/tests/integration/test_routing_router_require.cc
@@ -613,15 +613,14 @@ TestEnv *test_env{};
  */
 class TestWithSharedRouter {
  public:
-  static void SetUpTestSuite(std::span<SharedServer *const> servers) {
+  static void SetUpTestSuite(TcpPortPool &port_pool,
+                             std::span<SharedServer *const> servers) {
     for (const auto &s : servers) {
       if (s->mysqld_failed_to_start()) GTEST_SKIP();
     }
 
-    port_pool_ = new TcpPortPool;
-
     if (shared_router_ == nullptr) {
-      shared_router_ = new SharedRouter(*port_pool_);
+      shared_router_ = new SharedRouter(port_pool);
 
       SCOPED_TRACE("// spawn router");
       shared_router_->spawn_router(
@@ -633,24 +632,15 @@ class TestWithSharedRouter {
   static void TearDownTestSuite() {
     delete shared_router_;
     shared_router_ = nullptr;
-
-    delete port_pool_;
-    port_pool_ = nullptr;
   }
 
   static SharedRouter *router() { return shared_router_; }
 
  protected:
   static SharedRouter *shared_router_;
-
-  // TcpPortPool for this test-suite.
-  //
-  // As the router's get stopped, the port-pool has to be destroyed too.
-  static TcpPortPool *port_pool_;
 };
 
 SharedRouter *TestWithSharedRouter::shared_router_ = nullptr;
-TcpPortPool *TestWithSharedRouter::port_pool_{};
 
 class RouterRequireTestBase : public RouterComponentTest {
  public:
@@ -661,7 +651,8 @@ class RouterRequireTestBase : public RouterComponentTest {
       if (s->mysqld_failed_to_start()) GTEST_SKIP();
     }
 
-    TestWithSharedRouter::SetUpTestSuite(shared_servers());
+    TestWithSharedRouter::SetUpTestSuite(test_env->port_pool(),
+                                         shared_servers());
   }
 
   static void TearDownTestSuite() { TestWithSharedRouter::TearDownTestSuite(); }
