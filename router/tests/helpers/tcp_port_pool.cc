883f6a4c7853cb5e363cb6adc6f2893cbaa1d3d1
Jan Kneschke <jan.kneschke@oracle.com>
2023-11-14 13:46:31 +0100

    Bug#36054923 TcpPortPool on solaris gives out non-unique ports
    On Solaris the "router_require" tests fails all the time
    as the TcpPortPool gives out port's that are unique.
    
    The TcpPortPool allows to reserve TCP port ranges between multiple
    applications.
    
    Using multiple TcpPortPools in the same applications fails on Solaris as
    taking a file-lock that's own
    
    - by another process is exclusive
    - by same process is non-exclusive
    
    On all other Unixes, it is exclusive in both cases.
    
    Change
    ======
    
    - refactored the file-descriptor/handle handling into FileHandle class
    - track unique-ids owned by the process to make file-locks exclusive
      within the same process
    
    Change-Id: Ieeef9a2066f150a9be403a135a332cc90f7e145e

--- router/tests/helpers/tcp_port_pool.cc
+++ router/tests/helpers/tcp_port_pool.cc
@@ -22,113 +22,184 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "mysql/harness/filesystem.h"
-#include "mysql/harness/net_ts/io_context.h"
+#include "tcp_port_pool.h"
 
 #ifndef _WIN32
-#include <netdb.h>
-#include <netinet/in.h>
-#include <sys/file.h>
-#include <sys/socket.h>
+#include <fcntl.h>     // fcntl
+#include <sys/file.h>  // flock
 #include <sys/stat.h>  // chmod
-#include <sys/un.h>
-#include <unistd.h>
+#include <unistd.h>    // open, close
 #else
 #include <windows.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
 #endif
 
-#include <fcntl.h>
 #include <cstring>
-#include <stdexcept>
+#include <system_error>
+#include <utility>
 
-#include "mysql/harness/net_ts/internet.h"
+#include "mysql/harness/filesystem.h"  // Path
+#include "mysql/harness/stdx/expected.h"
 #include "mysqlrouter/utils.h"
 #include "router_test_helpers.h"
-#include "tcp_port_pool.h"
 
 using mysql_harness::Path;
 
+namespace {
+std::error_code posix_error_code() { return {errno, std::generic_category()}; }
+
+#ifdef _WIN32
+std::error_code win32_error_code() {
+  return {static_cast<int>(GetLastError()), std::generic_category()};
+}
+#endif
+
+}  // namespace
+
+stdx::expected<void, std::error_code> FileHandle::close() {
+  if (fh_ == kInvalidHandle) return {};
+
 #ifndef _WIN32
-bool UniqueId::lock_file(const std::string &file_name) {
-  lock_file_fd_ = open(file_name.c_str(), O_RDWR | O_CREAT, 0666);
+  int res = ::close(fh_);
+  if (res != 0) return stdx::unexpected(posix_error_code());
+#else
+  BOOL res = ::CloseHandle(fh_);
+  if (res == 0) return stdx::unexpected(win32_error_code());
+#endif
 
-  if (lock_file_fd_ >= 0) {
-    // don't pass the lock-fd to the child processes
-    int flag = fcntl(lock_file_fd_, F_GETFD);
-    flag |= FD_CLOEXEC;
-    fcntl(lock_file_fd_, F_SETFD, &flag);
+  fh_ = kInvalidHandle;
 
+  return {};
+}
+
+#ifndef _WIN32
+stdx::expected<FileHandle, std::error_code> FileHandle::open(
+    const std::string &filename, int opts, int mode) {
+  int res = ::open(filename.c_str(), opts, mode);
+  if (res < 0) return stdx::unexpected(posix_error_code());
+
+  return FileHandle(res);
+}
+#else
+stdx::expected<FileHandle, std::error_code> FileHandle::open(
+    const std::string &file_name, DWORD desired_access, DWORD share_mode,
+    SECURITY_ATTRIBUTES *security_attributes, DWORD creation_disposition,
+    DWORD flags_and_attributes, HANDLE template_file) {
+  HANDLE res = ::CreateFile(file_name.c_str(), desired_access, share_mode,
+                            security_attributes, creation_disposition,
+                            flags_and_attributes, template_file);
+
+  if (res == kInvalidHandle) return stdx::unexpected(win32_error_code());
+
+  return FileHandle(res);
+}
+#endif
+
+#ifndef _WIN32
+class LockedFileHandle {
+ public:
+  static stdx::expected<void, std::error_code> lock(
+      FileHandle::native_handle_type fd);
+};
+
+stdx::expected<void, std::error_code> LockedFileHandle::lock(
+    FileHandle::native_handle_type fd) {
+#ifdef __sun
+  // fcntl locks aren't inherited to other processes.
+  struct flock fl {};
+
+  fl.l_start = 0;
+  fl.l_len = 0;
+  fl.l_type = F_WRLCK;
+  fl.l_whence = SEEK_SET;
+
+  int lock = fcntl(fd, F_SETLK, &fl);
+#else
+  // don't pass the lock-fd to the child processes
+  {
+    int flag = ::fcntl(fd, F_GETFD);
+    if ((flag & FD_CLOEXEC) != 0) {
+      flag |= FD_CLOEXEC;
+      ::fcntl(fd, F_SETFD, &flag);
+    }
+  }
+
+  int lock = flock(fd, LOCK_EX | LOCK_NB);
+#endif
+  if (lock != 0) return stdx::unexpected(posix_error_code());
+
+  return {};
+}
+#endif
+
+stdx::expected<void, std::error_code> UniqueId::lock_file(
+    const std::string &file_name) {
+#ifndef _WIN32
+  auto open_res =
+      FileHandle::open(file_name, O_RDWR | O_CREAT | O_CLOEXEC, 0666);
+  if (!open_res) return stdx::unexpected(open_res.error());
+
+  lock_file_fd_ = std::move(*open_res);
+
+  {
     // open() honours umask and we want to make sure this directory is
     // accessible for every user regardless of umask settings
     ::chmod(file_name.c_str(), 0666);
-#ifdef __sun
-    struct flock fl;
+  }
 
-    fl.l_start = 0;
-    fl.l_len = 0;
-    fl.l_type = F_WRLCK;
-    fl.l_whence = SEEK_SET;
+  auto lock_res = LockedFileHandle::lock(lock_file_fd_.native_handle());
+  if (!lock_res) {
+    lock_file_fd_.close();
 
-    int lock = fcntl(lock_file_fd_, F_SETLK, &fl);
+    return stdx::unexpected(lock_res.error());
+  }
 #else
-    int lock = flock(lock_file_fd_, LOCK_EX | LOCK_NB);
+  auto open_res =
+      FileHandle::open(file_name,
+                       GENERIC_READ,  // read is enough
+                       0,  // prevent other processes from opening the file.
+                       nullptr,                    // no extra security
+                       OPEN_ALWAYS,                // if it exists, ok
+                       FILE_FLAG_DELETE_ON_CLOSE,  // delete the file on close
+                       nullptr);
+  if (!open_res) return stdx::unexpected(open_res.error());
+
+  lock_file_fd_ = std::move(*open_res);
 #endif
-    if (lock) {
-      // no lock so no luck, try the next one
-      close(lock_file_fd_);
-      lock_file_fd_ = -1;
-      return false;
-    }
 
-    // obtained the lock
-    return true;
-  }
-
-  return false;
+  // obtained the lock
+  return {};
 }
 
-std::string UniqueId::get_lock_file_dir() const {
+std::string UniqueId::get_lock_file_dir() {
   // this is what MTR uses, see mysql-test/lib/mtr_unique.pm for details
+#ifndef _WIN32
   return "/tmp/mysql-unique-ids";
-}
-
 #else
-
-bool UniqueId::lock_file(const std::string &file_name) {
-  lock_file_fd_ = ::CreateFile(file_name.c_str(), GENERIC_READ, 0, nullptr,
-                               OPEN_ALWAYS, 0, nullptr);
-  if (lock_file_fd_ != nullptr && lock_file_fd_ != INVALID_HANDLE_VALUE) {
-    return true;
-  }
-
-  return false;
-}
-
-std::string UniqueId::get_lock_file_dir() const {
   // this are env variables that MTR uses, see mysql-test/lib/mtr_unique.pm for
   // details
   DWORD buff_size = 65535;
-  std::vector<char> buffer;
+
+  std::string buffer;
   buffer.resize(buff_size);
-  buff_size = GetEnvironmentVariableA("ALLUSERSPROFILE", &buffer[0], buff_size);
-  if (!buff_size) {
-    buff_size = GetEnvironmentVariableA("TEMP", &buffer[0], buff_size);
+
+  buff_size =
+      GetEnvironmentVariableA("ALLUSERSPROFILE", buffer.data(), buffer.size());
+  if (buff_size == 0) {
+    buff_size = GetEnvironmentVariableA("TEMP", buffer.data(), buffer.size());
   }
 
-  if (!buff_size) {
+  if (buff_size == 0) {
     throw std::runtime_error("Could not get directory for lock files.");
   }
 
-  std::string result(buffer.begin(), buffer.begin() + buff_size);
-  result.append("\\mysql-unique-ids");
-  return result;
+  buffer.resize(buff_size);
+
+  buffer.append("\\mysql-unique-ids");
+  return buffer;
+#endif
 }
 
-#endif
-
-UniqueId::UniqueId(unsigned start_from, unsigned range) {
+UniqueId::UniqueId(value_type start_from, value_type range) {
   const std::string lock_file_dir = get_lock_file_dir();
   mysql_harness::mkdir(lock_file_dir, 0777);
 #ifndef _WIN32
@@ -137,79 +208,37 @@ UniqueId::UniqueId(unsigned start_from, unsigned range) {
   ::chmod(lock_file_dir.c_str(), 0777);
 #endif
 
-  for (unsigned i = 0; i < range; i++) {
-    id_ = start_from + i;
-    Path lock_file_path(lock_file_dir);
-    lock_file_path.append(std::to_string(id_));
-    lock_file_name_ = lock_file_path.str();
+  for (auto i = start_from; i < start_from + range; i++) {
+    if (process_unique_ids().contains(i)) continue;
 
-    if (lock_file(lock_file_name_.c_str())) {
+    Path lock_file_path(lock_file_dir);
+    lock_file_path.append(std::to_string(i));
+
+    auto lock_res = lock_file(lock_file_path.str());
+    if (lock_res) {
+      id_ = i;
+
+      process_unique_ids().insert(i);
+
       // obtained the lock, we are good to go
       return;
     }
   }
 
-  throw std::runtime_error("Could not get uniqe id from the given range");
+  throw std::runtime_error("Could not get unique id from the given range");
 }
 
 UniqueId::~UniqueId() {
-#ifndef _WIN32
-  if (lock_file_fd_ > 0) {
-    close(lock_file_fd_);
+  // release the process unique-id if we own one.
+  if (id_) {
+    process_unique_ids().erase(*id_);
   }
-
-  /*
-   * Removing lock file may result in race condition, both fcntl and flock are
-   * affected by this issue, consider the following scenario.
-   *
-   *           process A           process B
-   *     1. fd_a = open(file)                     // process A opens file
-   *     2. fcntl(fd_a) == 0                      // process A acquires lock
-   * on file
-   *     3.                    fd_b = open(file)  // process B opens file
-   *     4.                    fcntl(fd_b) == -1  // process B fails to
-   * acquire lock
-   *     5. close(fd_a)                           // process A closes file
-   *     6. unlink(file)                          // process A removes file
-   * name
-   *     7. fd_a = open(file)                     // process A opens file once
-   * again
-   *     8. fcntl(fd_a) == 0                      // process A acquires lock
-   * on the file
-   *     9.                    close(fd_b)        // process B closes file
-   *    10.                    unlink(file)       // process B removes file
-   * name
-   *    11.                    fd_b = open(file)  // process B opens file
-   *    12.                    fcntl(fd_b) == 0   // process B acquires lock
-   * on file
-   *
-   *    At this point both process A and process B have lock on the same file.
-   */
-
-#else
-  if (lock_file_fd_ != nullptr && lock_file_fd_ != INVALID_HANDLE_VALUE) {
-    ::CloseHandle(lock_file_fd_);
-  }
-
-  if (!lock_file_name_.empty()) {
-    mysql_harness::delete_file(lock_file_name_);
-  }
-#endif
 }
 
-UniqueId::UniqueId(UniqueId &&other) {
-  id_ = other.id_;
-  lock_file_fd_ = other.lock_file_fd_;
-  lock_file_name_ = other.lock_file_name_;
+UniqueId::ProcessUniqueIds &UniqueId::process_unique_ids() {
+  static ProcessUniqueIds ids;
 
-  // mark moved object as no longer owning of the resources
-#ifndef _WIN32
-  other.lock_file_fd_ = -1;
-#else
-  other.lock_file_fd_ = INVALID_HANDLE_VALUE;
-#endif
-
-  other.lock_file_name_ = "";
+  return ids;
 }
 
 uint16_t TcpPortPool::get_next_available() {
@@ -218,20 +247,20 @@ uint16_t TcpPortPool::get_next_available() {
   constexpr const auto bind_address = net::ip::address_v4::loopback();
 
   while (true) {
-    if (number_of_ids_used_ % kPortsPerFile == 0) {
-      number_of_ids_used_ = 0;
+    if (unique_ids_.empty()) {
+      unique_ids_.emplace_back(kPortsStartFrom, kPortsRange);
+    } else if (number_of_ids_used_ % kPortsPerFile == 0) {
       // need another lock file
-      auto start_from =
-          unique_ids_.empty() ? kPortsStartFrom : unique_ids_.back().get();
-      unique_ids_.emplace_back(start_from + 1, kPortsRange);
-    }
+      number_of_ids_used_ = 0;
 
-    assert(unique_ids_.size() > 0);
+      auto last_id = unique_ids_.back().id().value();
+      unique_ids_.emplace_back(last_id + 1, kPortsRange);
+    }
 
     // this is the formula that mysql-test also uses to map lock filename to
     // actual port number, they currently start from 13000 though
 
-    uint16_t port = 10000 + unique_ids_.back().get() * kPortsPerFile +
+    uint16_t port = 10000 + unique_ids_.back().id().value() * kPortsPerFile +
                     number_of_ids_used_++;
 
     if (is_port_bindable(io_ctx, {bind_address, port})) return port;
--- router/tests/helpers/tcp_port_pool.h
+++ router/tests/helpers/tcp_port_pool.h
@@ -25,39 +25,125 @@
 #ifndef _TCP_PORT_POOL_H_
 #define _TCP_PORT_POOL_H_
 
-#include <chrono>
 #include <cstdint>
+#include <optional>
 #include <string>
+#include <system_error>
+#include <utility>  // exchange
 #include <vector>
 
-/** @class UniqueID
+#ifdef _WIN32
+#include <Windows.h>  // HANDLE
+#endif
+
+#include "mysql/harness/stdx/expected.h"
+
+class FileHandle {
+ public:
+#ifndef _WIN32
+  using native_handle_type = int;
+  static constexpr const native_handle_type kInvalidHandle{-1};
+#else
+  using native_handle_type = HANDLE;
+  inline static const native_handle_type kInvalidHandle{INVALID_HANDLE_VALUE};
+#endif
+
+  FileHandle() = default;
+  explicit FileHandle(native_handle_type fh) : fh_(fh) {}
+
+  FileHandle(const FileHandle &) = delete;
+  FileHandle &operator=(const FileHandle &) = delete;
+
+  FileHandle(FileHandle &&other) noexcept
+      : fh_{std::exchange(other.fh_, kInvalidHandle)} {}
+  FileHandle &operator=(FileHandle &&other) noexcept {
+    fh_ = std::exchange(other.fh_, kInvalidHandle);
+
+    return *this;
+  }
+
+  native_handle_type native_handle() const noexcept { return fh_; }
+
+  ~FileHandle() { close(); }
+
+#ifndef _WIN32
+  static stdx::expected<FileHandle, std::error_code> open(
+      const std::string &filename, int opts, int mode);
+#else
+  static stdx::expected<FileHandle, std::error_code> open(
+      const std::string &file_name, DWORD desired_access, DWORD share_mode,
+      SECURITY_ATTRIBUTES *security_attributes, DWORD creation_disposition,
+      DWORD flags_and_attributes, HANDLE template_file);
+#endif
+
+  stdx::expected<void, std::error_code> close();
+
+ private:
+  native_handle_type fh_{kInvalidHandle};
+};
+
+/**
+ * system-level unique identifier.
  *
- * Helper class allowing mechanism to retrieve system-level unique identifier.
  * Compatible with mysql-test MTR, see mysql-test/lib/mtr_unique.pm for details
  *
  **/
 class UniqueId {
  public:
-  UniqueId(unsigned start_from, unsigned range);
-  UniqueId(UniqueId &&other);
-  ~UniqueId();
+  using value_type = unsigned;
+
+  // UniqueIds of this process.
+  //
+  // unique-ids are implemented on top of file-locking of
+  // /tmp/mysql-unique-ids/{num} (on Unix).
+  //
+  // file-locking via solaris fcntl() is only exclusive between processes,
+  // but not within the same process. Therefore, this class keeps all the active
+  // unique-ids of the current process.
+  //
+  class ProcessUniqueIds {
+   public:
+    using value_type = UniqueId::value_type;
+
+    void insert(value_type id) { id_.push_back(id); }
+    bool contains(value_type id) {
+      return std::find(std::begin(id_), std::end(id_), id) != std::end(id_);
+    }
+    size_t erase(value_type id) { return std::erase(id_, id); }
+
+   private:
+    std::vector<value_type> id_;
+  };
+
+  UniqueId(value_type start_from, value_type range);
 
   UniqueId(const UniqueId &) = delete;
   UniqueId &operator=(const UniqueId &) = delete;
+  UniqueId(UniqueId &&other) noexcept
+      : id_(std::exchange(other.id_, {})),
+        lock_file_fd_(std::exchange(other.lock_file_fd_, {})) {}
 
-  unsigned get() const { return id_; }
+  UniqueId &operator=(UniqueId &&other) noexcept {
+    id_ = std::exchange(other.id_, {});
+    lock_file_fd_ = std::exchange(other.lock_file_fd_, {});
+
+    return *this;
+  }
+
+  ~UniqueId();
+
+  std::optional<value_type> id() const { return *id_; }
 
  private:
-  bool lock_file(const std::string &file_name);
-  std::string get_lock_file_dir() const;
+  stdx::expected<void, std::error_code> lock_file(const std::string &file_name);
 
-  uint16_t id_;
-#ifndef _WIN32
-  int lock_file_fd_;
-#else
-  HANDLE lock_file_fd_;
-#endif
-  std::string lock_file_name_;
+  static ProcessUniqueIds &process_unique_ids();
+
+  static std::string get_lock_file_dir();
+
+  std::optional<value_type> id_{};
+
+  FileHandle lock_file_fd_;
 };
 
 /** @class TcpPortPool
@@ -69,12 +155,6 @@ class UniqueId {
  **/
 class TcpPortPool {
  public:
-  TcpPortPool() = default;
-
-  TcpPortPool(const TcpPortPool &) = delete;
-  TcpPortPool &operator=(const TcpPortPool &) = delete;
-  TcpPortPool(TcpPortPool &&other) = default;
-
   uint16_t get_next_available();
 
  private:
