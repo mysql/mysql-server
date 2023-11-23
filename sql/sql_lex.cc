ce36bcd6405c5fd9239db0fd645851ecd4e84b74
Magnus Brevik <magnus.brevik@oracle.com>
2023-11-23 14:24:45 +0100

    Bug#35208273 Add base table name to iterator-based EXPLAIN FORMAT=JSON. Bug#35337193 Add used columns to iterator-based EXPLAIN FORMAT=JSON. Bug#36027152 Add schema name to iterator-based EXPLAIN FORMAT=JSON.
    Other EXPLAIN JSON format changes:
    -Change the "table_name" field  to be the name of the base table instead
    of the alias.
    -Add "alias" field to hold the alias that was previously in "table_name".
    
    Change-Id: I1c0ac1c09b3f859f7edbca86c19f2fb0351936a2

--- sql/sql_lex.cc
+++ sql/sql_lex.cc
@@ -3798,6 +3798,9 @@ bool LEX::can_not_use_merged() {
 */
 
 bool LEX::need_correct_ident() {
+  if (is_explain() && explain_format->is_iterator_based() &&
+      explain_format->is_hierarchical())
+    return true;
   switch (sql_command) {
     case SQLCOM_SHOW_CREATE:
     case SQLCOM_SHOW_TABLES:
