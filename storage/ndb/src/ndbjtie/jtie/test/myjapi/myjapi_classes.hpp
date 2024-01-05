55cd7ce51cb6370033d927f33128e4d3ea881ef4
John David Duncan <john.duncan@oracle.com>
2024-01-05 10:42:55 -0800

    Bug#36159678 (3 of 6) JTie: Add missing specialization; test myjapi
    In jtie_tconv_object_impl.hpp, add a missing specialization
    of Target<J, C> for const C.
    
    Fix an incorrect mapping in myjapi_classes.hpp for
    C0::check().
    
    Then undefine JTIE_USE_WRAPPED_VARIANT_FOR_FUNCTION, and
    compile and run the tests (use "ctest -R jtie").
    
    Change-Id: I5911bb6ef9ea149c2242378d39956a4d4afaab84

--- storage/ndb/src/ndbjtie/jtie/test/myjapi/myjapi_classes.hpp
+++ storage/ndb/src/ndbjtie/jtie/test/myjapi/myjapi_classes.hpp
@@ -59,7 +59,7 @@ JTIE_DEFINE_JINT_ENUM_TYPE_MAPPING(E::EE, E_EE)
 // API JNI function stubs
 // ---------------------------------------------------------------------------
 
-#define JTIE_USE_WRAPPED_VARIANT_FOR_FUNCTION 1
+// #define JTIE_USE_WRAPPED_VARIANT_FOR_FUNCTION 1
 
 // The API stub functions in this file have mangled names that adhere
 // to the JVM specification.  It is not necessary to include the
@@ -770,7 +770,7 @@ JNIEXPORT void JNICALL Java_myjapi_CI_00024C0_check(JNIEnv *env, jobject obj,
                                                     jlong p0) {
   TRACE("Java_myjapi_CI_00024C0_check(JNIEnv *, jobject, jlong)");
 #ifndef JTIE_USE_WRAPPED_VARIANT_FOR_FUNCTION
-  gcall_mfv<ttrait_myjapi_CI_C0_ct, ttrait_int64, &C0::check>(env, obj);
+  gcall_mfv<ttrait_myjapi_CI_C0_ct, ttrait_int64, &C0::check>(env, obj, p0);
 #else
   gcall_fv<ttrait_myjapi_CI_C0_cr, ttrait_int64, &MyApiWrapper::C0__check>(
       env, NULL, obj, p0);
