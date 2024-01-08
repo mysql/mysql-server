d92c5c594524a2f775cc0933fc446bed7a59c6d2
Yanshi XU <36392244+xuyanshi@users.noreply.github.com>
2024-01-08 16:59:38 +0800

    Update log0buf.cc

--- storage/innobase/log/log0buf.cc
+++ storage/innobase/log/log0buf.cc
@@ -1027,6 +1027,8 @@ lsn_t log_buffer_write(log_t &log, const byte *str, size_t str_len,
       MetaManager* meta_mgr = MetaManager::get_instance();
 
       // todo: 怎么判断需要给 redo log 分配多少空间？
+      // log 数量极大，占用空间多，需要及时清理，远端数据管理是一个挑战
+
       // Redo Log Buffer is already exists in local Node, so I call it redo_log_remote_buf 
       char* redo_log_remote_buf = thd->rdma_buffer_allocator->Alloc(sizeof(latch_t));
 
