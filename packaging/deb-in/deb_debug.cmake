6141410a1d92d458172151d8b2ca2fdb591c2af3
Harin Vadodaria <harin.vadodaria@oracle.com>
2024-01-09 10:08:33 +0100

    WL#16140: Remove openssl_udf module
    Description: Removed openssl_udf library and corresponding tests
    Change-Id: I278c5c72f44d5cd5f04a0b3bd8b5b8b5b41ca8bc

--- packaging/deb-in/deb_debug.cmake
+++ packaging/deb-in/deb_debug.cmake
@@ -211,7 +211,6 @@ usr/lib/mysql/plugin/debug/data_masking.so
 usr/lib/mysql/plugin/debug/keyring_okv.so
 usr/lib/mysql/plugin/debug/keyring_encrypted_file.so
 usr/lib/mysql/plugin/debug/keyring_hashicorp.so
-usr/lib/mysql/plugin/debug/openssl_udf.so
 usr/lib/mysql/plugin/debug/thread_pool.so
 usr/lib/mysql/plugin/debug/firewall.so
 usr/lib/mysql/plugin/debug/component_keyring_encrypted_file.so
