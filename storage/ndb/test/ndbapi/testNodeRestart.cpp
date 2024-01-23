f3302c10fdb25d2765a8afee582bf57fcad1c70b
Vicente Fernandes <vicente.machado.fernandes@oracle.com>
2024-01-23 00:56:10 +0000

    Bug#36356615 testNodeRestart -n CommittedRead leaves Error Inserted after running
    Test injects error 8048 and 8049 to make TC not choose own node for
    simple/dirty read.
    Error is consumed by one DBTC instance, but it is never cleared.
    Test can fail due to error 8048 being left in Backup instances.
    Fix: clears EI from test side to prevent leftovers.
    
    Change-Id: I6aa3a4dbc0bfa057e50f5dd752b1f574963e8447

--- storage/ndb/test/ndbapi/testNodeRestart.cpp
+++ storage/ndb/test/ndbapi/testNodeRestart.cpp
@@ -850,6 +850,7 @@ done:
     restarter.waitClusterStarted(60) ;
     CHK_NDB_READY(pNdb);
   }
+  CHECK(restarter.insertErrorInAllNodes(0) == 0, "Failed to clear insertError");
   return result;
 err:
   hugoOps.closeTransaction(pNdb);
