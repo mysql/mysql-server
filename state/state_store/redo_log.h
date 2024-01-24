0e43b3462798b906a37d5c91c10ad76bc147e899
Yanshi XU <36392244+xuyanshi@users.noreply.github.com>
2024-01-25 02:30:33 +0800

    Create redo_log.h

---
+++ state/state_store/redo_log.h
@@ -0,0 +1,12 @@
+//
+// Created by xuyanshi on 1/25/24.
+//
+
+#pragma once
+
+#include "util/common.h"
+
+struct RedoLogItem {
+  // TODO: RedoLogItem 的成员是 log_t 中的吗？
+  // https://dev.mysql.com/doc/dev/mysql-server/latest/structlog__t.html#details
+};
