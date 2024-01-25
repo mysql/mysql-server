78a02c950ecc8ec7f9e4458e16529326d412b806
Yanshi XU <36392244+xuyanshi@users.noreply.github.com>
2024-01-25 15:16:14 +0800

    Update redo_log.h

--- state/state_store/redo_log.h
+++ state/state_store/redo_log.h
@@ -9,4 +9,5 @@
 struct RedoLogItem {
   // TODO: RedoLogItem 的成员是 log_t 中的吗？
   // https://dev.mysql.com/doc/dev/mysql-server/latest/structlog__t.html#details
+  // storage/innobase/include/log0sys.h::log_t
 };
