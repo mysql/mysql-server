7dd387e6a50d48e1d8046678ca255dbd32a5be7b
Yanshi XU <36392244+xuyanshi@users.noreply.github.com>
2024-01-25 20:36:14 +0800

    Update redo_log.h

--- state/state_store/redo_log.h
+++ state/state_store/redo_log.h
@@ -7,7 +7,10 @@
 #include "util/common.h"
 
 struct RedoLogItem {
-  // TODO: RedoLogItem 的成员是 log_t 中的吗？
+  // TODO: log_t 并不是 redo log 本身，而是 redo log buffer 及其相关的信息！！
+  // redo log buffer 本质上只是一个 byte 数组，但是为了维护这个 buffer
+  // 还需要设置很多其他的 meta data，这些 meta data 全部封装在 log_t 结构体中
+
   // https://dev.mysql.com/doc/dev/mysql-server/latest/structlog__t.html#details
   // storage/innobase/include/log0sys.h::log_t
 };
