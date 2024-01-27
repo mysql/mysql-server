7f007759116424638a835492c8a9892c5bc33d2e
Yanshi XU <36392244+xuyanshi@users.noreply.github.com>
2024-01-27 21:51:44 +0800

    state_fetch.h

--- state/state_fetch/redo_log_fetch.cpp
+++ state/state_fetch/redo_log_fetch.cpp
@@ -2,4 +2,4 @@
 // Created by xuyanshi on 1/27/24.
 //
 
-#include "redo_log_fetch.h"
+#include "state_fetch.h"
---
+++ state/state_fetch/state_fetch.h
@@ -0,0 +1,10 @@
+//
+// Created by xuyanshi on 1/27/24.
+//
+
+#ifndef MYSQL_STATE_FETCH_H
+#define MYSQL_STATE_FETCH_H
+
+class state_fetch {};
+
+#endif  // MYSQL_STATE_FETCH_H
