4d587f1215e74a2dd3a003eb777651b2a0099a8d
Yanshi XU <36392244+xuyanshi@users.noreply.github.com>
2024-01-28 17:12:04 +0800

    Update redo_log_fetch.cpp

--- state/state_fetch/redo_log_fetch.cpp
+++ state/state_fetch/redo_log_fetch.cpp
@@ -4,6 +4,14 @@
 
 #include "state_fetch.h"
 
+/**
+ * 把 redo log buffer 从状态层读回来
+ * @return
+ */
 bool redo_log_fetch() { return false; }
 
+/**
+ * 回放 buffer 中存储的 log，实现状态恢复
+ * @return
+ */
 bool redo_log_replay() { return false; }
