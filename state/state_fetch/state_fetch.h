a177ddeb781e7ea4127e6c5e207c84d2bacf08c0
Yanshi XU <36392244+xuyanshi@users.noreply.github.com>
2024-01-29 10:16:29 +0800

    Update state_fetch.h

--- state/state_fetch/state_fetch.h
+++ state/state_fetch/state_fetch.h
@@ -5,12 +5,18 @@
 #ifndef MYSQL_STATE_FETCH_H
 #define MYSQL_STATE_FETCH_H
 
+#include "state/state_store/redo_log.h"
+
 class StateFetch {
  public:
   StateFetch() {}
 
  private:
   bool failStatus = false;
+
+  RedoLogItem *redoLogItem = nullptr;
+
+  unsigned char *redo_log_buffer_buf = nullptr;
 };
 
 #endif  // MYSQL_STATE_FETCH_H
