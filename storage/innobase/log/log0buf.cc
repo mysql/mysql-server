5d7f5250283277901ee35981495dd71d6212e2d6
Yanshi XU <36392244+xuyanshi@users.noreply.github.com>
2024-02-22 00:47:16 +0800

    Update log0buf.cc

--- storage/innobase/log/log0buf.cc
+++ storage/innobase/log/log0buf.cc
@@ -1182,8 +1182,7 @@ lsn_t log_buffer_write(log_t &log, const byte *str, size_t str_len,
     unsigned char *log_buf_data = static_cast<byte *>(log.buf);
     if (!thd->coro_sched->RDMAWriteSync(
             0, qp, (char *)log_buf_data,
-            meta_mgr->GetRedoLogCurrAddr() +
-                sizeof(RedoLogItem),  // 实际数据传到RedoLogItem后面
+            meta_mgr->GetRedoLogCurrAddr() + sizeof(RedoLogItem),  // 实际数据传到RedoLogItem后面
             ut::INNODB_CACHE_LINE_SIZE)) {  // TODO: 不太确定log.buf的大小
                                             // log.buf_size? buf_size_sn?
       // Fail
