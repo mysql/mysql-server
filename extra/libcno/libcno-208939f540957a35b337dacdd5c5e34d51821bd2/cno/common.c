5fd5d4092999a040d264a7a0ba56c2e95ae1cc82
Lukasz Kotula <lukasz.kotula@oracle.com>
2024-02-27 15:20:04 +0100

    Bug#36357181 - add libcno [remove python gen/add c++]
    4. Replaced python version of hpack-huffman-optimization-data, with generator written in C++.
    
    * removed "hpack-data.py"
    * introduced "cno_huffman_generator.cc", and added CMake integration for it.
    
    The "hpack-data.py", was generating C header in ./cno/ folder. The new code
    generates the output file inside binary firectory and adds it to "CNO" target
    includes.
    
    3. Remove not needed files from libcno and its submodule(picohttpparser)
    
    2. Unpack 'picohttpparser' into libcno submodule dependencies:
    
     unzip -j picohttpparser-fd5a92f86b2270e69e7242a429b926d2ed01734e.zip  "picohttpparser-fd5a92f86b2270e69e7242a429b926d2ed01734e/*" -d ./libcno-208939f540957a35b337dacdd5c5e34d51821bd2/picohttpparser/
    
    1. Unpack 'libcno':
      unzip ./libcno-208939f540957a35b337dacdd5c5e34d51821bd2.zip -d .
    
    Change-Id: I055cc4b2f327bd33c39c3312a0074771db726b70

---
+++ extra/libcno/libcno-208939f540957a35b337dacdd5c5e34d51821bd2/cno/common.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "common.h"
+
+_Thread_local static struct cno_error_t E;
+
+const struct cno_error_t * cno_error(void) {
+    return &E;
+}
+
+int cno_error_set(int code, const char *fmt, ...) {
+    E.code = code;
+    va_list vl;
+    va_start(vl, fmt);
+    vsnprintf(E.text, sizeof(E.text), fmt, vl);
+    va_end(vl);
+    return -1;
+}
