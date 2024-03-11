5a0f5cacb30e30a3e3ec758cc7546927d7d4c3f2
Mauritz Sundell <mauritz.sundell@oracle.com>
2024-03-11 20:31:29 +0100

    Bug#36251565 ndb_bug14166590 Error injection not hit
    Removing test ndb.ndb_bug14166590 and companion error insertion code.
    
    Test occasionally failed and further analysis revealed that test did not
    have any associated code fix and did not reproduce any known issue.
    
    Change-Id: I77063c318224d36357e61b94ec97f9345392d0c8

--- storage/ndb/include/kernel/signaldata/TcContinueB.hpp
+++ storage/ndb/include/kernel/signaldata/TcContinueB.hpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2021, Oracle and/or its affiliates.
+   Copyright (c) 2003, 2024, Oracle and/or its affiliates.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -57,12 +57,8 @@ private:
     DelayTCKEYCONF                         = 18,
     ZNF_CHECK_TRANSACTIONS                 = 19,
     ZSEND_FIRE_TRIG_REQ                    = 20,
-    ZSTART_FRAG_SCANS                      = 21
-#ifdef ERROR_INSERT
-    ,ZDEBUG_DELAYED_ABORT                  = 22
-    ,ZDEBUG_DELAY_TCROLLBACKREP            = 23
-#endif
-    ,ZSCAN_FOR_READ_BACKUP                  = 24
+    ZSTART_FRAG_SCANS                      = 21,
+    ZSCAN_FOR_READ_BACKUP                  = 24
   };
 };
 
