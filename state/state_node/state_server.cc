91451eb9ae3db0963c084f79f3a3f3db634b477a
Yanshi XU <xuyanshi1999@gmail.com>
2024-03-11 16:54:18 +0800

    修改配置文件路径，解决bad_alloc

--- state/state_node/state_server.cc
+++ state/state_node/state_server.cc
@@ -60,7 +60,7 @@ void StateServer::CleanQP() {
 
 int main(int argc, char* argv[]) {
     // std::string config_path = "../config/state_server_config.json";
-    std::string config_path = "/mysql8/config/state_server_config.json";
+    std::string config_path = "/mysql_data3/config/state_server_config.json";
     // auto json_config = JsonConfig::load_file(config_path);
     
     // auto state_node = json_config.get("state_node");
