7eb034f0d89dc94996ce6b76a49ee8d4697390f5
Jan Kneschke <jan.kneschke@oracle.com>
2024-05-15 23:01:58 +0200

    Bug#36624536 asan-failure on shutdown at bootstrap
    routertest_component_... which call bootstrap fail the ASAN checks after
    the recent switch protobuf-lite to full protobuf.
    
    Background
    ==========
    
    Bootstrap loads the plugins which it created a config for to expose its
    configuration in the cluster's metadata tables.
    
    It loads the plugins, gets their "expose-config" hooks and collects the
    config descriptions.
    
    When the "metadata_cache" plugin is loaded, and later unloaded the
    reflection-data of the libprotobuf is lazy-loaded at library start, but
    not freed when the libprotobuf is unloaded again. This task normally
    handled by the "router_protobuf" plugin which calls
    ShutdownProtobufLibrary()" in its "deinit" function.
    
    But at bootstrap, the plugins are only loaded and never initialized, and
    with "init()", the "deinit()" function isn't called either.
    
    Change
    ======
    
    - move the "ShutdownProtobufLibrary()" into a "static Scope_guard" which
      is triggered then the router_protobuf plugin is unloaded
    
    Change-Id: I652ec11ac9ab4d0099c70cb9975a07500981f84c

--- router/src/protobuf/src/protobuf_plugin.cc
+++ router/src/protobuf/src/protobuf_plugin.cc
@@ -26,6 +26,7 @@
 #include "mysqlrouter/router_protobuf_export.h"
 
 #include "my_compiler.h"
+#include "scope_guard.h"
 
 MY_COMPILER_DIAGNOSTIC_PUSH()
 MY_COMPILER_CLANG_DIAGNOSTIC_IGNORE("-Wdeprecated-dynamic-exception-spec")
@@ -35,6 +36,14 @@ MY_COMPILER_CLANG_DIAGNOSTIC_IGNORE(
 MY_COMPILER_DIAGNOSTIC_POP()
 
 #include "mysql/harness/plugin.h"
+
+// deinit the protobuf-library on
+//
+// - deinit of this plugin and,
+// - unload of this plugin
+static Scope_guard static_guard([]() {
+  google::protobuf::ShutdownProtobufLibrary();
+});
 
 extern "C" {
 
@@ -46,9 +55,7 @@ mysql_harness::Plugin ROUTER_PROTOBUF_EXPORT harness_plugin_router_protobuf = {
     // conflicts
     0, nullptr,
     nullptr,  // init
-    [](mysql_harness::PluginFuncEnv *) {
-      google::protobuf::ShutdownProtobufLibrary();
-    },
+    [](mysql_harness::PluginFuncEnv *) { static_guard.rollback(); },
     nullptr,  // start
     nullptr,  // stop
     false,    // declare_readiness
