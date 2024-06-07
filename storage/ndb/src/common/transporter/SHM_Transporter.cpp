97fdbdaa8a286e1351f4480c271640045b9be520
Frazer Clement <frazer.clement@oracle.com>
2024-06-07 15:09:49 +0100

    Bug#36568752 Shared memory transporter used too early with ndbd
    Fix for issue where an attempt is made to use a SHM transporter's
    wakeup socket before it is ready.
    
    One new test added to give SHM transporter coverage of both
    ndbd + ndbmtd on Unix platforms with SHM.
      ndb_basic_ndbd_shm
    
    Change-Id: I4f5a084846a4c23c4e4d67f82fcb6476ba59ffb3

--- storage/ndb/src/common/transporter/SHM_Transporter.cpp
+++ storage/ndb/src/common/transporter/SHM_Transporter.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2003, 2021, Oracle and/or its affiliates.
+   Copyright (c) 2003, 2024, Oracle and/or its affiliates.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -619,6 +619,10 @@ SHM_Transporter::wakeup()
   unlock_reverse_mutex();
   if (awake_state)
   {
+    return;
+  }
+  if (!my_socket_valid(theSocket)) {
+    /* Wakeup socket not yet ready */
     return;
   }
   iov[0].iov_len = 1;
