aa9c14aeb90d9367ddbac9483c7e0cf18a670fd4
Tor Didriksen <tor.didriksen@oracle.com>
2024-07-03 16:53:14 +0200

    Bug#37039383 Backport Bug#36593265 MySQL server heap-buffer-overflow in strlen
    When converting a MySQL String to an std::string,
    use {ptr(), length()} rather than strlen().
    
    Change-Id: Ie59965ecccf9846cf5c18b90b572328263c1cefa

--- sql/item_strfunc.cc
+++ sql/item_strfunc.cc
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2000, 2023, Oracle and/or its affiliates.
+   Copyright (c) 2000, 2024, Oracle and/or its affiliates.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -436,7 +436,7 @@ class kdf_argument {
       *error_generated = TRUE;
       return FALSE;
     }
-    kdf_option = kdf_option_value->ptr();
+    kdf_option = string(kdf_option_value->ptr(), kdf_option_value->length());
     return TRUE;
   }
 
