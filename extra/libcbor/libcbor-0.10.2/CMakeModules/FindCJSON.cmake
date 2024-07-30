2fb1879a1f861896bf277c687b80f8f409b28586
Tor Didriksen <tor.didriksen@oracle.com>
2024-07-30 09:54:11 +0200

    Bug#36752604 Upgrade libfido2 to v1.15.0 [remove old]
    Remove old source files.
    
    Change-Id: I8414ff82a112aa3774ea67fe06a07e7a565270d3

--- extra/libcbor/libcbor-0.10.2/CMakeModules/FindCJSON.cmake
+++
@@ -1,9 +0,0 @@
-include (LibFindMacros)
-
-find_path(CJSON_INCLUDE_DIR NAMES cjson/cJSON.h)
-find_library(CJSON_LIBRARY NAMES cjson)
-
-set(CJSON_PROCESS_INCLUDES CJSON_INCLUDE_DIR)
-set(CJSON_PROCESS_LIBS CJSON_LIBRARY)
-
-libfind_process(CJSON)
