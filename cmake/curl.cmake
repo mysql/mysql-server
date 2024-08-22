c1de8c36369d5394e6e29c54ee8c2a93e641c72a
Tor Didriksen <tor.didriksen@oracle.com>
2024-08-22 12:49:35 +0200

    Bug#36978193 Cleanup of cmake policies
    CMAKE_MINIMUM_REQUIRED does two things:
     - sets the minimum required version
     - loads default policies for the cmake version specified
    
    We currently load defaults for 3.5.1 even though the minimal required
    version is 3.11.2 in mysql-8.0 and 3.14.6 in mysql-trunk
    
     - Align defaults with the cmake version we actually require.
     - Set policies CMP0091 and CMP0107 explicitly (see comments in the code)
     - Update abseil and protobuf cmake code to be aligned with our own choice of policies.
     - Add an ALIAS library CURL::libcurl unless it is already done by FIND_PACKAGE.
    
    Change-Id: I5e842a5d42307dd7d40d2ebd18e3e9ccdd11d774
    (cherry picked from commit a5e2286a0d5c8c138ba934406a758dcf7cf95867)

--- cmake/curl.cmake
+++ cmake/curl.cmake
@@ -104,6 +104,8 @@ FUNCTION(FIND_SYSTEM_CURL ARG_CURL_INCLUDE_DIR)
   #  CURL_LIBRARIES      - List of libraries when using curl.
   #  CURL_FOUND          - True if curl found.
   #  CURL_VERSION_STRING - the version of curl found (since CMake 2.8.8)
+  #
+  #  Newer versions of cmake will also define the IMPORTED target CURL::libcurl
   FIND_PACKAGE(CURL)
   IF(CURL_FOUND AND CURL_LIBRARIES)
     SET(CURL_LIBRARY ${CURL_LIBRARIES} CACHE FILEPATH "Curl library")
@@ -242,6 +244,10 @@ FUNCTION(MYSQL_CHECK_CURL)
   MESSAGE(STATUS "CURL_INCLUDE_DIR = ${CURL_INCLUDE_DIR}")
   FIND_CURL_VERSION()
   ADD_LIBRARY(ext::curl ALIAS curl_interface)
+  # 3rd party projects may depend on this:
+  IF(NOT TARGET CURL::libcurl)
+    ADD_LIBRARY(CURL::libcurl ALIAS curl_interface)
+  ENDIF()
   # Downgrade errors to warnings
   # We could instead do INTERFACE_COMPILE_DEFINITIONS CURL_DISABLE_DEPRECATION
   # That would silence curl warnings completely.
