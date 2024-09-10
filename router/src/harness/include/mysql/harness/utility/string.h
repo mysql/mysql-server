93f83d89976cf63096df7a3c909b68ad0a9a8356
Jan Kneschke <jan.kneschke@oracle.com>
2024-09-10 20:19:26 +0200

    Bug#36982911 mysql_harness::join does not handle std::string_view
    mysql_harness::join() concats the elements of a container into a string.
    
    It doesn't support joining if elements are std::string_view,
    nor if the container is a c-array or std::initializer_list.
    
    Also, benchmarking showed, that the use of std::accumulate() is slow in
    this case as it leads to copying the joined std::string in each round
    instead of appending to one string.
    
    Joining 1024 std::string_views into a comma-seperated string:
    
    join() via std::accumulate()    11310 ns/iter
    join() via direct loop           7043 ns/iter
    
    Change
    ======
    
    - simplified the implementation into a generic implementation which
      relies on std::begin() and avoids std::accumulate()
    - added support to join strings-views and c-arrays
    - added tests for string-views, c-arrays and initializer-lists
    - added benchmarks
    
    Change-Id: I3285b8470d85495c09cc51b1574d91173d38c099

--- router/src/harness/include/mysql/harness/utility/string.h
+++ router/src/harness/include/mysql/harness/utility/string.h
@@ -28,7 +28,6 @@
 
 #include "harness_export.h"
 
-#include <numeric>  // accumulate
 #include <string>
 #include <vector>
 
@@ -65,93 +64,49 @@ std::string string_format(const char *format, ...);
 }  // namespace utility
 
 namespace detail {
-template <class Container, class T>
-struct Join {
-  static std::string impl(Container, const std::string &);
-};
 
-template <class Container>
-struct Join<Container, std::string> {
-  static std::string impl(Container cont, const std::string &delim) {
-    if (cont.begin() == cont.end()) return {};
+// a simplified variant of the std::ranges::range concept
+//
+// C++20 ranges lib isn't fully available yet.
 
-    std::string o(*(cont.begin()));
-
-    // if T::value_type has a .size() method reallocs can be avoided
-    // when joining the strings by calculating the size upfront
-    {
-      const size_t delim_size = delim.size();
-      size_t space =
-          std::accumulate(std::next(cont.begin()), cont.end(), o.size(),
-                          [delim_size](size_t sum, const std::string &b) {
-                            return sum + delim_size + b.size();
-                          });
-      o.reserve(space);
-    }
-
-#if 0
-    // once benchmarked that this is equivalent of the hand-rolled version
-    // (number of allocs, ...) this implementation could be used.
-    return std::accumulate(std::next(cont.begin()), cont.end(), o,
-                           [&delim](std::string a, const std::string &b) {
-                             return a.append(delim).append(b);
-                           });
-#else
-    // add the first element directly
-    auto it = std::next(cont.begin());
-    const auto last = cont.end();
-
-    // all other elements with delim
-    for (; it != last; ++it) {
-      o += delim;
-
-      o += *it;
-    }
-
-    return o;
-#endif
-  }
-};
-
-template <class Container>
-struct Join<Container, const char *> {
-  static std::string impl(Container cont, const std::string &delim) {
-    if (cont.begin() == cont.end()) return {};
-
-    return std::accumulate(std::next(cont.begin()), cont.end(),
-                           std::string(*(cont.begin())),
-                           [&delim](const std::string &a, const char *b) {
-                             return a + delim + b;
-                           });
-  }
-};
+template <class R>
+concept range = requires(const R &rng) {
+                  std::begin(rng);
+                  std::end(rng);
+                };
 
 }  // namespace detail
 
 /**
- * join elements of an container into a string separated by a delimiter.
+ * join elements of a range into a string separated by a delimiter.
  *
- * Container MUST:
+ * works with:
  *
- * - have .begin() and end()
- * - ::iterator must be ForwardIterator + InputIterator
- * - ::value_type must be appendable to std::string
+ * - std::vector, std::array, c-array, list, forward_list, deque
+ * - and std::string, c-string, std::string_view
  *
- * should work with:
- *
- * - std::vector<const char *|std::string>
- * - std::array<const char *|std::string, N>
- * - std::list<const char *|std::string>
- *
- * @param cont a container
+ * @param rng a range of strings
  * @param delim delimiter
- * @returns string of elements of container separated by delim
+ * @returns string of elements of the range separated by delim
  */
-template <class Container>
-std::string join(Container cont, const std::string &delim) {
-  return detail::Join<Container, typename Container::value_type>::impl(cont,
-                                                                       delim);
+std::string join(const detail::range auto &rng, std::string_view delim)
+  requires(std::constructible_from<std::string, decltype(*std::begin(rng))>)
+{
+  auto cur = std::begin(rng);
+  const auto end = std::end(rng);
+
+  if (cur == end) return {};  // empty
+
+  std::string joined(*cur);  // first element.
+
+  // append delim + element
+  for (cur = std::next(cur); cur != end; cur = std::next(cur)) {
+    joined.append(delim).append(*cur);
+  }
+
+  return joined;
 }
+
 /* Checks that given string belongs to the collection of strings */
 template <class T>
 constexpr bool str_in_collection(const T &t, const std::string_view &k) {
--- router/src/harness/tests/test_utility_string.cc
+++ router/src/harness/tests/test_utility_string.cc
@@ -28,12 +28,15 @@
 #include <array>
 #include <deque>
 #include <forward_list>
+#include <initializer_list>
 #include <list>
 #include <set>
 #include <unordered_set>
 #include <vector>
 
 #include "mysql/harness/utility/string.h"
+
+#include "unittest/gunit/benchmark.h"
 
 using mysql_harness::join;
 
@@ -43,7 +46,16 @@ class JoinTest : public ::testing::Test {};
 TYPED_TEST_SUITE_P(JoinTest);
 
 TYPED_TEST_P(JoinTest, many) {
+  TypeParam c_array[]{
+      "abc",
+      "def",
+  };
+
+  EXPECT_EQ(join(c_array, "-"), "abc-def");
   EXPECT_EQ(join(std::array<TypeParam, 2>{"abc", "def"}, "-"), "abc-def");
+  EXPECT_EQ(join(std::initializer_list<const char *>{"abc", "def"}, "-"),
+            "abc-def");
+
   EXPECT_EQ(join(std::deque<TypeParam>{"abc", "def"}, "-"), "abc-def");
   EXPECT_EQ(join(std::forward_list<TypeParam>{"abc", "def"}, "-"), "abc-def");
   EXPECT_EQ(join(std::list<TypeParam>{"abc", "def"}, "-"), "abc-def");
@@ -59,6 +71,11 @@ TYPED_TEST_P(JoinTest, many) {
 }
 
 TYPED_TEST_P(JoinTest, one) {
+  TypeParam c_array[1]{
+      "abc",
+  };
+  EXPECT_EQ(join(c_array, "-"), "abc");
+
   EXPECT_EQ(join(std::array<TypeParam, 1>{"abc"}, "-"), "abc");
   EXPECT_EQ(join(std::deque<TypeParam>{"abc"}, "-"), "abc");
   EXPECT_EQ(join(std::forward_list<TypeParam>{"abc"}, "-"), "abc");
@@ -80,8 +97,52 @@ TYPED_TEST_P(JoinTest, none) {
 
 REGISTER_TYPED_TEST_SUITE_P(JoinTest, many, one, none);
 
-using JoinTestTypes = ::testing::Types<std::string, const char *>;
+using JoinTestTypes =
+    ::testing::Types<std::string, const char *, std::string_view>;
 INSTANTIATE_TYPED_TEST_SUITE_P(Spec, JoinTest, JoinTestTypes);
+
+namespace {
+
+using namespace std::string_view_literals;
+
+template <class T, size_t N>
+constexpr auto init_bench_data() {
+  std::array<T, N> data;
+
+  for (auto &el : data) {
+    el = "fuzbuzshnuzz";
+  }
+
+  return data;
+}
+
+auto bench_ar_sv = init_bench_data<std::string_view, 1024>();
+auto bench_ar_cs = init_bench_data<const char *, 1024>();
+auto bench_ar_s = init_bench_data<std::string, 1024>();
+
+void BenchJoinStdArrayStringView(size_t iter) {
+  while ((iter--) != 0) {
+    std::string joined = join(bench_ar_sv, ", ");
+  }
+}
+
+void BenchJoinStdArrayCString(size_t iter) {
+  while ((iter--) != 0) {
+    std::string joined = join(bench_ar_cs, ", ");
+  }
+}
+
+void BenchJoinStdArrayStdString(size_t iter) {
+  while ((iter--) != 0) {
+    std::string joined = join(bench_ar_s, ", ");
+  }
+}
+
+}  // namespace
+
+BENCHMARK(BenchJoinStdArrayStdString)
+BENCHMARK(BenchJoinStdArrayStringView)
+BENCHMARK(BenchJoinStdArrayCString)
 
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
