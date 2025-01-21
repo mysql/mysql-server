12d5e56798d5035a7d3a2e9db2c62275f1bb35c7
Brendan Plougonven <bplougonven@infovista.com>
2025-01-21 13:23:53 +0100

    AB#340942 Fixed value of CI_COMMIT_TAG

--- build_scripts/linux/compile-package-linux.sh
+++ build_scripts/linux/compile-package-linux.sh
@@ -332,19 +332,21 @@ if [ "$DO_NOT_PACK" = true ]; then
     exit 0
 fi
 
-echo `date +"%x %X"` "Zipping distribution package containing binaries and configuration files to be deployed."
+echo `date +"%x %X"` "Generating the distribution package which contains the binaries and configuration files to be deployed."
 generate_distrib_pack  $build_dir_arch  $build_dir_arch/_CPack_Packages/Linux/TGZ/mysql-$MYSQL_TAG-linux-x86_64  $distrib_dir  sparrow-distrib-$SPARROW_BUILD_NUM-x64-${BUILD_MODE}
 if [ $? -ne 0 ]; then
 	echo Failed to generate distribution package.
 	exit 1
 fi
 
+echo `date +"%x %X"` "Generating the MySQL API package which includes the header files and the library."
 generate_mysqlapi_pack  $build_dir_arch  $build_dir_arch/_CPack_Packages/Linux/TGZ/mysql-$MYSQL_TAG-linux-x86_64  $distrib_dir  mysqlapi-$SPARROW_BUILD_NUM-x64-${BUILD_MODE}
 if [ $? -ne 0 ]; then
 	echo Failed to generate MySQL API package.
 	exit 1
 fi
 
+echo `date +"%x %X"` "Generating the Sparrow API package which includes the header files and the library."
 generate_sparrowapi_pack  $build_dir_arch  $build_dir_arch/_CPack_Packages/Linux/TGZ/mysql-$MYSQL_TAG-linux-x86_64  $distrib_dir  sparrowapi-$SPARROW_BUILD_NUM-x64-${BUILD_MODE}
 if [ $? -ne 0 ]; then
 	echo Failed to generate Sparrow API package.
