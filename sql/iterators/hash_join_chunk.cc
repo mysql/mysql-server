e11e802b851e80f6ad74ce76eb5f403a6a86476c
Sveinung Olsen <sveinung.ols@gmail.com>
2026-02-11 13:26:07 +0100

    Refactor of m_bytes_written calculation, used to calculate bytes_used for hash join.

--- sql/iterators/hash_join_chunk.cc
+++ sql/iterators/hash_join_chunk.cc
@@ -133,17 +133,13 @@ bool HashJoinChunk::WriteRowToChunk(String *buffer, bool matched,
     return true;
   }
 
-  size_t local_bytes = 0;
-
   if (m_uses_match_flags) {
-    local_bytes += sizeof(matched);
     if (my_b_write(&m_file, pointer_cast<const uchar *>(&matched),
                    sizeof(matched)) != 0) {
       my_error(ER_TEMP_FILE_WRITE_FAILURE, MYF(0));
       return true;
     }
   } else if (set_index != std::numeric_limits<size_t>::max()) {
-    local_bytes += sizeof(set_index);
     if (my_b_write(&m_file, pointer_cast<const uchar *>(&set_index),
                    sizeof(set_index)) != 0) {
       my_error(ER_TEMP_FILE_WRITE_FAILURE, MYF(0));
@@ -165,12 +161,8 @@ bool HashJoinChunk::WriteRowToChunk(String *buffer, bool matched,
     my_error(ER_TEMP_FILE_WRITE_FAILURE, MYF(0));
     return true;
   }
-
-  local_bytes += sizeof(data_length);
-  local_bytes += data_length;
   
-  m_bytes_written += local_bytes;
-
+  m_bytes_written += data_length;
   m_num_rows++;
   return false;
 }
